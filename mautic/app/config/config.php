408ccaa5e42140ffd3b7c57d16a29a2a6adf3b86
Alan Hartless <alan@hartlessbydesign.com>
2014-03-17 10:30:56 -0500

    Implemented Assetic for asset management

--- mautic/app/config/config.php
+++ mautic/app/config/config.php
@@ -1,7 +1,16 @@
 <?php
+//Note Mautic specific bundles so they can be applied as needed without having to specify them individually
+$mauticbundles = array_filter(
+    $container->getParameter('kernel.bundles'),
+    function($v) {
+        return ((strpos($v, "Mautic") !== false) ? 1 : 0);
+    }
+);
+$container->setParameter("mautic.bundles", $mauticbundles);
 
 $loader->import("parameters.php");
 $loader->import("security.php");
+$loader->import("services.php");
 
 $container->loadFromExtension("framework", array(
     "secret"               => "%secret%",
@@ -18,6 +27,10 @@ $container->loadFromExtension("framework", array(
         "engines" => array(
             "twig",
             "php"
+        ),
+        "assets_base_urls" => array(
+            "http" => array("media/"),
+            "ssl"  => array("media/")
         )
     ),
     "default_locale"       => "%locale%",
@@ -34,17 +47,6 @@ $container->loadFromExtension("framework", array(
 $container->loadFromExtension("twig", array(
     "debug"            => "%kernel.debug%",
     "strict_variables" => "%kernel.debug%",
-));
-
-
-//Assetic Configuration
-$container->loadFromExtension("assetic", array(
-    "debug"          => "%kernel.debug%",
-    "use_controller" => false,
-    "bundles"        => array(),
-    "filters"        => array(
-        "cssrewrite" => null
-    )
 ));
 
 //Doctrine Configuration
@@ -76,5 +78,69 @@ $container->loadFromExtension("swiftmailer", array(
     "password"  => "%mailer_password%",
     "spool"     => array(
         "type" => "memory"
+    )
+));
+
+
+//Assetic Configuration
+
+//Assetic does not allow variables when loading resources in templates because it renders the media at compilation time and thus
+//the appropriate variables are not populated.  In order to not have to manually add each bundles' media files to
+//MauticBaseBundle's base.html.php file, we are doing the following.
+
+//For production, you must dump the assets via php app/console assetic:dump --env=prod
+
+$css = array();
+$js  = array();
+foreach ($mauticbundles as $bundle => $namespace) {
+    //parse the namespace into a filepath
+    $namespaceParts = explode("\\", $namespace);
+    $bundleDir      = __DIR__ . "/../../src/" . $namespaceParts[0] . "/" . $namespaceParts[1];
+
+    //define the function for use with CSS and JS files
+    $getFiles = function ($type) use ($bundleDir, &$css, &$js) {
+        $typeDir = "$bundleDir/Resources/public/$type/";
+
+        //get files within the directory
+        $iterator = new FilesystemIterator($typeDir);
+        //filter out inappropriate files
+        $filter   = new RegexIterator($iterator, "/.$type$/");
+        if (iterator_count($filter)) {
+            foreach ($filter as $file) {
+                //add the file to be loaded
+                ${$type}[] = $file->getPathname();
+            }
+        }
+    };
+
+    $getFiles("css");
+    $getFiles("js");
+}
+
+$container->loadFromExtension("assetic", array(
+    "debug"          => "%kernel.debug%",
+    "use_controller" => "%kernel.debug%",
+    "read_from"      => "%kernel.root_dir%/../../media/",
+    "write_to"       => "%kernel.root_dir%/../../media/",
+    "filters"        => array(
+        "cssrewrite" => array(
+            "apply_to" => "\\.css$"
+        )
+    ),
+    "assets"         => array(
+        "mautic_stylesheets" => array(
+            "inputs"  => $css,
+            "options" => array(
+                "combine" => true,
+                //"output"  => 'css/mautic.css'
+            )
+        ),
+        "mautic_javascripts" => array(
+            "inputs"  => $js,
+            "options" => array(
+                "combine" => true,
+                //"output"  => 'js/mautic.js'
+            )
+        )
     )
 ));
