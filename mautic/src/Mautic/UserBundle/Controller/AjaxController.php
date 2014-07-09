d493ffcd1d6efc9078abb93a24c9adaa3f9aad3f
Alan Hartless <alan@hartlessbydesign.com>
2014-07-09 17:11:41 -0500

    Fixed bad namespace in user ajax controller

--- mautic/src/Mautic/UserBundle/Controller/AjaxController.php
+++ mautic/src/Mautic/UserBundle/Controller/AjaxController.php
@@ -7,7 +7,7 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\FormBundle\Controller;
+namespace Mautic\UserBundle\Controller;
 
 use Mautic\CoreBundle\Controller\AjaxController as CommonAjaxController;
 use Mautic\CoreBundle\Helper\InputHelper;
