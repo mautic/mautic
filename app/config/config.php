bc0908391e6ca7ca4351843b6524f651bbeccb12
Alan Hartless <alan@Alans-Mac.local>
2014-07-19 02:37:46 -0500

    Fixed issue where other bundle's css/js were added to combined file prior to core's

--- app/config/config.php
+++ app/config/config.php
@@ -14,12 +14,21 @@ $buildBundles = function($namespace, $bundle) use ($container) {
     return false;
 };
 $symfonyBundles = $container->getParameter('kernel.bundles');
-$mauticBundles  = array_values(array_filter(
+$mauticBundles  = array_filter(
     array_map($buildBundles , $symfonyBundles, array_keys($symfonyBundles)),
     function ($v) { return (!empty($v)); }
-));
+);
 
-$container->setParameter('mautic.bundles', $mauticBundles);
+$setBundles = array();
+foreach ($mauticBundles as $bundle) {
+    $setBundles[$bundle['base']] = $bundle;
+}
+//Core bundle should be first for assetic generation
+$coreBundle = $setBundles['Core'];
+unset($setBundles['Core']);
+$setBundles = array_merge(array('Core' => $coreBundle), $setBundles);
+
+$container->setParameter('mautic.bundles', $setBundles);
 $loader->import('parameters.php');
 $container->loadFromExtension('mautic_core');
 
@@ -112,7 +121,6 @@ $container->loadFromExtension('swiftmailer', array(
     )
 ));
 
-
 //Also Assetic does not allow variables when loading resources in templates because it renders the media at compilation time and thus
 //the appropriate variables are not populated.  In order to not have to manually add each bundles' media files to
 //MauticBaseBundle's base.html.php file, we are doing the following which works because of Symfony's caching.
@@ -122,7 +130,7 @@ $container->loadFromExtension('swiftmailer', array(
 $css    = array();
 $js     = array();
 
-foreach ($mauticBundles as $bundle => $details) {
+foreach ($setBundles as $bundle => $details) {
    $bundleDir = $details['directory'];
     //define the function for use with CSS and JS files
     $getFiles = function ($type) use ($bundleDir, &$css, &$js) {
