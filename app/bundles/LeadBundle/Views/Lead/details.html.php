7c2135a0223931d97c2e5fc4d898d6a0a88db74a
David Hurley <david@websparkinc.com>
2014-07-26 00:50:48 -0400

    UX on lead detail view

--- app/bundles/LeadBundle/Views/Lead/details.html.php
+++ app/bundles/LeadBundle/Views/Lead/details.html.php
@@ -16,7 +16,9 @@
                 <?php  echo $fields['core']['position']['value']; ?>
             <?php endif; ?>
             at
-            <a href="#">Company Name</a></h4>
+            <?php if(isset($fields['core']['company']['value'])): 
+                echo $fields['core']['company']['value'];
+            endif; ?></h4>
     </div>
     <div class="col-sm-2">
         <div class="alert alert-success text-center pa15">
@@ -41,45 +43,59 @@
         </div>
     </div>
     <div class="panel-body">
-        <div class="col-sm-12">
-            <?php /** if ($lead->getOwner()): ?>
-                <div class="row">
-                    <div class="col-xs-3 field-label">
-                        <?php echo $view['translator']->trans('mautic.lead.lead.field.owner'); ?>
+        <div class="tab-content">
+            <?php $count = 0; ?>
+            <?php foreach ($groups as $key => $group): ?>
+            <div class="tab-pane<?php echo ($count === 0) ? ' active': ''; ?>" id="<?php echo $group; ?>">
+                <?php /** if ($lead->getOwner()): ?>
+                    <div class="row">
+                        <div class="col-xs-3 field-label">
+                            <?php echo $view['translator']->trans('mautic.lead.lead.field.owner'); ?>
+                        </div>
+                        <div class="col-xs-9 field-value">
+                            <a href="<?php echo $view['router']->generate('mautic_user_action', array(
+                                'objectAction' => 'contact',
+                                'objectId'     => $lead->getOwner()->getId(),
+                                'entity'       => 'lead',
+                                'id'           => $lead->getId(),
+                                'returnUrl'    => $view['router']->generate('mautic_lead_action', array(
+                                    'objectAction' => 'view',
+                                    'objectId'     => $lead->getId()
+                                ))
+                                )); ?>">
+                            <?php echo $lead->getOwner()->getName(); ?>
+                            </a>
+                        </div>
                     </div>
-                    <div class="col-xs-9 field-value">
-                        <a href="<?php echo $view['router']->generate('mautic_user_action', array(
-                            'objectAction' => 'contact',
-                            'objectId'     => $lead->getOwner()->getId(),
-                            'entity'       => 'lead',
-                            'id'           => $lead->getId(),
-                            'returnUrl'    => $view['router']->generate('mautic_lead_action', array(
-                                'objectAction' => 'view',
-                                'objectId'     => $lead->getId()
-                            ))
-                            )); ?>">
-                        <?php echo $lead->getOwner()->getName(); ?>
-                        </a>
-                    </div>
+                <?php endif; **/ ?>
+                <div class="col-sm-6">
+                <?php $total = count($fields[$group]); 
+                    $current = 1;
+                ?>
+                <?php foreach ($fields[$group] as $field): ?>
+                    <?php if($current == $total/2): ?>
+                        </div>
+                        <div class="col-sm-6">
+                    <?php endif; ?>
+                    <?php if (empty($field['value'])) continue; ?>
+                        <strong>
+                            <?php echo $field['label']; ?>
+                        </strong><br />
+                        <div>
+                            <?php echo $view->render('MauticLeadBundle:Lead:info_value.html.php', array(
+                                'value'             => $field['value'],
+                                'name'              => $field['alias'],
+                                'type'              => $field['type'],
+                                'dateFormats'       => $dateFormats,
+                                'socialProfileUrls' => $socialProfileUrls
+                            )); ?>
+                        </div>
+                    <?php $current++; ?>
+                <?php endforeach; ?>
                 </div>
-            <?php endif; **/ ?>
-            <?php foreach ($fields['core'] as $field): ?>
-                <?php if (empty($field['value'])) continue; ?>
-                <div class="row">
-                    <strong>
-                        <?php echo $field['label']; ?>
-                    </strong><br />
-                    <div>
-                        <?php echo $view->render('MauticLeadBundle:Lead:info_value.html.php', array(
-                            'value'             => $field['value'],
-                            'name'              => $field['alias'],
-                            'type'              => $field['type'],
-                            'dateFormats'       => $dateFormats,
-                            'socialProfileUrls' => $socialProfileUrls
-                        )); ?>
-                    </div>
-                </div>
+            </div>
+            <?php $count++; ?>
             <?php endforeach; ?>
         </div>
-        </div>
     </div>
+</div>
