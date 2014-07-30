5ac039c544c7dc2982c1cd0af5c39759850a92b9
David Hurley <david@websparkinc.com>
2014-07-30 01:38:26 -0400

    Added collapsible social panels

--- app/bundles/LeadBundle/Views/Lead/social.html.php
+++ app/bundles/LeadBundle/Views/Lead/social.html.php
@@ -29,70 +29,72 @@
                         <!--/ option -->
                     </div>
                 </div>
-                <div class="panel-body">
-                    <?php $col = 12; ?>
-                    <?php if (!empty($details['profile']['profileImage'])): ?>
-                    <div class="col-sm-3">
-                        <img class="img img-responsive img-thumbnail" src="<?php echo $details['profile']['profileImage']; ?>" />
-                    </div>
-                    <?php
-                        unset($details['profile']['profileImage']);
-                        $col = 9; ?>
-                    <?php endif; ?>
-                    <div class="col-sm-<?php echo $col; ?>">
-                        <ul class="nav nav-tabs" role="tablist">
-                            <?php if (!empty($details['profile']) && !empty($details['activity'])): ?>
-                            <?php $active = 'profile'; ?>
-                            <li class="active">
-                                <a href="#<?php echo $network; ?>Profile" role="tab" data-toggle="tab">
-                                    <?php echo $view['translator']->trans('mautic.lead.lead.tab.socialprofile'); ?>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#<?php echo $network; ?>Activity" role="tab" data-toggle="tab">
-                                    <?php echo $view['translator']->trans('mautic.lead.lead.tab.socialactivity'); ?>
-                                </a>
-                            </li>
-                            <?php elseif (!empty($details['profile'])): ?>
-                            <?php $active = 'profile'; ?>
-                            <li class="active">
-                                <a href="#<?php echo $network; ?>Profile" role="tab" data-toggle="tab">
-                                    <?php echo $view['translator']->trans('mautic.lead.lead.tab.socialprofile'); ?>
-                                </a>
-                            </li>
-                            <?php elseif (!empty($details['activity'])): ?>
-                            <?php $active = 'activity'; ?>
-                            <li class="active">
-                                <a href="#<?php echo $network; ?>Activity" role="tab" data-toggle="tab">
-                                    <?php echo $view['translator']->trans('mautic.lead.lead.tab.socialactivity'); ?>
-                                </a>
-                            </li>
-                            <?php endif; ?>
-                        </ul>
+                <div class="panel-collapse pull out">
+                    <div class="panel-body">
+                        <?php $col = 12; ?>
+                        <?php if (!empty($details['profile']['profileImage'])): ?>
+                        <div class="col-sm-3">
+                            <img class="img img-responsive img-thumbnail" src="<?php echo $details['profile']['profileImage']; ?>" />
+                        </div>
+                        <?php
+                            unset($details['profile']['profileImage']);
+                            $col = 9; ?>
+                        <?php endif; ?>
+                        <div class="col-sm-<?php echo $col; ?>">
+                            <ul class="nav nav-tabs" role="tablist">
+                                <?php if (!empty($details['profile']) && !empty($details['activity'])): ?>
+                                <?php $active = 'profile'; ?>
+                                <li class="active">
+                                    <a href="#<?php echo $network; ?>Profile" role="tab" data-toggle="tab">
+                                        <?php echo $view['translator']->trans('mautic.lead.lead.tab.socialprofile'); ?>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#<?php echo $network; ?>Activity" role="tab" data-toggle="tab">
+                                        <?php echo $view['translator']->trans('mautic.lead.lead.tab.socialactivity'); ?>
+                                    </a>
+                                </li>
+                                <?php elseif (!empty($details['profile'])): ?>
+                                <?php $active = 'profile'; ?>
+                                <li class="active">
+                                    <a href="#<?php echo $network; ?>Profile" role="tab" data-toggle="tab">
+                                        <?php echo $view['translator']->trans('mautic.lead.lead.tab.socialprofile'); ?>
+                                    </a>
+                                </li>
+                                <?php elseif (!empty($details['activity'])): ?>
+                                <?php $active = 'activity'; ?>
+                                <li class="active">
+                                    <a href="#<?php echo $network; ?>Activity" role="tab" data-toggle="tab">
+                                        <?php echo $view['translator']->trans('mautic.lead.lead.tab.socialactivity'); ?>
+                                    </a>
+                                </li>
+                                <?php endif; ?>
+                            </ul>
 
-                        <div class="tab-content">
-                            <?php if (!empty($details['profile'])): ?>
-                            <div class="tab-pane<?php echo ($active == 'profile') ? ' active': ''; ?>" id="<?php echo $network; ?>Profile">
-                                <?php echo $view->render('MauticLeadBundle:Social/' . $network . ':profile.html.php', array(
-                                    'lead'              => $lead,
-                                    'profile'           => $details['profile'],
-                                    'dateFormats'       => $dateFormats,
-                                    'network'           => $network,
-                                    'socialProfileUrls' => $socialProfileUrls
-                                )); ?>
-                            </div>
-                            <?php endif; ?>
+                            <div class="tab-content">
+                                <?php if (!empty($details['profile'])): ?>
+                                <div class="tab-pane<?php echo ($active == 'profile') ? ' active': ''; ?>" id="<?php echo $network; ?>Profile">
+                                    <?php echo $view->render('MauticLeadBundle:Social/' . $network . ':profile.html.php', array(
+                                        'lead'              => $lead,
+                                        'profile'           => $details['profile'],
+                                        'dateFormats'       => $dateFormats,
+                                        'network'           => $network,
+                                        'socialProfileUrls' => $socialProfileUrls
+                                    )); ?>
+                                </div>
+                                <?php endif; ?>
 
-                            <?php if (!empty($details['activity'])): ?>
-                            <div class="tab-pane<?php echo ($active == 'activity') ? ' active': ''; ?>" id="<?php echo $network; ?>Activity">
-                                <?php echo $view->render('MauticLeadBundle:Social/' . $network . ':activity.html.php', array(
-                                        'lead'        => $lead,
-                                        'activity'    => $details['activity'],
-                                        'dateFormats' => $dateFormats,
-                                        'network'     => $network
-                                )); ?>
+                                <?php if (!empty($details['activity'])): ?>
+                                <div class="tab-pane<?php echo ($active == 'activity') ? ' active': ''; ?>" id="<?php echo $network; ?>Activity">
+                                    <?php echo $view->render('MauticLeadBundle:Social/' . $network . ':activity.html.php', array(
+                                            'lead'        => $lead,
+                                            'activity'    => $details['activity'],
+                                            'dateFormats' => $dateFormats,
+                                            'network'     => $network
+                                    )); ?>
+                                </div>
+                                <?php endif; ?>
                             </div>
-                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
