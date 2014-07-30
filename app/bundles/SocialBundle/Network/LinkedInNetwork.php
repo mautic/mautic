96c1666e26cc0fa10290ced72acfaf14bec4b8dd
Alan Hartless <alan@Alans-Mac.local>
2014-07-30 16:10:55 -0500

    fixed linkedin file name

---
+++ app/bundles/SocialBundle/Network/LinkedInNetwork.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic, NP. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.com
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\SocialBundle\Network;
+
+class LinkedInNetwork extends AbstractNetwork
+{
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'LinkedIn';
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return array
+     */
+    public function getSupportedFeatures()
+    {
+        return array(
+            'share_button'
+
+        );
+    }
+
+    public function getIdentifierField()
+    {
+        return false;
+    }
+}
