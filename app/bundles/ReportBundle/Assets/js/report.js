90d3937090a6cf4384142865c9e750c696c354b7
Michael Babker <mbabker@flbab.com>
2014-09-02 12:36:07 -0400

    Add ability to move columns between select lists and order them

--- app/bundles/ReportBundle/Assets/js/report.js
+++ app/bundles/ReportBundle/Assets/js/report.js
@@ -11,4 +11,20 @@ Mautic.preprocessSaveReportForm = function(form) {
 	mQuery(selectedColumns).find('option').each(function($this) {
 		mQuery(this).attr('selected', 'selected');
 	});
-}
+};
+
+Mautic.moveReportColumns = function(fromSelect, toSelect) {
+	mQuery('#' + fromSelect + ' option:selected').remove().appendTo('#' + toSelect);
+
+	mQuery('#' + toSelect).find('option').each(function($this) {
+		mQuery(this).prop('selected', false);
+	});
+};
+
+Mautic.reorderColumns = function(select, direction) {
+	var options = mQuery('#' + select + ' option:selected');
+
+	if (options.length) {
+		(direction == 'up') ? options.first().prev().before(options) : options.last().next().after(options);
+	}
+};
--- app/bundles/ReportBundle/Views/Form/column_selector_row.html.php
+++ app/bundles/ReportBundle/Views/Form/column_selector_row.html.php
@@ -17,16 +17,20 @@ $labelClass = (empty($form->vars['label_attr']['class'])) ? 'control-label' : $f
                     <?php } ?>
                 </select>
             </div>
-            <div class="col-xs-2">
-                <div class="text-center" style="margin-bottom: 10px; padding-top: 18px;">
-                    <button type="button" id="column-move-left" class="btn btn-sm btn-default"><i class="fa fa-caret-square-o-left"></i></button>
+            <div class="columnSelectorButtons col-xs-1">
+                <div class="text-center">
+                    <button type="button" id="column-move-left" class="btn btn-sm btn-default" onclick="Mautic.moveReportColumns('<?php echo $form->vars['id']; ?>', '<?php echo $form->vars['id'] . '_available'; ?>');"><i class="fa fa-caret-square-o-left"></i></button>
                 </div>
                 <div class="text-center">
-                    <button type="button" id="column-move-right" class="btn btn-sm btn-default"><i class="fa fa-caret-square-o-right"></i></button>
+                    <button type="button" id="column-move-right" class="btn btn-sm btn-default" onclick="Mautic.moveReportColumns('<?php echo $form->vars['id'] . '_available'; ?>', '<?php echo $form->vars['id']; ?>');"><i class="fa fa-caret-square-o-right"></i></button>
                 </div>
             </div>
             <div class="choice-wrapper col-xs-5">
                 <label class="<?php echo $labelClass; ?>" for="<?php echo $form->vars['id']; ?>"><?php echo $view['translator']->trans('mautic.report.report.label.selectedcolumns'); ?></label>
+                <span data-toggle="tooltip" data-container="body" data-placement="top"
+                      data-original-title="<?php echo $view['translator']->trans('mautic.report.report.tooltip.selected.columns'); ?>">
+                    <i class="fa fa-question-circle"></i>
+                </span>
                 <select id="<?php echo $form->vars['id']; ?>" name="<?php echo $form->vars['full_name']; ?>[]" class="<?php echo $form->vars['attr']['class'] ?>"<?php if ($form->vars['multiple']) echo ' multiple="multiple"'; ?> size="<?php echo $form->vars['attr']['size'] ?>">
                     <?php foreach ($form->vars['choices'] as $choice) { ?>
                     <?php if ($selectedColumns[$choice->value]) { ?>
@@ -34,6 +38,14 @@ $labelClass = (empty($form->vars['label_attr']['class'])) ? 'control-label' : $f
                     <?php } ?>
                     <?php } ?>
                 </select>
+            </div>
+            <div class="columnSelectorButtons col-xs-1">
+                <div class="text-center">
+                    <button type="button" id="column-move-left" class="btn btn-sm btn-default" onclick="Mautic.reorderColumns('<?php echo $form->vars['id']; ?>', 'up');"><i class="fa fa-caret-square-o-up"></i></button>
+                </div>
+                <div class="text-center">
+                    <button type="button" id="column-move-right" class="btn btn-sm btn-default" onclick="Mautic.reorderColumns('<?php echo $form->vars['id']; ?>', 'down');"><i class="fa fa-caret-square-o-down"></i></button>
+                </div>
             </div>
         </div>
     </div>
