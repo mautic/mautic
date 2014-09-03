a827070a9dcaabc321767db62fa1988789a217f9
Michael Babker <mbabker@flbab.com>
2014-09-03 16:17:19 -0400

    Add a REPORT_ON_BUILD event and ReportBuilderEvent class

--- app/bundles/ReportBundle/ReportEvents.php
+++ app/bundles/ReportBundle/ReportEvents.php
@@ -26,7 +26,7 @@ final class ReportEvents
      *
      * @var string
      */
-    const REPORT_PRE_SAVE   = 'mautic.report_pre_save';
+    const REPORT_PRE_SAVE = 'mautic.report_pre_save';
 
     /**
      * The mautic.report_post_save event is thrown right after a report is persisted.
@@ -36,7 +36,7 @@ final class ReportEvents
      *
      * @var string
      */
-    const REPORT_POST_SAVE   = 'mautic.report_post_save';
+    const REPORT_POST_SAVE = 'mautic.report_post_save';
 
     /**
      * The mautic.report_pre_delete event is thrown prior to when a report is deleted.
@@ -46,8 +46,7 @@ final class ReportEvents
      *
      * @var string
      */
-    const REPORT_PRE_DELETE   = 'mautic.report_pre_delete';
-
+    const REPORT_PRE_DELETE = 'mautic.report_pre_delete';
 
     /**
      * The mautic.report_post_delete event is thrown after a report is deleted.
@@ -57,5 +56,16 @@ final class ReportEvents
      *
      * @var string
      */
-    const REPORT_POST_DELETE   = 'mautic.report_post_delete';
+    const REPORT_POST_DELETE = 'mautic.report_post_delete';
+
+    /**
+     * The mautic.report_on_build event is thrown before displaying the report builder form to allow
+     * bundles to specify report sources and columns
+     *
+     * The event listener receives a
+     * Mautic\ReportBundle\Event\ReportBuilderEvent instance.
+     *
+     * @var string
+     */
+    const REPORT_ON_BUILD = 'mautic.report_on_build';
 }
