57ced986eef0586e358716237a0a570c2b347d94
David Hurley <david@websparkinc.com>
2014-09-25 15:46:24 -0400

    Beginning modifications to email builder layout

--- app/bundles/EmailBundle/Views/builder.html.php
+++ app/bundles/EmailBundle/Views/builder.html.php
@@ -52,9 +52,9 @@ CUSTOM;
 $view['assets']->addScriptDeclaration($custom);
 
 $css = <<<CSS
-.mautic-editable { min-height: 75px; width: 100%; border: dashed 1px #000; margin-top: 3px; margin-bottom: 3px; }
+.mautic-editable { min-height: 75px; width: 100%; border: solid 1px #d5d4d4; margin-top: 3px; margin-bottom: 3px; }
 .mautic-content-placeholder { height: 100%; width: 100%; text-align: center; margin-top: 25px; }
-.mautic-editable.over-droppable { border: dashed 1px #ED9C28; }
+.mautic-editable.over-droppable { border: solid 1px #d5d4d4; }
 CSS;
 $view['assets']->addStyleDeclaration($css);
 
