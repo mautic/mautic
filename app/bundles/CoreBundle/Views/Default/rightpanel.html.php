69942d1e7e81e530de38824c123176f9c30451e0
Alan Hartless <alan@Alans-Mac.local>
2014-09-25 23:34:29 -0500

    Reactivated chat <--needs work

--- app/bundles/CoreBundle/Views/Default/rightpanel.html.php
+++ app/bundles/CoreBundle/Views/Default/rightpanel.html.php
@@ -14,7 +14,7 @@ $value = $app->getSession()->get('mautic.global_search');
 
 <!-- start: sidebar-header -->
 <div class="sidebar-header">
-    
+
 </div>
 <!--/ end: sidebar-header -->
 
@@ -22,61 +22,42 @@ $value = $app->getSession()->get('mautic.global_search');
 <div class="sidebar-content">
     <!-- scroll-content -->
     <div class="scroll-content slimscroll">
-        <div class="pa-md">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="offcanvas-container" data-toggle="offcanvas" data-options="{&quot;openerClass&quot;:&quot;offcanvas-opener&quot;, &quot;closerClass&quot;:&quot;offcanvas-closer&quot;}">
+            <div class="offcanvas-wrapper pa-md">
+                <div class="offcanvas-left">
+
+                </div>
+
+                <div class="offcanvas-content">
+                    <div class="content slimscroll">
+                        <?php echo $view['actions']->render(new ControllerReference('MauticChatBundle:Default:index')); ?>
+                        <div class="scrollbar" style="width: 8px; position: absolute; top: 0px; opacity: 0.4; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; z-index: 99; right: 0px; height: 597px; display: none; background: rgb(0, 0, 0);"></div>
+                        <div class="scrollrail" style="width: 8px; height: 100%; position: absolute; top: 0px; display: block; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; opacity: 0.2; z-index: 90; right: 0px; background: rgb(51, 51, 51);"></div>
+                    </div>
+                </div>
+
+                <div class="offcanvas-right has-footer">
+                    <div class="header pl0 pr0">
+                        <ul class="list-table nm">
+                            <li style="width:50px;">
+                                <a href="javascript:void(0);" onclick="Mautic.updateChatList(true);" class="btn btn-link text-default offcanvas-closer"><i class="fa fa-lg fa-fw fa-arrow-circle-left"></i></a>
+                            </li>
+                            <li class="text-center">
+                                <h5 class="semibold nm">
+                                    <p class="nm" id="ChatHeader"></p>
+                                    <small id="ChatSubHeader"></small>
+                                </h5>
+                            </li>
+                            <li style="width:50px;" class="text-right"></li>
+                        </ul>
+                    </div>
+                    <div class="content slimscroll" id="ChatConversation">
+                        <?php echo $view['actions']->render(new ControllerReference('MauticChatBundle:Default:dm')); ?>
+                    </div>
+
+                    <?php echo $view->render('MauticChatBundle:User:footer.html.php'); ?>
+                </div>
+            </div>
         </div>
     </div>
     <!--/ scroll-content -->
