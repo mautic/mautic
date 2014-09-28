2a3643ae23037dda8a39f619f90e84ec0c1453f0
John Pozy <pampersdry@gmail.com>
2014-09-28 23:23:01 +0800

    Update sidebar right chat UI <-- need to integrate with Mautic ChatBundle

--- app/bundles/CoreBundle/Views/Default/rightpanel.html.php
+++ app/bundles/CoreBundle/Views/Default/rightpanel.html.php
@@ -12,54 +12,281 @@ use Symfony\Component\HttpKernel\Controller\ControllerReference;
 $value = $app->getSession()->get('mautic.global_search');
 ?>
 
-<!-- start: sidebar-header -->
-<div class="sidebar-header">
-
-</div>
-<!--/ end: sidebar-header -->
-
-<!-- start: sidebar-content -->
-<div class="sidebar-content">
-    <!-- scroll-content -->
-    <div class="scroll-content slimscroll">
-        <div class="offcanvas-container" data-toggle="offcanvas" data-options="{&quot;openerClass&quot;:&quot;offcanvas-opener&quot;, &quot;closerClass&quot;:&quot;offcanvas-closer&quot;}">
-            <div class="offcanvas-wrapper pa-md">
-                <div class="offcanvas-left">
-
+<div class="offcanvas-container" data-toggle="offcanvas" data-options='{"openerClass":"offcanvas-opener", "closerClass":"offcanvas-closer"}'>
+    <!-- START Wrapper -->
+    <div class="offcanvas-wrapper">
+        <!--
+        Offcanvas Left:
+        for this container, we can put the add channel
+        form
+        -->
+        <form class="offcanvas-left" action="">
+            <!-- start: sidebar header -->
+            <div class="sidebar-header box-layout">
+                <div class="col-xs-6 va-m">
+                    <a href="javascript:void(0);" class="offcanvas-closer"><span class="fa fa-arrow-left fs-16"></span></a>
                 </div>
-
-                <div class="offcanvas-content">
-                    <div class="content slimscroll">
-                        <?php echo $view['actions']->render(new ControllerReference('MauticChatBundle:Default:index')); ?>
-                        <div class="scrollbar" style="width: 8px; position: absolute; top: 0px; opacity: 0.4; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; z-index: 99; right: 0px; height: 597px; display: none; background: rgb(0, 0, 0);"></div>
-                        <div class="scrollrail" style="width: 8px; height: 100%; position: absolute; top: 0px; display: block; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; opacity: 0.2; z-index: 90; right: 0px; background: rgb(51, 51, 51);"></div>
-                    </div>
-                </div>
-
-                <div class="offcanvas-right has-footer">
-                    <div class="header pl0 pr0">
-                        <ul class="list-table nm">
-                            <li style="width:50px;">
-                                <a href="javascript:void(0);" onclick="Mautic.updateChatList(true);" class="btn btn-link text-default offcanvas-closer"><i class="fa fa-lg fa-fw fa-arrow-circle-left"></i></a>
-                            </li>
-                            <li class="text-center">
-                                <h5 class="semibold nm">
-                                    <p class="nm" id="ChatHeader"></p>
-                                    <small id="ChatSubHeader"></small>
-                                </h5>
-                            </li>
-                            <li style="width:50px;" class="text-right"></li>
-                        </ul>
-                    </div>
-                    <div class="content slimscroll" id="ChatConversation">
-                        <?php echo $view['actions']->render(new ControllerReference('MauticChatBundle:Default:dm')); ?>
-                    </div>
-
-                    <?php echo $view->render('MauticChatBundle:User:footer.html.php'); ?>
+                <div class="col-xs-6 va-m text-right">
+                    <a href="javascript:void(0);"><span class="fa fa-info fs-16"></span></a>
                 </div>
             </div>
+            <!--/ end: sidebar header -->
+
+            <!-- start: sidebar footer -->
+            <div class="sidebar-footer box-layout">
+                <div class="col-xs-6 va-m">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-save mr-xs"></i> Save
+                    </button>
+                </div>
+                <div class="col-xs-6 va-m clearfix">
+                    <button type="reset" class="btn btn-default pull-right">
+                        <i class="fa fa-times text-danger mr-xs"></i> Cancel
+                    </button>
+                </div>
+            </div>
+            <!--/ end: sidebar footer -->
+
+            <!-- start: sidebar content -->
+            <div class="sidebar-content">
+                <!-- scroll-content -->
+                <div class="scroll-content slimscroll">
+                    <div class="pa-md">
+                        <div class="form-group">
+                            <label for="">Channel handle</label>
+                            <div class="input-group input-group">
+                                <span class="input-group-addon">#</span>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Short description</label>
+                            <textarea class="form-control" rows="3"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <!--/ scroll-content -->
+            </div>
+            <!--/ end: sidebar content -->
+        </form>
+        <!--/ Offcanvas Left -->
+
+        <!-- 
+        Offcanvas Main:
+        This will be default/initial view if sidebar right is in open
+        state. Put the chat list here.
+        -->
+        <div class="offcanvas-main">
+            <!-- start: sidebar header -->
+            <div class="sidebar-header box-layout">
+                <div class="col-xs-6 va-m">
+                    <h5 class="fw-sb">Channels</h5>
+                </div>
+                <div class="col-xs-6 va-m text-right">
+                    <!-- this will toggle offcanvas-left container-->
+                    <a href="javascript:void(0);" class="btn btn-primary offcanvas-opener offcanvas-open-ltr">Add</a>
+                </div>
+            </div>
+            <!--/ end: sidebar header -->
+
+            <!-- start: sidebar content -->
+            <div class="sidebar-content">
+                <!-- scroll-content -->
+                <div class="scroll-content slimscroll">
+                    <!-- put the chat list here -->
+                    <ul class="media-list media-list-contact">
+                        <li class="media-heading">CONTACT GROUP #1</li>
+                        <li class="media">
+                            <a href="javascript:void(0);" class="clearfix offcanvas-opener offcanvas-open-rtl">
+                                <span class="pull-left img-wrapper img-rounded mr-sm">
+                                    <img class="media-object" src="https://s3.amazonaws.com/uifaces/faces/twitter/jennyshen/128.jpg" style="width:36px">
+                                </span>
+                                <span class="media-body">
+                                    <span class="media-heading mb-0 text-white dark-sm">
+                                        <span class="bullet bullet-success mr-xs"></span> Jenny Sheng
+                                    </span>
+                                    <span class="meta text-white dark-lg">Online</span>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="media">
+                            <a href="javascript:void(0);" class="clearfix offcanvas-opener offcanvas-open-rtl">
+                                <span class="pull-left img-wrapper img-rounded mr-sm">
+                                    <img class="media-object" src="https://s3.amazonaws.com/uifaces/faces/twitter/ademilter/128.jpg" style="width:36px">
+                                </span>
+                                <span class="media-body">
+                                    <span class="media-heading mb-0 text-white dark-sm">
+                                        <span class="bullet bullet-default mr-xs"></span> Ade Milter
+                                    </span>
+                                    <span class="meta text-white dark-lg">Offline</span>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul class="media-list media-list-contact">
+                        <li class="media-heading">CONTACT GROUP #2</li>
+                        <li class="media">
+                            <a href="javascript:void(0);" class="clearfix offcanvas-opener offcanvas-open-rtl">
+                                <span class="pull-left img-wrapper img-rounded mr-sm">
+                                    <img class="media-object" src="https://s3.amazonaws.com/uifaces/faces/twitter/nilo/128.jpg" style="width:36px">
+                                </span>
+                                <span class="media-body">
+                                    <span class="media-heading mb-0 text-white dark-sm">
+                                        <span class="bullet bullet-warning mr-xs"></span> Nilo
+                                    </span>
+                                    <span class="meta text-white dark-lg">Away</span>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="media">
+                            <a href="javascript:void(0);" class="clearfix offcanvas-opener offcanvas-open-rtl">
+                                <span class="pull-left img-wrapper img-rounded mr-sm">
+                                    <img class="media-object" src="https://s3.amazonaws.com/uifaces/faces/twitter/raquelromanp/128.jpg" style="width:36px">
+                                </span>
+                                <span class="media-body">
+                                    <span class="media-heading mb-0 text-white dark-sm">
+                                        <span class="bullet bullet-danger mr-xs"></span> Raquel Romanp
+                                    </span>
+                                    <span class="meta text-white dark-lg">Don't Disturbe</span>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!--/ scroll-content -->
+            </div>
+            <!--/ end: sidebar content -->
         </div>
+        <!--/ Offcanvas Content -->
+
+        <!--
+        Offcanvas Right:
+        this will be the chat bubbles container. This container will be
+        trigger by chat list in `offcanvas-main`
+        -->
+        <div class="offcanvas-right">
+            <!-- start: sidebar header -->
+            <div class="sidebar-header box-layout">
+                <div class="col-xs-6 va-m">
+                    <a href="javascript:void(0);" class="offcanvas-closer"><span class="fa fa-arrow-left fs-16"></span></a>
+                </div>
+                <div class="col-xs-6 va-m text-right">
+                    <a href="javascript:void(0);"><span class="fa fa-info fs-16"></span></a>
+                </div>
+            </div>
+            <!--/ end: sidebar header -->
+
+            <!-- start: sidebar footer -->
+            <div class="sidebar-footer box-layout">
+                <div class="cell va-m">
+                    <div class="form-control-icon">
+                        <input type="text" class="form-control bg-transparent bdr-rds-0 bdr-w-0" placeholder="Type something...">
+                        <span class="the-icon fa fa-paper-plane text-white dark-sm"></span><!-- must below `form-control` -->
+                    </div>
+                </div>
+            </div>
+            <!--/ end: sidebar footer -->
+
+            <!-- start: sidebar content -->
+            <div class="sidebar-content">
+                <!-- scroll-content -->
+                <div class="scroll-content slimscroll">
+                    <!-- put the chat bubbles here -->
+                    <ul class="media-list media-list-bubble">
+                        <li class="media media-right">
+                            <a href="javascript:void(0);" class="media-object">
+                                <span class="img-wrapper img-rounded">
+                                    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/soyjavi/128.jpg" style="width:36px">
+                                </span>
+                            </a>
+                            <div class="media-body">
+                                <p class="media-text">eros non enim commodo hendrerit.</p>
+                                <span class="clearfix"></span>
+                                <p class="media-text">Suspendisse dui.</p>
+                                <span class="clearfix"></span>
+                                <p class="media-text">eu nulla at</p>
+                                <!-- meta -->
+                                <span class="clearfix"></span><!-- important: clearing floated media text -->
+                                <small class="text-white dark-lg">Sun, Mar 02</small>
+                            </div>
+                        </li>
+                        <li class="media">
+                            <a href="javascript:void(0);" class="media-object">
+                                <span class="img-wrapper img-rounded">
+                                    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/andrewaashen/128.jpg" style="width:36px">
+                                </span>
+                            </a>
+                            <div class="media-body">
+                                <p class="media-text">Etiam laoreet, libero et tristique pellentesque, tellus sem mollis dui, in sodales elit erat.</p>
+                                <span class="clearfix"></span>
+                                <p class="media-text">faucibus ut, nulla. Cras eu tellus</p>
+                                <!-- meta -->
+                                <span class="clearfix"></span><!-- important: clearing floated media text -->
+                                <small class="media-meta">Tue, Oct 01</small>
+                            </div>
+                        </li>
+                        <li class="media media-right">
+                            <a href="javascript:void(0);" class="media-object">
+                                <span class="img-wrapper img-rounded">
+                                    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/soyjavi/128.jpg" style="width:36px">
+                                </span>
+                            </a>
+                            <div class="media-body">
+                                <p class="media-text">Duis a mi fringilla mi lacinia mattis. Integer</p>
+                                <!-- meta -->
+                                <span class="clearfix"></span><!-- important: clearing floated media text -->
+                                <p class="media-meta">Fri, Sep 27</p>
+                            </div>
+                        </li>
+                        <li class="media">
+                            <a href="javascript:void(0);" class="media-object">
+                                <span class="img-wrapper img-rounded">
+                                    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/andrewaashen/128.jpg" style="width:36px">
+                                </span>
+                            </a>
+                            <div class="media-body">
+                                <p class="media-text">Praesent interdum ligula eu enim. Etiam imperdiet dictum magna.</p>
+                                <!-- meta -->
+                                <span class="clearfix"></span><!-- important: clearing floated media text -->
+                                <p class="media-meta">Wed, Aug 28</p>
+                            </div>
+                        </li>
+                        <li class="media media-right">
+                            <a href="javascript:void(0);" class="media-object">
+                                <span class="img-wrapper img-rounded">
+                                    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/soyjavi/128.jpg" style="width:36px">
+                                </span>
+                            </a>
+                            <div class="media-body">
+                                <p class="media-text">Aliquam rutrum lorem ac risus. Morbi metus. Vivamus euismod urna.</p>
+                                <!-- meta -->
+                                <span class="clearfix"></span><!-- important: clearing floated media text -->
+                                <p class="media-meta">Sat, Sep 27</p>
+                            </div>
+                        </li>
+                        <li class="media">
+                            <a href="javascript:void(0);" class="media-object">
+                                <span class="img-wrapper img-rounded">
+                                    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/andrewaashen/128.jpg" style="width:36px">
+                                </span>
+                            </a>
+                            <div class="media-body">
+                                <p class="media-text">Vestibulum accumsan neque et nunc. Quisque ornare tortor at risus. Nunc ac</p>
+                                <span class="clearfix"></span>
+                                <p class="media-text">Nam porttitor scelerisque neque</p>
+                                <!-- meta -->
+                                <span class="clearfix"></span><!-- important: clearing floated media text -->
+                                <p class="media-meta">Sun, Feb 22</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <!--/ scroll-content -->
+            </div>
+            <!--/ end: sidebar content -->
+        </div>
+        <!--/ Offcanvas Right -->
     </div>
-    <!--/ scroll-content -->
+    <!--/ END Wrapper -->
 </div>
-<!--/ end: sidebar-content -->
