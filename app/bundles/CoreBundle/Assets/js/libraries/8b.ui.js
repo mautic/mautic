e37bdb6714c38f86e1690e695ac707db328dd3b6
John Pozy <pampersdry@gmail.com>
2014-10-05 09:52:27 +0800

    Starting point of Mautic UI js

---
+++ app/bundles/CoreBundle/Assets/js/libraries/8b.ui.js
@@ -0,0 +1,10 @@
+/*! ========================================================================
+ * Mautic UI v1.0.0
+ * Copyright 2014 Mautic
+ * ======================================================================== */
+
+if (typeof jQuery === "undefined") { throw new Error("This application requires jQuery"); }
+
+;(function($, window, document, undefined) {
+  
+})(jQuery, window, document);
