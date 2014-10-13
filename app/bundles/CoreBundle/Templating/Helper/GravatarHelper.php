57a25300a9c97935717923ac7109a58b8dd8a9ab
Alan Hartless <alan@Alans-Mac.local>
2014-10-13 18:08:21 -0400

    Dev mode will show standard gravatar image; prod will show Mautic's local default (much better looking) image

--- app/bundles/CoreBundle/Templating/Helper/GravatarHelper.php
+++ app/bundles/CoreBundle/Templating/Helper/GravatarHelper.php
@@ -9,73 +9,34 @@
 
 namespace Mautic\CoreBundle\Templating\Helper;
 
+use Mautic\CoreBundle\Factory\MauticFactory;
+use Mautic\CoreBundle\Helper\UrlHelper;
 use Symfony\Component\Templating\Helper\Helper;
 
 class GravatarHelper extends Helper
 {
+    private static $devMode;
 
-    static public function getImage($email, $size = '250', $default = 'mm')
+    public function __construct(MauticFactory $factory)
     {
-        $url = 'https://www.gravatar.com/avatar/' . md5(strtolower(trim($email))) . '?s='.$size;
+        self::$devMode = $factory->getEnvironment() == 'dev';
+    }
 
-        if ($default !== false) {
+    static public function getImage($email, $size = '250', $default = null)
+    {
+        $localDefault     = 'assets/images/avatar.png';
+        $url              = 'https://www.gravatar.com/avatar/' . md5(strtolower(trim($email))) . '?s='.$size;
+
+        if ($default !== false && !self::$devMode) {
             if ($default === null) {
-                $default = 'assets/images/avatar.png';
+                $default = $localDefault;
             }
 
-            $default = (strpos($default, '.') !== false && strpos($default, 'http') !== 0) ? static::rel2abs($default) : $default;
+            $default = (strpos($default, '.') !== false && strpos($default, 'http') !== 0) ? UrlHelper::rel2abs($default) : $default;
             $url    .= '&d=' . urlencode($default);
         }
 
         return $url;
-    }
-
-    static public function rel2abs($rel)
-    {
-        $path = $host = $scheme = "";
-
-        $base = 'http';
-        if (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {
-            $base .= "s";
-        }
-        $base .= "://";
-        if ($_SERVER["SERVER_PORT"] != "80") {
-            $base .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-        } else {
-            $base .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-        }
-
-        $base = str_replace('/index_dev.php', '', $base);
-        $base = str_replace('/index.php', '', $base);
-
-        /* return if already absolute URL */
-        if (parse_url($rel, PHP_URL_SCHEME) != '') return $rel;
-
-        /* queries and anchors */
-        if ($rel[0]=='#' || $rel[0]=='?') return $base.$rel;
-
-        /* parse base URL and convert to local variables:
-           $scheme, $host, $path */
-        extract(parse_url($base));
-
-        /* remove non-directory element from path */
-        $path = preg_replace('#/[^/]*$#', '', $path);
-
-        /* destroy path if relative url points to root */
-        if ($rel[0] == '/') $path = '';
-
-        /* dirty absolute URL // with port number if exists */
-        if (parse_url($base, PHP_URL_PORT) != ''){
-            $abs = "$host:".parse_url($base, PHP_URL_PORT)."$path/$rel";
-        }else{
-            $abs = "$host$path/$rel";
-        }
-        /* replace '//' or '/./' or '/foo/../' with '/' */
-        $re = array('#(/\.?/)#', '#/(?!\.\.)[^/]+/\.\./#');
-        for($n=1; $n>0; $abs=preg_replace($re, '/', $abs, -1, $n)) {}
-
-        /* absolute URL is ready! */
-        return $scheme.'://'.$abs;
     }
 
     public function getName()
