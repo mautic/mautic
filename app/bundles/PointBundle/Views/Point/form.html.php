2716049402c5c5128187adfa6ba712cc0a01e293
David Hurley <david@websparkinc.com>
2014-10-17 02:37:20 -0400

    Removed hacked container wrapper

--- app/bundles/PointBundle/Views/Point/form.html.php
+++ app/bundles/PointBundle/Views/Point/form.html.php
@@ -17,39 +17,36 @@ $header = ($entity->getId()) ?
 $view['slots']->set("headerTitle", $header);
 echo $view['form']->start($form);
 ?>
-<!-- reset container-fluid padding -->
-<div class="mna-md">
-    <!-- start: box layout -->
-    <div class="box-layout">
-        <!-- container -->
-        <div class="col-md-9 bg-auto height-auto bdr-r">
-            <div class="bdr-rds-0 bdr-w-0">
-                <div class="pa-md bg-auto bg-light-xs bdr-b">
-                    <h4 class="fw-sb"><?php echo $header; ?></h4>
-                </div>
-                <div class="pa-md">
-				    <?php		    
-					echo $view['form']->row($form['name']);
-					echo $view['form']->row($form['description']);
-					echo $view['form']->row($form['type']);
+<!-- start: box layout -->
+<div class="box-layout">
+    <!-- container -->
+    <div class="col-md-9 bg-auto height-auto bdr-r">
+        <div class="bdr-rds-0 bdr-w-0">
+            <div class="pa-md bg-auto bg-light-xs bdr-b">
+                <h4 class="fw-sb"><?php echo $header; ?></h4>
+            </div>
+            <div class="pa-md">
+			    <?php		    
+				echo $view['form']->row($form['name']);
+				echo $view['form']->row($form['description']);
+				echo $view['form']->row($form['type']);
 
-				    if (isset($form['properties'])):
-				    	echo $view['form']->row($form['properties']);
-				    endif;
-				    ?>
-				</div>
+			    if (isset($form['properties'])):
+			    	echo $view['form']->row($form['properties']);
+			    endif;
+			    ?>
 			</div>
 		</div>
-	 	<div class="col-md-3 bg-white height-auto">
-			<div class="pr-lg pl-lg pt-md pb-md">
-				<?php
-					echo $view['form']->row($form['category_lookup']);
-					echo $view['form']->row($form['category']);
-					echo $view['form']->row($form['isPublished']);
-					echo $view['form']->row($form['publishUp']);
-					echo $view['form']->row($form['publishDown']);
-				?>
-			</div>
+	</div>
+ 	<div class="col-md-3 bg-white height-auto">
+		<div class="pr-lg pl-lg pt-md pb-md">
+			<?php
+				echo $view['form']->row($form['category_lookup']);
+				echo $view['form']->row($form['category']);
+				echo $view['form']->row($form['isPublished']);
+				echo $view['form']->row($form['publishUp']);
+				echo $view['form']->row($form['publishDown']);
+			?>
 		</div>
 	</div>
 </div>
