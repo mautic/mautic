df74e9e30d308168d2d4804f084cb5c0b2f4845b
David Hurley <david@websparkinc.com>
2014-10-17 02:36:04 -0400

    Updated no results, moved to template. Not yet implemented everywhere

---
+++ app/bundles/CoreBundle/Views/Default/noresults.html.php
@@ -0,0 +1,4 @@
+<div class="alert alert-warning col-md-6 col-md-offset-3 mt-md">
+    <h4><?php echo $view['translator']->trans('mautic.core.noresults.header'); ?></h4>
+    <p><?php echo $view['translator']->trans('mautic.core.noresults'); ?></p>
+</div>
