e82f3be888c3abdc02ec9f2d9f2a07fabe7a4c98
Jan Linhart <admin@escope.cz>
2014-10-19 12:21:02 +0200

    Upcoming Events from Campaign event log appears at Lead's left hand column when any.

--- app/bundles/CampaignBundle/Entity/LeadEventLogRepository.php
+++ app/bundles/CampaignBundle/Entity/LeadEventLogRepository.php
@@ -38,9 +38,55 @@ class LeadEventLogRepository extends EntityRepository
                     c.description AS campaignDescription')
             ->leftJoin('MauticCampaignBundle:Event', 'e', 'WITH', 'e.id = ll.event')
             ->leftJoin('MauticCampaignBundle:Campaign', 'c', 'WITH', 'c.id = e.campaign')
-            ->where('ll.lead = ' . $leadId)
+            ->where('ll.lead = ' . (int) $leadId)
             ->andWhere('e.eventType = :eventType')
             ->setParameter('eventType', 'trigger');
+
+        if (!empty($ipIds)) {
+            $query->orWhere('ll.ipAddress IN (' . implode(',', $ipIds) . ')');
+        }
+
+        return $query->getQuery()
+            ->getArrayResult();
+    }
+
+    /**
+     * Get a lead's upcoming events
+     *
+     * @param \Mautic\LeadBundle\Entity\Lead $lead entity
+     *
+     * @return array
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function getUpcomingEvents(\Mautic\LeadBundle\Entity\Lead $lead)
+    {
+        $leadIps = array();
+
+        /** @var \Mautic\CoreBundle\Entity\IpAddress $ip */
+        foreach ($lead->getIpAddresses() as $ip) {
+            $leadIps[] = $ip->getId();
+        }
+
+        $query = $this->createQueryBuilder('ll');
+        $query->select('IDENTITY(ll.event) AS event_id,
+                    IDENTITY(e.campaign) AS campaign_id,
+                    ll.triggerDate,
+                    e.name AS eventName,
+                    e.description AS eventDescription,
+                    c.name AS campaignName,
+                    c.description AS campaignDescription')
+            ->leftJoin('MauticCampaignBundle:Event', 'e', 'WITH', 'e.id = ll.event')
+            ->leftJoin('MauticCampaignBundle:Campaign', 'c', 'WITH', 'c.id = e.campaign')
+            ->where('ll.lead = :leadId')
+            ->setParameter('leadId', $lead->getId())
+            ->andWhere('e.eventType = :eventType')
+            ->setParameter('eventType', 'action')
+            ->andWhere('ll.isScheduled = :scheduled')
+            ->setParameter('scheduled', 1)
+            ->andwhere($query->expr()->gte('ll.triggerDate', ':today'))
+            ->setParameter('today', new \DateTime())
+            ->orderBy('ll.triggerDate');
 
         if (!empty($ipIds)) {
             $query->orWhere('ll.ipAddress IN (' . implode(',', $ipIds) . ')');
