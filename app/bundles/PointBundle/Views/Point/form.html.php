38bf0b285c6317dacac3f95d145145f6d92671d0
David Hurley <david@websparkinc.com>
2014-10-20 18:26:21 -0400

    Changed points header title

--- app/bundles/PointBundle/Views/Point/form.html.php
+++ app/bundles/PointBundle/Views/Point/form.html.php
@@ -11,9 +11,9 @@ $view->extend('MauticCoreBundle:Default:content.html.php');
 $view['slots']->set('mauticContent', 'point');
 
 $header = ($entity->getId()) ?
-    $view['translator']->trans('mautic.point.header.edit',
+    $view['translator']->trans('mautic.point.menu.edit',
         array('%name%' => $view['translator']->trans($entity->getName()))) :
-    $view['translator']->trans('mautic.point.header.new');
+    $view['translator']->trans('mautic.point.menu.new');
 $view['slots']->set("headerTitle", $header);
 
 echo $view['form']->start($form);
