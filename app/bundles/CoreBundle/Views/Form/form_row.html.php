dc118871b173712306b9d3fa487abc150ae30645
Alan Hartless <alan@Alans-Mac.local>
2014-10-20 18:26:28 -0400

    option to set bootstrap row width

--- app/bundles/CoreBundle/Views/Form/form_row.html.php
+++ app/bundles/CoreBundle/Views/Form/form_row.html.php
@@ -1,9 +1,15 @@
 <?php
 $hasErrors = count($form->vars['errors']);
 $feedbackClass = ($app->getRequest()->getMethod() == 'POST' && !empty($hasErrors)) ? " has-error" : "";
+if (isset($view['form']->vars['attr']['row-width'])) {
+    $colClasses = $view['form']->vars['attr']['row-width'];
+    unset($view['form']->vars['attr']['row-width']);
+} else {
+    $colClasses = 'col-xs-12 col-sm-8 col-md-6';
+}
 ?>
 <div class="row">
-    <div class="form-group col-xs-12 col-sm-8 col-md-6<?php echo $feedbackClass; ?>">
+    <div class="form-group <?php echo $colClasses . $feedbackClass; ?>">
         <?php echo $view['form']->label($form, $label) ?>
         <?php if (!empty($form->vars['attr']['tooltip'])): ?>
             <span data-toggle="tooltip" data-container="body" data-placement="top"
