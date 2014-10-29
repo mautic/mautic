8944c369033d79ba977b5afeedda2a28ac04be35
Michael Babker <mbabker@flbab.com>
2014-10-29 02:08:10 -0400

    Add the migrations bundle into the bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -79,6 +79,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
