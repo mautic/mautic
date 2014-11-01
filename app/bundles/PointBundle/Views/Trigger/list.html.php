e6f3b4f32c65853a4421950594ba6705d6aa462c
Michael Babker <mbabker@flbab.com>
2014-10-31 21:04:14 -0400

    Point trigger batch delete + layout

--- app/bundles/PointBundle/Views/Trigger/list.html.php
+++ app/bundles/PointBundle/Views/Trigger/list.html.php
@@ -1,4 +1,4 @@
-<?php
+`<?php
 /**
  * @package     Mautic
  * @copyright   2014 Mautic, NP. All rights reserved.
@@ -10,111 +10,103 @@ if ($tmpl == 'index')
 $view->extend('MauticPointBundle:Trigger:index.html.php');
 ?>
 
-    <?php if (count($items)): ?>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">
-                    <?php echo $view['translator']->trans('mautic.point.trigger.header.index');?>
-                </h3>
-            </div>
-            <div class="table-responsive scrollable body-white padding-sm page-list">
-                    <table class="table table-hover table-striped table-bordered pointtrigger-list" id="triggerTable">
-                        <thead>
-                        <tr>
-                            <th class="col-pointtrigger-actions pl-20">
-                                <div class="checkbox-inline custom-primary">
-                                    <label class="mb-0 pl-10">
-                                        <input type="checkbox" id="customcheckbox-one0" value="1" data-toggle="checkall" data-target="#triggerTable">
-                                        <span></span>
-                                    </label>
-                                </div>
-                            </th>
-                            <?php
-                            echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', array(
-                                'sessionVar' => 'pointtrigger',
-                                'orderBy'    => 't.name',
-                                'text'       => 'mautic.point.trigger.thead.name',
-                                'class'      => 'col-pointtrigger-name',
-                                'default'    => true
-                            ));
-
-                            echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', array(
-                                'sessionVar' => 'pointtrigger',
-                                'orderBy'    => 't.description',
-                                'text'       => 'mautic.point.trigger.thead.description',
-                                'class'      => 'col-pointtrigger-description'
-                            ));
-
-                            echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', array(
-                                'sessionVar' => 'pointtrigger',
-                                'orderBy'    => 't.points',
-                                'text'       => 'mautic.point.trigger.thead.points',
-                                'class'      => 'col-pointtrigger-points'
-                            ));
-
-                            echo "<th class='col-pointtrigger-color'>" . $view['translator']->trans('mautic.point.trigger.thead.color') . '</th>';
-
-                            echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', array(
-                                'sessionVar' => 'pointtrigger',
-                                'orderBy'    => 't.id',
-                                'text'       => 'mautic.point.trigger.thead.id',
-                                'class'      => 'col-pointtrigger-id'
-                            ));
-                            ?>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($items as $item): ?>
-                            <tr>
-                                <td>
-                                    <?php
-                                    echo $view->render('MauticCoreBundle:Helper:actions.html.php', array(
-                                        'item'      => $item,
-                                        'edit'      => $permissions['point:triggers:edit'],
-                                        'clone'     => $permissions['point:triggers:create'],
-                                        'delete'    => $permissions['point:triggers:delete'],
-                                        'routeBase' => 'pointtrigger',
-                                        'menuLink'  => 'mautic_pointtrigger_index',
-                                        'langVar'   => 'point.trigger'
-                                    ));
-                                    ?>
-                                </td>
-                                <td>
-                                    <?php echo $view->render('MauticCoreBundle:Helper:publishstatus.html.php',array(
-                                        'item'       => $item,
-                                        'model'      => 'point.trigger'
-                                    )); ?>
-                                    <a href="<?php echo $view['router']->generate('mautic_pointtrigger_action',
-                                        array("objectAction" => "view", "objectId" => $item->getId())); ?>"
-                                       data-toggle="ajax">
-                                        <?php echo $item->getName(); ?>
-                                    </a>
-                                </td>
-                                <td class="visible-md visible-lg"><?php echo $item->getDescription(); ?></td>
-                                <td class="visible-md visible-lg"><?php echo $item->getPoints(); ?></td>
-                                <?php
-                                $color = $item->getColor();
-                                $colorStyle = ($color) ? ' style="background-color: ' . $color . '"' : '';
-                                ?>
-                                <td<?php echo $colorStyle; ?> class="visible-md visible-lg"></td>
-                                <td class="visible-md visible-lg"><?php echo $item->getId(); ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                    </table>
+<?php if (count($items)): ?>
+<div class="table-responsive page-list">
+    <table class="table table-hover table-striped table-bordered pointtrigger-list" id="triggerTable">
+        <thead>
+        <tr>
+            <th class="col-pointtrigger-actions pl-20">
+                <div class="checkbox-inline custom-primary">
+                    <label class="mb-0 pl-10">
+                        <input type="checkbox" id="customcheckbox-one0" value="1" data-toggle="checkall" data-target="#triggerTable">
+                        <span></span>
+                    </label>
                 </div>
-                <div class="page-footer">
-                    <?php echo $view->render('MauticCoreBundle:Helper:pagination.html.php', array(
-                        "totalItems"      => count($items),
-                        "page"            => $page,
-                        "limit"           => $limit,
-                        "menuLinkId"      => 'mautic_pointtrigger_index',
-                        "baseUrl"         => $view['router']->generate('mautic_pointtrigger_index'),
-                        'sessionVar'      => 'pointtrigger'
+            </th>
+            <?php
+            echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', array(
+                'sessionVar' => 'pointtrigger',
+                'orderBy'    => 't.name',
+                'text'       => 'mautic.point.trigger.thead.name',
+                'class'      => 'col-pointtrigger-name',
+                'default'    => true
+            ));
+
+            echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', array(
+                'sessionVar' => 'pointtrigger',
+                'orderBy'    => 't.description',
+                'text'       => 'mautic.point.trigger.thead.description',
+                'class'      => 'col-pointtrigger-description'
+            ));
+
+            echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', array(
+                'sessionVar' => 'pointtrigger',
+                'orderBy'    => 't.points',
+                'text'       => 'mautic.point.trigger.thead.points',
+                'class'      => 'col-pointtrigger-points'
+            ));
+
+            echo "<th class='col-pointtrigger-color'>" . $view['translator']->trans('mautic.point.trigger.thead.color') . '</th>';
+
+            echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', array(
+                'sessionVar' => 'pointtrigger',
+                'orderBy'    => 't.id',
+                'text'       => 'mautic.point.trigger.thead.id',
+                'class'      => 'col-pointtrigger-id'
+            ));
+            ?>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($items as $item): ?>
+            <tr>
+                <td>
+                    <?php
+                    echo $view->render('MauticCoreBundle:Helper:actions.html.php', array(
+                        'item'      => $item,
+                        'edit'      => $permissions['point:triggers:edit'],
+                        'clone'     => $permissions['point:triggers:create'],
+                        'delete'    => $permissions['point:triggers:delete'],
+                        'routeBase' => 'pointtrigger',
+                        'menuLink'  => 'mautic_pointtrigger_index',
+                        'langVar'   => 'point.trigger'
+                    ));
+                    ?>
+                </td>
+                <td>
+                    <?php echo $view->render('MauticCoreBundle:Helper:publishstatus.html.php',array(
+                        'item'       => $item,
+                        'model'      => 'point.trigger'
                     )); ?>
-                </div>
-            </div>
-        </div>
-    <?php else: ?>
-        <?php echo $view->render('MauticCoreBundle:Default:noresults.html.php'); ?>
-    <?php endif; ?>
+                    <a href="<?php echo $view['router']->generate('mautic_pointtrigger_action',
+                        array("objectAction" => "view", "objectId" => $item->getId())); ?>"
+                       data-toggle="ajax">
+                        <?php echo $item->getName(); ?>
+                    </a>
+                </td>
+                <td class="visible-md visible-lg"><?php echo $item->getDescription(); ?></td>
+                <td class="visible-md visible-lg"><?php echo $item->getPoints(); ?></td>
+                <?php
+                $color = $item->getColor();
+                $colorStyle = ($color) ? ' style="background-color: ' . $color . '"' : '';
+                ?>
+                <td<?php echo $colorStyle; ?> class="visible-md visible-lg"></td>
+                <td class="visible-md visible-lg"><?php echo $item->getId(); ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+<div class="page-footer">
+    <?php echo $view->render('MauticCoreBundle:Helper:pagination.html.php', array(
+        "totalItems"      => count($items),
+        "page"            => $page,
+        "limit"           => $limit,
+        "menuLinkId"      => 'mautic_pointtrigger_index',
+        "baseUrl"         => $view['router']->generate('mautic_pointtrigger_index'),
+        'sessionVar'      => 'pointtrigger'
+    )); ?>
+</div>
+<?php else: ?>
+    <?php echo $view->render('MauticCoreBundle:Default:noresults.html.php'); ?>
+<?php endif; ?>
