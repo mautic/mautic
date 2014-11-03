a32e838c90a83c68f6067f130ce3dcfeed729e50
Michael Babker <mbabker@flbab.com>
2014-11-03 12:32:29 -0500

    Update the Profile view layout

--- app/bundles/UserBundle/Views/Profile/index.html.php
+++ app/bundles/UserBundle/Views/Profile/index.html.php
@@ -13,72 +13,87 @@ $view->extend('MauticCoreBundle:Default:content.html.php');
 $view['slots']->set('mauticContent', 'user');
 $view['slots']->set("headerTitle", $view['translator']->trans('mautic.user.account.header.index'));
 ?>
+<!-- start: box layout -->
+<div class="box-layout">
+           <!-- step container -->
+    <div class="col-md-3 bg-white height-auto">
+        <div class="pr-lg pl-lg pt-md pb-md">
+            <?php if ($me->getId()): ?>
+                <div class="media">
+                    <div class="pull-left">
+                        <img class="img-rounded img-bordered media-object" src="<?php echo $view['gravatar']->getImage($me->getEmail()); ?>" alt="" width="65px">
+                    </div>
+                    <div class="media-body">
+                        <h4><?php echo $me->getName(); ?></h4>
+                        <h5><?php echo $me->getPosition(); ?></h5>
+                    </div>
+                </div>
+                <hr />
+            <?php endif; ?>
 
-<div class="account-wrapper scrollable">
-    <div class="row padding-md">
-        <div class="col-sm-3 col-xs-12">
-            <div class="body-white padding-md profile-details rounded-corners text-center">
-                <img class="img img-responsive img-thumbnail"
-                     src="<?php echo $view['gravatar']->getImage($me->getEmail()); ?>" />
-                <h3 class="margin-sm-sides margin-md-top"><?php echo $me->getName(); ?></h3>
-                <h4 class="margin-sm-sides"><?php echo $me->getPosition(); ?></h4>
+            <ul class="list-group list-group-tabs">
+                <li class="list-group-item active">
+                    <a href="#profile" class="steps" data-toggle="tab">
+                        <?php echo $view['translator']->trans('mautic.user.account.header.details'); ?>
+                    </a>
+                </li>
+                <?php if ($permissions['apiAccess']): ?>
+                <li class="list-group-item">
+                    <a href="#clients" class="steps" data-toggle="tab">
+                        <?php echo $view['translator']->trans('mautic.user.account.header.authorizedclients'); ?>
+                    </a>
+                </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+    <!--/ step container -->
+
+    <!-- container -->
+    <div class="col-md-9 bg-auto height-auto bdr-l">
+        <?php echo $view['form']->start($userForm); ?>
+        <div class="tab-pane fade in active bdr-rds-0 bdr-w-0" id="profile">
+            <div class="pa-md bg-auto bg-light-xs bdr-b">
+                <h4 class="fw-sb"><?php echo $view['translator']->trans('mautic.user.account.header.details'); ?></h4>
+            </div>
+            <div class="pa-md">
+                <div class="col-md-6">
+                    <?php
+                    echo ($permissions['editUsername']) ? $view['form']->row($userForm['username']) : $view['form']->row($userForm['username_unbound']);
+                    echo ($permissions['editName']) ? $view['form']->row($userForm['firstName']) : $view['form']->row($userForm['firstName_unbound']);
+                    echo ($permissions['editName']) ? $view['form']->row($userForm['lastName']) : $view['form']->row($userForm['lastName_unbound']);
+                    echo ($permissions['editPosition']) ? $view['form']->row($userForm['position']) : $view['form']->row($userForm['position_unbound']);
+                    echo ($permissions['editEmail']) ? $view['form']->row($userForm['email']) : $view['form']->row($userForm['email_unbound']);
+                    ?>
+                </div>
+                <div class="col-md-6">
+                    <?php
+                    echo $view['form']->row($userForm['timezone']);
+                    echo $view['form']->row($userForm['locale']);
+                    echo $view['form']->row($userForm['currentPassword']);
+                    echo $view['form']->row($userForm['plainPassword']['password']);
+                    echo $view['form']->row($userForm['plainPassword']['confirm']);
+                    ?>
+                </div>
             </div>
         </div>
+        <?php echo $view['form']->end($userForm); ?>
 
-        <div class="col-sm-9 col-xs-12 padding-md-sides">
-            <?php echo $view['form']->start($userForm); ?>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <?php echo $view['translator']->trans('mautic.user.account.header.details'); ?>
-                    <div class="pull-right">
-                        <button type="submit"
-                                id ="btn-save-profile"
-                                class="btn btn-success btn-xs"
-                                data-toggle="tooltip"
-                                data-container="body"
-                                data-placement="top"
-                                data-original-title="<?php echo $view['translator']->trans('mautic.core.form.save'); ?>">
-                            <i class="fa fa-check"></i>
-                        </button>
-                    </div>
+        <?php if ($permissions['apiAccess']): ?>
+            <div class="tab-pane fade in active bdr-rds-0 bdr-w-0" id="clients">
+                <div class="pa-md bg-auto bg-light-xs bdr-b">
+                    <h4 class="fw-sb"><?php echo $view['translator']->trans('mautic.user.account.header.authorizedclients'); ?></h4>
                 </div>
-                <div class="panel-body">
-                    <div class="col-md-6">
-                        <?php
-                        echo ($permissions['editUsername']) ? $view['form']->row($userForm['username']) : $view['form']->row($userForm['username_unbound']);
-                        echo ($permissions['editName']) ? $view['form']->row($userForm['firstName']) : $view['form']->row($userForm['firstName_unbound']);
-                        echo ($permissions['editName']) ? $view['form']->row($userForm['lastName']) : $view['form']->row($userForm['lastName_unbound']);
-                        echo ($permissions['editPosition']) ? $view['form']->row($userForm['position']) : $view['form']->row($userForm['position_unbound']);
-                        echo ($permissions['editEmail']) ? $view['form']->row($userForm['email']) : $view['form']->row($userForm['email_unbound']);
-                        ?>
-                    </div>
-                    <div class="col-md-6">
-                        <?php
-                        echo $view['form']->row($userForm['timezone']);
-                        echo $view['form']->row($userForm['locale']);
-                        echo $view['form']->row($userForm['currentPassword']);
-                        echo $view['form']->row($userForm['plainPassword']['password']);
-                        echo $view['form']->row($userForm['plainPassword']['confirm']);
-                        ?>
-                    </div>
-                </div>
-            </div>
-            <?php echo $view['form']->end($userForm); ?>
-
-            <?php if ($permissions['apiAccess']): ?>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <?php echo $view['translator']->trans('mautic.user.account.header.authorizedclients'); ?>
-                </div>
-                <div class="panel-body">
+                <div class="pa-md">
                     <?php echo $view['actions']->render(
                         new Symfony\Component\HttpKernel\Controller\ControllerReference(
                             'MauticApiBundle:Client:authorizedClients'
                         ));
-                        ?>
+                    ?>
                 </div>
             </div>
-            <?php endif; ?>
-        </div>
+        <?php endif; ?>
     </div>
+    <!--/ end: container -->
 </div>
+<!--/ end: box layout -->
