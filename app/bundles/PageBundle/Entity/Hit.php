44650493964401b60436f95a5e116ec72420bf37
Alan Hartless <alan@Alans-Mac.local>
2014-11-06 23:56:15 -0600

    Fixed issue with not being able to delete a lead

--- app/bundles/PageBundle/Entity/Hit.php
+++ app/bundles/PageBundle/Entity/Hit.php
@@ -59,7 +59,7 @@ class Hit
 
     /**
      * @ORM\ManyToOne(targetEntity="Mautic\LeadBundle\Entity\Lead")
-     * @ORM\JoinColumn(name="lead_id", referencedColumnName="id", nullable=true)
+     * @ORM\JoinColumn(name="lead_id", referencedColumnName="id", nullable=true, onDelete="SET NULL")
      * @Serializer\Expose
      * @Serializer\Since("1.0")
      * @Serializer\Groups({"full"})
