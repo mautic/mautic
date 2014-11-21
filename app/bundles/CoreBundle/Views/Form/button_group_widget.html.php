b071d18bbfd0bc03032c15c96312b2794fc3cd8e
Alan Hartless <alan@Alans-Mac.local>
2014-11-21 15:59:37 -0600

    Pass form attr to radio for button group

--- app/bundles/CoreBundle/Views/Form/button_group_widget.html.php
+++ app/bundles/CoreBundle/Views/Form/button_group_widget.html.php
@@ -1,10 +1,14 @@
-<div <?php echo $view['form']->block($form, 'widget_container_attributes') ?> class="btn-group btn-block" data-toggle="buttons">
+<?php
+//apply attributes to radios
+$attr = $form->vars['attr'];
+?>
+<div class="btn-group btn-block" data-toggle="buttons">
     <?php foreach ($form as $child): ?>
         <?php $class =
             (!empty($child->vars['checked']) ? ' active' : '') .
             (!empty($child->vars['disabled']) || !empty($child->vars['read_only']) ? ' disabled' : ''); ?>
         <label class="btn btn-default<?php echo $class; ?>">
-            <?php echo $view['form']->widget($child); ?>
+            <?php echo $view['form']->widget($child, array('attr' => $attr)); ?>
             <?php echo $view['translator']->trans($child->vars['label']); ?>
         </label>
     <?php endforeach; ?>
