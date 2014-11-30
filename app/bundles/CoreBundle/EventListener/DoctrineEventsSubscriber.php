ae6ca0f7d8fc6163445ad4a11e03df43ff44c1dc
Alan Hartless <alan@Alans-Mac.local>
2014-11-30 14:32:10 -0600

    Attempt at preventing a PHP notice with the database prefix on cache clear

--- app/bundles/CoreBundle/EventListener/DoctrineEventsSubscriber.php
+++ app/bundles/CoreBundle/EventListener/DoctrineEventsSubscriber.php
@@ -32,8 +32,8 @@ class DoctrineEventsSubscriber implements \Doctrine\Common\EventSubscriber
      */
     public function loadClassMetadata(LoadClassMetadataEventArgs $args)
     {
-        if (defined('MAUTIC_INSTALLER') && !defined('MAUTIC_TABLE_PREFIX')) {
-            //only run this after the installer has set the MAUTIC_TABLE_PREFIX
+        if (!defined('MAUTIC_TABLE_PREFIX')) {
+            //cache is being built or in the installer and hasn't been set yet
             return;
         }
 
