69a6c19b8b1752773444a58223e6d9214d94ad2a
Alan Hartless <alan@Alans-Mac.local>
2014-11-30 16:47:40 -0600

    Fixed fatal errors related to changed helper name

--- app/bundles/PageBundle/EventListener/BuilderSubscriber.php
+++ app/bundles/PageBundle/EventListener/BuilderSubscriber.php
@@ -101,7 +101,7 @@ class BuilderSubscriber extends CommonSubscriber
         static $content = "";
 
         if (empty($content)) {
-            $shareButtons = $this->factory->getHelper('connector')->getShareButtons();
+            $shareButtons = $this->factory->getHelper('integration')->getShareButtons();
 
             $content = "<div class='share-buttons'>\n";
             foreach ($shareButtons as $network => $content) {
