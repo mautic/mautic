fdc720aabf9d7f07e9a758a08059718ba984447b
Jan Linhart <admin@escope.cz>
2014-12-04 14:54:27 +0100

    Added url link if Page ID is not known to Page Hit event (Lead Timeline)

--- app/bundles/PageBundle/Views/Timeline/index.html.php
+++ app/bundles/PageBundle/Views/Timeline/index.html.php
@@ -28,11 +28,15 @@ if ($event['extra']['hit']['dateLeft']) {
 	<div class="panel">
 	    <div class="panel-body">
 	    	<h3>
-	    		<a href="<?php echo $view['router']->generate('mautic_page_action',
-				    array("objectAction" => "view", "objectId" => $item->getId())); ?>"
-				   data-toggle="ajax">
-				    <?php echo $item->getTitle(); ?>
-				</a>
+		    	<?php if ($event['extra']['hit']['page_id']) : ?>
+		    		<a href="<?php echo $view['router']->generate('mautic_page_action',
+					    array("objectAction" => "view", "objectId" => $item->getId())); ?>"
+					   data-toggle="ajax">
+					    <?php echo $item->getTitle(); ?>
+					</a>
+				<?php else : ?>
+					<?php echo $view['assets']->makeLinks($event['extra']['hit']['url']); ?>
+				<?php endif; ?>
 			</h3>
             <p class="mb-0"><?php echo $view['translator']->trans('mautic.core.timeline.event.time', array('%date%' => $view['date']->toFullConcat($event['timestamp']), '%event%' => $event['eventLabel'])); ?></p>
 	    </div>
