8052e8a794b4c9e0e2db7404aa00beb252f8c8af
Michael Babker <mbabker@flbab.com>
2014-12-14 11:17:46 -0500

    Remove PHP 5.6 check, we're on fixed Doctrine version (Fix gh-4)

--- index_dev.php
+++ index_dev.php
@@ -13,14 +13,6 @@ date_default_timezone_set ('UTC');
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-// Cannot function on PHP 5.6 properly due to a Doctrine bug which is included in Doctrine 2.5
-// See http://www.doctrine-project.org/jira/browse/DDC-3120 for more
-if (version_compare(PHP_VERSION, '5.6', 'ge')) {
-    echo "Mautic will not function properly on PHP 5.6 due to an issue with a third party dependency.\n";
-    echo "Please downgrade to PHP 5.4 or 5.5.";
-    exit;
-}
-
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
