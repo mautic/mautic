770ad4b7ea2df93c30407a81821b31b5547fc9d5
Jan Linhart <admin@escope.cz>
2014-12-15 17:34:22 +0100

    Tabs in config form changed to left column list nav

--- app/bundles/ConfigBundle/Views/Config/form.html.php
+++ app/bundles/ConfigBundle/Views/Config/form.html.php
@@ -12,43 +12,47 @@ if ($tmpl == 'index') {
 ?>
 <?php if (!empty($params)) : ?>
 
-<div class="panel panel-default page-list bdr-t-wdh-0">
-    <div class="panel-body">
-        <div role="tabpanel">
+<!-- start: box layout -->
+<div class="box-layout">
+    <!-- step container -->
+    <div class="col-md-3 bg-white height-auto">
+        <div class="pr-lg pl-lg pt-md pb-md">
 
             <!-- Nav tabs -->
-            <ul class="nav nav-tabs" role="tablist">
+            <ul class="list-group list-group-tabs" role="tablist">
             <?php $i = 0; ?>
             <?php foreach ($params as $key => $paramArray) : ?>
-                <li role="presentation" class="<?php echo $i == 0 ? 'in active' : ''; ?>">
-                    <a href="#<?php echo $key; ?>" aria-controls="<?php echo $key; ?>" role="tab" data-toggle="tab">
+                <li role="presentation" class="list-group-item <?php echo $i == 0 ? 'in active' : ''; ?>">
+                    <a href="#<?php echo $key; ?>" aria-controls="<?php echo $key; ?>" role="tab" data-toggle="tab" class="steps">
                         <?php echo $key; ?>
                     </a>
                 </li>
                 <?php $i++; ?>
             <?php endforeach; ?>
             </ul>
-
-            <!-- Tab panes -->
-            <?php echo $view['form']->start($form); ?>
-            <div class="tab-content">
-                <?php $i = 0; ?>
-                <?php foreach ($params as $key => $paramArray) : ?>
-                <div role="tabpanel" class="tab-pane fade <?php echo $i == 0 ? 'in active' : ''; ?> bdr-w-0" id="<?php echo $key; ?>">
-                    <div class="pt-md pr-md pl-md pb-md">
-                    <?php foreach ($paramArray as $paramKey => $paramValue) : ?>
-                        <div class="col-md-6">
-                            <?php echo $view['form']->row($form[$paramKey]); ?>
-                        </div>
-                    <?php endforeach; ?>
-                    </div>
-                </div>
-                <?php $i++; ?>
-                <?php endforeach; ?>
-            </div>
-            <?php echo $view['form']->end($form); ?>
-
         </div>
+    </div>
+
+    <!-- container -->
+    <div class="col-md-9 bg-auto height-auto bdr-l">
+        <!-- Tab panes -->
+        <?php echo $view['form']->start($form); ?>
+        <div class="tab-content">
+            <?php $i = 0; ?>
+            <?php foreach ($params as $key => $paramArray) : ?>
+            <div role="tabpanel" class="tab-pane fade <?php echo $i == 0 ? 'in active' : ''; ?> bdr-w-0" id="<?php echo $key; ?>">
+                <div class="pt-md pr-md pl-md pb-md">
+                <?php foreach ($paramArray as $paramKey => $paramValue) : ?>
+                    <div class="col-md-6">
+                        <?php echo $view['form']->row($form[$paramKey]); ?>
+                    </div>
+                <?php endforeach; ?>
+                </div>
+            </div>
+            <?php $i++; ?>
+            <?php endforeach; ?>
+        </div>
+        <?php echo $view['form']->end($form); ?>
     </div>
 </div>
 
