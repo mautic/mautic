98a760353af2b33e5a6706cbc39393152b193325
Jan Linhart <admin@escope.cz>
2014-12-19 10:53:41 +0100

    More advanced show/hide field condition handeling in Config form

--- app/bundles/AssetBundle/Form/Type/ConfigType.php
+++ app/bundles/AssetBundle/Form/Type/ConfigType.php
@@ -16,6 +16,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * Class ConfigType
@@ -43,10 +44,15 @@ class ConfigType extends AbstractType
      */
     public function buildForm (FormBuilderInterface $builder, array $options)
     {
+        $constraints = array(
+            'required' => array(new NotBlank(array('message' => 'mautic.core.required')))
+        );
+
         $builder->add('upload_dir', 'text', array(
-            'label'      => 'mautic.asset.config.form.upload.dir',
-            'label_attr' => array('class' => 'control-label'),
-            'attr'       => array('class' => 'form-control')
+            'label'       => 'mautic.asset.config.form.upload.dir',
+            'label_attr'  => array('class' => 'control-label'),
+            'attr'        => array('class' => 'form-control'),
+            'constraints' => $constraints['required'],
         ));
     }
 
