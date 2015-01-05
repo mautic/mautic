fee46ad1c4e4024de14945dbe2933a3413d60bb3
Alan Hartless <alan@Alans-Mac-2.local>
2015-01-05 17:22:44 -0600

    Added panels to all ConfigBundle tabs

--- app/bundles/PageBundle/Views/FormTheme/Config/_config_pageconfig_widget.html.php
+++ app/bundles/PageBundle/Views/FormTheme/Config/_config_pageconfig_widget.html.php
@@ -8,10 +8,17 @@
  */
 ?>
 
-<?php foreach ($form->children as $f): ?>
-<div class="row">
-    <div class="col-md-6">
-        <?php echo $view['form']->row($f); ?>
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?php echo $view['translator']->trans('mautic.config.tab.pageconfig'); ?></h3>
+    </div>
+    <div class="panel-body">
+        <?php foreach ($form->children as $f): ?>
+            <div class="row">
+                <div class="col-md-6">
+                    <?php echo $view['form']->row($f); ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
 </div>
-<?php endforeach; ?>
