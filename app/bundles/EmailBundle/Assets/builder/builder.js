1c2c5301ecaf5954ba8c4e9b86e69a9adf864dfa
Alan Hartless <alan@Alans-Mac-2.local>
2015-01-08 09:28:55 -0600

    Removed bad JS from email builder

--- app/bundles/EmailBundle/Assets/builder/builder.js
+++ app/bundles/EmailBundle/Assets/builder/builder.js
@@ -1,5 +1,4 @@
 mQuery(document).ready( function() {
-    var mauticAjaxUrl = '{$view['router']->generate("mautic_core_ajax")}';
     CKEDITOR.disableAutoInline = true;
     mQuery("div[contenteditable='true']").each(function (index) {
         var content_id = mQuery(this).attr('id');
