e11fb0bf57837411a7992656379330030c2c89cf
Jan Linhart <admin@escope.cz>
2015-01-14 17:52:57 +0100

    Fixed template path for Email:read events in Lead timeline

--- app/bundles/EmailBundle/EventListener/LeadSubscriber.php
+++ app/bundles/EmailBundle/EventListener/LeadSubscriber.php
@@ -89,7 +89,7 @@ class LeadSubscriber extends CommonSubscriber
                     'extra'     => array(
                         'stats' => $stat
                     ),
-                    'contentTemplate' => 'MauticEmailBundle:Timeline:index.html.php'
+                    'contentTemplate' => 'MauticEmailBundle:SubscribedEvents\Timeline:index.html.php'
                 ));
             }
         }
