fbb47fc032160d01cdcd36fb7c48511e1b705b06
Alan Hartless <alan@devkardia.com>
2015-01-28 01:27:42 -0600

    Email marketing addon bundle with MailChimp support

---
+++ addons/MauticEmailMarketingBundle/Config/config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+return array(
+    'name'        => 'Email Marketing Integrations',
+    'description' => 'Enables integration with Mautic supported email marketing services.',
+    'version'     => '1.0',
+    'author'      => 'Mautic',
+
+    'services' => array(
+        'forms' => array(
+            'mautic.form.type.emailmarketing.mailchimp' => array(
+                'class'     => 'MauticAddon\MauticEmailMarketingBundle\Form\Type\MailchimpType',
+                'arguments' => 'mautic.factory',
+                'alias'     => 'emailmarketing_mailchimp'
+            )
+        )
+    )
+);
---
+++ addons/MauticEmailMarketingBundle/Integration/EmailAbstractIntegration.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticAddon\MauticEmailMarketingBundle\Integration;
+
+use Mautic\AddonBundle\Integration\AbstractIntegration;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\Form;
+
+/**
+ * Class EmailAbstractIntegration
+ *
+ * @package MauticAddon\MauticCrmBundle\Integration
+ */
+abstract class EmailAbstractIntegration extends AbstractIntegration
+{
+
+    /**
+     * @return array
+     */
+    public function getSupportedFeatures()
+    {
+        return array('push_lead');
+    }
+
+    /**
+     * @param FormBuilder|Form $builder
+     */
+    public function appendToForm(&$builder, $data, $formArea)
+    {
+        if ($formArea == 'features' || $formArea == 'integration') {
+            $name = strtolower($this->getName());
+            if ($this->factory->serviceExists('mautic.form.type.emailmarketing.' . $name)) {
+                $builder->add('list_settings', 'emailmarketing_' . $name, array(
+                    'label'     => false,
+                    'form_area' => $formArea,
+                    'data'      => (isset($data['list_settings'])) ? $data['list_settings'] : array()
+                ));
+            }
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormTheme()
+    {
+        return 'MauticEmailMarketingBundle:FormTheme\EmailMarketing';
+    }
+
+    /**
+     * Get the API helper
+     *
+     * @return Object
+     */
+    public function getApiHelper()
+    {
+        static $helper;
+        if (empty($helper)) {
+            $class = '\\MauticAddon\\MauticEmailMarketingBundle\\Api\\'.$this->getName().'Api';
+            $helper = new $class($this);
+        }
+
+        return $helper;
+    }
+}
