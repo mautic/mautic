7fafdc31184147685d122361db5c44582feea9bc
Alan Hartless <alan@devkardia.com>
2015-02-04 20:12:11 -0600

    Changed right "hamburger" icon to cogwheels (chat will be moved out of the right sidebar before RC)

--- app/bundles/CoreBundle/Views/Default/navbar.html.php
+++ app/bundles/CoreBundle/Views/Default/navbar.html.php
@@ -15,30 +15,15 @@
 
 <!-- start: navbar nocollapse -->
 <div class="navbar-nocollapse">
-    <!-- start: Sidebar left toggle -->
-    <div class="navbar-header navbar-left visible-xs-inline-block">
-        <button type="button" class="navbar-toggle" data-toggle="sidebar" data-direction="ltr">
-            <span class="icon-bar thin"></span>
-            <span class="icon-bar thin"></span>
-            <span class="icon-bar thin"></span>
-        </button>
-    </div>
-    <!--/ end: Sidebar left toggle -->
-
-    <!-- start: Sidebar right toggle -->
-    <div class="navbar-header navbar-right">
-        <button type="button" class="navbar-toggle" data-toggle="sidebar" data-direction="rtl">
-            <span class="icon-bar thin"></span>
-            <span class="icon-bar thin"></span>
-            <span class="icon-bar thin"></span>
-        </button>
-    </div>
-    <!--/ end: Sidebar right toggle -->
-
     <!-- start: left nav -->
     <ul class="nav navbar-nav navbar-left">
         <li class="hidden-xs" data-toggle="tooltip" data-placement="right" title="Minimize Sidebar">
             <a href="javascript:void(0)" data-toggle="minimize" class="sidebar-minimizer"><span class="arrow fs-14"></span></a>
+        </li>
+        <li class="visible-xs">
+            <a href="javascript: void(0);" data-toggle="sidebar" data-direction="ltr">
+                <i class="fa fa-navicon fs-16"></i>
+            </a>
         </li>
         <?php echo $view['actions']->render(new \Symfony\Component\HttpKernel\Controller\ControllerReference('MauticCoreBundle:Default:notifications')); ?>
         <?php echo $view['actions']->render(new \Symfony\Component\HttpKernel\Controller\ControllerReference('MauticCoreBundle:Default:globalSearch')); ?>
@@ -48,6 +33,11 @@
     <!-- start: right nav -->
     <ul class="nav navbar-nav navbar-right">
         <?php echo $view->render("MauticCoreBundle:Menu:profile.html.php"); ?>
+        <li>
+            <a href="javascript: void(0);" data-toggle="sidebar" data-direction="rtl">
+                <i class="fa fa-cogs fs-16"></i>
+            </a>
+        </li>
     </ul>
     <!--/ end: right nav -->
 </div>
