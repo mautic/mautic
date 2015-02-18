a3f0f446b60c12d3106c88b096c5ce17a75b36e7
Alan Hartless <alan@devkardia.com>
2015-02-18 15:14:35 -0600

    Added back accidentally removed docprops ckeditor plugin

---
+++ app/bundles/CoreBundle/Assets/js/libraries/ckeditor/plugins/docprops/lang/fr.js
@@ -0,0 +1,42 @@
+﻿/*
+Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'docprops', 'fr', {
+	bgColor: 'Couleur de fond',
+	bgFixed: 'Image fixe sans défilement',
+	bgImage: 'Image de fond',
+	charset: 'Encodage de caractère',
+	charsetASCII: 'ASCII',
+	charsetCE: 'Europe Centrale',
+	charsetCR: 'Cyrillique',
+	charsetCT: 'Chinois Traditionnel (Big5)',
+	charsetGR: 'Grec',
+	charsetJP: 'Japonais',
+	charsetKR: 'Coréen',
+	charsetOther: 'Autre encodage de caractère',
+	charsetTR: 'Turc',
+	charsetUN: 'Unicode (UTF-8)',
+	charsetWE: 'Occidental',
+	chooseColor: 'Choisissez',
+	design: 'Design',
+	docTitle: 'Titre de la page',
+	docType: 'Type de document',
+	docTypeOther: 'Autre type de document',
+	label: 'Propriétés du document',
+	margin: 'Marges',
+	marginBottom: 'Bas',
+	marginLeft: 'Gauche',
+	marginRight: 'Droite',
+	marginTop: 'Haut',
+	meta: 'Métadonnées',
+	metaAuthor: 'Auteur',
+	metaCopyright: 'Copyright',
+	metaDescription: 'Description',
+	metaKeywords: 'Mots-clés (séparés par des virgules)',
+	other: '<autre>',
+	previewHtml: '<p>Ceci est un <strong>texte d\'exemple</strong>. Vous utilisez <a href="javascript:void(0)">CKEditor</a>.</p>',
+	title: 'Propriétés du document',
+	txtColor: 'Couleur de texte',
+	xhtmlDec: 'Inclure les déclarations XHTML'
+} );
---
+++ app/bundles/CoreBundle/Assets/js/libraries/ckeditor/plugins/docprops/lang/ro.js
@@ -0,0 +1,42 @@
+﻿/*
+Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'docprops', 'ro', {
+	bgColor: 'Culoarea fundalului (Background Color)',
+	bgFixed: 'Fundal neflotant, fix (Non-scrolling Background)',
+	bgImage: 'URL-ul imaginii din fundal (Background Image URL)',
+	charset: 'Encoding setului de caractere',
+	charsetASCII: 'ASCII', // MISSING
+	charsetCE: 'Central European', // MISSING
+	charsetCR: 'Chirilic',
+	charsetCT: 'Chinezesc tradiţional (Big5)',
+	charsetGR: 'Grecesc',
+	charsetJP: 'Japonez',
+	charsetKR: 'Corean',
+	charsetOther: 'Alt encoding al setului de caractere',
+	charsetTR: 'Turcesc',
+	charsetUN: 'Unicode (UTF-8)', // MISSING
+	charsetWE: 'Vest european',
+	chooseColor: 'Alege',
+	design: 'Design', // MISSING
+	docTitle: 'Titlul paginii',
+	docType: 'Document Type Heading', // MISSING
+	docTypeOther: 'Alt Document Type Heading',
+	label: 'Proprietăţile documentului',
+	margin: 'Marginile paginii',
+	marginBottom: 'Jos',
+	marginLeft: 'Stânga',
+	marginRight: 'Dreapta',
+	marginTop: 'Sus',
+	meta: 'Meta Tags', // MISSING
+	metaAuthor: 'Autor',
+	metaCopyright: 'Drepturi de autor',
+	metaDescription: 'Descrierea documentului',
+	metaKeywords: 'Cuvinte cheie după care se va indexa documentul (separate prin virgulă)',
+	other: '<alt>',
+	previewHtml: '<p>This is some <strong>sample text</strong>. You are using <a href="javascript:void(0)">CKEditor</a>.</p>', // MISSING
+	title: 'Proprietăţile documentului',
+	txtColor: 'Culoarea textului',
+	xhtmlDec: 'Include declaraţii XHTML'
+} );
