5f2aba22f13ded0e193fd23abbfc4d4121457c74
Alan Hartless <alan@devkardia.com>
2015-03-30 16:11:36 -0500

    Removed the SensioFrameworkExtraBundle bundle that generated unused annotations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -124,7 +124,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            //new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new Bazinga\OAuthServerBundle\BazingaOAuthServerBundle(),
