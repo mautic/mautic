91666ec143c39701950a3d37a4df9216b1f57e6b
Alan Hartless <alan@devkardia.com>
2015-05-05 10:35:54 -0700

    Hotfix for variable misname that affected  campaigns

--- app/bundles/CampaignBundle/Model/EventModel.php
+++ app/bundles/CampaignBundle/Model/EventModel.php
@@ -333,7 +333,7 @@ class EventModel extends CommonFormModel
                         }
 
                         //trigger the action
-                        $response = $this->invokeEventCallback($event, $settings, $lead, $eventDetails, $systemTriggered);
+                        $response = $this->invokeEventCallback($child, $settings, $lead, $eventDetails, $systemTriggered);
                         if ($response !== false) {
                             $logger->debug('CAMPAIGN: ID# '.$child['id'].' successfully executed and logged.');
                             $log = $this->getLogEntity($child['id'], $event['campaign']['id'], $lead, $ipAddress, $systemTriggered);
@@ -342,7 +342,7 @@ class EventModel extends CommonFormModel
 
                             if ($response !== true) {
                                 // Some feed back was given to be passed back to the function calling triggerEvent
-                                $actionResponses[$type][$child['id']] = $response;
+                                $actionResponses[$child['eventType']][$child['id']] = $response;
                                 $log->setMetadata($response);
                             }
 
