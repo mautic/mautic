290dc5b2636a1443b33efaa6b56681a119046afa
David Hurley <david@websparkinc.com>
2015-05-19 14:21:20 -0400

    Removes form alias from list view

--- app/bundles/FormBundle/Views/Form/list.html.php
+++ app/bundles/FormBundle/Views/Form/list.html.php
@@ -92,7 +92,7 @@ if ($tmpl == 'index') {
                     <div>
                         <?php echo $view->render('MauticCoreBundle:Helper:publishstatus_icon.html.php',array('item' => $item, 'model' => 'form.form')); ?>
                         <a href="<?php echo $view['router']->generate('mautic_form_action', array('objectAction' => 'view', 'objectId' => $item->getId())); ?>" data-toggle="ajax" data-menu-link="mautic_form_index">
-                            <?php echo $item->getName() . ' (' . $item->getAlias() . ')'; ?>
+                            <?php echo $item->getName(); ?>
                         </a>
                     </div>
                     <?php if ($description = $item->getDescription()): ?>
