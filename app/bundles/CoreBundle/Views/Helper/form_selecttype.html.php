9582eab32885a95522f34fde39f1257cb136e900
Alan Hartless <alan@devkardia.com>
2015-06-01 00:40:18 -0500

    Moved the type selection for emails to a shared helper template.  Fixed some issues with double tmpl applied to the URL with pagination.

--- app/bundles/CoreBundle/Views/Helper/form_selecttype.html.php
+++ app/bundles/CoreBundle/Views/Helper/form_selecttype.html.php
@@ -6,20 +6,22 @@
  * @link        http://mautic.org
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
+
 ?>
 <script>
-    mauticLang.newListEmail = "<?php echo $view['translator']->trans('mautic.email.type.list.header'); ?>";
-    mauticLang.newTemplateEmail = "<?php echo $view['translator']->trans('mautic.email.type.template.header'); ?>";
+    <?php foreach ($mauticLang as $key => $string): ?>
+    mauticLang.<?php echo $key; ?> = "<?php echo $view['translator']->trans($string); ?>";
+    <?php endforeach; ?>
 </script>
-<div class="email-type-modal-backdrop" style="width: 100%; height: 100%; position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #000000; opacity: 0.9; z-index: 9000"></div>
+<div class="<?php echo $typePrefix; ?>-type-modal-backdrop" style="width: 100%; height: 100%; position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #000000; opacity: 0.9; z-index: 9000"></div>
 
-<div class="modal fade in email-type-modal" style="display: block; z-index: 9999;">
+<div class="modal fade in <?php echo $typePrefix; ?>-type-modal" style="display: block; z-index: 9999;">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <a href="<?php echo $view['router']->generate('mautic_email_index'); ?>" onclick="Mautic.startModalLoadingBar('.email-type-modal');" data-toggle="ajax" class="close" ><span aria-hidden="true">&times;</span></a>
-                <h4 class="modal-title" id="myModalLabel">
-                    <?php echo $view['translator']->trans('mautic.email.type.header'); ?>
+                <a href="javascript: void(0);" onclick="Mautic.closeModalAndRedirect('.<?php echo $typePrefix; ?>-type-modal', '<?php echo $view['router']->generate($cancelUrl); ?>');" class="close" ><span aria-hidden="true">&times;</span></a>
+                <h4 class="modal-title">
+                    <?php echo $view['translator']->trans($header); ?>
                 </h4>
                 <div class="modal-loading-bar"></div>
             </div>
@@ -29,17 +31,17 @@
                         <div class="panel panel-success">
                             <div class="panel-heading">
                                 <div class="col-xs-10 np">
-                                    <h3 class="panel-title"><?php echo $view['translator']->trans('mautic.email.type.list.header'); ?></h3>
+                                    <h3 class="panel-title"><?php echo $view['translator']->trans($typeOneHeader); ?></h3>
                                 </div>
                                 <div class="col-xs-2 pl-0 pr-0 pt-10 pb-10 text-right">
-                                    <i class="fa fa-list fa-lg"></i>
+                                    <i class="fa <?php echo $typeOneIconClass; ?> fa-lg"></i>
                                 </div>
                             </div>
                             <div class="panel-body" style="min-height: 150px;">
-                                <?php echo $view['translator']->trans('mautic.email.type.list.description'); ?>
+                                <?php echo $view['translator']->trans($typeOneDescription); ?>
                             </div>
                             <div class="panel-footer text-center">
-                                <button class="btn btn-lg btn-default btn-nospin text-success" onclick="Mautic.selectEmailType('list');"><?php echo $view['translator']->trans('mautic.core.select'); ?></button>
+                                <button class="btn btn-lg btn-default btn-nospin text-success" onclick="<?php echo $typeOneOnClick; ?>"><?php echo $view['translator']->trans('mautic.core.select'); ?></button>
                             </div>
                         </div>
                     </div>
@@ -47,17 +49,17 @@
                         <div class="panel panel-primary">
                             <div class="panel-heading">
                                 <div class="col-xs-10 np">
-                                    <h3 class="panel-title"><?php echo $view['translator']->trans('mautic.email.type.template.header'); ?></h3>
+                                    <h3 class="panel-title"><?php echo $view['translator']->trans($typeTwoHeader); ?></h3>
                                 </div>
                                 <div class="col-xs-2 pl-0 pr-0 pt-10 pb-10 text-right">
-                                    <i class="fa fa-cube fa-lg"></i>
+                                    <i class="fa <?php echo $typeTwoIconClass; ?> fa-lg"></i>
                                 </div>
                             </div>
                             <div class="panel-body" style="min-height: 150px;">
-                                <?php echo $view['translator']->trans('mautic.email.type.template.description'); ?>
+                                <?php echo $view['translator']->trans($typeTwoDescription); ?>
                             </div>
                             <div class="panel-footer text-center">
-                                <button class="btn btn-lg btn-default btn-nospin text-primary" onclick="Mautic.selectEmailType('template');"><?php echo $view['translator']->trans('mautic.core.select'); ?></button>
+                                <button class="btn btn-lg btn-default btn-nospin text-primary" onclick="<?php echo $typeTwoOnClick; ?>"><?php echo $view['translator']->trans('mautic.core.select'); ?></button>
                             </div>
                         </div>
                     </div>
