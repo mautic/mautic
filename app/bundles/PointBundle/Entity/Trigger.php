c3263c39b08c0fb30b4f52d0c84d9222477c9f32
Jan Linhart <admin@escope.cz>
2015-07-22 11:33:49 +0200

    Point Trigger can be cloned and canceled without saving an entity

--- app/bundles/PointBundle/Entity/Trigger.php
+++ app/bundles/PointBundle/Entity/Trigger.php
@@ -106,6 +106,13 @@ class Trigger extends FormEntity
      */
     private $events;
 
+    public function __clone()
+    {
+        $this->id = null;
+
+        parent::__clone();
+    }
+
     /**
      * Constructor
      */
