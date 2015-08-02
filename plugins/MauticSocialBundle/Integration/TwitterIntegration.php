800a92f12b98264555e500615dfe94ee12f5a21c
Alan Hartless <alan@devkardia.com>
2015-08-02 02:23:19 -0500

    Merge branch 'staging' into addon-to-plugin-conversion
    # Conflicts:
    #	plugins/MauticSocialBundle/Integration/TwitterIntegration.php

--- plugins/MauticSocialBundle/Integration/TwitterIntegration.php
+++ plugins/MauticSocialBundle/Integration/TwitterIntegration.php
@@ -7,7 +7,7 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace MauticAddon\MauticSocialBundle\Integration;
+namespace MauticPlugin\MauticSocialBundle\Integration;
 
 use Mautic\CoreBundle\Helper\EmojiHelper;
 
