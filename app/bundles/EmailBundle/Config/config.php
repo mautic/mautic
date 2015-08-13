d7d67be7b3cd864bfd8423c1633234f6c4bfbbe1
Chad Windnagle <drmmr763@gmail.com>
2015-08-13 17:22:47 -0400

    add event listener to the webhook form for email opens

--- app/bundles/EmailBundle/Config/config.php
+++ app/bundles/EmailBundle/Config/config.php
@@ -116,7 +116,10 @@ return array(
             ),
             'mautic.email.search.subscriber'         => array(
                 'class' => 'Mautic\EmailBundle\EventListener\SearchSubscriber'
-            )
+            ),
+            'mautic.webhook.email.subscriber'                => array(
+                'class' => 'Mautic\EmailBundle\EventListener\WebhookSubscriber'
+            ),
         ),
         'forms'  => array(
             'mautic.form.type.email'                      => array(
