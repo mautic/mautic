f9c84732f8b85deca5379cae7561bfcf98ad3f1b
Chad Windnagle <drmmr763@gmail.com>
2015-08-17 15:06:23 -0400

    add ordering for logs

--- app/bundles/WebhookBundle/Entity/Webhook.php
+++ app/bundles/WebhookBundle/Entity/Webhook.php
@@ -91,10 +91,12 @@ class Webhook extends FormEntity
             ->cascadePersist()
             ->build();
         // 1:M for logs
-        $builder->createOneToMany('logs', 'Log')
+        $builder->createOneToMany('logs', 'Log')->setOrderBy(array('dateAdded' => 'DESC'))
+            ->fetchExtraLazy()
             ->mappedBy('webhook')
             ->cascadePersist()
             ->build();
+
         // status code
         $builder->createField('webhookUrl', 'string')
             ->columnName('webhook_url')
