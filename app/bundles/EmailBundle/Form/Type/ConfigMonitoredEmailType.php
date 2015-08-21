18e13917d6db9bf9052a4fb550d77301b4d52a15
Alan Hartless <alan@devkardia.com>
2015-08-21 00:23:44 -0500

    Prevented fatal for when the  imap extension is not loaded caused by the couple of commits

--- app/bundles/EmailBundle/Form/Type/ConfigMonitoredEmailType.php
+++ app/bundles/EmailBundle/Form/Type/ConfigMonitoredEmailType.php
@@ -40,34 +40,36 @@ class ConfigMonitoredEmailType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $data = $options['data'];
+        if (function_exists('imap_open')) {
+            $data = $options['data'];
 
-        $event = new MonitoredEmailEvent($builder, $data);
+            $event = new MonitoredEmailEvent($builder, $data);
 
-        // Default email bundles
-        $event->addFolder('general', '', 'mautic.email.config.monitored_email.general');
-        $event->addFolder('EmailBundle', 'bounces', 'mautic.email.config.monitored_email.bounce_folder');
-        $event->addFolder('EmailBundle', 'unsubscribes', 'mautic.email.config.monitored_email.unsubscribe_folder');
+            // Default email bundles
+            $event->addFolder('general', '', 'mautic.email.config.monitored_email.general');
+            $event->addFolder('EmailBundle', 'bounces', 'mautic.email.config.monitored_email.bounce_folder');
+            $event->addFolder('EmailBundle', 'unsubscribes', 'mautic.email.config.monitored_email.unsubscribe_folder');
 
-        if ($this->factory->getDispatcher()->hasListeners(EmailEvents::MONITORED_EMAIL_CONFIG)) {
-            $this->factory->getDispatcher()->dispatch(EmailEvents::MONITORED_EMAIL_CONFIG, $event);
-        }
+            if ($this->factory->getDispatcher()->hasListeners(EmailEvents::MONITORED_EMAIL_CONFIG)) {
+                $this->factory->getDispatcher()->dispatch(EmailEvents::MONITORED_EMAIL_CONFIG, $event);
+            }
 
-        $folderSettings = $event->getFolders();
-        foreach ($folderSettings as $key => $settings) {
-            $folderData = (array_key_exists($key, $data)) ? $data[$key] : array();
-            $builder->add(
-                $key,
-                'monitored_mailboxes',
-                array(
-                    'label'          => $settings['label'],
-                    'mailbox'        => $key,
-                    'default_folder' => $settings['default'],
-                    'data'           => $folderData,
-                    'required'       => false,
-                    'general_settings' => (array_key_exists('general', $data)) ? $data['general'] : array(),
-                )
-            );
+            $folderSettings = $event->getFolders();
+            foreach ($folderSettings as $key => $settings) {
+                $folderData = (array_key_exists($key, $data)) ? $data[$key] : array();
+                $builder->add(
+                    $key,
+                    'monitored_mailboxes',
+                    array(
+                        'label'            => $settings['label'],
+                        'mailbox'          => $key,
+                        'default_folder'   => $settings['default'],
+                        'data'             => $folderData,
+                        'required'         => false,
+                        'general_settings' => (array_key_exists('general', $data)) ? $data['general'] : array(),
+                    )
+                );
+            }
         }
     }
 
