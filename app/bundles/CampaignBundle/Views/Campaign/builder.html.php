a8e33b1680c36f4063baa627a426b8f41f34fd49
Jan Linhart <admin@escope.cz>
2015-08-31 13:27:09 +0200

    condition event styling and behavior in the graph

--- app/bundles/CampaignBundle/Views/Campaign/builder.html.php
+++ app/bundles/CampaignBundle/Views/Campaign/builder.html.php
@@ -88,7 +88,7 @@
                 </div>
                 <div class="panel-body">
                     <?php foreach ($eventSettings['condition'] as $k => $e): ?>
-                        <a id="campaignEvent_<?php echo str_replace('.', '', $k); ?>" data-toggle="ajaxmodal" data-target="#CampaignEventModal" class="list-group-item list-campaign-decision" href="<?php echo $view['router']->generate('mautic_campaignevent_action', array('objectAction' => 'new', 'type' => $k, 'eventType'=> 'condition', 'campaignId' => $campaignId)); ?>">
+                        <a id="campaignEvent_<?php echo str_replace('.', '', $k); ?>" data-toggle="ajaxmodal" data-target="#CampaignEventModal" class="list-group-item list-campaign-condition" href="<?php echo $view['router']->generate('mautic_campaignevent_action', array('objectAction' => 'new', 'type' => $k, 'eventType'=> 'condition', 'campaignId' => $campaignId)); ?>">
                             <div data-toggle="tooltip" title="<?php echo $e['description']; ?>">
                                 <span><?php echo $e['label']; ?></span>
                             </div>
