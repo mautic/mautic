a472f19ec8e3781fd5ccde777d599b8cc25179a0
Jan Linhart <admin@escope.cz>
2015-11-30 18:15:35 +0100

    Dashboard module form prepared

---
+++ app/bundles/DashboardBundle/Form/Type/ModuleType.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\DashboardBundle\Form\Type;
+
+use Doctrine\ORM\EntityRepository;
+use Mautic\CoreBundle\Factory\MauticFactory;
+use Mautic\CoreBundle\Form\EventListener\CleanFormSubscriber;
+use Mautic\CoreBundle\Form\EventListener\FormExitSubscriber;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class ModuleType
+ *
+ * @package Mautic\DashboardBundle\Form\Type
+ */
+class ModuleType extends AbstractType
+{
+
+    private $translator;
+
+    /**
+     * @param MauticFactory $factory
+     */
+    public function __construct (MauticFactory $factory)
+    {
+        $this->translator = $factory->getTranslator();
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm (FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text', array(
+            'label'      => 'mautic.dashboard.module.form.name',
+            'label_attr' => array('class' => 'control-label'),
+            'attr'       => array('class' => 'form-control')
+        ));
+
+        // @todo: Make this configurable by other bundles through an event
+        $types = array(
+            'lead.timeline',
+            'page.times_on_site',
+            'email.open_rate'
+        );
+
+        $builder->add('type', 'choice', array(
+            'label'      => 'mautic.dashboard.module.form.type',
+            'choices'    => $types,
+            'label_attr' => array('class' => 'control-label'),
+            'attr'       => array('class' => 'form-control')
+        ));
+
+        $builder->add('width', 'choice', array(
+            'label'      => 'mautic.dashboard.module.form.width',
+            'choices'    => array(
+                '3' => '25%',
+                '4' => '33%',
+                '6' => '50%',
+                '8' => '67%',
+                '9' => '75%',
+                '12' => '100%',
+            ),
+            'label_attr' => array('class' => 'control-label'),
+            'attr'       => array('class' => 'form-control')
+        ));
+
+        $builder->add('height', 'choice', array(
+            'label'      => 'mautic.dashboard.module.form.height',
+            'choices'    => array(
+                '100' => '100px',
+                '200' => '200px',
+                '300' => '300px',
+                '400' => '400px',
+                '500' => '500px',
+                '600' => '600px',
+            ),
+            'label_attr' => array('class' => 'control-label'),
+            'attr'       => array('class' => 'form-control')
+        ));
+
+        // @todo load list of modules here
+        $insertBefore = array(
+            0 => 'mautic.dashboard.module.ordering.last'
+        );
+
+        $builder->add('ordering', 'choice', array(
+            'label'      => 'mautic.dashboard.module.form.ordering',
+            'choices'    => $insertBefore,
+            'label_attr' => array('class' => 'control-label'),
+            'attr'       => array('class' => 'form-control')
+        ));
+
+        $builder->add('buttons', 'form_buttons', array(
+            'apply_text' => false,
+            'save_text'  => 'mautic.core.form.save'
+        ));
+
+        if (!empty($options["action"])) {
+            $builder->setAction($options["action"]);
+        }
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    // public function setDefaultOptions (OptionsResolverInterface $resolver)
+    // {
+    //     $resolver->setDefaults(array(
+    //         'data_class' => 'Mautic\LeadBundle\Entity\LeadNote'
+    //     ));
+    // }
+
+    /**
+     * @return string
+     */
+    public function getName ()
+    {
+        return "module";
+    }
+}
