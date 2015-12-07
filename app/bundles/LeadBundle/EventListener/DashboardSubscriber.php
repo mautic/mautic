fdf7c524cd03f4ee55f0a0203566db650efd0d78
Jan Linhart <admin@escope.cz>
2015-12-07 16:34:22 +0100

    Anonymous vs Identified pie chart implemented as a dashboard module

--- app/bundles/LeadBundle/EventListener/DashboardSubscriber.php
+++ app/bundles/LeadBundle/EventListener/DashboardSubscriber.php
@@ -35,6 +35,9 @@ class DashboardSubscriber extends MainDashboardSubscriber
     protected $types = array(
         'created.leads.in.time' => array(
             'formAlias' => 'lead_dashboard_leads_in_time_module'
+        ),
+        'anonymous.vs.identified.leads' => array(
+            'formAlias' => null
         )
     );
 
@@ -58,13 +61,30 @@ class DashboardSubscriber extends MainDashboardSubscriber
             } else {
                 $data = array(
                     'chartType'   => 'line',
-                    'chartHeight' => $module->getHeight() - 70,
+                    'chartHeight' => $module->getHeight() - 80,
                     'chartData'   => $model->getLeadsLineChartData($params['amount'], $params['timeUnit'])
                 );
 
                 $event->setTemplate('MauticCoreBundle:Helper:chart.html.php');
                 $event->setTemplateData($data);
             }
+            
+            $event->stopPropagation();
+        }
+
+        if ($event->getType() == 'anonymous.vs.identified.leads') {
+            $model = $this->factory->getModel('lead');
+            $module = $event->getModule();
+            $params = $module->getParams();
+
+            $data = array(
+                'chartType'   => 'pie',
+                'chartHeight' => $module->getHeight() - 80,
+                'chartData'   => $model->getAnonymousVsIdentifiedPieChartData()
+            );
+
+            $event->setTemplate('MauticCoreBundle:Helper:chart.html.php');
+            $event->setTemplateData($data);
             
             $event->stopPropagation();
         }
