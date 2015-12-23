ef663a36b16ceb76cf54ad73d086181290450f8f
Alan Hartless <alan@devkardia.com>
2015-12-23 15:53:25 -0600

    Simply updates the file manager user authentication to use Mautic/Symfony's default session rather than $_SESSION in case it differs

--- app/bundles/CoreBundle/Assets/js/libraries/ckeditor/filemanager/connectors/php/user.config.php
+++ app/bundles/CoreBundle/Assets/js/libraries/ckeditor/filemanager/connectors/php/user.config.php
@@ -16,12 +16,13 @@
  *	@copyright	Authors
  */
 
-if (!isset($_COOKIE['mautic_session_name'])) {
-    die();
-}
-session_name($_COOKIE['mautic_session_name']);
-
-session_start();
+// Boot Symfony
+require_once __DIR__ . '/../../../../../../../../../bootstrap.php.cache';
+require_once __DIR__ . '/../../../../../../../../../AppKernel.php';
+$kernel = new AppKernel('prod', false);
+$kernel->boot();
+$session = $kernel->getContainer()->get('session');
+$userId  = $session->get('mautic.user', false);
 
 /**
  *	Check if user is authorized
@@ -30,12 +31,12 @@ session_start();
  *	@return boolean true if access granted, false if no access
  */
 function auth() {
+    global $userId;
     // You can insert your own code over here to check if the user is authorized.
     // If you use a session variable, you've got to start the session first (session_start())
 
-    return (!empty($_SESSION['_sf2_attributes']['mautic.user']));
+    return (!empty($userId));
 }
-
 
 // @todo Work on plugins registration
 // if (isset($config['plugin']) && !empty($config['plugin'])) {
@@ -50,10 +51,10 @@ function auth() {
 
 $fm = new Filemanager();
 
-if (isset($_SESSION['_sf2_attributes'])) {
-    $userDir = $_SESSION['_sf2_attributes']['mautic.imagepath'];
-    $baseDir = $_SESSION['_sf2_attributes']['mautic.basepath'];
-    $docRoot = $_SESSION['_sf2_attributes']['mautic.docroot'];
+if ($userId) {
+    $userDir = $session->get('mautic.imagepath', false);
+    $baseDir = $session->get('mautic.basepath', false);
+    $docRoot = $session->get('mautic.docroot', false);
 
     if (substr($userDir, -1) !== '/') {
         $userDir .= '/';
