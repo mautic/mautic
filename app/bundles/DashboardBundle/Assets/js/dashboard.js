35d6a5fe1dd6cc79fae0688d6673d637b05bfe76
Jan Linhart <admin@escope.cz>
2016-01-07 19:00:12 +0100

    Old dashboard removed

--- app/bundles/DashboardBundle/Assets/js/dashboard.js
+++ app/bundles/DashboardBundle/Assets/js/dashboard.js
@@ -1,23 +1,11 @@
 //DashboardBundle
 Mautic.dashboardOnLoad = function (container) {
-    Mautic.renderReturnRateDoughnut();
-    Mautic.renderClickRateDoughnut();
-    Mautic.updateActiveVisitorCount();
     Mautic.initWidgetSorting();
-
-    // Refresh page visits every 5 sec
-    Mautic.setModeratedInterval('ActiveVisitorsLoop', 'updateActiveVisitorCount', 5000);
 };
 
 Mautic.dashboardOnUnload = function(id) {
     // Trash initialized dashboard vars on app content change.
     mQuery('.jvectormap-tip').remove();
-    if (id === '#app-content') {
-        delete Mautic.dashboardClickRateDoughnutObject;
-        delete Mautic.dashboardReturnRateDoughnutObject;
-        delete Mautic.ActiveVisitorsCount;
-    }
-    Mautic.clearModeratedInterval('ActiveVisitorsLoop');
 };
 
 Mautic.widgetOnLoad = function(container, response) {
@@ -74,95 +62,6 @@ Mautic.saveWidgetSorting = function () {
     });
     Mautic.ajaxActionRequest('dashboard:updateWidgetOrdering', {'ordering': ordering}, function(response) {
         // @todo handle errors
-    });
-}
-
-Mautic.renderReturnRateDoughnut = function () {
-    // Initilize chart only for first time
-    if (typeof Mautic.dashboardReturnRateDoughnutObject === 'object') {
-        return;
-    }
-    var element = mQuery('#return-rate');
-    var visitCount = +element.attr('data-visit-count');
-    var returnCount = +element.attr('data-return-count');
-    var options = {percentageInnerCutout: 65, responsive: false}
-    var data = [
-        {
-            value: returnCount,
-            color:"#4E5D9D",
-            highlight: "#353F6A",
-            label: "Returned"
-        },
-        {
-            value: visitCount - returnCount,
-            color: "#efeeec",
-            highlight: "#EBEBEB",
-            label: "Unique"
-        }
-    ];
-    data = Mautic.emulateNoDataForPieChart(data);
-    var ctx = document.getElementById("return-rate").getContext("2d");
-    Mautic.dashboardReturnRateDoughnutObject = new Chart(ctx).Doughnut(data, options);
-}
-
-Mautic.renderClickRateDoughnut = function () {
-    // Initilize chart only for first time
-    if (typeof Mautic.dashboardClickRateDoughnutObject === 'object') {
-        return;
-    }
-    var element = mQuery('#click-rate');
-    var readCount = +element.attr('data-sent-count');
-    var clickCount = +element.attr('data-click-count');
-    var options = {percentageInnerCutout: 65, responsive: false}
-    var data = [
-        {
-            value: clickCount,
-            color:"#35B4B9",
-            highlight: "#227276",
-            label: "Clicked"
-        },
-        {
-            value: readCount - clickCount,
-            color: "#efeeec",
-            highlight: "#EBEBEB",
-            label: "Not clicked"
-        }
-    ];
-    data = Mautic.emulateNoDataForPieChart(data);
-    var ctx = document.getElementById("click-rate").getContext("2d");
-    Mautic.dashboardClickRateDoughnutObject = new Chart(ctx).Doughnut(data, options);
-}
-
-Mautic.updateActiveVisitorCount = function () {
-    mQuery.ajax({
-        url: mauticAjaxUrl,
-        type: "POST",
-        data: "action=dashboard:viewingVisitors",
-        dataType: "json",
-        success: function (response) {
-            if (response.success) {
-                var element = mQuery('#active-visitors');
-                element.text(response.viewingVisitors);
-                if (response.viewingVisitors != Mautic.ActiveVisitorsCount) {
-                    var color = '#34D43B';
-                    if (Mautic.ActiveVisitorsCount > response.viewingVisitors) {
-                        color = '#BC2525';
-                    }
-                    element.css('text-shadow', color+' 0px 0px 50px');
-                    setTimeout(function() {
-                        element.css('text-shadow', '#fff 0px 0px 50px');
-                    }, 3000);
-                }
-                Mautic.ActiveVisitorsCount = response.viewingVisitors;
-            }
-
-            Mautic.moderatedIntervalCallbackIsComplete('ActiveVisitorsLoop');
-        },
-        error: function (request, textStatus, errorThrown) {
-            Mautic.processAjaxError(request, textStatus, errorThrown);
-
-            Mautic.moderatedIntervalCallbackIsComplete('ActiveVisitorsLoop');
-        }
     });
 }
 
--- app/bundles/DashboardBundle/Controller/DashboardController.php
+++ app/bundles/DashboardBundle/Controller/DashboardController.php
@@ -28,120 +28,11 @@ class DashboardController extends FormController
      */
     public function indexAction()
     {
-        /** @var \Mautic\PageBundle\Entity\HitRepository $hitRepo */
-        $hitRepo        = $this->factory->getEntityManager()->getRepository('MauticPageBundle:Hit');
-        /** @var \Mautic\EmailBundle\Entity\StatRepository $emailStatRepo */
-        $emailStatRepo  = $this->factory->getEntityManager()->getRepository('MauticEmailBundle:Stat');
-        /** @var \Mautic\CoreBundle\Entity\IpAddressRepository $ipAddressRepo */
-        $ipAddressRepo  = $this->factory->getEntityManager()->getRepository('MauticCoreBundle:IpAddress');
-        /** @var \Mautic\EmailBundle\Entity\EmailRepository $emailRepo */
-        $emailRepo      = $this->factory->getModel('email')->getRepository();
-
-        $sentReadCount        = $emailRepo->getSentReadCount();
-        $clickthroughCount    = $hitRepo->countEmailClickthrough();
-        $newReturningVisitors = array(
-            'returning' => $hitRepo->countReturningIp(),
-            'unique'    => $ipAddressRepo->countIpAddresses()
-        );
-        $weekVisitors         = $hitRepo->countVisitors(604800);
-        $allTimeVisitors      = $hitRepo->countVisitors(0);
-        $allSentEmails        = $emailStatRepo->getSentCount();
-
-        /** @var \Mautic\PageBundle\Model\PageModel $pageModel */
-        $pageModel            = $this->factory->getModel('page');
-        $popularPages   = $pageModel->getRepository()->getPopularPages();
-
-        $popularAssets = $this->factory->getModel('asset')->getRepository()->getPopularAssets();
-
-        $popularCampaigns = $this->factory->getModel('campaign')->getRepository()->getPopularCampaigns();
-
-        $returnRate = 0;
-        $totalVisits = array_sum($newReturningVisitors);
-        if ($totalVisits > 0) {
-            $returnRate = round($newReturningVisitors['returning'] / $totalVisits * 100);
-        }
-
-        $clickRate = 0;
-
-        if ($sentReadCount['sent_count'] > 0) {
-            $clickRate = round($clickthroughCount / $sentReadCount['sent_count'] * 100);
-        }
-
-        /** @var \Mautic\PageBundle\Model\LeadModel $leadModel */
-        $leadModel = $this->factory->getModel('lead');
-        $mapData   = $leadModel->getLeadMapData();
-
-        // Audit Log
-        $logs = $this->factory->getModel('core.auditLog')->getLogForObject(null, null);
-
-        // Get names of log's items
-        $router = $this->factory->getRouter();
-        foreach ($logs as $key => &$log) {
-            if (!empty($log['bundle']) && !empty($log['object']) && !empty($log['objectId'])) {
-                try {
-                    $model = $this->factory->getModel($log['bundle'].'.'.$log['object']);
-                    $item  = $model->getEntity($log['objectId']);
-                    if (method_exists($item, $model->getNameGetter())) {
-                        $log['objectName'] = $item->{$model->getNameGetter()}();
-
-                        if ($log['bundle'] == 'lead' && $log['objectName'] == 'mautic.lead.lead.anonymous') {
-                            $log['objectName'] = $this->factory->getTranslator()->trans('mautic.lead.lead.anonymous');
-                        }
-                    } else {
-                        $log['objectName'] = '';
-                    }
-
-                    $routeName = 'mautic_'.$log['bundle'].'_action';
-                    if ($router->getRouteCollection()->get($routeName) !== null) {
-                        $log['route'] = $router->generate(
-                            'mautic_'.$log['bundle'].'_action',
-                            array('objectAction' => 'view', 'objectId' => $log['objectId'])
-                        );
-                    } else {
-                        $log['route'] = false;
-                    }
-                } catch (\Exception $e) {
-                    unset($logs[$key]);
-                }
-            }
-        }
-
-        $event = new IconEvent($this->factory->getSecurity());
-        $this->factory->getDispatcher()->dispatch(CoreEvents::FETCH_ICONS, $event);
-        $icons = $event->getIcons();
-
-        // Upcoming emails from Campaign Bundle
-        /** @var \Mautic\CampaignBundle\Entity\LeadEventLogRepository $leadEventLogRepository */
-        $leadEventLogRepository = $this->factory->getEntityManager()->getRepository('MauticCampaignBundle:LeadEventLog');
-        $upcomingEmails = $leadEventLogRepository->getUpcomingEvents(array('type' => 'email.send', 'scheduled' => 1, 'eventType' => 'action'));
-
-        $leadModel = $this->factory->getModel('lead.lead');
-
-        foreach ($upcomingEmails as &$email) {
-            $email['lead'] = $leadModel->getEntity($email['lead_id']);
-        }
-
-        /** @var \Mautic\DashBundle\Model\DashboardModel $pageModel */
+        /** @var \Mautic\DashBundle\Model\DashboardModel $model */
         $model = $this->factory->getModel('dashboard');
 
         return $this->delegateView(array(
             'viewParameters'  =>  array(
-                'sentReadCount'     => $sentReadCount,
-                'clickthroughCount' => $clickthroughCount,
-                'returnRate'        => $returnRate,
-                'clickRate'         => $clickRate,
-                'newReturningVisitors' => $newReturningVisitors,
-                'weekVisitors'      => $weekVisitors,
-                'allTimeVisitors'   => $allTimeVisitors,
-                'popularPages'      => $popularPages,
-                'popularAssets'     => $popularAssets,
-                'popularCampaigns'  => $popularCampaigns,
-                'allSentEmails'     => $allSentEmails,
-                'mapData'           => $mapData,
-                'logs'              => $logs,
-                'icons'             => $icons,
-                'upcomingEmails'    => $upcomingEmails,
-                'leadLineChart'     => $leadModel->getLeadsLineChartData(30, 'd'),
                 'security'          => $this->factory->getSecurity(),
                 'widgets'           => $model->getWidgets()
             ),
@@ -161,7 +52,6 @@ class DashboardController extends FormController
      */
     public function newAction()
     {
-
         //retrieve the entity
         $widget = new Widget();
 
