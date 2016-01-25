d2c640919fa27b814983f9b565122eb499c462ba
Alan Hartless <alan@devkardia.com>
2016-01-25 11:41:10 -0600

    Dev version bump

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,7 +40,7 @@ class AppKernel extends Kernel
      *
      * @const integer
      */
-    const PATCH_VERSION = 4;
+    const PATCH_VERSION = 5;
 
     /**
      * Extra version identifier
@@ -50,7 +50,7 @@ class AppKernel extends Kernel
      *
      * @const string
      */
-    const EXTRA_VERSION = '';
+    const EXTRA_VERSION = '-dev';
 
     /**
      * @var array
