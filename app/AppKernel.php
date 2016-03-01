271a9df1087e41e9956810e6b4c84047892ab5b7
Donald Gilbert <don@electriceasel.com>
2016-03-01 10:41:33 -0500

    Dev version bump

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,7 +40,7 @@ class AppKernel extends Kernel
      *
      * @const integer
      */
-    const PATCH_VERSION = 0;
+    const PATCH_VERSION = 1;
 
     /**
      * Extra version identifier
@@ -50,7 +50,7 @@ class AppKernel extends Kernel
      *
      * @const string
      */
-    const EXTRA_VERSION = '';
+    const EXTRA_VERSION = '-dev';
 
     /**
      * @var array
