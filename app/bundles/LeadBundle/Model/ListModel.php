d3200917d21c935f0057739eec0af1a15de44652
David Coutelle <dco@webmecanik.com>
2016-04-21 18:08:51 +0200

    Update ListModel.php
    Indentation fix

--- app/bundles/LeadBundle/Model/ListModel.php
+++ app/bundles/LeadBundle/Model/ListModel.php
@@ -321,18 +321,18 @@ class ListModel extends FormModel
                 ),
                 'operators'  => $operators['bool']
             ),
-            'hit_url'   => array(
-                'label'      => $this->translator->trans('mautic.page.url'),
-                'properties' => array(
-                    'type'     => 'text'
-                ),
-                'operators'  => array(
-                    'include' => array(
-                        '=',
-	                    'like'
-                    )
-                )
-            )
+	    'hit_url'   => array(
+	    'label'      => $this->translator->trans('mautic.page.url'),
+	    'properties' => array(
+	    'type'     => 'text'
+	    ),
+	        'operators'  => array(
+	        'include' => array(
+	            '=',
+	            'like'
+	        )
+	        )
+	    )
         		
         );
 
