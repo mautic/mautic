83e4116d6de7ffe8fa0acce16c5bc1bd115cbfde
Jan Linhart <admin@escope.cz>
2016-04-21 14:29:42 +0200

    Fix of importing donotcontact iformation

--- app/bundles/LeadBundle/Model/LeadModel.php
+++ app/bundles/LeadBundle/Model/LeadModel.php
@@ -984,6 +984,8 @@ class LeadModel extends FormModel
                     "%user%" => $this->factory->getUser()->getUsername()
                 ));
 
+                // The email must be set for successful unsubscribtion
+                $lead->addUpdatedField('email', $data[$fields['email']]);
                 $this->unsubscribeLead($lead, $reason, false);
             }
         }
