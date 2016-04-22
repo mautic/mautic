83977eac4058807e03de5aeaf9b0cbd00a62fdc3
gagan_deep <gagandeep@pelbox.com>
2016-04-22 19:01:50 +0530

    Changed records to record and validation usernamepass and fixed formating

--- app/bundles/CoreBundle/IpLookup/IP2LocationBinLookup.php
+++ app/bundles/CoreBundle/IpLookup/IP2LocationBinLookup.php
@@ -5,7 +5,7 @@
  * @author      Mautic
  * @link        http://mautic.org
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
-*/
+ */
 
 namespace Mautic\CoreBundle\IpLookup;
 use IP2Location;
@@ -22,9 +22,10 @@ class IP2LocationBinLookup extends AbstractLocalDataLookup
     /**
      * @return string
      */
-	 
+
     public function getLocalDataStoreFilepath()
-    {  return $this->getDataDir();
+    {
+        return $this->getDataDir();
     }
 
     /**
@@ -32,16 +33,20 @@ class IP2LocationBinLookup extends AbstractLocalDataLookup
      */
     public function getRemoteDateStoreDownloadUrl()
     {
-	   $usernamePass = explode(':',$this->auth);
-	   $data= array();
-	   $data['login'] = $usernamePass[0];
-	   $data['password'] = $usernamePass[1];
-	   $data['productcode'] = 'DB9BIN';
-       $queryString =  http_build_query($data);
-	   // the system gets the file name from end of remove file path url so use hardedcoded name
-	    $queryString .='&filename=/ip2locaion.zip';
-	   return 'https://www.ip2location.com/download?'.$queryString;
-	   
+        $usernamePass = explode(':',$this->auth);
+        $data= array();
+        if(isset($usernamePass[0]) && isset($usernamePass[1])){
+            $data['login'] = $usernamePass[0];
+            $data['password'] = $usernamePass[1];
+            $data['productcode'] = 'DB9BIN';
+            $queryString =  http_build_query($data);
+            // the system gets the file name from end of remove file path url so use hardedcoded name
+            $queryString .='&filename=/ip2locaion.zip';
+            return 'https://www.ip2location.com/download?'.$queryString;
+        }else{
+            $this->logger->warn('Both username and password are required');
+        }
+
     }
 
     /**
@@ -49,20 +54,20 @@ class IP2LocationBinLookup extends AbstractLocalDataLookup
      */
     protected function lookup()
     {
-		
+
 
         try {
-			$reader = new IP2Location($this->getLocalDataStoreFilepath().'/IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE.BIN', IP2Location::FILE_IO);
-			$record = $reader->lookup($this->ip, IP2Location::ALL);
-			
-			if(isset($record->countryName)){
-				$this->country = $record->countryName;
-				$this->region = $record->regionName;
-				$this->city = $record->cityName;
-				$this->latitude  = $record->latitude;
-				$this->longitude = $record->longitude;
-				$this->zipcode   = $records->zipCode;
-			}
+            $reader = new IP2Location($this->getLocalDataStoreFilepath().'/IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE.BIN', IP2Location::FILE_IO);
+            $record = $reader->lookup($this->ip, IP2Location::ALL);
+
+            if(isset($record->countryName)){
+                $this->country = $record->countryName;
+                $this->region = $record->regionName;
+                $this->city = $record->cityName;
+                $this->latitude  = $record->latitude;
+                $this->longitude = $record->longitude;
+                $this->zipcode   = $record->zipCode;
+            }
         } catch (\Exception $exception) {
             if ($this->logger) {
                 $this->logger->warn("IP LOOKUP: " . $exception->getMessage());
@@ -70,5 +75,3 @@ class IP2LocationBinLookup extends AbstractLocalDataLookup
         }
     }
 } 
-
-
