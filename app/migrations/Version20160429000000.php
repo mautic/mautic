b4ad6efd9b69d3ca8197bb771c4c595a06d3f7c6
Alan Hartless <alan@devkardia.com>
2016-05-03 01:23:17 -0500

    Migrations for trackable channel clicks

--- app/migrations/Version20160429000000.php
+++ app/migrations/Version20160429000000.php
@@ -19,6 +19,9 @@ use Mautic\LeadBundle\Entity\DoNotContact;
  */
 class Version20160429000000 extends AbstractMauticMigration
 {
+    private $redirectIdx;
+    private $redirectFk;
+
     /**
      * @param Schema $schema
      *
@@ -27,10 +30,13 @@ class Version20160429000000 extends AbstractMauticMigration
      */
     public function preUp(Schema $schema)
     {
-        if ($schema->hasTable($this->prefix.'url_trackables')) {
+        if ($schema->hasTable($this->prefix.'channel_url_trackables')) {
 
             throw new SkipMigrationException('Schema includes this migration');
         }
+
+        $this->redirectIdx = $this->generatePropertyName($this->prefix . 'channel_url_trackables', 'idx', array('redirect_id'));
+        $this->redirectFk  = $this->generatePropertyName($this->prefix . 'channel_url_trackables', 'fk', array('redirect_id'));
     }
 
     /**
@@ -40,10 +46,22 @@ class Version20160429000000 extends AbstractMauticMigration
     {
 
         $sql = <<<SQL
-
+CREATE TABLE {$this->prefix}channel_url_trackables (
+  redirect_id INT NOT NULL, 
+  channel_id INT NOT NULL, 
+  channel VARCHAR(255) NOT NULL, 
+  hits INT NOT NULL, 
+  unique_hits INT NOT NULL, 
+  INDEX {$this->redirectIdx} (redirect_id),
+  INDEX {$this->prefix}channel_url_trackable_search (channel, channel_id),
+  PRIMARY KEY(redirect_id, channel_id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB
 SQL;
-
         $this->addSql($sql);
+
+        $this->addSql("ALTER TABLE {$this->prefix}channel_url_trackables ADD CONSTRAINT {$this->redirectFk} FOREIGN KEY (redirect_id) REFERENCES {$this->prefix}page_redirects (id) ON DELETE CASCADE");
+
+        $this->addSql("CREATE INDEX {$this->prefix}page_redirect_url_search ON {$this->prefix}page_redirects (url)");
     }
 
     /**
@@ -51,17 +69,22 @@ SQL;
      */
     public function postgresqlUp(Schema $schema)
     {
-        $this->addSql("CREATE SEQUENCE {$this->prefix}lead_donotcontact_id_seq INCREMENT BY 1 MINVALUE 1 START 1");
-
         $sql = <<<SQL
-CREATE TABLE mautic_url_trackables (redirect_id INT NOT NULL, source_id INT NOT NULL, source VARCHAR(255) NOT NULL, PRIMARY KEY(redirect_id, source_id));
-CREATE INDEX IDX_AE477DCDB42D874D ON mautic_url_trackables (redirect_id);
-ALTER TABLE mautic_url_trackables ADD CONSTRAINT FK_AE477DCDB42D874D FOREIGN KEY (redirect_id) REFERENCES mautic_page_redirects (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE;
-
-);
+CREATE TABLE {$this->prefix}channel_url_trackables (
+  redirect_id INT NOT NULL, 
+  channel_id INT NOT NULL, 
+  channel VARCHAR(255) NOT NULL, 
+  hits INT NOT NULL, 
+  unique_hits INT NOT NULL, PRIMARY KEY(redirect_id, channel_id)
+)
 SQL;
-
         $this->addSql($sql);
+
+        $this->addSql("CREATE INDEX {$this->redirectIdx} ON {$this->prefix}channel_url_trackables (redirect_id)");
+        $this->addSql("CREATE INDEX {$this->prefix} ON {$this->prefix}channel_url_trackables (channel, channel_id)");
+        $this->addSql("ALTER TABLE {$this->prefix}channel_url_trackables ADD CONSTRAINT {$this->redirectFk} FOREIGN KEY (redirect_id) REFERENCES {$this->prefix}page_redirects (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE");
+
+        $this->addSql("CREATE INDEX {$this->prefix}page_redirect_url_search ON {$this->prefix}page_redirects (url)");
     }
 
     /**
@@ -74,7 +97,7 @@ SQL;
         $logger = $this->factory->getLogger();
         $qb = $this->connection->createQueryBuilder();
 
-        $qb->select('r.id, r.email_id')
+        $qb->select('r.id, r.email_id, r.hits, r.unique_hits')
             ->from($this->prefix.'page_redirects', 'r')
             ->where(
                 $qb->expr()->isNotNull('r.email_id')
@@ -90,7 +113,9 @@ SQL;
                 $insert = array(
                     'redirect_id' => $row['id'],
                     'channel'     => 'email',
-                    'channel_id'  => $row['email_id']
+                    'channel_id'  => $row['email_id'],
+                    'hits'        => $row['hits'],
+                    'unique_hits' => $row['unique_hits']
                 );
 
                 $this->connection->insert($this->prefix.'channel_url_trackables', $insert);
@@ -110,23 +135,5 @@ SQL;
             $start += 500;
             $qb->setFirstResult($start);
         }
-
-        // Update all redirects with an email_id as trackable
-        $qb = $this->connection->createQueryBuilder();
-        $qb->update($this->prefix.'page_redirects')
-            ->set($qb->expr()->eq('is_trackable', ':true'))
-            ->setParameter('true', true, 'boolean')
-            ->where($qb->expr()->isNotNull('email_id'))
-            ->execute();
-
-        // Update all redirects without an email_id as not trackable
-        $qb = $this->connection->createQueryBuilder();
-        $qb->update($this->prefix.'page_redirects')
-            ->set($qb->expr()->eq('is_trackable', ':false'))
-            ->setParameter('false', false, 'boolean')
-            ->where($qb->expr()->isNull('email_id'))
-            ->execute();
-
-
     }
 }
