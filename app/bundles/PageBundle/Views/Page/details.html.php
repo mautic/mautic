c93f9c7f6024f95d0a1579462be4c4f0b95f2e41
Jan Linhart <admin@escope.cz>
2016-05-11 19:38:28 +0200

    New line chart added to the landing page detail instead of the 3 small charts

--- app/bundles/PageBundle/Views/Page/details.html.php
+++ app/bundles/PageBundle/Views/Page/details.html.php
@@ -116,10 +116,35 @@ $view['slots']->set(
             </div>
             <!--/ page detail collapseable toggler -->
 
+            <!-- some stats -->
+            <div class="pa-md">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="panel">
+                            <div class="panel-body box-layout">
+                                <div class="col-md-3 va-m">
+                                    <h5 class="text-white dark-md fw-sb mb-xs">
+                                        <span class="fa fa-line-chart"></span>
+                                        <?php echo $view['translator']->trans('mautic.page.pageviews'); ?>
+                                    </h5>
+                                </div>
+                                <div class="col-md-9 va-m">
+                                    <?php echo $view->render('MauticCoreBundle:Helper:graph_dateselect.html.php', array('dateRangeForm' => $dateRangeForm, 'class' => 'pull-right')); ?>
+                                </div>
+                            </div>
+                            <div class="pt-0 pl-15 pb-10 pr-15">
+                                <?php echo $view->render('MauticCoreBundle:Helper:chart.html.php', array('chartData' => $stats['pageviews'], 'chartType' => 'line', 'chartHeight' => 300)); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/ stats -->
+
             <!--
             some stats: need more input on what type of form data to show.
             delete if it is not require
-            -->
+
             <div class="pa-md">
                 <div class="row stat-boxes">
                     <div class="col-md-4">
