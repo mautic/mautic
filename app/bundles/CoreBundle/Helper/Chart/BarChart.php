7d999bd5bfdd37f38d196d9af534e6b9ae5f2ee8
Jan Linhart <admin@escope.cz>
2016-05-13 11:16:21 +0200

    Line chart does not inherit bar chart anymore. Bar chart can be used for non time related data

--- app/bundles/CoreBundle/Helper/Chart/BarChart.php
+++ app/bundles/CoreBundle/Helper/Chart/BarChart.php
@@ -18,50 +18,20 @@ use Mautic\CoreBundle\Helper\Chart\ChartInterface;
 class BarChart extends AbstractChart implements ChartInterface
 {
     /**
-     * Configurable date format
-     *
-     * @var string
-     */
-    protected $dateFormat;
-
-    /**
-     * Match date/time unit to a humanly readable label
-     * {@link php.net/manual/en/function.date.php#refsect1-function.date-parameters}
-     *
-     * @var array
-     */
-    protected $labelFormats = array(
-        's' => 'H:i:s',
-        'i' => 'H:i',
-        'H' => 'M j ga',
-        'd' => 'M j, y', 'D' => 'M j, y', // ('D' is BC. Can be removed when all charts use this class)
-        'W' => '\W\e\e\k W', // (Week is escaped here so it's not interpreted when creating labels)
-        'm' => 'M Y', 'M' => 'M Y', // ('M' is BC. Can be removed when all charts use this class)
-        'Y' => 'Y',
-    );
-
-    /**
      * Defines the basic chart values, generates the time axe labels from it
      *
-     * @param string   $unit {@link php.net/manual/en/function.date.php#refsect1-function.date-parameters}
-     * @param \DateTime $dateFrom
-     * @param \DateTime $dateTo
-     * @param string   $dateFormat
+     * @param array $labels
      */
-    public function __construct($unit, $dateFrom, $dateTo, $dateFormat = null)
+    public function __construct(array $labels)
     {
-        $this->setDateRange($dateFrom, $dateTo);
-        $this->unit  = !$unit ? $this->getTimeUnitFromDateRange() : $unit;
-        $this->dateFormat = $dateFormat;
-        $this->amount = $this->countAmountFromDateRange();
-        $this->generateTimeLabels($this->amount);
-        $this->addOneUnitMinusOneSec($this->dateTo);
+        $this->labels = $labels;
     }
 
     /**
      * Render chart data
      */
     public function render() {
+        ksort($this->datasets);
         return array(
             'labels'   => $this->labels,
             'datasets' => $this->datasets
@@ -71,12 +41,13 @@ class BarChart extends AbstractChart implements ChartInterface
     /**
      * Define a dataset by name and data. Method will add the rest.
      *
-     * @param  string $label
-     * @param  array  $data
+     * @param  string  $label
+     * @param  array   $data
+     * @param  integer $order
      *
      * @return $this
      */
-    public function setDataset($label = null, array $data)
+    public function setDataset($label = null, array $data, $order = null)
     {
         $datasetId = count($this->datasets);
 
@@ -84,33 +55,14 @@ class BarChart extends AbstractChart implements ChartInterface
             'label' => $label,
             'data'  => $data,
         );
+
+        if ($order === null) {
+            $order = count($this->datasets);
+        }
         
-        $this->datasets[] = array_merge($baseData, $this->generateColors($datasetId));
+        $this->datasets[$order] = array_merge($baseData, $this->generateColors($datasetId));
 
         return $this;
-    }
-
-    /**
-     * Generate array of labels from the form data
-     *
-     * @param  integer  $amount
-     */
-    public function generateTimeLabels($amount)
-    {
-        if (!isset($this->labelFormats[$this->unit])) {
-            throw new \UnexpectedValueException('Date/Time unit "' . $this->unit . '" is not available for a label.');
-        }
-
-        $date    = clone $this->dateFrom;
-        $oneUnit = $this->getUnitInterval();
-        $format  = !empty($this->dateFormat) ? $this->dateFormat : $this->labelFormats[$this->unit];
-
-        for ($i = 0; $i < $amount; $i++) {
-            $this->labels[] = $date->format($format);
-            $date->add($oneUnit);
-        }
-        
-        $this->labels = $this->labels;
     }
 
     /**
@@ -126,7 +78,7 @@ class BarChart extends AbstractChart implements ChartInterface
 
         return array(
             'fill' => true,
-            'backgroundColor'           => $color->toRgba(0.1),
+            'backgroundColor'           => $color->toRgba(0.5),
             'borderColor'               => $color->toRgba(0.8),
             'pointHoverBackgroundColor' => $color->toRgba(0.75),
             'pointHoverBorderColor'     => $color->toRgba(1)
