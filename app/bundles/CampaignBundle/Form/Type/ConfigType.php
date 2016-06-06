cd857dbd395d2dd1f7a7b40969842f4eb017931d
lan <lan@webmecanik.com>
2016-06-06 14:32:06 +0200

    US6608 adding a period's list

--- app/bundles/CampaignBundle/Form/Type/ConfigType.php
+++ app/bundles/CampaignBundle/Form/Type/ConfigType.php
@@ -16,7 +16,7 @@ class ConfigType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add(
+        /*$builder->add(
             'campaign_time_wait_on_event_false',
             'text',
             array(
@@ -28,7 +28,39 @@ class ConfigType extends AbstractType
                     'tooltip' => 'mautic.campaignconfig.campaign_time_wait_on_event_false_tooltip'
                 )
             )
-        );
+        );*/
+
+        $builder->add(
+            'campaign_time_wait_on_event_false',
+            'choice',
+            array(
+                'label' => 'mautic.campaignconfig.campaign_time_wait_on_event_false',
+                'label_attr'  => array('class' => 'control-label'),
+                'data'  => $options['data']['campaign_time_wait_on_event_false'],
+                'choices'  => array(
+                    'null' => '0 mn',
+                    'PT15M' => '15 mn',
+                    'PT30M' => '30 mn',
+                    'PT45M' => '45 mn',
+                    'PT1H' => '1 h',
+                    'PT2H' => '2 h',
+                    'PT4H' => '4 h',
+                    'PT8H' => '8 h',
+                    'PT12H' => '12 h',
+                    'PT1D' => '24 h',
+                    'PT3D' => '3 days',
+                    'PT5D' => '5 days',
+                    'PT14D' => '1 week',
+                    'P3M' => '3 months',
+                ),
+                'attr'  => array(
+                    'class' => 'form-control',
+                    'tooltip' => 'mautic.campaignconfig.campaign_time_wait_on_event_false_tooltip'
+                ),
+                'required' => true,
+        ));
+
+
     }
 
     /**
