7ce7f3f68461762906a53b95d34dc7649814a53d
Jan Linhart <admin@escope.cz>
2016-06-16 21:30:42 +0200

    CKEditor library removed

--- app/bundles/CoreBundle/Assets/js/libraries/ckeditor/plugins/sourcedialog/lang/it.js
+++
@@ -1,9 +0,0 @@
-﻿/*
-Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.md or http://ckeditor.com/license
-*/
-
-CKEDITOR.plugins.setLang( 'sourcedialog', 'it', {
-	toolbar: 'Sorgente',
-	title: 'Sorgente'
-} );
