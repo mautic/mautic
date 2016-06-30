45997745c2cfa3013c1d1b3a1e8fe8bdd7e95e65
Don Gilbert <don.gilbert@mautic.com>
2016-06-30 17:25:06 -0400

    Remove type col from getLeadTimelineEvents method

--- app/bundles/LeadBundle/Entity/StagesChangeLog.php
+++ app/bundles/LeadBundle/Entity/StagesChangeLog.php
@@ -83,29 +83,6 @@ class StagesChangeLog
     }
 
     /**
-     * Set type
-     *
-     * @param string $type
-     * @return StagesChangeLog
-     */
-    public function setType($type)
-    {
-        $this->type = $type;
-
-        return $this;
-    }
-
-    /**
-     * Get type
-     *
-     * @return string
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
      * Set eventName
      *
      * @param string $eventName
