a9b2efc55f9fe68a44c7fec6cd40358bcb7fdcc7
Jan Linhart <admin@escope.cz>
2016-07-07 12:52:59 +0200

    Fixed error on week view for the report widgets

--- app/bundles/CoreBundle/Helper/Chart/ChartQuery.php
+++ app/bundles/CoreBundle/Helper/Chart/ChartQuery.php
@@ -296,7 +296,7 @@ class ChartQuery extends AbstractChart
                      * PHP DateTime cannot parse the Y W (ex 2016 09)
                      * format, so we transform it into d-M-Y.
                      */
-                    if ($this->unit === 'W') {
+                    if ($this->unit === 'W' && isset($item['date'])) {
                         list($year, $week) = explode(' ', $item['date']);
                         $newDate = new \DateTime();
                         $newDate->setISODate($year, $week);
