7befa12f8d47b605c98818ab64f0592b2a48f4db
Jan Linhart <admin@escope.cz>
2016-07-12 16:11:13 +0200

    Prevent error when sending an email with attachment from CLI (request doesn't exist)

--- app/bundles/AssetBundle/Model/AssetModel.php
+++ app/bundles/AssetBundle/Model/AssetModel.php
@@ -233,7 +233,10 @@ class AssetModel extends FormModel
 
         $download->setCode($code);
         $download->setIpAddress($ipAddress);
-        $download->setReferer($request->server->get('HTTP_REFERER'));
+
+        if (isset($request->server)) {
+            $download->setReferer($request->server->get('HTTP_REFERER'));
+        }
 
         // Dispatch event
         if ($this->dispatcher->hasListeners(AssetEvents::ASSET_ON_LOAD)) {
