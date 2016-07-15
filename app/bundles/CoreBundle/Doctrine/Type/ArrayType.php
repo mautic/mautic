c2e65c4a2c9effd1c55dba2b4a93cac438744070
Jan Linhart <admin@escope.cz>
2016-07-15 20:22:41 +0200

    Store new arrays as JSON instead of serialized PHP array

--- app/bundles/CoreBundle/Doctrine/Type/ArrayType.php
+++ app/bundles/CoreBundle/Doctrine/Type/ArrayType.php
@@ -16,73 +16,32 @@ use Doctrine\DBAL\Types\ConversionException;
 use Mautic\CoreBundle\Helper\UTF8Helper;
 
 /**
- * Type that maps a PHP array to a clob SQL type.
+ * Type that maps a PHP/JSON array to a clob SQL type.
  *
  * @since 2.0
  */
-class ArrayType extends \Doctrine\DBAL\Types\ArrayType
+class ArrayType extends \Doctrine\DBAL\Types\JsonArrayType
 {
     /**
      * {@inheritdoc}
      */
-    public function convertToDatabaseValue($value, AbstractPlatform $platform)
-    {
-        if (!is_array($value)) {
-            return (null === $value) ? 'N;' : 'a:0:{}';
-        }
-
-        // MySQL will crap out on corrupt UTF8 leading to broken serialized strings
-        array_walk(
-            $value,
-            function (&$entry) {
-                $entry = UTF8Helper::toUTF8($entry);
-            }
-        );
-
-        $serialized = serialize($value);
-
-        if (false !== strpos($serialized, chr(0))) {
-            $serialized = str_replace("\0", '__NULL_BYTE__', $serialized);
-            throw new ConversionException('Serialized array includes null-byte. This cannot be saved as a text. Please check if you not provided object with protected or private members. Serialized Array: '.$serialized);
-        }
-
-        return $serialized;
-    }
-
-    /**
-     * @param mixed $value
-     *
-     * @return array
-     */
     public function convertToPHPValue($value, AbstractPlatform $platform)
     {
-        try {
-            $value = parent::convertToPHPValue($value, $platform);
-            if (!is_array($value) || (1 > count($value))) {
-                return $value;
-            }
-
-            foreach ($value as $key => $element) {
-                if (!is_object($element)) {
-                    continue;
-                }
-
-                $reflectionObject     = new \ReflectionObject($element);
-                $reflectionProperties = $reflectionObject->getProperties(\ReflectionProperty::IS_PROTECTED | \ReflectionProperty::IS_PRIVATE);
-
-                // Let's check if $value contains objects with private or protected members.
-                // If it contains such objects we have to remove them from $array.
-                // This will "heal" the database. There must be no null bytes.
-                if (0 < count($reflectionProperties)) {
-                    unset($value[$key]);
-                }
-            }
-
-            return $value;
-        } catch (ConversionException $exception) {
-            return [];
-        } catch (\ErrorException $exeption) {
-            return [];
+        if ($value === null) {
+            return null;
         }
+
+        $value = (is_resource($value)) ? stream_get_contents($value) : $value;
+
+        if (substr($value, 0, 1) === '{') {
+            return parent::convertToPHPValue($value, $platform);
+        }
+
+        $val = unserialize($value);
+        if ($val === false && $value != 'b:0;') {
+            throw ConversionException::conversionFailed($value, $this->getName());
+        }
+
+        return $val;
     }
 }
