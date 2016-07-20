dd0862e0784c921f75cb0fa9687a2e5a9d673d9f
Alan Hartless <alan@devkardia.com>
2016-07-20 18:11:06 -0500

    Updated page bundle to new timeline format

--- app/bundles/PageBundle/EventListener/LeadSubscriber.php
+++ app/bundles/PageBundle/EventListener/LeadSubscriber.php
@@ -48,98 +48,106 @@ class LeadSubscriber extends CommonSubscriber
         $eventTypeName = $this->translator->trans('mautic.page.event.hit');
         $event->addEventType($eventTypeKey, $eventTypeName);
 
-        $filters = $event->getEventFilters();
-
         if (!$event->isApplicable($eventTypeKey)) {
+
             return;
         }
 
-        $lead    = $event->getLead();
-        $options = ['ipIds' => [], 'filters' => $filters];
-
-        /** @var \Mautic\CoreBundle\Entity\IpAddress $ip */
-        /*
-        foreach ($lead->getIpAddresses() as $ip) {
-            $options['ipIds'][] = $ip->getId();
-        }
-        */
+        $lead = $event->getLead();
 
         /** @var \Mautic\PageBundle\Entity\HitRepository $hitRepository */
-        $hitRepository = $this->factory->getEntityManager()->getRepository('MauticPageBundle:Hit');
+        $hitRepository = $this->em->getRepository('MauticPageBundle:Hit');;
+        $hits          = $hitRepository->getLeadHits($lead->getId(), $event->getQueryOptions());
 
-        $hits = $hitRepository->getLeadHits($lead->getId(), $options);
+        // Add to counter
+        $event->addToCounter($eventTypeKey, $hits);
 
-        $model = $this->factory->getModel('page.page');
+        if (!$event->isEngagementCount()) {
 
-        // Add the hits to the event array
-        foreach ($hits as $hit) {
-            $template   = 'MauticPageBundle:SubscribedEvents\Timeline:index.html.php';
-            $eventLabel = $eventTypeName;
+            $model = $this->factory->getModel('page.page');
 
-            if ($hit['source'] && $hit['sourceId']) {
-                $sourceModel = false;
-                try {
-                    $sourceModel = $this->factory->getModel($hit['source']);
-                } catch (\Exception $exception) {
-                    // Try a plugin
+            // Add the hits to the event array
+            foreach ($hits['results'] as $hit) {
+                $template = 'MauticPageBundle:SubscribedEvents\Timeline:index.html.php';
 
+                if ($hit['source'] && $hit['sourceId']) {
+                    $sourceModel = false;
                     try {
-                        $sourceModel = $this->factory->getModel('plugin.'.$hit['source']);
+                        $sourceModel = $this->factory->getModel($hit['source']);
                     } catch (\Exception $exception) {
-                        // No model found
+                        // Try a plugin
+
+                        try {
+                            $sourceModel = $this->factory->getModel('plugin.'.$hit['source']);
+                        } catch (\Exception $exception) {
+                            // No model found
+                        }
+                    }
+
+                    if ($sourceModel) {
+                        try {
+                            $sourceEntity = $sourceModel->getEntity($hit['sourceId']);
+                            if (method_exists($sourceEntity, $sourceModel->getNameGetter())) {
+                                $hit['sourceName'] = $sourceEntity->{$sourceModel->getNameGetter()}();
+                            }
+
+                            $baseRouteName = str_replace('.', '_', $hit['source']);
+                            if (method_exists($sourceModel, 'getActionRouteBase')) {
+                                $baseRouteName = $sourceModel->getActionRouteBase();
+                            }
+                            $routeSourceName = 'mautic_'.$baseRouteName.'_action';
+
+                            if ($this->factory->getRouter()->getRouteCollection()->get($routeSourceName) !== null) {
+                                $hit['sourceRoute'] = $this->factory->getRouter()->generate(
+                                    $routeSourceName,
+                                    [
+                                        'objectAction' => 'view',
+                                        'objectId'     => $hit['sourceId']
+                                    ]
+                                );
+                            }
+
+                            // Allow a custom template if applicable
+                            if (method_exists($sourceModel, 'getPageHitLeadTimelineTemplate')) {
+                                $template = $sourceModel->getPageHitLeadTimelineTemplate($hit);
+                            }
+
+                            if (method_exists($sourceModel, 'getPageHitLeadTimelineLabel')) {
+                                $eventTypeName = $sourceModel->getPageHitLeadTimelineLabel($hit);
+                            }
+                        } catch (\Exception $exception) {
+                            // Not found
+                        }
                     }
                 }
 
-                if ($sourceModel) {
-                    try {
-                        $sourceEntity = $sourceModel->getEntity($hit['sourceId']);
-                        if (method_exists($sourceEntity, $sourceModel->getNameGetter())) {
-                            $hit['sourceName'] = $sourceEntity->{$sourceModel->getNameGetter()}();
-                        }
-
-                        $baseRouteName = str_replace('.', '_', $hit['source']);
-                        if (method_exists($sourceModel, 'getActionRouteBase')) {
-                            $baseRouteName = $sourceModel->getActionRouteBase();
-                        }
-                        $routeSourceName = 'mautic_'.$baseRouteName.'_action';
-
-                        if ($this->factory->getRouter()->getRouteCollection()->get($routeSourceName) !== null) {
-                            $hit['sourceRoute'] = $this->factory->getRouter()->generate(
-                                $routeSourceName,
-                                [
-                                    'objectAction' => 'view',
-                                    'objectId'     => $hit['sourceId']
-                                ]
-                            );
-                        }
-
-                        // Allow a custom template if applicable
-                        if (method_exists($sourceModel, 'getPageHitLeadTimelineTemplate')) {
-                            $template = $sourceModel->getPageHitLeadTimelineTemplate($hit);
-                        }
-
-                        if (method_exists($sourceModel, 'getPageHitLeadTimelineLabel')) {
-                            $eventLabel = $sourceModel->getPageHitLeadTimelineLabel($hit);
-                        }
-                    } catch (\Exception $exception) {
-                        // Not found
-                    }
+                if (!empty($hit['page_id'])) {
+                    $page       = $model->getEntity($hit['page_id']);
+                    $eventLabel = [
+                        'label' => $page->getTitle(),
+                        'href'  => $this->router->generate('mautic_page_action', ['objectAction' => 'view', 'objectId' => $hit['page_id']])
+                    ];
+                } else {
+                    $eventLabel = [
+                        'label'      => (isset($hit['urlTitle'])) ? $hit['urlTitle'] : $hit['url'],
+                        'href'       => $hit['url'],
+                        'isExternal' => true
+                    ];
                 }
+                $event->addEvent(
+                    [
+                        'event'           => $eventTypeKey,
+                        'eventLabel'      => $eventLabel,
+                        'eventType'       => $eventTypeName,
+                        'timestamp'       => $hit['dateHit'],
+                        'extra'           => [
+                            'hit' => $hit
+                        ],
+                        'contentTemplate' => $template,
+                        'icon'            => 'fa-link'
+                    ]
+                );
             }
-
-            $event->addEvent(
-                [
-                    'event'           => $eventTypeKey,
-                    'eventLabel'      => $eventLabel,
-                    'timestamp'       => $hit['dateHit'],
-                    'extra'           => [
-                        'page' => $model->getEntity($hit['page_id']),
-                        'hit'  => $hit
-                    ],
-                    'contentTemplate' => $template,
-                    'icon'            => 'fa-link'
-                ]
-            );
         }
     }
 
