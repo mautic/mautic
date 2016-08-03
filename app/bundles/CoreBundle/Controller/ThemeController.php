649300cfbb780b8b056440416f667dfdcd6c88bc
Jan Linhart <admin@escope.cz>
2016-08-03 20:54:41 +0200

    The default themes cannot be overwritten

--- app/bundles/CoreBundle/Controller/ThemeController.php
+++ app/bundles/CoreBundle/Controller/ThemeController.php
@@ -52,14 +52,21 @@ class ThemeController extends FormController
         if ($this->request->getMethod() == 'POST') {
             if (isset($form) && !$cancelled = $this->isFormCancelled($form)) {
                 if ($this->isFormValid($form)) {
-                    $fileData = $form['file']->getData();
-                    $fileName = InputHelper::filename($fileData->getClientOriginalName());
+                    $fileData  = $form['file']->getData();
+                    $fileName  = InputHelper::filename($fileData->getClientOriginalName());
+                    $themeName = basename($fileName, '.zip');
 
-                    if (!empty($fileData)) {
+                    if (in_array($themeName, $this->defaultThemes)) {
+                        $form->addError(
+                            new FormError(
+                                $this->factory->getTranslator()->trans('mautic.core.theme.default.cannot.overwrite', ['%name%' => $themeName], 'validators')
+                            )
+                        );
+                    } elseif (!empty($fileData)) {
                         try {
                             $fileData->move($dir, $fileName);
                             $themeHelper->install($dir.'/'.$fileName);
-                            $this->addFlash('mautic.core.theme.installed', ['%name%' => $fileName]);
+                            $this->addFlash('mautic.core.theme.installed', ['%name%' => $themeName]);
                         } catch (\Exception $e) {
                             $form->addError(
                                 new FormError(
