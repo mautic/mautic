73b85943f92558621274f65b288e9e2c2196eae0
Marianela Queme <mqueme@gmail.com>
2016-08-08 20:54:10 +0100

    added migration file for new entity

---
+++ app/migrations/Version20160808000000.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160806000000 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        if ($schema->getTable($this->prefix.'integration_entity')) {
+
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $sql = <<<SQL
+CREATE TABLE {$this->prefix}integration_entity (
+	id INT AUTO_INCREMENT NOT NULL,
+	integration VARCHAR(255) DEFAULT NULL,
+	integration_entity VARCHAR(255) DEFAULT NULL,
+	integration_entity_id INT DEFAULT NULL,
+	date_added DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime)',
+	last_sync_date DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime)',
+	internal_entity VARCHAR(255) DEFAULT NULL,
+	internal_entity_id INT DEFAULT NULL,
+	internal LONGTEXT DEFAULT NULL COMMENT '(DC2Type:array)',
+	PRIMARY KEY(id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+SQL;
+        $this->addSql($sql);
+
+    }
+}
