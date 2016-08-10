2cd4d82abc45fb45e87c7c0ab8dfba7d104a8d86
Marianela Queme <mqueme@gmail.com>
2016-08-10 20:03:04 +0100

    changed shortcode in fetchleads

--- app/bundles/PluginBundle/Command/FetchLeadsCommand.php
+++ app/bundles/PluginBundle/Command/FetchLeadsCommand.php
@@ -50,7 +50,7 @@ class FetchLeadsCommand extends ContainerAwareCommand
             )
             ->addOption(
                 '--time-interval',
-                '-ti',
+                '-a',
                 InputOption::VALUE_OPTIONAL,
                 'Send time interval to check updates on Salesforce, it should be a correct php formatted time interval in the past eg:(-10 minutes)'
             )
