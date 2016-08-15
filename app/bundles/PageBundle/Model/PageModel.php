122aff768cc386b0126e7a015450c8e6c017fedc
Alan Hartless <alan@devkardia.com>
2016-08-15 15:08:39 -0500

    Added clickthrough support to PageHits for both ['channel' => ['email' => 1]] or ['channel' => ['email', 1]]

--- app/bundles/PageBundle/Model/PageModel.php
+++ app/bundles/PageBundle/Model/PageModel.php
@@ -421,8 +421,15 @@ class PageModel extends FormModel
             }
 
             if (!empty($clickthrough['channel'])) {
-                $hit->setSource($clickthrough['channel'][0]);
-                $hit->setSourceId($clickthrough['channel'][1]);
+                if (count($clickthrough['channel']) === 1) {
+                    $channelId = reset($clickthrough['channel']);
+                    $channel   = key($clickthrough['channel']);
+                } else {
+                    $channel   = $clickthrough['channel'][0];
+                    $channelId = (int) $clickthrough['channel'][1];
+                }
+                $hit->setSource($channel);
+                $hit->setSourceId($channelId);
             } elseif (!empty($clickthrough['source'])) {
                 $hit->setSource($clickthrough['source'][0]);
                 $hit->setSourceId($clickthrough['source'][1]);
