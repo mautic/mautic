ea3013ffffe64c728c7b00d7f1502ef6b9c43c3e
Prateek Shankar <ps92@live.in>
2016-09-04 00:50:31 +0530

    Update ProcessSegmentEmails.php

--- app/bundles/EmailBundle/Command/ProcessSegmentEmails.php
+++ app/bundles/EmailBundle/Command/ProcessSegmentEmails.php
@@ -2,7 +2,7 @@
 /**
  * @package     Mautic
  * @copyright   2014 Mautic Contributors. All rights reserved.
- * @author      Mautic
+ * @author      Prateek Shankar (ps92@live.in)
  * @link        http://mautic.org
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
