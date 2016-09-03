e8f217efcf523542cec14a0b106e7791ef535b63
Alan Hartless <alanhartless@users.noreply.github.com>
2016-09-03 06:25:18 -0500

    Fixed issue where places like forms received an exception for batch processing transports

--- app/bundles/EmailBundle/Model/EmailModel.php
+++ app/bundles/EmailBundle/Model/EmailModel.php
@@ -1170,7 +1170,8 @@ class EmailModel extends FormModel
 
         // Setup the mailer
         $mailer = $this->mailHelper->getMailer(!$sendBatchMail);
-
+		$mailer->enableQueue();
+		
         // Flushes the batch in case of using API mailers
         $flushQueue = function ($reset = true) use (&$mailer, &$saveEntities, &$errors, &$emailSentCounts, $sendBatchMail) {
 
