9309ae1bd98ad06e851c16c8de3bae66a87f8465
Alan Hartless <alan@devkardia.com>
2016-09-09 16:35:42 -0500

    Work around for symfony bug - https://github.com/symfony/symfony/issues/19898

--- app/bundles/CoreBundle/Views/FormTheme/Custom/choice_attributes.html.php
+++ app/bundles/CoreBundle/Views/FormTheme/Custom/choice_attributes.html.php
@@ -1,4 +1,3 @@
-<?php if ($disabled): ?>disabled="disabled" <?php endif ?>
 <?php foreach ($choice_attr as $k => $v): ?>
 <?php if ($v === true): ?>
 <?php printf('%s="%s" ', $view->escape($k), $view->escape($k)) ?>
