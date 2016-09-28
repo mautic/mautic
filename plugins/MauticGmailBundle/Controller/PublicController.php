1ad394c68ca2ce8b9b0af4062477e99502b6b039
Werner García <werner.garcia@gmail.com>
2016-09-28 13:03:05 -0600

    Fixed stat error
    Added index to lead object

--- plugins/MauticGmailBundle/Controller/PublicController.php
+++ plugins/MauticGmailBundle/Controller/PublicController.php
@@ -92,6 +92,7 @@ class PublicController extends CommonFormController
 
             // stat doesn't exist, create one
             if ($stat === null) {
+                $lead['email']=$email; // needed for stat
                 $this->addStat($lead, $email, $query, $idHash);
             } else { // Prevent marking the email as read on creation
                 $model->hitEmail($idHash, $this->request); // add email event
