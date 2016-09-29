c7943d6edfa66a6f5479e37989aaae7e8872a7f3
Werner García <werner.garcia@gmail.com>
2016-09-29 17:01:55 -0600

    Revert "Merge branch 'staging' of https://github.com/virlatinus/Mautic-Fork into staging"
    This reverts commit 0044100a10ffa972612e4166611a01e6282ba282, reversing
    changes made to f4d010845b319e9f42561c78c6669335a753a1ab.

---
+++ plugins/MauticOutlookBundle/Integration/OutlookIntegration.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\MauticOutlookBundle\Integration;
+
+use Mautic\PluginBundle\Entity\Integration;
+use Mautic\PluginBundle\Integration\AbstractIntegration;
+
+class OutlookIntegration extends AbstractIntegration
+{
+    public function getName()
+    {
+        return 'Outlook';
+    }
+
+    /**
+     * Return's authentication method such as oauth2, oauth1a, key, etc
+     *
+     * @return string
+     */
+    public function getAuthenticationType()
+    {
+        // Just use none for now and I'll build in "basic" later
+        return 'none';
+    }
+
+    /**
+     * Return array of key => label elements that will be converted to inputs to
+     * obtain from the user
+     *
+     * @return array
+     */
+    public function getRequiredKeyFields()
+    {
+        return array(
+            'secret'     => 'mautic.integration.outlook.secret',
+        );
+    }
+
+
+}
