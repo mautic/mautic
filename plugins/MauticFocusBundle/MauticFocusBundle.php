36e5982ffe007039a5b644a70e29e497f945c7c8
Jordan Ryan <jordan@facetinteractive.com>
2016-10-09 13:14:00 -0700

    Update FocusPermissions to be consistent with FormBuilderInterface $bundle naming convention.
    e.g. FormBuilderInterface expects `mauticFocusBundle` per the name of the $bundle and instead was getting `focusBundle`
    
    Addresses error message:
    
    mautic.CRITICAL: Uncaught PHP Exception InvalidArgumentException: "Permission class not found for focusBundle!" at /app/bundles/CoreBundle/Security/Permissions/CorePermissions.php line 129 {"exception":"object"}
    
    Issue: https://github.com/mautic/mautic/issues/2674

--- plugins/MauticFocusBundle/MauticFocusBundle.php
+++ plugins/MauticFocusBundle/MauticFocusBundle.php
@@ -12,6 +12,9 @@ namespace MauticPlugin\MauticFocusBundle;
 
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
 
+/**
+ * Class MauticFocusBundle.
+ */
 class MauticFocusBundle extends PluginBundleBase
 {
 }
--- plugins/MauticFocusBundle/Security/Permissions/MauticFocusPermissions.php
+++ plugins/MauticFocusBundle/Security/Permissions/MauticFocusPermissions.php
@@ -14,9 +14,9 @@ use Mautic\CoreBundle\Security\Permissions\AbstractPermissions;
 use Symfony\Component\Form\FormBuilderInterface;
 
 /**
- * Class FocusPermissions.
+ * Class MauticFocusPermissions.
  */
-class FocusPermissions extends AbstractPermissions
+class MauticFocusPermissions extends AbstractPermissions
 {
     /**
      * {@inheritdoc}
@@ -46,7 +46,7 @@ class FocusPermissions extends AbstractPermissions
      */
     public function buildForm(FormBuilderInterface &$builder, array $options, array $data)
     {
-        $this->addStandardFormFields('focus', 'categories', $builder, $data);
-        $this->addExtendedFormFields('focus', 'items', $builder, $data);
+        $this->addStandardFormFields('mauticFocus', 'categories', $builder, $data);
+        $this->addExtendedFormFields('mauticFocus', 'items', $builder, $data);
     }
 }
