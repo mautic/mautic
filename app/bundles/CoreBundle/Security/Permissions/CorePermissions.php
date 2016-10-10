984ed686440c71cdb203a9039d6b67aac0e9df6f
Alan Hartless <alan@devkardia.com>
2016-10-10 18:51:30 -0500

    Fixed issue where focus permissions couldn't be saved

--- app/bundles/CoreBundle/Security/Permissions/CorePermissions.php
+++ app/bundles/CoreBundle/Security/Permissions/CorePermissions.php
@@ -49,12 +49,12 @@ class CorePermissions
     /**
      * @var array
      */
-    private $permissionClasses = ['core' => [], 'plugins' => []];
+    private $permissionClasses = [];
 
     /**
      * @var array
      */
-    private $permissionObjects = ['core' => [], 'plugins' => []];
+    private $permissionObjects = [];
 
     /**
      * @var array
@@ -93,11 +93,9 @@ class CorePermissions
     public function getPermissionObjects()
     {
         $objects = [];
-        foreach ($this->permissionClasses as $bundleType => $permissionClasses) {
-            foreach ($permissionClasses as $key => $class) {
-                if ($object = $this->getPermissionObject($key, false, ('plugins' == $bundleType))) {
-                    $objects[] = $object;
-                }
+        foreach ($this->permissionClasses as $key => $class) {
+            if ($object = $this->getPermissionObject($key, false)) {
+                $objects[] = $object;
             }
         }
 
@@ -109,30 +107,28 @@ class CorePermissions
      *
      * @param string $bundle
      * @param bool   $throwException
-     * @param bool   $pluginBundle
      *
      * @return mixed
      *
      * @throws \InvalidArgumentException
      */
-    public function getPermissionObject($bundle, $throwException = true, $pluginBundle = false)
+    public function getPermissionObject($bundle, $throwException = true)
     {
         if (!empty($bundle)) {
-            $bundleType = $pluginBundle ? 'plugins' : 'core';
-            if (isset($this->permissionClasses[$bundleType][$bundle])) {
-                if (empty($this->permissionObjects[$bundleType][$bundle])) {
-                    $permissionClass                               = $this->permissionClasses[$bundleType][$bundle];
-                    $this->permissionObjects[$bundleType][$bundle] = new $permissionClass($this->getParams());
+            if (isset($this->permissionClasses[$bundle])) {
+                if (empty($this->permissionObjects[$bundle])) {
+                    $permissionClass                  = $this->permissionClasses[$bundle];
+                    $this->permissionObjects[$bundle] = new $permissionClass($this->getParams());
                 }
             } else {
                 if ($throwException) {
-                    throw new \InvalidArgumentException("Permission class not found for {$bundle}Bundle!");
+                    throw new \InvalidArgumentException("Permission class not found for {$bundle} in permissions classes");
                 }
 
                 return false;
             }
 
-            return $this->permissionObjects[$bundleType][$bundle];
+            return $this->permissionObjects[$bundle];
         }
 
         throw new \InvalidArgumentException("Bundle and permission type must be specified. '$bundle' given.");
@@ -183,9 +179,6 @@ class CorePermissions
             $classes[$bundle]->analyzePermissions($bundlePermissions[$bundle], $bundlePermissions, true);
         }
 
-        //get a list of plugin bundles so we can tell later if a bundle is core or plugin
-        $pluginBundles = $this->getPluginBundles();
-
         //create entities
         foreach ($bundlePermissions as $bundle => $permissions) {
             foreach ($permissions as $name => $perms) {
@@ -196,7 +189,7 @@ class CorePermissions
                 $entity->setName(strtolower($name));
 
                 $bit   = 0;
-                $class = $this->getPermissionObject($bundle, true, array_key_exists(ucfirst($bundle).'Bundle', $pluginBundles));
+                $class = $this->getPermissionObject($bundle, true);
 
                 foreach ($perms as $perm) {
                     //get the bit for the perm
@@ -258,7 +251,7 @@ class CorePermissions
                 $isPlugin = true;
                 array_shift($parts);
             } else {
-                $isPlugin = false;
+                $isPlugin = null;
             }
 
             if (count($parts) != 3) {
@@ -347,7 +340,7 @@ class CorePermissions
                 $isPlugin = true;
                 array_shift($parts);
             } else {
-                $isPlugin = false;
+                $isPlugin = null;
             }
 
             if (count($parts) != 3) {
@@ -498,13 +491,13 @@ class CorePermissions
     {
         foreach ($this->getBundles() as $bundle) {
             if (!empty($bundle['permissionClasses'])) {
-                $this->permissionClasses['core'] = array_merge($this->permissionClasses['core'], $bundle['permissionClasses']);
+                $this->permissionClasses = array_merge($this->permissionClasses, $bundle['permissionClasses']);
             }
         }
 
         foreach ($this->getPluginBundles() as $bundle) {
             if (!empty($bundle['permissionClasses'])) {
-                $this->permissionClasses['plugins'] = array_merge($this->permissionClasses['plugins'], $bundle['permissionClasses']);
+                $this->permissionClasses = array_merge($this->permissionClasses, $bundle['permissionClasses']);
             }
         }
     }
