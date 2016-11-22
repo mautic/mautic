57d1aa37b725e39c056f4e15ae1725b4e5efd830
Zdeno Kuzmany <zdeno@kuzmany.biz>
2016-11-22 23:00:32 +0100

    Add migrations

--- app/bundles/NotificationBundle/Views/Notification/preview.html.php
+++ app/bundles/NotificationBundle/Views/Notification/preview.html.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * @copyright   2016 Mautic Contributors. All rights reserved
  * @author      Mautic
  *
@@ -7,7 +7,7 @@
  *
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
-/** @var \Mautic\NotificationBundle\Entity\Notification $notification */
+/* @var \Mautic\NotificationBundle\Entity\Notification $notification */
 $url = $notification->getUrl();
 $button = $notification->getButton();
 
@@ -17,7 +17,7 @@ $button = $notification->getButton();
         <h3 class="panel-title">
             <?php echo $notification->getHeading()?>
             <?php if ($url) : ?>
-                <span class="pull-right">
+            <span class="pull-right">
                 <a href="<?php echo $url; ?>" target="_blank"><span class="fa fa-external-link"></span></a>
             </span>
             <?php endif; ?>
---
+++ app/migrations/Version20161122215214.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2016 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\Migrations;
+
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Migrations\SkipMigrationException;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161122215214 extends AbstractMauticMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws SkipMigrationException
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function preUp(Schema $schema)
+    {
+        $columns = $schema->getTable($this->prefix.'push_notifications')->getColumns();
+
+        if (array_key_exists('button', $columns)) {
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql("ALTER TABLE {$this->prefix}push_notifications ADD button LONGTEXT NOT NULL");
+    }
+}
