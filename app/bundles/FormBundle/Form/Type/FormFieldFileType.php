fc7b8d8874e604e190605f4682e3e73482aebdeb
Jachim Coudenys <jachimcoudenys@gmail.com>
2016-12-05 16:39:15 +0100

    Add mimetype and filesize valitors on the new file field

--- app/bundles/FormBundle/Form/Type/FormFieldFileType.php
+++ app/bundles/FormBundle/Form/Type/FormFieldFileType.php
@@ -11,6 +11,8 @@
 namespace Mautic\FormBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 /**
@@ -18,12 +20,39 @@ use Symfony\Component\Form\FormBuilderInterface;
  */
 class FormFieldFileType extends AbstractType
 {
+    const PROPERTY_ALLOWED_MIME_TYPES = 'allowed_mime_types';
+    const PROPERTY_ALLOWED_FILE_SIZE  = 'allowed_file_size';
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        // TODO Include fields for mime types, size, etc...
+        $builder->add(
+            self::PROPERTY_ALLOWED_MIME_TYPES,
+            TextareaType::class,
+            [
+                'label'      => 'mautic.form.field.allowed_mime_types',
+                'label_attr' => ['class' => 'control-label'],
+                'required'   => false,
+                'attr'       => [
+                    'class' => 'form-control',
+                ],
+            ]
+        );
+
+        $builder->add(
+            self::PROPERTY_ALLOWED_FILE_SIZE,
+            TextType::class,
+            [
+                'label'      => 'mautic.form.field.allowed_file_size',
+                'label_attr' => ['class' => 'control-label'],
+                'required'   => false,
+                'attr'       => [
+                    'class' => 'form-control',
+                ],
+            ]
+        );
     }
 
     /**
