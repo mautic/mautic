b731451a814524987f876160ed0ce147e73384e9
Alan Hartless <alan@devkardia.com>
2016-12-22 12:56:52 -0500

    Don't show the uh oh page for context saml exception

--- app/bundles/CoreBundle/EventListener/ExceptionListener.php
+++ app/bundles/CoreBundle/EventListener/ExceptionListener.php
@@ -12,6 +12,7 @@
 namespace Mautic\CoreBundle\EventListener;
 
 use LightSaml\Error\LightSamlAuthenticationException;
+use LightSaml\Error\LightSamlContextException;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\EventListener\ExceptionListener as KernelExceptionListener;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -31,7 +32,12 @@ class ExceptionListener extends KernelExceptionListener
         $exception = $event->getException();
 
         // Check for exceptions we don't want to handle
-        if ($exception instanceof AuthenticationException || $exception instanceof AccessDeniedException || $exception instanceof LogoutException || $exception instanceof LightSamlAuthenticationException) {
+        if ($exception instanceof AuthenticationException ||
+            $exception instanceof AccessDeniedException ||
+            $exception instanceof LogoutException ||
+            $exception instanceof LightSamlAuthenticationException ||
+            $exception instanceof LightSamlContextException
+        ) {
             return;
         }
 
