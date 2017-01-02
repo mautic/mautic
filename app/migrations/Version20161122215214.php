10eef4b924a8f7504e26c1779af500b2c73c38d3
Alan Hartless <alan@devkardia.com>
2017-01-01 23:31:53 -0600

    Updated language strings

--- app/migrations/Version20161122215214.php
+++ app/migrations/Version20161122215214.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * @package     Mautic
- * @copyright   2016 Mautic Contributors. All rights reserved.
+ * @copyright   2016 Mautic Contributors. All rights reserved
  * @author      Mautic
+ *
  * @link        http://mautic.org
+ *
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 namespace Mautic\Migrations;
 
-use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
-use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Migrations\SkipMigrationException;
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -26,9 +27,7 @@ class Version20161122215214 extends AbstractMauticMigration
      */
     public function preUp(Schema $schema)
     {
-        $columns = $schema->getTable($this->prefix.'push_notifications')->getColumns();
-
-        if (array_key_exists('button', $columns)) {
+        if ($schema->getTable($this->prefix.'push_notifications')->hasColumn('button')) {
             throw new SkipMigrationException('Schema includes this migration');
         }
     }
