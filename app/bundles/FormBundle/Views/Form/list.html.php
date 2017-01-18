3dd25236537b75ec5b3830df1e1b5a986e856be1
Alan Hartless <alan@devkardia.com>
2017-01-17 20:52:53 -0600

    Move batch rebuild button to the batch dropdown

--- app/bundles/FormBundle/Views/Form/list.html.php
+++ app/bundles/FormBundle/Views/Form/list.html.php
@@ -28,6 +28,23 @@ if ($tmpl == 'index') {
                         'templateButtons' => [
                             'delete' => $permissions['form:forms:deleteown'] || $permissions['form:forms:deleteother'],
                         ],
+                        'customButtons' => [
+                            [
+                                'confirm' => [
+                                    'message'       => $view['translator']->trans('mautic.form.confirm_batch_rebuild'),
+                                    'confirmText'   => $view['translator']->trans('mautic.form.rebuild'),
+                                    'confirmAction' => $view['router']->path(
+                                        'mautic_form_action',
+                                        array_merge(['objectAction' => 'batchRebuildHtml'])
+                                    ),
+                                    'iconClass'       => 'fa fa-fw fa-refresh',
+                                    'btnText'         => $view['translator']->trans('mautic.form.rebuild'),
+                                    'precheck'        => 'batchActionPrecheck',
+                                    'confirmCallback' => 'executeBatchAction',
+                                ],
+                                'primary' => true,
+                            ],
+                        ],
                     ]
                 );
 
