043cbe3ad8219acc14a5022adfff6c90c077bc90
John Linhart <admin@escope.cz>
2017-02-20 16:40:45 +0100

    Side margin moved from column slots to slot wrappers so every added slots will have the same margin

--- app/bundles/CoreBundle/Views/Sections/one-column.html.php
+++ app/bundles/CoreBundle/Views/Sections/one-column.html.php
@@ -13,8 +13,8 @@
     <center>
         <table data-section style="margin: 0 auto;border-collapse: collapse !important;width: 600px;" class="w320" cellpadding="0" cellspacing="0" width="600">
             <tr>
-                <td data-slot-container valign="top" class="mobile-block">
-                    <div data-slot="text" style="margin-left: 5px; margin-right: 5px;">
+                <td data-slot-container valign="top" class="mobile-block" style="margin-left: 5px; margin-right: 5px;">
+                    <div data-slot="text">
                         <p>Lorem ipsum dolor sit amet, alterum definitiones eu est. Eos no scripta voluptatum necessitatibus, ea his case movet. Porro vivendo delicatissimi ea qui, in usu aliquam consulatu conclusionemque. Eu vel mazim periculis consequat, quo fastidii salutandi eu, et sed nibh exerci consequuntur. Cu diam efficiendi eum, pri eu delenit insolens. Usu nihil oporteat an, et stet mucius vix, ex nostro assueverit mel.</p>
                         <p>ripidis mea id, ut mel zril tractatos inciderint. Id eam hinc omnes, est eu labore mnesarchum, prima deserunt erroribus no nam. Hinc liber epicurei at vel, ius eirmod conclusionemque an, ad nec lorem possit. Id mollis commune principes qui.</p>
                     </div>
