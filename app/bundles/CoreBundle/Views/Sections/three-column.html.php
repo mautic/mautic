358daab947da49771372ad72c765fdf21be7d42c
John Linhart <admin@escope.cz>
2017-02-21 10:14:19 +0100

    HTML attributes in the new section types which didn't have a value have one now

--- app/bundles/CoreBundle/Views/Sections/three-column.html.php
+++ app/bundles/CoreBundle/Views/Sections/three-column.html.php
@@ -9,21 +9,21 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 ?>
-<div data-section-wrapper>
+<div data-section-wrapper="1">
     <center>
-        <table data-section style="margin: 0 auto;border-collapse: collapse !important;width: 600px;" class="w320" cellpadding="0" cellspacing="0" width="600">
+        <table data-section="1" style="margin: 0 auto;border-collapse: collapse !important;width: 600px;" class="w320" cellpadding="0" cellspacing="0" width="600">
             <tr>
-                <td data-slot-container valign="top" class="mobile-block" style="margin-left: 5px; margin-right: 5px;">
+                <td data-slot-container="1" valign="top" class="mobile-block" style="margin-left: 5px; margin-right: 5px;">
                     <div data-slot="text">
                         <p>Lorem ipsum dolor sit amet, alterum definitiones eu est. Eos no scripta voluptatum necessitatibus, ea his case movet. Porro vivendo delicatissimi ea qui, in usu aliquam consulatu conclusionemque. Eu vel mazim periculis consequat, quo fastidii salutandi eu, et sed nibh exerci consequuntur. Cu diam efficiendi eum, pri eu delenit insolens. Usu nihil oporteat an, et stet mucius vix, ex nostro assueverit mel.</p>
                     </div>
                 </td>
-                <td data-slot-container valign="top" class="mobile-block" style="margin-left: 5px; margin-right: 5px;">
+                <td data-slot-container="1" valign="top" class="mobile-block" style="margin-left: 5px; margin-right: 5px;">
                     <div data-slot="text">
                         <p>Lorem ipsum dolor sit amet, alterum definitiones eu est. Eos no scripta voluptatum necessitatibus, ea his case movet. Porro vivendo delicatissimi ea qui, in usu aliquam consulatu conclusionemque. Eu vel mazim periculis consequat, quo fastidii salutandi eu, et sed nibh exerci consequuntur. Cu diam efficiendi eum, pri eu delenit insolens. Usu nihil oporteat an, et stet mucius vix, ex nostro assueverit mel.</p>
                     </div>
                 </td>
-                <td data-slot-container valign="top" class="mobile-block" style="margin-left: 5px; margin-right: 5px;">
+                <td data-slot-container="1" valign="top" class="mobile-block" style="margin-left: 5px; margin-right: 5px;">
                     <div data-slot="text">
                         <p>Lorem ipsum dolor sit amet, alterum definitiones eu est. Eos no scripta voluptatum necessitatibus, ea his case movet. Porro vivendo delicatissimi ea qui, in usu aliquam consulatu conclusionemque. Eu vel mazim periculis consequat, quo fastidii salutandi eu, et sed nibh exerci consequuntur. Cu diam efficiendi eum, pri eu delenit insolens. Usu nihil oporteat an, et stet mucius vix, ex nostro assueverit mel.</p>
                     </div>
