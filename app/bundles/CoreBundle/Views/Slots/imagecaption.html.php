ada7b0e8e726ef0d4591035b55174fd857bb649e
Werner Garcia <virlatinus@gmail.com>
2017-03-01 05:51:19 -0600

    fixed bug when changing properties

--- app/bundles/CoreBundle/Views/Slots/imagecaption.html.php
+++ app/bundles/CoreBundle/Views/Slots/imagecaption.html.php
@@ -9,8 +9,8 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 ?>
-<figure>
+<figure style="text-align:center;">
     <img class="fr-view" src="<?php echo $view['assets']->getUrl('themes/blank.png', null, null, true); ?>" alt="An image"/>
-    <figcaption style="padding: 5px;color:#000;font-size:16px;text-align: left;">Your image caption goes here. You can change the position of the caption and style in the customize slot tab.</figcaption>
+    <figcaption style="line-height:16px;padding: 5px;color:#000;font-size:16px;text-align: left;">Your image caption goes here. You can change the position of the caption and style in the customize slot tab.</figcaption>
 </figure>
 <div style="clear:both"></div>
