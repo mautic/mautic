36f4967084e061b0a8abb942cf6c034e37c9fccd
Marianela Queme <mqueme@gmail.com>
2017-03-03 19:16:24 +0000

    reverted checking owner at accoun

--- plugins/MauticCrmBundle/Api/SalesforceApi.php
+++ plugins/MauticCrmBundle/Api/SalesforceApi.php
@@ -281,7 +281,7 @@ class SalesforceApi extends CrmApi
             $fields   = implode(', ', array_unique($fields));
 
             $config = $this->integration->mergeConfigToFeatureSettings([]);
-            if (isset($config['updateOwner']) && isset($config['updateOwner'][0]) && $config['updateOwner'][0] == 'updateOwner' && $object != 'Account') {
+            if (isset($config['updateOwner']) && isset($config['updateOwner'][0]) && $config['updateOwner'][0] == 'updateOwner') {
                 $fields = 'Owner.Name, Owner.Email, '.$fields;
             }
 
