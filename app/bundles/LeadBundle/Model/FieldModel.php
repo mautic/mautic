0be2c3fdcf4adbd7328571720c744c7cb1c7240b
Sam <Spe@webmecanik.com>
2017-04-12 09:38:14 +0200

    add point field

--- app/bundles/LeadBundle/Model/FieldModel.php
+++ app/bundles/LeadBundle/Model/FieldModel.php
@@ -129,6 +129,11 @@ class FieldModel extends FormModel
             'listable' => true,
             'object'   => 'lead',
         ],
+        'points' => [
+            'type'   => 'number',
+            'fixed'  => true,
+            'object' => 'lead',
+        ],
         'facebook' => [
             'listable' => true,
             'group'    => 'social',
