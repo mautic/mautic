4abf5fa311c5a763859a8934e51050586fcfe804
Alan Hartless <alan@devkardia.com>
2017-04-27 09:12:56 -0500

    Notifications

--- plugins/MauticCrmBundle/Integration/SalesforceIntegration.php
+++ plugins/MauticCrmBundle/Integration/SalesforceIntegration.php
@@ -1326,6 +1326,11 @@ class SalesforceIntegration extends CrmAbstractIntegration
         return [$updated, $created];
     }
 
+    /**
+     * @return array
+     *
+     * @throws \Exception
+     */
     public function getCampaigns()
     {
         $silenceExceptions = (isset($settings['silence_exceptions'])) ? $settings['silence_exceptions'] : true;
@@ -1341,6 +1346,13 @@ class SalesforceIntegration extends CrmAbstractIntegration
 
         return $campaigns;
     }
+
+    /**
+     * @param $campaignId
+     * @param $settings
+     *
+     * @throws \Exception
+     */
     public function getCampaignMembers($campaignId, $settings)
     {
         $silenceExceptions = (isset($settings['silence_exceptions'])) ? $settings['silence_exceptions'] : true;
@@ -1462,6 +1474,12 @@ class SalesforceIntegration extends CrmAbstractIntegration
         }
     }
 
+    /**
+     * @param $fields
+     * @param $object
+     *
+     * @return array
+     */
     public function getMixedLeadFields($fields, $object)
     {
         $mixedFields = array_filter($fields['leadFields']);
@@ -1478,26 +1496,13 @@ class SalesforceIntegration extends CrmAbstractIntegration
         return $fields;
     }
 
-    public function getNotificationModel()
-    {
-        return $this->factory->getModel('core.notification');
-    }
-
     /**
-     * @param \Exception $e
+     * @param $campaignId
+     *
+     * @return array
+     *
+     * @throws \Exception
      */
-    public function logIntegrationError(\Exception $e)
-    {
-        $logger = $this->factory->getLogger();
-        if ('dev' == MAUTIC_ENV) {
-            $logger->addError('INTEGRATION ERROR: '.$this->getName().' - '.$e);
-            $this->getNotificationModel()->addNotification($e, $this->getName(), false, 'INTEGRATION ERROR: '.$this->getName().':', null, null, $this->factory->getUser());
-        } else {
-            $logger->addError('INTEGRATION ERROR: '.$this->getName().' - '.$e->getMessage());
-            $this->getNotificationModel()->addNotification($e->getMessage(), $this->getName(), false, 'INTEGRATION ERROR: '.$this->getName().':', null, null, $this->factory->getUser());
-        }
-    }
-
     public function getCampaignMemberStatus($campaignId)
     {
         $silenceExceptions    = (isset($settings['silence_exceptions'])) ? $settings['silence_exceptions'] : true;
@@ -1514,6 +1519,13 @@ class SalesforceIntegration extends CrmAbstractIntegration
         return $campaignMemberStatus;
     }
 
+    /**
+     * @param Lead $lead
+     * @param      $integrationCampaignId
+     * @param      $status
+     *
+     * @return array
+     */
     public function pushLeadToCampaign(Lead $lead, $integrationCampaignId, $status)
     {
         $mauticData = $salesforceIdMapping = [];
