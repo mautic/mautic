4595bac217aee4f32d98d9e8a7516adabdd5d889
Zdeno Kuzmany <zdeno@kuzmany.biz>
2017-05-03 17:09:04 +0200

    Minor

--- app/bundles/LeadBundle/Form/Type/CampaignEventLeadOwnerType.php
+++ app/bundles/LeadBundle/Form/Type/CampaignEventLeadOwnerType.php
@@ -15,7 +15,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 /**
- * Class CampaignEventLeadSegmentsType.
+ * Class CampaignEventLeadOwnerType.
  */
 class CampaignEventLeadOwnerType extends AbstractType
 {
