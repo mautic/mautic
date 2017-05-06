bd7603c4a516afdd7605b4ad700a9b5b025258e8
Marianela Queme <mqueme@gmail.com>
2017-05-06 09:10:41 +0100

    fixed custom template on plugin form

--- app/bundles/PluginBundle/Views/Integration/form.html.php
+++ app/bundles/PluginBundle/Views/Integration/form.html.php
@@ -120,15 +120,15 @@ $hasFeatureErrors =
                 </div>
             </div>
         <?php endif; ?>
-        <?php if (isset($formNotes['custom']['template'])):
-             if (is_array($formNotes['custom'])):
+        <?php if (isset($formNotes['custom'])):
+            if (is_string($formNotes['custom'])):
+                echo $formNotes['custom'];
+            elseif (!empty(isset($formNotes['custom']['template']))):
                 $template = $formNotes['custom']['template'];
                 $params   = isset($formNotes['custom']['parameters']) ? $formNotes['custom']['parameters'] : [];
 
                 echo $this->render($template, $params);
-             else:
-                echo $formNotes['custom'];
-             endif;
+            endif;
         endif;
         ?>
     </div>
