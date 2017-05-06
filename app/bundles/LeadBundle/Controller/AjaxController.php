287f035bf7f80ae1762a9343263531c44a789285
Alan Hartless <alan@devkardia.com>
2017-05-06 02:05:54 -0500

    Prevent SQL injection for ajax lead field lookup and prevent exception message from being disclosed with a json request header

--- app/bundles/LeadBundle/Controller/AjaxController.php
+++ app/bundles/LeadBundle/Controller/AjaxController.php
@@ -103,7 +103,7 @@ class AjaxController extends CommonAjaxController
     {
         $dataArray = ['success' => 0];
         $filter    = InputHelper::clean($request->query->get('filter'));
-        $leadField = InputHelper::clean($request->query->get('field'));
+        $leadField = InputHelper::alphanum($request->query->get('field'));
         if (!empty($leadField)) {
             if (strpos($leadField, 'company') === 0) {
                 $results = $this->getModel('lead.company')->getLookupResults('companyfield', [$leadField, $filter]);
