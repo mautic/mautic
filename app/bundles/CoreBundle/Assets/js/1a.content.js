0e234cdfa774304d9c3ec6f821cbdc5d88a46413
Don Gilbert <don.gilbert@mautic.com>
2017-05-16 17:23:54 -0400

    Add back the tracking_page_enabled OneSignal configuration option

--- app/bundles/CoreBundle/Assets/js/1a.content.js
+++ app/bundles/CoreBundle/Assets/js/1a.content.js
@@ -268,7 +268,24 @@ Mautic.onPageLoad = function (container, response, inModal) {
     });
 
     //initialize tooltips
-    mQuery(container + " *[data-toggle='tooltip']").tooltip({html: true, container: 'body'});
+    var pageTooltips = mQuery(container + " *[data-toggle='tooltip']");
+    pageTooltips.tooltip({html: true, container: 'body'});
+
+    // Enable tooltips on checkbox & radio input's to
+    // show when hovering their parent LABEL element
+    pageTooltips.each(function(i) {
+        var thisTooltip   = mQuery(pageTooltips.get(i));
+        var elementParent = thisTooltip.parent();
+
+        if (elementParent.get(0).tagName === 'LABEL') {
+            elementParent.hover(function () {
+                thisTooltip.tooltip('show')
+            }, function () {
+                thisTooltip.tooltip('hide');
+            });
+        }
+    });
+
 
     //initialize sortable lists
     mQuery(container + " *[data-toggle='sortablelist']").each(function (index) {
