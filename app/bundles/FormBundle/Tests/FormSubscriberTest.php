d03ea3d31118c7a131cdf5510887b7d2cefdfaf0
Marianela Queme <mqueme@gmail.com>
2017-05-18 12:31:19 +0100

    first test commit

---
+++ app/bundles/FormBundle/Tests/FormSubscriberTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * @copyright   2014 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\Test;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class FormSubscriberTest extends WebTestCase
+{
+}
