de1cfcab2888b3287cedeec5c38206811d0ab91f
John Linhart <admin@escope.cz>
2017-05-19 15:32:50 +0200

    Unused options removed from the import command

--- app/bundles/LeadBundle/Command/ImportCommand.php
+++ app/bundles/LeadBundle/Command/ImportCommand.php
@@ -14,7 +14,6 @@ namespace Mautic\LeadBundle\Command;
 use Mautic\LeadBundle\Helper\Progress;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -29,31 +28,11 @@ class ImportCommand extends ContainerAwareCommand
     {
         $this->setName('mautic:import')
             ->setDescription('Imports data to Mautic')
-            ->setDefinition(
-                [
-                    new InputOption(
-                        'batch',
-                        'b',
-                        InputOption::VALUE_OPTIONAL,
-                        'Batch limit for storing to database',
-                        25
-                    ),
-                    new InputOption('dry-run', 'r', InputOption::VALUE_NONE, 'Do a dry run without actually saving anything.'),
-                ]
-            )
             ->setHelp(
                 <<<'EOT'
-                The <info>%command.name%</info> command starts to import CSV files when some are submitted.
+The <info>%command.name%</info> command starts to import CSV files when some are created.
 
 <info>php %command.full_name%</info>
-
-Specify the batch number.
-
-<info>php %command.full_name% --batch=30</info>
-
-You can also optionally specify a dry run without saving any records:
-
-<info>php %command.full_name% --batch=30 --dry-run</info>
 EOT
             );
     }
@@ -71,11 +50,8 @@ EOT
         /** @var \Mautic\LeadBundle\Model\ImportModel $model */
         $model = $this->getContainer()->get('mautic.lead.model.import');
 
-        // $batch    = $input->getOption('batch');
-        // $dryRun   = $input->getOption('dry-run');
         $progress = new Progress($output);
-
-        $import = $model->processNext($progress);
+        $import   = $model->processNext($progress);
 
         // No import waiting in the queue
         if ($import === null) {
@@ -94,6 +70,7 @@ EOT
             return 1;
         }
 
+        // Import was delayed
         if ($import->getStatus() === $import::DELAYED) {
             $output->writeln('<info>'.$translator->trans(
                 'mautic.lead.import.delayed',
@@ -103,6 +80,7 @@ EOT
             ).'</info>');
         }
 
+        // Success
         $output->writeln('<info>'.$translator->trans(
             'mautic.lead.import.result',
             [
@@ -113,16 +91,6 @@ EOT
                 '%time%'    => round(microtime(true) - $start, 2),
             ]
         ).'</info>');
-
-        // if ('dev' == MAUTIC_ENV) {
-        //     $output->writeln('<comment>Debug</comment>');
-        //     $debug = $event->getDebug();
-
-        //     foreach ($debug as $key => $query) {
-        //         $output->writeln("<info>$key</info>");
-        //         $output->writeln($query);
-        //     }
-        // }
 
         return 0;
     }
