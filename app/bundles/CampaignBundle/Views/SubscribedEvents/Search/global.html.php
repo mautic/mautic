890b6e651016e621a5b39fd80353b1d2086e5c1b
John Linhart <admin@escope.cz>
2017-05-24 18:23:13 +0200

    Calling router in templates the right way, not using $this to get the router

--- app/bundles/CampaignBundle/Views/SubscribedEvents/Search/global.html.php
+++ app/bundles/CampaignBundle/Views/SubscribedEvents/Search/global.html.php
@@ -11,12 +11,12 @@
 ?>
 
 <?php if (!empty($showMore)): ?>
-    <a href="<?php echo $this->container->get('router')->generate('mautic_campaign_index', ['search' => $searchString]); ?>" data-toggle="ajax">
+    <a href="<?php echo $view['router']->path('mautic_campaign_index', ['search' => $searchString]); ?>" data-toggle="ajax">
         <span><?php echo $view['translator']->trans('mautic.core.search.more', ['%count%' => $remaining]); ?></span>
     </a>
 </div>
 <?php else: ?>
-<a href="<?php echo $this->container->get('router')->generate('mautic_campaign_action', ['objectAction' => 'view', 'objectId' => $campaign->getId()]); ?>" data-toggle="ajax">
+<a href="<?php echo $view['router']->generate('mautic_campaign_action', ['objectAction' => 'view', 'objectId' => $campaign->getId()]); ?>" data-toggle="ajax">
     <?php echo $campaign->getName(); ?>
 </a>
 <?php endif; ?>
