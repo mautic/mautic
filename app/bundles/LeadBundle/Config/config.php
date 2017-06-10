dd463caff4bff22d51fc4b9d582d1b2c172d228e
Zdeno Kuzmany <zdeno@kuzmany.biz>
2017-06-10 12:26:39 +0200

    Minor

--- app/bundles/LeadBundle/Config/config.php
+++ app/bundles/LeadBundle/Config/config.php
@@ -594,6 +594,7 @@ return [
                     'mautic.channel.helper.channel_list',
                     '%mautic.track_contact_by_ip%',
                     'mautic.helper.core_parameters',
+
                 ],
             ],
             'mautic.lead.model.field' => [
