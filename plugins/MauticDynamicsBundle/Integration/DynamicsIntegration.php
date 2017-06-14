7b6ec0271807ef71722eb9a61a21f62b70c8c2f2
Werner Garcia <virlatinus@gmail.com>
2017-06-14 08:21:19 -0600

    Added settings

--- plugins/MauticDynamicsBundle/Integration/DynamicsIntegration.php
+++ plugins/MauticDynamicsBundle/Integration/DynamicsIntegration.php
@@ -13,7 +13,6 @@ namespace MauticPlugin\MauticDynamicsBundle\Integration;
 
 use Mautic\PluginBundle\Integration\AbstractIntegration;
 use Symfony\Component\Form\Form;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DynamicsIntegration extends AbstractIntegration
 {
@@ -33,13 +32,21 @@ class DynamicsIntegration extends AbstractIntegration
     }
 
     /**
+     * @return array
+     */
+    public function getSupportedFeatures()
+    {
+        return ['push_lead', 'get_leads', 'push_leads'];
+    }
+
+    /**
      * Return's authentication method such as oauth2, oauth1a, key, etc.
      *
      * @return string
      */
     public function getAuthenticationType()
     {
-        return 'none';
+        return 'oauth2';
     }
 
     /**
@@ -51,28 +58,117 @@ class DynamicsIntegration extends AbstractIntegration
     public function getRequiredKeyFields()
     {
         return [
-            'apikey' => 'mautic.integration.dynamics.apikey',
+            'resource'      => 'mautic.integration.dynamics.resource',
+            'client_id'     => 'mautic.integration.dynamics.client_id',
+            'client_secret' => 'mautic.integration.dynamics.client_secret',
+            'username'      => 'mautic.integration.dynamics.username',
+            'password'      => 'mautic.integration.dynamics.password',
+        ];
+    }
+
+    /**
+     * @param \Mautic\PluginBundle\Integration\Form|FormBuilder $builder
+     * @param array                                             $data
+     * @param string                                            $formArea
+     */
+    public function appendToForm(&$builder, $data, $formArea)
+    {
+        if ($formArea == 'features') {
+            $builder->add(
+                'objects',
+                'choice',
+                [
+                    'choices' => [
+                        'contacts' => 'mautic.dynamics.object.contact',
+                        'company'  => 'mautic.dynamics.object.company',
+                    ],
+                    'expanded'    => true,
+                    'multiple'    => true,
+                    'label'       => 'mautic.dynamics.form.objects_to_pull_from',
+                    'label_attr'  => ['class' => ''],
+                    'empty_value' => false,
+                    'required'    => false,
+                ]
+            );
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getFormSettings()
+    {
+        return [
+            'requires_callback'      => false,
+            'requires_authorization' => false,
         ];
     }
 
     /**
      * {@inheritdoc}
-     *
-     * @param $section
-     *
-     * @return string|array
      */
-    public function getFormNotes($section)
+    public function sortFieldsAlphabetically()
     {
-        if ('custom' === $section) {
-            return [
-                'template'   => 'MauticDynamicsBundle:Integration:form.html.php',
-                'parameters' => [
-                    'mauticUrl' => $this->router->generate('mautic_plugin_dynamics_callback', [], UrlGeneratorInterface::ABSOLUTE_URL),
-                ],
-            ];
-        }
+        return false;
+    }
 
-        return parent::getFormNotes($section);
+    /**
+     * @return string
+     */
+    public function getApiUrl()
+    {
+        return 'https://login.microsoftonline.com/common';
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getAccessTokenUrl()
+    {
+        return $this->getApiUrl().'/oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getAuthenticationUrl()
+    {
+        return $this->getApiUrl().'/oauth2/authorize';
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return bool
+     */
+    public function isAuthorized()
+    {
+        $keys = $this->getKeys();
+
+        return isset($keys[$this->getAuthTokenKey()]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        $keys = $this->getKeys();
+
+        return $keys[$this->getAuthTokenKey()];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return bool
+     */
+    public function getDataPriority()
+    {
+        return true;
     }
 }
