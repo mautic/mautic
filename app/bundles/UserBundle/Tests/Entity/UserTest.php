47827a66c9545cb8c4f80ff884acb551075ae766
Don Gilbert <don.gilbert@mautic.com>
2017-06-15 10:43:54 -0400

    Fix code-style in staging

--- app/bundles/UserBundle/Tests/Entity/UserTest.php
+++ app/bundles/UserBundle/Tests/Entity/UserTest.php
@@ -11,12 +11,10 @@
 
 namespace Mautic\UserBundle\Tests\Entity;
 
-
 use Mautic\UserBundle\Entity\User;
 
 class UserTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testUserIsGuest()
     {
         $user = new User(true);
