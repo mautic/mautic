70d5cc20a531a7254e1c05ec2d68a8252d92f73b
Marianela Queme <mqueme@gmail.com>
2017-06-16 14:35:06 +0100

    formatting changes

--- app/bundles/CampaignBundle/Views/Campaign/events.html.php
+++ app/bundles/CampaignBundle/Views/Campaign/events.html.php
@@ -18,13 +18,13 @@
             <div class="progress-bar progress-bar-<?php echo $yesClass; ?>" style="width:<?php echo $event['yesPercent']; ?>%; left: 0;"></div>
             <div class="progress-bar progress-bar-danger" style="width:<?php echo $event['noPercent']; ?>%; left: <?php echo $event['yesPercent']; ?>%"></div>
             <div class="box-layout">
-                <div class="col-md-1 va-m">
-                    <span class="label label-<?php echo $yesClass; ?>"><?php echo $event['yesPercent'].'%'; ?></span> (<?= $event['logCount']; ?>/<?= $event['leadCount']; ?>)
-                    <?php if ('action' !== $event['eventType']): ?>
-                    <span class="label label-danger"><?php echo $event['noPercent'].'%'; ?></span>
+                <div class="visible-sm visible-md visible-lg col-stats">
+                    <span class="mt-xs label label-<?php echo $yesClass; ?>"><?php echo $event['yesPercent'].'%'; ?></span> <?php if ('action' !== $event['eventType']): ?>
+                        <span class="label label-danger"><?php echo $event['noPercent'].'%'; ?></span>
                     <?php endif; ?>
+                <span class="mt-xs label label-warning"><?= $event['logCount']; ?> completed</span> <span class="mt-xs label label-default"><?= $event['leadCount'] - $event['logCount']; ?> pending</span>
                 </div>
-                <div class="col-md-6 va-m">
+                    <div class="col-md-5 va-m">
                     <h5 class="fw-sb text-primary mb-xs">
                         <?php echo $event['name']; ?>
                         <?php if ('action' !== $event['eventType']): ?>
@@ -34,7 +34,7 @@
                     </h5>
                     <h6 class="text-white dark-sm"><?php echo $event['description']; ?></h6>
                 </div>
-                <div class="col-md-4 va-m text-right">
+                <div class="col-md-3 va-m text-right">
                     <em class="text-white dark-sm"><?php echo $view['translator']->trans('mautic.campaign.'.$event['type']); ?></em>
                 </div>
             </div>
