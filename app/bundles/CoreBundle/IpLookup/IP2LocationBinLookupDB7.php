b6866b9eb53692966b13a1cb3036b3a55cbe0fb3
Jurian Janssen <jurian.janssen@gmail.com>
2017-06-21 15:21:15 +0200

    Implemented local IP lookup for IP2Location DB7

---
+++ app/bundles/CoreBundle/IpLookup/IP2LocationBinLookupDB7.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * @copyright   2017 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Beechit\IpBundle\IpLookup;
+
+namespace Mautic\CoreBundle\IpLookup;
+
+use IP2Location\Database;
+
+class IP2LocationBinLookupDB7 extends AbstractLocalDataLookup
+{
+
+    /**
+     * Return attribution HTML displayed in the configuration UI.
+     *
+     * @return string
+     */
+    public function getAttribution()
+    {
+        return 'IP2Location Local Bin File DB7BIN only';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocalDataStoreFilepath()
+    {
+        return $this->getDataDir();
+    }
+
+    /**
+     * @return string
+     */
+    public function getRemoteDateStoreDownloadUrl()
+    {
+        $usernamePass = explode(':', $this->auth);
+        $data         = [];
+
+        if (isset($usernamePass[0]) && isset($usernamePass[1])) {
+            $data['login']       = $usernamePass[0];
+            $data['password']    = $usernamePass[1];
+            $data['productcode'] = 'DB7BIN';
+            $queryString         = http_build_query($data);
+            // the system gets the file name from end of remove file path url so use hardedcoded name
+            $queryString .= '&filename=/ip2location.zip';
+
+            return 'https://www.ip2location.com/download?'.$queryString;
+        } else {
+            $this->logger->warn('Both username and password are required');
+        }
+    }
+
+    /**
+     * Executes the lookup of the IP address.
+     */
+    protected function lookup()
+    {
+        try {
+            $reader = new Database($this->getLocalDataStoreFilepath().'/IP-COUNTRY-REGION-CITY-ISP-DOMAIN.BIN', Database::FILE_IO);
+            $record = $reader->lookup($this->ip, Database::ALL);
+
+            if (isset($record['countryName'])) {
+                $this->country      = $record['countryName'];
+                $this->region       = $record['regionName'];
+                $this->city         = $record['cityName'];
+                $this->latitude     = $record['latitude'];
+                $this->longitude    = $record['longitude'];
+                $this->zipcode      = $record['zipCode'];
+                $this->isp          = $record['isp'];
+                $this->timezone     = $record['timeZone'];
+                $this->organization = $record['domain'];
+            }
+        } catch (\Exception $exception) {
+            if ($this->logger) {
+                $this->logger->warn('IP LOOKUP: '.$exception->getMessage());
+            }
+        }
+    }
+}
