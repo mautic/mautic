f7976c5f586981eae71f42cebf633d873ab9f179
Werner Garcia <virlatinus@gmail.com>
2017-06-22 08:53:29 -0600

    Fixed array_flip error

--- app/bundles/CoreBundle/Form/ChoiceLoader/EntityLookupChoiceLoader.php
+++ app/bundles/CoreBundle/Form/ChoiceLoader/EntityLookupChoiceLoader.php
@@ -219,9 +219,9 @@ class EntityLookupChoiceLoader implements ChoiceLoaderInterface
 
         if (!$isGrouped) {
             // fix for array_count_values error when there are null values
-            $ar = array_replace($prepped, array_fill_keys(array_keys($prepped, null), ''));
+            $prepped = array_replace($prepped, array_fill_keys(array_keys($prepped, null), ''));
             // Same labels will cause options to be merged with Symfony 2.8+ so ensure labels are unique
-            $counts     = array_count_values($ar);
+            $counts     = array_count_values($prepped);
             $duplicates = array_filter(
                 $prepped,
                 function ($value) use ($counts) {
