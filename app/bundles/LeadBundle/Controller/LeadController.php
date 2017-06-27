521e3f8553ae160ec4466df3c365bb84220a0018
Marianela Queme <mqueme@gmail.com>
2017-06-27 13:21:45 +0100

    applied changes from prev PR

--- app/bundles/LeadBundle/Controller/LeadController.php
+++ app/bundles/LeadBundle/Controller/LeadController.php
@@ -184,7 +184,7 @@ class LeadController extends FormController
                     'currentList'      => $list,
                     'security'         => $this->get('mautic.security'),
                     'inSingleList'     => $inSingleList,
-                    'noContactList'    => $emailRepo->getDoNotEmailList(),
+                    'noContactList'    => $emailRepo->getDoNotEmailList(array_keys($leads)),
                     'maxLeadId'        => $maxLeadId,
                     'anonymousShowing' => $anonymousShowing,
                 ],
