af71699380c9e5c24da3c7603cfc786c8a356add
Zdeno Kuzmany <zdeno@kuzmany.biz>
2017-07-17 23:22:43 +0200

    Campaign action fix

--- plugins/MauticMessengerBundle/EventListener/CampaignSubscriber.php
+++ plugins/MauticMessengerBundle/EventListener/CampaignSubscriber.php
@@ -1,46 +1,44 @@
 <?php
+
 namespace MauticPlugin\MauticMessengerBundle\EventListener;
 
-use Mautic\CoreBundle\EventListener\CommonSubscriber;
-use Mautic\CampaignBundle\Model\EventModel;
+use Doctrine\DBAL\Connection;
+use Mautic\CampaignBundle\CampaignEvents;
+use Mautic\CampaignBundle\Event\CampaignBuilderEvent;
+use Mautic\CampaignBundle\Event\CampaignExecutionEvent;
 use Mautic\CampaignBundle\Model\CampaignModel;
+use Mautic\CampaignBundle\Model\EventModel;
+use Mautic\CoreBundle\EventListener\CommonSubscriber;
+use Mautic\CoreBundle\Helper\CookieHelper;
 use Mautic\LeadBundle\Model\LeadModel;
 use Mautic\PageBundle\Model\PageModel;
-use Mautic\PageBundle\Entity\Hit;
-use Mautic\CampaignBundle\CampaignEvents;
-use Mautic\CampaignBundle\Event\CampaignExecutionEvent;
-use Mautic\CampaignBundle\Event\CampaignLeadChangeEvent;
-use Mautic\CampaignBundle\Event\CampaignBuilderEvent;
 use MauticPlugin\MauticMessengerBundle\MessengerEvents;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Doctrine\DBAL\Connection;
-use Mautic\CoreBundle\Helper\CookieHelper;
 
 class CampaignSubscriber extends CommonSubscriber
 {
-
     /**
-     * @var $campaignModel
+     * @var
      */
     protected $campaignModel;
     /**
-     * @var $cookieHelper ;
+     * @var ;
      */
     protected $cookieHelper;
 
     /**
-     * @var $db
+     * @var
      */
     protected $db;
 
     /**
-     * @var $request
+     * @var
      */
     protected $request;
 
     /**
-     * @var $sesion
+     * @var
      */
     protected $sesion;
 
@@ -55,7 +53,7 @@ class CampaignSubscriber extends CommonSubscriber
     protected $campaignEventModel;
 
     /**
-     * @var $pageModel
+     * @var
      */
     protected $pageModel;
 
@@ -75,26 +73,23 @@ class CampaignSubscriber extends CommonSubscriber
         CampaignModel $campaignModel
     ) {
         $this->campaignEventModel = $campaignEventModel;
-        $this->leadModel = $leadModel;
-        $this->session = $session;
-        $this->pageModel = $pageModel;
-        $this->request = $requestStack->getCurrentRequest();
-        $this->db = $db;
-        $this->cookieHelper = $cookieHelper;
-        $this->campaignModel = $campaignModel;
+        $this->leadModel          = $leadModel;
+        $this->session            = $session;
+        $this->pageModel          = $pageModel;
+        $this->request            = $requestStack->getCurrentRequest();
+        $this->db                 = $db;
+        $this->cookieHelper       = $cookieHelper;
+        $this->campaignModel      = $campaignModel;
     }
 
-
-    static public function getSubscribedEvents()
+    public static function getSubscribedEvents()
     {
         return [
-            CampaignEvents::CAMPAIGN_ON_BUILD => ['onCampaignBuild', 0],
-            MessengerEvents::MESSENGER_ON_SEND => array('onCampaignTriggerAction', 0),
+            CampaignEvents::CAMPAIGN_ON_BUILD  => ['onCampaignBuild', 0],
+            MessengerEvents::MESSENGER_ON_SEND => ['onCampaignTriggerAction', 0],
 
         ];
     }
-
-
 
     /**
      * Add event triggers and actions.
@@ -103,27 +98,23 @@ class CampaignSubscriber extends CommonSubscriber
      */
     public function onCampaignBuild(CampaignBuilderEvent $event)
     {
-
         $action = [
-            'label' => 'plugin.messenger.send_to_messanger',
-            'eventName' => MessengerEvents::ON_CAMPAIGN_TRIGGER_ACTION,
-            'formType' => 'messengerevent_send_to_messenger',
-            'channel' => 'messenger',
+            'label'          => 'plugin.messenger.send_to_messanger',
+            'eventName'      => MessengerEvents::ON_CAMPAIGN_TRIGGER_ACTION,
+            'formType'       => 'messenger_send_to_messenger',
+            'channel'        => 'messenger',
             'channelIdField' => 'id',
         ];
         $event->addAction('messanger.send_to_messanger', $action);
-
     }
-
 
     /**
      * @param CampaignExecutionEvent $event
      */
-    public function onCampaignTriggerAction(CampaignExecutionEvent $event) {
-        $lead = $event->getLead();
-        $eventConfig = $event->getConfig();
+    public function onCampaignTriggerAction(CampaignExecutionEvent $event)
+    {
+        $lead         = $event->getLead();
+        $eventConfig  = $event->getConfig();
         $eventDetails = $event->getEventDetails();
-
-
     }
 }
