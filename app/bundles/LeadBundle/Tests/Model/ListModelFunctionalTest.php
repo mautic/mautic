936da7a7d7b6dbd4f465b573e90e30c1c49aa28e
Alan Hartless <alan@devkardia.com>
2017-07-31 18:41:01 -0400

    Added functional tests for include/exclude segments and fixed the existing tests to ensure filters are hydrated

--- app/bundles/LeadBundle/Tests/Model/ListModelFunctionalTest.php
+++ app/bundles/LeadBundle/Tests/Model/ListModelFunctionalTest.php
@@ -9,22 +9,48 @@ class ListModelFunctionalTest extends MauticWebTestCase
 {
     public function testSegmentCountIsCorrect()
     {
-        $repo              = $this->em->getRepository(LeadList::class);
-        $segmentTest1Ref   = $this->fixtures->getReference('segment-test-1');
-        $segmentTest2Ref   = $this->fixtures->getReference('segment-test-2');
-        $segmentTest3Ref   = $this->fixtures->getReference('segment-test-3');
-        $segmentTest4Ref   = $this->fixtures->getReference('segment-test-4');
-        $segmentTest5Ref   = $this->fixtures->getReference('segment-test-5');
-        $likePercentEndRef = $this->fixtures->getReference('like-percent-end');
+        $repo                                          = $this->em->getRepository(LeadList::class);
+        $segmentTest1Ref                               = $this->fixtures->getReference('segment-test-1');
+        $segmentTest2Ref                               = $this->fixtures->getReference('segment-test-2');
+        $segmentTest3Ref                               = $this->fixtures->getReference('segment-test-3');
+        $segmentTest4Ref                               = $this->fixtures->getReference('segment-test-4');
+        $segmentTest5Ref                               = $this->fixtures->getReference('segment-test-5');
+        $likePercentEndRef                             = $this->fixtures->getReference('like-percent-end');
+        $segmentTestWithoutFiltersRef                  = $this->fixtures->getReference('segment-test-without-filters');
+        $segmentTestIncludeMembershipWithFiltersRef    = $this->fixtures->getReference('segment-test-include-segment-with-filters');
+        $segmentTestExcludeMembershipWithFiltersRef    = $this->fixtures->getReference('segment-test-exclude-segment-with-filters');
+        $segmentTestIncludeMembershipWithoutFiltersRef = $this->fixtures->getReference('segment-test-include-segment-without-filters');
+        $segmentTestExcludeMembershipWithoutFiltersRef = $this->fixtures->getReference('segment-test-exclude-segment-without-filters');
+        $segmentTestIncludeMembershipMixedFiltersRef   = $this->fixtures->getReference('segment-test-include-segment-mixed-filters');
+        $segmentTestExcludeMembershipMixedFiltersRef   = $this->fixtures->getReference('segment-test-exclude-segment-mixed-filters');
+        $segmentTestMixedIncludeExcludeRef             = $this->fixtures->getReference('segment-test-mixed-include-exclude-filters');
+        $segmentTestManualMembership                   = $this->fixtures->getReference('segment-test-manual-membership');
+        $segmentTestIncludeMembershipManualMembersRef  = $this->fixtures->getReference('segment-test-include-segment-manual-members');
+        $segmentTestExcludeMembershipManualMembersRef  = $this->fixtures->getReference('segment-test-exclude-segment-manual-members');
 
-        $segmentContacts = $repo->getLeadsByList([
-            $segmentTest1Ref->getId(),
-            $segmentTest2Ref->getId(),
-            $segmentTest3Ref->getId(),
-            $segmentTest4Ref->getId(),
-            $segmentTest5Ref->getId(),
-            $likePercentEndRef->getId(),
-        ], ['countOnly' => true]);
+        // These expect filters to be part of the $lists passed to getLeadsByList so pass the entity
+        $segmentContacts = $repo->getLeadsByList(
+            [
+                $segmentTest1Ref,
+                $segmentTest2Ref,
+                $segmentTest3Ref,
+                $segmentTest4Ref,
+                $segmentTest5Ref,
+                $likePercentEndRef,
+                $segmentTestWithoutFiltersRef,
+                $segmentTestIncludeMembershipWithFiltersRef,
+                $segmentTestExcludeMembershipWithFiltersRef,
+                $segmentTestIncludeMembershipWithoutFiltersRef,
+                $segmentTestExcludeMembershipWithoutFiltersRef,
+                $segmentTestIncludeMembershipMixedFiltersRef,
+                $segmentTestExcludeMembershipMixedFiltersRef,
+                $segmentTestMixedIncludeExcludeRef,
+                $segmentTestManualMembership,
+                $segmentTestIncludeMembershipManualMembersRef,
+                $segmentTestExcludeMembershipManualMembersRef
+            ],
+            ['countOnly' => true]
+        );
 
         $this->assertEquals(
             1,
@@ -39,9 +65,9 @@ class ListModelFunctionalTest extends MauticWebTestCase
         );
 
         $this->assertEquals(
-            0,
+            24,
             $segmentContacts[$segmentTest3Ref->getId()]['count'],
-            'There should be 0 contacts in the segment-test-3 segment because the segment has not been built yet.'
+            'There should be 24 contacts in the segment-test-3 segment'
         );
 
         $this->assertEquals(
@@ -51,15 +77,81 @@ class ListModelFunctionalTest extends MauticWebTestCase
         );
 
         $this->assertEquals(
-            49,
+            50,
             $segmentContacts[$segmentTest5Ref->getId()]['count'],
-            'There should be 49 contacts in the segment-test-5 segment.'
+            'There should be 50 contacts in the segment-test-5 segment.'
         );
 
         $this->assertEquals(
             32,
             $segmentContacts[$likePercentEndRef->getId()]['count'],
             'There should be 32 contacts in the like-percent-end segment.'
+        );
+
+        $this->assertEquals(
+            0,
+            $segmentContacts[$segmentTestWithoutFiltersRef->getId()]['count'],
+            'There should be 0 contacts in the segment-test-without-filters segment.'
+        );
+
+        $this->assertEquals(
+            26,
+            $segmentContacts[$segmentTestIncludeMembershipWithFiltersRef->getId()]['count'],
+            'There should be 26 contacts in the segment-test-include-segment-with-filters segment. 24 from segment-test-3 that was not added yet plus 4 from segment-test-2 minus 2 for being in both = 26.'
+        );
+
+        $this->assertEquals(
+            7,
+            $segmentContacts[$segmentTestExcludeMembershipWithFiltersRef->getId()]['count'],
+            'There should be 7 contacts in the segment-test-exclude-segment-with-filters segment. 8 that are in the US minus 1 that is in segment-test-3.'
+        );
+
+        $this->assertEquals(
+            0,
+            $segmentContacts[$segmentTestIncludeMembershipWithoutFiltersRef->getId()]['count'],
+            'There should be 0 contacts as there is no one in segment-test-without-filters'
+        );
+
+        $this->assertEquals(
+            11,
+            $segmentContacts[$segmentTestExcludeMembershipWithoutFiltersRef->getId()]['count'],
+            'There should be 11 contacts in the United Kingdom and 0 from segment-test-without-filters.'
+        );
+
+        $this->assertEquals(
+            24,
+            $segmentContacts[$segmentTestIncludeMembershipMixedFiltersRef->getId()]['count'],
+            'There should be 24 contacts. 0 from segment-test-without-filters and 24 from segment-test-3.'
+        );
+
+        $this->assertEquals(
+            27,
+            $segmentContacts[$segmentTestExcludeMembershipMixedFiltersRef->getId()]['count'],
+            'There should be 24 contacts. 0 from segment-test-without-filters and 24 from segment-test-3.'
+        );
+
+        $this->assertEquals(
+            8,
+            $segmentContacts[$segmentTestMixedIncludeExcludeRef->getId()]['count'],
+            'There should be 8 contacts. 32 from like-percent-end minus 24 from segment-test-3.'
+        );
+
+        $this->assertEquals(
+            12,
+            $segmentContacts[$segmentTestManualMembership->getId()]['count'],
+            'There should be 12 contacts. 11 in the United Kingdom plus 3 manually added minus 2 manually removed.'
+        );
+
+        $this->assertEquals(
+            12,
+            $segmentContacts[$segmentTestIncludeMembershipManualMembersRef->getId()]['count'],
+            'There should be 12 contacts in the included segment-test-manual-membership segment'
+        );
+
+        $this->assertEquals(
+            25,
+            $segmentContacts[$segmentTestExcludeMembershipManualMembersRef->getId()]['count'],
+            'There should be 12 contacts in the included segment-test-manual-membership segment'
         );
     }
 
@@ -89,7 +181,7 @@ class ListModelFunctionalTest extends MauticWebTestCase
         ]);
 
         $segmentContacts = $repo->getLeadsByList([
-            $segmentTest3Ref->getId(),
+            $segmentTest3Ref,
         ], ['countOnly' => true]);
 
         $this->assertEquals(
@@ -107,7 +199,7 @@ class ListModelFunctionalTest extends MauticWebTestCase
         ]);
 
         $segmentContacts = $repo->getLeadsByList([
-            $segmentTest3Ref->getId(),
+            $segmentTest3Ref,
         ], ['countOnly' => true]);
 
         $this->assertEquals(
