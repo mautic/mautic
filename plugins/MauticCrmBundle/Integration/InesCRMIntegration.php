884eba7182edf4447257b578f12b0fe27779a0c3
Maximilien Mellen <mme@webmecanik.com>
2017-08-07 17:16:42 +0200

    Display Ines CRM thumbnail

---
+++ plugins/MauticCrmBundle/Integration/InesCRMIntegration.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MauticPlugin\MauticCrmBundle\Integration;
+
+class InesCRMIntegration extends CrmAbstractIntegration
+{
+    public function getName()
+    {
+        return 'InesCRM';
+    }
+
+    public function getDisplayName()
+    {
+        return 'Ines CRM';
+    }
+}
