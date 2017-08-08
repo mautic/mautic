f26a1d36e38c6c42f242d7b70b159c31badabd08
Maximilien Mellen <mme@webmecanik.com>
2017-08-08 14:22:02 +0200

    Set up required fields

--- plugins/MauticCrmBundle/Integration/InesCRMIntegration.php
+++ plugins/MauticCrmBundle/Integration/InesCRMIntegration.php
@@ -13,4 +13,29 @@ class InesCRMIntegration extends CrmAbstractIntegration
     {
         return 'Ines CRM';
     }
+
+    public function getRequiredKeyFields()
+    {
+        return [
+            'account'  => 'mautic.ines_crm.form.account',
+            'username' => 'mautic.ines_crm.form.username',
+            'password' => 'mautic.ines_crm.form.password',
+        ];
+    }
+
+    public function getSecretKeys()
+    {
+        return [
+            'password',
+        ];
+    }
+
+    public function getSupportedFeatures()
+    {
+        return ['push_lead'];
+    }
+
+    public function pushLead($lead, $config = []) {
+        // Push lead to integration
+    }
 }
