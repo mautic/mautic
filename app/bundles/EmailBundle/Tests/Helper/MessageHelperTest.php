ab3b02975dfefe623e0b1396b57290675d7d1d07
andrassgray <andrass.grau@gmail.com>
2017-08-10 21:57:34 +0200

    fixed bounce recognitions for "No such user" added "Relay disallowed" added  "Host or domain name not found" added some test cases

--- app/bundles/EmailBundle/Tests/Helper/MessageHelperTest.php
+++ app/bundles/EmailBundle/Tests/Helper/MessageHelperTest.php
@@ -76,7 +76,7 @@ BODY;
     public function bodyLinesProvider ()
     {
         return [
-            "mailbox full-1" => ["full", "554 5.2.2 <user@free.fr>: Recipient address rejected: Quota exceeded (mailbox for user is full)"],
+            "mailbox full-1" => ["full", "554 5.2.2 <user@domain.fr>: Recipient address rejected: Quota exceeded (mailbox for user is full)"],
             "mailbox full-2" => ["full", "The email account that you tried to reach is over quota."],
             "unknown-1" => ["unknown", "550 5.1.1 The email account that you tried to reach does not exist."],
             "spam-1" => ["antispam", "550-5.7.1 [193.198.1.11] Our system has detected that this message is\r550-5.7.1 likely unsolicited mail."]
