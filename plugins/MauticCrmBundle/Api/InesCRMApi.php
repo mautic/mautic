be74df2a3dd5e50de83f9b8e2079dadd8285f477
Maximilien Mellen <mme@webmecanik.com>
2017-08-11 17:21:03 +0200

    Log into and get custom fields from INES CRM

--- plugins/MauticCrmBundle/Api/InesCRMApi.php
+++ plugins/MauticCrmBundle/Api/InesCRMApi.php
@@ -2,17 +2,59 @@
 
 namespace MauticPlugin\MauticCrmBundle\Api;
 
-use GuzzleHttp;
+use GuzzleHttp; // FIXME: to remove along with mock requests
 use MauticPlugin\MauticCrmBundle\Integration\CrmAbstractIntegration;
+use Mautic\PluginBundle\Exception\ApiErrorException;
 
 class InesCRMApi extends CrmApi
 {
+    private $translator;
+
+    private $rootUrl = 'https://webservices.inescrm.com';
+
+    // FIXME: to remove along with mock requests
     private $client;
+
+    private $loginClient;
+
+    private $automationSyncClient;
 
     public function __construct(CrmAbstractIntegration $integration) {
         parent::__construct($integration);
+        $this->translator = $integration->getTranslator();
 
+        // FIXME: to remove along with mock requests
         $this->client = new GuzzleHttp\Client();
+
+        $this->loginClient = $this->makeClient('/wslogin/login.asmx');
+        $this->automationSyncClient = $this->makeClient('/ws/WSAutomationSync.asmx');
+    }
+
+    private function makeClient($path) {
+        return new \SoapClient($this->rootUrl . $path . '?wsdl');
+    }
+
+    private function getSessionId() {
+        // TODO: cache session ID
+
+        $keys = $this->integration->getDecryptedApiKeys();
+
+        try {
+            $response = $this->loginClient->authenticationWs([
+                'request' => $keys,
+            ]);
+        } catch (\SoapFault $e) {
+            throw new ApiErrorException($this->translator->trans('mautic.ines_crm.form.invalid_identifiers'));
+        }
+
+        return $response->authenticationWsResult->idSession;
+    }
+
+    private function setAuthHeaders($client) {
+        $sessionId = $this->getSessionId();
+
+        $headers = new \SoapHeader('http://webservice.ines.fr', 'SessionID', ['ID' => $sessionId]);
+        $client->__setSoapHeaders($headers);
     }
 
     public function createLead($mappedData) {
@@ -25,5 +67,14 @@ class InesCRMApi extends CrmApi
         $this->client->request('POST', 'http://localhost:4567/push_company', [
             'form_params' => $mappedData
         ]);
+    }
+
+    public function getLeadFields() {
+        $client = $this->automationSyncClient;
+        $this->setAuthHeaders($client);
+
+        $response = $client->GetSyncInfo();
+
+        // TODO: Do something with the response
     }
 }
