a0404a47b055372c253b8012742de35c40a2508e
John Linhart <admin@escope.cz>
2017-08-23 10:09:53 +0200

    Fixing "Class Mautic\LeadBundle\Tests\LeadRepositoryTest was not found"

--- app/bundles/LeadBundle/Tests/Entity/LeadRepositoryTest.php
+++ app/bundles/LeadBundle/Tests/Entity/LeadRepositoryTest.php
@@ -9,7 +9,7 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\LeadBundle\Tests;
+namespace Mautic\LeadBundle\Tests\Entity;
 
 use Mautic\LeadBundle\Entity\CustomFieldRepositoryTrait;
 
