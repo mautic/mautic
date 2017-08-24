b314f589c9e8d3d716748e9a57e7f352430cf5dc
Gabe Alvarez-Millard <gabe@Gabes-MBP.fios-router.home>
2017-08-24 13:16:55 -0400

    switch back to prod staging

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
 /*
- * @copyright   2014 Mautic, NP
+ * @copyright   2014 Mautic Contributors. All rights reserved
  * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 define('MAUTIC_ROOT_DIR', __DIR__);
@@ -11,18 +14,21 @@ define('MAUTIC_ROOT_DIR', __DIR__);
 date_default_timezone_set('UTC');
 
 use Mautic\Middleware\MiddlewareBuilder;
+use Symfony\Component\ClassLoader\ApcClassLoader;
 
-$loader = require_once __DIR__.'/vendor/autoload.php';
+$loader = require_once __DIR__.'/app/autoload.php';
 
 /*
- * If you don't want to setup permissions the proper way, just uncomment the following PHP line
- * read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+ * Use APC for autoloading to improve performance. Change 'sf2' to a unique prefix
+ * in order to prevent cache key conflicts with other applications also using APC.
  */
-//umask(0000);
+//$apcLoader = new ApcClassLoader('sf2', $loader);
+//$loader->unregister();
+//$apcLoader->register(true);
 
-\Mautic\CoreBundle\ErrorHandler\ErrorHandler::register('dev');
+\Mautic\CoreBundle\ErrorHandler\ErrorHandler::register('prod');
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
-Stack\run((new MiddlewareBuilder('dev'))->resolve($kernel));
+Stack\run((new MiddlewareBuilder('prod'))->resolve($kernel));
