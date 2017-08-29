a5308153f735da112f0f702d15f16ab6d6ac3fd1
Arul Raj <arulrajsm2802@gmail.com>
2017-08-29 13:44:11 +0530

    migration file to email reply campaign event

---
+++ app/migrations/Version20170829081048.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mautic\Migrations;
+
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170829081048 extends AbstractMauticMigration
+{
+    
+    /**
+     * @param Schema $schema
+     *
+     * @throws SkipMigrationException
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function preUp(Schema $schema)
+    {
+        $table = $schema->getTable($this->prefix.'email_stats');
+
+        if ($table->hasColumn('is_replyed')) {
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+    
+    
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE '.$this->prefix.'email_stats ADD is_replyed TINYINT(1) NOT NULL');
+    }
+}
