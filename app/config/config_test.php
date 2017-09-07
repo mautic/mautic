7d94895562bf88c8c1c2c6955660bd6610557185
Don Gilbert <don.gilbert@mautic.com>
2017-09-07 12:39:46 -0400

    Migrate corebundle tests to codeception format

--- app/config/config_test.php
+++ app/config/config_test.php
@@ -1,6 +1,7 @@
 <?php
 
 use MauticPlugin\MauticCrmBundle\Tests\Pipedrive\Mock\Client;
+use Symfony\Component\Dotenv\Dotenv;
 
 /*
  * @copyright   2014 Mautic Contributors. All rights reserved
@@ -11,6 +12,14 @@ use MauticPlugin\MauticCrmBundle\Tests\Pipedrive\Mock\Client;
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 $loader->import('config.php');
+
+// Load environment variables from .env.test file
+$env = new Dotenv();
+$env->load(__DIR__.'/../../.env.test');
+
+// Define some constants from .env
+defined('MAUTIC_DB_PREFIX') || define('MAUTIC_DB_PREFIX', getenv('MAUTIC_DB_PREFIX') ?: '');
+defined('MAUTIC_ENV') || define('MAUTIC_ENV', getenv('MAUTIC_ENV') ?: 'test');
 
 $container->loadFromExtension('framework', [
     'test'    => true,
@@ -47,11 +56,11 @@ $container->loadFromExtension('doctrine', [
         'connections'        => [
             'default' => [
                 'driver'   => 'pdo_mysql',
-                'host'     => isset($_SERVER['DB_HOST']) ? $_SERVER['DB_HOST'] : '%mautic.db_host%',
-                'port'     => isset($_SERVER['DB_PORT']) ? $_SERVER['DB_PORT'] : '%mautic.db_port%',
-                'dbname'   => isset($_SERVER['DB_NAME']) ? $_SERVER['DB_NAME'] : '%mautic.db_name%',
-                'user'     => isset($_SERVER['DB_USER']) ? $_SERVER['DB_USER'] : '%mautic.db_user%',
-                'password' => isset($_SERVER['DB_PASSWD']) ? $_SERVER['DB_PASSWD'] : '%mautic.db_password%',
+                'host'     => getenv('DB_HOST') ?: '%mautic.db_host%',
+                'port'     => getenv('DB_PORT') ?: '%mautic.db_port%',
+                'dbname'   => getenv('DB_NAME') ?: '%mautic.db_name%',
+                'user'     => getenv('DB_USER') ?: '%mautic.db_user%',
+                'password' => getenv('DB_PASSWD') ?: '%mautic.db_password%',
                 'charset'  => 'UTF8',
                 // Prevent Doctrine from crapping out with "unsupported type" errors due to it examining all tables in the database and not just Mautic's
                 'mapping_types' => [
