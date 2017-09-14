c10c55c9e3b7e08284756714a27fdbd7f875841a
Marianela Queme <mqueme@gmail.com>
2017-09-14 15:33:23 +0100

    update dnc based on last updated date

--- app/bundles/PluginBundle/Integration/AbstractIntegration.php
+++ app/bundles/PluginBundle/Integration/AbstractIntegration.php
@@ -2683,12 +2683,12 @@ abstract class AbstractIntegration
 
     public function getLeadDonotContact($leadId, $channel = 'email',  $sfObject = 'Lead', $sfIds = [])
     {
-        $isContactable = 0;
+        $isContactable = 1;
         $lead          = $this->leadModel->getEntity($leadId);
         if ($lead) {
             $isContactableReason = $this->leadModel->isContactable($lead, $channel);
             if ($isContactableReason === 0) {
-                $isContactable = 1;
+                $isContactable = 0;
             }
         }
 
