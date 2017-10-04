71fd024b5831904bb2b0f26317d89aadbf7c4cba
Virlatinus <werner.garcia@gmail.com>
2017-10-04 08:04:03 -0600

    Added missing column to migration file

--- app/migrations/Version20170923014659.php
+++ app/migrations/Version20170923014659.php
@@ -17,6 +17,13 @@ class Version20170923014659 extends AbstractMauticMigration
     public function up(Schema $schema)
     {
         $this->addSql('ALTER TABLE '.$this->prefix.'pages ADD is_preference_center tinyint(1) DEFAULT NULL');
+
+        $idx = $this->generatePropertyName('emails', 'idx', ['preference_center_id']);
+        $fk  = $this->generatePropertyName('emails', 'fk', ['preference_center_id']);
+
+        $this->addSql("ALTER TABLE {$this->prefix}emails ADD preference_center_id int DEFAULT NULL");
+        $this->addSql("ALTER TABLE {$this->prefix}emails ADD CONSTRAINT $fk FOREIGN KEY (preference_center_id) REFERENCES {$this->prefix}pages (id) ON DELETE SET NULL");
+        $this->addSql("CREATE INDEX $idx ON {$this->prefix}emails (preference_center_id)");
     }
 
     /**
@@ -27,9 +34,10 @@ class Version20170923014659 extends AbstractMauticMigration
      */
     public function preUp(Schema $schema)
     {
-        $table = $schema->getTable($this->prefix.'pages');
+        $table1 = $schema->getTable($this->prefix.'pages');
+        $table2 = $schema->getTable($this->prefix.'emails');
 
-        if ($table->hasColumn('is_preference_center')) {
+        if ($table1->hasColumn('is_preference_center') || $table2->hasColumn('preference_center_id')) {
             throw new SkipMigrationException('Schema includes this migration');
         }
     }
