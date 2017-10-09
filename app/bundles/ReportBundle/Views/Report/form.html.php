87d29cba5c9819854fef25e2289f73f89aa3803e
Zdeno Kuzmany <zdeno@kuzmany.biz>
2017-10-09 18:40:35 +0200

    Move dynamic filters settings to right column

--- app/bundles/ReportBundle/Views/Report/form.html.php
+++ app/bundles/ReportBundle/Views/Report/form.html.php
@@ -12,8 +12,10 @@ $view->extend('MauticCoreBundle:Default:content.html.php');
 $view['slots']->set('mauticContent', 'report');
 
 $header = ($report->getId()) ?
-    $view['translator']->trans('mautic.report.report.header.edit',
-        ['%name%' => $view['translator']->trans($report->getName())]) :
+    $view['translator']->trans(
+        'mautic.report.report.header.edit',
+        ['%name%' => $view['translator']->trans($report->getName())]
+    ) :
     $view['translator']->trans('mautic.report.report.header.new');
 
 $view['slots']->set('headerTitle', $header);
@@ -22,124 +24,136 @@ $showGraphTab = count($form['graphs']->vars['choices']);
 
 <?php echo $view['form']->start($form); ?>
 <?php echo $view['form']->errors($form, true); ?>
-<div class="box-layout">
-    <div class="col-md-9 bg-white height-auto">
-        <div class="row">
-            <div class="col-xs-12">
-                <!-- tabs controls -->
-                <ul class="bg-auto nav nav-tabs pr-md pl-md">
-                    <li class="active">
-                        <a href="#details-container" role="tab" data-toggle="tab"><?php echo $view['translator']->trans('mautic.core.details'); ?></a>
-                    </li>
-                    <li class="">
-                        <a href="#data-container" role="tab" data-toggle="tab"><?php echo $view['translator']->trans('mautic.report.tab.data'); ?></a>
-                    </li>
-                    <li class="<?php if (!$showGraphTab): echo 'hide'; endif; ?>" id="graphs-tab">
-                        <a href="#graphs-container" role="tab" data-toggle="tab"><?php echo $view['translator']->trans('mautic.report.tab.graphs'); ?></a>
-                    </li>
-                </ul>
-                <!--/ tabs controls -->
+    <div class="box-layout">
+        <div class="col-md-9 bg-white height-auto">
+            <div class="row">
+                <div class="col-xs-12">
+                    <!-- tabs controls -->
+                    <ul class="bg-auto nav nav-tabs pr-md pl-md">
+                        <li class="active">
+                            <a href="#details-container" role="tab"
+                               data-toggle="tab"><?php echo $view['translator']->trans('mautic.core.details'); ?></a>
+                        </li>
+                        <li class="">
+                            <a href="#data-container" role="tab"
+                               data-toggle="tab"><?php echo $view['translator']->trans('mautic.report.tab.data'); ?></a>
+                        </li>
+                        <li class="<?php if (!$showGraphTab): echo 'hide'; endif; ?>" id="graphs-tab">
+                            <a href="#graphs-container" role="tab"
+                               data-toggle="tab"><?php echo $view['translator']->trans(
+                                    'mautic.report.tab.graphs'
+                                ); ?></a>
+                        </li>
+                    </ul>
+                    <!--/ tabs controls -->
 
-                <div class="tab-content pa-md">
-                    <div class="tab-pane fade in active bdr-w-0" id="details-container">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="pa-md">
-                                    <?php echo $view['form']->row($form['name']); ?>
+                    <div class="tab-content pa-md">
+                        <div class="tab-pane fade in active bdr-w-0" id="details-container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="pa-md">
+                                        <?php echo $view['form']->row($form['name']); ?>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="pa-md">
+                                        <?php echo $view['form']->row($form['source']); ?>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="pa-md">
-                                    <?php echo $view['form']->row($form['source']); ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="pa-md">
-                                    <?php echo $view['form']->row($form['description']); ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="tab-pane fade bdr-w-0" id="data-container">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="pa-md">
-                                    <h4><strong><?php echo $view['translator']->trans('mautic.report.report.form.columnselector'); ?></strong></h4>
-                                    <?php echo $view['form']->row($form['columns']); ?>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="ml-md">
-                                    <h4><strong><?php echo $view['translator']->trans('mautic.core.order'); ?></strong></h4>
-                                    <?php echo $view['form']->row($form['tableOrder']); ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="pa-md">
-                                    <h4><strong><?php echo $view['translator']->trans('mautic.core.filters'); ?></strong></h4>
-                                    <?php echo $view['form']->row($form['filters']); ?>
-                                </div>
-                                <div class="hide">
-                                    <div id="filterValueYesNoTemplate">
-                                        <?php echo $view['form']->widget($form['value_template_yesno']); ?>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="pa-md">
+                                        <?php echo $view['form']->row($form['description']); ?>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="pa-md">
-                                    <h4><strong><?php echo $view['translator']->trans('mautic.report.form.groupby'); ?></strong></h4>
-                                    <?php echo $view['form']->row($form['groupBy']); ?>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="pa-md">
-                                    <h4><strong><?php echo $view['translator']->trans('mautic.core.calculated.fields'); ?></strong></h4>
-                                    <?php echo $view['form']->row($form['aggregators']); ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="pa-md">
-                                    <h4><strong><?php echo $view['translator']->trans('mautic.report.report.form.display.dynamic.filters.settings'); ?></strong></h4>
-                                </div>
-                                <div class="col-md-6">
-                                    <?php echo $view['form']->row($form->vars['form']->children['settings']['showDynamicFilters']); ?>
-                                </div>
-                                <div class="col-md-6">
-                                    <?php echo $view['form']->row($form->vars['form']->children['settings']['hideDateRangeFilter']); ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade bdr-w-0<?php if (!$showGraphTab): echo 'hide'; endif; ?>" id="graphs-container">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="pa-md">
-                                    <?php echo $view['form']->row($form->vars['form']->children['settings']['showGraphsAboveTable']); ?>
-                                    <?php echo $view['form']->row($form['graphs']); ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
+                        <div class="tab-pane fade bdr-w-0" id="data-container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="pa-md">
+                                        <h4><strong><?php echo $view['translator']->trans(
+                                                    'mautic.report.report.form.columnselector'
+                                                ); ?></strong></h4>
+                                        <?php echo $view['form']->row($form['columns']); ?>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="ml-md">
+                                        <h4><strong><?php echo $view['translator']->trans(
+                                                    'mautic.core.order'
+                                                ); ?></strong></h4>
+                                        <?php echo $view['form']->row($form['tableOrder']); ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="pa-md">
+                                        <h4><strong><?php echo $view['translator']->trans(
+                                                    'mautic.core.filters'
+                                                ); ?></strong></h4>
+                                        <?php echo $view['form']->row($form['filters']); ?>
+                                    </div>
+                                    <div class="hide">
+                                        <div id="filterValueYesNoTemplate">
+                                            <?php echo $view['form']->widget($form['value_template_yesno']); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="pa-md">
+                                        <h4><strong><?php echo $view['translator']->trans(
+                                                    'mautic.report.form.groupby'
+                                                ); ?></strong></h4>
+                                        <?php echo $view['form']->row($form['groupBy']); ?>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="pa-md">
+                                        <h4><strong><?php echo $view['translator']->trans(
+                                                    'mautic.core.calculated.fields'
+                                                ); ?></strong></h4>
+                                        <?php echo $view['form']->row($form['aggregators']); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade bdr-w-0<?php if (!$showGraphTab): echo 'hide'; endif; ?>"
+                             id="graphs-container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="pa-md">
+                                        <?php echo $view['form']->row(
+                                            $form->vars['form']->children['settings']['showGraphsAboveTable']
+                                        ); ?>
+                                        <?php echo $view['form']->row($form['graphs']); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-3 bg-white height-auto bdr-l">
-        <div class="pr-lg pl-lg pt-md pb-md">
-            <?php echo $view['form']->row($form['isPublished']); ?>
-            <?php echo $view['form']->row($form['system']); ?>
-            <?php echo $view['form']->row($form['createdBy']); ?>
+        <div class="col-md-3 bg-white height-auto bdr-l">
+            <div class="pr-lg pl-lg pt-md pb-md">
+                <?php echo $view['form']->row($form['isPublished']); ?>
+                <?php echo $view['form']->row($form['system']); ?>
+                <?php echo $view['form']->row($form['createdBy']); ?>
+                <hr>
+                <h5><?php echo $view['translator']->trans(
+                        'mautic.report.report.form.display.dynamic.filters.settings'
+                    ); ?></h5>
+                <br>
+                <?php echo $view['form']->row($form->vars['form']->children['settings']['showDynamicFilters']); ?>
+                <?php echo $view['form']->row($form->vars['form']->children['settings']['hideDateRangeFilter']); ?>
+            </div>
         </div>
     </div>
-</div>
 <?php echo $view['form']->end($form); ?>
