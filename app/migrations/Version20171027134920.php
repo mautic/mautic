3f8bb144ab907c37c1ffbed380ae38ca63e320f9
Petr Fidler <fidler@freshservices.cz>
2017-11-07 12:35:15 +0100

    (#4794) Migration fix

--- app/migrations/Version20171027134920.php
+++ app/migrations/Version20171027134920.php
@@ -31,8 +31,11 @@ class Version20171027134920 extends AbstractMauticMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql("CREATE TABLE {$this->prefix}reports_schedulers (id INT AUTO_INCREMENT NOT NULL, report_id INT NOT NULL, schedule_date DATETIME NOT NULL COMMENT '(DC2Type:datetime)', INDEX IDX_C74CA6B84BD2A4C0 (report_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
-        $this->addSql("ALTER TABLE {$this->prefix}reports_schedulers ADD CONSTRAINT FK_C74CA6B84BD2A4C0 FOREIGN KEY (report_id) REFERENCES reports (id) ON DELETE CASCADE");
+        $idx = $this->generatePropertyName('report_schedulers', 'idx', ['report_id']);
+        $fk  = $this->generatePropertyName('report_schedulers', 'fk', ['report_id']);
+
+        $this->addSql("CREATE TABLE {$this->prefix}reports_schedulers (id INT AUTO_INCREMENT NOT NULL, report_id INT NOT NULL, schedule_date DATETIME NOT NULL COMMENT '(DC2Type:datetime)', INDEX $idx (report_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
+        $this->addSql("ALTER TABLE {$this->prefix}reports_schedulers ADD CONSTRAINT $fk FOREIGN KEY (report_id) REFERENCES {$this->prefix}reports (id) ON DELETE CASCADE");
     }
 
     /**
