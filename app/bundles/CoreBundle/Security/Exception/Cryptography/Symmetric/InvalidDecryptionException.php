ab72fb7398b6988345a2b1a180cd7312af1ca661
Jakub Hadamčík <jakub@hadamcik.cz>
2017-11-09 17:01:07 +0100

    Added missing Mautic codeblock

--- app/bundles/CoreBundle/Security/Exception/Cryptography/Symmetric/InvalidDecryptionException.php
+++ app/bundles/CoreBundle/Security/Exception/Cryptography/Symmetric/InvalidDecryptionException.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * @copyright   2017 Mautic Contributors. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 namespace Mautic\CoreBundle\Security\Exception\Cryptography\Symmetric;
 
 /**
