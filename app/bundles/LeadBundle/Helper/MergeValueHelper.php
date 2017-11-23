527719c40856d836cbbb39945303d189f3b938ef
Alan Hartless <alan@devkardia.com>
2017-11-23 09:56:26 -0600

    Prevent overwriting of values

---
+++ app/bundles/LeadBundle/Helper/MergeValueHelper.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * @copyright   2017 Mautic Contributors. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\LeadBundle\Helper;
+
+
+use Mautic\LeadBundle\Exception\ValueNotMergeable;
+
+class MergeValueHelper
+{
+    /**
+     * @param $newerValue
+     * @param $olderValue
+     *
+     * @return null
+     * @throws ValueNotMergeable
+     */
+    public static function getMergeValue($newerValue, $olderValue)
+    {
+        if ($newerValue === $olderValue) {
+            throw new ValueNotMergeable($newerValue, $olderValue);
+        }
+
+        if (self::isNotEmpty($newerValue)) {
+            return $newerValue;
+        }
+
+        if (self::isNotEmpty($olderValue)) {
+            return $olderValue;
+        }
+
+        throw new ValueNotMergeable($newerValue, $olderValue);
+    }
+
+    /**
+     * @param $value
+     *
+     * @return bool
+     */
+    public static function isNotEmpty($value)
+    {
+        return (null !== $value && '' !== $value);
+    }
+
+}
--- app/bundles/LeadBundle/Model/MergeModel.php
+++ app/bundles/LeadBundle/Model/MergeModel.php
@@ -15,6 +15,10 @@ use Mautic\LeadBundle\Entity\Lead;
 use Mautic\LeadBundle\Entity\MergeRecord;
 use Mautic\LeadBundle\Entity\MergeRecordRepository;
 use Mautic\LeadBundle\Event\LeadMergeEvent;
+use Mautic\LeadBundle\Exception\MissingMergeSubjectException;
+use Mautic\LeadBundle\Exception\SameContactException;
+use Mautic\LeadBundle\Exception\ValueNotMergeable;
+use Mautic\LeadBundle\Helper\MergeValueHelper;
 use Mautic\LeadBundle\LeadEvents;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -67,31 +71,27 @@ class MergeModel
     }
 
     /**
-     * @param Lead $lead
-     * @param Lead $lead2
+     * @param Lead $winner
      *
-     * @return Lead
+     * @return MergeModel
      */
-    public function mergeOldIntoNew(Lead $lead, Lead $lead2)
+    public function setWinner(Lead $winner)
     {
-        $winner = ($lead->getDateAdded() > $lead2->getDateAdded()) ? $lead : $lead2;
-        $loser  = ($winner->getId() === $lead->getId()) ? $lead2 : $lead;
+        $this->winner = $winner;
 
-        return $this->merge($loser, $winner);
+        return $this;
     }
 
     /**
-     * @param Lead $lead
-     * @param Lead $lead2
+     * @param Lead $loser
      *
-     * @return Lead
+     * @return MergeModel
      */
-    public function mergeNewIntoOld(Lead $lead, Lead $lead2)
+    public function setLoser(Lead $loser)
     {
-        $winner = ($lead->getDateAdded() < $lead2->getDateAdded()) ? $lead : $lead2;
-        $loser  = ($winner->getId() === $lead->getId()) ? $lead2 : $lead;
+        $this->loser = $loser;
 
-        return $this->merge($loser, $winner);
+        return $this;
     }
 
     /**
@@ -99,51 +99,62 @@ class MergeModel
      * @param Lead $winner
      *
      * @return Lead
+     * @throws MissingMergeSubjectException
      */
-    public function merge(Lead $loser, Lead $winner)
+    public function merge()
     {
-        $this->loser  = $loser;
-        $this->winner = $winner;
-
-        //if they are the same lead, then just return one
-        if ($loser === $winner) {
+        try {
+            $this->checkIfMergeable();
+        } catch (SameContactException $exception) {
             $this->logger->debug('CONTACT: Contacts are the same');
 
-            return $loser;
+            return $this->winner;
         }
 
-        $this->logger->debug('CONTACT: ID# '.$loser->getId().' will be merged into ID# '.$winner->getId());
+        $this->logger->debug('CONTACT: ID# '.$this->loser->getId().' will be merged into ID# '.$this->winner->getId());
 
         // Dispatch pre merge event
-        $event = new LeadMergeEvent($winner, $loser);
+        $event = new LeadMergeEvent($this->winner, $this->loser);
         $this->dispatcher->dispatch(LeadEvents::LEAD_PRE_MERGE, $event);
 
         // Merge everything
-        $this->updateMergeRecords();
-        $this->mergeTimestamps();
-        $this->mergeIpAddressHistory();
-        $this->mergeFieldData();
-        $this->mergeOwners();
-        $this->mergePoints();
-        $this->mergeTags();
+        try {
+            $this->updateMergeRecords()
+                ->mergeTimestamps()
+                ->mergeIpAddressHistory()
+                ->mergeFieldData()
+                ->mergeOwners()
+                ->mergePoints()
+                ->mergeTags();
+        } catch (SameContactException $exception) {
+            // Already handled; this is to just to make IDE happy
+        } catch (MissingMergeSubjectException $exception) {
+            // Already handled; this is to just to make IDE happy
+        }
 
         // Save the updated contact
-        $this->leadModel->saveEntity($winner, false);
+        $this->leadModel->saveEntity($this->winner, false);
 
         // Dispatch post merge event
         $this->dispatcher->dispatch(LeadEvents::LEAD_POST_MERGE, $event);
 
         // Delete the loser
-        $this->leadModel->deleteEntity($loser);
+        $this->leadModel->deleteEntity($this->loser);
 
-        return $winner;
+        return $this->winner;
     }
 
     /**
      * Merge timestamps.
+     *
+     * @return $this
+     * @throws SameContactException
+     * @throws MissingMergeSubjectException
      */
-    protected function mergeTimestamps()
+    public function mergeTimestamps()
     {
+        $this->checkIfMergeable();
+
         // The winner should keep the most recent last active timestamp of the two
         if ($this->loser->getLastActive() > $this->winner->getLastActive()) {
             $this->winner->setLastActive($this->loser->getLastActive());
@@ -153,13 +164,145 @@ class MergeModel
         if ($this->loser->getDateIdentified() < $this->winner->getDateIdentified()) {
             $this->winner->setDateIdentified($this->loser->getDateIdentified());
         }
+
+        return $this;
+    }
+
+    /**
+     * Merge IP history into the winner.
+     *
+     * @return $this
+     * @throws SameContactException
+     * @throws MissingMergeSubjectException
+     */
+    public function mergeIpAddressHistory()
+    {
+        $this->checkIfMergeable();
+
+        $ipAddresses = $this->loser->getIpAddresses();
+        foreach ($ipAddresses as $ip) {
+            $this->winner->addIpAddress($ip);
+
+            $this->logger->debug('CONTACT: Associating '.$this->winner->getId().' with IP '.$ip->getIpAddress());
+        }
+
+        return $this;
+    }
+
+    /**
+     * Merge custom field data into winner.
+     *
+     * @return $this
+     * @throws SameContactException
+     * @throws MissingMergeSubjectException
+     */
+    public function mergeFieldData()
+    {
+        $this->checkIfMergeable();
+
+        // Use the modified date if applicable or date added if the contact has never been edited
+        $loserDate  = ($this->loser->getDateModified()) ? $this->loser->getDateModified() : $this->loser->getDateAdded();
+        $winnerDate = ($this->winner->getDateModified()) ? $this->winner->getDateModified() : $this->winner->getDateAdded();
+
+        // When it comes to data, keep the newest value regardless of the winner/loser
+        $newest = ($loserDate > $winnerDate) ? $this->loser : $this->winner;
+        $oldest = ($newest->getId() === $this->winner->getId()) ? $this->loser : $this->winner;
+
+        $newestFields = $newest->getProfileFields();
+        $oldestFields = $oldest->getProfileFields();
+
+        foreach (array_keys($newestFields) as $field) {
+            if (in_array($field, ['id', 'points'])) {
+                // Let mergePoints() take care of this
+                continue;
+            }
+
+            try {
+                $newValue = MergeValueHelper::getMergeValue($newestFields[$field], $oldestFields[$field]);
+                $this->winner->addUpdatedField($field, $newValue);
+
+                $fromValue = empty($winnerFields[$field]) ? 'empty' : $winnerFields[$field];
+                $this->logger->debug("CONTACT: Updated $field from $fromValue to $newValue for {$this->winner->getId()}");
+            } catch (ValueNotMergeable $exception) {
+                $this->logger->info("CONTACT: $field is not mergeable for {$this->winner->getId()} - ".$exception->getMessage());
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Merge owners if the winner isn't already assigned an owner.
+     *
+     * @return $this
+     * @throws SameContactException
+     * @throws MissingMergeSubjectException
+     */
+    public function mergeOwners()
+    {
+        $this->checkIfMergeable();
+
+        $oldOwner = $this->winner->getOwner();
+        $newOwner = $this->loser->getOwner();
+
+        if ($oldOwner === null && $newOwner !== null) {
+            $this->winner->setOwner($newOwner);
+
+            $this->logger->debug("CONTACT: New owner of {$this->winner->getId()} is {$newOwner->getId()}");
+        }
+
+        return $this;
+    }
+
+    /**
+     * Sum points from both contacts.
+     *
+     * @return $this
+     * @throws SameContactException
+     * @throws MissingMergeSubjectException
+     */
+    public function mergePoints()
+    {
+        $this->checkIfMergeable();
+
+        $winnerPoints = (int) $this->winner->getPoints();
+        $loserPoints  = (int) $this->loser->getPoints();
+        $this->winner->setPoints($winnerPoints + $loserPoints);
+        $this->logger->debug("CONTACT: Adding {$loserPoints} points to {$this->winner->getId()}");
+
+        return $this;
+    }
+
+    /**
+     * Merge tags from loser into winner.
+     *
+     * @return $this
+     * @throws SameContactException
+     * @throws MissingMergeSubjectException
+     */
+    public function mergeTags()
+    {
+        $this->checkIfMergeable();
+
+        $loserTags = $this->loser->getTags();
+        $addTags   = $loserTags->getKeys();
+
+        $this->leadModel->modifyTags($this->winner, $addTags, null, false);
+
+        return $this;
     }
 
     /**
      * Merge past merge records into the winner.
+     *
+     * @return $this
+     * @throws SameContactException
+     * @throws MissingMergeSubjectException
      */
-    protected function updateMergeRecords()
+    private function updateMergeRecords()
     {
+        $this->checkIfMergeable();
+
         // Update merge records for the lead about to be deleted
         $this->repo->moveMergeRecord($this->loser->getId(), $this->winner->getId());
 
@@ -172,106 +315,22 @@ class MergeModel
 
         $this->repo->saveEntity($mergeRecord);
         $this->repo->clear();
+
+        return $this;
     }
 
     /**
-     * Merge IP history into the winner.
+     * @throws SameContactException
+     * @throws MissingMergeSubjectException
      */
-    protected function mergeIpAddressHistory()
+    private function checkIfMergeable()
     {
-        $ipAddresses = $this->loser->getIpAddresses();
-        foreach ($ipAddresses as $ip) {
-            $this->winner->addIpAddress($ip);
-
-            $this->logger->debug('CONTACT: Associating '.$this->winner->getId().' with IP '.$ip->getIpAddress());
+        if (!$this->winner || !$this->loser) {
+            throw new MissingMergeSubjectException();
         }
-    }
 
-    /**
-     * Merge custom field data into winner.
-     */
-    protected function mergeFieldData()
-    {
-        // Use the modified date if applicable or date added if the contact has never been edited
-        $loserDate  = ($this->loser->getDateModified()) ? $this->loser->getDateModified() : $this->loser->getDateAdded();
-        $winnerDate = ($this->winner->getDateModified()) ? $this->winner->getDateModified() : $this->winner->getDateAdded();
-
-        // When it comes to data, keep the newest value regardless of the winner/loser
-        $newest = ($loserDate > $winnerDate) ? $this->loser : $this->winner;
-        $oldest = ($newest->getId() === $this->winner->getId()) ? $this->loser : $this->winner;
-
-        $newestFields = $newest->getProfileFields();
-        $oldestFields = $oldest->getProfileFields();
-
-        $winnerFields = $this->winner->getProfileFields();
-        foreach (array_keys($winnerFields) as $field) {
-            if (in_array($field, ['id', 'points'])) {
-                // Let mergePoints() take care of this
-                continue;
-            }
-
-            // Don't overwrite with an empty value (error on the side of not losing any data)
-            if ($this->valueIsEmpty($winnerFields[$field])) {
-                // Give precedence to the newest value
-                $newValue = (!$this->valueIsEmpty($newestFields[$field])) ? $newestFields[$field] : $oldestFields[$field];
-            } elseif (!$this->valueIsEmpty($newestFields[$field]) && $winnerFields[$field] !== $newestFields[$field]) {
-                $newValue = $newestFields[$field];
-            }
-
-            if (isset($newValue)) {
-                $this->winner->addUpdatedField($field, $newValue);
-                $fromValue = empty($winnerFields[$field]) ? 'empty' : $winnerFields[$field];
-                $this->logger->debug("CONTACT: Updated $field from $fromValue to $newValue for {$this->winner->getId()}");
-            }
+        if ($this->winner->getId() === $this->loser->getId()) {
+            throw new SameContactException();
         }
-    }
-
-    /**
-     * Merge owners if the winner isn't already assigned an owner.
-     */
-    protected function mergeOwners()
-    {
-        $oldOwner = $this->winner->getOwner();
-        $newOwner = $this->loser->getOwner();
-
-        if ($oldOwner === null && $newOwner !== null) {
-            $this->winner->setOwner($newOwner);
-
-            $this->logger->debug("CONTACT: New owner of {$this->winner->getId()} is {$newOwner->getId()}");
-        }
-    }
-
-    /**
-     * Sum points from both contacts.
-     */
-    protected function mergePoints()
-    {
-        $winnerPoints = (int) $this->winner->getPoints();
-        $loserPoints  = (int) $this->loser->getPoints();
-        $this->winner->setPoints($winnerPoints + $loserPoints);
-        $this->logger->debug("CONTACT: Adding {$loserPoints} points to {$this->winner->getId()}");
-    }
-
-    /**
-     * Merge tags from loser into winner.
-     */
-    protected function mergeTags()
-    {
-        $loserTags = $this->loser->getTags();
-        $addTags   = $loserTags->getKeys();
-
-        $this->leadModel->modifyTags($this->winner, $addTags, null, false);
-    }
-
-    /**
-     * Check if value is empty but don't include false or 0.
-     *
-     * @param $value
-     *
-     * @return bool
-     */
-    protected function valueIsEmpty($value)
-    {
-        return null === $value || '' === $value;
     }
 }
