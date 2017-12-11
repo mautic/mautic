f09b7c39d903bccdc9c2672cc1cb7fbe3e5d9a7b
Zdeno Kuzmany <zdeno@kuzmany.biz>
2017-12-11 09:18:15 +0100

    Minor

--- plugins/MauticFocusBundle/EventListener/StatSubscriber.php
+++ plugins/MauticFocusBundle/EventListener/StatSubscriber.php
@@ -11,7 +11,6 @@
 
 namespace MauticPlugin\MauticFocusBundle\EventListener;
 
-use Aws\S3\Enum\Event;
 use Mautic\CoreBundle\EventListener\CommonSubscriber;
 use Mautic\FormBundle\Event\SubmissionEvent;
 use Mautic\FormBundle\FormEvents;
