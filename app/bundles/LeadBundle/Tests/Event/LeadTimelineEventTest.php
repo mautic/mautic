6a45cc6a3b93fd3e50c538367ae40f1321626c44
Alan Hartless <alan@devkardia.com>
2017-12-13 08:29:54 -0600

    Fixed namespace

--- app/bundles/LeadBundle/Tests/Event/LeadTimelineEventTest.php
+++ app/bundles/LeadBundle/Tests/Event/LeadTimelineEventTest.php
@@ -9,7 +9,7 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\LeadBundle\Event;
+namespace Mautic\LeadBundle\Tests\Event;
 
 class LeadTimelineEventTest extends \PHPUnit_Framework_TestCase
 {
