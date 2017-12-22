c1e282114e27b3ed2f4a8f85887b765067b947da
Alan Hartless <alan@devkardia.com>
2017-12-22 09:53:25 -0600

    Fixed failed tests in staging

--- app/bundles/CoreBundle/Tests/functional/Entity/CommonRepositoryTest.php
+++ app/bundles/CoreBundle/Tests/functional/Entity/CommonRepositoryTest.php
@@ -9,7 +9,7 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\CoreBundle\Tests\Entity;
+namespace Mautic\CoreBundle\Tests\functional\Entity;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 
