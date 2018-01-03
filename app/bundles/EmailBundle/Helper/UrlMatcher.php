d867f8b835910fc1ea9065b7c0ac6dfbf4690954
Alan Hartless <alan@devkardia.com>
2018-01-03 14:43:39 -0600

    Add support for more URL matching

--- app/bundles/EmailBundle/Helper/UrlMatcher.php
+++ app/bundles/EmailBundle/Helper/UrlMatcher.php
@@ -21,13 +21,40 @@ class UrlMatcher
      */
     public static function hasMatch(array $urlsToCheckAgainst, $urlToFind)
     {
+        $urlToFind = self::sanitizeUrl($urlToFind);
+
         foreach ($urlsToCheckAgainst as $url) {
-            $url = str_replace('\\/', '/', $url);
+            $url = self::sanitizeUrl($url);
+
             if (preg_match('/'.preg_quote($url, '/').'/i', $urlToFind)) {
                 return true;
             }
         }
 
         return false;
+    }
+
+    /**
+     * @param $url
+     *
+     * @return mixed|string
+     */
+    private static function sanitizeUrl($url)
+    {
+        // Handle escaped forward slashes as BC
+        $url = str_replace('\\/', '/', $url);
+
+        // Ignore ending slash
+        $url = rtrim($url, '/');
+
+        // Ignore http/https
+        $url = str_replace(['http://', 'https://'], '', $url);
+
+        // Remove preceding //
+        if (strpos($url, '//') === 0) {
+            $url = str_replace('//', '', $url);
+        }
+
+        return $url;
     }
 }
