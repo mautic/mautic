6a1775c732efa545092b56fd8c03d715a1c1cce8
Alan Hartless <alan@devkardia.com>
2018-01-03 14:43:39 -0600

    Add support for more URL matching

--- app/bundles/EmailBundle/Tests/Helper/UrlMatcherTest.php
+++ app/bundles/EmailBundle/Tests/Helper/UrlMatcherTest.php
@@ -41,4 +41,61 @@ class UrlMatcherTest extends \PHPUnit_Framework_TestCase
 
         $this->assertTrue(UrlMatcher::hasMatch($urls, 'https://google.com/hello'));
     }
+
+    public function testUrlWithEndingSlash()
+    {
+        $urls = [
+            'https://google.com/hello/',
+        ];
+
+        $this->assertTrue(UrlMatcher::hasMatch($urls, 'https://google.com/hello'));
+        $this->assertTrue(UrlMatcher::hasMatch($urls, 'https://google.com/hello/'));
+    }
+
+    public function testUrlWithoutHttpPrefix()
+    {
+        $urls = [
+            'google.com/hello',
+        ];
+
+        $this->assertTrue(UrlMatcher::hasMatch($urls, 'https://google.com/hello'));
+        $this->assertTrue(UrlMatcher::hasMatch($urls, 'http://google.com/hello/'));
+    }
+
+    public function testUrlWithoutHttp()
+    {
+        $urls = [
+            '//google.com/hello',
+        ];
+
+        $this->assertTrue(UrlMatcher::hasMatch($urls, 'https://google.com/hello'));
+        $this->assertTrue(UrlMatcher::hasMatch($urls, '//google.com/hello'));
+    }
+
+    public function testUrlMismatch()
+    {
+        $urls = [
+            'http://google.com',
+        ];
+
+        $this->assertFalse(UrlMatcher::hasMatch($urls, 'https://yahoo.com'));
+    }
+
+    public function testFTPSchemeMisMatch()
+    {
+        $urls = [
+            'ftp://google.com',
+        ];
+
+        $this->assertFalse(UrlMatcher::hasMatch($urls, 'https://google.com'));
+    }
+
+    public function testFTPSchemeMatch()
+    {
+        $urls = [
+            'ftp://google.com',
+        ];
+
+        $this->assertTrue(UrlMatcher::hasMatch($urls, 'ftp://google.com'));
+    }
 }
