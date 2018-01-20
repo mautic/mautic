f670bc05d8ea0daad1da4c1cb7d3bab6d1e32aa9
Brad Stancel <brad@processfast.com>
2018-01-20 13:41:50 -0500

    Fix for issue #5103 - Company/Accounts being duplicated on CRM Sync of edited Company/Account

--- app/bundles/LeadBundle/Helper/IdentifyCompanyHelper.php
+++ app/bundles/LeadBundle/Helper/IdentifyCompanyHelper.php
@@ -31,7 +31,7 @@ class IdentifyCompanyHelper
         list($company, $companyEntities) = self::findCompany($parameters, $companyModel);
         if (!empty($company)) {
             $leadAdded = false;
-            if (count($companyEntities)) {
+            if (!empty($companyEntities)) {
                 foreach ($companyEntities as $entity) {
                     $companyEntity   = $entity;
                     $companyLeadRepo = $companyModel->getCompanyLeadRepository();
