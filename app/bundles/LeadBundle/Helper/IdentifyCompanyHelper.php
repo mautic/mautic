f2e1847a0b0f09e0158ca10da7d4fbae618a1d46
Brad Stancel <brad@processfast.com>
2018-01-20 13:41:50 -0500

    Fix for issue #5103 - Company/Accounts being duplicated on CRM Sync of edited Company/Account

--- app/bundles/LeadBundle/Helper/IdentifyCompanyHelper.php
+++ app/bundles/LeadBundle/Helper/IdentifyCompanyHelper.php
@@ -75,9 +75,12 @@ class IdentifyCompanyHelper
             $companyName = filter_var($parameters['company']);
         } elseif (isset($parameters['companyname'])) {
             $companyName = filter_var($parameters['companyname']);
-        } elseif (isset($parameters['email']) || isset($parameters['companyemail'])) {
-            $companyName = isset($parameters['email']) ? self::domainExists($parameters['email']) : self::domainExists($parameters['companyemail']);
         }
+        
+        if (isset($parameters['email']) || isset($parameters['companyemail'])) {
+            $companyDomain = isset($parameters['email']) ? self::domainExists($parameters['email']) : self::domainExists($parameters['companyemail']);
+        } 
+        
         if (empty($parameters['companywebsite']) && !empty($parameters['companyemail'])) {
             $companyDomain = self::domainExists($parameters['companyemail']);
         }
