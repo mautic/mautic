9ae6157a3cb4049c0c870fde5f0ca1ab39998279
Brad Stancel <brad@processfast.com>
2018-01-27 19:53:15 -0500

    Fix for issue #5103 - correcting syntax/spaces for passing Travis CI PHP 7.1 tests

--- app/bundles/LeadBundle/Helper/IdentifyCompanyHelper.php
+++ app/bundles/LeadBundle/Helper/IdentifyCompanyHelper.php
@@ -82,7 +82,7 @@ class IdentifyCompanyHelper
 
         if (isset($parameters['email']) || isset($parameters['companyemail'])) {
             $companyDomain = isset($parameters['email']) ? self::domainExists($parameters['email']) : self::domainExists($parameters['companyemail']);
-        } 
+        }
 
         if (empty($parameters['companywebsite']) && !empty($parameters['companyemail'])) {
             $companyDomain = self::domainExists($parameters['companyemail']);
