177d40f77926bf3433193acd4dfaf47d40141b21
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-02-01 22:01:47 +0100

    Remove Robots Txt support and revert back robots.txt. We'e using X-Robots-Tag header

--- app/bundles/AssetBundle/Views/Asset/form.html.php
+++ app/bundles/AssetBundle/Views/Asset/form.html.php
@@ -38,7 +38,7 @@ $view['slots']->set('mauticContent', 'asset');
 
 			        <div class="col-md-5 text-left mt-lg<?php if ($startOnLocal) {
     echo ' hide';
-} ?>" id="remote-button">		<?php echo $view['form']->row($form['storageLocation']); ?>
+} ?>" id="remote-button">
 						<?php if ($integrations) : ?>
 							<a data-toggle="ajaxmodal" data-target="#RemoteFileModal" data-header="<?php echo $view['translator']->trans('mautic.asset.remote.file.browse'); ?>" href="<?php echo $view['router']->path('mautic_asset_remote'); ?>?tmpl=modal" class="btn btn-primary">
 								<?php echo $view['translator']->trans('mautic.asset.remote.file.browse'); ?>
