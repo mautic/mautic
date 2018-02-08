f7e87418371a4838e150562da98e33c1073680b1
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-02-08 18:13:44 +0100

    Remove buttons

--- app/bundles/LeadBundle/Views/Form/list.html.php
+++ app/bundles/LeadBundle/Views/Form/list.html.php
@@ -17,11 +17,6 @@
         ?>
         <div class="tab-pane fade bdr-w-0  <?php if ($key == 0): ?> active in<?php endif; ?>" id="form-<?php echo $leadForm['entity']->getAlias()?>">
             <?php echo $leadForm['content']; ?>
-            <p class="text-right">
-                <a class="btn btn-primary btn-sm" href="<?php echo $view['router']->generate('mautic_form_results', ['objectAction' => 'index', 'objectId' => $leadForm['entity']->getId()]); ?>" data-toggle="ajax"><?php echo $view['translator']->trans('mautic.form.form.contacttab.show.results'); ?></a>&nbsp;
-                &nbsp;<a class="btn btn-primary btn-sm" href="<?php echo $view['router']->generate('mautic_form_action', ['objectAction' => 'view', 'objectId' => $leadForm['entity']->getId()]); ?>" data-toggle="ajax"><?php echo $view['translator']->trans('mautic.form.form.contacttab.show.form'); ?></a>
-            </p>
-
         </div>
     <?php endforeach; ?>
 
