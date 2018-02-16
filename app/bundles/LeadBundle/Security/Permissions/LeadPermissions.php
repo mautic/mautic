f36d5ece30dfc10cf70b8cacd1d50e48af952b3d
filipelbc <filipelbc@brickabode.com>
2018-02-16 15:42:48 -0200

    Feature: add 'form submission' and 'contact' exportation capability
    This enables one to allow or disallow some roles to export contacts and
    form submission results.

--- app/bundles/LeadBundle/Security/Permissions/LeadPermissions.php
+++ app/bundles/LeadBundle/Security/Permissions/LeadPermissions.php
@@ -24,6 +24,11 @@ class LeadPermissions extends AbstractPermissions
         parent::__construct($params);
 
         $this->permissions = [
+            'batch' => [
+                'import' => 2,
+                'export' => 4,
+                'full'=> 1024,
+            ],
             'lists' => [
                 'viewother'   => 2,
                 'editother'   => 8,
@@ -70,6 +75,17 @@ class LeadPermissions extends AbstractPermissions
             'data'   => (!empty($data['lists']) ? $data['lists'] : []),
             'bundle' => 'lead',
             'level'  => 'lists',
+        ]);
+
+        $builder->add('lead:batch', 'permissionlist', [
+            'choices' => [
+                'export'   => 'mautic.core.permissions.export',
+                'import'   => 'mautic.core.permissions.import',
+            ],
+            'label'  => 'mautic.lead.permissions.batch',
+            'data'   => (!empty($data['batch']) ? $data['batch'] : []),
+            'bundle' => 'lead',
+            'level'  => 'batch',
         ]);
 
         $builder->add('lead:fields', 'permissionlist', [
