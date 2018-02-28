d03061a4e19459b1bd66ead4475ecb1362a37b46
Petr Fidler <info@maxell-cz.cz>
2018-02-28 15:36:26 +0100

    Simplify calling date tests, add filter to segment to be sure only date segments will apply, add test for today

--- app/bundles/LeadBundle/Tests/Segment/Decorator/Date/RelativeDateFunctionalTest.php
+++ app/bundles/LeadBundle/Tests/Segment/Decorator/Date/RelativeDateFunctionalTest.php
@@ -2,6 +2,7 @@
 
 namespace Mautic\LeadBundle\Tests\Segment\Decorator\Date;
 
+use Mautic\CoreBundle\Helper\InputHelper;
 use Mautic\CoreBundle\Test\MauticWebTestCase;
 use Mautic\LeadBundle\Entity\Lead;
 use Mautic\LeadBundle\Entity\LeadRepository;
@@ -12,47 +13,56 @@ use Mautic\LeadBundle\Segment\ContactSegmentService;
  */
 class RelativeDateFunctionalTest extends MauticWebTestCase
 {
+    public function testSegmentCountIsCorrectForToday()
+    {
+        $name = 'Today';
+        $lead = $this->createLead($name, 'midnight today', '+10 seconds');
+
+        $this->checkSegmentResult($name, $lead);
+    }
+
     public function testSegmentCountIsCorrectForTomorrow()
     {
+        $name = 'Tomorrow';
         $lead = $this->createLead('Tomorrow', 'midnight tomorrow', '+10 seconds');
 
-        /** @var ContactSegmentService $contactSegmentService */
-        $contactSegmentService = $this->container->get('mautic.lead.model.lead_segment_service');
-
-        $segmentTomorrowRef = $this->fixtures->getReference('segment-with-relative-date-tomorrow');
-        $segmentContacts    = $contactSegmentService->getTotalLeadListLeadsCount($segmentTomorrowRef);
-
-        $this->assertEquals(
-            1,
-            $segmentContacts[$segmentTomorrowRef->getId()]['count'],
-            'There should be 1 contacts in the segment-with-relative-date-tomorrow segment.'
-        );
-        $this->assertEquals(
-            $lead->getId(),
-            $segmentContacts[$segmentTomorrowRef->getId()]['maxId'],
-            'MaxId in the segment-with-relative-date-tomorrow segment should be ID of Lead.'
-        );
+        $this->checkSegmentResult($name, $lead);
     }
 
     public function testSegmentCountIsCorrectForLastWeek()
     {
+        $name = 'Last week';
         $lead = $this->createLead('Last week', 'midnight monday last week', '+2 days');
 
+        $this->checkSegmentResult($name, $lead);
+    }
+
+    /**
+     * @param string $name
+     * @param Lead   $lead
+     */
+    private function checkSegmentResult($name, Lead $lead)
+    {
         /** @var ContactSegmentService $contactSegmentService */
         $contactSegmentService = $this->container->get('mautic.lead.model.lead_segment_service');
 
-        $segmentLastWeekRef = $this->fixtures->getReference('segment-with-relative-date-last-week');
-        $segmentContacts    = $contactSegmentService->getTotalLeadListLeadsCount($segmentLastWeekRef);
+        $alias = strtolower(InputHelper::alphanum($name, false, '-'));
+
+        $segmentName     = 'segment-with-relative-date-'.$alias;
+        $segmentRef      = $this->fixtures->getReference($segmentName);
+        $segmentContacts = $contactSegmentService->getTotalLeadListLeadsCount($segmentRef);
+
+        $this->removeAllDateRelatedLeads(); //call before assert to be sure cleaning will process
 
         $this->assertEquals(
             1,
-            $segmentContacts[$segmentLastWeekRef->getId()]['count'],
-            'There should be 1 contacts in the segment-with-relative-date-last-week segment.'
+            $segmentContacts[$segmentRef->getId()]['count'],
+            'There should be 1 contacts in the '.$segmentName.' segment.'
         );
         $this->assertEquals(
             $lead->getId(),
-            $segmentContacts[$segmentLastWeekRef->getId()]['maxId'],
-            'MaxId in the segment-with-relative-date-last-week segment should be ID of Lead.'
+            $segmentContacts[$segmentRef->getId()]['maxId'],
+            'MaxId in the '.$segmentName.' segment should be ID of Lead.'
         );
     }
 
@@ -65,8 +75,7 @@ class RelativeDateFunctionalTest extends MauticWebTestCase
      */
     private function createLead($name, $initialTime, $dateModifier)
     {
-        // Remove the title from all contacts, rebuild the list, and check that list is updated
-        $this->em->getConnection()->query(sprintf("DELETE FROM %sleads WHERE lastname = 'Date';", MAUTIC_TABLE_PREFIX));
+        $this->removeAllDateRelatedLeads();
 
         /** @var LeadRepository $leadRepository */
         $leadRepository = $this->container->get('doctrine.orm.default_entity_manager')->getRepository(Lead::class);
@@ -82,5 +91,11 @@ class RelativeDateFunctionalTest extends MauticWebTestCase
         $leadRepository->saveEntity($lead);
 
         return $lead;
+    }
+
+    private function removeAllDateRelatedLeads()
+    {
+        // Remove all date related leads to not affect other test
+        $this->em->getConnection()->query(sprintf("DELETE FROM %sleads WHERE lastname = 'Date';", MAUTIC_TABLE_PREFIX));
     }
 }
