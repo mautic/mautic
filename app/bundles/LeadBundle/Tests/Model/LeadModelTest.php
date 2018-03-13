8fa296f32bf11cf6e80a8c12526a6033e1f97ea8
John Linhart <admin@escope.cz>
2018-03-13 17:14:43 +0100

    Tests for the LeadEventLog bug fix

--- app/bundles/LeadBundle/Tests/Model/LeadModelTest.php
+++ app/bundles/LeadBundle/Tests/Model/LeadModelTest.php
@@ -2,6 +2,9 @@
 
 namespace Mautic\LeadBundle\Tests\Model;
 
+use Mautic\LeadBundle\Entity\Lead;
+use Mautic\LeadBundle\Entity\LeadEventLog;
+use Mautic\LeadBundle\Model\CompanyModel;
 use Mautic\LeadBundle\Model\FieldModel;
 use Mautic\LeadBundle\Model\LeadModel;
 
@@ -37,10 +40,7 @@ class LeadModelTest extends \PHPUnit_Framework_TestCase
             ->setMethods(null)
             ->getMock();
 
-        $reflectedProp = new \ReflectionProperty(LeadModel::class, 'leadFieldModel');
-
-        $reflectedProp->setAccessible(true);
-        $reflectedProp->setValue($mockLeadModel, $mockFieldModel);
+        $this->setProperty($mockLeadModel, LeadModel::class, 'leadFieldModel', $mockFieldModel);
 
         $this->assertAttributeEquals(
             [],
@@ -52,11 +52,86 @@ class LeadModelTest extends \PHPUnit_Framework_TestCase
         $mockLeadModel->checkForDuplicateContact([]);
         $this->assertAttributeEquals(['a' => 1], 'availableLeadFields', $mockLeadModel);
 
-        $reflectedProp = new \ReflectionProperty(LeadModel::class, 'availableLeadFields');
-        $reflectedProp->setAccessible(true);
-        $reflectedProp->setValue($mockLeadModel, []);
+        $this->setProperty($mockLeadModel, LeadModel::class, 'availableLeadFields', []);
 
         $mockLeadModel->checkForDuplicateContact([], null, false, true);
         $this->assertAttributeEquals(['a' => 3], 'availableLeadFields', $mockLeadModel);
+    }
+
+    /**
+     * Test that the Lead won't be set to the LeadEventLog if the Lead save fails.
+     */
+    public function testImportWillNotSetLeadToLeadEventLogWhenLeadSaveFails()
+    {
+        $leadEventLog  = new LeadEventLog();
+        $mockLeadModel = $this->getMockBuilder(LeadModel::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['saveEntity', 'checkForDuplicateContact'])
+            ->getMock();
+
+        $mockCompanyModel = $this->getMockBuilder(CompanyModel::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['extractCompanyDataFromImport'])
+            ->getMock();
+
+        $mockCompanyModel->expects($this->once())->method('extractCompanyDataFromImport')->willReturn([[], []]);
+
+        $this->setProperty($mockLeadModel, LeadModel::class, 'companyModel', $mockCompanyModel);
+        $this->setProperty($mockLeadModel, LeadModel::class, 'leadFields', [['alias' => 'email', 'type' => 'email', 'defaultValue' => '']]);
+
+        $mockLeadModel->expects($this->once())->method('saveEntity')->willThrowException(new \Exception());
+        $mockLeadModel->expects($this->once())->method('checkForDuplicateContact')->willReturn(new Lead());
+
+        try {
+            $mockLeadModel->import([], [], null, null, null, true, $leadEventLog);
+        } catch (\Exception $e) {
+            $this->assertNull($leadEventLog->getLead());
+        }
+    }
+
+    /**
+     * Test that the Lead won't be set to the LeadEventLog if the Lead save fails.
+     */
+    public function testImportWillSetLeadToLeadEventLogWhenLeadSaveSucceed()
+    {
+        $leadEventLog  = new LeadEventLog();
+        $lead          = new Lead();
+        $mockLeadModel = $this->getMockBuilder(LeadModel::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['saveEntity', 'checkForDuplicateContact'])
+            ->getMock();
+
+        $mockCompanyModel = $this->getMockBuilder(CompanyModel::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['extractCompanyDataFromImport'])
+            ->getMock();
+
+        $mockCompanyModel->expects($this->once())->method('extractCompanyDataFromImport')->willReturn([[], []]);
+
+        $this->setProperty($mockLeadModel, LeadModel::class, 'companyModel', $mockCompanyModel);
+        $this->setProperty($mockLeadModel, LeadModel::class, 'leadFields', [['alias' => 'email', 'type' => 'email', 'defaultValue' => '']]);
+
+        $mockLeadModel->expects($this->once())->method('checkForDuplicateContact')->willReturn($lead);
+
+        try {
+            $mockLeadModel->import([], [], null, null, null, true, $leadEventLog);
+        } catch (\Exception $e) {
+            $this->assertEquals($lead, $leadEventLog->getLead());
+        }
+    }
+
+    /**
+     * Set protected property to an object.
+     *
+     * @param object $object
+     * @param string $class
+     * @param string $property
+     * @param mixed  $value
+     */
+    private function setProperty($object, $class, $property, $value)
+    {
+        $reflectedProp = new \ReflectionProperty($class, $property);
+        $reflectedProp->setAccessible(true);
+        $reflectedProp->setValue($object, $value);
     }
 }
