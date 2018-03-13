865efc0287b38b3aa76878fbcba6e6a1c64242a4
Petr Fidler <info@maxell-cz.cz>
2018-03-13 16:57:02 +0100

    Tests for baseDecorator

---
+++ app/bundles/LeadBundle/Tests/Segment/Decorator/BaseDecoratorTest.php
@@ -0,0 +1,209 @@
+<?php
+
+/*
+ * @copyright   2017 Mautic Contributors. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\LeadBundle\Tests\Segment\Decorator;
+
+use Mautic\LeadBundle\Segment\ContactSegmentFilterCrate;
+use Mautic\LeadBundle\Segment\ContactSegmentFilterOperator;
+use Mautic\LeadBundle\Segment\Decorator\BaseDecorator;
+
+class BaseDecoratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        defined('MAUTIC_TABLE_PREFIX') or define('MAUTIC_TABLE_PREFIX', '');
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getField
+     */
+    public function testGetField()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+        $baseDecorator                = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([
+            'glue'     => 'and',
+            'field'    => 'date_identified',
+            'object'   => 'lead',
+            'type'     => 'datetime',
+            'filter'   => null,
+            'display'  => null,
+            'operator' => '!empty',
+        ]);
+
+        $this->assertSame('date_identified', $baseDecorator->getField($contactSegmentFilterCrate));
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getTable
+     */
+    public function testGetTableLead()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+        $baseDecorator                = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([
+            'object'   => 'lead',
+        ]);
+
+        $this->assertSame(MAUTIC_TABLE_PREFIX.'leads', $baseDecorator->getTable($contactSegmentFilterCrate));
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getTable
+     */
+    public function testGetTableCompany()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+        $baseDecorator                = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([
+            'object'   => 'company',
+        ]);
+
+        $this->assertSame(MAUTIC_TABLE_PREFIX.'companies', $baseDecorator->getTable($contactSegmentFilterCrate));
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getOperator
+     */
+    public function testGetOperatorEqual()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+        $contactSegmentFilterOperator->expects($this->once())
+            ->method('fixOperator')
+            ->with('=')
+            ->willReturn('eq');
+
+        $baseDecorator = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([
+            'operator' => '=',
+        ]);
+
+        $this->assertSame('eq', $baseDecorator->getOperator($contactSegmentFilterCrate));
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getOperator
+     */
+    public function testGetOperatorStartsWith()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+        $contactSegmentFilterOperator->expects($this->once())
+            ->method('fixOperator')
+            ->with('startsWith')
+            ->willReturn('startsWith');
+
+        $baseDecorator = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([
+            'operator' => 'startsWith',
+        ]);
+
+        $this->assertSame('like', $baseDecorator->getOperator($contactSegmentFilterCrate));
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getOperator
+     */
+    public function testGetOperatorEndsWith()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+        $contactSegmentFilterOperator->expects($this->once())
+            ->method('fixOperator')
+            ->with('endsWith')
+            ->willReturn('endsWith');
+
+        $baseDecorator = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([
+            'operator' => 'endsWith',
+        ]);
+
+        $this->assertSame('like', $baseDecorator->getOperator($contactSegmentFilterCrate));
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getOperator
+     */
+    public function testGetOperatorContainsWith()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+        $contactSegmentFilterOperator->expects($this->once())
+            ->method('fixOperator')
+            ->with('contains')
+            ->willReturn('contains');
+
+        $baseDecorator = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([
+            'operator' => 'contains',
+        ]);
+
+        $this->assertSame('like', $baseDecorator->getOperator($contactSegmentFilterCrate));
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getQueryType
+     */
+    public function testGetQueryType()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+
+        $baseDecorator = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([]);
+
+        $this->assertSame('mautic.lead.query.builder.basic', $baseDecorator->getQueryType($contactSegmentFilterCrate));
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getParameterHolder
+     */
+    public function testGetParameterHolderSingle()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+
+        $baseDecorator = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([]);
+
+        $this->assertSame(':argument', $baseDecorator->getParameterHolder($contactSegmentFilterCrate, 'argument'));
+    }
+
+    /**
+     * @covers \Mautic\LeadBundle\Segment\Decorator\BaseDecorator::getParameterHolder
+     */
+    public function testGetParameterHolderArray()
+    {
+        $contactSegmentFilterOperator = $this->createMock(ContactSegmentFilterOperator::class);
+
+        $baseDecorator = new BaseDecorator($contactSegmentFilterOperator);
+
+        $contactSegmentFilterCrate = new ContactSegmentFilterCrate([]);
+
+        $argument = [
+            'argument1',
+            'argument2',
+            'argument3',
+        ];
+
+        $expected = [
+            ':argument1',
+            ':argument2',
+            ':argument3',
+        ];
+        $this->assertSame($expected, $baseDecorator->getParameterHolder($contactSegmentFilterCrate, $argument));
+    }
+}
