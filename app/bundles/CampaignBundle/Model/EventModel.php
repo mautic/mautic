083b3df4246a5441509e500c91ddaa4d572a5608
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-03-23 18:26:00 +0100

    Remove lost logger

--- app/bundles/CampaignBundle/Model/EventModel.php
+++ app/bundles/CampaignBundle/Model/EventModel.php
@@ -845,7 +845,6 @@ class EventModel extends CommonFormModel
 
             // Get a count
             $events = $repo->getScheduledEvents($campaignId, false, $limit);
-            $this->logger->debug('CAMPAIGN: events #'.print_r($events, true));
 
             if (empty($events)) {
                 unset($campaignEvents, $event, $leads, $eventSettings);
