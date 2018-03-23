86f0cdfbf921b358a2726ef5c6a39ac280610023
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-03-23 12:25:13 +0100

    Minor html style

--- app/bundles/LeadBundle/Views/Form/list.html.php
+++ app/bundles/LeadBundle/Views/Form/list.html.php
@@ -1,11 +1,11 @@
 <!-- tabs controls -->
 <ul class="nav nav-tabs pr-md pl-md mt-10">
-    <?php foreach ($leadForms as $key=>$leadForm): ?>
+    <?php foreach ($leadForms as $key => $leadForm): ?>
         <li<?php if ($key == 0): ?> class="active"<?php endif; ?>>
-            <a href="#form-<?php echo $leadForm['entity']->getAlias()?>" role="tab" data-toggle="tab">
-                        <span class="label label-primary mr-sm" id="form-label-<?php echo $leadForm['entity']->getAlias()?>">
-                             <?php echo $leadForm['results']['count']; ?>
-                        </span>
+            <a href="#form-<?php echo $leadForm['entity']->getAlias() ?>" role="tab" data-toggle="tab">
+       <span class="label label-primary mr-sm" id="form-label-<?php echo $leadForm['entity']->getAlias() ?>">
+           <?php echo $leadForm['results']['count']; ?>
+       </span>
                 <?php echo $leadForm['entity']->getName(); ?>
             </a>
         </li>
