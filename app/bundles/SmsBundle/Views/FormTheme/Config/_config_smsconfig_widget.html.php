a25188aab869cb6fbb992d38911b2c31b5f4a352
Alan Hartless <alan@devkardia.com>
2018-03-27 12:53:08 -0500

    Add formatting to the config tab

---
+++ app/bundles/SmsBundle/Views/FormTheme/Config/_config_smsconfig_widget.html.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+?>
+
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?php echo $view['translator']->trans('mautic.config.header.smsconfig'); ?></h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-6">
+                <?php echo $view['form']->row($form['sms_transport']); ?>
+            </div>
+        </div>
+    </div>
+</div>
