98f6e47b6dafe7d945233631a3560c6205781cb2
John Linhart <admin@escope.cz>
2018-04-17 17:24:59 +0200

    Optimise the new JS code a little

--- app/bundles/LeadBundle/Assets/js/lead.js
+++ app/bundles/LeadBundle/Assets/js/lead.js
@@ -924,30 +924,15 @@ Mautic.toggleLeadList = function(toggleId, leadId, listId) {
 };
 
 Mautic.togglePreferredChannel = function(channel) {
-    if (channel == 'all') {
-        var status;
-
-        if (mQuery('form[name="lead_contact_channels"]').find('#lead_contact_channels_subscribed_channels_0').length > 0) {
-            status = mQuery('#lead_contact_channels_subscribed_channels_0')[0].checked;
-
-            mQuery('form[name="lead_contact_channels"]').find('input:checkbox').each(function(){ //iterate all listed checkbox items
-                if (this.checked != status) {
-                    this.checked = status;
-                    Mautic.setPreferredChannel(this.value);
-                }
-            });
-
-        } else if (mQuery('form[name="lead_contact_frequency_rules"]').find('#lead_contact_frequency_rules_subscribed_channels_0').length > 0) {
-            status = mQuery('#lead_contact_frequency_rules_subscribed_channels_0')[0].checked;
-
-            mQuery('form[name="lead_contact_frequency_rules"]').find('input:checkbox').each(function(){ //iterate all listed checkbox items
-                if (this.checked != status) {
-                    this.checked = status;
-                    Mautic.setPreferredChannel(this.value);
-                }
-            });
-        }
-
+    if (channel === 'all') {
+        var channelsForm = mQuery('form[name="lead_contact_channels"]');
+        var status = channelsForm.find('#lead_contact_channels_subscribed_channels_0:checked').length;
+        channelsForm.find('tbody input:checkbox').each(function() {
+            if (this.checked != status) {
+                this.checked = status;
+                Mautic.setPreferredChannel(this.value);
+            }
+        });
     } else {
         Mautic.setPreferredChannel(channel);
     }
