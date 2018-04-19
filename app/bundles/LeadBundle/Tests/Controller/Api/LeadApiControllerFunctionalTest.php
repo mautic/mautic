c51bd26f05d94f139e0f8ab24cef893209d4a1bb
Alan Hartless <alan@devkardia.com>
2018-04-19 15:52:47 -0600

    Add an unsanitized email to ensure that duplicate are not created

--- app/bundles/LeadBundle/Tests/Controller/Api/LeadApiControllerFunctionalTest.php
+++ app/bundles/LeadBundle/Tests/Controller/Api/LeadApiControllerFunctionalTest.php
@@ -38,15 +38,24 @@ class LeadApiControllerFunctionalTest extends MauticMysqlTestCase
         $response       = json_decode($clientResponse->getContent(), true);
 
         $this->assertEquals(Codes::HTTP_CREATED, $response['statusCodes'][0]);
+        $contactId1 = $response['contacts'][0]['id'];
         $this->assertEquals(Codes::HTTP_CREATED, $response['statusCodes'][1]);
+        $contactId2 = $response['contacts'][1]['id'];
         $this->assertEquals(Codes::HTTP_CREATED, $response['statusCodes'][2]);
+        $contactId3 = $response['contacts'][2]['id'];
+
+        // Emulate an unsanitized email to ensure that doesn't cause duplicates
+        $payload[0]['email'] = 'batchemail1@email.com,';
 
         $this->client->request('POST', '/api/contacts/batch/new', $payload);
         $clientResponse = $this->client->getResponse();
         $response       = json_decode($clientResponse->getContent(), true);
 
         $this->assertEquals(Codes::HTTP_OK, $response['statusCodes'][0]);
+        $this->assertEquals($contactId1, $response['contacts'][0]['id']);
         $this->assertEquals(Codes::HTTP_OK, $response['statusCodes'][1]);
+        $this->assertEquals($contactId2, $response['contacts'][1]['id']);
         $this->assertEquals(Codes::HTTP_OK, $response['statusCodes'][2]);
+        $this->assertEquals($contactId3, $response['contacts'][2]['id']);
     }
 }
