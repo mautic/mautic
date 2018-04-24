mautic/mautic

commit 6bd917083908923f6ef8f314d1a1be2a12b74932

change the way tags are define for SMS Transport, no need for duplicate tags anymore

app/bundles/CoreBundle/DependencyInjection/Compiler/SmsTransportPass.php
@@ -16,8 +16,19 @@ use Symfony\Component\DependencyInjection\Compiler\RepeatedPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
+/**
+ * Class SmsTransportPass.
+ */
 class SmsTransportPass implements CompilerPassInterface, RepeatablePassInterface
 {
+    /**
+     * @var RepeatedPass
+     */
+    private $repeatedPass;
+
+    /**
+     * @param ContainerBuilder $container
+     */
     public function process(ContainerBuilder $container)
     {
         if (!$container->has('mautic.sms.transport_chain')) {
@@ -41,6 +52,9 @@ class SmsTransportPass implements CompilerPassInterface, RepeatablePassInterface
         }
     }
 
+    /**
+     * @param RepeatedPass $repeatedPass
+     */
     public function setRepeatedPass(RepeatedPass $repeatedPass)
     {
         $this->repeatedPass = $repeatedPass;

app/bundles/SmsBundle/Config/config.php
@@ -112,9 +112,7 @@ return [
                     'monolog.logger.mautic',
                 ],
                 'alias' => 'mautic.sms.transport.twilio',
-                'tags'  => [
-                    'name' => 'mautic.sms_transport', 'Twilio',
-                ],
+                'tag'   => 'mautic.sms_transport',
             ],
         ],
         'models' => [

commit cce17656ac531380dc67890b3c5657fca9e7b8c2

Fixed issue that prevented Twilio from showing up in the dropdown list inadvertently caused by #5983

app/bundles/CoreBundle/DependencyInjection/Compiler/SmsTransportPass.php
@@ -37,17 +37,12 @@ class SmsTransportPass implements CompilerPassInterface, RepeatablePassInterface
 
         $definition     = $container->getDefinition('mautic.sms.transport_chain');
         $taggedServices = $container->findTaggedServiceIds('mautic.sms_transport');
-
         foreach ($taggedServices as $id => $tags) {
-            $serviceTags   = $container->findDefinition($id)->getTags();
-            $serviceTags   = array_keys($serviceTags);
-            $integrationId = array_pop($serviceTags);
-
             $definition->addMethodCall('addTransport', [
                 $id,
                 new Reference($id),
-                $tags[0]['alias'],
-                $integrationId,
+                !empty($tags[0]['alias']) ? $tags[0]['alias'] : $id,
+                !empty($tags[0]['integrationAlias']) ? $tags[0]['integrationAlias'] : $id,
             ]);
         }
     }

app/bundles/SmsBundle/Config/config.php
@@ -100,19 +100,25 @@ return [
                 'alias' => 'sms_api',
             ],
             'mautic.sms.transport_chain' => [
-                'class'     => '\Mautic\SmsBundle\Sms\TransportChain',
-                'arguments' => ['%mautic.sms_transport%', 'mautic.helper.integration', 'monolog.logger.mautic'],
+                'class'     => \Mautic\SmsBundle\Sms\TransportChain::class,
+                'arguments' => [
+                    '%mautic.sms_transport%',
+                    'mautic.helper.integration',
+                    'monolog.logger.mautic',
+                ],
             ],
             'mautic.sms.transport.twilio' => [
-                'class'     => 'Mautic\SmsBundle\Api\TwilioApi',
-                'arguments' => [
+                'class'        => \Mautic\SmsBundle\Api\TwilioApi::class,
+                'arguments'    => [
                     'mautic.page.model.trackable',
                     'mautic.helper.phone_number',
                     'mautic.helper.integration',
                     'monolog.logger.mautic',
                 ],
-                'alias' => 'mautic.sms.transport.twilio',
-                'tag'   => 'mautic.sms_transport',
+                'tag'          => 'mautic.sms_transport',
+                'tagArguments' => [
+                    'integrationAlias' => 'Twilio',
+                ],
             ],
         ],
         'models' => [