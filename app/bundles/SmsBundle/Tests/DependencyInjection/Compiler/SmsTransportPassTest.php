ac90e9c8ff0226790acafe1e88ecad2b5283ada3
Alan Hartless <alan@devkardia.com>
2018-04-24 13:16:26 -0500

    OCD - Moved sms compiler to SMS bundle

--- app/bundles/SmsBundle/Tests/DependencyInjection/Compiler/SmsTransportPassTest.php
+++ app/bundles/SmsBundle/Tests/DependencyInjection/Compiler/SmsTransportPassTest.php
@@ -8,7 +8,7 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\CoreBundle\DependencyInjection\Compiler;
+namespace Mautic\SmsBundle\DependencyInjection\Compiler;
 
 use Mautic\CoreBundle\Test\AbstractMauticTestCase;
 use Mautic\SmsBundle\Sms\TransportChain;
