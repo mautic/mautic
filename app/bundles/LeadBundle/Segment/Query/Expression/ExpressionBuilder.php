f995db09c3d617fd1f657b6bdc183acf03e1df7b
Petr Fidler <info@maxell-cz.cz>
2018-05-16 18:40:10 +0200

    Remove unnecessary check for arguments - done in another function

--- app/bundles/LeadBundle/Segment/Query/Expression/ExpressionBuilder.php
+++ app/bundles/LeadBundle/Segment/Query/Expression/ExpressionBuilder.php
@@ -149,10 +149,6 @@ class ExpressionBuilder
      */
     public function notBetween($x, $arr)
     {
-        if (!is_array($arr) || count($arr) != 2) {
-            throw new SegmentQueryException('Not between expression expects second argument to be an array with exactly two elements');
-        }
-
         return 'NOT '.$this->between($x, $arr);
     }
 
