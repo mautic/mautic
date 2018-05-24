ad14319ce73edffa741541e4e5a20ffa7b2c2156
Unknown <peter.osvath@d-code.hu>
2018-05-24 16:06:22 +0200

    Revert "start consumer sh script"
    This reverts commit 5f24b25714835f044d55d0d2f9beff656e3c265a.

--- plugins/RabbitMQBundle/!start_mautic_consumer.sh
+++ plugins/RabbitMQBundle/!start_mautic_consumer.sh
@@ -2,4 +2,4 @@
 #This file for: quickly start the RabbitMQ Mautic consumer
 #---------------------------------------
 
-screen -dmS mautic_consumer php ../../app/console rabbitmq:consumer:mautic && screen -ls && exit(0)
+screen -dmS mautic_consumer php ../../app/console rabbitmq:consumer:mautic && screen -ls
