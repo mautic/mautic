68292cf2ae117f498f1c0603c6ab27edd7677f5c
Alan Hartless <alan@devkardia.com>
2018-05-29 12:03:07 -0500

    Fix campaign model tests

--- app/bundles/CampaignBundle/Tests/CampaignTestAbstract.php
+++ app/bundles/CampaignBundle/Tests/CampaignTestAbstract.php
@@ -14,6 +14,7 @@ namespace Mautic\CampaignBundle\Tests;
 use Doctrine\ORM\EntityManager;
 use Mautic\CampaignBundle\EventCollector\EventCollector;
 use Mautic\CampaignBundle\Helper\RemovedContactTracker;
+use Mautic\CampaignBundle\Membership\MembershipManager;
 use Mautic\CampaignBundle\Model\CampaignModel;
 use Mautic\CoreBundle\Helper\CoreParametersHelper;
 use Mautic\CoreBundle\Helper\UserHelper;
@@ -86,7 +87,9 @@ class CampaignTestAbstract extends \PHPUnit_Framework_TestCase
 
         $removedContactTracker = $this->createMock(RemovedContactTracker::class);
 
-        $campaignModel = new CampaignModel($coreParametersHelper, $leadModel, $leadListModel, $formModel, $eventCollector, $removedContactTracker);
+        $membershipManager = $this->createMock(MembershipManager::class);
+
+        $campaignModel = new CampaignModel($coreParametersHelper, $leadModel, $leadListModel, $formModel, $eventCollector, $removedContactTracker, $membershipManager);
 
         $leadModel->setEntityManager($entityManager);
         $leadListModel->setEntityManager($entityManager);
