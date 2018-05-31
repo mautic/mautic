22ab0bcbf7b4814b5ab3c8aee160998862bf5d59
cykonetic <nicholaibush@yahoo.com>
2018-05-31 11:21:59 -0400

    [Campaign Tagging] CampaignBundle/Controller/AjaxController.php
    Allowing Campaigns to be tagged.

--- app/bundles/CampaignBundle/Controller/AjaxController.php
+++ app/bundles/CampaignBundle/Controller/AjaxController.php
@@ -138,4 +138,85 @@ class AjaxController extends CommonAjaxController
 
         return null;
     }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    protected function updateCampaignTagsAction(Request $request)
+    {
+        /** @var \Mautic\CampaignBundle\Model\CampaignModel $campaignModel */
+        $campaignModel   = $this->getModel('campaign');
+        $post            = $request->request->get('campaign_tags', [], true);
+        $campaign        = $campaignModel->getEntity((int) $post['id']);
+        $updatedTags     = (!empty($post['tags']) && is_array($post['tags'])) ? $post['tags'] : [];
+        $data            = ['success' => 0];
+
+        if ($campaign !== null && $this->get('mautic.security')->hasEntityAccess('campaign:campaigns:editown', 'campaign:campaigns:editother', $campaign->getPermissionUser())) {
+            $campaignModel->setTags($campaign, $updatedTags, true);
+
+            /** @var \Doctrine\ORM\PersistentCollection $campaignTags */
+            $campaignTags    = $campaign->getTags();
+            $campaignTagKeys = $campaignTags->getKeys();
+
+            // Get an updated list of tags
+            $tags       = $campaignModel->getTagRepository()->getSimpleList(null, [], 'tag');
+            $tagOptions = '';
+
+            foreach ($tags as $tag) {
+                $selected = (in_array($tag['label'], $campaignTagKeys)) ? ' selected="selected"' : '';
+                $tagOptions .= '<option'.$selected.' value="'.$tag['value'].'">'.$tag['label'].'</option>';
+            }
+
+            $data['success'] = 1;
+            $data['tags']    = $tagOptions;
+        }
+
+        return $this->sendJsonResponse($data);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    protected function addCampaignTagsAction(Request $request)
+    {
+        $tags = $request->request->get('tags');
+        $tags = json_decode($tags, true);
+
+        if (is_array($tags)) {
+            $campaignModel = $this->getModel('campaign');
+            $newTags       = [];
+
+            foreach ($tags as $tag) {
+                if (!is_numeric($tag)) {
+                    $newTags[] = $campaignModel->getTagRepository()->getTagByNameOrCreateNewOne($tag);
+                }
+            }
+
+            if (!empty($newTags)) {
+                $campaignModel->getTagRepository()->saveEntities($newTags);
+            }
+
+            // Get an updated list of tags
+            $allTags    = $campaignModel->getTagRepository()->getSimpleList(null, [], 'tag');
+            $tagOptions = '';
+
+            foreach ($allTags as $tag) {
+                $selected = (in_array($tag['value'], $tags) || in_array($tag['label'], $tags)) ? ' selected="selected"' : '';
+                $tagOptions .= '<option'.$selected.' value="'.$tag['value'].'">'.$tag['label'].'</option>';
+            }
+
+            $data = [
+                'success' => 1,
+                'tags'    => $tagOptions,
+            ];
+        } else {
+            $data = ['success' => 0];
+        }
+
+        return $this->sendJsonResponse($data);
+    }
 }
