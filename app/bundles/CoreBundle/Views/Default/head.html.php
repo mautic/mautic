132782fa76d201813724c5a6cd9aba48cfd1651c
Péter Osváth <peter.osvath@d-code.hu>
2018-06-13 14:16:40 +0200

    setting page title to kiazaki

--- app/bundles/CoreBundle/Views/Default/head.html.php
+++ app/bundles/CoreBundle/Views/Default/head.html.php
@@ -14,7 +14,7 @@
     <title><?php if (!empty($view['slots']->get('headerTitle', ''))): ?>
         <?php echo strip_tags(str_replace('<', ' <', $view['slots']->get('headerTitle', ''))); ?> | 
     <?php endif; ?>
-	<?php echo $view['slots']->get('pageTitle', 'Mautic'); ?>
+	<?php echo $view['slots']->get('pageTitle', 'Kiazaki'); ?>
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="icon" type="image/x-icon" href="<?php echo $view['assets']->getUrl('media/images/favicon.ico') ?>" />
