44fe1ccbfa7348852c771def657a73520ff7caf0
heathdutton <hdutton@thedmsgrp.com>
2018-06-18 12:42:08 -0400

    Add testCampaignExecutionForAllInQuietMode.

--- app/bundles/CampaignBundle/Tests/Command/TriggerCampaignCommandTest.php
+++ app/bundles/CampaignBundle/Tests/Command/TriggerCampaignCommandTest.php
@@ -178,6 +178,168 @@ class TriggerCampaignCommandTest extends AbstractCampaignCommand
     /**
      * @throws \Exception
      */
+    public function testCampaignExecutionForAllInQuietMode()
+    {
+        // Process in batches of 10 to ensure batching is working as expected
+        $this->runCommand('mautic:campaigns:trigger', ['-i' => 1, '-l' => 10, '-q' => true]);
+
+        // Let's analyze
+        $byEvent = $this->getCampaignEventLogs([1, 2, 11, 12, 13]);
+        $tags    = $this->getTagCounts();
+
+        // Everyone should have been tagged with CampaignTest and have been sent Campaign Test Email 1
+        $this->assertCount(50, $byEvent[1]);
+        $this->assertCount(50, $byEvent[2]);
+
+        // Sending Campaign Test Email 1 should be scheduled
+        foreach ($byEvent[2] as $log) {
+            if (0 === (int) $log['is_scheduled']) {
+                $this->fail('Sending Campaign Test Email 1 was not scheduled for lead ID '.$log['lead_id']);
+            }
+        }
+
+        // Everyone should have had the Is US condition processed
+        $this->assertCount(50, $byEvent[11]);
+
+        // 42 should have been send down the non-action path (red) of the condition
+        $nonActionCount = $this->getNonActionPathTakenCount($byEvent[11]);
+        $this->assertEquals(42, $nonActionCount);
+
+        // 8 contacts are from the US and should be labeled with US:Action
+        $this->assertCount(8, $byEvent[12]);
+        $this->assertEquals(8, $tags['US:Action']);
+
+        // The rest (42) contacts are not from the US and should be labeled with NonUS:Action
+        $this->assertCount(42, $byEvent[13]);
+        $this->assertEquals(42, $tags['NonUS:Action']);
+
+        // No emails should be sent till after 5 seconds and the command is ran again
+        $stats = $this->db->createQueryBuilder()
+            ->select('*')
+            ->from($this->prefix.'email_stats', 'stat')
+            ->where('stat.lead_id <= 25')
+            ->execute()
+            ->fetchAll();
+        $this->assertCount(0, $stats);
+
+        // Wait 20 seconds then execute the campaign again to send scheduled events
+        sleep(20);
+        $this->runCommand('mautic:campaigns:trigger', ['-i' => 1, '-l' => 10, '-q' => true]);
+
+        // Send email 1 should no longer be scheduled
+        $byEvent = $this->getCampaignEventLogs([2, 4]);
+        $this->assertCount(50, $byEvent[2]);
+        foreach ($byEvent[2] as $log) {
+            if (1 === (int) $log['is_scheduled']) {
+                $this->fail('Sending Campaign Test Email 1 is still scheduled for lead ID '.$log['lead_id']);
+            }
+        }
+
+        // The non-action events attached to the decision should have no logs entries
+        $this->assertCount(0, $byEvent[4]);
+
+        // Check that the emails actually sent
+        $stats = $this->db->createQueryBuilder()
+            ->select('*')
+            ->from($this->prefix.'email_stats', 'stat')
+            ->where('stat.lead_id <= 25')
+            ->execute()
+            ->fetchAll();
+        $this->assertCount(25, $stats);
+
+        // Now let's simulate email opens
+        foreach ($stats as $stat) {
+            $this->client->request('GET', '/email/'.$stat['tracking_hash'].'.gif');
+            $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), var_export($this->client->getResponse()->getContent()));
+        }
+
+        $byEvent = $this->getCampaignEventLogs([3, 4, 5, 10, 14, 15]);
+
+        // The non-action events attached to the decision should have no logs entries
+        $this->assertCount(0, $byEvent[4]);
+        $this->assertCount(0, $byEvent[5]);
+        $this->assertCount(0, $byEvent[14]);
+        $this->assertCount(0, $byEvent[15]);
+
+        // Those 25 should now have open email decisions logged and the next email sent
+        $this->assertCount(25, $byEvent[3]);
+        $this->assertCount(25, $byEvent[10]);
+
+        // Wait 20 seconds to go beyond the inaction timeframe
+        sleep(20);
+
+        // Execute the command again to trigger inaction related events
+        $this->runCommand('mautic:campaigns:trigger', ['-i' => 1, '-l' => 10, '-q' => true]);
+
+        // Now we should have 50 email open decisions
+        $byEvent = $this->getCampaignEventLogs([3, 4, 5, 14, 15]);
+        $this->assertCount(50, $byEvent[3]);
+
+        // 25 should be marked as non_action_path_taken
+        $nonActionCount = $this->getNonActionPathTakenCount($byEvent[3]);
+        $this->assertEquals(25, $nonActionCount);
+
+        // A condition should be logged as evaluated for each of the 25 contacts
+        $this->assertCount(25, $byEvent[4]);
+        $this->assertCount(25, $byEvent[5]);
+
+        // Tag EmailNotOpen should all be scheduled for these 25 contacts because the condition's timeframe was shorter and therefore the
+        // contact was sent down the inaction path
+        $this->assertCount(25, $byEvent[14]);
+        $this->assertCount(25, $byEvent[15]);
+
+        $utcTimezone = new \DateTimeZone('UTC');
+        foreach ($byEvent[14] as $log) {
+            if (0 === (int) $log['is_scheduled']) {
+                $this->fail('Tag EmailNotOpen is not scheduled for lead ID '.$log['lead_id']);
+            }
+
+            $scheduledFor = new \DateTime($log['trigger_date'], $utcTimezone);
+            $diff         = $this->eventDate->diff($scheduledFor);
+
+            if (2 !== $diff->i) {
+                $this->fail('Tag EmailNotOpen should be scheduled for around 2 minutes ('.$diff->i.' minutes)');
+            }
+        }
+
+        foreach ($byEvent[15] as $log) {
+            if (0 === (int) $log['is_scheduled']) {
+                $this->fail('Tag EmailNotOpen Again is not scheduled for lead ID '.$log['lead_id']);
+            }
+
+            $scheduledFor = new \DateTime($log['trigger_date'], $utcTimezone);
+            $diff         = $this->eventDate->diff($scheduledFor);
+
+            if (6 !== $diff->i) {
+                $this->fail('Tag EmailNotOpen Again should be scheduled for around 6 minutes ('.$diff->i.' minutes)');
+            }
+        }
+        $byEvent = $this->getCampaignEventLogs([6, 7, 8, 9]);
+        $tags    = $this->getTagCounts();
+
+        // Of those that did not open the email, 6 should be tagged US:NotOpen
+        $this->assertCount(6, $byEvent[6]);
+        $this->assertEquals(6, $tags['US:NotOpen']);
+
+        // And 19 should be tagged NonUS:NotOpen
+        $this->assertCount(19, $byEvent[7]);
+        $this->assertEquals(19, $tags['NonUS:NotOpen']);
+
+        // And 4 should be tagged UK:NotOpen
+        $this->assertCount(4, $byEvent[8]);
+        $this->assertEquals(4, $tags['UK:NotOpen']);
+
+        // And 21 should be tagged NonUK:NotOpen
+        $this->assertCount(21, $byEvent[9]);
+        $this->assertEquals(21, $tags['NonUK:NotOpen']);
+
+        // No one should be tagged as EmailNotOpen because the actions are still scheduled
+        $this->assertFalse(isset($tags['EmailNotOpen']));
+    }
+
+    /**
+     * @throws \Exception
+     */
     public function testCampaignExecutionForOne()
     {
         $this->runCommand('mautic:campaigns:trigger', ['-i' => 1, '--contact-id' => 1]);
