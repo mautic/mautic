82ab42d28bbfaaac74a4591c182daa4e263b2158
Jan Kozak <galvani78@gmail.com>
2018-06-20 12:16:42 +0200

    use exists i.o. joins for foreign value filter queries

--- app/bundles/LeadBundle/Segment/Query/Filter/ForeignValueFilterQueryBuilder.php
+++ app/bundles/LeadBundle/Segment/Query/Filter/ForeignValueFilterQueryBuilder.php
@@ -42,122 +42,65 @@ class ForeignValueFilterQueryBuilder extends BaseFilterQueryBuilder
 
         $filterParametersHolder = $filter->getParameterHolder($parameters);
 
-        switch ($filterOperator) {
-            case 'notIn':
-                $tableAlias = $this->generateRandomParameterName();
+        $tableAlias = $this->generateRandomParameterName();
 
-                if (!is_null($filter->getWhere())) {
-                    $expression = ' AND '.str_replace(str_replace(MAUTIC_TABLE_PREFIX, '', $filter->getTable()).'.',
-                            $tableAlias.'.', $filter->getWhere());
-                } else {
-                    $expression = '';
-                }
+        $subQueryBuilder = $queryBuilder->getConnection()->createQueryBuilder();
+        $subQueryBuilder
+            ->select('id')->from($filter->getTable(), $tableAlias)
+            ->andWhere($tableAlias.'.lead_id = l.id');
 
-                $queryBuilder = $queryBuilder->leftJoin(
-                    $queryBuilder->getTableAlias(MAUTIC_TABLE_PREFIX.'leads'),
-                    $filter->getTable(),
-                    $tableAlias,
-                    $tableAlias.'.lead_id = l.id'
-                );
-
-                $expression = $queryBuilder->expr()->in(
-                    $tableAlias.'.'.$filter->getField(),
-                    $filterParametersHolder
-                );
-
-                $queryBuilder->setParametersPairs($filterParametersHolder, $filter->getParameterValue());
-
-                //$queryBuilder->addJoinCondition($tableAlias, ' ('.$expression.')');
-                $queryBuilder->addLogic($tableAlias, $expression, $filter->getGlue());
-
-                break;
-            case 'neq':
-            case 'notLike':
-                $tableAlias   = $this->generateRandomParameterName();
-                $queryBuilder->leftJoin(
-                    $queryBuilder->getTableAlias(MAUTIC_TABLE_PREFIX.'leads'),
-                    $filter->getTable(),
-                    $tableAlias,
-                    $tableAlias.'.lead_id = l.id'
-                );
-
-                $queryBuilder->addLogic($queryBuilder->expr()->isNull($tableAlias.'.lead_id'), $filter->getGlue());
-                break;
-            case 'empty':
-            case 'notEmpty':
-                $tableAlias = $this->generateRandomParameterName();
-
-                $queryBuilder->leftJoin(
-                    $queryBuilder->getTableAlias(MAUTIC_TABLE_PREFIX.'leads'),
-                    $filter->getTable(),
-                    $tableAlias,
-                    $tableAlias.'.lead_id = l.id'
-                );
-
-                if ($filterOperator == 'empty') {
-                    $queryBuilder->addJoinCondition($tableAlias, $queryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField()),
-                        $filter->getGlue());
-                } else {
-                    $queryBuilder->addJoinCondition($tableAlias, $queryBuilder->expr()->isNotNull($tableAlias.'.'.$filter->getField()),
-                        $filter->getGlue());
-                }
-
-                break;
-            default:
-                $tableAlias = $this->generateRandomParameterName();
-
-                if (!is_null($filter->getWhere())) {
-                    $where = ' AND '.str_replace(str_replace(MAUTIC_TABLE_PREFIX, '', $filter->getTable()).'.', $tableAlias.'.', $filter->getWhere());
-                } else {
-                    $where = '';
-                }
-
-                $queryBuilder->leftJoin(
-                    $queryBuilder->getTableAlias(MAUTIC_TABLE_PREFIX.'leads'),
-                    $filter->getTable(),
-                    $tableAlias,
-                    $tableAlias.'.lead_id = l.id'.$where
-                );
-
-                $queryBuilder->addLogic($queryBuilder->expr()->isNotNull($tableAlias.'.lead_id'), $filter->getGlue());
+        if (!is_null($filter->getWhere())) {
+            $subQueryBuilder->andWhere(str_replace(str_replace(MAUTIC_TABLE_PREFIX, '', $filter->getTable()).'.', $tableAlias.'.', $filter->getWhere()));
         }
 
         switch ($filterOperator) {
             case 'empty':
+                $subQueryBuilder->andWhere($subQueryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField()));
+                $queryBuilder->addLogic($queryBuilder->expr()->exists($subQueryBuilder->getSQL()), $filter->getGlue());
+                break;
             case 'notEmpty':
-                $expression = $queryBuilder->expr()->isNotNull($tableAlias.'.lead_id');
-                $queryBuilder->addLogic($expression, $filter->getGlue());
+                $subQueryBuilder->andWhere($subQueryBuilder->expr()->isNotNull($tableAlias.'.'.$filter->getField()));
+                $queryBuilder->addLogic($queryBuilder->expr()->exists($subQueryBuilder->getSQL()), $filter->getGlue());
                 break;
             case 'notIn':
-                $expression = $queryBuilder->expr()->isNull($tableAlias.'.lead_id');
-                $queryBuilder->addLogic($expression, $filter->getGlue());
-                break;
-            case 'neq':
-                $expression = $queryBuilder->expr()->orX(
-                    $queryBuilder->expr()->eq($tableAlias.'.'.$filter->getField(), $filterParametersHolder),
-                    $queryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField())
-                );
-
-                $queryBuilder->addJoinCondition($tableAlias, $expression);
-                $queryBuilder->setParametersPairs($parameters, $filterParameters);
-                break;
-            case 'notLike':
-                $expression = $queryBuilder->expr()->orX(
-                    $queryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField()),
-                    $queryBuilder->expr()->like($tableAlias.'.'.$filter->getField(), $filterParametersHolder)
-                );
-
-                $queryBuilder->addJoinCondition($tableAlias, ' ('.$expression.')');
-                $queryBuilder->setParametersPairs($parameters, $filterParameters);
-                break;
-            default:
-                $expression = $queryBuilder->expr()->$filterOperator(
+                $expression = $subQueryBuilder->expr()->$filterOperator(
                     $tableAlias.'.'.$filter->getField(),
                     $filterParametersHolder
                 );
-                $queryBuilder->addJoinCondition($tableAlias, ' ('.$expression.')');
-                $queryBuilder->setParametersPairs($parameters, $filterParameters);
+                $subQueryBuilder->andWhere($expression);
+                $queryBuilder->addLogic($queryBuilder->expr()->notExists($subQueryBuilder->getSQL()), $filter->getGlue());
+                break;
+            case 'neq':
+                $expression = $subQueryBuilder->expr()->orX(
+                    $subQueryBuilder->expr()->eq($tableAlias.'.'.$filter->getField(), $filterParametersHolder),
+                    $subQueryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField())
+                );
+
+                $subQueryBuilder->andWhere($expression);
+
+                $queryBuilder->addLogic($queryBuilder->expr()->notExists($subQueryBuilder->getSQL()), $filter->getGlue());
+                break;
+            case 'notLike':
+                $expression = $subQueryBuilder->expr()->orX(
+                    $subQueryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField()),
+                    $subQueryBuilder->expr()->like($tableAlias.'.'.$filter->getField(), $filterParametersHolder)
+                );
+
+                $subQueryBuilder->andWhere($expression);
+
+                $queryBuilder->addLogic($queryBuilder->expr()->notExists($subQueryBuilder->getSQL()), $filter->getGlue());
+                break;
+            default:
+                $expression = $subQueryBuilder->expr()->$filterOperator(
+                    $tableAlias.'.'.$filter->getField(),
+                    $filterParametersHolder
+                );
+                $subQueryBuilder->andWhere($expression);
+
+                $queryBuilder->addLogic($queryBuilder->expr()->exists($subQueryBuilder->getSQL()), $filter->getGlue());
         }
+
+        $queryBuilder->setParametersPairs($parameters, $filterParameters);
 
         return $queryBuilder;
     }
