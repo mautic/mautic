971d897968df2f56926f4ce7a601848e7fdfa670
Péter Osváth <peter.osvath@d-code.hu>
2018-06-20 17:56:10 +0200

    whitelabel branding

--- app/bundles/CoreBundle/Views/Default/base.html.php
+++ app/bundles/CoreBundle/Views/Default/base.html.php
@@ -41,7 +41,7 @@
             <footer id="app-footer">
                 <div class="container-fluid">
                     <div class="row">
-                        <div class="col-xs-6 text-muted"><?php echo $view['translator']->trans('mautic.core.copyright', ['%date%' => date('Y')]); ?></div>
+                        <div class="col-xs-6 text-muted"><?php echo $whitelabelBrandingCopyright?:$view['translator']->trans('mautic.core.copyright', ['%date%' => date('Y')]); ?></div>
                         <div class="col-xs-6 text-muted text-right small">v<?php
                             /** @var \Mautic\CoreBundle\Templating\Helper\VersionHelper $version */
                             $version = $view['version'];
