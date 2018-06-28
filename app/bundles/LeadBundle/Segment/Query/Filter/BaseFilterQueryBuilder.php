3cd3e052921f1874f93ffb79f502b96b175ef4f9
Jan Kozak <galvani78@gmail.com>
2018-06-28 15:45:32 -0400

    fix incorrect glue argument for foregin tables
    # Conflicts:
    #	app/bundles/LeadBundle/Segment/Query/Filter/ForeignValueFilterQueryBuilder.php

--- app/bundles/LeadBundle/Segment/Query/Filter/BaseFilterQueryBuilder.php
+++ app/bundles/LeadBundle/Segment/Query/Filter/BaseFilterQueryBuilder.php
@@ -46,9 +46,9 @@ class BaseFilterQueryBuilder implements FilterQueryBuilderInterface
      */
     public function applyQuery(QueryBuilder $queryBuilder, ContactSegmentFilter $filter)
     {
+        //var_dump($filter);
         $filterOperator = $filter->getOperator();
         $filterGlue     = $filter->getGlue();
-        $filterAggr     = $filter->getAggregateFunction();
 
         // Check if the column exists in the table
         $filter->getColumn();
@@ -66,53 +66,31 @@ class BaseFilterQueryBuilder implements FilterQueryBuilderInterface
 
         $filterParametersHolder = $filter->getParameterHolder($parameters);
 
-        $tableAlias = $queryBuilder->getTableAlias($filter->getTable());
-
-        // for aggregate function we need to create new alias and not reuse the old one
-        if ($filterAggr) {
-            $tableAlias = false;
-        }
-
-        if (!$tableAlias) {
-            $tableAlias = $this->generateRandomParameterName();
-
-            if ($filter->getTable() == MAUTIC_TABLE_PREFIX.'companies') {
-                $relTable = $this->generateRandomParameterName();
-                $queryBuilder->leftJoin('l', MAUTIC_TABLE_PREFIX.'companies_leads', $relTable, $relTable.'.lead_id = l.id');
-                $queryBuilder->leftJoin($relTable, $filter->getTable(), $tableAlias, $tableAlias.'.id = '.$relTable.'.company_id');
-            } else {
-                $queryBuilder->leftJoin(
-                        $queryBuilder->getTableAlias(MAUTIC_TABLE_PREFIX.'leads'),
-                        $filter->getTable(),
-                        $tableAlias,
-                        sprintf('%s.id = %s.lead_id', $queryBuilder->getTableAlias(MAUTIC_TABLE_PREFIX.'leads'), $tableAlias)
-                    );
-            }
-        }
+        var_dump($filterOperator);
 
         switch ($filterOperator) {
             case 'empty':
                 $expression = new CompositeExpression(CompositeExpression::TYPE_OR,
                     [
-                        $queryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField()),
-                        $queryBuilder->expr()->eq($tableAlias.'.'.$filter->getField(), $queryBuilder->expr()->literal('')),
+                        $queryBuilder->expr()->isNull('l.'.$filter->getField()),
+                        $queryBuilder->expr()->eq('l.'.$filter->getField(), $queryBuilder->expr()->literal('')),
                     ]
                 );
                 break;
             case 'notEmpty':
                 $expression = new CompositeExpression(CompositeExpression::TYPE_AND,
                     [
-                        $queryBuilder->expr()->isNotNull($tableAlias.'.'.$filter->getField()),
-                        $queryBuilder->expr()->neq($tableAlias.'.'.$filter->getField(), $queryBuilder->expr()->literal('')),
+                        $queryBuilder->expr()->isNotNull('l.'.$filter->getField()),
+                        $queryBuilder->expr()->neq('l.'.$filter->getField(), $queryBuilder->expr()->literal('')),
                     ]
                 );
 
                 break;
             case 'neq':
                 $expression = $queryBuilder->expr()->orX(
-                    $queryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField()),
+                    $queryBuilder->expr()->isNull('l.'.$filter->getField()),
                     $queryBuilder->expr()->$filterOperator(
-                        $tableAlias.'.'.$filter->getField(),
+                        'l.'.$filter->getField(),
                         $filterParametersHolder
                     )
                 );
@@ -130,7 +108,7 @@ class BaseFilterQueryBuilder implements FilterQueryBuilderInterface
             case 'regexp':
             case 'notRegexp': //Different behaviour from 'notLike' because of BC (do not use condition for NULL). Could be changed in Mautic 3.
                 $expression = $queryBuilder->expr()->$filterOperator(
-                    $tableAlias.'.'.$filter->getField(),
+                    'l.'.$filter->getField(),
                     $filterParametersHolder
                 );
                 break;
@@ -138,8 +116,8 @@ class BaseFilterQueryBuilder implements FilterQueryBuilderInterface
             case 'notBetween': //Used only for date with week combination (NOT EQUAL [this week, next week, last week])
             case 'notIn':
                 $expression = $queryBuilder->expr()->orX(
-                    $queryBuilder->expr()->$filterOperator($tableAlias.'.'.$filter->getField(), $filterParametersHolder),
-                    $queryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField())
+                    $queryBuilder->expr()->$filterOperator('l.'.$filter->getField(), $filterParametersHolder),
+                    $queryBuilder->expr()->isNull('l.'.$filter->getField())
                 );
                 break;
             case 'multiselect':
@@ -147,17 +125,13 @@ class BaseFilterQueryBuilder implements FilterQueryBuilderInterface
                 $operator    = $filterOperator === 'multiselect' ? 'regexp' : 'notRegexp';
                 $expressions = [];
                 foreach ($filterParametersHolder as $parameter) {
-                    $expressions[] = $queryBuilder->expr()->$operator($tableAlias.'.'.$filter->getField(), $parameter);
+                    $expressions[] = $queryBuilder->expr()->$operator('l.'.$filter->getField(), $parameter);
                 }
 
                 $expression = $queryBuilder->expr()->andX($expressions);
                 break;
             default:
                 throw new \Exception('Dunno how to handle operator "'.$filterOperator.'"');
-        }
-
-        if ($queryBuilder->isJoinTable($filter->getTable())) {
-            $queryBuilder->addJoinCondition($tableAlias, ' ('.$expression.')');
         }
 
         $queryBuilder->addLogic($expression, $filterGlue);
