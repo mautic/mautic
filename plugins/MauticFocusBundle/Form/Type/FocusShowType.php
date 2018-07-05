2459154600355b8f2d92c546624be003d09e21b0
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-07-05 16:26:16 +0200

    Documentation update

--- plugins/MauticFocusBundle/Form/Type/FocusShowType.php
+++ plugins/MauticFocusBundle/Form/Type/FocusShowType.php
@@ -59,7 +59,7 @@ class FocusShowType extends AbstractType
                         ['message' => 'mautic.focus.choosefocus.notblank']
                     ),
                 ],
-                'data' => $options['data']['properties']['focus'],
+                'data' => $options['data']['focus'],
             ]
         );
 
