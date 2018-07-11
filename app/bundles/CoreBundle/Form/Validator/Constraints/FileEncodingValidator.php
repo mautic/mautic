ea69a8d2c0155c033168593d37e596c235c19d1e
mitresh95 <mitreshpandya95@gmail.com>
2018-07-11 16:06:16 -0400

    Adding the code that will detect the encoding of the uploaded CSV file and prevent the upload if file is not in UTF-8

---
+++ app/bundles/CoreBundle/Form/Validator/Constraints/FileEncodingValidator.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\Form\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Throws an exception if the field alias is equal some segment filter keyword.
+ * It would cause odd behavior with segment filters otherwise.
+ */
+class FileEncodingValidator extends ConstraintValidator
+{
+    /**
+     * @param LeadField  $field
+     * @param Constraint $constraint
+     */
+    public function validate($field, Constraint $constraint)
+    {
+        if (!mb_check_encoding(file_get_contents($field->getPathname()), 'UTF-8')) {
+            $this->context->addViolation($constraint->encodingFormatMessage, ['%keyword%' => $field->getClientOriginalName()]);
+        }
+    }
+}
