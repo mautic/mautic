3e0de52721dbf38fdd13fb483623ee4f7f3c6a15
mitresh95 <mitreshpandya95@gmail.com>
2018-07-11 17:30:22 -0400

    Changing from nested if to separate ifs to make it more efficient

--- app/bundles/CoreBundle/Form/Validator/Constraints/FileEncodingValidator.php
+++ app/bundles/CoreBundle/Form/Validator/Constraints/FileEncodingValidator.php
@@ -26,10 +26,20 @@ class FileEncodingValidator extends ConstraintValidator
      */
     public function validate($field, Constraint $constraint)
     {
-        if ($field->getPathname() != null) {
-            if (!mb_check_encoding(file_get_contents($field->getPathname()), 'UTF-8')) {
-                $this->context->addViolation($constraint->encodingFormatMessage, ['%keyword%' => $field->getClientOriginalName()]);
-            }
+        /*
+            If the file uploaded exceeds the max size, it will not be considered,
+            and the file path will be an empty string "". If that is the case
+            no further checks are required. Just return.
+        */
+        if ($field->getPathname() === '' || $field->getPathname() === null) {
+            return;
+        }
+
+        /*
+            If file is below the max size then only check for UTF-8 encoding.
+        */
+        if (!mb_check_encoding(file_get_contents($field->getPathname()), 'UTF-8')) {
+            $this->context->addViolation($constraint->encodingFormatMessage, ['%keyword%' => $field->getClientOriginalName()]);
         }
     }
 }
