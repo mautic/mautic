3618ef49a6a4cc0de5e4096631162b4c6d6c4d5b
Alan Hartless <alan@devkardia.com>
2018-07-12 16:15:14 -0500

    Removed join with redirect as it prevented some results from showing when using correct/strict SQL required by default for MySQL 5.7. Fixed company filter that failed to include results. Fixed segment issue that skewed results if the contact was in more than one segment.

--- app/bundles/PageBundle/Entity/RedirectRepository.php
+++ app/bundles/PageBundle/Entity/RedirectRepository.php
@@ -111,15 +111,18 @@ class RedirectRepository extends CommonRepository
         $campaignId = null,
         $segmentId = null
     ) {
-        $q = $this->_em->getConnection()->createQueryBuilder();
+        $q = $this->getEntityManager()->getConnection()->createQueryBuilder();
         $q->addSelect('ph.url')
             ->addSelect('count(ph.id) as hits')
             ->addSelect('count(distinct ph.tracking_id) as unique_hits')
-            ->from(MAUTIC_TABLE_PREFIX.'page_redirects', 'pr')
-            ->leftJoin('pr', MAUTIC_TABLE_PREFIX.'page_hits', 'ph', 'pr.redirect_id = ph.redirect_id')
-            ->leftJoin('ph', MAUTIC_TABLE_PREFIX.'emails', 'e', 'ph.email_id = e.id')
+            ->from(MAUTIC_TABLE_PREFIX.'page_hits', 'ph')
+            ->join('ph', MAUTIC_TABLE_PREFIX.'email_stats', 'es', 'ph.source = "email" and ph.source_id = es.email_id and ph.lead_id = es.lead_id')
+            ->join('es', MAUTIC_TABLE_PREFIX.'emails', 'e', 'es.email_id = e.id')
             ->addSelect('e.id AS email_id')
             ->addSelect('e.name AS email_name');
+
+        // Group by the page hit URL instead of redirect ID because the redirect could be a token
+        $q->groupBy('ph.url, e.id, e.name, campaign.id, campaign.name');
 
         if ($createdByUserId !== null) {
             $q->andWhere('e.created_by = :userId')
@@ -131,12 +134,24 @@ class RedirectRepository extends CommonRepository
             ->setParameter('dateTo', $dateTo->format('Y-m-d H:i:s'));
 
         if ($companyId !== null) {
-            $q->leftJoin('ph', MAUTIC_TABLE_PREFIX.'companies_leads', 'cl', 'ph.lead_id = cl.lead_id')
-                ->andWhere('cl.company_id = :companyId')
+            $sb = $this->getEntityManager()->getConnection()->createQueryBuilder();
+
+            $sb->select('null')
+                ->from(MAUTIC_TABLE_PREFIX.'companies_leads', 'cl')
+                ->where(
+                    $sb->expr()->andX(
+                        $sb->expr()->eq('cl.company_id', ':companyId'),
+                        $sb->expr()->eq('cl.lead_id', 'ph.lead_id')
+                    )
+                );
+
+            $q->andWhere(
+                sprintf('EXISTS (%s)', $sb->getSQL())
+            )
                 ->setParameter('companyId', $companyId);
         }
 
-        $q->leftJoin('ph', MAUTIC_TABLE_PREFIX.'campaign_events', 'ce', 'ph.source_id = ce.id AND ph.source = "campaign.event"')
+        $q->leftJoin('es', MAUTIC_TABLE_PREFIX.'campaign_events', 'ce', 'es.source = "campaign.event" and es.source_id = ce.id')
             ->leftJoin('ce', MAUTIC_TABLE_PREFIX.'campaigns', 'campaign', 'ce.campaign_id = campaign.id')
             ->addSelect('campaign.id AS campaign_id')
             ->addSelect('campaign.name AS campaign_name');
@@ -146,20 +161,28 @@ class RedirectRepository extends CommonRepository
                 ->setParameter('campaignId', $campaignId);
         }
 
-        $q->leftJoin('ph', MAUTIC_TABLE_PREFIX.'lead_lists_leads', 'lll', 'ph.lead_id = lll.lead_id')
-            ->leftJoin('lll', MAUTIC_TABLE_PREFIX.'lead_lists', 'll', 'lll.leadlist_id = ll.id')
-            ->addSelect('ll.id AS segment_id')
-            ->addSelect('ll.name AS segment_name');
-
         if ($segmentId !== null) {
+            $q->leftJoin('ph', MAUTIC_TABLE_PREFIX.'lead_lists_leads', 'lll', 'ph.lead_id = lll.lead_id')
+                ->leftJoin('lll', MAUTIC_TABLE_PREFIX.'lead_lists', 'll', 'lll.leadlist_id = ll.id')
+                ->addSelect('ll.id AS segment_id')
+                ->addSelect('ll.name AS segment_name');
+
+            // Prevent strict errors in MySQL 5.7
+            $q->addGroupBy('ll.id, ll.name');
+
             $q->andWhere('lll.leadlist_id = :segmentId')
                 ->setParameter('segmentId', $segmentId);
+        } else {
+            // Due to many-to-many relationship between contact and segment, we cannot include segments unless filtering by segment or else
+            // each segment a contact is in will result in a click leading to confusing results. Imagine there is a click from a contact that is in
+            // one segment and a click from a contact that is in the same plus another. The results will show 1 click for the one segment and
+            // 2 clicks for the second. But there were only two clicks total and so there's no way to determine actual number of clicks.
+            // Since these results are not time aware (and thus not usable in a line graph), we should not include duplicate click counts.
+            $q->addSelect('null as segment_id, null as segment_name');
         }
 
         $q->setMaxResults($limit);
 
-        // Group by the page hit URL instead of redirect ID because the redirect could be a token
-        $q->groupBy('ph.url');
         $q->orderBy('hits', 'DESC');
 
         return $q->execute()->fetchAll();
