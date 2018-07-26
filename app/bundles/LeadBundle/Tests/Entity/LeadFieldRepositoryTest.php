a418432a8a784c2123fcef8ba8ce495af8771063
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-07-26 08:39:54 +0200

    Change to tests

--- app/bundles/LeadBundle/Tests/Entity/LeadFieldRepositoryTest.php
+++ app/bundles/LeadBundle/Tests/Entity/LeadFieldRepositoryTest.php
@@ -11,27 +11,76 @@
 
 namespace Mautic\LeadBundle\Tests\Entity;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\Expression\ExpressionBuilder;
 use Doctrine\DBAL\Query\QueryBuilder;
-use Mautic\CoreBundle\Test\MauticWebTestCase;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Mautic\EmailBundle\Entity\EmailRepository;
+use Mautic\LeadBundle\Entity\LeadFieldRepository;
 
-class LeadFieldRepositoryTest extends MauticWebTestCase
+class LeadFieldRepositoryTest extends \PHPUnit_Framework_TestCase
 {
+    private $mockConnection;
+    private $em;
+    private $cm;
+
+    /**
+     * @var EmailRepository
+     */
+    private $repo;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        defined('MAUTIC_TABLE_PREFIX') or define('MAUTIC_TABLE_PREFIX', '');
+
+        $this->mockConnection = $this->createMock(Connection::class);
+        $this->em             = $this->createMock(EntityManager::class);
+        $this->cm             = $this->createMock(ClassMetadata::class);
+        $this->repo           = new LeadFieldRepository($this->em, $this->cm);
+
+        $this->mockConnection->method('createQueryBuilder')
+            ->willReturnCallback(
+                function () {
+                    return new QueryBuilder($this->mockConnection);
+                }
+            );
+
+        $this->mockConnection->method('getExpressionBuilder')
+            ->willReturnCallback(
+                function () {
+                    return new ExpressionBuilder($this->mockConnection);
+                }
+            );
+
+        $this->mockConnection->method('quote')
+            ->willReturnCallback(
+                function ($value) {
+                    return "'$value'";
+                }
+            );
+
+        $this->em->method('getConnection')
+            ->willReturn($this->mockConnection);
+    }
+
     public function testGetPropertyByFieldForLead()
     {
         $leadFieldRepo    = $this->em->getRepository('MauticLeadBundle:LeadField');
         $queryBuilderMock = $this->createMock(QueryBuilder::class);
         $queryBuilderMock->expects($this->any())->method('join')->willReturn('');
-        $this->assertEquals('l.email', $leadFieldRepo->getPropertyByField('email', $queryBuilderMock));
+        $this->assertEquals('l.email', $this->repo->getPropertyByField('email', $queryBuilderMock));
     }
 
     public function testGetPropertyByFieldForCompany()
     {
-        $leadFieldRepo    = $this->em->getRepository('MauticLeadBundle:LeadField');
         $queryBuilderMock = $this->createMock(QueryBuilder::class);
         $queryBuilderMock->expects($this->any())->method('join')->willReturn('');
         $this->assertEquals(
             'company.companyemail',
-            $leadFieldRepo->getPropertyByField('company.companyemail', $queryBuilderMock)
+            $this->repo->getPropertyByField('company.companyemail', $queryBuilderMock)
         );
     }
 }
