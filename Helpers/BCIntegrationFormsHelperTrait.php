17169f41ff8170804fd8307b62d82c76ac08aec6
Petr Fidler <info@maxell-cz.cz>
2018-07-31 16:27:21 +0200

    Change signature of appendToForm function

--- Helpers/BCIntegrationFormsHelperTrait.php
+++ Helpers/BCIntegrationFormsHelperTrait.php
@@ -187,7 +187,7 @@ trait BCIntegrationFormsHelperTrait
      *                                   integration is called when adding an integration to events like point triggers,
      *                                   campaigns actions, forms actions, etc
      */
-    public function appendToForm(&$builder, $data, $formArea)
+    public function appendToForm(FormBuilder $builder, array $data, string $formArea)
     {
     }
 
