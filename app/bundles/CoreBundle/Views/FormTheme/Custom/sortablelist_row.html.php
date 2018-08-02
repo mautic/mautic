80054c17e04b1041942db8614c44099f421778cf
Enc3phale <bahaalfr@gmail.com>
2018-08-02 10:38:18 +0200

    Fix repopulating collection field values when form contains errors

--- app/bundles/CoreBundle/Views/FormTheme/Custom/sortablelist_row.html.php
+++ app/bundles/CoreBundle/Views/FormTheme/Custom/sortablelist_row.html.php
@@ -1,16 +1,17 @@
 <?php
 $list            = $form->children['list'];
 $parentHasErrors = $view['form']->containsErrors($form->parent);
-if ($parentHasErrors && empty($list->vars['value']) && isset($form->parent->children['properties']['list'])) {
+$hasErrors       = count($list->vars['errors']);
+
+if ($parentHasErrors && $hasErrors && empty($list->vars['value']) && isset($form->parent->children['properties']['list'])) {
     // Work around for Symfony bug not repopulating values
     $list = $form->parent->children['properties']['list'];
 }
 
-$hasErrors     = count($list->vars['errors']);
 $feedbackClass = (!empty($hasErrors)) ? ' has-error' : '';
 $datePrototype = (isset($list->vars['prototype'])) ?
     $view->escape('<div class="sortable">'.$view['form']->widget($list->vars['prototype']).'</div>') : '';
-$feedbackClass = (!empty($hasErrors)) ? ' has-error' : '';
+
 ?>
 <div class="row">
     <div data-toggle="sortablelist" data-prefix="<?php echo $form->vars['id']; ?>" class="form-group col-xs-12 <?php echo $feedbackClass; ?>" id="<?php echo $form->vars['id']; ?>_list" style="overflow:auto">
@@ -19,6 +20,7 @@ $feedbackClass = (!empty($hasErrors)) ? ' has-error' : '';
            class="btn btn-warning btn-xs btn-add-item" href="#" id="<?php echo $form->vars['id']; ?>_additem">
             <?php echo $view['translator']->trans($addValueButton); ?>
         </a>
+        <?php echo $view['form']->errors($list); ?>
         <?php if ($isSortable): ?>
         <div id="sortable-<?php echo $form->vars['id']; ?>" class="list-sortable" <?php foreach ($attr as $k => $v) {
     printf('%s="%s" ', $view->escape($k), $view->escape($v));
@@ -28,7 +30,6 @@ $feedbackClass = (!empty($hasErrors)) ? ' has-error' : '';
             <?php echo $view['form']->block($item, 'sortablelist_entry_row'); ?>
             <?php endforeach; ?>
         </div>
-        <?php echo $view['form']->errors($list); ?>
         <?php if ($isSortable): ?>
         <input type="hidden" class="sortable-itemcount" id="<?php echo $form->vars['id']; ?>_itemcount" value="<?php echo count($list); ?>" />
         <?php endif; ?>
