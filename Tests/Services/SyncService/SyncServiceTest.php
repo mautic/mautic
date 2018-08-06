82f062b94329804c9007813d953257ce98898006
Alan Hartless <alan@devkardia.com>
2018-08-06 01:35:31 -0500

    Functional test to help write sync execution

--- Tests/Services/SyncService/SyncServiceTest.php
+++ Tests/Services/SyncService/SyncServiceTest.php
@@ -12,7 +12,31 @@
 namespace MauticPlugin\MauticIntegrationsBundle\Tests\Services\SyncService;
 
 
-class SyncServiceTest
-{
+use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use MauticPlugin\MauticIntegrationsBundle\Event\SyncEvent;
+use MauticPlugin\MauticIntegrationsBundle\IntegrationEvents;
+use MauticPlugin\MauticIntegrationsBundle\Services\SyncService\SyncService;
+use MauticPlugin\MauticIntegrationsBundle\Tests\Services\SyncService\TestExamples\EventListener\IntegrationSubscriber;
+use MauticPlugin\MauticIntegrationsBundle\Tests\Services\SyncService\TestExamples\Facade\SyncDataExchange\ExampleSyncDataExchange;
+use MauticPlugin\MauticIntegrationsBundle\Tests\Services\SyncService\TestExamples\Integration\ExampleIntegration;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
+class SyncServiceTest extends MauticMysqlTestCase
+{
+    public function testSync()
+    {
+        /** @var EventDispatcher $dispatcher */
+        $dispatcher = $this->container->get('event_dispatcher');
+
+        $dataExchange = new ExampleSyncDataExchange();
+        $dispatcher->addSubscriber(new IntegrationSubscriber($dataExchange));
+
+        $event = new SyncEvent(ExampleIntegration::NAME, new \DateTimeImmutable());
+        $dispatcher->dispatch(IntegrationEvents::ON_SYNC_TRIGGERED, $event);
+
+        /** @var SyncService $syncService */
+        $syncService = $this->container->get('mautic.integrations.sync.service');
+
+        $syncService->processIntegrationSync($event->getDataExchange(), $event->getMappingManual(), $event->getStartDate());
+    }
 }
