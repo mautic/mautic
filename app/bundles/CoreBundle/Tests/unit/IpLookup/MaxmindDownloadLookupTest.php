f156fcb05464d22d673cc018332bc5029691a92b
Enc3phale <bahaalfr@gmail.com>
2018-08-08 14:52:13 +0200

    Fix unit test GeoLite2 IP address because older doesn't work anymore

--- app/bundles/CoreBundle/Tests/unit/IpLookup/MaxmindDownloadLookupTest.php
+++ app/bundles/CoreBundle/Tests/unit/IpLookup/MaxmindDownloadLookupTest.php
@@ -33,14 +33,14 @@ class MaxmindDownloadLookupTest extends \PHPUnit_Framework_TestCase
         // Keep the file contained to cache/test
         $ipService = new MaxmindDownloadLookup(null, null, __DIR__.'/../../../../../cache/test');
 
-        $details = $ipService->setIpAddress('192.30.252.131')->getDetails();
+        $details = $ipService->setIpAddress('192.30.150.130')->getDetails();
 
-        $this->assertEquals('San Francisco', $details['city']);
-        $this->assertEquals('California', $details['region']);
+        $this->assertEquals('Norwalk', $details['city']);
+        $this->assertEquals('Connecticut', $details['region']);
         $this->assertEquals('United States', $details['country']);
         $this->assertEquals('', $details['zipcode']);
-        $this->assertEquals('37.7697', $details['latitude']);
-        $this->assertEquals('-122.3933', $details['longitude']);
-        $this->assertEquals('America/Los_Angeles', $details['timezone']);
+        $this->assertEquals('41.1003', $details['latitude']);
+        $this->assertEquals('-73.3983', $details['longitude']);
+        $this->assertEquals('America/New_York', $details['timezone']);
     }
 }
