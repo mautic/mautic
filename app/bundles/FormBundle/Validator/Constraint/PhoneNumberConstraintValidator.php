2b1988dbcd51698641c9dd18707bbd0faa0bb32f
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-08-14 14:55:42 +0200

    Modify file header

--- app/bundles/FormBundle/Validator/Constraint/PhoneNumberConstraintValidator.php
+++ app/bundles/FormBundle/Validator/Constraint/PhoneNumberConstraintValidator.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * This file is part of the Symfony2 PhoneNumberBundle.
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic
  *
- * (c) University of Cambridge
+ * @link        http://mautic.org
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 namespace Mautic\FormBundle\Validator\Constraint;
