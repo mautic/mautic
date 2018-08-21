b0c56287ad54ed8841b191a201359748b0b6f419
John Linhart <admin@escope.cz>
2018-08-21 17:04:00 +0200

    A DB index added to speed up webhook queue count query

---
+++ app/migrations/Version20180821144204.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * @package     Mautic
+ * @copyright   2018 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Migrations\SkipMigrationException;
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+
+class Version20180821144204 extends AbstractMauticMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws SkipMigrationException
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function preUp(Schema $schema)
+    {
+        if ($schema->getTable("{$this->prefix}webhook_queue")->hasIndex("{$this->prefix}tmp_id_date")) {
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql("CREATE INDEX {$this->prefix}tmp_id_date ON {$this->prefix}webhook_queue (webhook_id, date_added)");
+    }
+}
