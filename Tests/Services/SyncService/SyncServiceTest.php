b1044656fbe36359db6e64fadd87d47fd58d8d74
Alan Hartless <alan@devkardia.com>
2018-08-25 17:36:15 -0500

    Fixed functional test for syncing

--- Tests/Services/SyncService/SyncServiceTest.php
+++ Tests/Services/SyncService/SyncServiceTest.php
@@ -12,19 +12,31 @@
 namespace MauticPlugin\IntegrationsBundle\Tests\Services\SyncService;
 
 
+use Doctrine\DBAL\Connection;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use MauticPlugin\IntegrationsBundle\Event\SyncEvent;
 use MauticPlugin\IntegrationsBundle\IntegrationEvents;
 use MauticPlugin\IntegrationsBundle\Sync\SyncService\SyncService;
 use MauticPlugin\IntegrationsBundle\Tests\Services\SyncService\TestExamples\EventListener\IntegrationSubscriber;
-use MauticPlugin\IntegrationsBundle\Tests\Services\SyncService\TestExamples\Facade\SyncDataExchange\ExampleSyncDataExchange;
+use MauticPlugin\IntegrationsBundle\Tests\Services\SyncService\TestExamples\Sync\SyncDataExchange\ExampleSyncDataExchange;
 use MauticPlugin\IntegrationsBundle\Tests\Services\SyncService\TestExamples\Integration\ExampleIntegration;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class SyncServiceTest extends MauticMysqlTestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Populate contacts
+        $this->installDatabaseFixtures([dirname(__DIR__).'/../../../../app/bundles/LeadBundle/DataFixtures/ORM/LoadLeadData.php']);
+    }
+
     public function testSync()
     {
+        // Sleep one second to ensure that the modified date/time stamps of the contacts just created are in the past
+        sleep(1);
+
         /** @var EventDispatcher $dispatcher */
         $dispatcher = $this->container->get('event_dispatcher');
 
@@ -38,5 +50,57 @@ class SyncServiceTest extends MauticMysqlTestCase
         $syncService = $this->container->get('mautic.integrations.sync.service');
 
         $syncService->processIntegrationSync($event->getDataExchange(), $event->getMappingManual(), true);
+
+        $payload = $dataExchange->getOrderPayload();
+
+        // Created the 50 known contacts already in Mautic
+        $this->assertCount(50, $payload['create']);
+
+        // Updated the 2 contacts that were already in Mautic
+        $this->assertCount(2, $payload['update']);
+        $this->assertEquals(
+            [
+                4 =>
+                    [
+                        'id'         => 4,
+                        'object'     => 'lead',
+                        'first_name' => 'Lewis',
+                        'last_name'  => 'Syed',
+                        'email'      => 'LewisTSyed@gustr.com',
+                    ],
+                3 =>
+                    [
+                        'id'         => 3,
+                        'object'     => 'lead',
+                        'first_name' => 'Nellie',
+                        'last_name'  => 'Baird',
+                        'email'      => 'NellieABaird@armyspy.com',
+                    ],
+            ],
+            $payload['update']
+        );
+
+        // Validate mapping table
+        /** @var Connection $connection */
+        $prefix = $this->container->getParameter('mautic.db_table_prefix');
+        $connection = $this->container->get('doctrine.dbal.default_connection');
+        $qb = $connection->createQueryBuilder();
+        $results = $qb->select('count(*) as the_count, m.integration_object_name, m.integration')
+            ->from($prefix.'sync_object_mapping', 'm')
+            ->groupBy('m.integration, m.integration_object_name')
+            ->execute()
+            ->fetchAll();
+
+        $this->assertCount(2, $results);
+        foreach ($results as $result) {
+            $this->assertEquals(ExampleIntegration::NAME, $result['integration']);
+            if (ExampleSyncDataExchange::OBJECT_LEAD === $result['integration_object_name']) {
+                // Two leads were created in Mautic from the integration
+                $this->assertEquals(2, $result['the_count']);
+            } else {
+                // Fifty two Mautic contacts were pushed to the integration as the contact object
+                $this->assertEquals(52, $result['the_count']);
+            }
+        }
     }
 }
