9c3d2832ac46306496a322bff05781fb3983974e
Lukas Drahy <lukas@drahy.net>
2018-09-06 12:28:15 +0200

    Add company log unit tests

---
+++ app/bundles/LeadBundle/Tests/EventListener/CompanySubscriberTest.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Mautic\LeadBundle\Tests\EventListener;
+
+use Mautic\CoreBundle\Helper\IpLookupHelper;
+use Mautic\CoreBundle\Model\AuditLogModel;
+use Mautic\LeadBundle\Entity\Company;
+use Mautic\LeadBundle\Event\CompanyEvent;
+use Mautic\LeadBundle\EventListener\CompanySubscriber;
+use Mautic\LeadBundle\LeadEvents;
+
+class CompanySubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetSubscribedEvents()
+    {
+        $ipLookupHelper = $this->getMockBuilder(IpLookupHelper::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $auditLogModel = $this->getMockBuilder(AuditLogModel::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $subscriber = new CompanySubscriber($ipLookupHelper, $auditLogModel);
+
+        $this->assertEquals(
+            [
+                LeadEvents::COMPANY_POST_SAVE   => ['onCompanyPostSave', 0],
+                LeadEvents::COMPANY_POST_DELETE => ['onCompanyDelete', 0],
+            ],
+            $subscriber->getSubscribedEvents()
+        );
+    }
+
+    public function testOnCompanyPostSave()
+    {
+        $this->onCompanyPostSaveMethodCall(false); // update company log
+        $this->onCompanyPostSaveMethodCall(true); // create company log
+    }
+
+    public function testOnCompanyDelete()
+    {
+        $companyId        = 1;
+        $companyName      = 'name';
+        $ip               = '127.0.0.2';
+
+        $log = [
+            'bundle'    => 'lead',
+            'object'    => 'company',
+            'objectId'  => $companyId,
+            'action'    => 'delete',
+            'details'   => ['name', $companyName],
+            'ipAddress' => $ip,
+        ];
+
+        $ipLookupHelper = $this->getMockBuilder(IpLookupHelper::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $ipLookupHelper->expects($this->once())
+            ->method('getIpAddressFromRequest')
+            ->will($this->returnValue($ip));
+
+        $auditLogModel = $this->getMockBuilder(AuditLogModel::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $auditLogModel->expects($this->once())
+            ->method('writeToLog')
+            ->with($log);
+
+        $subscriber = new CompanySubscriber($ipLookupHelper, $auditLogModel);
+
+        $company = $this->getMockBuilder(Company::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $company->deletedId = $companyId;
+
+        $company->expects($this->once())
+            ->method('getPrimaryIdentifier')
+            ->will($this->returnValue($companyName));
+
+        $event = $this->getMockBuilder(CompanyEvent::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $event->expects($this->once())
+            ->method('getCompany')
+            ->will($this->returnValue($company));
+
+        $subscriber->onCompanyDelete($event);
+    }
+
+    /**
+     * Test create or update company logging.
+     *
+     * @param bool $isNew
+     */
+    private function onCompanyPostSaveMethodCall($isNew)
+    {
+        $companyId = 1;
+        $changes   = ['changes'];
+        $ip        = '127.0.0.2';
+
+        $log = [
+            'bundle'    => 'lead',
+            'object'    => 'company',
+            'objectId'  => $companyId,
+            'action'    => ($isNew) ? 'create' : 'update',
+            'details'   => $changes,
+            'ipAddress' => $ip,
+        ];
+
+        $ipLookupHelper = $this->getMockBuilder(IpLookupHelper::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $ipLookupHelper->expects($this->once())
+            ->method('getIpAddressFromRequest')
+            ->will($this->returnValue($ip));
+
+        $auditLogModel = $this->getMockBuilder(AuditLogModel::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $auditLogModel->expects($this->once())
+            ->method('writeToLog')
+            ->with($log);
+
+        $subscriber = new CompanySubscriber($ipLookupHelper, $auditLogModel);
+
+        $company = $this->getMockBuilder(Company::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $company->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue($companyId));
+
+        $event = $this->getMockBuilder(CompanyEvent::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $event->expects($this->once())
+            ->method('getCompany')
+            ->will($this->returnValue($company));
+
+        $event->expects($this->once())
+            ->method('getChanges')
+            ->will($this->returnValue($changes));
+
+        $event->expects($this->once())
+            ->method('isNew')
+            ->will($this->returnValue($isNew));
+
+        $subscriber->onCompanyPostSave($event);
+    }
+}
