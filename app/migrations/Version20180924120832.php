3ab1afd6ce9dd8213d398653b6f0b4cfde7854d3
Petr Gregor <gregy@gregy.cz>
2018-09-24 13:39:47 +0200

    Remove redundant index (first part of different compound index) and create new prefix page_hit url index

---
+++ app/migrations/Version20180924120832.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * @package     Mautic
+ * @copyright   2018 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Migrations\SkipMigrationException;
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+
+class Version20180924120832 extends AbstractMauticMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws SkipMigrationException
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function preUp(Schema $schema)
+    {
+        $table = $schema->getTable("{$this->prefix}page_hits");
+        if (!$table->hasIndex("{$this->prefix}page_date_hit")) {
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql("DROP INDEX {$this->prefix}page_date_hit ON {$this->prefix}page_hits");
+    }
+}
