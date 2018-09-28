f014c2c1954dfdf6f3cf990cefcb0f679576a97c
Max Lawton <max@mauldineconomics.com>
2018-09-28 14:14:07 -0400

    Define get_mail_message event name and GetMailMessageEvent class

---
+++ app/bundles/EmailBundle/Swiftmailer/SendGrid/Event/GetMailMessageEvent.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\EmailBundle\Swiftmailer\SendGrid\Event;
+
+use SendGrid\Mail;
+use Swift_Mime_Message;
+use Symfony\Component\EventDispatcher\Event;
+
+class GetMailMessageEvent extends Event
+{
+    /** @var Mail */
+    private $mail;
+
+    /** @var Swift_Mime_Message */
+    private $message;
+
+    /**
+     * Constructor.
+     *
+     * @param Mail $mail
+     * @param Swift_Mime_Message $message
+     */
+    public function __construct(Mail $mail, Swift_Mime_Message $message)
+    {
+        $this->mail    = $mail;
+        $this->message = $message;
+    }
+
+    /**
+     * Get mail.
+     *
+     * @return Mail
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Get message.
+     *
+     * @return Swift_Mime_Message
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
