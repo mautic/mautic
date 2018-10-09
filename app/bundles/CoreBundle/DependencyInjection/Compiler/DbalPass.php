4e436b7466bd9b8bd26531acbc9c6dc9bfac8009
heathdutton <hdutton@thedmsgrp.com>
2018-10-09 18:07:21 -0400

    Support "Campaign by date range" feature
    This merges in and refactors #6021 and includes some basic query
    caching for those who enable APCu or similar at the ORM level.

---
+++ app/bundles/CoreBundle/DependencyInjection/Compiler/DbalPass.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Class DbalPass.
+ */
+class DbalPass implements CompilerPassInterface
+{
+    /**
+     * Allows result caching with DBAL using the same configuration as the orm if provided and enabled.
+     *
+     * See config_prod.php
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $id = 'doctrine.dbal.default_connection.configuration';
+
+        if ($container->hasDefinition($id)) {
+            $container
+                ->getDefinition($id)
+                ->addMethodCall('setResultCacheImpl', [new Reference('doctrine.orm.default_result_cache')]);
+        }
+    }
+}
