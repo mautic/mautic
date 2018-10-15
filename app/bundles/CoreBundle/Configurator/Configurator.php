c219cd8f5b15872a7fc054c9cb7fb6b72bb006bc
Woeler <woeler@woeler.eu>
2018-10-15 08:40:51 +0200

    Change double quotes to single quotes

--- app/bundles/CoreBundle/Configurator/Configurator.php
+++ app/bundles/CoreBundle/Configurator/Configurator.php
@@ -257,13 +257,13 @@ class Configurator
                 if ($counter === $count) {
                     $string .= str_repeat("\t", $level + 1);
                 }
-                $string .= "'".$key."' => ";
+                $string .= '\''.$key.'\' => ';
             }
 
             if (is_array($value)) {
                 $string .= $this->renderArray($value, $level + 1);
             } else {
-                $string .= "'".addcslashes($value, '\\\'')."'";
+                $string .= '\''.addcslashes($value, '\\\'').'\'';
             }
 
             --$counter;
