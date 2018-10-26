1742f8444cb2d5e6f63efd3acbe5729f4b1d1272
scottshipman <k.scott.shipman@gmail.com>
2018-10-26 14:35:30 -0400

    clean up scheduledExecutioner spacing

--- app/bundles/CampaignBundle/Executioner/ScheduledExecutioner.php
+++ app/bundles/CampaignBundle/Executioner/ScheduledExecutioner.php
@@ -138,10 +138,10 @@ class ScheduledExecutioner implements ExecutionerInterface
      */
     public function execute(Campaign $campaign, ContactLimiter $limiter, OutputInterface $output = null)
     {
-        $this->campaign = $campaign;
-        $this->limiter  = $limiter;
-        $this->output   = ($output) ? $output : new NullOutput();
-        $this->counter  = new Counter();
+        $this->campaign   = $campaign;
+        $this->limiter    = $limiter;
+        $this->output     = ($output) ? $output : new NullOutput();
+        $this->counter    = new Counter();
 
         $this->logger->debug('CAMPAIGN: Triggering scheduled events');
 
