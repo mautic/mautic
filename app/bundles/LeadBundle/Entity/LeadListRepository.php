a9a3750eebc98ebc1bdd91a4a06dfa54321f1b9a
Anton Vlasenko <anton.vlasenko@mautic.com>
2018-11-01 17:07:47 +0100

    Edited a translation string to remove search commands we don't use anymore (on segments page): is:active, is:inactive, is:mine. Removed is:mine command from the query parse to prevent it from being parsed (because we don't use is:mine request anyway).

--- app/bundles/LeadBundle/Entity/LeadListRepository.php
+++ app/bundles/LeadBundle/Entity/LeadListRepository.php
@@ -1990,10 +1990,6 @@ class LeadListRepository extends CommonRepository
         $returnParameter = false; //returning a parameter that is not used will lead to a Doctrine error
 
         switch ($command) {
-            case $this->translator->trans('mautic.core.searchcommand.ismine'):
-            case $this->translator->trans('mautic.core.searchcommand.ismine', [], null, 'en_US'):
-                $expr = $q->expr()->eq('l.createdBy', $this->currentUser->getId());
-                break;
             case $this->translator->trans('mautic.lead.list.searchcommand.isglobal'):
             case $this->translator->trans('mautic.lead.list.searchcommand.isglobal', [], null, 'en_US'):
                 $expr            = $q->expr()->eq('l.isGlobal', ":$unique");
