5ce1145b1ec94a1197812cba28550838b24507fb
Lukas Drahy <lukas@drahy.net>
2018-11-06 11:56:58 +0100

    Add AuthorizatorInterface

---
+++ Auth/Provider/Oauth2ThreeLegged/AuthorizatorInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\IntegrationsBundle\Auth\Provider\Oauth2ThreeLegged;
+
+use Symfony\Component\HttpFoundation\Request;
+
+interface AuthorizatorInterface
+{
+    /**
+     * @return bool
+     */
+    public function isAuthorized(): bool;
+
+    /**
+     * @param CredentialsInterface $credentials
+     *
+     * @return string
+     */
+    public function getAuthorizationUri(CredentialsInterface $credentials): string;
+
+    /**
+     * @param Request $request
+     */
+    public function handleCallback(Request $request): void;
+}
