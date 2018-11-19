3471e4fe73e0d5805ee420e63e472ace41e9922e
Alan Hartless <alan@devkardia.com>
2018-11-19 08:13:11 -0600

    Force router's context to be what is configured for site_url

---
+++ app/bundles/CoreBundle/EventListener/RouterSubscriber.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\RouterInterface;
+
+class RouterSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var string|null
+     */
+    private $baseUrl;
+
+    /**
+     * @var string|null
+     */
+    private $scheme;
+
+    /**
+     * @var string|null
+     */
+    private $host;
+
+    /**
+     * @var string|null
+     */
+    private $httpsPort;
+
+    /**
+     * @var string|null
+     */
+    private $httpPort;
+
+    /**
+     * RouterSubscriber constructor.
+     *
+     * @param RouterInterface $router
+     * @param null|string     $scheme
+     * @param null|string     $host
+     * @param null|string     $httpsPort
+     * @param null|string     $httpPort
+     * @param null|string     $baseUrl
+     */
+    public function __construct(RouterInterface $router, $scheme, $host, $httpsPort, $httpPort, $baseUrl)
+    {
+        $this->router    = $router;
+        $this->scheme    = $scheme;
+        $this->host      = $host;
+        $this->httpsPort = $httpsPort;
+        $this->httpPort  = $httpPort;
+        $this->baseUrl   = $baseUrl;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => ['setRouterRequestContext', 0],
+        ];
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function setRouterRequestContext(GetResponseEvent $event)
+    {
+        if (empty($this->host)) {
+            return;
+        }
+
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        if ('dev' === MAUTIC_ENV) {
+            $this->baseUrl = '/index_dev.php'.$this->baseUrl;
+        }
+
+        $context = $this->router->getContext();
+        $context->setBaseUrl($this->baseUrl);
+        $context->setScheme($this->scheme);
+        $context->setHost($this->host);
+        $context->setHttpPort($this->httpPort);
+        $context->setHttpsPort($this->httpsPort);
+    }
+}
