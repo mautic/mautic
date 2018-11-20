043fd3fe4d4554437d41b298b2719283b01db4ff
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-11-20 12:00:01 +0100

    Change logic of conditions

--- app/bundles/FormBundle/Entity/SubmissionRepository.php
+++ app/bundles/FormBundle/Entity/SubmissionRepository.php
@@ -457,6 +457,10 @@ class SubmissionRepository extends CommonRepository
     {
         // Modify operator
         switch ($operatorExpr) {
+            case 'like':
+            case 'notLike':
+                $value = strpos($value, '%') === false ? '%'.$value.'%' : $value;
+                break;
             case 'startsWith':
                 $operatorExpr    = 'like';
                 $value           = $value.'%';
@@ -468,10 +472,6 @@ class SubmissionRepository extends CommonRepository
             case 'contains':
                 $operatorExpr   = 'like';
                 $value          = '%'.$value.'%';
-                break;
-            case 'like':
-            case 'notLike':
-                $value = strpos($value, '%') === false ? '%'.$value.'%' : $value;
                 break;
         }
 
