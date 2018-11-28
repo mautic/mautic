d5acb28d9f09681e514407f1453017efe2118c2a
luke <develooped14@gmail.com>
2018-11-28 21:43:40 +0100

    Field system Mysql8 compatibility

--- app/bundles/ReportBundle/Entity/Report.php
+++ app/bundles/ReportBundle/Entity/Report.php
@@ -133,7 +133,6 @@ class Report extends FormEntity implements SchedulerInterface
 
         $builder->addIdColumns();
 
-        #$builder->addField('system', Type::BOOLEAN, array("columnName"=>"`system`"));
         $builder->addField('system', Type::BOOLEAN, array("columnName"=>""));
 
         $builder->addField('source', Type::STRING);
