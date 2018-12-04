d16dc4864c2f7c340fd4f30cdae464b7c2435083
Enc3phale <bahaalfr@gmail.com>
2018-12-04 17:50:49 +0100

    Add migration schema for PR #6240

---
+++ app/migrations/Version20181204000000.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * @package     Mautic
+ * @copyright   2018 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+
+class Version20181204000000 extends AbstractMauticMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $roles = $this->connection->createQueryBuilder()
+            ->select('r.*')
+            ->from(MAUTIC_TABLE_PREFIX.'roles', 'r')
+            ->execute()
+            ->fetchAll();
+
+        foreach ($roles as $role) {
+            $unserializedArray = unserialize($role["readable_permissions"]);
+
+            if ($unserializedArray !== NULL && isset($unserializedArray["campaign:campaigns"])) {
+                $newPermissions = [];
+
+                if (in_array('view', $unserializedArray["campaign:campaigns"])) {
+                    $newPermissions[] = 'viewown';
+                    $newPermissions[] = 'viewother';
+                }
+
+                if (in_array('edit', $unserializedArray["campaign:campaigns"])) {
+                    $newPermissions[] = 'editown';
+                    $newPermissions[] = 'editother';
+                }
+
+                if (in_array('create', $unserializedArray["campaign:campaigns"])) {
+                    $newPermissions[] = 'create';
+                }
+
+                if (in_array('delete', $unserializedArray["campaign:campaigns"])) {
+                    $newPermissions[] = 'deleteown';
+                    $newPermissions[] = 'deleteother';
+                }
+
+                if (in_array('publish', $unserializedArray["campaign:campaigns"])) {
+                    $newPermissions[] = 'publishown';
+                    $newPermissions[] = 'publishother';
+                }
+
+                $unserializedArray["campaign:campaigns"] = $newPermissions;
+                $serializedArray = serialize($unserializedArray);
+
+                $this->connection->update(MAUTIC_TABLE_PREFIX.'roles',
+                    [
+                        'readable_permissions'          => $serializedArray,
+                    ],
+                    [
+                        'id' => $role['id'],
+                    ]
+                );
+            }
+        }
+    }
+}
