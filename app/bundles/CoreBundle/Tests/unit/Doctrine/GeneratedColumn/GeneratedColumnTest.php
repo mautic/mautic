987c673f5a875841bdf1b2cb4eaeda2d4f7a1291
John Linhart <admin@escope.cz>
2018-12-04 10:03:49 +0100

    Tests fixed after the query modification

--- app/bundles/CoreBundle/Tests/unit/Doctrine/GeneratedColumn/GeneratedColumnTest.php
+++ app/bundles/CoreBundle/Tests/unit/Doctrine/GeneratedColumn/GeneratedColumnTest.php
@@ -24,9 +24,8 @@ class GeneratedColumnTest extends \PHPUnit_Framework_TestCase
         $generatedColumn->setOriginalDateColumn('date_hit', 'd');
 
         $expectedColumnDefinition = "DATE AS (CONCAT(YEAR(date_hit), \"-\", LPAD(MONTH(date_hit), 2, \"0\"), \"-\", LPAD(DAY(date_hit), 2, \"0\"))) COMMENT '(DC2Type:generated)'";
-        $expectedAlterQuery       = "ALTER TABLE page_hits
-            ADD generated_hit_date DATE AS (CONCAT(YEAR(date_hit), \"-\", LPAD(MONTH(date_hit), 2, \"0\"), \"-\", LPAD(DAY(date_hit), 2, \"0\"))) COMMENT '(DC2Type:generated)',
-            ADD INDEX `generated_hit_date_page_id`(generated_hit_date, page_id)";
+        $expectedAlterQuery       = "ALTER TABLE page_hits ADD generated_hit_date DATE AS (CONCAT(YEAR(date_hit), \"-\", LPAD(MONTH(date_hit), 2, \"0\"), \"-\", LPAD(DAY(date_hit), 2, \"0\"))) COMMENT '(DC2Type:generated)';
+            ALTER TABLE page_hits ADD INDEX `generated_hit_date_page_id`(generated_hit_date, page_id)";
 
         $this->assertSame($expectedAlterQuery, $generatedColumn->getAlterTableSql());
         $this->assertSame($expectedColumnDefinition, $generatedColumn->getColumnDefinition());
