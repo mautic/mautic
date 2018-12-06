7d4123437acf5d3ea11b54d9c782cc04587594f1
Alan Hartless <alan@devkardia.com>
2018-12-06 17:10:43 -0600

    Created an object to store stats

---
+++ app/bundles/StatsBundle/Aggregate/Collection/StatCollection.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\StatsBundle\Aggregate\Collection;
+
+use Mautic\StatsBundle\Aggregate\Collection\DAO\StatsDAO;
+
+class StatCollection
+{
+    /**
+     * @var string
+     */
+    private $statName;
+
+    /**
+     * @var int|null
+     */
+    private $itemId;
+
+    /**
+     * @var StatsDAO
+     */
+    private $stats;
+
+    /**
+     * StatCollection constructor.
+     *
+     * @param string   $statName
+     * @param int|null $itemId
+     */
+    public function __construct($statName, $itemId = null)
+    {
+        $this->statName = $statName;
+        $this->itemId   = $itemId;
+        $this->stats    = new StatsDAO();
+    }
+
+    /**
+     * Add a stat by datetime components represented in UTC.
+     *
+     * @param int $year
+     * @param int $month
+     * @param int $day
+     * @param int $hour
+     * @param int $count
+     *
+     * @return $this
+     */
+    public function addStat($year, $month, $day, $hour, $count)
+    {
+        $this->stats
+            ->getYear($year)
+            ->getMonth($month)
+            ->getDay($day)
+            ->getHour($hour)
+            ->setCount($count);
+
+        return $this;
+    }
+
+    /**
+     * @param \DateTime $dateTime
+     * @param int       $count
+     *
+     * @return $this
+     */
+    public function addStatByDateTime(\DateTime $dateTime, $count)
+    {
+        $dateTime->setTimezone(new \DateTimeZone('UTC'));
+
+        $this->addStat(
+            $dateTime->format('Y'),
+            $dateTime->format('m'),
+            $dateTime->format('d'),
+            $dateTime->format('h'),
+            $count
+        );
+
+        return $this;
+    }
+
+    /**
+     * @return StatsDAO
+     */
+    public function getStats()
+    {
+        return $this->stats;
+    }
+}
