247342c202409b24050d7f466aaf5d246d075198
Alan Hartless <alan@devkardia.com>
2018-12-08 00:21:18 -0600

    Add config file

--- app/bundles/StatsBundle/Config/config.php
+++ app/bundles/StatsBundle/Config/config.php
@@ -11,5 +11,13 @@
 
 return [
     'services' => [
+        'other' => [
+            'mautic.stats.aggregate.collector' => [
+                'class'     => \Mautic\StatsBundle\Aggregate\Collector::class,
+                'arguments' => [
+                    'event_dispatcher',
+                ],
+            ],
+        ],
     ],
 ];
