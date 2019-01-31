31ef0668dad5dc084f73e879ca73712d3375519e
Jan Kozak <galvani78@gmail.com>
2019-01-31 13:26:58 +0100

    add events to segment filters dictionary generation, alter choices event to be more dynamic, make filter type public, alter choices form to allow translation keys without prefix as fallback. update reference to CO bundle

--- app/bundles/LeadBundle/LeadEvents.php
+++ app/bundles/LeadBundle/LeadEvents.php
@@ -591,6 +591,7 @@ final class LeadEvents
      */
     const LIST_FILTERS_CHOICES_ON_GENERATE = 'mautic.list_filters_choices_on_generate';
 
+    const SEGMENT_DICTIONARY_ON_GENERATE = 'mautic.list_dictionary_on_generate';
     /**
      * The event is dispatched to allow inserting segment filters translations.
      *
