3da1bb1a7e6b652f1ebb19cf29e16e7bb7a79e77
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-02-11 11:09:55 +0100

    Readable unit test

--- app/bundles/CoreBundle/Tests/unit/Helper/DateRelativeParsersTest.php
+++ app/bundles/CoreBundle/Tests/unit/Helper/DateRelativeParsersTest.php
@@ -26,22 +26,31 @@ class DateRelativeParsersTest extends \PHPUnit_Framework_TestCase
     {
         $dateRelativeParser = new DateRelativeParser($this->dictionary, '+1 day');
         $this->assertTrue($dateRelativeParser->hasRelativeDate());
+
         $dateRelativeParser = new DateRelativeParser($this->dictionary, 'anniversary +1 day');
         $this->assertTrue($dateRelativeParser->hasRelativeDate());
+
         $dateRelativeParser = new DateRelativeParser($this->dictionary, '2019-01-01');
         $this->assertFalse($dateRelativeParser->hasRelativeDate());
+
         $dateRelativeParser = new DateRelativeParser($this->dictionary, '+1 day', ['datetime', 'date']);
         $this->assertTrue($dateRelativeParser->hasRelativeDate());
+
         $dateRelativeParser = new DateRelativeParser($this->dictionary, 'datetime +1 day', ['datetime', 'date']);
         $this->assertTrue($dateRelativeParser->hasRelativeDate());
+
         $dateRelativeParser = new DateRelativeParser($this->dictionary, 'date +1 day', ['datetime', 'date']);
         $this->assertTrue($dateRelativeParser->hasRelativeDate());
+
         $dateRelativeParser = new DateRelativeParser($this->dictionary, 'date +1 day', ['datetime', 'date']);
         $this->assertEquals('+1 day', $dateRelativeParser->getTimeframePart());
+
         $dateRelativeParser = new DateRelativeParser($this->dictionary, 'date +1 day', ['datetime', 'date']);
         $this->assertEquals('date', $dateRelativeParser->getPrefix());
+
         $dateRelativeParser = new DateRelativeParser($this->dictionary, 'date anniversary +1 day', ['datetime', 'date']);
         $this->assertEquals('+1 day', $dateRelativeParser->getTimeframePart());
+
         $dateRelativeParser = new DateRelativeParser($this->dictionary, 'date anniversary +1 day', ['datetime', 'date']);
         $this->assertEquals('date', $dateRelativeParser->getPrefix());
         $this->assertEquals(date('Y').'-07-07', $dateRelativeParser->getAnniversaryDate('2018-07-07'));
