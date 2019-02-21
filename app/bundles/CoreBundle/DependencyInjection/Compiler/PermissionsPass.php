1762de41ca5b1ad0ebe38824a6db23e128ffca17
John Linhart <admin@escope.cz>
2019-02-21 12:59:50 +0100

    Allow defining the permission classes in config files so dependencies could be used

---
+++ app/bundles/CoreBundle/DependencyInjection/Compiler/PermissionsPass.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class PermissionsPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $corePermissions = $container->findDefinition('mautic.security');
+
+        foreach ($container->findTaggedServiceIds('mautic.permissions') as $id => $tags) {
+            $permissionObject = $container->findDefinition($id);
+            $corePermissions->addMethodCall('setPermissionObject', [$permissionObject]);
+        }
+    }
+}
