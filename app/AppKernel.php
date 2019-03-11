3125e630dfe5da402d4b9016998e593dc89cafa0
Enc3phale <bahaalfr@gmail.com>
2019-03-11 10:25:26 +0100

    Fix cache warmup on first load

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -527,7 +527,9 @@ class AppKernel extends Kernel
 
         // Warm up the cache if classes.php is missing or in dev mode
         if (!$fresh && $this->container->has('cache_warmer')) {
-            $this->container->get('cache_warmer')->warmUp($this->container->getParameter('kernel.cache_dir'));
+            $warmer = $this->container->get('cache_warmer');
+            $warmer->enableOptionalWarmers();
+            $warmer->warmUp($this->container->getParameter('kernel.cache_dir'));
         }
     }
 
