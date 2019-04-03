241239175f74f40104219c85b337190d45c6bb53
Jan Kozak <galvani78@gmail.com>
2019-04-03 11:40:47 +0200

    fix js references to form

--- app/bundles/EmailBundle/Views/Lead/preference_options.html.php
+++ app/bundles/EmailBundle/Views/Lead/preference_options.html.php
@@ -16,17 +16,18 @@ $leadChannelsForm = $form['lead_channels'];
 $js            = <<<'JS'
 function togglePreferredChannel(channel){
        var status = document.getElementById(channel).checked;
+       var formPrefix = 'lead_contact_frequency_rules_lead_channels_';
        if(status)
            {
-                document.getElementById('lead_contact_frequency_rules_frequency_number_' + channel).disabled = false;
-                document.getElementById('lead_contact_frequency_rules_frequency_time_' + channel).disabled = false;
-                document.getElementById('lead_contact_frequency_rules_contact_pause_start_date_' + channel).disabled = false;
-                document.getElementById('lead_contact_frequency_rules_contact_pause_end_date_' + channel).disabled = false;
+                document.getElementById(formPrefix + 'frequency_number_' + channel).disabled = false;
+                document.getElementById(formPrefix + 'frequency_time_' + channel).disabled = false;
+                document.getElementById(formPrefix + 'contact_pause_start_date_' + channel).disabled = false;
+                document.getElementById(formPrefix + 'contact_pause_end_date_' + channel).disabled = false;
             } else {
-                document.getElementById('lead_contact_frequency_rules_frequency_number_' + channel).disabled = true;
-                document.getElementById('lead_contact_frequency_rules_frequency_time_' + channel).disabled = true;
-                document.getElementById('lead_contact_frequency_rules_contact_pause_start_date_' + channel).disabled = true;
-                document.getElementById('lead_contact_frequency_rules_contact_pause_end_date_' + channel).disabled = true;
+                document.getElementById(formPrefix + 'frequency_number_' + channel).disabled = true;
+                document.getElementById(formPrefix + 'frequency_time_' + channel).disabled = true;
+                document.getElementById(formPrefix + 'contact_pause_start_date_' + channel).disabled = true;
+                document.getElementById(formPrefix + 'contact_pause_end_date_' + channel).disabled = true;
             }
         }
 JS;
