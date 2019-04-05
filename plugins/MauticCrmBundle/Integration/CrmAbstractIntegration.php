5859f6bb5e05c57588947094ec2248f1b3904b5a
Anton Vlasenko <vlasenko.anton@gmail.com>
2019-04-05 16:26:27 +0200

    Revert "Remove debug code"
    This reverts commit 953da5dee10f0aeb3629c92dd4de720ddb7b9ea2.

--- plugins/MauticCrmBundle/Integration/CrmAbstractIntegration.php
+++ plugins/MauticCrmBundle/Integration/CrmAbstractIntegration.php
@@ -370,7 +370,7 @@ abstract class CrmAbstractIntegration extends AbstractIntegration
      *
      * @return Lead
      */
-    public function getMauticLead($data, $persist = true, $socialCache = null, $identifiers = null, $object = null, $skipValidation = [])
+    public function getMauticLead($data, $persist = true, $socialCache = null, $identifiers = null, $object = null, $skipValidation = array())
     {
         if (is_object($data)) {
             // Convert to array in all levels
@@ -440,6 +440,7 @@ abstract class CrmAbstractIntegration extends AbstractIntegration
         }
 
         $leadModel->setFieldValues($lead, $matchedFields, false, false);
+
 
         foreach ($skipValidation as $field) {
             // We need to set them again
