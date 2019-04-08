03bf6c860bb408d6e7e7aa904bbc8c3fecb180c5
Jan Kozak <galvani78@gmail.com>
2019-04-08 13:55:32 +0200

    remove debug, it causes out of memory

--- app/bundles/EmailBundle/Views/Lead/preference_options.html.php
+++ app/bundles/EmailBundle/Views/Lead/preference_options.html.php
@@ -179,6 +179,5 @@ JS;
     <?php
     unset($form['lead_channels']['preferred_channel']);
     unset($form['lead_channels']['subscribed_channels']);
-    var_dump($form['lead_channels']);
     echo $view['form']->end($form); ?>
 </div>
