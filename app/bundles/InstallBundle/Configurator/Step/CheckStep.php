d2bf07b44c58ffd7d1f07251c6b672767f46c560
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-04-08 23:24:13 +0200

    :construction: Create install service and install command
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- app/bundles/InstallBundle/Configurator/Step/CheckStep.php
+++ app/bundles/InstallBundle/Configurator/Step/CheckStep.php
@@ -70,7 +70,7 @@ class CheckStep implements StepInterface
         $this->configIsWritable = $configurator->isFileWritable();
         $this->kernelRoot       = $kernelRoot;
         if (!empty($request)) {
-            $this->site_url         = $request->getSchemeAndHttpHost().$request->getBasePath();
+            $this->site_url     = $request->getSchemeAndHttpHost().$request->getBasePath();
         }
         $this->openSSLCipher    = $openSSLCipher;
     }
