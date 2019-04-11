bbbd289a99545694eac77931f6da155e95390310
John Linhart <admin@escope.cz>
2019-04-11 12:41:00 +0200

    Now builder added to the tests

--- app/bundles/ReportBundle/Tests/Scheduler/Factory/SchedulerTemplateFactoryTest.php
+++ app/bundles/ReportBundle/Tests/Scheduler/Factory/SchedulerTemplateFactoryTest.php
@@ -9,9 +9,19 @@ use Mautic\ReportBundle\Scheduler\Entity\SchedulerEntity;
 use Mautic\ReportBundle\Scheduler\Enum\SchedulerEnum;
 use Mautic\ReportBundle\Scheduler\Exception\NotSupportedScheduleTypeException;
 use Mautic\ReportBundle\Scheduler\Factory\SchedulerTemplateFactory;
+use Mautic\ReportBundle\Scheduler\Builder\SchedulerNowBuilder;
 
 class SchedulerTemplateFactoryTest extends \PHPUnit\Framework\TestCase
 {
+    public function testNowBuilder()
+    {
+        $schedulerEntity          = new SchedulerEntity(true, SchedulerEnum::UNIT_NOW, null, null);
+        $schedulerTemplateFactory = new SchedulerTemplateFactory();
+        $builder                  = $schedulerTemplateFactory->getBuilder($schedulerEntity);
+
+        $this->assertInstanceOf(SchedulerNowBuilder::class, $builder);
+    }
+
     public function testDailyBuilder()
     {
         $schedulerEntity          = new SchedulerEntity(true, SchedulerEnum::UNIT_DAILY, null, null);
