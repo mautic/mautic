2e67c233e025c2172d5ba2f28aa15f04bfc0346f
John Linhart <admin@escope.cz>
2019-05-14 17:25:38 +0200

    New webhook event for SMS sends

---
+++ app/bundles/SmsBundle/EventListener/WebhookSubscriber.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\SmsBundle\EventListener;
+
+use Mautic\CoreBundle\EventListener\CommonSubscriber;
+use Mautic\WebhookBundle\Event\WebhookBuilderEvent;
+use Mautic\WebhookBundle\EventListener\WebhookModelTrait;
+use Mautic\WebhookBundle\WebhookEvents;
+use Mautic\SmsBundle\SmsEvents;
+use Mautic\SmsBundle\Event\SmsSendEvent;
+
+class WebhookSubscriber extends CommonSubscriber
+{
+    use WebhookModelTrait;
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            SmsEvents::SMS_ON_SEND          => 'onSend',
+            WebhookEvents::WEBHOOK_ON_BUILD => 'onWebhookBuild',
+        ];
+    }
+
+    /**
+     * Add event triggers and actions.
+     *
+     * @param WebhookBuilderEvent $event
+     */
+    public function onWebhookBuild(WebhookBuilderEvent $event)
+    {
+        $event->addEvent(
+            SmsEvents::SMS_ON_SEND,
+            [
+                'label'       => 'mautic.sms.webhook.event.send',
+                'description' => 'mautic.sms.webhook.event.send_desc',
+            ]
+        );
+    }
+
+    /**
+     * @param SmsSendEvent $event
+     */
+    public function onSend(SmsSendEvent $event)
+    {
+        $this->webhookModel->queueWebhooksByType(
+            SmsEvents::SMS_ON_SEND,
+            [
+                'smsId'   => $event->getSmsId(),
+                'contact' => $event->getLead(),
+                'content' => $event->getContent(),
+            ]
+        );
+    }
+}
