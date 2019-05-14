73730b78acae62bbba1a90ce5c228e284e4af93f
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-05-14 22:57:07 +0200

    Fix to access_token expired after 1 hour

--- plugins/MauticCitrixBundle/Api/CitrixApi.php
+++ plugins/MauticCitrixBundle/Api/CitrixApi.php
@@ -19,12 +19,13 @@ class CitrixApi
      * @param string $operation
      * @param array  $settings
      * @param string $route
+     * @param bool   $refreshToken
      *
      * @return mixed|string
      *
      * @throws ApiErrorException
      */
-    protected function _request($operation, array $settings, $route = 'rest')
+    protected function _request($operation, array $settings, $route = 'rest', $refreshToken = true)
     {
         $requestSettings = [
             'encode_parameters'   => 'json',
@@ -51,7 +52,15 @@ class CitrixApi
             $requestSettings
         );
         $status  = $request->code;
-        $message = '';
+
+        // Try refresh access_token with refresh_token (ttps://goto-developer.logmeininc.com/how-use-refresh-tokens)
+        if ($refreshToken && $this->isInvalidTokenFromReponse($request)) {
+            $error = $this->integration->authCallback(['use_refresh_token' => true]);
+            if (!$error) {
+                $request = $this->_request($operation, $settings, $route, false);
+                $status  = $request->code;
+            }
+        }
 
         switch ($status) {
             case 200:
@@ -85,5 +94,20 @@ class CitrixApi
         }
 
         return $this->integration->parseCallbackResponse($request->body);
+    }
+
+    /**
+     * @param Response $request
+     *
+     * @return bool
+     */
+    private function isInvalidTokenFromReponse(Response $request)
+    {
+        $responseData = $this->integration->parseCallbackResponse($request->body);
+        if (isset($responseData['int_err_code']) && $responseData['int_err_code'] == 'InvalidToken') {
+            return true;
+        }
+
+        return false;
     }
 }
