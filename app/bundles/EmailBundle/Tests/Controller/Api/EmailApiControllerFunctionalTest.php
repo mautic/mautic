db80cea897bca4cb02af94e388da0e4aebee7ed1
John Linhart <admin@escope.cz>
2019-05-24 13:39:18 +0200

    Functional test that cover new API endpoints for email replies

--- app/bundles/EmailBundle/Tests/Controller/Api/EmailApiControllerFunctionalTest.php
+++ app/bundles/EmailBundle/Tests/Controller/Api/EmailApiControllerFunctionalTest.php
@@ -11,7 +11,10 @@
 
 namespace Mautic\EmailBundle\Tests\Controller\Api;
 
+use FOS\RestBundle\Util\Codes;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\EmailBundle\Entity\Stat;
+use Mautic\EmailBundle\Entity\StatRepository;
 
 class EmailApiControllerFunctionalTest extends MauticMysqlTestCase
 {
@@ -138,5 +141,50 @@ class EmailApiControllerFunctionalTest extends MauticMysqlTestCase
 
         $this->assertSame(['lists' => []], $response);
         $this->assertSame(200, $clientResponse->getStatusCode(), $clientResponse->getContent());
+    }
+
+    public function testReplyActionIfNotFound()
+    {
+        $trackingHash = 'tracking_hash_123';
+
+        // Create new email reply.
+        $this->client->request('POST', "/api/emails/reply/{$trackingHash}");
+        $response     = $this->client->getResponse();
+        $responseData = json_decode($response->getContent(), true);
+
+        $this->assertSame(Codes::HTTP_NOT_FOUND, $response->getStatusCode());
+        $this->assertSame('Email Stat with tracking hash tracking_hash_123 was not found', $responseData['errors'][0]['message']);
+    }
+
+    public function testReplyAction()
+    {
+        $trackingHash = 'tracking_hash_123';
+
+        /** @var StatRepository $statRepository */
+        $statRepository = $this->container->get('mautic.email.repository.stat');
+
+        // Create a test email stat.
+        $stat = new Stat();
+        $stat->setTrackingHash($trackingHash);
+        $stat->setEmailAddress('john@doe.email');
+        $stat->setDateSent(new \DateTime());
+
+        $statRepository->saveEntity($stat);
+
+        // Create new email reply.
+        $this->client->request('POST', "/api/emails/reply/{$trackingHash}");
+        $response = $this->client->getResponse();
+
+        $this->assertSame(Codes::HTTP_CREATED, $response->getStatusCode());
+        $this->assertSame(['success' => true], json_decode($response->getContent(), true));
+
+        // Get the email reply that was just created from the stat API.
+        $statReplyQuery = ['where' => [['col' => 'stat_id', 'expr' => 'eq', 'val' => $stat->getId()]]];
+        $this->client->request('GET', '/api/stats/email_stat_replies', $statReplyQuery);
+        $fetchedReplyData = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertSame('1', $fetchedReplyData['total']);
+        $this->assertSame($stat->getId(), $fetchedReplyData['stats'][0]['stat_id']);
+        $this->assertRegExp('/api-[a-z0-9]*/', $fetchedReplyData['stats'][0]['message_id']);
     }
 }
