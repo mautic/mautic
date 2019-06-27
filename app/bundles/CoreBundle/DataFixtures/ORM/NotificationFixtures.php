d8d0fed39c492ab5c1bbd47b592d96acabf03cb3
Dean Martin <contact@deanmartin.pro>
2019-06-27 16:54:21 -0400

    NotificationFixtures now has it's date_added to be random dates

--- app/bundles/CoreBundle/DataFixtures/ORM/NotificationFixtures.php
+++ app/bundles/CoreBundle/DataFixtures/ORM/NotificationFixtures.php
@@ -2,6 +2,7 @@
 
 namespace Mautic\CoreBundle\ORM\DataFixtures;
 
+use DateTime;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -27,11 +28,20 @@ class NotificationFixtures extends AbstractFixture implements OrderedFixtureInte
             $n = new Notification();
             $n->setHeader('notification'.$i);
             $n->setMessage('lorem ipsum');
-            $n->setDateAdded(new \DateTime());
+            $n->setDateAdded($this->randomDateInRange(new \DateTime('-8 day'), new DateTime()));
             $n->setUser($user);
             $manager->persist($n);
         }
 
         $manager->flush();
+    }
+
+    private function randomDateInRange(DateTime $start, DateTime $end)
+    {
+        $randomTimestamp = mt_rand($start->getTimestamp(), $end->getTimestamp());
+        $randomDate      = new DateTime();
+        $randomDate->setTimestamp($randomTimestamp);
+
+        return $randomDate;
     }
 }
