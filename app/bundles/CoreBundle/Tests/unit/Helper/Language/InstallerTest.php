038e59e40de4e2ee0098800111a7616e205b6f2d
Alan Hartless <alan@devkardia.com>
2019-07-04 10:48:28 -0500

    Added tests for the language installer

---
+++ app/bundles/CoreBundle/Tests/unit/Helper/Language/InstallerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic Inc. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://www.mautic.com
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\Tests\Helper\Language;
+
+
+use Mautic\CoreBundle\Helper\Language\Installer;
+use Symfony\Component\Filesystem\Filesystem;
+
+class InstallerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstaller()
+    {
+        $translationsDirectory = __DIR__.'/../resource/language';
+
+        $installer = new Installer($translationsDirectory.'/translations');
+
+        $zipper  = new \ZipArchive();
+        $zipper->open($translationsDirectory.'/es.zip');
+        $zipper->extractTo($translationsDirectory.'/tmp');
+
+        $this->assertFileExists($translationsDirectory.'/tmp/es');
+
+        $installer->install($translationsDirectory.'/tmp', 'es');
+
+        // did the installer create the language folder?
+        $languagePath = $translationsDirectory.'/translations/es';
+        $this->assertFileExists($languagePath);
+
+        // did it copy the config?
+        $this->assertFileExists($languagePath.'/config.json');
+
+        // did it ignore the php config?
+        $this->assertFileNotExists($languagePath.'/config.php');
+
+        // did it ignore the extra files?
+        $this->assertFileNotExists($languagePath.'/random.txt');
+        $this->assertFileNotExists($languagePath.'/RandomFolder');
+
+        // did it create the bundles?
+        $this->assertFileExists($languagePath.'/CoreBundle');
+        $this->assertFileExists($languagePath.'/CampaignBundle');
+
+        // did it copy the INI files?
+        $this->assertFileExists($languagePath.'/CoreBundle/messages.ini');
+        $this->assertFileExists($languagePath.'/CoreBundle/flashes.ini');
+        $this->assertFileExists($languagePath.'/CampaignBundle/messages.ini');
+        $this->assertFileExists($languagePath.'/CampaignBundle/flashes.ini');
+
+        // did it ignore the bundle's extra files?
+        $this->assertFileNotExists($languagePath.'/CoreBundle/random.txt');
+
+        // did the installer cleanup appropriately
+        $this->assertFileExists($translationsDirectory.'/tmp/es');
+        $installer->cleanup();
+        $this->assertFileNotExists($translationsDirectory.'/tmp/es');
+
+        // cleanup the test
+        (new Filesystem())->remove($languagePath);
+    }
+}
---
+++ app/bundles/CoreBundle/Tests/unit/Helper/LanguageHelperTest.php
@@ -0,0 +1,158 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic Inc. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://www.mautic.com
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\Tests\Helper;
+
+
+use Joomla\Http\Http;
+use Mautic\CoreBundle\Helper\CoreParametersHelper;
+use Mautic\CoreBundle\Helper\LanguageHelper;
+use Mautic\CoreBundle\Helper\PathsHelper;
+use Monolog\Logger;
+use Symfony\Component\Filesystem\Filesystem;
+
+class LanguageHelperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PathsHelper|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $pathsHelper;
+
+    /**
+     * @var Logger|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $logger;
+
+    /**
+     * @var CoreParametersHelper|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $coreParametersHelper;
+
+    /**
+     * @var Http|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $connector;
+
+    /**
+     * @var string
+     */
+    private $translationsPath;
+
+    /**
+     * @var string
+     */
+    private $tmpPath;
+
+    protected function setUp()
+    {
+        $this->pathsHelper          = $this->createMock(PathsHelper::class);
+        $this->logger               = $this->createMock(Logger::class);
+        $this->coreParametersHelper = $this->createMock(CoreParametersHelper::class);
+        $this->connector            = $this->createMock(Http::class);
+
+        $this->translationsPath = __DIR__.'/resource/language';
+        $this->tmpPath          = $this->translationsPath.'/tmp';
+    }
+
+    public function testLanguageIsInstalled()
+    {
+        $this->pathsHelper->method('getSystemPath')
+            ->withConsecutive(['translations_root'], ['cache'], ['cache'], ['tmp'])
+            ->willReturnOnConsecutiveCalls($this->translationsPath, $this->tmpPath, $this->tmpPath, $this->tmpPath);
+
+        $filesystem = new Filesystem();
+
+        // copy the zip to the tmp folder so the helper does not delete the test zip
+        $filesystem->copy($this->translationsPath.'/es.zip', $this->tmpPath.'/es.zip');
+
+        $helper = $this->getHelper();
+        $error = $helper->extractLanguagePackage('es');
+
+        $this->assertFalse($error['error']);
+        $this->assertFileExists($this->translationsPath.'/translations/es');
+
+        // Cleanup the test
+        $filesystem->remove($this->translationsPath.'/translations/es');
+    }
+
+    public function testLanguageListIsFetchedAndWritten()
+    {
+        $langFile = $this->tmpPath.'/../languageList.txt';
+        $this->coreParametersHelper->method('getParameter')
+            ->withConsecutive(['language_list_file'], ['translations_list_url'])
+            ->willReturnOnConsecutiveCalls(
+                '',
+                'https://languages.test'
+            );
+
+        $this->pathsHelper->method('getSystemPath')
+            ->withConsecutive(['translations_root'], ['cache'])
+            ->willReturnOnConsecutiveCalls($this->translationsPath, $this->tmpPath);
+
+        $languages = ['languages' => ['es' => []]];
+        $response = new \stdClass();
+        $response->code = 200;
+        $response->body = json_encode($languages);
+
+        $this->connector->expects($this->once())
+            ->method('post')
+            ->with('https://languages.test', [], [], 10)
+            ->willReturn($response);
+
+        $this->getHelper()->fetchLanguages();
+
+        $this->assertFileExists($langFile);
+
+        $written = json_decode(file_get_contents($langFile), true);
+        $this->assertEquals($languages['languages'], $written['languages']);
+
+        @unlink($langFile);
+    }
+
+    public function testLanguageIsFetched()
+    {
+        $languages = ['languages' => ['es' => []]];
+        $langFile = $this->tmpPath.'/../languageList.txt';
+        file_put_contents($langFile, json_encode($languages));
+
+        $this->coreParametersHelper->method('getParameter')
+            ->with('translations_fetch_url')
+            ->willReturn('https://languages.test?locale=');
+
+        $this->pathsHelper->method('getSystemPath')
+            ->withConsecutive(['translations_root'], ['cache'], ['cache'])
+            ->willReturnOnConsecutiveCalls($this->translationsPath, $this->tmpPath, $this->tmpPath);
+
+        $response = new \stdClass();
+        $response->code = 200;
+        $response->body = file_get_contents($this->translationsPath.'/es.zip');
+
+        $this->connector->expects($this->once())
+            ->method('get')
+            ->with('https://languages.test?locale=es')
+            ->willReturn($response);
+
+        $error = $this->getHelper()->fetchPackage('es');
+        @unlink($langFile);
+        $this->assertFalse($error['error']);
+
+        $this->assertFileExists($this->tmpPath.'/es.zip');
+        @unlink($this->tmpPath.'/es.zip');
+    }
+
+    /**
+     * @return LanguageHelper
+     */
+    private function getHelper()
+    {
+        return new LanguageHelper($this->pathsHelper, $this->logger, $this->coreParametersHelper, $this->connector);
+    }
+}
