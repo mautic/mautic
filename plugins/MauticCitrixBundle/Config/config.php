5fc405ed644639e20fe1ddccc03b95dbc9eaeb7d
Anton Vlasenko <vlasenko.anton@gmail.com>
2019-07-23 18:45:00 +0200

    There is no need to specify empty arguments.

--- plugins/MauticCitrixBundle/Config/config.php
+++ plugins/MauticCitrixBundle/Config/config.php
@@ -68,7 +68,6 @@ return [
             ],
             'mautic.citrix.integration.request' => [
                 'class'     => \MauticPlugin\MauticCitrixBundle\EventListener\IntegrationRequestSubscriber::class,
-                'arguments' => [],
             ],
         ],
         'forms' => [
