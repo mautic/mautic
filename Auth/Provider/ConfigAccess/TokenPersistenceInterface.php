623b6b50249b1c8f8c5f607341178cafa5b77e47
Lukas Drahy <lukas@drahy.net>
2019-07-29 14:38:44 +0200

    Add config access interfaces

---
+++ Auth/Provider/ConfigAccess/TokenPersistenceInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright   2019 Mautic, Inc. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://mautic.com
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\IntegrationsBundle\Auth\Provider\ConfigAccess;
+
+use kamermans\OAuth2\Persistence\TokenPersistenceInterface as KamermansTokenPersistenceInterface;
+
+interface TokenPersistenceInterface extends KamermansTokenPersistenceInterface
+{
+
+}
