15a608a71bf51aff06ba98ce634790fb03e4d0b2
Lukas Drahy <lukas@drahy.net>
2019-08-06 10:57:07 +0200

    remove callback

--- Assets/js/integrations.js
+++ Assets/js/integrations.js
@@ -135,5 +135,5 @@ Mautic.activateIntegrationFieldUpdateActions = function () {
 Mautic.authorizeIntegration = function () {
     mQuery('#integration_details_in_auth').val(1);
 
-    Mautic.postForm(mQuery('form[name="integration_config"]'), 'loadIntegrationAuthWindow');
+    Mautic.postForm(mQuery('form[name="integration_config"]'));
 };
