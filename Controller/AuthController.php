3f35aa9a7d5fbadc6730d8462d30520969068821
Lukas Drahy <lukas@drahy.net>
2019-08-08 14:51:42 +0200

    Handle unauthorized callback return from service

--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -14,6 +14,7 @@ namespace MauticPlugin\IntegrationsBundle\Controller;
 
 use Mautic\CoreBundle\Controller\CommonController;
 use MauticPlugin\IntegrationsBundle\Exception\IntegrationNotFoundException;
+use MauticPlugin\IntegrationsBundle\Exception\UnauthorizedException;
 use MauticPlugin\IntegrationsBundle\Helper\AuthIntegrationsHelper;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -29,6 +30,8 @@ class AuthController extends CommonController
             return $authIntegration->authenticateIntegration($request);
         } catch (IntegrationNotFoundException $exception) {
             return $this->notFound();
+        } catch (UnauthorizedException $exception) {
+            return $this->accessDenied();
         }
     }
 }
