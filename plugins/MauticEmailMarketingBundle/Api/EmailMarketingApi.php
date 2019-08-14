73b1e80229eda6bb77abcf3978d11ded408dc985
Anton Vlasenko <vlasenko.anton@gmail.com>
2019-08-14 15:42:06 +0200

    Merge UnifiedIntegrationInterface from Mautic.

--- plugins/MauticEmailMarketingBundle/Api/EmailMarketingApi.php
+++ plugins/MauticEmailMarketingBundle/Api/EmailMarketingApi.php
@@ -11,14 +11,14 @@
 
 namespace MauticPlugin\MauticEmailMarketingBundle\Api;
 
-use Mautic\PluginBundle\Integration\AbstractIntegration;
+use Mautic\PluginBundle\Integration\UnifiedIntegrationInterface;
 
 class EmailMarketingApi
 {
     protected $integration;
     protected $keys;
 
-    public function __construct(AbstractIntegration $integration)
+    public function __construct(UnifiedIntegrationInterface $integration)
     {
         $this->integration = $integration;
         $this->keys        = $integration->getKeys();
