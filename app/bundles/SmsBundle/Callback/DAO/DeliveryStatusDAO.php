bb75d4178554cad98124486c39ffd43d934e7088
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-08-18 14:07:00 +0200

    Add stat to DeliveryStatusDAO

--- app/bundles/SmsBundle/Callback/DAO/DeliveryStatusDAO.php
+++ app/bundles/SmsBundle/Callback/DAO/DeliveryStatusDAO.php
@@ -12,6 +12,7 @@
 namespace Mautic\SmsBundle\Callback\DAO;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Mautic\SmsBundle\Entity\Stat;
 
 class DeliveryStatusDAO
 {
@@ -31,14 +32,14 @@ class DeliveryStatusDAO
     private $isFailed = false;
 
     /**
-     * @var string
-     */
-    private $trackingHash;
-
-    /**
      * @var ArrayCollection
      */
     private $contacts;
+
+    /**
+     * @var Stat
+     */
+    private $stat;
 
     /**
      * @return bool
@@ -89,22 +90,6 @@ class DeliveryStatusDAO
     }
 
     /**
-     * @return string
-     */
-    public function getTrackingHash()
-    {
-        return $this->trackingHash;
-    }
-
-    /**
-     * @param string $trackingHash
-     */
-    public function setTrackingHash($trackingHash)
-    {
-        $this->trackingHash = $trackingHash;
-    }
-
-    /**
      * @return ArrayCollection
      */
     public function getContacts()
@@ -118,5 +103,21 @@ class DeliveryStatusDAO
     public function setContacts($contacts)
     {
         $this->contacts = $contacts;
+    }
+
+    /**
+     * @return Stat
+     */
+    public function getStat()
+    {
+        return $this->stat;
+    }
+
+    /**
+     * @param Stat $stat
+     */
+    public function setStat($stat)
+    {
+        $this->stat = $stat;
     }
 }
