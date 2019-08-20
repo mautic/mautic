f80a7291a9cbe87ab966ba97fa5db9065835e463
heathdutton <hdutton@thedmsgrp.com>
2019-08-20 13:33:37 -0400

    Do not duplicate internal IPs with a DB cluster.

--- app/bundles/CampaignBundle/Entity/LeadEventLog.php
+++ app/bundles/CampaignBundle/Entity/LeadEventLog.php
@@ -135,7 +135,11 @@ class LeadEventLog implements ChannelInterface
             ->addJoinColumn('campaign_id', 'id')
             ->build();
 
-        $builder->addIpAddress(true);
+        $builder->createManyToOne('ipAddress', 'Mautic\CoreBundle\Entity\IpAddress')
+            ->cascadeDetach()
+            ->cascadeMerge()
+            ->addJoinColumn('ip_id', 'id', true)
+            ->build();
 
         $builder->createField('dateTriggered', 'datetime')
             ->columnName('date_triggered')
