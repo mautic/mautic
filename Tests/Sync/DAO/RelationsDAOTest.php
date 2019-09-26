9ab217f1f679f05506f761a04013a03a91406e2f
Ondrej Sibl <ondrej@sibl.info>
2019-09-26 23:21:30 +0200

    Associate the Mautic contact - updating tests

--- Tests/Sync/DAO/RelationsDAOTest.php
+++ Tests/Sync/DAO/RelationsDAOTest.php
@@ -20,11 +20,12 @@ class RelationsDAOTest extends \PHPUnit_Framework_TestCase
         $relationsDAO = new RelationsDAO;
         $integrationObjectId    = 'IntegrationId-123';
         $integrationRelObjectId = 'IntegrationId-456';
+        $objectName             = 'Contact';
         $relObjectName          = 'Account';
 
         $relationObject   = new RelationDAO(
-            'Contact',
-            'AccountId',
+            $objectName,
+            $relObjectName,
             $relObjectName,
             $integrationObjectId,
             $integrationRelObjectId
@@ -36,5 +37,9 @@ class RelationsDAOTest extends \PHPUnit_Framework_TestCase
         $returnedRelations = $relationsDAO->getRelations();
 
         $this->assertEquals($returnedRelations[0], $relationObject);
+        $this->assertEquals($returnedRelations[0]->getObjectName(), $objectName);
+        $this->assertEquals($returnedRelations[0]->getRelObjectName(), $relObjectName);
+        $this->assertEquals($returnedRelations[0]->getObjectIntegrationId(), $integrationObjectId);
+        $this->assertEquals($returnedRelations[0]->getRelObjectIntegrationId(), $integrationRelObjectId);
     }
 }
