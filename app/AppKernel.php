a63b9236445fdd2292409f87d16ad34ac7181bde
Woeler <woeler@woeler.eu>
2019-10-04 13:53:12 +0200

    Set version to 2.15.3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,14 +34,14 @@ class AppKernel extends Kernel
      *
      * @const integer
      */
-    const MINOR_VERSION = 16;
+    const MINOR_VERSION = 15;
 
     /**
      * Patch version number.
      *
      * @const integer
      */
-    const PATCH_VERSION = 0;
+    const PATCH_VERSION = 3;
 
     /**
      * Extra version identifier.
@@ -51,7 +51,7 @@ class AppKernel extends Kernel
      *
      * @const string
      */
-    const EXTRA_VERSION = '-dev';
+    const EXTRA_VERSION = '';
 
     /**
      * @var array
