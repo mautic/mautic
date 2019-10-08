268d321fe96c4e5c24d9746e03b9523454cdc432
John Linhart <admin@escope.cz>
2019-10-08 12:31:10 +0200

    A normalized Date/Time value can be either a DateTime object or null

--- Tests/Unit/Sync/ValueNormalizer/ValueNormalizerTest.php
+++ Tests/Unit/Sync/ValueNormalizer/ValueNormalizerTest.php
@@ -24,7 +24,17 @@ class ValueNormalizerTest extends \PHPUnit_Framework_TestCase
         $valueNormalizer    = new ValueNormalizer();
         $normalizedValueDAO = $valueNormalizer->normalizeForMautic(NormalizedValueDAO::DATETIME_TYPE, null);
 
-        $this->assertInstanceOf(DateTimeInterface::class, $normalizedValueDAO->getNormalizedValue());
+        $this->assertNull($normalizedValueDAO->getNormalizedValue());
         $this->assertNull($normalizedValueDAO->getOriginalValue());
+    }
+
+    public function testNotNullDateTimeValue(): void
+    {
+        $valueNormalizer    = new ValueNormalizer();
+        $normalizedValueDAO = $valueNormalizer->normalizeForMautic(NormalizedValueDAO::DATETIME_TYPE, '2019-10-08');
+
+        $this->assertInstanceOf(DateTimeInterface::class, $normalizedValueDAO->getNormalizedValue());
+        $this->assertSame('2019-10-08', $normalizedValueDAO->getNormalizedValue()->format('Y-m-d'));
+        $this->assertSame('2019-10-08', $normalizedValueDAO->getOriginalValue());
     }
 }
