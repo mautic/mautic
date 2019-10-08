b18833188e2832c21669bd317a80945c5cf854a9
Enrico Deleo <hello@enricodeleo.com>
2019-10-09 00:35:05 +0200

    feature: support https through reverse proxy

--- index.php
+++ index.php
@@ -19,6 +19,13 @@ use Mautic\CoreBundle\ErrorHandler\ErrorHandler;
 use Mautic\Middleware\MiddlewareBuilder;
 use function Stack\run;
 
+// in some setups HTTP_X_FORWARDED_PROTO might contain
+// a comma-separated list e.g. http,https
+// so check for https existence
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && false !== strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https')) {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 ErrorHandler::register('prod');
 
 run((new MiddlewareBuilder(new AppKernel('prod', false)))->resolve());
