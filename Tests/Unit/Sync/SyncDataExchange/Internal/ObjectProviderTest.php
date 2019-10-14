6cc40f11a1e3916f13d6039d810ac899155cd156
John Linhart <admin@escope.cz>
2019-10-14 17:33:22 +0200

    Namespace fix

--- Tests/Unit/Sync/SyncDataExchange/Internal/ObjectProviderTest.php
+++ Tests/Unit/Sync/SyncDataExchange/Internal/ObjectProviderTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace MauticPlugin\IntegrationsBundle\Tests\Unit\Sync\SyncDataExchange\Internal\ObjectHelper;
+namespace MauticPlugin\IntegrationsBundle\Tests\Unit\Sync\SyncDataExchange\Internal;
 
 use Mautic\LeadBundle\Entity\Lead;
 use MauticPlugin\IntegrationsBundle\Event\InternalObjectEvent;
