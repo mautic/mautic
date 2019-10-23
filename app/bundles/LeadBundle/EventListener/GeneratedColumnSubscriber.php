e322b0e6a181599d001d89f79b1dde037ce9c66a
Don Gilbert <don@dongilbert.net>
2019-10-23 09:41:53 -0400

    Add generated_email_domain as a text field to segments

--- app/bundles/LeadBundle/EventListener/GeneratedColumnSubscriber.php
+++ app/bundles/LeadBundle/EventListener/GeneratedColumnSubscriber.php
@@ -6,13 +6,27 @@ use Mautic\CoreBundle\CoreEvents;
 use Mautic\CoreBundle\Doctrine\GeneratedColumn\GeneratedColumn;
 use Mautic\CoreBundle\Event\GeneratedColumnsEvent;
 use Mautic\CoreBundle\EventListener\CommonSubscriber;
+use Mautic\LeadBundle\Event\LeadListFiltersChoicesEvent;
+use Mautic\LeadBundle\LeadEvents;
+use Mautic\LeadBundle\Model\ListModel;
 
 class GeneratedColumnSubscriber extends CommonSubscriber
 {
+    /**
+     * @var ListModel
+     */
+    private $listModel;
+
+    public function __construct(ListModel $listModel)
+    {
+        $this->listModel = $listModel;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
-            CoreEvents::ON_GENERATED_COLUMNS_BUILD => ['onGeneratedColumnsBuild', 0],
+            CoreEvents::ON_GENERATED_COLUMNS_BUILD       => ['onGeneratedColumnsBuild', 0],
+            LeadEvents::LIST_FILTERS_CHOICES_ON_GENERATE => ['onGenerateSegmentFilters', 0],
         ];
     }
 
@@ -26,5 +40,31 @@ class GeneratedColumnSubscriber extends CommonSubscriber
         );
 
         $event->addGeneratedColumn($emailDomain);
+    }
+
+    public function onGenerateSegmentFilters(LeadListFiltersChoicesEvent $event)
+    {
+        $event->addChoice('lead', 'generated_email_domain', [
+            'label'      => $this->translator->trans('mautic.email.segment.choice.generated_email_domain'),
+            'properties' => ['type' => 'text'],
+            'operators'  => $this->listModel->getOperatorsForFieldType(
+                [
+                    'include' => [
+                        '=',
+                        '!=',
+                        'empty',
+                        '!empty',
+                        'like',
+                        '!like',
+                        'regexp',
+                        '!regexp',
+                        'startsWith',
+                        'endsWith',
+                        'contains',
+                    ],
+                ]
+            ),
+            'object' => 'lead',
+        ]);
     }
 }
