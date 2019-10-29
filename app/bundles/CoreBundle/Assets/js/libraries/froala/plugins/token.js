f053475281aa83a2f13c8d98174887b3f253ca56
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-10-29 11:41:00 +0100

    Remove encodeURI and use own normalize replace

--- app/bundles/CoreBundle/Assets/js/libraries/froala/plugins/token.js
+++ app/bundles/CoreBundle/Assets/js/libraries/froala/plugins/token.js
@@ -91,8 +91,9 @@
                                                                 str.replace(/_BADGE_/,'dwc') : '';
                     var title = tokens[val];
                     if (title.length>24) title = title.substr(0, 24) + '...';
-                    var newOption = '<li role="presentation"><a class="fr-command" tabIndex="-1" role="option" data-cmd="token" data-param1="' + encodeURI(val) + '" title="' + title + '">' + title + badge + '</a></li>';
-                    options.push(newOption);
+                    var normalizedValue = val.replace(new RegExp('"', 'g'), "'");
+                    var newOption = '<li role="presentation"><a class="fr-command" tabIndex="-1" role="option"' +
+                        ' data-cmd="token" data-param1="' + normalizedValue + '" title="' + title + '">' + title + badge + '</a></li>';
                 }
 
                 mQuery('button[data-cmd="token"]').next().find('ul').html(options.join(''));
