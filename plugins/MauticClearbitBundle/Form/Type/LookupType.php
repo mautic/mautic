c9732a8f819a7ba785069ef6107dc6b800f104b6
Anton Vlasenko <vlasenko.anton@gmail.com>
2019-11-01 17:34:24 +0100

    Reference form types by FQCN.

--- plugins/MauticClearbitBundle/Form/Type/LookupType.php
+++ plugins/MauticClearbitBundle/Form/Type/LookupType.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * @copyright   2015 Mautic Contributors. All rights reserved
+ * @copyright   2019 Mautic Contributors. All rights reserved
  * @author      Mautic
  *
  * @link        http://mautic.org
@@ -11,7 +11,10 @@
 
 namespace MauticPlugin\MauticClearbitBundle\Form\Type;
 
+use Mautic\CoreBundle\Form\Type\FormButtonsType;
+use Mautic\CoreBundle\Form\Type\YesNoButtonGroupType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class LookupType extends AbstractType
@@ -24,7 +27,7 @@ class LookupType extends AbstractType
     {
         $builder->add(
             'objectId',
-            'hidden',
+            HiddenType::class,
             [
                 'attr' => [
                     'value' => $options['data']['objectId'],
@@ -34,7 +37,7 @@ class LookupType extends AbstractType
 
         $builder->add(
             'buttons',
-            'form_buttons',
+            FormButtonsType::class,
             [
                 'apply_text'     => false,
                 'save_text'      => 'mautic.core.form.submit',
@@ -47,7 +50,7 @@ class LookupType extends AbstractType
 
         $builder->add(
             'notify',
-            'yesno_button_group',
+            YesNoButtonGroupType::class,
             [
                 'label'      => 'mautic.plugin.clearbit.notify',
                 'label_attr' => ['class' => 'control-label'],
