6105fc9d0acbad10296468a0d2e34a7d3d3b8393
John Linhart <admin@escope.cz>
2019-11-01 17:11:48 +0100

    Deprecated event AssetEvents::ASSET_ON_DOWNLOAD removed

--- app/bundles/AssetBundle/Controller/PublicController.php
+++ app/bundles/AssetBundle/Controller/PublicController.php
@@ -63,15 +63,6 @@ class PublicController extends CommonFormController
                 $model->trackDownload($entity, $this->request, 301);
 
                 return $this->redirect($url, 301);
-            }
-
-            //all the checks pass so provide the asset for download
-            // @deprecated 2.0 - to be removed in 3.0
-            $dispatcher = $this->get('event_dispatcher');
-            if ($dispatcher->hasListeners(AssetEvents::ASSET_ON_DOWNLOAD)) {
-                $event = new AssetEvent($entity);
-                $dispatcher->dispatch(AssetEvents::ASSET_ON_DOWNLOAD, $event);
-            }
 
             if ($entity->isRemote()) {
                 $model->trackDownload($entity, $this->request, 200);
