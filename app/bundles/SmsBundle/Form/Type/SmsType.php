e41046658879a92a82e7d56c40fbb0441b610afc
Lukas Drahy <lukas@drahy.net>
2019-11-01 09:06:28 -0700

    Fix SmsType deprecations

--- app/bundles/SmsBundle/Form/Type/SmsType.php
+++ app/bundles/SmsBundle/Form/Type/SmsType.php
@@ -17,7 +17,7 @@ use Mautic\CoreBundle\Form\EventListener\FormExitSubscriber;
 use Mautic\CoreBundle\Translation\Translator;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Translation\TranslatorInterface;
 
 /**
@@ -175,9 +175,9 @@ class SmsType extends AbstractType
     }
 
     /**
-     * @param OptionsResolverInterface $resolver
+     * @param OptionsResolver $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(
             [
@@ -185,7 +185,7 @@ class SmsType extends AbstractType
             ]
         );
 
-        $resolver->setOptional(['update_select']);
+        $resolver->setDefined(['update_select']);
     }
 
     /**
