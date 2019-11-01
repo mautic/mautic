4f018f7f317a16b4d1f7e39597649bb893601a13
David <dco@webmecanik.com>
2019-11-01 14:59:29 +0100

    Fix Point action type

--- app/bundles/PointBundle/Form/Type/PointType.php
+++ app/bundles/PointBundle/Form/Type/PointType.php
@@ -22,6 +22,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -65,7 +66,8 @@ class PointType extends AbstractType
             'attr'       => ['class' => 'form-control'],
         ]);
 
-        $builder->add('description', TextType::class, [
+        $builder->add('description',
+          TextareaType::class, [
             'label'      => 'mautic.core.description',
             'label_attr' => ['class' => 'control-label'],
             'attr'       => ['class' => 'form-control editor'],
