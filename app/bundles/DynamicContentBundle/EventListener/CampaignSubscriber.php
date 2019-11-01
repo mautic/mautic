b51d5943558a3e544a3b90d6477ac98ba86da420
Don Gilbert <don@dongilbert.net>
2019-11-01 12:10:53 +0100

    Update campaign subscriber to use FQDN form names

--- app/bundles/DynamicContentBundle/EventListener/CampaignSubscriber.php
+++ app/bundles/DynamicContentBundle/EventListener/CampaignSubscriber.php
@@ -18,6 +18,8 @@ use Mautic\CoreBundle\Event\TokenReplacementEvent;
 use Mautic\CoreBundle\EventListener\CommonSubscriber;
 use Mautic\DynamicContentBundle\DynamicContentEvents;
 use Mautic\DynamicContentBundle\Entity\DynamicContent;
+use Mautic\DynamicContentBundle\Form\Type\DynamicContentDecisionType;
+use Mautic\DynamicContentBundle\Form\Type\DynamicContentSendType;
 use Mautic\DynamicContentBundle\Model\DynamicContentModel;
 use Mautic\LeadBundle\Model\LeadModel;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -75,7 +77,7 @@ class CampaignSubscriber extends CommonSubscriber
                 'label'                  => 'mautic.dynamicContent.campaign.send_dwc',
                 'description'            => 'mautic.dynamicContent.campaign.send_dwc.tooltip',
                 'eventName'              => DynamicContentEvents::ON_CAMPAIGN_TRIGGER_ACTION,
-                'formType'               => 'dwcsend_list',
+                'formType'               => DynamicContentSendType::class,
                 'formTypeOptions'        => ['update_select' => 'campaignevent_properties_dynamicContent'],
                 'formTheme'              => 'MauticDynamicContentBundle:FormTheme\DynamicContentPushList',
                 'timelineTemplate'       => 'MauticDynamicContentBundle:SubscribedEvents\Timeline:index.html.php',
@@ -101,7 +103,7 @@ class CampaignSubscriber extends CommonSubscriber
                 'label'           => 'mautic.dynamicContent.campaign.decision_dwc',
                 'description'     => 'mautic.dynamicContent.campaign.decision_dwc.tooltip',
                 'eventName'       => DynamicContentEvents::ON_CAMPAIGN_TRIGGER_DECISION,
-                'formType'        => 'dwcdecision_list',
+                'formType'        => DynamicContentDecisionType::class,
                 'formTypeOptions' => ['update_select' => 'campaignevent_properties_dynamicContent'],
                 'formTheme'       => 'MauticDynamicContentBundle:FormTheme\DynamicContentDecisionList',
                 'channel'         => 'dynamicContent',
