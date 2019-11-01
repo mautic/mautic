a6b663e8a79485ff0211e7ddef0194479e4bda40
Lukas Drahy <lukas@drahy.net>
2019-11-01 09:59:38 -0700

    Remove deprecated usage of addDncForContact method from SmsHelper

--- app/bundles/SmsBundle/Helper/SmsHelper.php
+++ app/bundles/SmsBundle/Helper/SmsHelper.php
@@ -14,7 +14,9 @@ namespace Mautic\SmsBundle\Helper;
 use Doctrine\ORM\EntityManager;
 use libphonenumber\PhoneNumberFormat;
 use Mautic\CoreBundle\Helper\PhoneNumberHelper;
-use Mautic\LeadBundle\Entity\DoNotContact;
+use Mautic\LeadBundle\Entity\DoNotContact as DoNotContactEntity;
+use Mautic\LeadBundle\Entity\LeadRepository;
+use Mautic\LeadBundle\Model\DoNotContact;
 use Mautic\LeadBundle\Model\LeadModel;
 use Mautic\PluginBundle\Helper\IntegrationHelper;
 use Mautic\SmsBundle\Model\SmsModel;
@@ -47,6 +49,11 @@ class SmsHelper
     protected $integrationHelper;
 
     /**
+     * @var DoNotContact
+     */
+    private $doNotContact;
+
+    /**
      * SmsHelper constructor.
      *
      * @param EntityManager     $em
@@ -54,21 +61,28 @@ class SmsHelper
      * @param PhoneNumberHelper $phoneNumberHelper
      * @param SmsModel          $smsModel
      * @param IntegrationHelper $integrationHelper
+     * @param DoNotContact      $doNotContact
      */
-    public function __construct(EntityManager $em, LeadModel $leadModel, PhoneNumberHelper $phoneNumberHelper, SmsModel $smsModel, IntegrationHelper $integrationHelper)
-    {
+    public function __construct(EntityManager $em,
+        LeadModel $leadModel,
+        PhoneNumberHelper $phoneNumberHelper,
+        SmsModel $smsModel,
+        IntegrationHelper $integrationHelper,
+        DoNotContact $doNotContact
+    ) {
         $this->em                   = $em;
         $this->leadModel            = $leadModel;
         $this->phoneNumberHelper    = $phoneNumberHelper;
         $this->smsModel             = $smsModel;
         $this->integrationHelper    = $integrationHelper;
+        $this->doNotContact         = $doNotContact;
     }
 
     public function unsubscribe($number)
     {
         $number = $this->phoneNumberHelper->format($number, PhoneNumberFormat::E164);
 
-        /** @var \Mautic\LeadBundle\Entity\LeadRepository $repo */
+        /** @var LeadRepository $repo */
         $repo = $this->em->getRepository('MauticLeadBundle:Lead');
 
         $args = [
@@ -100,7 +114,7 @@ class SmsHelper
             }
         }
 
-        return $this->leadModel->addDncForLead($lead, 'sms', null, DoNotContact::UNSUBSCRIBED);
+        return $this->doNotContact->addDncForContact($lead, 'sms', null, DoNotContactEntity::UNSUBSCRIBED);
     }
 
     /**
@@ -108,8 +122,8 @@ class SmsHelper
      */
     public function getDisableTrackableUrls()
     {
-        $integration                = $this->integrationHelper->getIntegrationObject('Twilio');
-        $settings                   = $integration->getIntegrationSettings()->getFeatureSettings();
+        $integration = $this->integrationHelper->getIntegrationObject('Twilio');
+        $settings    = $integration->getIntegrationSettings()->getFeatureSettings();
 
         return !empty($settings['disable_trackable_urls']) ? true : false;
     }
