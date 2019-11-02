8415aa4b938701100f87f089b8af8efbb9469910
Lukas Drahy <lukas@drahy.net>
2019-11-02 17:06:05 +0100

    Remove deprecations from AbTestPropertiesType

--- app/bundles/PageBundle/Form/Type/AbTestPropertiesType.php
+++ app/bundles/PageBundle/Form/Type/AbTestPropertiesType.php
@@ -13,7 +13,7 @@ namespace Mautic\PageBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class AbTestPropertiesType.
@@ -35,9 +35,9 @@ class AbTestPropertiesType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setOptional([
+        $resolver->setDefined([
             'formType',
             'formTypeOptions',
         ]);
