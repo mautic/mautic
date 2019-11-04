10af79576a27de97be57a9a6248c6b1166a71d81
DB Hurley <david.hurley@mautic.org>
2019-11-04 10:38:16 -0500

    Updated to fix issue with naming.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -179,7 +179,7 @@ class AppKernel extends Kernel
             new Mautic\EmailBundle\MauticEmailBundle(),
             new Mautic\FormBundle\MauticFormBundle(),
             new Mautic\InstallBundle\MauticInstallBundle(),
-            new Mautic\IntegrationsBundle\IntegrationsBundle(),
+            new Mautic\IntegrationsBundle\MauticIntegrationsBundle(),
             new Mautic\LeadBundle\MauticLeadBundle(),
             new Mautic\NotificationBundle\MauticNotificationBundle(),
             new Mautic\PageBundle\MauticPageBundle(),
