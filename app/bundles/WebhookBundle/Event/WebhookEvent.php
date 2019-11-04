a2b77aa5d28892ca6a45e4ac078e2fe6280df69a
Lukas Drahy <lukas@drahy.net>
2019-11-04 14:20:54 +0100

    Code cleanup

--- app/bundles/WebhookBundle/Event/WebhookEvent.php
+++ app/bundles/WebhookBundle/Event/WebhookEvent.php
@@ -23,7 +23,7 @@ class WebhookEvent extends CommonEvent
      * @param Webhook $webhook
      * @param bool    $isNew
      */
-    public function __construct(Webhook &$webhook, $isNew = false)
+    public function __construct(Webhook $webhook, $isNew = false)
     {
         $this->entity = &$webhook;
         $this->isNew  = $isNew;
