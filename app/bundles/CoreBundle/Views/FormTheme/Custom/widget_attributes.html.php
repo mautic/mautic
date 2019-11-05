3034f62cdeeaec0d3d576ad0d4eb5b189168a6bd
Alan Hartless <alan@devkardia.com>
2019-11-05 21:15:51 +0100

    Removed variables no longer available to teh widget_attributes.html.php template

--- app/bundles/CoreBundle/Views/FormTheme/Custom/widget_attributes.html.php
+++ app/bundles/CoreBundle/Views/FormTheme/Custom/widget_attributes.html.php
@@ -1,8 +1,6 @@
-id="<?php echo $view->escape($id) ?>" name="<?php echo $view->escape($full_name) ?>" <?php if ($read_only): ?>readonly="readonly" <?php endif ?>
+id="<?php echo $view->escape($id) ?>" name="<?php echo $view->escape($full_name) ?>"
 <?php if ($disabled): ?>disabled="disabled" <?php endif ?>
 <?php if ($required): ?>required="required" <?php endif ?>
-<?php if ($max_length): ?>maxlength="<?php echo $view->escape($max_length) ?>" <?php endif ?>
-<?php if ($pattern): ?>pattern="<?php echo $view->escape($pattern) ?>" <?php endif ?>
 <?php
 
 use Mautic\FormBundle\Helper\FormFieldHelper;
