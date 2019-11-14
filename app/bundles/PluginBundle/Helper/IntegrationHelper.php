092fcaa71c3f25295c5d9ec0f0c7da23dba70682
Lukas Drahy <lukas@drahy.net>
2019-11-14 11:37:53 +0100

    Remove support for deprecated 3rd party plugins in \Mautic\PluginBundle\Helper\IntegrationHelper

--- app/bundles/PluginBundle/Helper/IntegrationHelper.php
+++ app/bundles/PluginBundle/Helper/IntegrationHelper.php
@@ -178,30 +178,6 @@ class IntegrationHelper
                                 $newIntegrations[] = $newIntegration;
 
                                 unset($newIntegration);
-                            } else {
-                                /**
-                                 * @deprecated: 2.8.2 To be removed in 3.0
-                                 *            This keeps BC for 3rd party plugins
-                                 */
-                                $class    = '\\MauticPlugin\\'.$pluginNamespace.'\\Integration\\'.$integrationName.'Integration';
-                                $refClass = new \ReflectionClass($class);
-
-                                if ($refClass->isInstantiable()) {
-                                    $this->integrations[$integrationName] = new $class($this->factory);
-                                    $features                             = $this->integrations[$integrationName]->getSupportedFeatures();
-
-                                    $newIntegration->setSupportedFeatures($features);
-
-                                    // Go ahead and stash it since it's built already
-                                    $this->integrations[$integrationName]->setIntegrationSettings($newIntegration);
-
-                                    $newIntegrations[] = $newIntegration;
-
-                                    unset($newIntegration);
-                                } else {
-                                    // Something is bad so ignore
-                                    continue;
-                                }
                             }
                         }
 
@@ -334,23 +310,6 @@ class IntegrationHelper
                 if ($this->container->has($integrationContainerKey)) {
                     $this->integrations[$integrationName] = $this->container->get($integrationContainerKey);
                     $this->integrations[$integrationName]->setIntegrationSettings($integration['settings']);
-                } else {
-                    /**
-                     * @deprecated: 2.8.2 To be removed in 3.0
-                     *            This keeps BC for 3rd party plugins
-                     */
-                    $rootNamespace = $integration['isPlugin'] ? '\\MauticPlugin\\' : '\\Mautic\\';
-                    $class         = $rootNamespace.$integration['namespace'].'\\Integration\\'.$integrationName.'Integration';
-                    $refClass      = new \ReflectionClass($class);
-
-                    if ($refClass->isInstantiable()) {
-                        $this->integrations[$integrationName] = new $class($this->factory);
-
-                        $this->integrations[$integrationName]->setIntegrationSettings($integration['settings']);
-                    } else {
-                        // Something is bad so ignore
-                        continue;
-                    }
                 }
             }
 
