e06726773eabbf0cc03a3af16fbab38f45f73b30
Lukas Drahy <lukas@drahy.net>
2019-11-29 14:53:17 +0100

    Fix DynamicsIntegrationTest::setUp() method visibility

--- plugins/MauticCrmBundle/Tests/DynamicsIntegrationTest.php
+++ plugins/MauticCrmBundle/Tests/DynamicsIntegrationTest.php
@@ -16,7 +16,7 @@ class DynamicsIntegrationTest extends AbstractIntegrationTestCase
     /** @var DynamicsIntegration */
     private $integration;
 
-    protected function setUp()
+    public function setUp()
     {
         parent::setUp();
 
