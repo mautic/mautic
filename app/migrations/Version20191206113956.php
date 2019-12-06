b65901f0bed74510fd802a9d3c775ed172b89c0b
Patryk Gruszka <patryk.gruszka@comarch.com>
2019-12-06 13:18:44 +0100

    Lead lists public_name migration update

--- app/migrations/Version20191206113956.php
+++ app/migrations/Version20191206113956.php
@@ -1,33 +1,63 @@
 <?php
 
+/*
+ * @package     Mautic
+ * @copyright   2019 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 namespace Mautic\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Migrations\AbortMigrationException;
+use Doctrine\DBAL\Migrations\SkipMigrationException;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\SchemaException;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20191206113956 extends AbstractMigration
+class Version20191206113956 extends AbstractMauticMigration
 {
     /**
      * @param Schema $schema
+     *
+     * @throws SkipMigrationException
+     * @throws SchemaException
+     */
+    public function preUp(Schema $schema)
+    {
+        $table = $schema->getTable($this->prefix.'lead_lists');
+
+        if ($table->hasColumn('public_name')) {
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws AbortMigrationException
      */
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('ALTER TABLE lead_lists ADD public_name VARCHAR(255) NOT NULL');
-        $this->addSql('UPDATE lead_lists SET public_name=name');
+        $this->addSql("ALTER TABLE {$this->prefix}lead_lists ADD public_name VARCHAR(255) NOT NULL");
+        $this->addSql("UPDATE {$this->prefix}lead_lists SET public_name=name");
     }
 
     /**
      * @param Schema $schema
+     *
+     * @throws AbortMigrationException
      */
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('ALTER TABLE lead_lists DROP public_name');
+        $this->addSql("{$this->prefix}ALTER TABLE lead_lists DROP public_name");
     }
 }
