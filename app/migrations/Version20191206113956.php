74859fcbd50bd111b71081f168cf58cacbeca5bd
Patryk Gruszka <patryk.gruszka@comarch.com>
2019-12-06 12:47:03 +0100

    Segment public name property for preference center

---
+++ app/migrations/Version20191206113956.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20191206113956 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE lead_lists ADD public_name VARCHAR(255) NOT NULL');
+        $this->addSql('UPDATE lead_lists SET public_name=name');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE lead_lists DROP public_name');
+    }
+}
