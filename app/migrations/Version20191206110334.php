d5aa4c3df31be65bdad86044e12c175d03dd6c7c
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-12-06 12:06:04 +0100

    Add migrations

---
+++ app/migrations/Version20191206110334.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * @package     Mautic
+ * @copyright   2019 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Migrations\SkipMigrationException;
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20191206110334 extends AbstractMauticMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws SkipMigrationException
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function preUp(Schema $schema)
+    {
+        // Test to see if this migration has already been applied
+        $formTable = $schema->getTable($this->prefix.'form_fields');
+        if ($formTable->hasColumn('lead_field_not_overwrite')) {
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE '.$this->prefix.'form_fields ADD COLUMN lead_field_not_overwrite TINYINT(1) DEFAULT NULL');
+    }
+}
