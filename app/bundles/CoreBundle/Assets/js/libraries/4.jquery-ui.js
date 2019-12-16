14e2893360d87fc264fff2a86509376abbf08277
Anton Vlasenko <vlasenko.anton@gmail.com>
2019-12-16 14:05:42 +0100

    jQuery.unique is deprecated.

--- app/bundles/CoreBundle/Assets/js/libraries/4.jquery-ui.js
+++ app/bundles/CoreBundle/Assets/js/libraries/4.jquery-ui.js
@@ -514,7 +514,7 @@
                 for ( i = 0; i < classes.length; i++ ) {
                     current = that.classesElementLookup[ classes[ i ] ] || $();
                     if ( options.add ) {
-                        current = $( $.unique( current.get().concat( options.element.get() ) ) );
+                        current = $( $.uniqueSort( current.get().concat( options.element.get() ) ) );
                     } else {
                         current = $( current.not( options.element ).get() );
                     }
