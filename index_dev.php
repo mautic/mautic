7acbcd591ba3823eae980fc131cd4802d5912664
Don Gilbert <don@dongilbert.net>
2019-12-19 14:18:07 -0500

    Only the apcu_clear_cache() method is required

--- index_dev.php
+++ index_dev.php
@@ -20,10 +20,8 @@ $loader = require_once __DIR__.'/vendor/autoload.php';
  */
 umask(0000);
 
-if (extension_loaded('apc') && in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '172.17.0.1'])) {
+if (extension_loaded('apcu') && in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '172.17.0.1'])) {
     @apcu_clear_cache();
-    @apcu_clear_cache('user');
-    @apcu_clear_cache('opcode');
 }
 
 \Mautic\CoreBundle\ErrorHandler\ErrorHandler::register('dev');
