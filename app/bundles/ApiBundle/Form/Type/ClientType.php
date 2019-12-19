710c2fa2440215510b90a6b2909d632b1b2d9c66
mtshaw3 <mike.shaw@acquia.com>
2019-12-19 11:28:59 -0500

    Revert "test fixing formatting"
    This reverts commit ac2ff265483723b215169daf4f6fc4c77a9a428e.

--- app/bundles/ApiBundle/Form/Type/ClientType.php
+++ app/bundles/ApiBundle/Form/Type/ClientType.php
@@ -104,9 +104,9 @@ class ClientType extends AbstractType
                         'OAuth 1.0a' => 'oauth1a',
                         'OAuth 2'    => 'oauth2',
                     ],
-                    'required'          => false,
-                    'empty_value'       => false,
-                    'data'              => $this->apiMode,
+                                        'required'          => false,
+                    'empty_value'                           => false,
+                    'data'                                  => $this->apiMode,
                 ]
             );
         }
