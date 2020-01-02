8ced2401b1db2a7933a40661607314e0ca2119d7
Anton Vlasenko <vlasenko.anton@gmail.com>
2020-01-02 17:48:07 +0100

    Fix components dir for filemanager (FMElfinderBundle).

---
+++ media/assets/require.config.js
@@ -0,0 +1,33 @@
+var components = {
+    "packages": [
+        {
+            "name": "elfinder",
+            "main": "elfinder-built.js"
+        },
+        {
+            "name": "jquery",
+            "main": "jquery-built.js"
+        },
+        {
+            "name": "jquery-ui",
+            "main": "jquery-ui-built.js"
+        }
+    ],
+    "shim": {
+        "jquery-ui": {
+            "deps": [
+                "jquery"
+            ],
+            "exports": "jQuery"
+        }
+    },
+    "baseUrl": "components"
+};
+if (typeof require !== "undefined" && require.config) {
+    require.config(components);
+} else {
+    var require = components;
+}
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = components;
+}
