091176f6dc6da7a6a47e55029268e3b158e103e8
Lukas Drahy <lukas@drahy.net>
2020-01-07 13:14:14 +0100

    Fix buildViolation method call

--- app/bundles/ApiBundle/Form/Validator/Constraints/OAuthCallbackValidator.php
+++ app/bundles/ApiBundle/Form/Validator/Constraints/OAuthCallbackValidator.php
@@ -48,7 +48,7 @@ class OAuthCallbackValidator extends ConstraintValidator
 
         $value = (string) $value;
         if (!preg_match(static::PATTERN, $value)) {
-            $this->buildViolation($constraint->message)
+            $this->context->buildViolation($constraint->message)
                 ->setParameter('{{ value }}', $this->formatValue($value))
                 ->addViolation();
         }
