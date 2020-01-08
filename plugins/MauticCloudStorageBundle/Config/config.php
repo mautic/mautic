cbbd8253fbadff7b5af7c55e417402ca23e37af6
John Linhart <admin@escope.cz>
2020-01-08 10:20:21 +0100

    Rackspace integration removed

--- plugins/MauticCloudStorageBundle/Config/config.php
+++ plugins/MauticCloudStorageBundle/Config/config.php
@@ -25,9 +25,6 @@ return [
             'mautic.form.type.cloudstorage.openstack' => [
                 'class' => \MauticPlugin\MauticCloudStorageBundle\Form\Type\OpenStackType::class,
             ],
-            'mautic.form.type.cloudstorage.rackspace' => [
-                'class' => \MauticPlugin\MauticCloudStorageBundle\Form\Type\RackspaceType::class,
-            ],
         ],
         'integrations' => [
             'mautic.integration.amazons3' => [
@@ -52,26 +49,6 @@ return [
             ],
             'mautic.integration.openstack' => [
                 'class'     => \MauticPlugin\MauticCloudStorageBundle\Integration\OpenStackIntegration::class,
-                'arguments' => [
-                    'event_dispatcher',
-                    'mautic.helper.cache_storage',
-                    'doctrine.orm.entity_manager',
-                    'session',
-                    'request_stack',
-                    'router',
-                    'translator',
-                    'logger',
-                    'mautic.helper.encryption',
-                    'mautic.lead.model.lead',
-                    'mautic.lead.model.company',
-                    'mautic.helper.paths',
-                    'mautic.core.model.notification',
-                    'mautic.lead.model.field',
-                    'mautic.plugin.model.integration_entity',
-                ],
-            ],
-            'mautic.integration.rackspace' => [
-                'class'     => \MauticPlugin\MauticCloudStorageBundle\Integration\RackspaceIntegration::class,
                 'arguments' => [
                     'event_dispatcher',
                     'mautic.helper.cache_storage',
