eb3875ef5031400c4c6d2ae02cabaa255f9977dd
John Linhart <admin@escope.cz>
2020-01-08 10:21:33 +0100

    OpenStack integration removed

--- plugins/MauticCloudStorageBundle/Config/config.php
+++ plugins/MauticCloudStorageBundle/Config/config.php
@@ -21,34 +21,9 @@ return [
                 'class' => \MauticPlugin\MauticCloudStorageBundle\EventListener\RemoteAssetBrowseSubscriber::class,
             ],
         ],
-        'forms' => [
-            'mautic.form.type.cloudstorage.openstack' => [
-                'class' => \MauticPlugin\MauticCloudStorageBundle\Form\Type\OpenStackType::class,
-            ],
-        ],
         'integrations' => [
             'mautic.integration.amazons3' => [
                 'class'     => \MauticPlugin\MauticCloudStorageBundle\Integration\AmazonS3Integration::class,
-                'arguments' => [
-                    'event_dispatcher',
-                    'mautic.helper.cache_storage',
-                    'doctrine.orm.entity_manager',
-                    'session',
-                    'request_stack',
-                    'router',
-                    'translator',
-                    'logger',
-                    'mautic.helper.encryption',
-                    'mautic.lead.model.lead',
-                    'mautic.lead.model.company',
-                    'mautic.helper.paths',
-                    'mautic.core.model.notification',
-                    'mautic.lead.model.field',
-                    'mautic.plugin.model.integration_entity',
-                ],
-            ],
-            'mautic.integration.openstack' => [
-                'class'     => \MauticPlugin\MauticCloudStorageBundle\Integration\OpenStackIntegration::class,
                 'arguments' => [
                     'event_dispatcher',
                     'mautic.helper.cache_storage',
