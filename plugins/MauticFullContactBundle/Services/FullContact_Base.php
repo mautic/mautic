f15b48268f5f70fa031bd6d9ebba7f5bb2eaaaa0
John Linhart <admin@escope.cz>
2020-01-08 15:20:50 +0100

    CS fix

--- plugins/MauticFullContactBundle/Services/FullContact_Base.php
+++ plugins/MauticFullContactBundle/Services/FullContact_Base.php
@@ -121,10 +121,7 @@ class FullContact_Base
     protected function _execute($params = [], $postData = null)
     {
         if (null === $postData && !in_array($params['method'], $this->_supportedMethods, true)) {
-            throw new NotImplementedException(
-                __CLASS__.
-                ' does not support the ['.$params['method'].'] method'
-            );
+            throw new NotImplementedException(__CLASS__.' does not support the ['.$params['method'].'] method');
         }
 
         if (array_key_exists('method', $params)) {
