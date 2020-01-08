5737d053413abef2543682c8b438c595b052e108
John Linhart <admin@escope.cz>
2020-01-08 17:13:50 +0100

    Namespace of a test fixed

--- app/bundles/CoreBundle/Tests/Unit/Factory/TransifexFactoryTest.php
+++ app/bundles/CoreBundle/Tests/Unit/Factory/TransifexFactoryTest.php
@@ -9,7 +9,7 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\CoreBundle\Tests\EventListener;
+namespace Mautic\CoreBundle\Tests\Factory;
 
 use Mautic\CoreBundle\Exception\BadConfigurationException;
 use Mautic\CoreBundle\Factory\TransifexFactory;
