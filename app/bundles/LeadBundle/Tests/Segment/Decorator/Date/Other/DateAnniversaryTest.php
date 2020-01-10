mautic/mautic

commit 8226f0e60b3c8574ee8eb5a555b54614566e3f93

Removing most of deprecated code

app/bundles/FormBundle/Tests/FormTestAbstract.php
@@ -176,10 +176,6 @@ class FormTestAbstract extends WebTestCase
             ->method('getFieldListWithProperties')
             ->willReturn($mockLeadField);
 
-        $leadFieldModel->expects($this->any())
-            ->method('getUniqueIdentiferFields')
-            ->willReturn($mockLeadField);
-
         $entityManager->expects($this->any())
             ->method('getRepository')
             ->will(

app/bundles/LeadBundle/Controller/Api/LeadApiController.php
@@ -53,24 +53,6 @@ class LeadApiController extends CommonApiController
     }
 
     /**
-     * Get existing duplicated contact based on unique fields and the request data.
-     *
-     * @param array $parameters
-     * @param null  $id
-     *
-     * @return null|Lead
-     *
-     * @deprecated since 2.12.2, to be removed in 3.0.0. Use $model->checkForDuplicateContact directly instead
-     */
-    protected function getExistingLead(array $parameters, $id = null)
-    {
-        $model   = $this->getModel(self::MODEL_ID);
-        $contact = $id ? $model->getEntity($id) : null;
-
-        return $model->checkForDuplicateContact($parameters, $contact);
-    }
-
-    /**
      * Obtains a list of users for lead owner edits.
      *
      * @return \Symfony\Component\HttpFoundation\Response
@@ -535,35 +517,6 @@ class LeadApiController extends CommonApiController
     public function removeUtmTagsAction($id, $utmid)
     {
         return $this->applyUtmTagsAction($id, 'removeUtmTags', (int) $utmid);
-    }
-
-    /**
-     * Obtains a list of contact events.
-     *
-     * @deprecated 2.10.0 to be removed in 3.0
-     *
-     * @param $id
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function getEventsAction($id)
-    {
-        $entity = $this->model->getEntity($id);
-
-        if ($entity === null) {
-            return $this->notFound();
-        }
-
-        if (!$this->checkEntityAccess($entity, 'view')) {
-            return $this->accessDenied();
-        }
-
-        $filters = $this->sanitizeEventFilter(InputHelper::clean($this->request->get('filters', [])));
-        $order   = InputHelper::clean($this->request->get('order', ['timestamp', 'DESC']));
-        $page    = (int) $this->request->get('page', 1);
-        $events  = $this->model->getEngagements($entity, $filters, $order, $page);
-
-        return $this->handleView($this->view($events));
     }
 
     /**

app/bundles/LeadBundle/Entity/CompanyLead.php
@@ -14,9 +14,6 @@ namespace Mautic\LeadBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Mautic\CoreBundle\Doctrine\Mapping\ClassMetadataBuilder;
 
-/**
- * Class CompanyLead.
- */
 class CompanyLead
 {
     /**
@@ -40,20 +37,6 @@ class CompanyLead
     private $primary = false;
 
     /**
-     * @deprecated 2.9 to be removed in 3.0
-     *
-     * @var bool
-     */
-    private $manuallyRemoved = false;
-
-    /**
-     * @deprecated 2.9 to be removed in 3.0
-     *
-     * @var bool
-     */
-    private $manuallyAdded = false;
-
-    /**
      * @param ORM\ClassMetadata $metadata
      */
     public static function loadMetadata(ORM\ClassMetadata $metadata)
@@ -61,7 +44,7 @@ class CompanyLead
         $builder = new ClassMetadataBuilder($metadata);
 
         $builder->setTable('companies_leads')
-            ->setCustomRepositoryClass('Mautic\LeadBundle\Entity\CompanyLeadRepository');
+            ->setCustomRepositoryClass(CompanyLeadRepository::class);
 
         $builder->createManyToOne('company', 'Company')
             ->isPrimaryKey()
@@ -75,15 +58,6 @@ class CompanyLead
         $builder->createField('primary', 'boolean')
             ->columnName('is_primary')
             ->nullable()
-            ->build();
-
-        // @deprecated 2.9 to be removed in 3.0
-        $builder->createField('manuallyRemoved', 'boolean')
-            ->columnName('manually_removed')
-            ->build();
-
-        $builder->createField('manuallyAdded', 'boolean')
-            ->columnName('manually_added')
             ->build();
     }
 
@@ -157,65 +131,5 @@ class CompanyLead
     public function getPrimary()
     {
         return $this->primary;
-    }
-
-    /**
-     * @deprecatd 2.9 to be removed in 3.0
-     *
-     * @return bool
-     */
-    public function getManuallyRemoved()
-    {
-        return $this->manuallyRemoved;
-    }
-
-    /**
-     * @deprecatd 2.9 to be removed in 3.0
-     *
-     * @param bool $manuallyRemoved
-     */
-    public function setManuallyRemoved($manuallyRemoved)
-    {
-        $this->manuallyRemoved = $manuallyRemoved;
-    }
-
-    /**
-     * @deprecatd 2.9 to be removed in 3.0
-     *
-     * @return bool
-     */
-    public function wasManuallyRemoved()
-    {
-        return $this->manuallyRemoved;
-    }
-
-    /**
-     * @deprecatd 2.9 to be removed in 3.0
-     *
-     * @return bool
-     */
-    public function getManuallyAdded()
-    {
-        return $this->manuallyAdded;
-    }
-
-    /**
-     * @deprecatd 2.9 to be removed in 3.0
-     *
-     * @param bool $manuallyAdded
-     */
-    public function setManuallyAdded($manuallyAdded)
-    {
-        $this->manuallyAdded = $manuallyAdded;
-    }
-
-    /**
-     * @deprecatd 2.9 to be removed in 3.0
-     *
-     * @return bool
-     */
-    public function wasManuallyAdded()
-    {
-        return $this->manuallyAdded;
     }
 }

app/bundles/LeadBundle/Entity/Lead.php
@@ -121,7 +121,7 @@ class Lead extends FormEntity implements CustomFieldEntityInterface
     private $country;
 
     /**
-     * @var \Mautic\UserBundle\Entity\User
+     * @var User
      */
     private $owner;
 
@@ -235,7 +235,7 @@ class Lead extends FormEntity implements CustomFieldEntityInterface
     private $tags;
 
     /**
-     * @var \Mautic\StageBundle\Entity\Stage
+     * @var Stage
      */
     private $stage;
 
@@ -250,7 +250,7 @@ class Lead extends FormEntity implements CustomFieldEntityInterface
     private $utmtags;
 
     /**
-     * @var \Mautic\LeadBundle\Entity\FrequencyRule[]
+     * @var FrequencyRule[]
      */
     private $frequencyRules;
 
@@ -1157,21 +1157,6 @@ class Lead extends FormEntity implements CustomFieldEntityInterface
             'comments' => $doNotContact->getComments(),
         ];
 
-        // @deprecated - to be removed in 2.0
-        switch ($doNotContact->getReason()) {
-            case DoNotContact::BOUNCED:
-                $type = 'bounced';
-                break;
-            case DoNotContact::MANUAL:
-                $type = 'manual';
-                break;
-            case DoNotContact::UNSUBSCRIBED:
-            default:
-                $type = 'unsubscribed';
-                break;
-        }
-        $this->changes['dnc_status'] = [$type, $doNotContact->getComments()];
-
         $this->doNotContact[$doNotContact->getChannel()] = $doNotContact;
 
         return $this;
@@ -1187,9 +1172,6 @@ class Lead extends FormEntity implements CustomFieldEntityInterface
             'old_reason' => $doNotContact->getReason(),
             'comments'   => $doNotContact->getComments(),
         ];
-
-        // @deprecated to be removed in 2.0
-        $this->changes['dnc_status'] = ['removed', $doNotContact->getComments()];
 
         $this->doNotContact->removeElement($doNotContact);
     }

app/bundles/LeadBundle/Entity/LeadDevice.php
@@ -442,24 +442,4 @@ class LeadDevice
     {
         $this->dateAdded = $dateAdded;
     }
-
-    /**
-     * @return mixed
-     *
-     * @deprecated 2.4.0 to be removed 3.0; use getDateAdded instead
-     */
-    public function getDateOpen()
-    {
-        return $this->getDateAdded();
-    }
-
-    /**
-     * @param mixed $dateOpen
-     *
-     * @deprecated 2.4.0 to be removed 3.0; use setDateAdded instead
-     */
-    public function setDateOpen($dateOpen)
-    {
-        $this->setDateAdded($dateOpen);
-    }
 }

app/bundles/LeadBundle/Entity/LeadEventLogRepository.php
@@ -15,9 +15,6 @@ use Doctrine\DBAL\Connection;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Mautic\CoreBundle\Entity\CommonRepository;
 
-/**
- * Class LeadEventLogRepository.
- */
 class LeadEventLogRepository extends CommonRepository
 {
     use TimelineTrait;
@@ -161,42 +158,5 @@ class LeadEventLogRepository extends CommonRepository
     public function getTableAlias()
     {
         return 'lel';
-    }
-
-    /**
-     * Loads data for specified lead events.
-     *
-     * @deprecated 2.14.1 to be removed in 3.0; use getEvents() instead
-     *
-     * @param string    $bundle
-     * @param string    $object
-     * @param Lead|null $lead
-     * @param array     $options
-     *
-     * @return array
-     */
-    public function getEventsByLead($bundle, $object, Lead $lead = null, array $options = [])
-    {
-        trigger_error('LeadEventLogRepository::getEventsByLead is deprecated. Use LeadEventLogRepository::getEvents instead', E_USER_DEPRECATED);
-
-        return $this->getEvents($lead, $bundle, $object, null, $options);
-    }
-
-    /**
-     * Loads data for specified lead events by action.
-     *
-     * @deprecated 2.14.1 to be removed in 3.0; use getEvents() instead
-     *
-     * @param           $action
-     * @param Lead|null $lead
-     * @param array     $options
-     *
-     * @return array
-     */
-    public function getEventsByAction($action, Lead $lead = null, array $options = [])
-    {
-        trigger_error('LeadEventLogRepository::getEventsByAction is deprecated. Use LeadEventLogRepository::getEvents instead', E_USER_DEPRECATED);
-
-        return $this->getEvents($lead, null, null, $action, $options);
     }
 }

app/bundles/LeadBundle/Entity/LeadListRepository.php
@@ -16,7 +16,6 @@ use Doctrine\DBAL\Types\DateType;
 use Doctrine\DBAL\Types\FloatType;
 use Doctrine\DBAL\Types\IntegerType;
 use Doctrine\DBAL\Types\TimeType;
-use Doctrine\ORM\PersistentCollection;
 use Mautic\CoreBundle\Doctrine\QueryFormatter\AbstractFormatter;
 use Mautic\CoreBundle\Doctrine\Type\UTCDateTimeType;
 use Mautic\CoreBundle\Entity\CommonRepository;
@@ -26,7 +25,6 @@ use Mautic\CoreBundle\Helper\Serializer;
 use Mautic\LeadBundle\Event\LeadListFilteringEvent;
 use Mautic\LeadBundle\Event\LeadListFiltersOperatorsEvent;
 use Mautic\LeadBundle\LeadEvents;
-use Monolog\Logger;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
@@ -348,298 +346,6 @@ class LeadListRepository extends CommonRepository
         $now = \DateTime::createFromFormat('U.u', number_format($inputSeconds, 6, '.', ''));
 
         return $now->format('H:i:s.u');
-    }
-
-    /**
-     * @deprecated in 2.14, to be removed in Mautic 3 - Use methods in the ContactSegmentService class
-     *
-     * @param        $lists
-     * @param array  $args
-     * @param Logger $logger
-     *
-     * @return array
-     */
-    public function getLeadsByList($lists, $args = [], Logger $logger = null)
-    {
-        // Return only IDs
-        $idOnly = (!array_key_exists('idOnly', $args)) ? false : $args['idOnly']; //Always TRUE
-        // Return counts
-        $countOnly = (!array_key_exists('countOnly', $args)) ? false : $args['countOnly'];
-        // Return only leads that have not been added or manually manipulated to the lists yet
-        $newOnly = (!array_key_exists('newOnly', $args)) ? false : $args['newOnly'];
-        // Return leads that do not belong to a list based on filters
-        $nonMembersOnly = (!array_key_exists('nonMembersOnly', $args)) ? false : $args['nonMembersOnly'];
-        // Use filters to dynamically generate the list
-        $dynamic = ($newOnly || $nonMembersOnly || (!$newOnly && !$nonMembersOnly && $countOnly)); ///This is always true - we can ommit conditions
-        // Limiters
-        $batchLimiters = (!array_key_exists('batchLimiters', $args)) ? false : $args['batchLimiters'];
-        $start         = (!array_key_exists('start', $args)) ? false : $args['start'];
-        $limit         = (!array_key_exists('limit', $args)) ? false : $args['limit'];
-        $withMinId     = (!array_key_exists('withMinId', $args)) ? false : $args['withMinId'];
-
-        if ((!($lists instanceof PersistentCollection) && !is_array($lists)) || isset($lists['id'])) {
-            $lists = [$lists];
-        }
-
-        $return = [];
-        foreach ($lists as $l) {
-            $leads = ($countOnly) ? 0 : [];
-
-            if ($l instanceof LeadList) {
-                $id      = $l->getId();
-                $filters = $l->getFilters();
-            } elseif (is_array($l)) {
-                $id      = $l['id'];
-                $filters = (!$dynamic) ? [] : $l['filters'];
-            } elseif (!$dynamic) {
-                $id      = $l;
-                $filters = [];
-            }
-
-            $parameters = [];
-
-            if ($dynamic && count($filters)) {
-                $q = $this->getEntityManager()->getConnection()->createQueryBuilder();
-                if ($countOnly) {
-                    $count  = 'count(distinct(l.id))';
-                    $select = $count.' as lead_count, max(l.id) as max_id';
-                    if ($withMinId) {
-                        $select .= ', min(l.id) as min_id';
-                    }
-                } elseif ($idOnly) {
-                    $select = 'l.id';
-                } else {
-                    $select = 'l.*';
-                }
-
-                $q->select($select)
-                    ->from(MAUTIC_TABLE_PREFIX.'leads', 'l');
-
-                $batchExpr = $q->expr()->andX();
-                // Only leads that existed at the time of count
-                if ($batchLimiters) {
-                    if (!empty($batchLimiters['minId']) && !empty($batchLimiters['maxId'])) {
-                        $batchExpr->add(
-                            $q->expr()->comparison('l.id', 'BETWEEN', "{$batchLimiters['minId']} and {$batchLimiters['maxId']}")
-                        );
-                    } elseif (!empty($batchLimiters['maxId'])) {
-                        $batchExpr->add(
-                            $q->expr()->lte('l.id', $batchLimiters['maxId'])
-                        );
-                    }
-                }
-
-                if ($newOnly || !$nonMembersOnly) { // !$nonMembersOnly is mainly used for tests as we just want a live count
-                    $expr = $this->generateSegmentExpression($filters, $parameters, $q, null, $id);
-                    if (!$this->hasCompanyFilter && !$expr->count()) {
-                        // Treat this as if it has no filters since all the filters are now invalid (fields were deleted)
-                        $return[$id] = [];
-                        if ($countOnly) {
-                            $return[$id] = [
-                                'count' => 0,
-                                'maxId' => 0,
-                            ];
-                            if ($withMinId) {
-                                $return[$id]['minId'] = 0;
-                            }
-                        }
-
-                        continue;
-                    }
-
-                    // Leads that do not have any record in the lead_lists_leads table for this lead list
-                    // For non null fields - it's apparently better to use left join over not exists due to not using nullable
-                    // fields - https://explainextended.com/2009/09/18/not-in-vs-not-exists-vs-left-join-is-null-mysql/
-                    $listOnExpr = $q->expr()->andX(
-                        $q->expr()->eq('ll.leadlist_id', $id),
-                        $q->expr()->eq('ll.lead_id', 'l.id')
-                    );
-
-                    if (!empty($batchLimiters['dateTime'])) {
-                        // Only leads in the list at the time of count
-                        $listOnExpr->add(
-                            $q->expr()->lte('ll.date_added', $q->expr()->literal($batchLimiters['dateTime']))
-                         );
-                    }
-
-                    $q->leftJoin(
-                        'l',
-                        MAUTIC_TABLE_PREFIX.'lead_lists_leads',
-                        'll',
-                        $listOnExpr
-                    );
-
-                    if ($newOnly) {
-                        $expr->add($q->expr()->isNull('ll.lead_id'));
-                    }
-
-                    if ($batchExpr->count()) {
-                        $expr->add($batchExpr);
-                    }
-
-                    if ($expr->count()) {
-                        $q->andWhere($expr);
-                    }
-
-                    if (!$newOnly) { // live count
-                        // Include manually added
-                        $q->orWhere(
-                            $q->expr()->eq('ll.manually_added', 1)
-                        );
-
-                        $q->andWhere(
-                            $q->expr()->orX(
-                                $q->expr()->isNull('ll.manually_removed'), // account for those not in a list yet
-                                $q->expr()->eq('ll.manually_removed', 0) //exclude manually removed
-                            )
-                        );
-                    }
-                } elseif ($nonMembersOnly) {
-                    // Only leads that are part of the list that no longer match filters and have not been manually removed
-                    $q->join('l', MAUTIC_TABLE_PREFIX.'lead_lists_leads', 'll', 'l.id = ll.lead_id');
-
-                    $mainExpr = $q->expr()->andX();
-                    if ($batchLimiters && !empty($batchLimiters['dateTime'])) {
-                        // Only leads in the list at the time of count
-                        $mainExpr->add(
-                            $q->expr()->lte('ll.date_added', $q->expr()->literal($batchLimiters['dateTime']))
-                        );
-                    }
-
-                    // Ignore those that have been manually added
-                    $mainExpr->addMultiple(
-                        [
-                            $q->expr()->eq('ll.manually_added', ':false'),
-                            $q->expr()->eq('ll.leadlist_id', (int) $id),
-                        ]
-                    );
-                    $q->setParameter('false', false, 'boolean');
-
-                    // Find the contacts that are in the segment but no longer have filters that are applicable
-                    $sq = $this->getEntityManager()->getConnection()->createQueryBuilder();
-                    $sq->select('l.id')
-                        ->from(MAUTIC_TABLE_PREFIX.'leads', 'l');
-
-                    $expr = $this->generateSegmentExpression($filters, $parameters, $sq, $q);
-
-                    if ($expr->count()) {
-                        $sq->andWhere($expr);
-                    }
-                    $mainExpr->add(
-                        sprintf('l.id NOT IN (%s)', $sq->getSQL())
-                    );
-
-                    if ($batchExpr->count()) {
-                        $mainExpr->add($batchExpr);
-                    }
-
-                    if (!empty($mainExpr) && $mainExpr->count() > 0) {
-                        $q->andWhere($mainExpr);
-                    }
-                }
-
-                // Set limits if applied
-                if (!empty($limit)) {
-                    $q->setFirstResult($start)
-                        ->setMaxResults($limit);
-                }
-
-                if ($countOnly) {
-                    // remove any possible group by
-                    $q->resetQueryPart('groupBy');
-                }
-
-                $params  = $q->getParameters();
-                $sqlT    = $q->getSQL();
-                foreach ($params as $key=>$val) {
-                    if (!is_int($val) and !is_float($val) and !is_array($val)) {
-                        $val = "'$val'";
-                    }
-                    if (is_array($val)) {
-                        $val = join(',', $val);
-                    }
-                    $sqlT                    = str_replace(":{$key}", $val, $sqlT);
-                }
-
-                if (null !== $logger) {
-                    $logger->debug(sprintf('Old version SQL: %s', $sqlT));
-                }
-                $timer   = microtime(true);
-                $results = $q->execute()->fetchAll();
-                $timer   = microtime(true) - $timer;
-                if (null !== $logger) {
-                    $logger->debug(sprintf('Old version SQL took: %s', $this->format_period($timer)));
-                }
-
-                foreach ($results as $r) {
-                    if ($countOnly) {
-                        $leads = [
-                            'count' => $r['lead_count'],
-                            'maxId' => $r['max_id'],
-                        ];
-                        if ($withMinId) {
-                            $leads['minId'] = $r['min_id'];
-                        }
-                    } elseif ($idOnly) {
-                        $leads[$r['id']] = $r['id'];
-                    } else {
-                        $leads[$r['id']] = $r;
-                    }
-                }
-            } elseif (!$dynamic) {
-                $q = $this->getEntityManager()->getConnection()->createQueryBuilder();
-                if ($countOnly) {
-                    $q->select('max(ll.lead_id) as max_id, count(ll.lead_id) as lead_count')
-                        ->from(MAUTIC_TABLE_PREFIX.'lead_lists_leads', 'll');
-                } elseif ($idOnly) {
-                    $q->select('ll.lead_id as id')
-                        ->from(MAUTIC_TABLE_PREFIX.'lead_lists_leads', 'll');
-                } else {
-                    $q->select('l.*')
-                        ->from(MAUTIC_TABLE_PREFIX.'leads', 'l')
-                        ->join('l', MAUTIC_TABLE_PREFIX.'lead_lists_leads', 'll', 'l.id = ll.lead_id');
-                }
-
-                // Filter by list
-                $expr = $q->expr()->andX(
-                    $q->expr()->eq('ll.leadlist_id', ':list'),
-                    $q->expr()->eq('ll.manually_removed', ':false')
-                );
-
-                $q->setParameter('list', (int) $id)
-                    ->setParameter('false', false, 'boolean');
-
-                // Set limits if applied
-                if (!empty($limit)) {
-                    $q->setFirstResult($start)
-                        ->setMaxResults($limit);
-                }
-                if (!empty($expr) && $expr->count() > 0) {
-                    $q->where($expr);
-                }
-
-                $results = $q->execute()->fetchAll();
-
-                foreach ($results as $r) {
-                    if ($countOnly) {
-                        $leads = [
-                            'count' => $r['lead_count'],
-                            'maxId' => $r['max_id'],
-                        ];
-                    } elseif ($idOnly) {
-                        $leads[] = $r['id'];
-                    } else {
-                        $leads[] = $r;
-                    }
-                }
-            }
-
-            $return[$id] = $leads;
-
-            unset($filters, $parameters, $q, $expr, $results, $dynamicExpr, $leads);
-        }
-
-        return $return;
     }
 
     /**

app/bundles/LeadBundle/Entity/PointsChangeLogRepository.php
@@ -14,9 +14,6 @@ namespace Mautic\LeadBundle\Entity;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Mautic\CoreBundle\Entity\CommonRepository;
 
-/**
- * PointsChangeLogRepository.
- */
 class PointsChangeLogRepository extends CommonRepository
 {
     use TimelineTrait;
@@ -87,31 +84,6 @@ class PointsChangeLogRepository extends CommonRepository
         $results = $query->execute()->fetchAll();
 
         return $results;
-    }
-
-    /**
-     * Count a value in a column.
-     *
-     * @param QueryBuilder $query
-     *
-     * @return array
-     *
-     * @throws \Doctrine\ORM\NoResultException
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     *
-     * @deprecated 2.10 - to be removed in 3.0 - never used in the codebase
-     */
-    public function countValue(QueryBuilder $query, $column, $value)
-    {
-        $query->select('count('.$column.') as quantity')
-            ->from(MAUTIC_TABLE_PREFIX.'leads', 'l')
-            ->leftJoin('l', MAUTIC_TABLE_PREFIX.'lead_points_change_log', 'lp', 'lp.lead_id = l.id')
-            ->andwhere($query->expr()->eq($column, ':value'))
-            ->setParameter('value', $value);
-
-        $result = $query->execute()->fetch();
-
-        return $result['quantity'];
     }
 
     /**

commit 4aa56d24c6ca0a6c128d25b8ca3d9862766905d4

Fix DateAnniversaryTest:testGetParameterValue
Caused by commit 8226f0e60b3c8574ee8eb5a555b54614566e3f93

app/bundles/LeadBundle/Tests/Segment/Decorator/Date/Other/DateAnniversaryTest.php
@@ -46,14 +46,22 @@ class DateAnniversaryTest extends \PHPUnit\Framework\TestCase
      */
     public function testGetParameterValue()
     {
+        /**
+         * Today in '%-m-d' format.
+         *
+         * @var string
+         */
+        $expectedResult = '%'.(new \DateTime(null, new \DateTimeZone('UTC')))->format('-m-d');
+
         $dateDecorator    = $this->createMock(DateDecorator::class);
         $timezoneResolver = $this->createMock(TimezoneResolver::class);
 
-        $date = new DateTimeHelper('2018-03-02', null, 'local');
-
         $timezoneResolver->method('getDefaultDate')
-            ->with()
-            ->willReturn($date);
+            ->with(false)
+            ->willReturn(
+                new DateTimeHelper(
+                    new \DateTime('midnight today', new \DateTimeZone('UTC')), null, 'UTC')
+            );
 
         $filter        = [
             'operator' => '=',
@@ -65,6 +73,6 @@ class DateAnniversaryTest extends \PHPUnit\Framework\TestCase
 
         $filterDecorator = new DateAnniversary($dateDecorator, $dateOptionParameters);
 
-        $this->assertEquals('%-03-02', $filterDecorator->getParameterValue($contactSegmentFilterCrate));
+        $this->assertEquals($expectedResult, $filterDecorator->getParameterValue($contactSegmentFilterCrate));
     }
 }