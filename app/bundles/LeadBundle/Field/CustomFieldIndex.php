d896ba1d1b8f764d7cc152a96f5532557ab5994a
Alan Hartless <alan@devkardia.com>
2020-02-01 12:16:34 -0600

    Update unique identifier index when it no longer matches field configuration

--- app/bundles/LeadBundle/Field/CustomFieldIndex.php
+++ app/bundles/LeadBundle/Field/CustomFieldIndex.php
@@ -36,19 +36,7 @@ class CustomFieldIndex
             $modifySchema->addIndex([$alias], $alias.'_search');
             $modifySchema->allowColumn($alias);
 
-            if ($leadField->getIsUniqueIdentifier()) {
-                // Get list of current uniques
-                $uniqueIdentifierFields = $this->fieldsWithUniqueIdentifier->getFieldsWithUniqueIdentifier();
-
-                // Always use email
-                $indexColumns   = ['email'];
-                $indexColumns   = array_merge($indexColumns, array_keys($uniqueIdentifierFields));
-                $indexColumns[] = $alias;
-
-                // Only use three to prevent max key length errors
-                $indexColumns = array_slice($indexColumns, 0, 3);
-                $modifySchema->addIndex($indexColumns, 'unique_identifier_search');
-            }
+            $this->updateUniqueIdentifierIndex($leadField);
 
             $modifySchema->executeChanges();
         } catch (DriverException $e) {
@@ -99,6 +87,10 @@ class CustomFieldIndex
             return true;
         }
 
+        if (!$this->hasMatchingUniqueIdentifierIndex($leadField)) {
+            return true;
+        }
+
         return false;
     }
 
@@ -108,5 +100,98 @@ class CustomFieldIndex
     public function hasIndex(LeadField $leadField): bool
     {
         return $this->indexSchemaHelper->hasIndex($leadField);
+    }
+
+    public function hasMatchingUniqueIdentifierIndex(LeadField $leadField)
+    {
+        $hasIndex = $this->indexSchemaHelper->hasUniqueIdentifierIndex($leadField);
+        $isUniqueIdentifier = $leadField->getIsUniqueIdentifier();
+
+        if ($isUniqueIdentifier && !$hasIndex)
+        {
+            return false;
+        }
+
+        if (!$isUniqueIdentifier && $hasIndex)
+        {
+            return false;
+        }
+
+        $uniqueIdentifierColumns = $this->getUniqueIdentifierIndexColumns();
+        if ($uniqueIdentifierColumns && !$hasIndex) {
+            return false;
+        }
+
+        if (!$uniqueIdentifierColumns && $hasIndex) {
+            return false;
+        }
+
+        if (!$uniqueIdentifierColumns && !$hasIndex) {
+            return true;
+        }
+
+        return $this->indexSchemaHelper->hasMatchingUniqueIdentifierIndex($leadField, $uniqueIdentifierColumns);
+    }
+
+    /**
+     * @param LeadField $leadField
+     *
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     * @throws \Mautic\CoreBundle\Exception\SchemaException
+     */
+    public function updateUniqueIdentifierIndex(LeadField $leadField)
+    {
+        if ($this->hasMatchingUniqueIdentifierIndex($leadField)) {
+            return;
+        }
+
+        /** @var \Mautic\CoreBundle\Doctrine\Helper\IndexSchemaHelper $modifySchema */
+        $modifySchema = $this->indexSchemaHelper->setName($leadField->getCustomFieldObject());
+
+        $indexColumns = $this->getUniqueIdentifierIndexColumns();
+        if (!$indexColumns) {
+            $this->dropIndexForUniqueIdentifiers($leadField);
+
+            return;
+        }
+
+        $modifySchema->addIndex($indexColumns, 'unique_identifier_search');
+        $modifySchema->executeChanges();
+    }
+
+    /**
+     * @param LeadField $leadField
+     *
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     * @throws \Mautic\CoreBundle\Exception\SchemaException
+     */
+    private function dropIndexForUniqueIdentifiers(LeadField $leadField)
+    {
+        if (!$this->indexSchemaHelper->hasUniqueIdentifierIndex($leadField)) {
+            return;
+        }
+
+        /** @var IndexSchemaHelper $modifySchema */
+        $modifySchema = $this->indexSchemaHelper->setName($leadField->getCustomFieldObject());
+
+        $indexColumns = $this->getUniqueIdentifierIndexColumns();
+        $modifySchema->dropIndex($indexColumns, 'unique_identifier_search');
+
+        $modifySchema->executeChanges();
+    }
+
+    /**
+     * @return array
+     */
+    private function getUniqueIdentifierIndexColumns()
+    {
+        // Get list of current uniques
+        $uniqueIdentifierFields = $this->fieldsWithUniqueIdentifier->getFieldsWithUniqueIdentifier();
+
+        // Always use email
+        $indexColumns = array_keys($uniqueIdentifierFields);
+
+        // Only use three to prevent max key length errors
+        return array_slice($indexColumns, 0, 3);
     }
 }
