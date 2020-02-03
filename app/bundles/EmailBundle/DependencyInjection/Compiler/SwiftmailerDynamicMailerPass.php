ac3e9be4431b2bd0c81203e803288e47b8984dc7
Alan Hartless <alan@devkardia.com>
2020-02-03 13:51:38 -0600

    Prevent exception for Jenkins running tests since a mailer is disabled in test environment

--- app/bundles/EmailBundle/DependencyInjection/Compiler/SwiftmailerDynamicMailerPass.php
+++ app/bundles/EmailBundle/DependencyInjection/Compiler/SwiftmailerDynamicMailerPass.php
@@ -20,6 +20,10 @@ class SwiftmailerDynamicMailerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
+        if (!$container->hasDefinition('swiftmailer.mailer.default.transport.dynamic')) {
+            return;
+        }
+
         $definitionDecorator = $container->getDefinition('swiftmailer.mailer.default.transport.dynamic');
         $definitionDecorator->setFactory([SwiftmailerTransportFactory::class, 'createTransport']);
         $definitionDecorator->setArgument(3, new Reference('service_container'));
