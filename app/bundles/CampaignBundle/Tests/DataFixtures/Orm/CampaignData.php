f360348bb22bc8d7df175b1160069f5f1827e301
John Linhart <admin@escope.cz>
2020-02-04 10:36:39 +0100

    Creating functional test that shows the error

---
+++ app/bundles/CampaignBundle/Tests/DataFixtures/Orm/CampaignData.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * @copyright   2017 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CampaignBundle\Tests\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Mautic\CampaignBundle\Entity\Campaign;
+
+class CampaignData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $campaign = new Campaign();
+
+        $campaign->setName('Campaign A');
+        $campaign->setCanvasSettings([
+            'nodes' => [
+              0 => [
+                'id'        => '148',
+                'positionX' => '760',
+                'positionY' => '155',
+              ],
+              1 => [
+                'id'        => 'lists',
+                'positionX' => '860',
+                'positionY' => '50',
+              ],
+            ],
+            'connections' => [
+              0 => [
+                'sourceId' => 'lists',
+                'targetId' => '148',
+                'anchors'  => [
+                  'source' => 'leadsource',
+                  'target' => 'top',
+                ],
+              ],
+            ],
+          ]
+        );
+
+        $manager->persist($campaign);
+        $manager->flush();
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 0;
+    }
+}
