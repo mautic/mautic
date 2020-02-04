7b603860a7f0deed477c433ebde2846847c4125d
John Linhart <admin@escope.cz>
2020-02-04 13:56:24 +0100

    Fixing convertPHPSizeToBytes back after Rector's wrong dead-code minification.
    See https://github.com/rectorphp/rector/issues/2730

--- app/bundles/CoreBundle/Tests/Unit/Helper/FileHelperTest.php
+++ app/bundles/CoreBundle/Tests/Unit/Helper/FileHelperTest.php
@@ -21,11 +21,8 @@ class FileHelperTest extends \PHPUnit\Framework\TestCase
      * @covers       \Mautic\CoreBundle\Helper\FileHelper::convertBytesToMegabytes
      *
      * @dataProvider bytesToMegabytesProvider
-     *
-     * @param int   $byte
-     * @param float $megabyte
      */
-    public function testConversionFromBytesToMegabytes($byte, $megabyte)
+    public function testConversionFromBytesToMegabytes(int $byte, float $megabyte)
     {
         $fileHelper = new FileHelper();
 
@@ -48,11 +45,8 @@ class FileHelperTest extends \PHPUnit\Framework\TestCase
      * @covers       \Mautic\CoreBundle\Helper\FileHelper::convertMegabytesToBytes
      *
      * @dataProvider megabytesToBytesProvider
-     *
-     * @param int $megabyte
-     * @param int $byte
      */
-    public function testConversionFromMegabytesToBytes($megabyte, $byte)
+    public function testConversionFromMegabytesToBytes(int $megabyte, int $byte)
     {
         $fileHelper = new FileHelper();
 
@@ -65,6 +59,34 @@ class FileHelperTest extends \PHPUnit\Framework\TestCase
             [0, 0],
             [1, 1048576],
             [5, 5242880],
+        ];
+    }
+
+    /**
+     * @testdox Conversion of PHP size to Bytes
+     *
+     * @covers       \Mautic\CoreBundle\Helper\FileHelper::convertPHPSizeToBytes
+     *
+     * @dataProvider phpSizeToBytesProvider
+     */
+    public function testConvertPHPSizeToBytes(string $phpSize, int $bytes)
+    {
+        $fileHelper = new FileHelper();
+
+        $this->assertSame($bytes, $fileHelper::convertPHPSizeToBytes($phpSize));
+    }
+
+    public function phpSizeToBytesProvider()
+    {
+        return [
+            ['3048M', 3196059648],
+            ['127M', 133169152],
+            ['1k', 1024],
+            ['1K ', 1024],
+            ['1M', 1048576],
+            ['1G', 1073741824],
+            ['1P', 1125899906842624],
+            ['1024', 1024],
         ];
     }
 }
