e5e3d56fed32cb240cf3ce851348a96fed6cc4c0
John Linhart <admin@escope.cz>
2020-02-05 13:08:20 +0100

    Update the migration for M3

--- app/migrations/Version20181111095447.php
+++ app/migrations/Version20181111095447.php
@@ -10,32 +10,28 @@
 
 namespace Mautic\Migrations;
 
-use Doctrine\DBAL\Migrations\SkipMigrationException;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Exception\SkipMigration;
 use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 use Mautic\CoreBundle\Helper\EncryptionHelper;
 
 class Version20181111095447 extends AbstractMauticMigration
 {
-    /**
-     * @throws SkipMigrationException
-     * @throws \Doctrine\DBAL\Schema\SchemaException
-     */
-    public function preUp(Schema $schema)
+    public function preUp(Schema $schema): void
     {
         if ($schema->getTable($this->prefix.'webhooks')->hasColumn('secret')) {
-            throw new SkipMigrationException('Schema includes this migration');
+            throw new SkipMigration('Schema includes this migration');
         }
     }
 
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         $secret = EncryptionHelper::generateKey();
         $this->addSql("ALTER TABLE {$this->prefix}webhooks ADD secret VARCHAR(255) DEFAULT NULL");
         $this->addSql("UPDATE {$this->prefix}webhooks SET secret = '{$secret}' WHERE secret IS NULL;");
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         $this->addSql("ALTER TABLE {$this->prefix}webhooks DROP COLUMN secret");
     }
