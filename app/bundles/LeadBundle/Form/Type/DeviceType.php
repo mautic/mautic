347425fc79eaa2c0a024852f9544e6744fb55020
Alan Hartless <alan@devkardia.com>
2020-02-07 17:45:41 -0600

    Fixed DeviceType's use of `text` to `TextType::class`

--- app/bundles/LeadBundle/Form/Type/DeviceType.php
+++ app/bundles/LeadBundle/Form/Type/DeviceType.php
@@ -14,6 +14,7 @@ namespace Mautic\LeadBundle\Form\Type;
 use Mautic\CoreBundle\Form\Type\FormButtonsType;
 use Mautic\LeadBundle\Entity\LeadDevice;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -21,18 +22,22 @@ class DeviceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('device', 'text');
-        $builder->add('deviceOsName', 'text');
-        $builder->add('deviceOsShortName', 'text');
-        $builder->add('deviceOsVersion', 'text');
-        $builder->add('deviceOsPlatform', 'text');
-        $builder->add('deviceModel', 'text');
-        $builder->add('deviceBrand', 'text');
+        $builder->add('device', TextType::class);
+        $builder->add('deviceOsName', TextType::class);
+        $builder->add('deviceOsShortName', TextType::class);
+        $builder->add('deviceOsVersion', TextType::class);
+        $builder->add('deviceOsPlatform', TextType::class);
+        $builder->add('deviceModel', TextType::class);
+        $builder->add('deviceBrand', TextType::class);
 
-        $builder->add('buttons', FormButtonsType::class, [
-            'apply_text' => false,
-            'save_text'  => 'mautic.core.form.save',
-        ]);
+        $builder->add(
+            'buttons',
+            FormButtonsType::class,
+            [
+                'apply_text' => false,
+                'save_text'  => 'mautic.core.form.save',
+            ]
+        );
 
         if (!empty($options['action'])) {
             $builder->setAction($options['action']);
@@ -41,9 +46,11 @@ class DeviceType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => LeadDevice::class,
-        ]);
+        $resolver->setDefaults(
+            [
+                'data_class' => LeadDevice::class,
+            ]
+        );
     }
 
     /**
