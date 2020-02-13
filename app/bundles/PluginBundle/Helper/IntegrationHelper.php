47e99b99c715771515cf6ca195372f2748642569
Alan Hartless <alan@devkardia.com>
2020-02-12 21:35:12 -0600

    Fixed AbstractIntegration to be UnifiedIntegrationInterface in order to support icons for plugins based on IntegrationsBundle

--- app/bundles/PluginBundle/Helper/IntegrationHelper.php
+++ app/bundles/PluginBundle/Helper/IntegrationHelper.php
@@ -21,6 +21,7 @@ use Mautic\CoreBundle\Helper\TemplatingHelper;
 use Mautic\PluginBundle\Entity\Integration;
 use Mautic\PluginBundle\Entity\Plugin;
 use Mautic\PluginBundle\Integration\AbstractIntegration;
+use Mautic\PluginBundle\Integration\UnifiedIntegrationInterface;
 use Mautic\PluginBundle\Model\PluginModel;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\Finder\Finder;
@@ -722,7 +723,7 @@ class IntegrationHelper
         } elseif ($integration instanceof Plugin) {
             // A bundle so check for an icon
             $icon = $pluginPath.'/'.$integration->getBundle().'/Assets/img/icon.png';
-        } elseif ($integration instanceof AbstractIntegration) {
+        } elseif ($integration instanceof UnifiedIntegrationInterface) {
             return $integration->getIcon();
         }
 
