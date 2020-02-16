8ab8a2b6c35e37c4a0eea00924541dcb665486cb
John Linhart <admin@escope.cz>
2020-02-16 15:54:07 +0100

    Create PageHelper for common pagination calculations
    Als fixing PHP Deprecated:  Unparenthesized `a ? b : c ?: d` is deprecated. Use either `(a ? b : c) ?: d` or `a ? b : (c ?: d)`

---
+++ app/bundles/CoreBundle/Tests/Unit/Helper/PageHelperTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright   2020 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\Tests\Unit\Helper;
+
+use Mautic\CoreBundle\Helper\CoreParametersHelper;
+use Mautic\CoreBundle\Helper\PageHelper;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class PageHelperTest extends \PHPUnit\Framework\TestCase
+{
+    private $session;
+    private $coreParametersHelper;
+    private $pageHelper;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->session              = $this->createMock(SessionInterface::class);
+        $this->coreParametersHelper = $this->createMock(CoreParametersHelper::class);
+        $this->pageHelper           = new PageHelper($this->session, $this->coreParametersHelper, 'mautic.test', 0);
+    }
+
+    /**
+     * @dataProvider PageProvider
+     */
+    public function testCountPage(int $count, int $limit, int $page)
+    {
+        $this->assertSame($page, $this->pageHelper->countPage($count, $limit));
+    }
+
+    public function pageProvider()
+    {
+        return [
+            [0, 10, 1],
+            [1, 10, 1],
+            [5, 10, 1],
+            [10, 10, 1],
+            [11, 10, 2],
+            [20, 10, 2],
+            [21, 10, 3],
+            [15, 15, 1],
+            [16, 15, 2],
+        ];
+    }
+
+    /**
+     * @dataProvider startProvider
+     */
+    public function testCountStart(int $page, int $limit, int $start)
+    {
+        $this->assertSame($start, $this->pageHelper->countPage($page, $limit));
+    }
+
+    public function startProvider()
+    {
+        return [
+            [0, 10, 1],
+            [1, 10, 1],
+            [10, 10, 1],
+            [11, 10, 2],
+        ];
+    }
+}
