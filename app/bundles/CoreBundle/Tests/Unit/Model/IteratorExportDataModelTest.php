0d7fa7f3e69266130fa62bd4c4ecf1f3a720b1ac
John Linhart <admin@escope.cz>
2020-02-17 13:02:59 +0100

    Namespace fix

--- app/bundles/CoreBundle/Tests/Unit/Model/IteratorExportDataModelTest.php
+++ app/bundles/CoreBundle/Tests/Unit/Model/IteratorExportDataModelTest.php
@@ -9,7 +9,7 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\CoreBundle\Tests\Unit\Validator;
+namespace Mautic\CoreBundle\Tests\Unit\Model;
 
 use Mautic\CoreBundle\Entity\CommonRepository;
 use Mautic\CoreBundle\Model\AbstractCommonModel;
