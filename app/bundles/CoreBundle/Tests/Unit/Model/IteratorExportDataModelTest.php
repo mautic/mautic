38ffabd1342a3e88e9678ce0c8b6ed75aa48bcf9
John Linhart <admin@escope.cz>
2020-02-17 11:35:24 +0100

    Fixing PHP Warning - count(): Parameter must be an array or an object that implements Countable in IteratorExportDataModel.php:71

---
+++ app/bundles/CoreBundle/Tests/Unit/Model/IteratorExportDataModelTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * @copyright   2020 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\Tests\Unit\Validator;
+
+use Mautic\CoreBundle\Entity\CommonRepository;
+use Mautic\CoreBundle\Model\AbstractCommonModel;
+use Mautic\CoreBundle\Model\IteratorExportDataModel;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class IteratorExportDataModelTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var MockObject|AbstractCommonModel
+     */
+    private $commonModel;
+
+    /**
+     * @var MockObject|CommonRepository
+     */
+    private $commonRepository;
+
+    /**
+     * @var IteratorExportDataModel
+     */
+    private $iteratorExportDataModel;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->commonModel      = $this->createMock(AbstractCommonModel::class);
+        $this->commonRepository = $this->createMock(CommonRepository::class);
+        $args                   = ['limit' => 1000];
+        $callback               = function ($var) {
+            return $var;
+        };
+
+        $this->iteratorExportDataModel = new IteratorExportDataModel($this->commonModel, $args, $callback);
+    }
+
+    public function testWorkflowWithItems(): void
+    {
+        $this->commonModel->expects($this->once())
+            ->method('getEntities')
+            ->with(['limit' => 1000, 'start' => 0])
+            ->willReturn(['results' => [['a'], ['b']]]);
+
+        $this->commonModel->method('getRepository')->willReturn($this->commonRepository);
+
+        $this->assertSame(0, $this->iteratorExportDataModel->key());
+        $this->iteratorExportDataModel->rewind();
+        $this->iteratorExportDataModel->next();
+        $this->assertSame(1, $this->iteratorExportDataModel->key());
+    }
+
+    public function testWorkflowWithoutItems(): void
+    {
+        $this->commonModel->expects($this->once())
+            ->method('getEntities')
+            ->with(['limit' => 1000, 'start' => 0])
+            ->willReturn(['results' => []]);
+
+        $this->commonModel->method('getRepository')->willReturn($this->commonRepository);
+
+        $this->assertSame(0, $this->iteratorExportDataModel->key());
+        $this->iteratorExportDataModel->rewind();
+        $this->iteratorExportDataModel->next();
+        $this->assertSame(1, $this->iteratorExportDataModel->key());
+    }
+}
