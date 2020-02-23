c3a152c21d4da88fc695025e42b34924ff8d5b95
Alan Hartless <alan@devkardia.com>
2020-02-23 16:24:10 -0600

    Fixed copyrights

--- app/bundles/CoreBundle/DependencyInjection/Builder/BundleMetadata.php
+++ app/bundles/CoreBundle/DependencyInjection/Builder/BundleMetadata.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * @copyright   2020 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        https://www.mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 namespace Mautic\CoreBundle\DependencyInjection\Builder;
 
 class BundleMetadata
--- app/bundles/CoreBundle/DependencyInjection/Builder/Metadata/EntityMetadata.php
+++ app/bundles/CoreBundle/DependencyInjection/Builder/Metadata/EntityMetadata.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
- * @copyright   2014 Mautic Contributors. All rights reserved
+ * @copyright   2020 Mautic Contributors. All rights reserved
  * @author      Mautic
  *
- * @link        http://mautic.org
+ * @link        https://www.mautic.org
  *
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
