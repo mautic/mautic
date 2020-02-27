0a35e8c4b18efad93837ab2b886fa34561e0494a
Valdemar Vovchok <volodymyrvovchok@codefive.pt>
2020-02-27 11:51:19 +0000

    fix a report with the same values in the bounced and unsubscribed columns

---
+++ app/migrations/Version20200227110431.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * @package     Mautic
+ * @copyright   2020 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+
+/**
+ * Migration.
+ */
+class Version20200227110431 extends AbstractMauticMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('CREATE INDEX '.$this->prefix.'dnc_channel_id_search ON '.$this->prefix.'lead_donotcontact (channel_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql('DROP INDEX '.$this->prefix.'dnc_channel_id_search ON '.$this->prefix.'lead_donotcontact');
+    }
+}
