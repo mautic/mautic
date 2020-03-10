201049b467bd7e784a61b77294ac5a089a502b25
Enc3phale <bahaalfr@gmail.com>
2020-03-10 16:56:07 +0100

    Add options and features

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 export default (editor, opts = {}) => {
     const am = editor.AssetManager;
     const pm = editor.Panels;
+    const modal = editor.Modal;
+    const cmd = editor.Commands;
+    const cfg = editor.getConfig();
+
+    let mjmlTemplate = false;
+
+    // Check if MJML plugin is on
+    if (cfg.plugins.includes('grapesjs-mjml')) {
+        mjmlTemplate = true;
+    }
+
+    let config = {
+        sourceEdit: 1,
+        sourceEditBtnLabel: 'Edit',
+        sourceEditModalTitle: 'Edit code',
+        deleteAssetConfirmText: 'Are you sure?',
+        showLayersManager: 0,
+        showImportButton: 0,
+        ...opts,
+    };
 
     // Extend the original `image` and add a confirm dialog before removing it
     am.addType('image', {
@@ -14,7 +34,7 @@ export default (editor, opts = {}) => {
                 e.stopPropagation();
                 const model = this.model;
 
-                if (confirm('Are you sure?')) {
+                if (confirm(config.deleteAssetConfirmText)) {
                     model.collection.remove(model);
                 }
             }
@@ -31,7 +51,18 @@ export default (editor, opts = {}) => {
         openSm && openSm.set('active', 1);
 
         pm.removeButton("views", "open-tm");
-        pm.removeButton("views", "open-layers");
+
+        if (!config.showLayersManager) {
+            pm.removeButton("views", "open-layers");
+        }
+
+        if (!config.showImportButton) {
+            if (mjmlTemplate) {
+                pm.removeButton("options", "mjml-import");
+            } else {
+                pm.removeButton("options", "gjs-open-import-template");
+            }
+        }
 
         // Add Settings Sector
         let traitsSector = $('<div class="gjs-sm-sector no-select">'+
@@ -56,4 +87,74 @@ export default (editor, opts = {}) => {
         let openBlocksBtn = editor.Panels.getButton('views', 'open-blocks');
         openBlocksBtn && openBlocksBtn.set('active', 1);
     });
+
+    // Add function within builder to edit source code
+    if (config.sourceEdit) {
+        let pfx = cfg.stylePrefix;
+        let codeViewer = editor.CodeManager.getViewer('CodeMirror').clone();
+        let container = document.createElement('div');
+        let btnEdit = document.createElement('button');
+
+        codeViewer.set({
+            codeName: 'htmlmixed',
+            readOnly: 0,
+            theme: 'hopscotch',
+            autoBeautify: true,
+            autoCloseTags: true,
+            autoCloseBrackets: true,
+            lineWrapping: true,
+            styleActiveLine: true,
+            smartIndent: true,
+            indentWithTabs: true
+        });
+
+        btnEdit.innerHTML = config.sourceEditBtnLabel;
+        btnEdit.className = pfx + 'btn-prim ' + pfx + 'btn-import';
+        btnEdit.onclick = function () {
+            let code = codeViewer.editor.getValue();
+            editor.DomComponents.getWrapper().set('content', '');
+            editor.setComponents(code.trim());
+            modal.close();
+        };
+
+        cmd.add('html-edit', {
+            run: function (editor, sender) {
+                sender && sender.set('active', 0);
+                let viewer = codeViewer.editor;
+                modal.setTitle(config.sourceEditModalTitle);
+
+                if (!viewer) {
+                    let textarea = document.createElement('textarea');
+                    container.appendChild(textarea);
+                    container.appendChild(btnEdit);
+                    codeViewer.init(textarea);
+                    viewer = codeViewer.editor;
+                }
+
+                let content;
+                if (mjmlTemplate) {
+                    content = editor.getHtml();
+                } else {
+                    content = editor.runCommand('gjs-get-inlined-html');
+                }
+
+                modal.setContent('');
+                modal.setContent(container);
+                codeViewer.setContent(content);
+                modal.open();
+                viewer.refresh();
+            }
+        });
+
+        pm.addButton('options', [
+            {
+                id: 'edit',
+                className: 'fa fa-edit',
+                command: 'html-edit',
+                attributes: {
+                    title: config.sourceEditModalTitle
+                }
+            }
+        ]);
+    }
 };
