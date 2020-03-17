52e980c0ef3abcb18d430b553439edda7b7710f3
John Linhart <admin@escope.cz>
2020-03-17 19:27:04 +0100

    PHPSTAN fixes

--- app/bundles/LeadBundle/Tests/Provider/FormAdjustmentsProviderTest.php
+++ app/bundles/LeadBundle/Tests/Provider/FormAdjustmentsProviderTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\LeadBundle\Tests\EventListener;
+namespace Mautic\LeadBundle\Tests\Provider;
 
 use Mautic\LeadBundle\Event\FormAdjustmentEvent;
 use Mautic\LeadBundle\LeadEvents;
