47a1d8000273e97f73e7db3f15f03462a7953180
Enc3phale <bahaalfr@gmail.com>
2020-03-18 16:49:48 +0100

    Replace GrapesJs RTE by Froala

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ export default (editor, opts = {}) => {
         deleteAssetConfirmText: 'Are you sure?',
         showImportButton: 0,
         mjmlTemplate: false,
+        replaceRteWithFroala: true,
         ...opts,
     };
 
@@ -39,6 +40,54 @@ export default (editor, opts = {}) => {
             }
         },
     });
+
+    if (config.replaceRteWithFroala && typeof mQuery.FroalaEditor !== 'undefined') {
+        // Hiding other toolbars already created
+        let rteToolbar = editor.RichTextEditor.getToolbarEl();
+        [].forEach.call(rteToolbar.children, (child) => {
+            child.style.display = 'none';
+        });
+
+        editor.setCustomRte({
+            enable: function(el, rte) {
+                rte = mQuery(el).froalaEditor({
+                    enter: mQuery.FroalaEditor.ENTER_BR,
+                    pastePlain: true,
+
+                    htmlAllowedTags: ['a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'blockquote', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'hgroup', 'hr', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'menu', 'menuitem', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'queue', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'style', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr', 'center'],
+                    htmlAllowedAttrs: ['data-atwho-at-query', 'data-section', 'data-section-wrapper', 'accept', 'accept-charset', 'accesskey', 'action', 'align', 'allowfullscreen', 'alt', 'async', 'autocomplete', 'autofocus', 'autoplay', 'autosave', 'background', 'bgcolor', 'border', 'charset', 'cellpadding', 'cellspacing', 'checked', 'cite', 'class', 'color', 'cols', 'colspan', 'content', 'contenteditable', 'contextmenu', 'controls', 'coords', 'data', 'data-.*', 'datetime', 'default', 'defer', 'dir', 'dirname', 'disabled', 'download', 'draggable', 'dropzone', 'enctype', 'for', 'form', 'formaction', 'frameborder', 'headers', 'height', 'hidden', 'high', 'href', 'hreflang', 'http-equiv', 'icon', 'id', 'ismap', 'itemprop', 'keytype', 'kind', 'label', 'lang', 'language', 'list', 'loop', 'low', 'max', 'maxlength', 'media', 'method', 'min', 'mozallowfullscreen', 'multiple', 'name', 'novalidate', 'open', 'optimum', 'pattern', 'ping', 'placeholder', 'poster', 'preload', 'pubdate', 'radiogroup', 'readonly', 'rel', 'required', 'reversed', 'rows', 'rowspan', 'sandbox', 'scope', 'scoped', 'scrolling', 'seamless', 'selected', 'shape', 'size', 'sizes', 'span', 'src', 'srcdoc', 'srclang', 'srcset', 'start', 'step', 'summary', 'spellcheck', 'style', 'tabindex', 'target', 'title', 'type', 'translate', 'usemap', 'value', 'valign', 'webkitallowfullscreen', 'width', 'wrap'],
+
+                    toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'quote', 'clearFormatting', '-', 'formatOL', 'formatUL', 'indent', 'outdent', 'token', 'insertLink'],
+
+                    toolbarContainer: editor.RichTextEditor.getToolbarEl(),
+                    linkEditButtons: ['linkOpen', 'linkRemove'],
+                });
+
+                mQuery(el).on('froalaEditor.popups.show.link.edit', function (e, editor) {
+                    // Get the link DOM object of the current selection.
+                    let currentLink = mQuery(el).froalaEditor('link.get');
+
+                    // Get popup link.edit
+                    let popupLink = mQuery(el).froalaEditor('popups.get', 'link.edit');
+
+                    if (typeof currentLink !== 'undefined') {
+                        let top = currentLink.getBoundingClientRect().top;
+                        let height = mQuery(currentLink).outerHeight();
+
+                        // Set position of popup
+                        popupLink.css('top', parseInt(top) + parseInt(height) + 35);
+                    }
+                });
+
+                return rte;
+            },
+            disable: function(el, rte) {
+                // Remove events and destroy editor
+                mQuery(el).off('froalaEditor.popups.show.link.edit');
+                mQuery(el).froalaEditor('destroy');
+            },
+        });
+    }
 
     editor.on('load', function() {
         let $ = grapesjs.$;
