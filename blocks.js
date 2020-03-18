705c3555c77f2744c35e5cc21476c11dd926746a
Enc3phale <bahaalfr@gmail.com>
2020-03-18 16:49:07 +0100

    Separate blocks and buttons Customized buttons Fix bug

---
+++ blocks.js
@@ -0,0 +1,67 @@
+export default (editor, opts = {}) => {
+    const bm = editor.BlockManager;
+
+    let blocks = bm.getAll();
+
+    // All block inside Blocks category
+    blocks.forEach(block => {
+        block.set({
+            category: 'Blocks'
+        });
+    });
+
+    // Custom block inside Sections category
+    if (typeof bm.get('mj-1-column') !== 'undefined') {
+        bm.get('mj-1-column').set({
+            category: 'Sections'
+        });
+    }
+
+    if (typeof bm.get('mj-2-columns') !== 'undefined') {
+        bm.get('mj-2-columns').set({
+            category: 'Sections'
+        });
+    }
+
+    if (typeof bm.get('mj-3-columns') !== 'undefined') {
+        bm.get('mj-3-columns').set({
+            category: 'Sections'
+        });
+    }
+
+    if (typeof bm.get('sect100') !== 'undefined') {
+        bm.get('sect100').set({
+            category: 'Sections'
+        });
+    }
+
+    if (typeof bm.get('sect50') !== 'undefined') {
+        bm.get('sect50').set({
+            category: 'Sections'
+        });
+    }
+
+    if (typeof bm.get('sect30') !== 'undefined') {
+        bm.get('sect30').set({
+            category: 'Sections'
+        });
+    }
+
+    if (typeof bm.get('sect37') !== 'undefined') {
+        bm.get('sect37').set({
+            category: 'Sections'
+        });
+    }
+
+    // Add icon to mj-hero
+    if (typeof bm.get('mj-hero') !== 'undefined') {
+        bm.get('mj-hero').set({
+            attributes: { class: 'gjs-fonts gjs-f-hero' },
+        });
+    }
+
+    // Delete mj-wrapper
+    if (typeof bm.get('mj-wrapper') !== 'undefined') {
+        bm.remove('mj-wrapper');
+    }
+};
--- index.js
+++ index.js
@@ -1,16 +1,10 @@
+import loadButtons from './buttons';
+import loadBlocks from './blocks';
+
 export default (editor, opts = {}) => {
     const am = editor.AssetManager;
     const pm = editor.Panels;
-    const modal = editor.Modal;
-    const cmd = editor.Commands;
     const cfg = editor.getConfig();
-
-    let mjmlTemplate = false;
-
-    // Check if MJML plugin is on
-    if (cfg.plugins.includes('grapesjs-mjml')) {
-        mjmlTemplate = true;
-    }
 
     let config = {
         sourceEdit: 1,
@@ -18,8 +12,14 @@ export default (editor, opts = {}) => {
         sourceEditModalTitle: 'Edit code',
         deleteAssetConfirmText: 'Are you sure?',
         showImportButton: 0,
+        mjmlTemplate: false,
         ...opts,
     };
+
+    // Check if MJML plugin is on
+    if (cfg.plugins.includes('grapesjs-mjml')) {
+        config.mjmlTemplate = true;
+    }
 
     // Extend the original `image` and add a confirm dialog before removing it
     am.addType('image', {
@@ -30,7 +30,7 @@ export default (editor, opts = {}) => {
             // If you want to see more methods to extend check out
             // https://github.com/artf/grapesjs/blob/dev/src/asset_manager/view/AssetImageView.js
             onRemove(e) {
-                e.stopPropagation();
+                e.stopImmediatePropagation();
                 const model = this.model;
 
                 if (confirm(config.deleteAssetConfirmText)) {
@@ -75,81 +75,6 @@ export default (editor, opts = {}) => {
         openBlocksBtn && openBlocksBtn.set('active', 1);
     });
 
-    if (!config.showImportButton) {
-        if (mjmlTemplate) {
-            pm.removeButton("options", "mjml-import");
-        } else {
-            pm.removeButton("options", "gjs-open-import-template");
-        }
-    }
-
-    // Add function within builder to edit source code
-    if (config.sourceEdit) {
-        let pfx = cfg.stylePrefix;
-        let codeViewer = editor.CodeManager.getViewer('CodeMirror').clone();
-        let container = document.createElement('div');
-        let btnEdit = document.createElement('button');
-
-        codeViewer.set({
-            codeName: 'htmlmixed',
-            readOnly: 0,
-            theme: 'hopscotch',
-            autoBeautify: true,
-            autoCloseTags: true,
-            autoCloseBrackets: true,
-            lineWrapping: true,
-            styleActiveLine: true,
-            smartIndent: true,
-            indentWithTabs: true
-        });
-
-        btnEdit.innerHTML = config.sourceEditBtnLabel;
-        btnEdit.className = pfx + 'btn-prim ' + pfx + 'btn-import';
-        btnEdit.onclick = function () {
-            let code = codeViewer.editor.getValue();
-            editor.DomComponents.getWrapper().set('content', '');
-            editor.setComponents(code.trim());
-            modal.close();
-        };
-
-        cmd.add('html-edit', {
-            run: function (editor, sender) {
-                sender && sender.set('active', 0);
-                let viewer = codeViewer.editor;
-                modal.setTitle(config.sourceEditModalTitle);
-
-                if (!viewer) {
-                    let textarea = document.createElement('textarea');
-                    container.appendChild(textarea);
-                    container.appendChild(btnEdit);
-                    codeViewer.init(textarea);
-                    viewer = codeViewer.editor;
-                }
-
-                let content;
-                if (mjmlTemplate) {
-                    content = editor.getHtml();
-                } else {
-                    content = editor.runCommand('gjs-get-inlined-html');
-                }
-
-                modal.setContent('');
-                modal.setContent(container);
-                codeViewer.setContent(content);
-                modal.open();
-                viewer.refresh();
-            }
-        });
-
-        pm.addButton('options', [
-            {
-                id: 'edit',
-                className: 'fa fa-edit',
-                command: 'html-edit',
-                attributes: {
-                    title: config.sourceEditModalTitle
-                }
-            }
-        ]);
-    }
+    loadButtons(editor, config);
+    loadBlocks(editor, config);
 };
