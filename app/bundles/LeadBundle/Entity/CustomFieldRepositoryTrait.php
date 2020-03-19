06d97f2eb258c48add5e52dfb8e4693bddb263e4
Dennis Ameling <dennis@dennisameling.com>
2020-03-19 08:55:59 +0100

    Revert "Speed up leads custom fields"

--- app/bundles/LeadBundle/Entity/CustomFieldRepositoryTrait.php
+++ app/bundles/LeadBundle/Entity/CustomFieldRepositoryTrait.php
@@ -114,7 +114,12 @@ trait CustomFieldRepositoryTrait
                 //since we have to be cross-platform; it's way ugly
 
                 //We should probably totally ditch orm for leads
-                $order = $this->getTableAlias().'.id AS HIDDEN ORD';
+                $order = '(CASE';
+                foreach ($ids as $count => $id) {
+                    $order .= ' WHEN '.$this->getTableAlias().'.id = '.$id.' THEN '.$count;
+                    ++$count;
+                }
+                $order .= ' ELSE '.$count.' END) AS HIDDEN ORD';
 
                 //ORM - generates lead entities
                 /** @var \Doctrine\ORM\QueryBuilder $q */
