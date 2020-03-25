e5d7324efd0ea4fa62c88f702ab6f4772e48f4fe
Lukas Sykora <lukas.sykora@acquia.com>
2020-03-25 14:39:48 +0100

    MAUT-3399 / CS Fixer

--- app/bundles/CoreBundle/Command/UpdateDoNotSellListCommand.php
+++ app/bundles/CoreBundle/Command/UpdateDoNotSellListCommand.php
@@ -3,10 +3,10 @@
 namespace Mautic\CoreBundle\Command;
 
 use Mautic\CoreBundle\Helper\MaxMindDoNotSellDownloadHelper;
-use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class UpdateDoNotSellListCommand extends Command
 {
