09665994b91bee2f1566d6556ade5462abba833b
Nick Veenhof <nick.veenhof@gmail.com>
2020-04-07 21:15:07 +0200

    Make sure the lifetime interval is an integer

--- app/config/security.php
+++ app/config/security.php
@@ -110,7 +110,7 @@ $firewalls = [
         ],
         'remember_me' => [
             'secret'   => '%mautic.rememberme_key%',
-            'lifetime' => '%mautic.rememberme_lifetime%',
+            'lifetime' => intval('%mautic.rememberme_lifetime%'),
             'path'     => '%mautic.rememberme_path%',
             'domain'   => '%mautic.rememberme_domain%',
         ],
