db38adc64c25fc2cb817896d3eb93bdf6af16981
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2020-04-11 16:29:56 +0100

    Set a session snapshot and check if one exists before authenticating

--- tests/_support/Step/Acceptance/Login.php
+++ tests/_support/Step/Acceptance/Login.php
@@ -9,9 +9,19 @@ class Login extends \AcceptanceTester
     public function loginAsUser()
     {
         $I = $this;
+
+        // if snapshot exists - skipping login
+
+        if ($I->loadSessionSnapshot('login')) {
+            return 'User logged in';
+        }
+
+        // continue with login
+
         $I->amOnPage(LoginPage::$URL);
         $I->fillField(LoginPage::$username, 'admin');
         $I->fillField(LoginPage::$password, 'mautic');
         $I->click(LoginPage::$login);
+        $I->saveSessionSnapshot('login');
     }
 }
