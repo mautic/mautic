19356a073114592352dc5a0dfcf8e2b4ced08756
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2020-04-12 15:43:20 +0100

    Need to increase the wait time as some fields were not getting fully completed

--- tests/acceptance/ContactCest.php
+++ tests/acceptance/ContactCest.php
@@ -4,8 +4,8 @@ class ContactCest
 {
     public function _before(\Step\Acceptance\Login $I)
     {
-        $I->loginAsUser();
-        $I->amOnPage('/s/dashboard');
+        $I->loginAsUser(); // Check for existing session or log in
+        $I->amOnPage('/s/dashboard'); // Go to dashboard
     }
 
     public function _after(AcceptanceTester $I)
@@ -15,11 +15,12 @@ class ContactCest
     // tests
     public function viewContact(AcceptanceTester $I)
     {
-        $I->amOnPage('/s/contacts');
-        $contactName = $I->grabTextFrom('//*[@id="leadTable"]/tbody/tr[1]/td[2]/a/div[1]');
+        $I->amOnPage('/s/contacts'); //Go to contacts list
+        $contactName = $I->grabTextFrom('//*[@id="leadTable"]/tbody/tr[1]/td[2]/a/div[1]'); // Grab the name of the first contact in the list
         $I->click('//*[@id="leadTable"]/tbody/tr[1]/td[2]');
-        $I->click(['link' => $contactName]);
-        $I->see($contactName);
+        $I->click(['link' => $contactName]); // Click on the first contact in the list
+        $I->see($contactName); // Check we see the expected name
+        $I->makeScreenshot('view-contact'); // Take a screenshot of the contact for confirmation
     }
 
     public function editContact(AcceptanceTester $I)
@@ -34,11 +35,11 @@ class ContactCest
         $I->see("Edit $contactName");
         $I->fillField('//*[@id="lead_firstname"]', 'Test-First-Name');
         $I->click('//*[@id="lead_buttons_apply_toolbar"]');
-        $I->wait(1);
+        $I->wait(5);
         $I->fillField('//*[@id="lead_lastname"]', 'Test-Last-Name');
-        $I->wait(1);
+        $I->wait(5);
         $I->click('//*[@id="lead_buttons_save_toolbar"]');
-        $I->wait(1);
+        $I->wait(5);
         $I->see('Test-First-Name Test-Last-Name has been updated!');
     }
 }
