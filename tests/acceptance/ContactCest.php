20f8c7225a054013315b0b2ef8b78e230d4ec714
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2020-04-12 15:23:28 +0100

    Add test for editing contact

--- tests/acceptance/ContactCest.php
+++ tests/acceptance/ContactCest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class ContactCest
 {
     public function _before(\Step\Acceptance\Login $I)
@@ -17,8 +16,29 @@ class ContactCest
     public function viewContact(AcceptanceTester $I)
     {
         $I->amOnPage('/s/contacts');
-        $leadName = $I->grabTextFrom('//*[@id="leadTable"]/tbody/tr[1]/td[2]/a/div[1]');
-        $I->click('//*[@id="leadTable"]/tbody/tr[1]/td[2]/a');
-        $I->see($leadName);
+        $contactName = $I->grabTextFrom('//*[@id="leadTable"]/tbody/tr[1]/td[2]/a/div[1]');
+        $I->click('//*[@id="leadTable"]/tbody/tr[1]/td[2]');
+        $I->click(['link' => $contactName]);
+        $I->see($contactName);
+    }
+
+    public function editContact(AcceptanceTester $I)
+    {
+        $I->amOnPage('/s/contacts');
+        $contactName = $I->grabTextFrom('//*[@id="leadTable"]/tbody/tr[1]/td[2]/a/div[1]');
+        $I->click('//*[@id="leadTable"]/tbody/tr[1]/td[2]');
+        $I->click(['link' => $contactName]);
+        $I->see($contactName);
+        $I->click('//*[@id="toolbar"]/div[1]/a[1]'); // Click on edit button
+        $I->waitForElement('//*[@id="core"]/div[1]/h4', 2); // Wait for the form to show (in secs)
+        $I->see("Edit $contactName");
+        $I->fillField('//*[@id="lead_firstname"]', 'Test-First-Name');
+        $I->click('//*[@id="lead_buttons_apply_toolbar"]');
+        $I->wait(1);
+        $I->fillField('//*[@id="lead_lastname"]', 'Test-Last-Name');
+        $I->wait(1);
+        $I->click('//*[@id="lead_buttons_save_toolbar"]');
+        $I->wait(1);
+        $I->see('Test-First-Name Test-Last-Name has been updated!');
     }
 }
