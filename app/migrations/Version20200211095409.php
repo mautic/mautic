f0a20995614fdbef30803fae2158d03a4b284ddd
Don Gilbert <don@dongilbert.net>
2020-04-14 14:39:34 -0400

    Fix migration for 3.x

--- app/migrations/Version20200211095409.php
+++ app/migrations/Version20200211095409.php
@@ -2,8 +2,8 @@
 
 namespace Mautic\Migrations;
 
-use Doctrine\DBAL\Migrations\SkipMigrationException;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Exception\SkipMigration;
 use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 
 /**
@@ -12,30 +12,22 @@ use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 class Version20200211095409 extends AbstractMauticMigration
 {
     /**
-     * @param Schema $schema
-     *
-     * @throws SkipMigrationException
+     * @throws SkipMigration
      * @throws \Doctrine\DBAL\Schema\SchemaException
      */
-    public function preUp(Schema $schema)
+    public function preUp(Schema $schema): void
     {
         if (!$schema->getTable("{$this->prefix}users")->hasColumn('online_status')) {
-            throw new SkipMigrationException("The online_status column on the {$this->prefix}users table has already been removed.");
+            throw new SkipMigration("The online_status column on the {$this->prefix}users table has already been removed.");
         }
     }
 
-    /**
-     * @param Schema $schema
-     */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         $this->addSql("ALTER TABLE {$this->prefix}users DROP online_status");
     }
 
-    /**
-     * @param Schema $schema
-     */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         $this->addSql("ALTER TABLE {$this->prefix}users ADD online_status VARCHAR(255) DEFAULT NULL");
     }
