69788e5db9ad9719859f6d74f5920d087b1ee8c7
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2020-04-18 13:31:36 +0100

    Add test for batch adding categories to contacts. Required a helper function to check if a field is empty or not

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -7,4 +7,15 @@ namespace Helper;
 
 class Acceptance extends \Codeception\Module
 {
+    // Function to check if a field is empty
+    public function seeFieldIsEmpty($value)
+    {
+        $this->assertTrue(empty($value));
+    }
+
+    // Function to check if a field is not empty
+    public function seeFieldIsNotEmpty($value)
+    {
+        $this->assertFalse(empty($value));
+    }
 }
