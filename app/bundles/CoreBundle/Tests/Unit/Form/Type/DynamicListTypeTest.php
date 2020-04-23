791d4950aa24523310a6aae15a9a92f6a8b3d9e3
John Linhart <admin@escope.cz>
2020-04-23 13:30:39 +0200

    Email edit form couldn't be saved when the form data was null

---
+++ app/bundles/CoreBundle/Tests/Unit/Form/Type/DynamicListTypeTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright   2020 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\CoreBundle\Tests\Unit\Form\Validator\Constraints;
+
+use Mautic\CoreBundle\Form\Type\DynamicListType;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+final class DynamicListTypeTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var MockObject|FormBuilderInterface
+     */
+    private $formBuilder;
+
+    /**
+     * @var DynamicListType
+     */
+    private $form;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->formBuilder = $this->createMock(FormBuilderInterface::class);
+        $this->form        = new DynamicListType();
+    }
+
+    public function testBuildFormWhenDataIsNull()
+    {
+        $this->formBuilder->expects($this->once())
+            ->method('addEventListener')
+            ->with(
+                FormEvents::PRE_SUBMIT,
+                $this->callback(function ($formModifier) {
+                    $formEvent = $this->createMock(FormEvent::class);
+
+                    $formEvent->expects($this->once())
+                        ->method('getData')
+                        ->willReturn(null);
+
+                    $formEvent->expects($this->never())
+                        ->method('setData');
+
+                    $formModifier($formEvent);
+
+                    return true;
+                })
+            );
+
+        $this->form->buildForm($this->formBuilder, []);
+    }
+
+    public function testBuildFormWhenDataIsArray()
+    {
+        $this->formBuilder->expects($this->once())
+            ->method('addEventListener')
+            ->with(
+                FormEvents::PRE_SUBMIT,
+                $this->callback(function ($formModifier) {
+                    $formEvent = $this->createMock(FormEvent::class);
+                    $data = [['content' => 'dynamic slot content']];
+
+                    $formEvent->expects($this->once())
+                        ->method('getData')
+                        ->willReturn($data);
+
+                    $formEvent->expects($this->once())
+                        ->method('setData')
+                        ->with($data);
+
+                    $formModifier($formEvent);
+
+                    return true;
+                })
+            );
+
+        $this->form->buildForm($this->formBuilder, []);
+    }
+}
