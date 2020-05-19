27a5228f693cb5ed698df2f8853ca25fe0d2cff5
Alfredo Arena <alfredo.arena@ctmobi.it>
2020-05-19 09:50:26 +0200

    Added company view and contacts listing in company

---
+++ app/bundles/LeadBundle/Views/Company/company.html.php
@@ -0,0 +1,289 @@
+<?php
+
+/*
+ * @copyright   2014 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/** @var \Mautic\LeadBundle\Entity\Company $company */
+/** @var array $fields */
+$view->extend('MauticCoreBundle:Default:content.html.php');
+
+$view['slots']->set(
+    'headerTitle',
+    $view->escape($company->getName())
+);
+
+$groups = array_keys($fields);
+$edit   = $view['security']->hasEntityAccess(
+    $permissions['lead:leads:editown'],
+    $permissions['lead:leads:editother'],
+    $company->getPermissionUser()
+);
+
+$buttons = [];
+
+//Merge button
+if (($view['security']->hasEntityAccess(
+    $permissions['lead:leads:deleteown'],
+    $permissions['lead:leads:deleteother'],
+    $company->getPermissionUser()
+))
+    && $edit
+) {
+    $buttons[] = [
+        'attr' => [
+            'data-toggle' => 'ajaxmodal',
+            'data-target' => '#MauticSharedModal',
+            'data-header' => $view['translator']->trans(
+                'mautic.lead.company.header.merge',
+                ['%name%' => $view->escape($company->getPrimaryIdentifier())]
+            ),
+            'href' => $view['router']->path(
+                'mautic_company_action',
+                ['objectId' => $company->getId(), 'objectAction' => 'merge']
+            ),
+        ],
+        'btnText'   => $view['translator']->trans('mautic.lead.merge'),
+        'iconClass' => 'fa fa-user',
+    ];
+}
+
+//Download button
+if ($view['security']->hasEntityAccess(
+    $permissions['lead:leads:viewown'],
+    $permissions['lead:leads:viewother'],
+    $company->getPermissionUser()
+)
+) {
+    $buttons[] = [
+        'attr' => [
+            'data-toggle'=> 'download',
+            'href'       => $view['router']->path(
+                'mautic_company_export_action',
+                ['companyId' => $company->getId()]
+            ),
+        ],
+        'btnText'   => $view['translator']->trans('mautic.core.export'),
+        'iconClass' => 'fa fa-download',
+    ];
+}
+
+$view['slots']->set(
+    'actions',
+    $view->render(
+        'MauticCoreBundle:Helper:page_actions.html.php',
+        [
+            'item'            => $company,
+            'routeBase'       => 'company',
+            'langVar'         => 'company',
+            'customButtons'   => $buttons,
+            'templateButtons' => [
+                'edit'   => $edit,
+                'delete' => $view['security']->hasEntityAccess(
+                    $permissions['lead:leads:deleteown'],
+                    $permissions['lead:leads:deleteother'],
+                    $company->getPermissionUser()
+                ),
+                'close' => $view['security']->hasEntityAccess(
+                    $permissions['lead:leads:viewown'],
+                    $permissions['lead:leads:viewother'],
+                    $company->getPermissionUser()
+                ),
+            ],
+        ]
+    )
+);
+?>
+
+<!-- start: box layout -->
+<div class="box-layout">
+    <!-- left section -->
+    <div class="col-md-9 bg-white height-auto">
+        <div class="bg-auto">
+            <!--/ lead detail header -->
+
+            <!-- lead detail collapseable -->
+            <div class="collapse" id="lead-details">
+                <ul class="pt-md nav nav-tabs pr-md pl-md" role="tablist">
+                    <?php $step = 0; ?>
+                    <?php foreach ($groups as $g) : ?>
+                        <?php if (!empty($fields[$g])) : ?>
+                            <li class="<?php echo (0 === $step) ? 'active' : ''; ?>">
+                                <a href="#<?php echo $g; ?>" class="group" data-toggle="tab">
+                                    <?php echo $view['translator']->trans('mautic.lead.field.group.'.$g); ?>
+                                </a>
+                            </li>
+                            <?php ++$step; ?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </ul>
+
+                <!-- start: tab-content -->
+                <div class="tab-content pa-md bg-white">
+                    <?php $i = 0; ?>
+                    <?php foreach ($groups as $group): ?>
+                        <div class="tab-pane fade <?php echo 0 == $i ? 'in active' : ''; ?> bdr-w-0" id="<?php echo $group; ?>">
+                            <div class="pr-md pl-md pb-md">
+                                <div class="panel shd-none mb-0">
+                                    <table class="table table-bordered table-striped mb-0">
+                                        <tbody>
+                                        <?php foreach ($fields[$group] as $field) : ?>
+                                            <tr>
+                                                <td width="20%"><span class="fw-b"><?php echo $view->escape($field['label']); ?></span>
+                                                </td>
+                                                <td>
+                                                    <?php if ('core' == $group && 'country' == $field['alias'] && !empty($flag)) : ?>
+                                                    <img class="mr-sm" src="<?php echo $flag; ?>" alt="" style="max-height: 24px;"/>
+                                                    <span class="mt-1"><?php echo $view->escape($field['value']); ?>
+                                                    <?php else : ?>
+                                                        <?php if (is_array($field['value']) && 'multiselect' === $field['type']) : ?>
+                                                            <?php echo implode(', ', $field['value']); ?>
+                                                        <?php elseif (is_string($field['value']) && 'url' === $field['type']) : ?>
+                                                            <a href="<?php echo $view->escape($field['value']); ?>" target="_blank">
+                                                                <?php echo $field['value']; ?>
+                                                            </a>
+                                                        <?php else : ?>
+                                                            <?php echo $view->escape($field['normalizedValue']); ?>
+                                                        <?php endif; ?>
+                                                    <?php endif; ?>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <?php ++$i; ?>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+            <!--/ lead detail collapseable -->
+        </div>
+
+        <div class="bg-auto bg-dark-xs">
+            <!-- lead detail collapseable toggler -->
+            <div class="hr-expand nm">
+                <span data-toggle="tooltip" title="<?php echo $view['translator']->trans('mautic.core.details'); ?>">
+                    <a href="javascript:void(0)" class="arrow text-muted collapsed" data-toggle="collapse" data-target="#lead-details">
+                       <span class="caret"></span>
+                       <?php echo $view['translator']->trans('mautic.core.details'); ?>
+                    </a>
+                </span>
+            </div>
+            <!--/ lead detail collapseable toggler -->
+        </div>
+
+        <div class="pa-md enage">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="panel">
+                        <div class="panel-body box-layout">
+                            <div class="col-xs-8 va-m">
+                                <h5 class="text-white dark-md fw-sb mb-xs">
+                                    <?php echo $view['translator']->trans('mautic.lead.field.header.engagements'); ?>
+                                </h5>
+                            </div>
+                            <div class="col-xs-4 va-t text-right">
+                                <h3 class="text-white dark-sm"><span class="fa fa-eye"></span></h3>
+                            </div>
+                        </div>
+                        <?php echo $view->render(
+                            'MauticCoreBundle:Helper:chart.html.php',
+                            ['chartData' => $engagementData, 'chartType' => 'line', 'chartHeight' => 250]
+                        ); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- contacts section -->
+        <div class="pa-md enage">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div id="contacts-table">
+                        <?php echo $view->render(
+                            'MauticLeadBundle:Company:list_rows_contacts.html.php',
+                            [
+                                'contacts'    => $items,
+                                'company'     => $company,
+                                'tmpl'        => 'index',
+                                'permissions' => $permissions,
+                                'page'        => $page,
+                                'limit'       => $limit,
+                                'totalItems'  => $totalItems,
+                            ]
+                        ); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/ contacts section -->
+
+        <!--/ end: tab-content -->
+    </div>
+
+    <!--/ left section -->
+
+    <!-- right section -->
+    <div class="col-md-3 bg-white bdr-l height-auto">
+        <!-- form HTML -->
+        <div class="panel bg-transparent shd-none bdr-rds-0 bdr-w-0 mb-0">
+            <div class="mt-sm points-panel text-center">
+                <h1>
+                    <?php echo $view['translator']->transChoice(
+                        'mautic.company.score.count',
+                        $company->getScore(),
+                        ['%score%' => $company->getScore()]
+                    ); ?>
+                </h1>
+                <hr/>
+            </div>
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <?php echo $view['translator']->trans('mautic.lead.field.group.company'); ?>
+                </div>
+            </div>
+            <div class="panel-body pt-sm">
+            <?php if ($company->getOwner()) : ?>
+                <h6 class="fw-sb"><?php echo $view['translator']->trans('mautic.lead.lead.field.owner'); ?></h6>
+                <p class="text-muted"><?php echo $view->escape($company->getOwner()->getName()); ?></p>
+            <?php endif; ?>
+
+                <h6 class="fw-sb">
+                    <?php echo $view['translator']->trans('mautic.lead.field.address'); ?>
+                </h6>
+                <address class="text-muted">
+                    <?php
+                    $address1 = $company->getAddress1();
+                    if (!empty($address1)) : ?>
+                        <?php echo $view->escape($address1); ?><br>
+                    <?php endif; ?>
+                    <?php
+                        $address2 = $company->getAddress2();
+                        echo (!empty($address2)) ? $view->escape($address2).'<br>' : ''; ?>
+                    <?php echo $view->escape($company->getCity()); ?>
+                    <?php
+                        $zipcode = $company->getZipcode();
+                        echo isset($zipcode) ? $view->escape($zipcode) : ''; ?>
+                    <br>
+                </address>
+
+                <h6 class="fw-sb"><?php echo $view['translator']->trans('mautic.core.type.email'); ?></h6>
+                <p class="text-muted"><?php echo $view->escape($company->getEmail()); ?></p>
+
+                <?php if (isset($fields['core']['phone'])) : ?>
+                    <h6 class="fw-sb"><?php echo $view['translator']->trans('mautic.lead.field.type.tel'); ?></h6>
+                    <p class="text-muted"><?php echo $view->escape($company->getPhone()); ?></p>
+                <?php endif; ?>
+            </div>
+        </div>
+        <!--/ form HTML -->
+    </div>
+    <!--/ right section -->
+</div>
+<!--/ end: box layout -->
---
+++ app/bundles/LeadBundle/Views/Company/list_rows_contacts.html.php
@@ -0,0 +1,213 @@
+
+<?php
+
+$baseUrl = $view['router']->path(
+    'mautic_company_contacts_list',
+    [
+        'objectId' => $company->getId(),
+    ]
+);
+
+$customButtons = [
+    [
+        'attr' => [
+            'class'       => 'btn btn-default btn-sm btn-nospin',
+            'data-toggle' => 'ajaxmodal',
+            'data-target' => '#MauticSharedModal',
+            'href'        => $view['router']->path('mautic_segment_batch_contact_view'),
+            'data-header' => $view['translator']->trans('mautic.lead.batch.lists'),
+        ],
+        'btnText'   => $view['translator']->trans('mautic.lead.batch.lists'),
+        'iconClass' => 'fa fa-pie-chart',
+    ],
+    [
+        'attr' => [
+            'class'       => 'btn btn-default btn-sm btn-nospin',
+            'data-toggle' => 'ajaxmodal',
+            'data-target' => '#MauticSharedModal',
+            'href'        => $view['router']->path('mautic_contact_action', ['objectAction' => 'batchStages']),
+            'data-header' => $view['translator']->trans('mautic.lead.batch.stages'),
+        ],
+        'btnText'   => $view['translator']->trans('mautic.lead.batch.stages'),
+        'iconClass' => 'fa fa-tachometer',
+    ],
+    [
+        'attr' => [
+            'class'       => 'btn btn-default btn-sm btn-nospin',
+            'data-toggle' => 'ajaxmodal',
+            'data-target' => '#MauticSharedModal',
+            'href'        => $view['router']->path('mautic_contact_action', ['objectAction' => 'batchCampaigns']),
+            'data-header' => $view['translator']->trans('mautic.lead.batch.campaigns'),
+        ],
+        'btnText'   => $view['translator']->trans('mautic.lead.batch.campaigns'),
+        'iconClass' => 'fa fa-clock-o',
+    ],
+    [
+        'attr' => [
+            'class'       => 'btn btn-default btn-sm btn-nospin',
+            'data-toggle' => 'ajaxmodal',
+            'data-target' => '#MauticSharedModal',
+            'href'        => $view['router']->path('mautic_contact_action', ['objectAction' => 'batchOwners']),
+            'data-header' => $view['translator']->trans('mautic.lead.batch.owner'),
+        ],
+        'btnText'   => $view['translator']->trans('mautic.lead.batch.owner'),
+        'iconClass' => 'fa fa-user',
+    ],
+    [
+        'attr' => [
+            'class'       => 'btn btn-default btn-sm btn-nospin',
+            'data-toggle' => 'ajaxmodal',
+            'data-target' => '#MauticSharedModal',
+            'href'        => $view['router']->path('mautic_contact_action', ['objectAction' => 'batchDnc']),
+            'data-header' => $view['translator']->trans('mautic.lead.batch.dnc'),
+        ],
+        'btnText'   => $view['translator']->trans('mautic.lead.batch.dnc'),
+        'iconClass' => 'fa fa-ban text-danger',
+    ],
+];
+?>
+
+<?php if (count($contacts)): ?>
+    <div class="table-responsive">
+        <table class="table table-hover table-striped table-bordered" id="leadTable">
+            <thead>
+                <tr>
+                    <?php
+                    echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', [
+                        'sessionVar' => 'company.'.$company->getId().'.contacts',
+                        'orderBy'    => 'l.lastname, l.firstname, l.company, l.email',
+                        'text'       => 'mautic.core.name',
+                        'class'      => 'col-lead-name',
+                        'target'     => '#contacts-table',
+                        'baseUrl'    => $baseUrl,
+                    ]);
+
+                    echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', [
+                        'sessionVar' => 'company.'.$company->getId().'.contacts',
+                        'orderBy'    => 'l.email',
+                        'text'       => 'mautic.core.type.email',
+                        'class'      => 'col-lead-email visible-md visible-lg',
+                        'target'     => '#contacts-table',
+                        'baseUrl'    => $baseUrl,
+                    ]);
+
+                    echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', [
+                        'sessionVar' => 'company.'.$company->getId().'.contacts',
+                        'orderBy'    => 'l.city, l.state',
+                        'text'       => 'mautic.lead.lead.thead.location',
+                        'class'      => 'col-lead-location visible-md visible-lg',
+                        'target'     => '#contacts-table',
+                        'baseUrl'    => $baseUrl,
+                    ]);
+                    echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', [
+                        'sessionVar' => 'company.'.$company->getId().'.contacts',
+                        'orderBy'    => 'l.stage_id',
+                        'text'       => 'mautic.lead.stage.label',
+                        'class'      => 'col-lead-stage',
+                        'target'     => '#contacts-table',
+                        'baseUrl'    => $baseUrl,
+                    ]);
+                    echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', [
+                        'sessionVar' => 'company.'.$company->getId().'.contacts',
+                        'orderBy'    => 'l.points',
+                        'text'       => 'mautic.lead.points',
+                        'class'      => 'visible-md visible-lg col-lead-points',
+                        'target'     => '#contacts-table',
+                        'baseUrl'    => $baseUrl,
+                    ]);
+
+                    echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', [
+                        'sessionVar' => 'company.'.$company->getId().'.contacts',
+                        'orderBy'    => 'l.last_active',
+                        'text'       => 'mautic.lead.lastactive',
+                        'class'      => 'col-lead-lastactive visible-md visible-lg',
+                        'default'    => true,
+                        'target'     => '#contacts-table',
+                        'baseUrl'    => $baseUrl,
+                    ]);
+
+                    echo $view->render('MauticCoreBundle:Helper:tableheader.html.php', [
+                        'sessionVar' => 'company.'.$company->getId().'.contacts',
+                        'orderBy'    => 'l.id',
+                        'text'       => 'mautic.core.id',
+                        'class'      => 'col-lead-id visible-md visible-lg',
+                        'target'     => '#contacts-table',
+                        'baseUrl'    => $baseUrl,
+                    ]);
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($contacts as $contact) : ?>
+                <?php $fields = $contact->getFields(); ?>
+                <tr>
+                    <td>
+                        <a href="<?php echo $view['router']->path('mautic_contact_action', ['objectAction' => 'view', 'objectId' => $contact->getId()]); ?>" data-toggle="ajax">
+
+                            <div><?php echo $view->escape($contact->isAnonymous() ? $view['translator']->trans($contact->getPrimaryIdentifier()) : $contact->getPrimaryIdentifier()); ?></div>
+                            <div class="small"><?php echo $view->escape($contact->getSecondaryIdentifier()); ?></div>
+                        </a>
+                    </td>
+                    <td class="visible-md visible-lg"><?php echo $view->escape($fields['core']['email']['value']); ?></td>
+                    <td class="visible-md visible-lg">
+                        <?php
+                        $flag = (!empty($fields['core']['country'])) ? $view['assets']->getCountryFlag($fields['core']['country']['value']) : '';
+                        if (!empty($flag)) :
+                            ?>
+                        <img src="<?php echo $flag; ?>" style="max-height: 24px;" class="mr-sm" />
+                            <?php
+                        endif;
+                        $location = [];
+                        if (!empty($fields['core']['city']['value'])) :
+                            $location[] = $fields['core']['city']['value'];
+                        endif;
+                        if (!empty($fields['core']['state']['value'])) :
+                            $location[] = $fields['core']['state']['value'];
+                        elseif (!empty($fields['core']['country']['value'])) :
+                            $location[] = $fields['core']['country']['value'];
+                        endif;
+                        echo $view->escape(implode(', ', $location));
+                        ?>
+                        <div class="clearfix"></div>
+                    </td>
+                    <td class="text-center">
+                        <?php
+                        $color = $contact->getColor();
+                        $style = !empty($color) ? ' style="background-color: '.$color.';"' : '';
+                        ?>
+                        <?php if ($contact->getStage()) :?>
+                        <span class="label label-default"<?php echo $style; ?>><?php echo $view->escape($contact->getStage()->getName()); ?></span>
+                        <?php endif; ?>
+                    </td>
+                    <td class="visible-md visible-lg text-center">
+                        <?php
+                        $color = $contact->getColor();
+                        $style = !empty($color) ? ' style="background-color: '.$color.';"' : '';
+                        ?>
+                        <span class="label label-default"<?php echo $style; ?>><?php echo $contact->getPoints(); ?></span>
+                    </td>
+                    <td class="visible-md visible-lg">
+                        <abbr title="<?php echo $view['date']->toFull($contact->getLastActive()); ?>">
+                            <?php echo $view['date']->toText($contact->getLastActive()); ?>
+                        </abbr>
+                    </td>
+                    <td class="visible-md visible-lg"><?php echo $contact->getId(); ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <?php echo $view->render(
+        'MauticCoreBundle:Helper:pagination.html.php',
+        [
+            'page'       => $page,
+            'limit'      => $limit,
+            'baseUrl'    => $baseUrl,
+            'target'     => '#contacts-table',
+            'totalItems' => $totalItems,
+            'sessionVar' => 'company.'.$company->getId().'.contacts',
+        ]
+    ); ?>
+<?php else : ?>
+    <?php echo $view->render('MauticCoreBundle:Helper:noresults.html.php'); ?>
+<?php endif; ?>
