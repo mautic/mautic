580cdec0be198080b4c64ab40af00dec8b155ca8
John Linhart <admin@escope.cz>
2020-05-29 12:18:34 +0200

    The param cannot be null, but the value can. Retyping

--- app/bundles/FormBundle/Controller/FieldController.php
+++ app/bundles/FormBundle/Controller/FieldController.php
@@ -6,6 +6,7 @@ use Mautic\CoreBundle\Controller\FormController as CommonFormController;
 use Mautic\CoreBundle\Model\AbstractCommonModel;
 use Mautic\FormBundle\Collector\AlreadyMappedFieldCollectorInterface;
 use Mautic\FormBundle\Collector\FieldCollector;
+use Mautic\FormBundle\Collector\MappedObjectCollectorInterface;
 use Mautic\FormBundle\Entity\Field;
 use Mautic\FormBundle\Event\FormBuilderEvent;
 use Mautic\FormBundle\FormEvents;
@@ -15,7 +16,6 @@ use Mautic\FormBundle\Model\FormModel;
 use Mautic\LeadBundle\Model\FieldModel as LeadFieldModel;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
-use Mautic\FormBundle\Collector\MappedObjectCollectorInterface;
 
 class FieldController extends CommonFormController
 {
@@ -337,7 +337,7 @@ class FieldController extends CommonFormController
                     'field'                => $formField,
                     'id'                   => $objectId,
                     'formId'               => $formId,
-                    'mappedFields'         => $this->mappedObjectCollector->buildCollection($formField['mappedObject']),
+                    'mappedFields'         => $this->mappedObjectCollector->buildCollection((string) $formField['mappedObject']),
                     'inBuilder'            => true,
                     'fields'               => $this->fieldHelper->getChoiceList($customComponents['fields']),
                     'formFields'           => $fields,
