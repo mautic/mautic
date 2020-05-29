668d8c57aa883fe58014ef9ee6a6c1b50e70406b
John Linhart <admin@escope.cz>
2020-05-29 12:17:43 +0200

    Shorter, readable syntax

--- app/bundles/FormBundle/Views/Field/file.html.php
+++ app/bundles/FormBundle/Views/Field/file.html.php
@@ -4,12 +4,12 @@ echo $view->render(
     'MauticFormBundle:Field:text.html.php',
     [
         'field'    => $field,
-        'fields'   => isset($fields) ? $fields : [],
-        'inForm'   => (isset($inForm)) ? $inForm : false,
+        'fields'   => $fields ?? [],
+        'inForm'   => $inForm ?? false,
         'type'     => 'file',
         'id'       => $id,
-        'deleted'  => (!empty($deleted)) ? true : false,
-        'formId'   => (isset($formId)) ? $formId : 0,
-        'formName' => (isset($formName)) ? $formName : '',
+        'deleted'  => !empty($deleted),
+        'formId'   => $formId ?? 0,
+        'formName' => $formName ?? '',
     ]
 );
