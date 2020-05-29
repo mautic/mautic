2c768b163df55e257aee2c13c736b810a4e86c62
John Linhart <admin@escope.cz>
2020-05-29 12:18:09 +0200

    Mapped fields must be passed to select via country template

--- app/bundles/FormBundle/Views/Field/country.html.php
+++ app/bundles/FormBundle/Views/Field/country.html.php
@@ -3,12 +3,13 @@
 echo $view->render(
     'MauticFormBundle:Field:select.html.php',
     [
-        'field'    => $field,
-        'fields'   => isset($fields) ? $fields : [],
-        'inForm'   => (isset($inForm)) ? $inForm : false,
-        'list'     => \Mautic\LeadBundle\Helper\FormFieldHelper::getCountryChoices(),
-        'id'       => $id,
-        'formId'   => (isset($formId)) ? $formId : 0,
-        'formName' => (isset($formName)) ? $formName : '',
+        'field'        => $field,
+        'fields'       => $fields ?? [],
+        'mappedFields' => $mappedFields,
+        'inForm'       => $inForm ?? false,
+        'list'         => \Mautic\LeadBundle\Helper\FormFieldHelper::getCountryChoices(),
+        'id'           => $id,
+        'formId'       => $formId ?? 0,
+        'formName'     => $formName ?? '',
     ]
 );
