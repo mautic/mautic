639afeee3bc56aca1660e7e1e181db691f6ece4e
Lukas Drahy <lukas@drahy.net>
2020-06-02 13:22:28 +0200

    Fix notice

--- app/bundles/CampaignBundle/Controller/EventController.php
+++ app/bundles/CampaignBundle/Controller/EventController.php
@@ -259,6 +259,7 @@ class EventController extends CommonFormController
          * AJAX request, or we are not granted campaign edit/create, deny access.
          */
         if (empty($event)
+            || empty($event['eventType'])
             || !in_array($event['eventType'], $this->supportedEventTypes)
             || !isset($event['type'])
             || !$this->request->isXmlHttpRequest()
