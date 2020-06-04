7292ce83789f78bb94303aeeae4bbecb0cd38beb
John Linhart <admin@escope.cz>
2020-06-04 12:33:33 +0200

    Ensure the $type is string (can be null)

--- app/bundles/FormBundle/Form/Type/FieldType.php
+++ app/bundles/FormBundle/Form/Type/FieldType.php
@@ -457,7 +457,7 @@ class FieldType extends AbstractType
                         'tooltip' => 'mautic.form.field.help.mapped.field',
                     ],
                     'required' => false,
-                    'data'     => $mappedField ?? $this->getDefaultMappedField($type),
+                    'data'     => $mappedField ?? $this->getDefaultMappedField((string) $type),
                 ]
             );
 
