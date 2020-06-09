efbc16797fde58ebdcc3a2d10ac404af9a8177f1
John Linhart <admin@escope.cz>
2020-06-09 12:32:26 +0200

    Returning back JS tag removal so it would not trigger in the builder.
    https://github.com/mautic/mautic/pull/8794#pullrequestreview-426807273

--- app/bundles/FormBundle/Views/Field/freehtml.html.php
+++ app/bundles/FormBundle/Views/Field/freehtml.html.php
@@ -12,7 +12,7 @@ $defaultInputClass = $containerType = 'freehtml';
 include __DIR__.'/field_helper.php';
 
 if ($inBuilder) {
-    $htmlContent = $properties['text'];
+    $htmlContent = $view['content']->showScriptTags($properties['text']);
 } else {
     $htmlContent = $properties['text'];
 }
