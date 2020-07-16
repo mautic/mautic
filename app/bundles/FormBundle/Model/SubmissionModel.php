0171e9cc957435149a5d455ff48eaf25d11a9f12
Alfredo Arena <alfredo.arena@ctmobi.it>
2020-07-16 10:17:16 +0200

    Removed unused variable

--- app/bundles/FormBundle/Model/SubmissionModel.php
+++ app/bundles/FormBundle/Model/SubmissionModel.php
@@ -695,7 +695,7 @@ class SubmissionModel extends CommonFormModel
         switch ($format) {
             case 'csv':
                 $response = new StreamedResponse(
-                    function () use ($results, $page, $translator) {
+                    function () use ($results, $translator) {
                         $handle = fopen('php://output', 'r+');
 
                         //build the header row
@@ -754,7 +754,7 @@ class SubmissionModel extends CommonFormModel
             case 'xlsx':
                 if (class_exists(Spreadsheet::class)) {
                     $response = new StreamedResponse(
-                        function () use ($results, $page, $translator, $name) {
+                        function () use ($results, $translator, $name) {
                             $objPHPExcel = new Spreadsheet();
                             $objPHPExcel->getProperties()->setTitle($name);
 
