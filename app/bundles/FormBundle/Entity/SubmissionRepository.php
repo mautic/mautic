41471e1227b9c58fffc4bd40b73f70afdcb390e5
Alfredo Arena <alfredo.arena@ctmobi.it>
2020-07-16 09:53:58 +0200

    Added documentation for getEntitiesByPage method

--- app/bundles/FormBundle/Entity/SubmissionRepository.php
+++ app/bundles/FormBundle/Entity/SubmissionRepository.php
@@ -205,7 +205,9 @@ class SubmissionRepository extends CommonRepository
     }
 
     /**
-     * {@inheritdoc}
+     * Get all submissions that derive from a landing page.
+     *
+     * @return array<int,Submission>
      */
     public function getEntitiesByPage(array $args = [])
     {
