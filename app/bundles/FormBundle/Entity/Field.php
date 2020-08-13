704d138bc97a3ed9780db8e5e868f2ebb88376e3
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-08-13 16:58:59 +0200

    Fix focus with progressive form

--- app/bundles/FormBundle/Entity/Field.php
+++ app/bundles/FormBundle/Entity/Field.php
@@ -893,9 +893,7 @@ class Field
         }
 
         if ($displayManager && $displayManager->useProgressiveProfilingLimit()) {
-            if ($displayManager->showForField($this)) {
-                return true;
-            } else {
+            if (!$displayManager->showForField($this)) {
                 return false;
             }
         }
