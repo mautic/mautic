d423d351ac5eccb6b75ab927717b9226b4e40a74
Anton Vlasenko <vlasenko.anton@gmail.com>
2020-08-13 13:35:20 +0200

    Add SQL queries for migration.

--- app/migrations/Version20200812180900.php
+++ app/migrations/Version20200812180900.php
@@ -11,7 +11,7 @@
 namespace Mautic\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Types\TextType;
+use Doctrine\DBAL\Schema\SchemaException;
 use Doctrine\Migrations\Exception\SkipMigration;
 use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 
@@ -20,4 +20,24 @@ use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
  */
 class Version20200812180900 extends AbstractMauticMigration
 {
+    /**
+     * @throws SkipMigration
+     * @throws SchemaException
+     */
+    public function preUp(Schema $schema): void
+    {
+        if ($schema->getTable("{$this->prefix}lead_lists")->hasColumn('last_build_date')) {
+            throw new SkipMigration("The last_build_date column has already been added to the {$this->prefix}lead_lists table.");
+        }
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("ALTER TABLE `{$this->prefix}lead_lists` ADD COLUMN `last_build_date` DATETIME NULL DEFAULT NULL AFTER `checked_out_by_user`");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("ALTER TABLE {$this->prefix}email_copies DROP COLUMN `last_build_date`");
+    }
 }
