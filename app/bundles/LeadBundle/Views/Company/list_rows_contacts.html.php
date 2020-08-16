362ed2739e1dfe6664bdf77b03e9f75db011ab4b
Alfredo Arena <alfredo.arena@ctmobi.it>
2020-08-16 09:18:47 +0200

    Updated year of new files

--- app/bundles/LeadBundle/Views/Company/list_rows_contacts.html.php
+++ app/bundles/LeadBundle/Views/Company/list_rows_contacts.html.php
@@ -1,6 +1,15 @@
 
 <?php
 
+/*
+ * @copyright   2020 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 $baseUrl = $view['router']->path(
     'mautic_company_contacts_list',
     [
