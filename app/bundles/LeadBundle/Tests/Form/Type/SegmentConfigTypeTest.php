3befb54ef4245797fc42656c63b552ea6f2934bd
Anton Vlasenko <vlasenko.anton@gmail.com>
2020-08-20 15:57:15 +0200

    Add a segment config type test.

---
+++ app/bundles/LeadBundle/Tests/Form/Type/SegmentConfigTypeTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright   2020 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\LeadBundle\Tests\Form\Type;
+
+use Mautic\LeadBundle\Form\Type\SegmentConfigType;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class SegmentConfigTypeTest extends TestCase
+{
+    /**
+     * @var SegmentConfigType
+     */
+    private $segmentConfigType;
+
+    /**
+     * @var FormBuilderInterface
+     */
+    private $formBuilderInterface;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->segmentConfigType    = new SegmentConfigType();
+        $this->formBuilderInterface = $this->createMock(FormBuilderInterface::class);
+    }
+
+    public function testThatGetBlockPrefixReturnsAValue(): void
+    {
+        $blockPrefix = $this->segmentConfigType->getBlockPrefix();
+        $this->assertNotEmpty($blockPrefix);
+        $this->assertTrue(is_string($blockPrefix));
+    }
+
+    public function testThatBuildFormMethodAddsSegmentRebuildTimeWarningOption(): void
+    {
+        $parameters = [
+            'label'      => 'mautic.lead.list.form.config.segment_rebuild_time_warning',
+            'label_attr' => [
+                    'class' => 'control-label',
+                ],
+            'attr' => [
+                    'class'   => 'form-control',
+                    'tooltip' => 'mautic.lead.list.form.config.segment_rebuild_time_warning.tooltip',
+                ],
+            'required' => false,
+        ];
+
+        $this->formBuilderInterface->expects($this->once())
+            ->method('add')
+            ->with(
+                'segment_rebuild_time_warning',
+                NumberType::class,
+                $parameters
+            );
+
+        $this->segmentConfigType->buildForm($this->formBuilderInterface, []);
+    }
+}
