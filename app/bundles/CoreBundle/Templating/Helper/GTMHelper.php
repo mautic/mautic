b37ebf04ee9e7c2391651fbdf68e299831468e4f
Alfredo Arena <alfredo.arena@ctmobi.it>
2020-08-27 09:18:15 +0200

    Change method of instantiate variable

--- app/bundles/CoreBundle/Templating/Helper/GTMHelper.php
+++ app/bundles/CoreBundle/Templating/Helper/GTMHelper.php
@@ -65,13 +65,12 @@ class GTMHelper extends Helper
     public function getHeadGTMCode()
     {
         $id = $this->code;
-        $js = <<<JS
+        $js = "
             <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','{$id}');</script>
-    JS;
+    })(window,document,'script','dataLayer','{$id}');</script>";
 
         return $id ? $js : '';
     }
