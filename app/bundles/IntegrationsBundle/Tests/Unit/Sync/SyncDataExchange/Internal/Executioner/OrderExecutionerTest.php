1d123f7d65a4ac8365ed0ab44065f35b574acebc
fedys <miroslav.fedeles@gmail.com>
2020-09-07 17:45:04 +0200

    Fixed existing tests

--- app/bundles/IntegrationsBundle/Tests/Unit/Sync/SyncDataExchange/Internal/Executioner/OrderExecutionerTest.php
+++ app/bundles/IntegrationsBundle/Tests/Unit/Sync/SyncDataExchange/Internal/Executioner/OrderExecutionerTest.php
@@ -2,18 +2,37 @@
 
 declare(strict_types=1);
 
+/*
+ * @copyright   2018 Mautic Inc. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://www.mautic.com
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 namespace Mautic\IntegrationsBundle\Tests\Unit\Sync\SyncDataExchange\Internal\Executioner;
 
+use DateTime;
+use DateTimeImmutable;
+use Mautic\IntegrationsBundle\Entity\ObjectMapping;
 use Mautic\IntegrationsBundle\Event\InternalObjectCreateEvent;
 use Mautic\IntegrationsBundle\Event\InternalObjectUpdateEvent;
 use Mautic\IntegrationsBundle\IntegrationEvents;
+use Mautic\IntegrationsBundle\Sync\DAO\Mapping\UpdatedObjectMappingDAO;
 use Mautic\IntegrationsBundle\Sync\DAO\Sync\Order\ObjectChangeDAO;
 use Mautic\IntegrationsBundle\Sync\DAO\Sync\Order\OrderDAO;
+use Mautic\IntegrationsBundle\Sync\Exception\ObjectNotFoundException;
 use Mautic\IntegrationsBundle\Sync\Helper\MappingHelper;
+use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\Executioner\FieldValidator;
 use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\Executioner\OrderExecutioner;
+use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\Executioner\ReferenceResolver;
 use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\Object\Company;
 use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\Object\Contact;
+use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\Object\ObjectInterface;
 use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\ObjectProvider;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -22,17 +41,17 @@ class OrderExecutionerTest extends TestCase
     private const INTEGRATION_NAME = 'Test';
 
     /**
-     * @var MappingHelper|\PHPUnit\Framework\MockObject\MockObject
+     * @var MappingHelper|MockObject
      */
     private $mappingHelper;
 
     /**
-     * @var EventDispatcherInterface|\PHPUnit\Framework\MockObject\MockObject
+     * @var EventDispatcherInterface|MockObject
      */
     private $dispatcher;
 
     /**
-     * @var ObjectProvider|\PHPUnit\Framework\MockObject\MockObject
+     * @var ObjectProvider|MockObject
      */
     private $objectProvider;
 
@@ -41,15 +60,29 @@ class OrderExecutionerTest extends TestCase
      */
     private $orderExecutioner;
 
+    /**
+     * @var ReferenceResolver|MockObject
+     */
+    private $referenceResolver;
+
+    /**
+     * @var FieldValidator|MockObject
+     */
+    private $fieldValidator;
+
     protected function setup(): void
     {
-        $this->mappingHelper    = $this->createMock(MappingHelper::class);
-        $this->dispatcher       = $this->createMock(EventDispatcherInterface::class);
-        $this->objectProvider   = $this->createMock(ObjectProvider::class);
-        $this->orderExecutioner = new OrderExecutioner(
+        $this->mappingHelper     = $this->createMock(MappingHelper::class);
+        $this->dispatcher        = $this->createMock(EventDispatcherInterface::class);
+        $this->objectProvider    = $this->createMock(ObjectProvider::class);
+        $this->referenceResolver = $this->createMock(ReferenceResolver::class);
+        $this->fieldValidator    = $this->createMock(FieldValidator::class);
+        $this->orderExecutioner  = new OrderExecutioner(
             $this->mappingHelper,
             $this->dispatcher,
-            $this->objectProvider
+            $this->objectProvider,
+            $this->referenceResolver,
+            $this->fieldValidator
         );
     }
 
@@ -66,9 +99,9 @@ class OrderExecutionerTest extends TestCase
                 [
                     IntegrationEvents::INTEGRATION_UPDATE_INTERNAL_OBJECTS,
                     $this->callback(function (InternalObjectUpdateEvent $event) {
-                        $this->assertSame(Contact::NAME, $event->getObject()->getName());
-                        $this->assertSame([1, 2], $event->getIdentifiedObjectIds());
-                        $this->assertCount(2, $event->getUpdateObjects());
+                        Assert::assertSame(Contact::NAME, $event->getObject()->getName());
+                        Assert::assertSame([1, 2], $event->getIdentifiedObjectIds());
+                        Assert::assertCount(2, $event->getUpdateObjects());
 
                         return true;
                     }),
@@ -76,8 +109,8 @@ class OrderExecutionerTest extends TestCase
                 [
                     IntegrationEvents::INTEGRATION_CREATE_INTERNAL_OBJECTS,
                     $this->callback(function (InternalObjectCreateEvent $event) {
-                        $this->assertSame(Contact::NAME, $event->getObject()->getName());
-                        $this->assertCount(1, $event->getCreateObjects());
+                        Assert::assertSame(Contact::NAME, $event->getObject()->getName());
+                        Assert::assertCount(1, $event->getCreateObjects());
 
                         return true;
                     }),
@@ -89,6 +122,81 @@ class OrderExecutionerTest extends TestCase
 
         $this->mappingHelper->expects($this->exactly(1))
             ->method('saveObjectMappings');
+
+        $this->referenceResolver->expects($this->exactly(2))
+            ->method('resolveReferences');
+
+        $this->fieldValidator->expects($this->exactly(2))
+            ->method('validateFields');
+
+        $this->orderExecutioner->execute($this->getSyncOrder(Contact::NAME));
+    }
+
+    public function testUpdatedObjectsWithoutAnObjectMappingDoesNotGetAddedToObjectMappingsDAO(): void
+    {
+        $this->objectProvider->expects($this->exactly(2))
+            ->method('getObjectByName')
+            ->with(Contact::NAME)
+            ->willReturn(new Contact());
+
+        $this->dispatcher->expects($this->exactly(2))
+            ->method('dispatch')
+            ->withConsecutive(
+                [
+                    IntegrationEvents::INTEGRATION_UPDATE_INTERNAL_OBJECTS,
+                    $this->callback(function (InternalObjectUpdateEvent $event) {
+                        Assert::assertSame(Contact::NAME, $event->getObject()->getName());
+                        Assert::assertSame([1, 2], $event->getIdentifiedObjectIds());
+                        Assert::assertCount(2, $event->getUpdateObjects());
+
+                        $updatedObjectMappings = [];
+                        foreach ($event->getUpdateObjects() as $key => $updateObject) {
+                            $updatedObjectMappings[] = $updatedObjectMapping = new UpdatedObjectMappingDAO(
+                                $updateObject->getIntegration(),
+                                $updateObject->getObject(),
+                                $updateObject->getObjectId(),
+                                new DateTime()
+                            );
+
+                            if (0 !== $key) {
+                                // Only inject an object mapping for one of the objects
+                                break;
+                            }
+
+                            $objectMapping = new ObjectMapping();
+                            $objectMapping->setIntegration($updateObject->getIntegration());
+                            $objectMapping->setIntegrationObjectName($updateObject->getObject());
+                            $objectMapping->setIntegrationObjectId($updateObject->getObjectId());
+                            $updatedObjectMapping->setObjectMapping($objectMapping);
+                        }
+
+                        $event->setUpdatedObjectMappings($updatedObjectMappings);
+
+                        return true;
+                    }),
+                ],
+                [
+                    IntegrationEvents::INTEGRATION_CREATE_INTERNAL_OBJECTS,
+                    $this->callback(function (InternalObjectCreateEvent $event) {
+                        Assert::assertSame(Contact::NAME, $event->getObject()->getName());
+                        Assert::assertCount(1, $event->getCreateObjects());
+
+                        return true;
+                    }),
+                ]
+            );
+
+        $this->mappingHelper->expects($this->exactly(1))
+            ->method('updateObjectMappings');
+
+        $this->mappingHelper->expects($this->exactly(1))
+            ->method('saveObjectMappings');
+
+        $this->referenceResolver->expects($this->exactly(2))
+            ->method('resolveReferences');
+
+        $this->fieldValidator->expects($this->exactly(2))
+            ->method('validateFields');
 
         $this->orderExecutioner->execute($this->getSyncOrder(Contact::NAME));
     }
@@ -106,9 +214,9 @@ class OrderExecutionerTest extends TestCase
                 [
                     IntegrationEvents::INTEGRATION_UPDATE_INTERNAL_OBJECTS,
                     $this->callback(function (InternalObjectUpdateEvent $event) {
-                        $this->assertSame(Company::NAME, $event->getObject()->getName());
-                        $this->assertSame([1, 2], $event->getIdentifiedObjectIds());
-                        $this->assertCount(2, $event->getUpdateObjects());
+                        Assert::assertSame(Company::NAME, $event->getObject()->getName());
+                        Assert::assertSame([1, 2], $event->getIdentifiedObjectIds());
+                        Assert::assertCount(2, $event->getUpdateObjects());
 
                         return true;
                     }),
@@ -116,8 +224,8 @@ class OrderExecutionerTest extends TestCase
                 [
                     IntegrationEvents::INTEGRATION_CREATE_INTERNAL_OBJECTS,
                     $this->callback(function (InternalObjectCreateEvent $event) {
-                        $this->assertSame(Company::NAME, $event->getObject()->getName());
-                        $this->assertCount(1, $event->getCreateObjects());
+                        Assert::assertSame(Company::NAME, $event->getObject()->getName());
+                        Assert::assertCount(1, $event->getCreateObjects());
 
                         return true;
                     }),
@@ -129,6 +237,12 @@ class OrderExecutionerTest extends TestCase
 
         $this->mappingHelper->expects($this->exactly(1))
             ->method('saveObjectMappings');
+
+        $this->referenceResolver->expects($this->exactly(2))
+            ->method('resolveReferences');
+
+        $this->fieldValidator->expects($this->exactly(2))
+            ->method('validateFields');
 
         $syncOrder = $this->getSyncOrder(Company::NAME);
         $this->orderExecutioner->execute($syncOrder);
@@ -157,7 +271,24 @@ class OrderExecutionerTest extends TestCase
                 [
                     IntegrationEvents::INTEGRATION_UPDATE_INTERNAL_OBJECTS,
                     $this->callback(function (InternalObjectUpdateEvent $event) {
-                        $this->assertSame(Contact::NAME, $event->getObject()->getName());
+                        Assert::assertSame(Contact::NAME, $event->getObject()->getName());
+
+                        $updatedObjectMappings = [];
+                        foreach ($event->getUpdateObjects() as $updateObject) {
+                            $updatedObjectMappings[] = $updatedObjectMapping = new UpdatedObjectMappingDAO(
+                                $updateObject->getIntegration(),
+                                $updateObject->getObject(),
+                                $updateObject->getObjectId(),
+                                new DateTime()
+                            );
+
+                            $objectMapping = new ObjectMapping();
+                            $objectMapping->setIntegration($updateObject->getIntegration());
+                            $objectMapping->setIntegrationObjectName($updateObject->getObject());
+                            $objectMapping->setIntegrationObjectId($updateObject->getObjectId());
+                            $updatedObjectMapping->setObjectMapping($objectMapping);
+                        }
+                        $event->setUpdatedObjectMappings($updatedObjectMappings);
 
                         return true;
                     }),
@@ -165,7 +296,25 @@ class OrderExecutionerTest extends TestCase
                 [
                     IntegrationEvents::INTEGRATION_UPDATE_INTERNAL_OBJECTS,
                     $this->callback(function (InternalObjectUpdateEvent $event) {
-                        $this->assertSame(Company::NAME, $event->getObject()->getName());
+                        Assert::assertSame(Company::NAME, $event->getObject()->getName());
+
+                        $updatedObjectMappings = [];
+                        foreach ($event->getUpdateObjects() as $updateObject) {
+                            $updatedObjectMappings[] = $updatedObjectMapping = new UpdatedObjectMappingDAO(
+                                $updateObject->getIntegration(),
+                                $updateObject->getObject(),
+                                $updateObject->getObjectId(),
+                                new DateTime()
+                            );
+
+                            $objectMapping = new ObjectMapping();
+                            $objectMapping->setIntegration($updateObject->getIntegration());
+                            $objectMapping->setIntegrationObjectName($updateObject->getObject());
+                            $objectMapping->setIntegrationObjectId($updateObject->getObjectId());
+                            $updatedObjectMapping->setObjectMapping($objectMapping);
+                        }
+
+                        $event->setUpdatedObjectMappings($updatedObjectMappings);
 
                         return true;
                     }),
@@ -173,7 +322,18 @@ class OrderExecutionerTest extends TestCase
                 [
                     IntegrationEvents::INTEGRATION_CREATE_INTERNAL_OBJECTS,
                     $this->callback(function (InternalObjectCreateEvent $event) {
-                        $this->assertSame(Contact::NAME, $event->getObject()->getName());
+                        Assert::assertSame(Contact::NAME, $event->getObject()->getName());
+
+                        $createdObjectMappings = [];
+                        foreach ($event->getCreateObjects() as $createObject) {
+                            $objectMapping = new ObjectMapping();
+                            $objectMapping->setIntegration($createObject->getIntegration());
+                            $objectMapping->setIntegrationObjectName($createObject->getObject());
+                            $objectMapping->setIntegrationObjectId($createObject->getObjectId());
+
+                            $createdObjectMappings[] = $objectMapping;
+                        }
+                        $event->setObjectMappings($createdObjectMappings);
 
                         return true;
                     }),
@@ -181,7 +341,18 @@ class OrderExecutionerTest extends TestCase
                 [
                     IntegrationEvents::INTEGRATION_CREATE_INTERNAL_OBJECTS,
                     $this->callback(function (InternalObjectCreateEvent $event) {
-                        $this->assertSame(Company::NAME, $event->getObject()->getName());
+                        Assert::assertSame(Company::NAME, $event->getObject()->getName());
+
+                        $createdObjectMappings = [];
+                        foreach ($event->getCreateObjects() as $createObject) {
+                            $objectMapping = new ObjectMapping();
+                            $objectMapping->setIntegration($createObject->getIntegration());
+                            $objectMapping->setIntegrationObjectName($createObject->getObject());
+                            $objectMapping->setIntegrationObjectId($createObject->getObjectId());
+
+                            $createdObjectMappings[] = $objectMapping;
+                        }
+                        $event->setObjectMappings($createdObjectMappings);
 
                         return true;
                     }),
@@ -201,6 +372,58 @@ class OrderExecutionerTest extends TestCase
             $syncOrder->addObjectChange($objectChange);
         }
 
+        $orderMappings = $this->orderExecutioner->execute($syncOrder);
+        Assert::assertCount(4, $orderMappings->getUpdatedMappings());
+        Assert::assertCount(2, $orderMappings->getNewMappings());
+    }
+
+    public function testEmptyObjectsForUpdateDoesNothing()
+    {
+        $syncOrder = new OrderDAO(new DateTimeImmutable(), false, self::INTEGRATION_NAME);
+        $syncOrder->addObjectChange(new ObjectChangeDAO(self::INTEGRATION_NAME, 'bar', null, 'bar', 4));
+
+        $this->dispatcher->expects($this->once())
+            ->method('dispatch')
+            ->with(IntegrationEvents::INTEGRATION_CREATE_INTERNAL_OBJECTS, $this->isInstanceOf(InternalObjectCreateEvent::class));
+
+        $this->orderExecutioner->execute($syncOrder);
+    }
+
+    public function testEmptyObjectsForCreateDoesNothing()
+    {
+        $syncOrder = new OrderDAO(new DateTimeImmutable(), false, self::INTEGRATION_NAME);
+        $syncOrder->addObjectChange(new ObjectChangeDAO(self::INTEGRATION_NAME, 'bar', 4, 'bar', 4));
+
+        $this->dispatcher->expects($this->once())
+            ->method('dispatch')
+            ->with(IntegrationEvents::INTEGRATION_UPDATE_INTERNAL_OBJECTS, $this->isInstanceOf(InternalObjectUpdateEvent::class));
+
+        $this->orderExecutioner->execute($syncOrder);
+    }
+
+    public function testObjectNotFoundExceptionIsLoggedAndNothingElse()
+    {
+        $syncOrder = $this->getSyncOrder('foo');
+        $syncOrder->addObjectChange(new ObjectChangeDAO(self::INTEGRATION_NAME, 'bar', 4, 'bar', 4));
+        $syncOrder->addObjectChange(new ObjectChangeDAO(self::INTEGRATION_NAME, 'bar', null, 'bar', 4));
+
+        // update and create per object
+        $this->objectProvider->expects($this->exactly(4))
+            ->method('getObjectByName')
+            ->willReturnCallback(
+                function (string $objectName) {
+                    if ('bar' === $objectName) {
+                        throw new ObjectNotFoundException($objectName);
+                    }
+
+                    return $this->createMock(ObjectInterface::class);
+                }
+            );
+
+        // only foo should recognized and processed
+        $this->dispatcher->expects($this->exactly(2))
+            ->method('dispatch');
+
         $this->orderExecutioner->execute($syncOrder);
     }
 
@@ -209,7 +432,7 @@ class OrderExecutionerTest extends TestCase
      */
     private function getSyncOrder(string $objectName): OrderDAO
     {
-        $syncOrder = new OrderDAO(new \DateTimeImmutable(), false, self::INTEGRATION_NAME);
+        $syncOrder = new OrderDAO(new DateTimeImmutable(), false, self::INTEGRATION_NAME);
 
         // Two updates
         $syncOrder->addObjectChange(new ObjectChangeDAO(self::INTEGRATION_NAME, $objectName, 1, $objectName, 1));
