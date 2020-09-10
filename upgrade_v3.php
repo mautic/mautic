169db20e41b58b739315ba2c8a763e843e9732bd
Jacob Shilitz <jacobshilitz@gmail.com>
2020-09-10 11:16:55 -0400

    Fix V3 Upgrade to detect that its already in CLI
    We should detect CLI before recommending it, it makes now seance to tell the user `GO TO CLI` in the time I'm already there

--- upgrade_v3.php
+++ upgrade_v3.php
@@ -196,7 +196,7 @@ function runPreUpgradeChecks()
             } else {
                 $count_leads = intval($result->fetch_row()[0]);
 
-                if ($count_leads >= 10000) {
+                if ($count_leads >= 10000 && php_sapi_name() != 'cli') {
                     $preUpgradeWarnings[] = 'You have 10000 or more contacts in your system. We recommend upgrading by using the CLI (command line) for the best performance and stability.';
                 }
             }
