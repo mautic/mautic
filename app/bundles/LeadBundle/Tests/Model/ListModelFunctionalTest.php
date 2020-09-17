6c535750ef2465a1ab3d705c5f9a8c549b301332
John Linhart <admin@escope.cz>
2020-09-17 18:05:20 +0200

    Functional test started

--- app/bundles/LeadBundle/Tests/Model/ListModelFunctionalTest.php
+++ app/bundles/LeadBundle/Tests/Model/ListModelFunctionalTest.php
@@ -3,9 +3,12 @@
 namespace Mautic\LeadBundle\Tests\Model;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\LeadBundle\Entity\Lead;
 use Mautic\LeadBundle\Entity\LeadList;
 use Mautic\LeadBundle\Entity\LeadListRepository;
+use Mautic\LeadBundle\Entity\LeadRepository;
 use Mautic\UserBundle\Entity\User;
+use Mautic\LeadBundle\Model\ListModel;
 use PHPUnit\Framework\Assert;
 
 class ListModelFunctionalTest extends MauticMysqlTestCase
@@ -43,5 +46,39 @@ class ListModelFunctionalTest extends MauticMysqlTestCase
         $this->em->persist($leadList);
 
         return $leadList;
+    }
+
+    public function testSegmentLineChartData(): void
+    {
+        /** @var ListModel $segmentModel */
+        $segmentModel = $this->container->get('mautic.lead.model.list');
+
+        /** @var LeadRepository $contactRepository */
+        $contactRepository = $this->em->getRepository(Lead::class);
+
+        $segment = new LeadList();
+        $segment->setName('Segment A');
+
+        $segmentModel->saveEntity($segment);
+
+        $contactA = new Lead();
+        $contactB = new Lead();
+        $contactC = new Lead();
+
+        $contactRepository->saveEntities([$contactA, $contactB, $contactC]);
+
+        $segmentModel->addLead($contactA, $segment); // Emulating adding by a filter.
+        $segmentModel->addLead($contactB, $segment); // Emulating adding by a filter.
+        $segmentModel->addLead($contactC, $segment, true); // Manually added.
+
+        $data = $segmentModel->getSegmentContactsLineChartData(
+            'd',
+            new \DateTime('1 month ago', new \DateTimeZone('UTC')),
+            new \DateTime('now', new \DateTimeZone('UTC')),
+            null,
+            ['leadlist_id' => ['value' => $segment->getId(), 'list_column_name' => 't.lead_id']]
+        );
+        dump($data);
+        Assert::assertSame(3, $data['datasets'][0]['data'][31]); // Total count for today.
     }
 }
