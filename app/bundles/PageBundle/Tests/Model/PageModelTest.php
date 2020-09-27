5f2f96a0c60f7d8af7825889e31de60d7fe31b5f
Adrian Schimpf <adrian@smolio.ch>
2020-09-27 12:53:34 +0200

    add test for UTM params

--- app/bundles/PageBundle/Tests/Model/PageModelTest.php
+++ app/bundles/PageBundle/Tests/Model/PageModelTest.php
@@ -1,10 +1,11 @@
 <?php
 
-/*
+declare(strict_types=1);
+/**
  * @copyright   2014 Mautic Contributors. All rights reserved
  * @author      Mautic
  *
- * @link        http://mautic.org
+ * @see        http://mautic.org
  *
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -15,6 +16,7 @@ use Mautic\CoreBundle\Entity\IpAddress;
 use Mautic\LeadBundle\Entity\Lead;
 use Mautic\PageBundle\Entity\Hit;
 use Mautic\PageBundle\Entity\Page;
+use Mautic\PageBundle\Entity\Redirect;
 use Mautic\PageBundle\Tests\PageTestAbstract;
 use ReflectionClass;
 use Symfony\Component\HttpFoundation\Request;
@@ -67,5 +69,75 @@ class PageModelTest extends PageTestAbstract
             'page_url'      => 'http://mautic.com/page/test?hello=world&lorem=ipsum',
             'page_language' => 'en',
         ]);
+    }
+
+    public function testGetHitQueryRequest()
+    {
+        $pageModel         = $this->getPageModel();
+
+        foreach ($this->getQueryParams() as $params) {
+            $request = new Request($params);
+
+            $query = $pageModel->getHitQuery($request);
+            $this->assertQuery($query);
+        }
+    }
+
+    public function testGetHitQueryRedirect()
+    {
+        $pageModel         = $this->getPageModel();
+        $request           = new Request();
+        $redirect          = new Redirect();
+
+        foreach ($this->getQueryParams() as $params) {
+            $redirect->setUrl($params['page_url']);
+            $query = $pageModel->getHitQuery($request, $redirect);
+            $this->assertQuery($query);
+        }
+    }
+
+    private function assertQuery(array $query)
+    {
+        $this->assertArrayHasKey('utm_source', $query, 'utm_source not found');
+        $this->assertArrayHasKey('utm_medium', $query, 'utm_medium not found');
+        $this->assertArrayHasKey('utm_campaign', $query, 'utm_campaign not found');
+        $this->assertArrayHasKey('utm_content', $query, 'utm_content not found');
+        // evaluate all utm tags that they contain the key name in the value
+        foreach ($query as $key => $value) {
+            if (false !== strpos($key, 'utm_')) {
+                $this->assertNotFalse(strpos($value, $key), sprintf('%s not found in %s', $key, $value));
+            }
+        }
+    }
+
+    private function getQueryParams()
+    {
+        return [[
+            'page_title'      => 'Testpage',
+            'page_language'   => 'en-GB',
+            'page_referrer'   => '',
+            'page_url'        => 'https://www.domain.com/testpage/?utm_source=test-utm_source&utm_medium=test-utm_medium&utm_campaign=test-utm_campaign&utm_content=test-utm_content',
+            'counter'         => 0,
+            'mautic_device_id'=> 'nowvkqdf6113236eokcg7qs',
+            'resolution'      => '1792x1120',
+            'timezone_offset' => -120,
+            'platform'        => 'MacIntel',
+            'do_not_track'    => 1,
+            'adblock'         => false,
+            'fingerprint'     => 'fec25ab2d659c4153c7f1d5724841132',
+        ], [
+            'page_title'      => 'Testpage Special Chars',
+            'page_language'   => 'en-GB',
+            'page_referrer'   => '',
+            'page_url'        => 'https://www.domain.com/testpage/?utm_source=t%C3%A9%C3%A0%C3%A8st-utm_source&utm_medium=t%C3%A4%C3%B6ust-utm_medium&utm_campaign=te+%20%C2%B0st-utm_campaign&utm_content=t%E4%BD%A0%E5%A5%BDt-utm_content',
+            'counter'         => 0,
+            'mautic_device_id'=> 'nowvkqdf6113236eokcg7qs',
+            'resolution'      => '1792x1120',
+            'timezone_offset' => -120,
+            'platform'        => 'MacIntel',
+            'do_not_track'    => 1,
+            'adblock'         => false,
+            'fingerprint'     => 'fec25ab2d659c4153c7f1d5724841132',
+        ]];
     }
 }
