ffcff92ba195ef0815953580ba71b733d39a6ae3
Saurabh Gupta <saurabh.gupta@acquia.com>
2020-10-05 21:11:35 +0530

    Add redirection for 404 page

--- app/bundles/CoreBundle/Config/config.php
+++ app/bundles/CoreBundle/Config/config.php
@@ -764,6 +764,8 @@ return [
                     'router',
                     '"MauticCoreBundle:Exception:show"',
                     'monolog.logger.mautic',
+                    'mautic.helper.core_parameters',
+                    'mautic.model.factory',
                 ],
                 'tag'          => 'kernel.event_listener',
                 'tagArguments' => [
