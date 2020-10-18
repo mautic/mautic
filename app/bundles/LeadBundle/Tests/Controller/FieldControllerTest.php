4df25e76176af7500634ee1c9eebf72d8209796f
Rohit Pandita <rohit3nov@gmail.com>
2020-10-18 20:04:53 +0530

    MAUT-4089: Bug Fix : improve functional test cases

--- app/bundles/LeadBundle/Tests/Controller/FieldControllerTest.php
+++ app/bundles/LeadBundle/Tests/Controller/FieldControllerTest.php
@@ -3,6 +3,7 @@
 namespace Mautic\LeadBundle\Tests\Controller;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\LeadBundle\Entity\LeadField;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,32 +11,29 @@ class FieldControllerTest extends MauticMysqlTestCase
 {
     public function testLengthValidationOnLabelFieldWhenAddingCustomFieldFailure()
     {
-        $crawler    = $this->client->request(Request::METHOD_GET, '/s/contacts/fields/new');
+        $crawler = $this->client->request(Request::METHOD_GET, '/s/contacts/fields/new');
 
-        $form = $crawler->selectButton('Save & Close')->form();
-        $form['leadfield[label]']->setValue('The leading Drupal Cloud platform to securely develop, deliver, and run websites, applications, and content. Top-of-the-line hosting options are paired with automated testing and development tools. Documentation is also included for the following components');
+        $form  = $crawler->selectButton('Save & Close')->form();
+        $label = 'The leading Drupal Cloud platform to securely develop, deliver, and run websites, applications, and content. Top-of-the-line hosting options are paired with automated testing and development tools. Documentation is also included for the following components';
+        $form['leadfield[label]']->setValue($label);
+        $crawler = $this->client->submit($form);
 
-        $crawler    = $this->client->submit($form);
-
-        $labelErrorMessage = trim($crawler->filter('#leadfield_label')->nextAll()->text());
-        // $maxLengthErrorMessageTemplate = $this->container->get('translator')->trans('mautic.lead.field.label.maxlength',[]);
+        $labelErrorMessage             = trim($crawler->filter('#leadfield_label')->nextAll()->text());
         $maxLengthErrorMessageTemplate = 'Label value cannot be longer than 191 characters';
-        $this->assertEquals($maxLengthErrorMessageTemplate, $labelErrorMessage);
 
-        // $this->assertContains('Label value cannot be longer than 192 characters', $this->client->getResponse()->getContent());
+        $this->assertEquals($maxLengthErrorMessageTemplate, $labelErrorMessage);
     }
 
     public function testLengthValidationOnLabelFieldWhenAddingCustomFieldSuccess()
     {
-        $crawler    = $this->client->request(Request::METHOD_GET, '/s/contacts/fields/new');
+        $crawler = $this->client->request(Request::METHOD_GET, '/s/contacts/fields/new');
 
-        $form = $crawler->selectButton('Save & Close')->form();
-        $form['leadfield[label]']->setValue('Random Department 3');
+        $form  = $crawler->selectButton('Save & Close')->form();
+        $label = 'Test value for custom field 4';
+        $form['leadfield[label]']->setValue($label);
+        $crawler = $this->client->submit($form);
 
-        $crawler    = $this->client->submit($form);
-
-        // check if the submitted field is in list
-        $crawler    = $this->client->request(Request::METHOD_GET, '/s/contacts/fields/2');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $field = $this->em->getRepository(LeadField::class)->findOneBy(['label' => $label]);
+        $this->assertNotNull($field);
     }
 }
