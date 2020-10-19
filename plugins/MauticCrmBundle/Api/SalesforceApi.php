a1b75af84ed863ba3302b64a44ecf5939d1f1453
Anton Vlasenko <vlasenko.anton@gmail.com>
2020-10-19 10:46:11 +0200

    Fix PHP warnings.

--- plugins/MauticCrmBundle/Api/SalesforceApi.php
+++ plugins/MauticCrmBundle/Api/SalesforceApi.php
@@ -111,7 +111,7 @@ class SalesforceApi extends CrmApi
         //try searching for lead as this has been changed before in updated done to the plugin
         if (isset($config['objects']) && false !== array_search('Contact', $config['objects']) && !empty($data['Contact']['Email'])) {
             $fields      = $this->integration->getFieldsForQuery('Contact');
-            unset($fields[array_search('HasOptedOutOfEmail', $fields)]);
+            unset($fields[array_search('HasOptedOutOfEmail', (array) $fields)]);
             $fields[]    = 'Id';
             $fields      = implode(', ', array_unique($fields));
             $findContact = 'select '.$fields.' from Contact where email = \''.$this->escapeQueryValue($data['Contact']['Email']).'\'';
@@ -124,7 +124,7 @@ class SalesforceApi extends CrmApi
 
         if (!empty($data['Lead']['Email'])) {
             $fields   = $this->integration->getFieldsForQuery('Lead');
-            unset($fields[array_search('HasOptedOutOfEmail', $fields)]);
+            unset($fields[array_search('HasOptedOutOfEmail', (array) $fields)]);
             $fields[] = 'Id';
             $fields   = implode(', ', array_unique($fields));
             $findLead = 'select '.$fields.' from Lead where email = \''.$this->escapeQueryValue($data['Lead']['Email']).'\' and ConvertedContactId = NULL';
