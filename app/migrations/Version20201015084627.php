02ebc2f5cc0bfeda72e26f6da85c4aae4136f5b5
Tejas Navghane <ts.navghane@gmail.com>
2020-10-20 11:54:30 +0530

    MAUT-3907 : Change query with update statement

--- app/migrations/Version20201015084627.php
+++ app/migrations/Version20201015084627.php
@@ -11,13 +11,12 @@ declare(strict_types=1);
 
 namespace Mautic\Migrations;
 
-use Doctrine\DBAL\DBALException;
-use Doctrine\DBAL\Driver\Statement as DriverStatement;
-use Doctrine\DBAL\FetchMode;
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\Migrations\Exception\SkipMigration;
 use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 
+/**
+ * Class Version20201015084627.
+ */
 final class Version20201015084627 extends AbstractMauticMigration
 {
     /**
@@ -25,38 +24,15 @@ final class Version20201015084627 extends AbstractMauticMigration
      */
     private $table = 'lead_fields';
 
-    /**
-     * @var string
-     */
-    private $transId = 'mautic.lead.field.timezone';
-
-    /**
-     * @throws DBALException
-     */
-    public function preUp(Schema $schema): void
+    public function up(Schema $schema): void
     {
-        $stmt = $this->getDriverStatement($this->getValue());
-        $stmt->execute();
-        $found = (bool) $stmt->fetch(FetchMode::ASSOCIATIVE);
-
-        if ($found) {
-            throw new SkipMigration('Schema includes this migration');
-        }
-    }
-
-    /**
-     * @throws DBALException
-     */
-    private function getDriverStatement(string $value): DriverStatement
-    {
-        $tableName = $this->getTableName();
-        $sql       = "
-            SELECT id
-            FROM $tableName
-            WHERE label = '$value'
-        ";
-
-        return $this->connection->prepare($sql);
+        $this->addSql(
+            sprintf(
+                "UPDATE %s SET label = '%s' WHERE alias = 'timezone';",
+                $this->getTableName(),
+                $this->getValue()
+            )
+        );
     }
 
     private function getTableName(): string
@@ -66,25 +42,6 @@ final class Version20201015084627 extends AbstractMauticMigration
 
     private function getValue(): string
     {
-        return $this->container->get('translator')->trans($this->transId);
-    }
-
-    /**
-     * @throws DBALException
-     */
-    public function up(Schema $schema): void
-    {
-        $stmt = $this->getDriverStatement($this->transId);
-        $stmt->execute();
-        $row = $stmt->fetch(FetchMode::ASSOCIATIVE);
-
-        if (!isset($row['id'])) {
-            throw new SkipMigration("Row with `$this->transId` value not found");
-        }
-
-        $id        = $row['id'];
-        $tableName = $this->getTableName();
-        $value     = $this->getValue();
-        $this->addSql("UPDATE $tableName SET label = '$value' WHERE id = $id;");
+        return $this->container->get('translator')->trans('mautic.lead.field.timezone');
     }
 }
