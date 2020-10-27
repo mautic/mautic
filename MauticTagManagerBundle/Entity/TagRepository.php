11f938872b6480eba9230f423275136282964481
Nikola Nedic <nikolaunedic@gmail.com>
2020-10-27 23:21:18 +0100

    Add tag controller
    Add index and list tag views
    
    Add nav menu item for tag manager
    
    Add lang file records
    
    Create tag form view
    
    Fix comments, menu item text and typos
    
    Add tag creating action
    
    Remove unesessary config item
    
    Fix navigation item for tag manager
    
    Exclude publish from tag manager permissions
    
    Extend tag model/entity/repository in order to define table alias
    
    Fix search
    
    Fix some typos
    
    Use overwritten tag model
    
    Fix permissions
    
    Add lang file records
    
    Add edit, clone and delete actions
    
    Add missing function
    
    Fix tags deletion
    
    Move tag form override to plugin
    
    Register tag manager repository
    
    Fix repository function name

---
+++ MauticTagManagerBundle/Entity/TagRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * @copyright   2015 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\MauticTagManagerBundle\Entity;
+
+use Mautic\LeadBundle\Entity\TagRepository as BaseTagRepository;
+
+/**
+ * Class TagRepository.
+ */
+class TagRepository extends BaseTagRepository
+{
+    /**
+     * @return array
+     */
+    protected function getDefaultOrder()
+    {
+        return [
+            ['lt.tag', 'ASC'],
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getTableAlias()
+    {
+        return 'lt';
+    }
+
+    /**
+     * Get a count of leads that belong to the tag.
+     *
+     * @param $tagIds
+     *
+     * @return array
+     */
+    public function countByLeads($tagIds)
+    {
+        $q = $this->getEntityManager()->getConnection()->createQueryBuilder();
+
+        $q->select('count(ltx.lead_id) as thecount, ltx.tag_id')
+            ->from(MAUTIC_TABLE_PREFIX.'lead_tags_xref', 'ltx');
+
+        $returnArray = (is_array($tagIds));
+
+        if (!$returnArray) {
+            $tagIds = [$tagIds];
+        }
+
+        $q->where(
+            $q->expr()->in('ltx.tag_id', $tagIds)
+        )
+            ->groupBy('ltx.tag_id');
+
+        $result = $q->execute()->fetchAll();
+
+        $return = [];
+        foreach ($result as $r) {
+            $return[$r['tag_id']] = $r['thecount'];
+        }
+
+        // Ensure lists without leads have a value
+        foreach ($tagIds as $l) {
+            if (!isset($return[$l])) {
+                $return[$l] = 0;
+            }
+        }
+        return ($returnArray) ? $return : $return[tagIds[0]];
+    }
+}
