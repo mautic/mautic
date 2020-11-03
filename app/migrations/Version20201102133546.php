3651e207a8ce448d9dd71b655188a1b8c848fe0f
Tejas Navghane <ts.navghane@gmail.com>
2020-11-03 17:00:42 +0530

    MAUT-4485 : match the index names

--- app/migrations/Version20201102133546.php
+++ app/migrations/Version20201102133546.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace Mautic\Migrations;
 
-use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Schema\SchemaException;
 use Doctrine\Migrations\Exception\SkipMigration;
@@ -22,48 +21,18 @@ use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
  */
 final class Version20201102133546 extends AbstractMauticMigration
 {
-    private $indexName;
-    private $columnName = 'email_id';
-
-    public function preUp(Schema $schema): void
-    {
-        $schemaName = $schema->getName();
-        $tableName  = $this->getTableName();
-        $sql        = <<<SQL
-            SELECT INDEX_NAME FROM INFORMATION_SCHEMA.STATISTICS
-            WHERE INDEX_SCHEMA='$schemaName' AND TABLE_NAME='$tableName'
-            AND COLUMN_NAME='$this->columnName' AND INDEX_NAME<>'PRIMARY' LIMIT 1;
-SQL;
-
-        $stmt = $this->connection->prepare($sql);
-        $stmt->execute();
-        $this->indexName = $stmt->fetchColumn();
-        $stmt->closeCursor();
-
-        if (!$this->indexName) {
-            throw new SkipMigration('Schema includes this migration');
-        }
-    }
-
     /**
-     * @throws DBALException
      * @throws SchemaException
      */
     public function up(Schema $schema): void
     {
-        $schema->getTable($this->getTableName())->dropIndex($this->indexName);
-    }
+        $tableName = $this->prefix.'email_assets_xref';
+        $indexName = 'IDX_39CFAB07A832C1C9';
 
-    public function down(Schema $schema): void
-    {
-        $tableName = $this->getTableName();
-        $this->addSql(
-            'ALTER TABLE '.$tableName.' ADD INDEX IDX_'.strtoupper($this->columnName).' ('.$this->columnName.')'
-        );
-    }
+        if (!$schema->getTable($tableName)->hasIndex($indexName)) {
+            throw new SkipMigration('Schema includes this migration');
+        }
 
-    private function getTableName(): string
-    {
-        return $this->prefix.'email_assets_xref';
+        $this->addSql('ALTER TABLE '.$tableName.' DROP INDEX '.$indexName.';');
     }
 }
