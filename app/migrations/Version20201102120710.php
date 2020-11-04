5cef0c6031e137b784c2efaee48133a47aaaf386
Rohit Pandita <rohit3nov@gmail.com>
2020-11-04 13:38:38 +0530

    MAUT-4486: Performance Scaling : move skip migration condition to preUp

--- app/migrations/Version20201102120710.php
+++ app/migrations/Version20201102120710.php
@@ -20,11 +20,15 @@ final class Version20201102120710 extends AbstractMauticMigration
     private $table  = 'email_list_xref';
     private $index  = 'IDX_11DC9DF2A832C1C9';
 
-    public function up(Schema $schema): void
+    public function preUp(Schema $schema): void
     {
         if (!$schema->getTable($this->prefix.$this->table)->hasIndex($this->index)) {
             throw new SkipMigration('Schema includes this migration');
         }
+    }
+
+    public function up(Schema $schema): void
+    {
         $this->addSql('ALTER TABLE '.$this->prefix.$this->table.' DROP INDEX '.$this->index);
     }
 }
