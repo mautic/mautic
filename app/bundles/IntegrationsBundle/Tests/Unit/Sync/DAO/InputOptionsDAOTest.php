3eddd8ea75be6602dbbd8708858f9979eccb10ff
John Linhart <admin@escope.cz>
2020-11-05 15:57:41 +0100

    New option in InputOptionsDAOTest tested

--- app/bundles/IntegrationsBundle/Tests/Unit/Sync/DAO/InputOptionsDAOTest.php
+++ app/bundles/IntegrationsBundle/Tests/Unit/Sync/DAO/InputOptionsDAOTest.php
@@ -23,6 +23,7 @@ class InputOptionsDAOTest extends TestCase
                 'first-time-sync'       => true,
                 'disable-push'          => false,
                 'disable-pull'          => true,
+                'disable-activity-push' => true,
                 'mautic-object-id'      => ['contact:12', 'contact:13', 'company:45'],
                 'integration-object-id' => ['Lead:hfskjdhf', 'Lead:hfskjdhr'],
                 'start-datetime'        => '2019-09-12T12:01:20',
@@ -35,6 +36,7 @@ class InputOptionsDAOTest extends TestCase
         $this->assertTrue($inputOptionsDAO->isFirstTimeSync());
         $this->assertFalse($inputOptionsDAO->pullIsEnabled());
         $this->assertTrue($inputOptionsDAO->pushIsEnabled());
+        $this->assertFalse($inputOptionsDAO->activityPushIsEnabled());
         $this->assertSame(['12', '13'], $inputOptionsDAO->getMauticObjectIds()->getObjectIdsFor(Contact::NAME));
         $this->assertSame(['45'], $inputOptionsDAO->getMauticObjectIds()->getObjectIdsFor(MauticSyncDataExchange::OBJECT_COMPANY));
         $this->assertSame(['hfskjdhf', 'hfskjdhr'], $inputOptionsDAO->getIntegrationObjectIds()->getObjectIdsFor('Lead'));
@@ -56,6 +58,7 @@ class InputOptionsDAOTest extends TestCase
         $this->assertFalse($inputOptionsDAO->isFirstTimeSync());
         $this->assertTrue($inputOptionsDAO->pullIsEnabled());
         $this->assertTrue($inputOptionsDAO->pushIsEnabled());
+        $this->assertTrue($inputOptionsDAO->activityPushIsEnabled());
         $this->assertNull($inputOptionsDAO->getMauticObjectIds());
         $this->assertNull($inputOptionsDAO->getIntegrationObjectIds());
         $this->assertNull($inputOptionsDAO->getStartDateTime());
@@ -76,6 +79,7 @@ class InputOptionsDAOTest extends TestCase
                 'first-time-sync'       => true,
                 'disable-push'          => false,
                 'disable-pull'          => true,
+                'disable-activity-push' => false,
                 'mautic-object-id'      => $mauticObjectIds,
                 'integration-object-id' => $integrationObjectIds,
                 'start-datetime'        => $start,
@@ -88,6 +92,7 @@ class InputOptionsDAOTest extends TestCase
         $this->assertTrue($inputOptionsDAO->isFirstTimeSync());
         $this->assertFalse($inputOptionsDAO->pullIsEnabled());
         $this->assertTrue($inputOptionsDAO->pushIsEnabled());
+        $this->assertTrue($inputOptionsDAO->activityPushIsEnabled());
         $this->assertSame($mauticObjectIds, $inputOptionsDAO->getMauticObjectIds());
         $this->assertSame($integrationObjectIds, $inputOptionsDAO->getIntegrationObjectIds());
         $this->assertSame($start, $inputOptionsDAO->getStartDateTime());
