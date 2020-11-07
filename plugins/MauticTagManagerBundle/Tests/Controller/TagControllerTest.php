bedc07c4606541f05900e7f84b8a9d7fa55fe386
Nikola Nedic <nikolaunedic@gmail.com>
2020-11-07 18:24:43 +0100

    Add tag deletion test

--- plugins/MauticTagManagerBundle/Tests/Controller/TagControllerTest.php
+++ plugins/MauticTagManagerBundle/Tests/Controller/TagControllerTest.php
@@ -2,33 +2,27 @@
 
 namespace MauticPlugin\MauticTagManagerBundle\Tests\Controller;
 
-use Doctrine\DBAL\Connection;
-use Mautic\CampaignBundle\Tests\DataFixtures\Orm\CampaignData;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
-use Mautic\InstallBundle\InstallFixtures\ORM\LeadFieldData;
 use Mautic\InstallBundle\InstallFixtures\ORM\RoleData;
-use Mautic\LeadBundle\DataFixtures\ORM\LoadLeadData;
 use Mautic\LeadBundle\Entity\Tag;
 use Mautic\LeadBundle\Model\TagModel;
 use Mautic\UserBundle\DataFixtures\ORM\LoadRoleData;
 use Mautic\UserBundle\DataFixtures\ORM\LoadUserData;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class TagControllerTest extends MauticMysqlTestCase
 {
     /**
-     * @var Connection
+     * @var TagModel
      */
-    private $connection;
+    private $tagModel;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->connection = $this->container->get('doctrine.dbal.default_connection');
-
-        defined('MAUTIC_TABLE_PREFIX') or define('MAUTIC_TABLE_PREFIX', '');
+        $this->loadFixtures(
+            [RoleData::class, LoadRoleData::class, LoadUserData::class]
+        );
 
         $tags = [
             'tag1',
@@ -36,13 +30,13 @@ class TagControllerTest extends MauticMysqlTestCase
             'tag3',
             'tag4',
         ];
-        /** @var TagModel $model */
-        $model = $this->container->get('mautic.lead.model.tag');
+
+        $this->tagModel = $this->container->get('mautic.lead.model.tag');
 
         foreach ($tags as $tagName) {
             $tag = new Tag();
             $tag->setTag($tagName);
-            $model->saveEntity($tag);
+            $this->tagModel->saveEntity($tag);
         }
     }
 
@@ -72,5 +66,14 @@ class TagControllerTest extends MauticMysqlTestCase
         $this->assertSame(200, $clientResponse->getStatusCode(), 'Return code must be 200.');
         $this->assertStringContainsString('tag1', $clientResponseContent, 'The return must contain tag1');
         $this->assertStringNotContainsString('tag2', $clientResponseContent, 'The return must not contain tag2');
+    }
+
+    public function testTagDeletion(): void
+    {
+        $this->client->request('POST', '/s/tags/delete/1');
+        $clientResponse         = $this->client->getResponse();
+
+        $this->assertSame(200, $clientResponse->getStatusCode(), 'Return code must be 200.');
+        $this->assertSame($this->tagModel->getRepository()->find(1), null, 'Assert that tag is deleted');
     }
 }
