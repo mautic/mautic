6b53f7dc746a52e963e8a33fd8b985e56fbd5aaf
Anton Vlasenko <vlasenko.anton@gmail.com>
2020-11-12 18:01:47 +0100

    Implement dictionary for password evaluating.

--- app/bundles/UserBundle/EventListener/PasswordSubscriber.php
+++ app/bundles/UserBundle/EventListener/PasswordSubscriber.php
@@ -13,20 +13,86 @@ declare(strict_types=1);
 
 namespace Mautic\UserBundle\EventListener;
 
+use Mautic\LeadBundle\Entity\CompanyRepository;
+use Mautic\UserBundle\Entity\User;
+use Mautic\UserBundle\Entity\UserRepository;
 use Mautic\UserBundle\Event\AuthenticationEvent;
 use Mautic\UserBundle\UserEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use ZxcvbnPhp\Zxcvbn as PasswordStrengthEstimator;
 
 class PasswordSubscriber implements EventSubscriberInterface
 {
+    private const MINIMUIM_ALLOWED_PASSWORD = 3;
+
+    /**
+     * @var PasswordStrengthEstimator
+     */
+    private $passwordStrengthEstimator;
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @var CompanyRepository
+     */
+    private $companyRepository;
+
+    public function __construct(PasswordStrengthEstimator $passwordStrengthEstimator, UserRepository $userRepository, CompanyRepository $companyRepository)
+    {
+        $this->passwordStrengthEstimator = $passwordStrengthEstimator;
+        $this->userRepository            = $userRepository;
+        $this->companyRepository         = $companyRepository;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
-            UserEvents::USER_FORM_AUTHENTICATION     => ['onUserFormAuthentication', 0],
+            UserEvents::USER_FORM_AUTHENTICATION => ['onUserFormAuthentication', 0],
         ];
     }
 
     public function onUserFormAuthentication(AuthenticationEvent $authenticationEvent): void
     {
+        $credentials = $authenticationEvent->getToken()->getCredentials();
+        if (!is_string($credentials)) {
+            return;
+        }
+
+        $user       = $this->initializeUser($authenticationEvent);
+        $dictionary = $user ? $this->buildDictionary($user) : [];
+
+        $score = $this->passwordStrengthEstimator->passwordStrength($authenticationEvent->getToken(), $dictionary);
+        $a     = 5;
+    }
+
+    private function buildDictionary(User $user): array
+    {
+        $dictionary = [
+            $user->getEmail(),
+            $user->getUsername(),
+            $user->getFirstName(),
+            $user->getLastName(),
+            $user->getPosition(),
+            $user->getSignature(),
+        ];
+
+        return array_unique(array_filter($dictionary));
+    }
+
+    private function initializeUser(AuthenticationEvent $authenticationEvent): ?User
+    {
+        $token = $authenticationEvent->getToken();
+        if ($token->getUser() instanceof User) {
+            return $token->getUser();
+        }
+
+        if ($token->getUsername()) {
+            return $this->userRepository->findOneByUsername($token->getUsername()) ?? null;
+        }
+
+        return null;
     }
 }
