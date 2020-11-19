b40dc59b6e6e9b857d5a5ccddd396d3d20f6b68b
Lukas Günther <lukas.guenther@pm.me>
2020-11-19 07:20:30 +0100

    added return type
    Co-authored-by: John Linhart <jan@linhart.email>

--- plugins/MauticFocusBundle/Tests/EventListener/ConfigSubscriberTest.php
+++ plugins/MauticFocusBundle/Tests/EventListener/ConfigSubscriberTest.php
@@ -29,7 +29,7 @@ class ConfigSubscriberTest extends TestCase
      */
     private $subscriber;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->configEvent          = $this->createMock(ConfigEvent::class);
         $this->subscriber           = new ConfigSubscriber();
