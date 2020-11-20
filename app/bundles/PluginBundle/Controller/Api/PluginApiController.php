ed51a9173399d859f1d9b3c9e90ec028576fade6
John Linhart <admin@escope.cz>
2020-11-20 11:13:11 +0100

    Using types now that we support PHP 7

--- app/bundles/PluginBundle/Controller/Api/PluginApiController.php
+++ app/bundles/PluginBundle/Controller/Api/PluginApiController.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * @copyright   2019 Mautic Contributors. All rights reserved
  * @author      Mautic
  *
- * @link        http://mautic.org
+ * @link        https://mautic.org
  *
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -14,15 +16,9 @@ namespace Mautic\PluginBundle\Controller\Api;
 use Mautic\ApiBundle\Controller\CommonApiController;
 use Symfony\Component\HttpFoundation\Response;
 
-/**
- * Class PluginApiController.
- */
 class PluginApiController extends CommonApiController
 {
-    /**
-     * @return Response
-     */
-    public function reloadAction()
+    public function reloadAction(): Response
     {
         if (!$this->get('mautic.security')->isGranted('plugin:plugins:manage')) {
             return $this->accessDenied();
@@ -33,12 +29,7 @@ class PluginApiController extends CommonApiController
         return $this->handleView($view);
     }
 
-    /**
-     * @param $integrationName
-     *
-     * @return Response
-     */
-    public function viewSettingsAction($integrationName)
+    public function viewSettingsAction(string $integrationName): Response
     {
         if (!$this->get('mautic.security')->isGranted('plugin:plugins:manage')) {
             return $this->accessDenied();
@@ -55,12 +46,7 @@ class PluginApiController extends CommonApiController
         return $this->notFound();
     }
 
-    /**
-     * @param $package
-     *
-     * @return Response
-     */
-    public function installAction()
+    public function installAction(): Response
     {
         if (!$this->get('mautic.security')->isGranted('plugin:plugins:manage')) {
             return $this->accessDenied();
