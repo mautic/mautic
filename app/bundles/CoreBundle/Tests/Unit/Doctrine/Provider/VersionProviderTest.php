04b485a36e906ccfe4057307d7e242f73699e65c
Dennis Ameling <dennis@dennisameling.com>
2020-11-24 12:40:15 +0100

    Fix test for MySQL 5.7

--- app/bundles/CoreBundle/Tests/Unit/Doctrine/Provider/VersionProviderTest.php
+++ app/bundles/CoreBundle/Tests/Unit/Doctrine/Provider/VersionProviderTest.php
@@ -74,7 +74,7 @@ class VersionProviderTest extends \PHPUnit\Framework\TestCase
     {
         $this->coreParametersHelper->expects($this->once())
             ->method('getParameter')
-            ->willReturn('5.5');
+            ->willReturn('5.7');
 
         $this->connection->expects($this->once())
             ->method('executeQuery')
