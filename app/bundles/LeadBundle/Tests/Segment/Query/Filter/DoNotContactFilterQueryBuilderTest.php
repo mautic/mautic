120d51a6ca982293d975a0dac4f6928f9df40887
fedys <miroslav.fedeles@gmail.com>
2020-12-01 19:49:55 +0100

    DoNotContactFilterQueryBuilder test

---
+++ app/bundles/LeadBundle/Tests/Segment/Query/Filter/DoNotContactFilterQueryBuilderTest.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * @copyright   2020 Mautic. All rights reserved
+ * @author      Mautic
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\LeadBundle\Tests\Segment\Query\Filter;
+
+use Doctrine\DBAL\Connection;
+use Mautic\LeadBundle\Segment\ContactSegmentFilter;
+use Mautic\LeadBundle\Segment\DoNotContact\DoNotContactParts;
+use Mautic\LeadBundle\Segment\Query\Filter\DoNotContactFilterQueryBuilder;
+use Mautic\LeadBundle\Segment\Query\QueryBuilder;
+use Mautic\LeadBundle\Segment\RandomParameterName;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+class DoNotContactFilterQueryBuilderTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        defined('MAUTIC_TABLE_PREFIX') or define('MAUTIC_TABLE_PREFIX', '');
+    }
+
+    public function testGetServiceId(): void
+    {
+        Assert::assertSame('mautic.lead.query.builder.special.dnc', DoNotContactFilterQueryBuilder::getServiceId());
+    }
+
+    /**
+     * @dataProvider dataApplyQuery
+     */
+    public function testApplyQuery(string $operator, string $parameterValue, string $expectedQuery): void
+    {
+        $queryBuilder = new QueryBuilder($this->createConnection());
+        $queryBuilder->select('1');
+
+        $filter             = $this->createFilter($operator, $parameterValue);
+        $filterQueryBuilder = new DoNotContactFilterQueryBuilder(new RandomParameterName(), new EventDispatcher());
+
+        Assert::assertSame($queryBuilder, $filterQueryBuilder->applyQuery($queryBuilder, $filter));
+        Assert::assertSame($expectedQuery, $queryBuilder->getDebugOutput());
+    }
+
+    public function dataApplyQuery(): iterable
+    {
+        yield ['eq', '1', 'SELECT 1 WHERE l.id IN (SELECT par0.lead_id FROM lead_donotcontact par0 WHERE (par0.reason = 1) AND (par0.channel = \'email\'))'];
+        yield ['eq', '0', 'SELECT 1 WHERE l.id NOT IN (SELECT par0.lead_id FROM lead_donotcontact par0 WHERE (par0.reason = 1) AND (par0.channel = \'email\'))'];
+        yield ['neq', '1', 'SELECT 1 WHERE l.id NOT IN (SELECT par0.lead_id FROM lead_donotcontact par0 WHERE (par0.reason = 1) AND (par0.channel = \'email\'))'];
+        yield ['neq', '0', 'SELECT 1 WHERE l.id IN (SELECT par0.lead_id FROM lead_donotcontact par0 WHERE (par0.reason = 1) AND (par0.channel = \'email\'))'];
+    }
+
+    private function createConnection(): Connection
+    {
+        return new class() extends Connection {
+            /** @noinspection PhpMissingParentConstructorInspection */
+            public function __construct()
+            {
+            }
+        };
+    }
+
+    private function createFilter(string $operator, string $parameterValue): ContactSegmentFilter
+    {
+        return new class($operator, $parameterValue) extends ContactSegmentFilter {
+            /**
+             * @var string
+             */
+            private $operator;
+
+            /**
+             * @var string
+             */
+            private $parameterValue;
+
+            /** @noinspection PhpMissingParentConstructorInspection */
+            public function __construct(string $operator, string $parameterValue)
+            {
+                $this->operator       = $operator;
+                $this->parameterValue = $parameterValue;
+            }
+
+            public function getDoNotContactParts()
+            {
+                return new DoNotContactParts('some');
+            }
+
+            public function getOperator()
+            {
+                return $this->operator;
+            }
+
+            public function getParameterValue()
+            {
+                return $this->parameterValue;
+            }
+
+            public function getGlue()
+            {
+                return 'and';
+            }
+        };
+    }
+}
