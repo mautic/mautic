f55d6489ce8ec8ff74b4408c30ebe989082d6198
Tejas Navghane <ts.navghane@gmail.com>
2020-12-15 11:32:11 +0530

    add test case

--- app/bundles/LeadBundle/Tests/Controller/ListControllerTest.php
+++ app/bundles/LeadBundle/Tests/Controller/ListControllerTest.php
@@ -1,10 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Mautic\LeadBundle\Tests\Controller;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use Mautic\CoreBundle\Tests\Traits\ControllerTrait;
+use Mautic\LeadBundle\DataFixtures\ORM\LoadLeadData;
+use Mautic\LeadBundle\DataFixtures\ORM\LoadLeadListData;
 use Mautic\LeadBundle\Entity\LeadList;
+use Mautic\LeadBundle\Tests\DataFixtures\ORM\LoadSegmentsData;
+use Mautic\UserBundle\DataFixtures\ORM\LoadRoleData;
+use Mautic\UserBundle\DataFixtures\ORM\LoadUserData;
+use Symfony\Component\HttpFoundation\Request;
 
 class ListControllerTest extends MauticMysqlTestCase
 {
@@ -76,5 +84,40 @@ class ListControllerTest extends MauticMysqlTestCase
         $list->setCreatedByUser('Test User');
 
         return $list;
+    }
+
+    public function testLeadListCountFromCache(): void
+    {
+        $this->loadFixtures(
+            [
+                LoadLeadListData::class,
+                LoadLeadData::class,
+                LoadSegmentsData::class,
+                LoadRoleData::class,
+                LoadUserData::class,
+            ],
+            false
+        )->getReferenceRepository();
+
+        $id = 3;
+
+        // load the page for first time - cache not set
+        $crawler = $this->client->request(Request::METHOD_GET, '/s/segments');
+        $content = $crawler->filter('a.col-count')->filter('a[data-id="'.$id.'"]')->html();
+        self::assertSame('No Contacts', trim($content));
+
+        // call ajax - cache set
+        $parameter = ['id' => $id];
+        $this->client->request(Request::METHOD_POST, '/s/ajax?action=lead:getLeadCount', $parameter);
+        $clientResponse = $this->client->getResponse();
+        $response       = json_decode($clientResponse->getContent(), true);
+        self::assertSame(1, $response['success']);
+        self::assertSame('View 4 Contacts', $response['html']);
+        self::assertSame('4', $response['leadCount']);
+
+        // load the page again - get cached value
+        $crawler = $this->client->request(Request::METHOD_GET, '/s/segments');
+        $content = $crawler->filter('a.col-count')->filter('a[data-id="'.$id.'"]')->html();
+        self::assertSame('View 4 Contacts', trim($content));
     }
 }
