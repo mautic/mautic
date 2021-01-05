09244b342ca7fef07e9c3ef06e5329fb98337338
Nick Veenhof <nick.veenhof@gmail.com>
2021-01-05 11:13:55 +0100

    The API defines Contacts not Contact causing the API to not receive the correct mapping

--- plugins/MauticCrmBundle/Integration/DynamicsIntegration.php
+++ plugins/MauticCrmBundle/Integration/DynamicsIntegration.php
@@ -413,7 +413,7 @@ class DynamicsIntegration extends CrmAbstractIntegration
      */
     public function getLeads($params = [], $query = null, &$executed = null, $result = [], $object = 'contacts')
     {
-        if ('Contact' === $object) {
+        if ('Contacts' === $object) {
             $object = 'contacts';
         }
         $executed    = 0;
