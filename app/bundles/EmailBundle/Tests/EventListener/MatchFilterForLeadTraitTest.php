2b3d4bbfe3685719386ca3144f9779f0348697e6
Lukas Drahy <lukas@drahy.net>
2021-01-13 13:47:51 +0100

    Remove unused test cases

--- app/bundles/EmailBundle/Tests/EventListener/MatchFilterForLeadTraitTest.php
+++ app/bundles/EmailBundle/Tests/EventListener/MatchFilterForLeadTraitTest.php
@@ -45,69 +45,6 @@ class MatchFilterForLeadTraitTest extends TestCase
 
         self::assertFalse($this->matchFilterForLeadTrait->match($this->filter, $this->lead));
     }
-
-    public function testDWCContactEndWidth(): void
-    {
-        $this->filter[0]['operator'] = 'endsWith';
-        $this->filter[0]['filter']   = 'text';
-
-        self::assertTrue($this->matchFilterForLeadTrait->match($this->filter, $this->lead));
-
-        $this->lead['custom'] = 'another words';
-
-        self::assertFalse($this->matchFilterForLeadTrait->match($this->filter, $this->lead));
-    }
-
-    public function testDWCContactContains(): void
-    {
-        $this->filter[0]['operator'] = 'contains';
-        $this->filter[0]['filter']   = 'custom';
-
-        self::assertTrue($this->matchFilterForLeadTrait->match($this->filter, $this->lead));
-
-        $this->lead['custom'] = 'another words';
-
-        self::assertFalse($this->matchFilterForLeadTrait->match($this->filter, $this->lead));
-    }
-
-    /**
-     * @dataProvider dateMatchTestProvider
-     */
-    public function testMatchFilterForLeadTraitForDate(?string $value, string $operator, bool $expect)
-    {
-        $filters = [
-            [
-                'glue'     => 'and',
-                'field'    => 'date',
-                'object'   => 'lead',
-                'type'     => 'date',
-                'filter'   => '2021-05-01',
-                'display'  => null,
-                'operator' => $operator,
-            ],
-        ];
-
-        $lead = [
-            'id'   => 1,
-            'date' => $value,
-        ];
-
-        $this->assertEquals($expect, $this->matchFilterForLeadTrait->match($filters, $lead));
-    }
-
-    public function dateMatchTestProvider(): iterable
-    {
-        $date = '2021-05-01';
-
-        yield [$date, '=', true];
-        yield [$date, '!=', false];
-        yield ['2020-02-02', '!=', true];
-        yield [$date, '!=', false];
-        yield [null, 'empty', true];
-        yield [$date, 'empty', false];
-        yield [$date, '!empty', true];
-        yield [null, '!empty', false];
-    }
 }
 
 class MatchFilterForLeadTraitTestable
