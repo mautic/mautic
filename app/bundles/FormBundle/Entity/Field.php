a7a4ea87ad594cb107390c4c096b5e47392b68e2
Zdeno Kuzmany <zdeno@kuzmany.biz>
2021-01-14 10:11:53 +0100

    Fix progressive proffilin If submission is empty

--- app/bundles/FormBundle/Entity/Field.php
+++ app/bundles/FormBundle/Entity/Field.php
@@ -871,7 +871,7 @@ class Field
         }
 
         // Hide the field if there is the submission count limit and hide it until the limit is overcame
-        if (!$this->alwaysDisplay && $this->showAfterXSubmissions > 0 && $submissions && $this->showAfterXSubmissions > count($submissions)) {
+        if (!$this->alwaysDisplay && $this->showAfterXSubmissions > 0 && null !== $submissions && $this->showAfterXSubmissions > count($submissions)) {
             return false;
         }
 
