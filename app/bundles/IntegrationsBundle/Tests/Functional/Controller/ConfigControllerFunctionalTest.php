70fcda0dab110acfff0b2cb335fc80d9f55d8c96
Dennis Ameling <dennis@dennisameling.com>
2021-01-16 11:50:36 +0100

    Fix return type on ConfigControllerFunctionalTest

--- app/bundles/IntegrationsBundle/Tests/Functional/Controller/ConfigControllerFunctionalTest.php
+++ app/bundles/IntegrationsBundle/Tests/Functional/Controller/ConfigControllerFunctionalTest.php
@@ -18,7 +18,7 @@ class ConfigControllerFunctionalTest extends MauticMysqlTestCase
      *
      * @throws \Exception
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
