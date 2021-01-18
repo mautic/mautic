95d048af1728d59156cf425bfe4d2f8ebf2fead3
Alfredo Arena <alfredo.arena@ctmobi.it>
2021-01-18 14:36:42 +0100

    Values of forms select can be saved

--- app/bundles/LeadBundle/Form/Type/FilterType.php
+++ app/bundles/LeadBundle/Form/Type/FilterType.php
@@ -108,6 +108,7 @@ class FilterType extends AbstractType
                 'stage',
                 'locales',
                 'globalcategory',
+                'forms',
             ]
         );
 
