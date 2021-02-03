902a0422c1bb6602fb5df5be8527c3e479c945b6
Anton Vlasenko <vlasenko.anton@gmail.com>
2021-02-03 22:12:09 +0100

    Fix namespace.

--- app/bundles/ApiBundle/Tests/Form/Type/ClientTypeTest.php
+++ app/bundles/ApiBundle/Tests/Form/Type/ClientTypeTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\ApiBundle\Tests\Helper;
+namespace Mautic\ApiBundle\Tests\Form\Type;
 
 use Mautic\ApiBundle\Entity\oAuth1\Consumer;
 use Mautic\ApiBundle\Form\Type\ClientType;
