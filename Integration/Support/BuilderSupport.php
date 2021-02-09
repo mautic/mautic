3a67adf41f011c623a4846ac25790b5d0712d950
alanhartless <alan@devkardia.com>
2021-02-09 01:19:40 -0600

    Register plugin as a "builder"

---
+++ Integration/Support/BuilderSupport.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace MauticPlugin\GrapesJsBuilderBundle\Integration\Support;
+
+
+use Mautic\IntegrationsBundle\Integration\Interfaces\BuilderInterface;
+use MauticPlugin\GrapesJsBuilderBundle\Integration\GrapesJsBuilderIntegration;
+
+class BuilderSupport extends GrapesJsBuilderIntegration implements BuilderInterface
+{
+    private $featuresSupported = ['email', 'page'];
+
+    public function isSupported(string $featureName): bool
+    {
+        return in_array($featureName, $this->featuresSupported);
+    }
+}
