0346309769f0099ec7ae0233120644dc4890a5b7
fedys <miroslav.fedeles@gmail.com>
2021-02-11 10:40:58 +0100

    Fixed PreviewFunctionalTest

--- app/bundles/PageBundle/Tests/Controller/PreviewFunctionalTest.php
+++ app/bundles/PageBundle/Tests/Controller/PreviewFunctionalTest.php
@@ -23,26 +23,22 @@ class PreviewFunctionalTest extends MauticMysqlTestCase
 {
     public function testPreviewAdmin(): void
     {
-        $page = new Page();
-        $page->setIsPublished(true);
-        $page->setDateAdded(new DateTime());
-        $page->setTitle('Preview settings test - main page');
-        $page->setAlias('page-main');
-        $page->setTemplate('Blank');
-        $page->setCustomHtml('Test Html');
-        $page->setLanguage('en');
-
-        $this->em->persist($page);
+        $page = $this->createPage();
         $this->em->flush();
 
-        // Admin user is allowed to access preview
-        $this->client->followRedirects(true);
-        $this->client->setMaxRedirects(10);
         $this->client->request(Request::METHOD_GET, "/page/preview/{$page->getId()}");
+
+        // Anonymous visitor is not allowed to access preview
+        self::assertSame(Response::HTTP_FORBIDDEN, $this->client->getResponse()->getStatusCode());
+
+        $this->loginUser('admin');
+        $this->client->request(Request::METHOD_GET, "/page/preview/{$page->getId()}");
+
+        // Admin user is allowed to access preview
         self::assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testPreviewAnonymous(): void
+    private function createPage(): Page
     {
         $page = new Page();
         $page->setIsPublished(true);
@@ -52,15 +48,8 @@ class PreviewFunctionalTest extends MauticMysqlTestCase
         $page->setTemplate('Blank');
         $page->setCustomHtml('Test Html');
         $page->setLanguage('en');
-
         $this->em->persist($page);
-        $this->em->flush();
 
-        // Anonymous visitor is not allowed to access preview
-        $clientAnonymous = $this->createAnonymousClient();
-        $clientAnonymous->followRedirects(true);
-        $clientAnonymous->setMaxRedirects(10);
-        $clientAnonymous->request(Request::METHOD_GET, "/page/preview/{$page->getId()}");
-        self::assertSame(Response::HTTP_NOT_FOUND, $clientAnonymous->getResponse()->getStatusCode());
+        return $page;
     }
 }
