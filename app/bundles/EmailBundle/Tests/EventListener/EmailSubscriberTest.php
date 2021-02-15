e608d591208bd9e65f38eaf2f7ab6de67b26c196
Simon Balz <simon@balz.me>
2021-02-15 14:09:00 +0100

    Fix package in test

--- app/bundles/EmailBundle/Tests/EventListener/EmailSubscriberTest.php
+++ app/bundles/EmailBundle/Tests/EventListener/EmailSubscriberTest.php
@@ -9,7 +9,7 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\EmailBundle\Tests;
+namespace Mautic\EmailBundle\Tests\EventListener;
 
 use Doctrine\ORM\EntityManager;
 use Mautic\CoreBundle\Helper\IpLookupHelper;
