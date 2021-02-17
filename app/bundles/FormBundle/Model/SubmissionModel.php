eed8e2dd6af0dc16b8e2a47deee70ec502e9ab3a
Alfredo Arena <alfredo.arena@ctmobi.it>
2021-02-17 15:21:35 +0100

    Changed content-type header

--- app/bundles/FormBundle/Model/SubmissionModel.php
+++ app/bundles/FormBundle/Model/SubmissionModel.php
@@ -727,8 +727,7 @@ class SubmissionModel extends CommonFormModel
                     }
                 );
 
-                $response->headers->set('Content-Type', 'application/force-download');
-                $response->headers->set('Content-Type', 'application/octet-stream');
+                $response->headers->set('Content-Type', 'text/csv; charset=UTF-8');
                 $response->headers->set('Content-Disposition', 'attachment; filename="'.$name.'.csv"');
                 $response->headers->set('Expires', 0);
                 $response->headers->set('Cache-Control', 'must-revalidate');
@@ -792,8 +791,7 @@ class SubmissionModel extends CommonFormModel
                         $objWriter->save('php://output');
                     }
                 );
-                $response->headers->set('Content-Type', 'application/force-download');
-                $response->headers->set('Content-Type', 'application/octet-stream');
+                $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
                 $response->headers->set('Content-Disposition', 'attachment; filename="'.$name.'.xlsx"');
                 $response->headers->set('Expires', 0);
                 $response->headers->set('Cache-Control', 'must-revalidate');
