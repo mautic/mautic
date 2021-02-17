a1de96259c8d9f31acb35e8e1a05ab35fae6e453
Alfredo Arena <alfredo.arena@ctmobi.it>
2021-02-17 12:35:47 +0100

    Added new action test and index test with filters

--- app/bundles/CampaignBundle/Tests/Controller/CampaignControllerTest.php
+++ app/bundles/CampaignBundle/Tests/Controller/CampaignControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Mautic\FormBundle\Tests\Controller;
+namespace Mautic\CampaignBundle\Tests\Controller;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 
@@ -24,5 +24,16 @@ class CampaignControllerTest extends MauticMysqlTestCase
         $this->client->request('GET', '/s/campaigns?search=has%3Aresults&tmpl=list');
         $clientResponse = $this->client->getResponse();
         $this->assertSame(200, $clientResponse->getStatusCode(), 'Return code must be 200.');
+    }
+
+    /**
+     * Get campaign's create page.
+     */
+    public function testNewActionCampaign(): void
+    {
+        $this->client->request('GET', '/s/campaigns/new/');
+        $clientResponse         = $this->client->getResponse();
+        $clientResponseContent  = $clientResponse->getContent();
+        $this->assertEquals(Response::HTTP_OK, $clientResponse->getStatusCode());
     }
 }
