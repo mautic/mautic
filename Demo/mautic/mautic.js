e4bde3a52e76da46ba76065c97450dcceab3312a
Adrian Schimpf <adrian@idea2.ch>
2021-02-19 16:41:51 +0100

    improve standalone dev/demo

--- Demo/mautic/mautic.js
+++
@@ -1,6 +0,0 @@
-// Mautic.initGrapesJS('emailform');
-// Mautic.launchBuilder('page');
-Mautic.launchBuilder('emailform');
-// Mautic.initSelectTheme(mQuery('#emailform_template'));
-// Mautic.initEmailDynamicContent();
-// Mautic.setThemeHtml('blank-mjml');
