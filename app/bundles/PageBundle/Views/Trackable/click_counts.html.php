6d6577f875f2f089e1d60dbda89202432b7d79c2
Alfredo Arena <alfredo.arena@ctmobi.it>
2021-02-22 13:24:21 +0100

    Fixed click through rate

--- app/bundles/PageBundle/Views/Trackable/click_counts.html.php
+++ app/bundles/PageBundle/Views/Trackable/click_counts.html.php
@@ -13,16 +13,22 @@
             </thead>
             <tbody>
                 <?php
-                    $totalClicks       = 0;
-                    $totalUniqueClicks = 0;
+                    $clickCounts = array_reduce($trackables, function ($accumulator, $link) {
+                        $accumulator[0] += $link['hits'];
+                        $accumulator[1] += $link['unique_hits'];
+
+                        return $accumulator;
+                    }, [
+                        0,
+                        0,
+                    ]);
+                    [$totalClicks, $totalUniqueClicks] = $clickCounts;
                     foreach ($trackables as $link):
-                        $totalClicks += $link['hits'];
-                        $totalUniqueClicks += $link['unique_hits'];
                         ?>
                         <tr>
                             <td class="long-text"><a href="<?php echo $link['url']; ?>"><?php echo $link['url']; ?></a></td>
                             <td class="text-center"><?php echo $link['hits']; ?></td>
-                            <td class="text-center"><?php echo isset($entity) && 0 !== $entity->getReadCount(true) ? round($link['unique_hits'] / $entity->getReadCount(true) * 100, 2).'%' : '0%'; ?></td>
+                            <td class="text-center"><?php echo isset($entity) && 0 !== $entity->getReadCount(true) ? round($link['unique_hits'] / $totalUniqueClicks * 100, 2).'%' : '0%'; ?></td>
                             <td class="text-center">
                                 <span class="mt-xs label label-primary has-click-event clickable-stat">
                         <?php if (isset($channel) && isset($entity)): ?>
