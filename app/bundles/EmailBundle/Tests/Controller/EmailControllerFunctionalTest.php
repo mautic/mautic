347559fc1ab60a3680c34b88bb1e2d8148e47421
Alfredo Arena <alfredo.arena@ctmobi.it>
2021-02-26 15:35:47 +0100

    Added test for index action

--- app/bundles/EmailBundle/Tests/Controller/EmailControllerFunctionalTest.php
+++ app/bundles/EmailBundle/Tests/Controller/EmailControllerFunctionalTest.php
@@ -34,6 +34,26 @@ final class EmailControllerFunctionalTest extends MauticMysqlTestCase
     }
 
     /**
+     * Index should return status code 200.
+     */
+    public function testIndexActionWhenNotFiltered(): void
+    {
+        $this->client->request('GET', '/s/emails');
+        $clientResponse = $this->client->getResponse();
+        $this->assertSame(200, $clientResponse->getStatusCode(), 'Return code must be 200.');
+    }
+
+    /**
+     * Filtering should return status code 200.
+     */
+    public function testIndexActionWhenFiltering(): void
+    {
+        $this->client->request('GET', '/s/emails?search=has%3Aresults&tmpl=list');
+        $clientResponse = $this->client->getResponse();
+        $this->assertSame(200, $clientResponse->getStatusCode(), 'Return code must be 200.');
+    }
+
+    /**
      * Ensure there is no query for DNC reasons if there are no contacts who received the email
      * because it loads the whole DNC table if no contact IDs are provided. It can lead to
      * memory limit error if the DNC table is big.
