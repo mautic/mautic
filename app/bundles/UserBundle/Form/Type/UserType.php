266a87606a0b6298e91fcded215e5936f3d3155b
Alfredo Arena <alfredo.arena@ctmobi.it>
2021-02-26 10:51:52 +0100

    Changed texts and name variable

--- app/bundles/UserBundle/Form/Type/UserType.php
+++ app/bundles/UserBundle/Form/Type/UserType.php
@@ -165,12 +165,12 @@ class UserType extends AbstractType
         );
         if (!$existing) {
             $builder->add(
-                'automaticCredentials',
+                'automaticPassword',
                 YesNoButtonGroupType::class,
                 [
-                    'label' => 'mautic.user.user.form.automaticcredentials',
+                    'label' => 'mautic.user.user.form.automaticpassword',
                     'attr'  => [
-                        'tooltip'  => 'mautic.user.user.form.automaticcredentials.descr',
+                        'tooltip'  => 'mautic.user.user.form.automaticpassword.descr',
                         'onchange' => 'Mautic.togglePasswordInput()',
                     ],
                     'data' => false,
