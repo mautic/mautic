a7bcfc44fbd84c9bd848a0f9d56200da2290bc27
Ruth Cheesley <ruth.cheesley@acquia.com>
2021-03-05 13:17:38 +0000

    Update configuration spec

--- cypress/integration/MauticSpecs/mautic.configuration.spec.js
+++ cypress/integration/MauticSpecs/mautic.configuration.spec.js
@@ -2,27 +2,20 @@
 /// <reference types="Cypress" />
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const settings = require("../../Pages/Settings");
 const configuration = require("../../Pages/Configuration");
 
-context("Configuration settings", () => {
+context("Verify that user is able to update the configuration settings", () => {
   it("Update Email Settings", () => {
-    settings.settingsMenuButton.click({force: true});
-    configuration.clickOnConfigurationSetings.click({force: true});
-    cy.wait(60000);
+    cy.visit('s/config/edit');
     configuration.waitforPageLoad();
-    configuration.clickOnEmailSettings.click({force: true});
-    configuration.waitforEmailSettingPageLoad();
-    configuration.selectFrequencyForEmail.clear().type('5');
-    configuration.clickFrequencyEach.click();
-    configuration.selectFrequencyForWeek.click();
-    configuration.applyEmailSetting.click();
-    configuration.waitTillSettingsGetsApplied();
-    configuration.clickOnEmailSettings.click({force: true});
-    configuration.saveAndCloseEmailSetting.click({force: true});
-    configuration.waitforSettingApplied();
-    configuration.waitTillUserRedirectedToDashboard();
-    configuration.closeAlert.click();
+    configuration.clickOnEmailSettings.click({force: true})
+    configuration.waitforEmailSettingPageLoad()
+    configuration.selectFrquencyForEmail.clear().type('5')
+    configuration.clickFrequencyEach.click()
+    configuration.selectFrequencyForWeek.click()
+    configuration.saveAndCloseEmailSetting.click({force: true})
+    configuration.waitTillUserRedirectedToDashboard()
+    configuration.closeAlert.click()
   })
 
   });
