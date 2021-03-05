beebf2234643ea6c08171274e6af6483e23ec30b
Ruth Cheesley <ruth.cheesley@acquia.com>
2021-03-05 13:14:24 +0000

    Update stages spec

--- cypress/integration/MauticSpecs/mautic.stages.spec.js
+++ cypress/integration/MauticSpecs/mautic.stages.spec.js
@@ -2,46 +2,41 @@
 /// <reference types="Cypress" />
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const leftNavigation = require("../../Pages/LeftNavigation");
 const stages = require("../../Pages/Stages");
 const search = require("../../Pages/Search");
 
-context("Add new a stage", () => {
-   it("Add a Stage", () => {
-    leftNavigation.StagesSection.click();
-    stages.checkNoResultFoundMessage.should('contain','No Results Found');
+var testStage = "TestStage";
+
+context("Verify that user is able to create and delete stages", () => {
+  beforeEach("Visit HomePage", () => {
+    cy.visit("s/stages");
+  });
+
+  it("Add a Stage", () => {
+    stages.checkNoResultFoundMessage.should("contain", "No Results Found");
     stages.addNewButton.click();
-    stages.stageName.type("TestStage");
+    stages.stageName.type(testStage);
     stages.stageWeight.type("40");
     stages.saveAndCloseButton.click();
     stages.waitforStageCreation();
-  })
+  });
 
   it("Edit newly added Stage", () => {
-    leftNavigation.StagesSection.click();
-    search.searchBox.clear();
-    search.searchBox.type("TestStage");
-    cy.wait(2000);
-    stages.searchAndClickOnFirstElement.contains("Test").click();
+    cy.visit("/s/stages?search=" + testStage);
+    stages.searchAndClickOnFirstElement.contains(testStage).click();
     stages.waitforPageLoad;
     stages.stageWeight.clear();
     stages.stageWeight.type("50");
     stages.saveAndCloseButton.click();
     stages.waitforStageCreation();
-  })
+  });
 
   it("search and delete newly added stage", () => {
-    leftNavigation.StagesSection.click();
-    search.searchBox.clear();
-    search.searchBox.type("TestStage");
-    cy.wait(2000);
+    cy.visit("/s/stages?search=" + testStage);
     stages.searchAndClickOnFirstCheckbox.click();
     search.selectAndClickFirstItemsOption.click();
     search.deleteButtonForFirstItem.click();
     search.confirmDeleteButton.click();
-    stages.checkNoResultFoundMessage.should('contain','No Results Found');
-  })
-
+    stages.checkNoResultFoundMessage.should("contain", "No Results Found");
   });
-
-
+});
