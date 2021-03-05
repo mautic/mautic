a13a28a4da2ae6cf2cf28b2f9f3d70bcd7e00679
Ruth Cheesley <ruth.cheesley@acquia.com>
2021-03-05 13:09:01 +0000

    Update roles spec

--- cypress/integration/MauticSpecs/mautic.roles.spec.js
+++ cypress/integration/MauticSpecs/mautic.roles.spec.js
@@ -2,42 +2,42 @@
 /// <reference types="Cypress" />
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const settings = require("../../Pages/Settings");
 const roles = require("../../Pages/Roles");
 const search=require("../../Pages/Search");
 
-context("Roles", () => {
+var readRole = "Standard"
+
+context("Verify that user is able to create and delete roles", () => {
+  beforeEach("Visit HomePage", () => {
+    cy.visit("s/roles");
+  });
+
   it("Add new role for reading contact", () => {
-    cy.wait(3000);
-    settings.settingsMenuButton.click();
-    settings.rolesSection.click();
     roles.waitForPageLoad();
     roles.addNewRoleButton.click();
     roles.roleNameTextBox.click();
-    roles.roleNameTextBox.type("testContactsReadRule");
+    roles.roleNameTextBox.type(readRole);
     roles.permissionTab.click();
+    roles.apiPermissionTab.click();
+    roles.apiAccessGrantedCheckBox.click();
     cy.wait(1000);
     roles.contactPermissionTab.click();
-    roles.contacts_Access_ViewOwn.click();
-    roles.contacts_Access_ViewOthers.click();
+    roles.contacts_Access_Full.click();
+    roles.contacts_Segments_ViewOthers.click()
+    roles.contacts_Segments_EditOthers.click()
+    roles.contacts_import_View.click()
+    roles.contacts_import_Edit.click()
     roles.saveAndCloseTab.click();
     roles.waitforRoleCreation();
   });
 
-  it("Search and Delete newly added role", () => {
-    settings.settingsMenuButton.click();
-    settings.rolesSection.click({force: true});
-    cy.wait(1000);
+  it("Search and delete newly added role", () => {
     roles.waitForPageLoad();
-    search.searchBox.clear();
-    search.searchBox.type("testContactsReadRule");
-    cy.wait(2000);
+    cy.visit('/s/roles?search='+ readRole);
     search.selectCheckBoxForFirstItem.click();
     search.OptionsDropdownForFirstItem.click();
     search.deleteButtonForFirstItem.click();
     search.confirmDeleteButton.click();
   });
 
-  });
-
-
+});
