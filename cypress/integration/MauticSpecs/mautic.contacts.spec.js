2c8aaa41d9582401254afb764e8d5eb7a1fd9d8e
Ruth Cheesley <ruth.cheesley@acquia.com>
2021-03-05 12:57:42 +0000

    Updates to Contacts Spec

--- cypress/integration/MauticSpecs/mautic.contacts.spec.js
+++ cypress/integration/MauticSpecs/mautic.contacts.spec.js
@@ -2,50 +2,18 @@
 /// <reference types="Cypress" />
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const leftNavigation = require("../../Pages/LeftNavigation");
 const contact = require("../../Pages/Contacts");
-const company = require("../../Pages/Company");
 const search = require("../../Pages/Search");
 
-var companyName = "CompanyAddedByCypress";
 var contactName = "ContactAddedCypress";
 
-context("Contacts", () => {
-  it("Add new Company", () => {
-    leftNavigation.companySection.click();
-    company.waitforPageLoad()
-    company.addNewButton.click({ force: true });
-    company.companyName.type(companyName);
-    company.saveButton.click();
-    company.alertMessage.should('contain', 'CompanyAddedByCypress has been created!');
-  });
+context("Verify that user is able to create and delete the contacts", () => {
 
-  it("Edit newly added Company", () => {
-    leftNavigation.companySection.click();
-    company.waitforPageLoad();
-    cy.wait(2000);
-    cy.visit('/s/companies?search=' + companyName);
-    company.clickCompanyEdit.click();
-    company.editCompany.click();
-    company.companyCity.type("Pune");
-    company.companyZipCode.type("412308");
-    company.saveButton.click();
-    company.waitforCompanyCreation();
-  });
-
-  it("Search and Delete Company", () => {
-    leftNavigation.companySection.click();
-    company.waitforPageLoad();
-    cy.visit('/s/companies?search=' + companyName);
-    company.waitTillSearchResultGetsDisplayed();
-    search.selectCheckBoxForFirstItem.click({ force: true });
-    search.OptionsDropdownForFirstItem.click();
-    search.deleteButtonForFirstItem.click();
-    search.confirmDeleteButton.click();
+  beforeEach("Visit HomePage", () => {
+    cy.visit("s/contacts");
   });
 
   it("Add new Contact", () => {
-    leftNavigation.contactsSection.click();
     contact.waitforPageLoad();
     contact.addNewButton.click({ force: true });
     contact.title.type("Mr");
@@ -57,50 +25,7 @@ context("Contacts", () => {
     contact.waitForContactCreation();
   });
 
-  it("Add new Contact for Bidar City", () => {
-    leftNavigation.contactsSection.click();
-    contact.waitforPageLoad();
-    contact.addNewButton.click({ force: true });
-    contact.title.type("Mr");
-    contact.firstName.type("User1");
-    contact.lastName.type("Tester");
-    contact.leadEmail.type("Cypress1@mailtest.mautic.com");
-    contact.leadCity.type('Bidar')
-    contact.SaveButton.click();
-    contact.closeButton.click({ force: true });
-    contact.waitForContactCreation();
-  });
-
-  it("Add new Contact for Bidar City", () => {
-    leftNavigation.contactsSection.click();
-    contact.waitforPageLoad();
-    contact.addNewButton.click({ force: true });
-    contact.title.type("Mr");
-    contact.firstName.type("User2");
-    contact.lastName.type("Tester");
-    contact.leadEmail.type("Cypress2@mailtest.mautic.com");
-    contact.leadCity.type('Bidar')
-    contact.SaveButton.click();
-    contact.closeButton.click({ force: true });
-    contact.waitForContactCreation();
-  });
-
-  it("Add new Contact for Hydrabad City", () => {
-    leftNavigation.contactsSection.click();
-    contact.waitforPageLoad();
-    contact.addNewButton.click({ force: true });
-    contact.title.type("Mr");
-    contact.firstName.type("User3");
-    contact.lastName.type("Tester");
-    contact.leadEmail.type("Cypress3@mailtest.mautic.com");
-    contact.leadCity.type('Hydrabad')
-    contact.SaveButton.click();
-    contact.closeButton.click({ force: true });
-    contact.waitForContactCreation();
-  });
-
   it("Edit newly added contact", () => {
-    leftNavigation.contactsSection.click();
     contact.waitforPageLoad();
     cy.visit('/s/contacts?search=' + contactName);
     contact.searchAndClickForFirstElement.contains(contactName).click();
@@ -113,8 +38,7 @@ context("Contacts", () => {
     contact.waitForContactCreation();
   });
 
-  it("Search and Delete a Contact", () => {
-    leftNavigation.contactsSection.click();
+  it("Search and delete newly added contact", () => {
     contact.waitforPageLoad();
     cy.visit('/s/contacts?search=' + contactName);
     contact.waitTillSearchResultGetsDisplayed();
@@ -125,7 +49,6 @@ context("Contacts", () => {
   });
   
   it("import new Contacts", () => {
-    leftNavigation.contactsSection.click();
     contact.waitforPageLoad();
     contact.importExportDropdownMenu.click({ force: true });
     contact.importButton.click({ force: true });
