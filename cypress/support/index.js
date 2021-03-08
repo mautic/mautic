80f076bd209d7bf67a38797812f835f38fd837fc
Ruth Cheesley <ruth.cheesley@acquia.com>
2021-03-08 16:18:42 +0000

    Think we no longer need to create these before all tests as we are now creating them in the test themselves.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -38,77 +38,7 @@ Cypress.Cookies.defaults({
       cy.log('Login successful')
     }
   })
-
-  //adding sample contacts to be used across test
-  cy.visit('s/contacts');
-  contact.waitforPageLoad();
-  contact.addNewButton.click({ force: true });
-  contact.title.type("Mr");
-  contact.firstName.type(testContact);
-  contact.lastName.type("Data");
-  contact.leadEmail.type(testContact +"@mailtest.mautic.com");
-  contact.SaveButton.click();
-  contact.closeButton.click({ force: true });
-  contact.waitForContactCreation();
-
-  //adding sample email to be used across test
-  cy.visit('s/emails')
-  emails.waitforPageLoad();
-  emails.addNewButton.click({ force: true });
-  emails.waitforEmailSelectorPageGetsLoaded();
-  emails.templateEmailSelector.click();
-  emails.emailSubject.type("Test Email");
-  emails.emailInternalName.type("Test Email");
-  emails.saveEmailButton.click();
-  emails.closeButton.click();
-  emails.waitforEmailCreation();
-
-  //adding sample segment to be used across test
-  cy.visit('s/segments')
-  segments.waitForPageLoad();
-  segments.addNewButton.click({ force: true });
-  segments.segmentName.type("TestSegment");
-  segments.filterTab.click();
-  cy.wait(2000); //Community specific
-  segments.filterDropDown.click();
-  segments.filterSearchBox.type("First");
-  segments.filterField.click();
-  segments.filterValue.type("testContact");
-  segments.saveAndCloseButton.click();
-  cy.wait(1000); //Community specific
-  segments.waitforSegmentCreation();
 });
-
-after("Delete Test Data", () => {
-  //deleting created contact
-  cy.visit('s/contacts');
-  contact.waitforPageLoad();
-  cy.visit('/s/contacts?search='+ testContact);
-  search.selectCheckBoxForFirstItem.click({ force: true });
-  search.OptionsDropdownForFirstItem.click();
-  search.deleteButtonForFirstItem.click();
-  search.confirmDeleteButton.click();
-
-  //deleting created Email
-  cy.visit('s/emails');
-  emails.waitforPageLoad();
-  cy.visit('/s/emails?search=Test')
-  search.selectCheckBoxForFirstItem.click({ force: true });
-  search.OptionsDropdownForFirstItem.click();
-  search.deleteButtonForFirstItem.click();
-  search.confirmDeleteButton.click();
-
-  //deleting created segment
-  cy.visit('s/segments');
-  segments.waitForPageLoad();
-  cy.visit('/s/segments?search=TestSegment')
-  segments.firstCheckbox.click();
-  segments.firstDropDown.click();
-  segments.deleteOption.click();
-  segment.deleteConfirmation.click();
-});
-
-
 
 Cypress.on("uncaught:exception", (err, runnable) => {
   // returning false here prevents Cypress from
