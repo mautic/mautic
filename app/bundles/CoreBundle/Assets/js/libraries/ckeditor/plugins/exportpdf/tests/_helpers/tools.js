1262211dffec70eabe955e8bd289f3f78f3a1bab
saurabh.gupta <saurabh.gupta@acquia.com>
2021-03-15 14:39:06 +0530

    removed unused files

--- app/bundles/CoreBundle/Assets/js/libraries/ckeditor/plugins/exportpdf/tests/_helpers/tools.js
+++
@@ -1,5 +0,0 @@
-﻿/*
- Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
-*/
-(function(){window.exportPdfUtils={useXHR:function(b,a){var c=sinon.useFakeXMLHttpRequest(),d;c.onCreate=function(a){d=a};b.execCommand("exportPdf");a&&a(d);c.restore()},getDefaultConfig:function(b,a){return CKEDITOR.tools.object.merge({extraPlugins:"exportpdf",exportPdf_appId:"cke4-tests-"+b},a)},initManualTest:function(){bender.loadExternalPlugin("exportpdf","/apps/plugin/");bender.tools.ignoreUnsupportedEnvironment("exportpdf")},toAbsoluteUrl:function(b,a){return(a?a:window.location.origin)+b}}})();
