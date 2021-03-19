19b7841f89760b7650cf4214f523109a0f969bc0
Zdeno Kuzmany <zdeno@kuzmany.biz>
2021-03-19 19:21:48 +0100

    Add unit tests

---
+++ app/bundles/IntegrationsBundle/Tests/Unit/Sync/DAO/UpdatedObjectMappingDAOTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace IntegrationsBundle\Sync\DAO\Mapping;
+
+use Mautic\IntegrationsBundle\Sync\DAO\Mapping\UpdatedObjectMappingDAO;
+use PHPUnit\Framework\TestCase;
+
+class UpdatedObjectMappingDAOTest extends TestCase
+{
+    public function testUpdatedObjectMappingDAO()
+    {
+        $integration             = 'integration';
+        $integrationObjectName   = 'contact';
+        $integrationObjectId     = 1;
+        $objectModifiedDate      = new \DateTime('2020-02-02');
+        $internalObjectId        = 99;
+
+        $updatedObjectMappingDAO = new UpdatedObjectMappingDAO(
+            $integration,
+            $integrationObjectName,
+            $integrationObjectId,
+            $objectModifiedDate,
+            $internalObjectId
+        );
+        $this->assertEquals($integration, $updatedObjectMappingDAO->getIntegration());
+        $this->assertEquals($integrationObjectName, $updatedObjectMappingDAO->getIntegrationObjectName());
+        $this->assertEquals($integrationObjectId, $updatedObjectMappingDAO->getIntegrationObjectId());
+        $this->assertEquals($objectModifiedDate, $updatedObjectMappingDAO->getObjectModifiedDate());
+        $this->assertEquals($internalObjectId, $updatedObjectMappingDAO->getInternalObjectId());
+    }
+}
