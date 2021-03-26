3c91c10e2135b4d0d0b03db9bbd4218861d7b996
fedys <miroslav.fedeles@gmail.com>
2021-03-26 08:41:02 +0100

    Assert nullable properties in PHP 7.4

--- app/bundles/CoreBundle/Test/Listeners/CleanupListener.php
+++ app/bundles/CoreBundle/Test/Listeners/CleanupListener.php
@@ -13,10 +13,12 @@ declare(strict_types=1);
 
 namespace Mautic\CoreBundle\Test\Listeners;
 
+use LogicException;
 use PHPUnit\Framework\Test;
 use PHPUnit\Framework\TestListener;
 use PHPUnit\Framework\TestListenerDefaultImplementation;
 use ReflectionObject;
+use ReflectionProperty;
 
 /**
  * Prevents memory leaks by resetting all the test properties.
@@ -31,9 +33,24 @@ class CleanupListener implements TestListener
 
         foreach ($reflection->getProperties() as $property) {
             if (!$property->isStatic() && 0 !== strpos($property->getDeclaringClass()->getName(), 'PHPUnit\\')) {
+                $this->assertTypeIsNullable($property);
                 $property->setAccessible(true);
                 $property->setValue($test, null);
             }
+        }
+    }
+
+    private function assertTypeIsNullable(ReflectionProperty $property): void
+    {
+        if (PHP_VERSION_ID < 70400) {
+            return;
+        }
+
+        /** @noinspection PhpElementIsNotAvailableInCurrentPhpVersionInspection */
+        $type = $property->getType();
+
+        if ($type && !$type->allowsNull()) {
+            throw new LogicException(sprintf('Property "%s::$%s" must be nullable to prevent memory leaks', $property->getDeclaringClass()->getName(), $property->getName()));
         }
     }
 }
