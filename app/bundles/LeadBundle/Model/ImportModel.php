c57cda417cfebda39fb71b0b8b057cc0d4ba5741
Anton Vlasenko <vlasenko.anton@gmail.com>
2021-04-07 21:15:30 +0200

    Fix the comment.
    Signed-off-by: Anton Vlasenko <vlasenko.anton@gmail.com>

--- app/bundles/LeadBundle/Model/ImportModel.php
+++ app/bundles/LeadBundle/Model/ImportModel.php
@@ -370,7 +370,7 @@ class ImportModel extends FormModel
 
             if ($errorMessage) {
                 if (!$this->em->isOpen()) {
-                    // Something bad has happened if the entity manager is closed.
+                    // Something bad must have happened if the entity manager is closed.
                     // We will not be able to save any entities.
                     throw new ORMException($errorMessage);
                 }
