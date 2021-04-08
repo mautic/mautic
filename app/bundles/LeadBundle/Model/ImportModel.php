e0c6fef9aa841ea81dd3fb0511d30a01708c217c
Anton Vlasenko <vlasenko.anton@gmail.com>
2021-04-08 18:28:30 +0200

    Log the error.
    Signed-off-by: Anton Vlasenko <vlasenko.anton@gmail.com>

--- app/bundles/LeadBundle/Model/ImportModel.php
+++ app/bundles/LeadBundle/Model/ImportModel.php
@@ -380,6 +380,7 @@ class ImportModel extends FormModel
                 if (!$this->em->isOpen()) {
                     // Something bad must have happened if the entity manager is closed.
                     // We will not be able to save any entities.
+                    $this->logDebug('Line '.$lineNumber.' error: '.$errorMessage, $import);
                     throw new ORMException($errorMessage);
                 }
                 $import->increaseIgnoredCount();
