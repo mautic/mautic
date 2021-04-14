9a8695e922adedcaac7983c8a22fd5449fad510b
fedys <miroslav.fedeles@gmail.com>
2021-04-14 10:28:37 +0200

    Segment CompositeExpression marked as deprecated

--- app/bundles/LeadBundle/Segment/Query/Expression/CompositeExpression.php
+++ app/bundles/LeadBundle/Segment/Query/Expression/CompositeExpression.php
@@ -2,6 +2,9 @@
 
 namespace Mautic\LeadBundle\Segment\Query\Expression;
 
+/**
+ * @depreacated Use \Doctrine\DBAL\Query\Expression\CompositeExpression instead.
+ */
 class CompositeExpression extends \Doctrine\DBAL\Query\Expression\CompositeExpression
 {
 }
