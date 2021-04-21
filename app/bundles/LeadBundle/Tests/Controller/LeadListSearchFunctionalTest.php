3d60e6d24e36502c5129f1cf2b6de430d0845725
fedys <miroslav.fedeles@gmail.com>
2021-04-21 10:04:45 +0200

    Lead search functional test

---
+++ app/bundles/LeadBundle/Tests/Controller/LeadListSearchFunctionalTest.php
@@ -0,0 +1,162 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright   2021 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\LeadBundle\Tests\Controller;
+
+use DateTime;
+use Doctrine\Bundle\DoctrineBundle\DataCollector\DoctrineDataCollector;
+use Doctrine\Bundle\DoctrineBundle\Twig\DoctrineExtension;
+use Doctrine\ORM\ORMException;
+use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\LeadBundle\Entity\Lead;
+use Mautic\LeadBundle\Entity\LeadList;
+use Mautic\LeadBundle\Entity\ListLead;
+use PHPUnit\Framework\Assert;
+use Symfony\Component\HttpFoundation\Request;
+
+class LeadListSearchFunctionalTest extends MauticMysqlTestCase
+{
+    protected $clientOptions = ['debug' => true];
+
+    public function testSegmentSearch(): void
+    {
+        // create some leads
+        $leadOne   = $this->createLead('one');
+        $leadTwo   = $this->createLead('two');
+        $leadThree = $this->createLead('three');
+        $leadFour  = $this->createLead('four');
+        $leadFive  = $this->createLead('five');
+        $leadSix   = $this->createLead('six');
+
+        // add some leads in lists
+        $listOne  = $this->createLeadList('first-list', $leadOne, $leadTwo, $leadThree);
+        $listTwo  = $this->createLeadList('second-list', $leadOne, $leadFour, $leadFive, $leadSix);
+
+        $this->client->enableProfiler();
+        $previousQueries = [];
+
+        // non-existent segment search
+        $this->assertSearchResult('segment%3AnonExistent', [], [$leadOne, $leadTwo, $leadThree, $leadFour, $leadFive, $leadSix]);
+        $this->assertQueries([
+            'SELECT list.id FROM mautic_lead_lists list WHERE list.alias = \'nonexistent\'',
+            'SELECT COUNT(DISTINCT(l.id)) as count FROM mautic_leads l USE INDEX FOR JOIN (`PRIMARY`) INNER JOIN mautic_lead_lists_leads list_lead ON l.id = list_lead.lead_id WHERE ((list_lead.manually_removed = 0) AND (list_lead.leadlist_id IN (0))) AND (l.date_identified IS NOT NULL)',
+        ], $previousQueries);
+
+        // first-list segment search
+        $this->assertSearchResult('segment%3A'.$listOne->getAlias(), [$leadOne, $leadTwo, $leadThree], [$leadFour, $leadFive, $leadSix]);
+        $this->assertQueries([
+            "SELECT list.id FROM mautic_lead_lists list WHERE list.alias = '{$listOne->getAlias()}'",
+            "SELECT COUNT(DISTINCT(l.id)) as count FROM mautic_leads l USE INDEX FOR JOIN (`PRIMARY`) INNER JOIN mautic_lead_lists_leads list_lead ON l.id = list_lead.lead_id WHERE ((list_lead.manually_removed = 0) AND (list_lead.leadlist_id IN ('{$listOne->getId()}'))) AND (l.date_identified IS NOT NULL)",
+            "SELECT l.* FROM mautic_leads l USE INDEX FOR JOIN (`PRIMARY`) INNER JOIN mautic_lead_lists_leads list_lead ON l.id = list_lead.lead_id WHERE ((list_lead.manually_removed = 0) AND (list_lead.leadlist_id IN ('{$listOne->getId()}'))) AND (l.date_identified IS NOT NULL) GROUP BY l.id ORDER BY l.last_active DESC LIMIT 30",
+        ], $previousQueries);
+        $this->assertSearchResult('!segment%3A'.$listOne->getAlias(), [$leadOne, $leadFour, $leadFive, $leadSix], [$leadTwo, $leadThree]);
+        $this->assertQueries([
+            "SELECT list.id FROM mautic_lead_lists list WHERE list.alias = '{$listOne->getAlias()}'",
+            "SELECT COUNT(DISTINCT(l.id)) as count FROM mautic_leads l USE INDEX FOR JOIN (`PRIMARY`) LEFT JOIN mautic_lead_lists_leads list_lead ON l.id = list_lead.lead_id WHERE ((list_lead.manually_removed <> 0) OR ((list_lead.leadlist_id NOT IN ('{$listOne->getId()}')) OR (list_lead.leadlist_id IS NULL))) AND (l.date_identified IS NOT NULL)",
+            "SELECT l.* FROM mautic_leads l USE INDEX FOR JOIN (`PRIMARY`) LEFT JOIN mautic_lead_lists_leads list_lead ON l.id = list_lead.lead_id WHERE ((list_lead.manually_removed <> 0) OR ((list_lead.leadlist_id NOT IN ('{$listOne->getId()}')) OR (list_lead.leadlist_id IS NULL))) AND (l.date_identified IS NOT NULL) GROUP BY l.id ORDER BY l.last_active DESC LIMIT 30",
+        ], $previousQueries);
+
+        // second-list segment search
+        $this->assertSearchResult('segment%3A'.$listTwo->getAlias(), [$leadOne, $leadFour, $leadFive, $leadSix], [$leadTwo, $leadThree]);
+        $this->assertQueries([
+            "SELECT list.id FROM mautic_lead_lists list WHERE list.alias = '{$listTwo->getAlias()}'",
+            "SELECT COUNT(DISTINCT(l.id)) as count FROM mautic_leads l USE INDEX FOR JOIN (`PRIMARY`) INNER JOIN mautic_lead_lists_leads list_lead ON l.id = list_lead.lead_id WHERE ((list_lead.manually_removed = 0) AND (list_lead.leadlist_id IN ('{$listTwo->getId()}'))) AND (l.date_identified IS NOT NULL)",
+            "SELECT l.* FROM mautic_leads l USE INDEX FOR JOIN (`PRIMARY`) INNER JOIN mautic_lead_lists_leads list_lead ON l.id = list_lead.lead_id WHERE ((list_lead.manually_removed = 0) AND (list_lead.leadlist_id IN ('{$listTwo->getId()}'))) AND (l.date_identified IS NOT NULL) GROUP BY l.id ORDER BY l.last_active DESC LIMIT 30",
+        ], $previousQueries);
+        $this->assertSearchResult('!segment%3A'.$listTwo->getAlias(), [$leadOne, $leadTwo, $leadThree], [$leadFour, $leadFive, $leadSix]);
+        $this->assertQueries([
+            "SELECT list.id FROM mautic_lead_lists list WHERE list.alias = '{$listTwo->getAlias()}'",
+            "SELECT COUNT(DISTINCT(l.id)) as count FROM mautic_leads l USE INDEX FOR JOIN (`PRIMARY`) LEFT JOIN mautic_lead_lists_leads list_lead ON l.id = list_lead.lead_id WHERE ((list_lead.manually_removed <> 0) OR ((list_lead.leadlist_id NOT IN ('{$listTwo->getId()}')) OR (list_lead.leadlist_id IS NULL))) AND (l.date_identified IS NOT NULL)",
+            "SELECT l.* FROM mautic_leads l USE INDEX FOR JOIN (`PRIMARY`) LEFT JOIN mautic_lead_lists_leads list_lead ON l.id = list_lead.lead_id WHERE ((list_lead.manually_removed <> 0) OR ((list_lead.leadlist_id NOT IN ('{$listTwo->getId()}')) OR (list_lead.leadlist_id IS NULL))) AND (l.date_identified IS NOT NULL) GROUP BY l.id ORDER BY l.last_active DESC LIMIT 30",
+        ], $previousQueries);
+    }
+
+    /**
+     * @param Lead[] $expectedLeads
+     * @param Lead[] $notExpectedLeads
+     */
+    private function assertSearchResult(string $search, array $expectedLeads, array $notExpectedLeads): void
+    {
+        $responseText = $this->client->request(Request::METHOD_GET, '/s/contacts?search='.$search)->text();
+
+        foreach ($expectedLeads as $expectedLead) {
+            Assert::assertStringContainsString($expectedLead->getEmail(), $responseText, sprintf('Lead with the email "%s" should be in the result.', $expectedLead->getEmail()));
+        }
+
+        foreach ($notExpectedLeads as $notExpectedLead) {
+            Assert::assertStringNotContainsString($notExpectedLead->getEmail(), $responseText, sprintf('Lead with the email "%s" should not be in the result.', $notExpectedLead->getEmail()));
+        }
+    }
+
+    private function assertQueries(array $expectedQueries, array &$previousQueries): void
+    {
+        /** @var DoctrineDataCollector $dbCollector */
+        $dbCollector       = $this->client->getProfile()->getCollector('db');
+        $allQueries        = $dbCollector->getQueries()['default'];
+        $queries           = array_diff_key($allQueries, $previousQueries);
+        $previousQueries   = $allQueries;
+        $doctrineExtension = new DoctrineExtension();
+
+        $queries = array_map(function (array $query) use ($doctrineExtension) {
+            return $doctrineExtension->replaceQueryParameters($query['sql'], $query['params']);
+        }, $queries);
+
+        foreach ($expectedQueries as $expectedQuery) {
+            $matchedQueries = array_filter($queries, function (string $query) use ($expectedQuery) {
+                return $expectedQuery === $query;
+            });
+            Assert::assertCount(1, $matchedQueries, sprintf('The query "%s" was expected to be executed once.', $expectedQuery));
+        }
+    }
+
+    /**
+     * @throws ORMException
+     */
+    private function createLead(string $lastName): Lead
+    {
+        $lead = new Lead();
+        $lead->setLastname($lastName);
+        $lead->setEmail(sprintf('%s@mail.tld', $lastName));
+        $this->em->persist($lead);
+
+        return $lead;
+    }
+
+    /**
+     * @param Lead ...$leads
+     *
+     * @throws ORMException
+     */
+    private function createLeadList(string $name, ...$leads): LeadList
+    {
+        $leadList = new LeadList();
+        $leadList->setName($name);
+        $leadList->setAlias(mb_strtolower($name));
+        $this->em->persist($leadList);
+
+        foreach ($leads as $lead) {
+            $this->addLeadToList($lead, $leadList);
+        }
+
+        return $leadList;
+    }
+
+    private function addLeadToList(Lead $leadOne, LeadList $sourceList): void
+    {
+        $listLead = new ListLead();
+        $listLead->setLead($leadOne);
+        $listLead->setList($sourceList);
+        $listLead->setDateAdded(new DateTime());
+        $this->em->persist($listLead);
+    }
+}
