a76226e418bf01d6d08d60bc21b537a7ca0101c2
Adrian Schimpf <adrian@idea2.ch>
2021-04-24 12:53:33 +0200

    linter fixes

--- buttons.js
+++ buttons.js
@@ -1,5 +1,5 @@
 export default (editor, opts = {}) => {
-  const $ = editor.$;
+  const { $ } = editor;
   const pm = editor.Panels;
 
   // Add function within builder to edit source code
@@ -18,9 +18,9 @@ export default (editor, opts = {}) => {
 
   // Disable Import code button
   if (!opts.showImportButton) {
-    let mjmlImportBtn = pm.getButton('options', 'mjml-import');
-    let htmlImportBtn = pm.getButton('options', 'gjs-open-import-template');
-    let pageImportBtn = pm.getButton('options', 'gjs-open-import-webpage');
+    const mjmlImportBtn = pm.getButton('options', 'mjml-import');
+    const htmlImportBtn = pm.getButton('options', 'gjs-open-import-template');
+    const pageImportBtn = pm.getButton('options', 'gjs-open-import-webpage');
 
     // MJML import
     if (mjmlImportBtn !== null) {
@@ -39,8 +39,8 @@ export default (editor, opts = {}) => {
   }
 
   // Move Undo & Redo inside Commands Panel
-  let undo = pm.getButton('options', 'undo');
-  let redo = pm.getButton('options', 'redo');
+  const undo = pm.getButton('options', 'undo');
+  const redo = pm.getButton('options', 'redo');
 
   if (undo !== null) {
     pm.removeButton('options', 'undo');
@@ -49,7 +49,7 @@ export default (editor, opts = {}) => {
         id: 'undo',
         className: 'fa fa-undo',
         attributes: { title: 'Undo' },
-        command: function () {
+        command() {
           editor.runCommand('core:undo');
         },
       },
@@ -63,7 +63,7 @@ export default (editor, opts = {}) => {
         id: 'redo',
         className: 'fa fa-repeat',
         attributes: { title: 'Redo' },
-        command: function () {
+        command() {
           editor.runCommand('core:redo');
         },
       },
@@ -71,31 +71,31 @@ export default (editor, opts = {}) => {
   }
 
   // Remove preview button
-  let preview = pm.getButton('options', 'preview');
+  const preview = pm.getButton('options', 'preview');
 
   if (preview !== null) {
     pm.removeButton('options', 'preview');
   }
 
   // Remove clear button
-  let clear = pm.getButton('options', 'canvas-clear');
+  const clear = pm.getButton('options', 'canvas-clear');
 
   if (clear !== null) {
     pm.removeButton('options', 'canvas-clear');
   }
 
   // Remove toggle images button
-  let toggleImages = pm.getButton('options', 'gjs-toggle-images');
+  const toggleImages = pm.getButton('options', 'gjs-toggle-images');
 
   if (toggleImages !== null) {
     pm.removeButton('options', 'gjs-toggle-images');
   }
 
   // Do stuff on load
-  editor.on('load', function () {
+  editor.on('load', () => {
     // Hide Layers Manager
     if (!opts.showLayersManager) {
-      let openLayersBtn = pm.getButton('views', 'open-layers');
+      const openLayersBtn = pm.getButton('views', 'open-layers');
 
       if (openLayersBtn !== null) {
         openLayersBtn.set('attributes', {
@@ -105,30 +105,30 @@ export default (editor, opts = {}) => {
     }
 
     // Activate by default View Components button
-    let viewComponents = pm.getButton('options', 'sw-visibility');
+    const viewComponents = pm.getButton('options', 'sw-visibility');
     viewComponents && viewComponents.set('active', 1);
 
     // Load and show settings and style manager
-    let openTmBtn = pm.getButton('views', 'open-tm');
+    const openTmBtn = pm.getButton('views', 'open-tm');
     openTmBtn && openTmBtn.set('active', 1);
-    let openSm = pm.getButton('views', 'open-sm');
+    const openSm = pm.getButton('views', 'open-sm');
     openSm && openSm.set('active', 1);
 
     pm.removeButton('views', 'open-tm');
 
     // Add Settings Sector
-    let traitsSector = $(
+    const traitsSector = $(
       '<div class="gjs-sm-sector no-select">' +
         '<div class="gjs-sm-title"><span class="icon-settings fa fa-cog"></span> Settings</div>' +
         '<div class="gjs-sm-properties" style="display: none;"></div></div>'
     );
-    let traitsProps = traitsSector.find('.gjs-sm-properties');
+    const traitsProps = traitsSector.find('.gjs-sm-properties');
 
     traitsProps.append($('.gjs-trt-traits'));
     $('.gjs-sm-sectors').before(traitsSector);
-    traitsSector.find('.gjs-sm-title').on('click', function () {
-      let traitStyle = traitsProps.get(0).style;
-      let hidden = traitStyle.display === 'none';
+    traitsSector.find('.gjs-sm-title').on('click', () => {
+      const traitStyle = traitsProps.get(0).style;
+      const hidden = traitStyle.display === 'none';
 
       if (hidden) {
         traitStyle.display = 'block';
@@ -141,7 +141,7 @@ export default (editor, opts = {}) => {
     traitsProps.get(0).style.display = 'block';
 
     // Open block manager
-    let openBlocksBtn = editor.Panels.getButton('views', 'open-blocks');
+    const openBlocksBtn = editor.Panels.getButton('views', 'open-blocks');
     openBlocksBtn && openBlocksBtn.set('active', 1);
   });
 };
