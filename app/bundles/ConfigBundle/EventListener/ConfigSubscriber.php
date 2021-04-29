31987f29e19b702bb673609d692532585df5c779
saurabh.gupta <saurabh.gupta@acquia.com>
2021-04-29 10:43:00 +0530

    add background delete flag

--- app/bundles/ConfigBundle/EventListener/ConfigSubscriber.php
+++ app/bundles/ConfigBundle/EventListener/ConfigSubscriber.php
@@ -14,6 +14,8 @@ namespace Mautic\ConfigBundle\EventListener;
 use Mautic\ConfigBundle\ConfigEvents;
 use Mautic\ConfigBundle\Event\ConfigEvent;
 use Mautic\ConfigBundle\Service\ConfigChangeLogger;
+use Mautic\CoreBundle\Entity\IpAddressRepository;
+use Mautic\CoreBundle\Helper\CoreParametersHelper;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class ConfigSubscriber implements EventSubscriberInterface
@@ -23,9 +25,21 @@ class ConfigSubscriber implements EventSubscriberInterface
      */
     private $configChangeLogger;
 
-    public function __construct(ConfigChangeLogger $configChangeLogger)
+    /**
+     * @var IpAddressRepository
+     */
+    private $ipAddressRepository;
+
+    /**
+     * @var CoreParametersHelper
+     */
+    protected $coreParametersHelper;
+
+    public function __construct(ConfigChangeLogger $configChangeLogger, IpAddressRepository $ipAddressRepository, CoreParametersHelper $coreParametersHelper)
     {
-        $this->configChangeLogger = $configChangeLogger;
+        $this->configChangeLogger   = $configChangeLogger;
+        $this->ipAddressRepository  = $ipAddressRepository;
+        $this->coreParametersHelper = $coreParametersHelper;
     }
 
     public static function getSubscribedEvents(): array
@@ -38,10 +52,18 @@ class ConfigSubscriber implements EventSubscriberInterface
     public function onConfigPostSave(ConfigEvent $event): void
     {
         if ($originalNormData = $event->getOriginalNormData()) {
+            $norData = $event->getNormData();
             // We have something to log
             $this->configChangeLogger
                 ->setOriginalNormData($originalNormData)
-                ->log($event->getNormData());
+                ->log($norData);
+
+            $oldAnonymize_ip = $originalNormData['trackingconfig']['parameters']['anonymize_ip'];
+            $newAnonymize_ip = $norData['trackingconfig']['anonymize_ip'];
+
+            if ($oldAnonymize_ip !== $newAnonymize_ip && $newAnonymize_ip && !$this->coreParametersHelper->get('delete_ip_address_in_background', false)) {
+                $this->ipAddressRepository->deleteAllIpAddress();
+            }
         }
     }
 }
