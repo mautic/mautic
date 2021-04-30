c8061156356c0edd3bca09e2c54f62215d400fd0
lukassykora <lukassykora@seznam.cz>
2021-04-30 19:00:59 +0200

    Fixed getBatchEntities (#9745)
    * Fixed getBatchEntities
    
    * Fixed test
    
    Co-authored-by: Alan Hartless (he/him) <alan@devkardia.com>
    Co-authored-by: Anton Vlasenko <43744263+anton-vlasenko@users.noreply.github.com>

--- app/bundles/ApiBundle/Tests/Controller/CommonApiControllerTest.php
+++ app/bundles/ApiBundle/Tests/Controller/CommonApiControllerTest.php
@@ -12,7 +12,13 @@
 namespace Mautic\ApiBundle\Tests\Controller;
 
 use Mautic\ApiBundle\Controller\CommonApiController;
+use Mautic\ApiBundle\Helper\EntityResultHelper;
 use Mautic\CampaignBundle\Tests\CampaignTestAbstract;
+use Mautic\CoreBundle\Model\AbstractCommonModel;
+use Mautic\UserBundle\Entity\User;
+use Mautic\UserBundle\Entity\UserRepository;
+use Mautic\UserBundle\Model\UserModel;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class CommonApiControllerTest extends CampaignTestAbstract
@@ -99,5 +105,86 @@ class CommonApiControllerTest extends CampaignTestAbstract
         $method->setAccessible(true);
 
         return $method->invokeArgs($controller, $args);
+    }
+
+    public function testGetBatchEntities(): void
+    {
+        $controller = new class() extends CommonApiController {
+            public function testGetBatchEntities(array $parameters, array $errors, AbstractCommonModel $model): ?array
+            {
+                return $this->getBatchEntities($parameters, $errors, false, 'id', $model);
+            }
+        };
+
+        $container = $this->createMock(ContainerInterface::class);
+        $container->method('get')
+            ->with('mautic.api.helper.entity_result')
+            ->willReturn(new EntityResultHelper());
+        $controller->setContainer($container);
+
+        $errors     = [];
+        $parameters = [
+            [
+                'id'            => 3,
+                'username'      => 'API_0YjVvxlg',
+                'firstName'     => 'APIAPI_0YjVvxlg',
+                'lastName'      => 'TestAPI_0YjVvxlg',
+                'email'         => 'API_0YjVvxlg@email.com',
+                'plainPassword' => [
+                    'password' => 'topSecret007',
+                    'confirm'  => 'topSecret007',
+                ],
+                'role' => 1,
+            ],
+            1 => [
+                'id'            => 4,
+                'username'      => 'API_PlEiXJyp',
+                'firstName'     => 'APIAPI_PlEiXJyp',
+                'lastName'      => 'TestAPI_PlEiXJyp',
+                'email'         => 'API_PlEiXJyp@email.com',
+                'plainPassword' => [
+                    'password' => 'topSecret007',
+                    'confirm'  => 'topSecret007',
+                ],
+                'role' => 1,
+            ],
+            2 => [
+                'id'            => 5,
+                'username'      => 'API_AfhKVHTr',
+                'firstName'     => 'APIAPI_AfhKVHTr',
+                'lastName'      => 'TestAPI_AfhKVHTr',
+                'email'         => 'API_AfhKVHTr@email.com',
+                'plainPassword' => [
+                    'password' => 'topSecret007',
+                    'confirm'  => 'topSecret007',
+                ],
+                'role' => 1,
+            ],
+        ];
+
+        $users = [];
+        foreach ([3, 5, 4] as $userId) {
+            $user = $this->createMock(User::class);
+            $user->expects($this->any())
+                ->method('getId')
+                ->willReturn($userId);
+            $users[] = $user;
+        }
+
+        $repository = $this->createMock(UserRepository::class);
+        $repository->expects($this->once())
+            ->method('getTableAlias')
+            ->willReturn('user');
+        $model      = $this->createMock(UserModel::class);
+        $model->expects($this->once())
+            ->method('getRepository')
+            ->willReturn($repository);
+        $model->expects($this->once())
+            ->method('getEntities')
+            ->willReturn($users);
+        $entities   = $controller->testGetBatchEntities($parameters, $errors, $model);
+        $this->assertSame(3, $entities[0]->getId());
+        $this->assertSame(4, $entities[1]->getId());
+        $this->assertSame(5, $entities[2]->getId());
     }
 }
