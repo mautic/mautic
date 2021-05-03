8b00343114556e95f1c2518f8df2bb8702cd6f95
Anton Vlasenko <vlasenko.anton@gmail.com>
2021-05-03 14:20:57 +0200

    1. Fix the setup method. 2. Add a new unit test to increase code coverage.

--- plugins/MauticTagManagerBundle/Tests/Unit/Security/Permissions/TagManagerPermissionsTest.php
+++ plugins/MauticTagManagerBundle/Tests/Unit/Security/Permissions/TagManagerPermissionsTest.php
@@ -18,7 +18,7 @@ class TagManagerPermissionsTest extends TestCase
      */
     private $formBuilder;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
