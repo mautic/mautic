b122071d26f7e8bbab1512226671c47b2c99b9e1
Adrian Schimpf <adrian@idea2.ch>
2021-05-10 11:39:12 +0200

    set mode by config

--- util.service.js
+++ util.service.js
@@ -8,16 +8,10 @@ export default class UtilService {
   static getMode(editor) {
     const cfg = editor.getConfig();
 
-    if ('grapesjsnewsletter' in cfg.pluginsOpts) {
-      return 'email-html';
-    }
-    if ('grapesjsmjml' in cfg.pluginsOpts) {
-      return 'email-mjml';
-    }
-    if ('grapesjswebpage' in cfg.pluginsOpts) {
-      return 'page-html';
+    if (!cfg.pluginsOpts || !cfg.pluginsOpts.grapesjsmautic) {
+      throw new Error('Wrong Mautic Grapesjs mode');
     }
 
-    throw new Error('wrong Mautic Grapesjs mode');
+    return cfg.pluginsOpts.grapesjsmautic.mode;
   }
 }
