6888682c3e980f71541967e9a256c296ceb902c8
Tejas Navghane <ts.navghane@gmail.com>
2021-06-15 14:27:57 +0530

    Fixed installer cache warmup issues: db table prefix not honoured.

---
+++ app/bundles/InstallBundle/DependencyInjection/Compiler/InstallCommandPass.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\InstallBundle\DependencyInjection\Compiler;
+
+use Mautic\InstallBundle\Command\InstallCommand;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class InstallCommandPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        $input       = new ArgvInput($_SERVER['argv'], (new InstallCommand())->getDefinition());
+        $tablePrefix = $input->hasOption('db_table_prefix')
+            ? $input->getOption('db_table_prefix')
+            : MAUTIC_TABLE_PREFIX;
+
+        if (!$tablePrefix) {
+            return;
+        }
+
+        $container->setParameter('mautic.db_table_prefix', $tablePrefix);
+        $container->getDefinition('mautic.tblprefix_subscriber')->setArgument('$tablePrefix', $tablePrefix);
+        $container->getDefinition('mautic.schema.helper.column')->setArgument('$prefix', $tablePrefix);
+        $container->getDefinition('mautic.schema.helper.index')->setArgument('$prefix', $tablePrefix);
+    }
+}
