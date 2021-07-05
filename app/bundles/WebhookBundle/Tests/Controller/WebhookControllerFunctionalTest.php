8984fda14a5181a1e227360dbe5959c8e71be90a
saurabh.gupta <saurabh.gupta@acquia.com>
2021-07-05 17:18:30 +0530

    fix phpstan issue

--- app/bundles/WebhookBundle/Tests/Controller/WebhookControllerFunctionalTest.php
+++ app/bundles/WebhookBundle/Tests/Controller/WebhookControllerFunctionalTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\WebhookBundle\Tests\Entity;
+namespace Mautic\WebhookBundle\Tests\Controller;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use Mautic\WebhookBundle\Entity\Event;
