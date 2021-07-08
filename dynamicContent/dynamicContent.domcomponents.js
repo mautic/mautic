443f44ec1014289e07d00f6090328f74a8b71629
Adrian Schimpf <adrian@idea2.ch>
2021-07-08 18:29:13 +0200

    fix isComponent check for dynamic content block

--- dynamicContent/dynamicContent.domcomponents.js
+++ dynamicContent/dynamicContent.domcomponents.js
@@ -6,55 +6,55 @@ export default class DynamicContentDomComponents {
   }
 
   addDynamicContentType() {
-    // Not sure this is still used. https://grapesjs.com/docs/modules/Components.html#define-custom-component-type
-    function isComponent(el) {
-      console.trace('we use it: isComponent has been called');
-      if (el.getAttribute && el.getAttribute('data-slot') === 'dynamicContent') {
-        console.warn('itistrue', el.getAttribute);
-        return {
-          type: 'dynamic-content',
-        };
-      }
-      console.warn('itisfalse', el.getAttribute);
-      return false;
-    }
     const dc = this.editor.DomComponents;
     const defaultType = dc.getType('default');
     const defaultModel = defaultType.model;
 
-    const model = defaultModel.extend({
-      defaults: {
-        ...defaultModel.prototype.defaults,
-        name: 'Dynamic Content',
-        // draggable: '[data-gjs-type=cell]',
-        // droppable: false,
-        editable: false,
-        stylable: false,
-        propagate: ['droppable', 'editable'],
-        attributes: {
-          // Default attributes
-          'data-gjs-type': 'dynamic-content', // Type for GrapesJS
-          'data-slot': 'dynamicContent', // Retro compatibility with old template
+    const model = defaultModel.extend(
+      {
+        defaults: {
+          ...defaultModel.prototype.defaults,
+          name: 'Dynamic Content',
+          draggable: '[data-gjs-type=cell]',
+          droppable: false,
+          editable: false,
+          stylable: false,
+          propagate: ['droppable', 'editable'],
+          attributes: {
+            // Default attributes
+            'data-gjs-type': 'dynamic-content', // Type for GrapesJS
+            'data-slot': 'dynamicContent', // Retro compatibility with old template
+          },
+        },
+        /**
+         * Initilize the component
+         */
+        init() {
+          // Add toolbar edit button if it's not already in
+          const toolbar = this.get('toolbar');
+          const id = 'toolbar-dynamic-content';
+
+          if (!toolbar.filter((tlb) => tlb.id === id).length) {
+            toolbar.unshift({
+              id,
+              command: 'preset-mautic:dynamic-content-open',
+              attributes: { class: 'fa fa-pencil-square-o' },
+            });
+          }
         },
       },
-
-      /**
-       * Initilize the component
-       */
-      init() {
-        // Add toolbar edit button if it's not already in
-        const toolbar = this.get('toolbar');
-        const id = 'toolbar-dynamic-content';
-
-        if (!toolbar.filter((tlb) => tlb.id === id).length) {
-          toolbar.unshift({
-            id,
-            command: 'preset-mautic:dynamic-content-open',
-            attributes: { class: 'fa fa-pencil-square-o' },
-          });
-        }
-      },
-    });
+      {
+        // Dynamic Content component detection
+        isComponent(el) {
+          if (el.getAttribute && el.getAttribute('data-slot') === 'dynamicContent') {
+            return {
+              type: 'dynamic-content',
+            };
+          }
+          return false;
+        },
+      }
+    );
 
     const view = defaultType.view.extend({
       attributes: {
@@ -82,7 +82,6 @@ export default class DynamicContentDomComponents {
 
     // add the Dynamic Content component
     dc.addType('dynamic-content', {
-      isComponent,
       model,
       view,
     });
