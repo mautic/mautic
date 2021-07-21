b1df0a4db942fa2bd8fd07cc80636402056c7338
John Linhart <admin@escope.cz>
2021-07-21 11:21:59 +0200

    One rector rule working, another prepared for the next PR

--- rector.php
+++ rector.php
@@ -3,13 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
-use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;
-use Rector\DeadCode\Rector\For_\RemoveDeadIfForeachForRector;
-use Rector\DeadCode\Rector\If_\SimplifyIfElseWithSameContentRector;
-use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
-use Rector\DeadCode\Rector\Switch_\RemoveDuplicatedCaseInSwitchRector;
-use Rector\DeadCode\Rector\Ternary\TernaryToBooleanOrFalseToBooleanAndRector;
-use Rector\Set\ValueObject\SetList;
+use Rector\DeadCode\Rector\BooleanAnd\RemoveAndTrueRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -17,7 +11,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
     // Define what rule sets will be applied
-    $containerConfigurator->import(SetList::DEAD_CODE);
+    // $containerConfigurator->import(SetList::DEAD_CODE);
 
     $parameters->set(
         Option::PATHS,
@@ -44,21 +38,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             __DIR__.'/app/bundles/WebhookBundle/Entity/Webhook.php',
             __DIR__.'/app/bundles/UserBundle/Entity/UserToken.php',
             __DIR__.'/app/bundles/EmailBundle/Entity/EmailReplyRepository.php',
-            RemoveUnusedParameterRector::class, // Causes BC breaks and broken services. Use only manually with caution.
-            RemoveDefaultArgumentValueRector::class, // Doesn't play nicely every time.
-            SimplifyIfElseWithSameContentRector::class, // Removes code that is not the same.
-            RemoveDeadIfForeachForRector::class, // Problematic with some copy-pasted code.
-            TernaryToBooleanOrFalseToBooleanAndRector::class, // see https://github.com/rectorphp/rector/issues/2765
-            RemoveDuplicatedCaseInSwitchRector::class, // see https://github.com/rectorphp/rector/issues/2730
-            RecastingRemovalRector::class, // Does some dangerous changes in e.g. queries
-            RemoveUnusedDoctrineEntityMethodAndPropertyRector::class, // Incorrectly removes loadMetadata() methods
-            RemoveUnusedPrivatePropertyRector::class, // Incorrectly removes some private properties
-            RemoveUnusedClassConstantRector::class, // Incorrectly removes some class constants that we use
-            RemoveDeadRecursiveClassMethodRector::class, // Changed quite some code, didn't have time to check if it's correct
         ]
     );
 
-//    register single rule
-//    $services = $containerConfigurator->services();
-//    $services->set(TypedPropertyRector::class);
+    $services = $containerConfigurator->services();
+    $services->set(RemoveAndTrueRector::class);
+    // $services->set(RemoveAlwaysTrueIfConditionRector::class); // 41 files would have changed (we should have a separate PR to enable this)
 };
