aca2e3ae3339a1db83c9d54881a476121a7ea6c6
jazeabby <abhishek.jain@acquia.com>
2021-07-22 15:01:06 +0530

    Removing check for Alter query

--- app/migrations/Version20210520100503.php
+++ app/migrations/Version20210520100503.php
@@ -12,21 +12,10 @@ declare(strict_types=1);
 namespace Mautic\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use Mautic\CoreBundle\Doctrine\PreUpAssertionMigration;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 
-final class Version20210520100503 extends PreUpAssertionMigration
+final class Version20210520100503 extends AbstractMauticMigration
 {
-    protected function preUpAssertions(): void
-    {
-        $this->skipAssertion(
-            function (Schema $schema) {
-                return 'utf8mb4' === $schema->getTable("{$this->prefix}email_stats")
-                        ->getColumn('tokens')->getPlatformOption('charset');
-            },
-            "`tokens` column of `{$this->prefix}email_stats` table has already utf8mb4 charset"
-        );
-    }
-
     public function up(Schema $schema): void
     {
         $this->addSql("ALTER TABLE `{$this->prefix}email_stats` CHANGE COLUMN `tokens` `tokens` LONGTEXT CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL COMMENT '(DC2Type:array)';");
