10fa4981024f47bc7b7d6b9d51f5150b141c99b2
Zdeno Kuzmany <zdeno@kuzmany.biz>
2021-07-26 17:47:21 +0200

    Just add description of logs in source code - recent activity logs

--- app/bundles/CoreBundle/Views/Helper/recentactivity.html.php
+++ app/bundles/CoreBundle/Views/Helper/recentactivity.html.php
@@ -40,7 +40,7 @@
             </li>
             <?php endforeach; ?>
         </ul>
-        <?php echo '<!-- '.PHP_EOL.json_encode($logs, JSON_PRETTY_PRINT).PHP_EOL.' -->'; ?>
+        <?php echo '<!-- recent activity logs '.PHP_EOL.json_encode($logs, JSON_PRETTY_PRINT).PHP_EOL.' -->'; ?>
         <?php endif; ?>
     </div>
 </div>
