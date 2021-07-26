ca24dca95128ad68f727f607f9d8960d27d03930
Nick Veenhof <nick.veenhof@gmail.com>
2021-07-26 15:51:17 +0200

    Update CheckStep.php

--- app/bundles/InstallBundle/Configurator/Step/CheckStep.php
+++ app/bundles/InstallBundle/Configurator/Step/CheckStep.php
@@ -13,7 +13,7 @@ namespace Mautic\InstallBundle\Configurator\Step;
 
 use Mautic\CoreBundle\Loader\ParameterLoader;
 use Mautic\CoreBundle\Configurator\Configurator;
-use Mautic\CoreBundle\Configurator\Step\StepInterface;Paramete
+use Mautic\CoreBundle\Configurator\Step\StepInterface;
 use Mautic\CoreBundle\Helper\FileHelper;
 use Mautic\CoreBundle\Security\Cryptography\Cipher\Symmetric\OpenSSLCipher;
 use Mautic\InstallBundle\Configurator\Form\CheckStepType;
