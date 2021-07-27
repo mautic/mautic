14a3ba33c8eae38b7bdb0aea101cc4d18400388b
disha2402 <dvpishavadia24@gmail.com>
2021-07-27 16:06:54 +0530

    Button code

--- blocks.js
+++ blocks.js
@@ -14,6 +14,35 @@ export default (editor, opts = {}) => {
     dcb.addDynamciContentBlock();
   }
 
+  //add button block for landing page 
+  const mode = ContentService.getMode(editor);   
+  if (mode === ContentService.modePageHtml) {   
+    bm.add('button', {
+      label: 'Button',
+      category : Mautic.translate('grapesjsbuilder.categoryBlockLabel'),
+      attributes: {
+        class: 'gjs-fonts gjs-f-button'
+      },
+      content: 
+        `<style>
+            .button {
+              display:inline-block;
+              text-decoration:none;
+              border-color:#4e5d9d;
+              border-width:10px 20px;
+              border-style:solid;
+              -webkit-border-radius:3px;
+              -moz-border-radius:3px;
+              border-radius:3px;
+              background-color:#4e5d9d;
+              font-size:16px;
+              color:#ffffff;
+            }           
+         </style>
+         <a href="#" target="_blank" class="button">Button</a>`,
+    });
+  }
+  
   // Add icon to mj-hero
   if (typeof bm.get('mj-hero') !== 'undefined') {
     bm.get('mj-hero').set({
