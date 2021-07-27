fbc8be0ae127515df9c6cba652535f7b3b5343ce
Adrian Schimpf <adrian@idea2.ch>
2021-07-27 11:02:44 +0200

    prettier fixes

--- listeners.js
+++ listeners.js
@@ -3,15 +3,12 @@ export default () => {
   // this.editor.on('run:mautic-editor-email-mjml-close:before', () => {
   //   mQuery('textarea.builder-html').val(this.getBody());
   // });
-
   // getBody() {
   //   // =>return the body based on the orginal content as string
-
   //   // Parse HTML template
   //   const parser = new DOMParser();
   //   const content = parser.parseFromString(this.originalContent, 'text/html');
   //   const body = content.body.innerHTML || mQuery('textarea.builder-mjml').val();
-
   //   if (!body) {
   //     throw Error('No HTML or MJML content found');
   //   }
