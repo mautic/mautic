54bce62b8600a7a27a78db5ffc6e1fd5ab152c17
disha2402 <dvpishavadia24@gmail.com>
2021-07-28 18:43:39 +0530

    variable for style of the button

--- buttonBlock.js
+++ buttonBlock.js
@@ -12,6 +12,21 @@ export default class ButtonBlock {
   }
 
   addButtonBlock() {
+    const style = `<style>
+                      .button {
+                        display:inline-block;
+                        text-decoration:none;
+                        border-color:#4e5d9d;
+                        border-width:10px 20px;
+                        border-style:solid;
+                        -webkit-border-radius:3px;
+                        -moz-border-radius:3px;
+                        border-radius:3px;
+                        background-color:#4e5d9d;
+                        font-size:16px;
+                        color:#ffffff;
+                      }           
+                    </style>`;
     this.blockManager.add('button', {
       label: Mautic.translate('grapesjsbuilder.buttonBlockLabel'),
       category : Mautic.translate('grapesjsbuilder.categoryBlockLabel'),
@@ -19,21 +34,7 @@ export default class ButtonBlock {
         class: 'gjs-fonts gjs-f-button'
       },
       content: 
-        `<style>
-            .button {
-              display:inline-block;
-              text-decoration:none;
-              border-color:#4e5d9d;
-              border-width:10px 20px;
-              border-style:solid;
-              -webkit-border-radius:3px;
-              -moz-border-radius:3px;
-              border-radius:3px;
-              background-color:#4e5d9d;
-              font-size:16px;
-              color:#ffffff;
-            }           
-         </style>
+        `${style}
          <a href="#" target="_blank" class="button">Button</a>`,
     });
   }
