3da4bf97d3463ccefe12fb2deed85a4beb0b2cdf
saurabh.gupta <saurabh.gupta@acquia.com>
2021-08-04 18:11:20 +0530

    Fix phpstan issue

--- app/bundles/EmailBundle/Tests/Command/SendWinnerEmailCommandFunctionalTest.php
+++ app/bundles/EmailBundle/Tests/Command/SendWinnerEmailCommandFunctionalTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\AllydeBundle\Tests\Command;
+namespace Mautic\EmailBundle\Tests\Command;
 
 use Mautic\CoreBundle\Helper\ExitCode;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
