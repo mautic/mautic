d5ccb649b3399fca87f15a32f553b6acfcc20b5f
Tejas Navghane <ts.navghane@gmail.com>
2021-08-09 14:17:25 +0530

    Add event to respect sync strategies while full object report build

---
+++ app/bundles/IntegrationsBundle/Event/InternalObjectFindByIdEvent.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\IntegrationsBundle\Event;
+
+use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\Object\ObjectInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+class InternalObjectFindByIdEvent extends Event
+{
+    /**
+     * @var ObjectInterface
+     */
+    private $object;
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var object
+     */
+    private $entity;
+
+    public function __construct(ObjectInterface $object)
+    {
+        $this->object = $object;
+    }
+
+    public function getObject(): ObjectInterface
+    {
+        return $this->object;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getEntity(): object
+    {
+        return $this->entity;
+    }
+
+    public function setEntity(object $entity): void
+    {
+        $this->entity = $entity;
+    }
+}
--- app/bundles/IntegrationsBundle/Sync/SyncDataExchange/Internal/ReportBuilder/FullObjectReportBuilder.php
+++ app/bundles/IntegrationsBundle/Sync/SyncDataExchange/Internal/ReportBuilder/FullObjectReportBuilder.php
@@ -13,7 +13,11 @@ declare(strict_types=1);
 
 namespace Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\ReportBuilder;
 
+use Mautic\IntegrationsBundle\Event\InternalCompanyEvent;
+use Mautic\IntegrationsBundle\Event\InternalContactEvent;
+use Mautic\IntegrationsBundle\Event\InternalObjectFindByIdEvent;
 use Mautic\IntegrationsBundle\Event\InternalObjectFindEvent;
+use Mautic\IntegrationsBundle\Exception\InvalidValueException;
 use Mautic\IntegrationsBundle\IntegrationEvents;
 use Mautic\IntegrationsBundle\Sync\DAO\DateRange;
 use Mautic\IntegrationsBundle\Sync\DAO\Sync\Report\ObjectDAO as ReportObjectDAO;
@@ -25,6 +29,8 @@ use Mautic\IntegrationsBundle\Sync\Exception\ObjectNotFoundException;
 use Mautic\IntegrationsBundle\Sync\Logger\DebugLogger;
 use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\ObjectProvider;
 use Mautic\IntegrationsBundle\Sync\SyncDataExchange\MauticSyncDataExchange;
+use Mautic\LeadBundle\Entity\Company;
+use Mautic\LeadBundle\Entity\Lead;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class FullObjectReportBuilder
@@ -115,9 +121,17 @@ class FullObjectReportBuilder
         return $syncReport;
     }
 
+    /**
+     * @throws ObjectNotFoundException
+     */
     private function processObjects(ObjectDAO $requestedObjectDAO, ReportDAO $syncReport, array $foundObjects): void
     {
         $fields = $requestedObjectDAO->getFields();
+
+        if ($this->dispatcher->hasListeners(IntegrationEvents::INTEGRATION_FIND_INTERNAL_RECORD)) {
+            $event = new InternalObjectFindByIdEvent($this->objectProvider->getObjectByName($requestedObjectDAO->getObject()));
+        }
+
         foreach ($foundObjects as $object) {
             $modifiedDateTime = new \DateTime(
                 !empty($object['date_modified']) ? $object['date_modified'] : $object['date_added'],
@@ -125,6 +139,18 @@ class FullObjectReportBuilder
             );
             $reportObjectDAO  = new ReportObjectDAO($requestedObjectDAO->getObject(), $object['id'], $modifiedDateTime);
             $syncReport->addObject($reportObjectDAO);
+
+            try {
+                if (isset($event)) {
+                    // Update object id rather than creating the event again.
+                    $event->setId((int) $object['id']);
+                    $this->dispatcher->dispatch(IntegrationEvents::INTEGRATION_FIND_INTERNAL_RECORD, $event);
+                    $this->dispatchBeforeFieldChangesEvent($syncReport->getIntegration(), $event->getEntity());
+                }
+            } catch (InvalidValueException $e) {
+                // Object is not eligible, continue.
+                continue;
+            }
 
             foreach ($fields as $field) {
                 try {
@@ -140,5 +166,35 @@ class FullObjectReportBuilder
                 }
             }
         }
+    }
+
+    /**
+     * @throws InvalidValueException
+     */
+    private function dispatchBeforeFieldChangesEvent(string $integrationName, object $object): void
+    {
+        if ($object instanceof Lead) {
+            if ($this->dispatcher->hasListeners(IntegrationEvents::INTEGRATION_BEFORE_FULL_CONTACT_REPORT_BUILD)) {
+                $this->dispatcher->dispatch(
+                    IntegrationEvents::INTEGRATION_BEFORE_FULL_CONTACT_REPORT_BUILD,
+                    new InternalContactEvent($integrationName, $object)
+                );
+            }
+
+            return;
+        }
+
+        if ($object instanceof Company) {
+            if ($this->dispatcher->hasListeners(IntegrationEvents::INTEGRATION_BEFORE_FULL_COMPANY_REPORT_BUILD)) {
+                $this->dispatcher->dispatch(
+                    IntegrationEvents::INTEGRATION_BEFORE_FULL_COMPANY_REPORT_BUILD,
+                    new InternalCompanyEvent($integrationName, $object)
+                );
+            }
+
+            return;
+        }
+
+        throw new InvalidValueException('An object type should be specified. None matches.');
     }
 }
