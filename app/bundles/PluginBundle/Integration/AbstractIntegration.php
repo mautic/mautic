84f352f8d89ce9057a2b1f64249e3b2ae568036c
Nick Veenhof <nick.veenhof@gmail.com>
2021-08-20 12:04:15 +0200

    POST data is being forwarded as an array but not accepted

--- app/bundles/PluginBundle/Integration/AbstractIntegration.php
+++ app/bundles/PluginBundle/Integration/AbstractIntegration.php
@@ -826,7 +826,7 @@ abstract class AbstractIntegration implements UnifiedIntegrationInterface
                 case 'PUT':
                 case 'PATCH':
                     $result = $client->request($method, $url, [
-                        \GuzzleHttp\RequestOptions::BODY    => $parameters,
+                        \GuzzleHttp\RequestOptions::FORM_PARAMS => $parameters,
                         \GuzzleHttp\RequestOptions::HEADERS => $headers,
                         \GuzzleHttp\RequestOptions::TIMEOUT => $timeout,
                     ]);
