5d6f14aa200935fe996167a162ba45954d8d4b8d
Adrian Schimpf <adrian@idea2.ch>
2021-08-21 15:29:46 +0200

    working hello world with cke

--- plugins/GrapesJsBuilderBundle/Demo/helloWorld/helloWorld.js
+++ plugins/GrapesJsBuilderBundle/Demo/helloWorld/helloWorld.js
@@ -2,62 +2,17 @@
 /* eslint-disable no-unused-expressions */
 import 'grapesjs/dist/css/grapes.min.css';
 import grapesjs from 'grapesjs';
+import 'grapesjs-plugin-ckeditor';
+
 
 const editor = grapesjs.init({
   fromElement: 1,
   container: '#gjs',
   height: '100%',
   avoidInlineStyle: false,
-  plugins: [],
-});
-
-editor.setCustomRte({
-  /**
-   * Enabling the custom RTE
-   * @param  {HTMLElement} el This is the HTML node which was selected to be edited
-   * @param  {Object} rte It's the instance you'd return from the first call of enable().
-   *                      At the first call it'd be undefined. This is useful when you need
-   *                      to check if the RTE is already enabled on the component
-   * @return {Object} The return should be the RTE initialized instance
-   */
-  enable(el, rte) {
-    // If already exists just focus
-    if (rte) {
-      this.focus(el, rte); // implemented later
-      return rte;
-    }
-
-    // CKEditor initialization
-    // eslint-disable-next-line no-undef
-    rte = CKEDITOR.inline(el, {
-      // Your configurations...
-      sharedSpaces: {
-        top: editor.RichTextEditor.getToolbarEl(),
-      },
-    });
-
-    this.focus(el, rte);
-    return rte;
-  },
-  focus(el, rte) {
-    // Do nothing if already focused
-    if (rte && rte.focusManager.hasFocus) {
-      return;
-    }
-    el.contentEditable = true;
-    rte && rte.focus();
-  },
-  disable(el, rte) {
-    el.contentEditable = false;
-    if (rte && rte.focusManager) {
-      rte.focusManager.blur(true);
-    }
-  },
-});
-
-editor.on('rteToolbarPosUpdate', (pos) => {
-  if (pos.top <= pos.canvasTop) {
-    pos.top = pos.elementTop + pos.elementHeight;
+  plugins: ['gjs-plugin-ckeditor'],
+  pluginsOpts: {
+    'gjs-plugin-ckeditor': {}
   }
 });
 
