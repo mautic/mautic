e132953e4b431629b4a75853903ac39b19af1655
Julien Ulm <jul@webmecanik.com>
2021-08-27 09:57:51 +0200

    Review changes

--- app/migrations/Version20210623071326.php
+++ app/migrations/Version20210623071326.php
@@ -17,24 +17,14 @@ use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 
 final class Version20210623071326 extends AbstractMauticMigration
 {
-    /**
-     * @throw SkipMigrationException
-     */
-    public function preUp(Schema $schema): void
-    {
-    }
-
     public function up(Schema $schema): void
     {
-        // Changes remote_path and original_file_name columns to Longtext
         $this->addSql("ALTER TABLE {$this->prefix}forms MODIFY post_action_property LONGTEXT ");
     }
 
     public function down(Schema $schema): void
     {
-        // Changes remote_path and original_file_name columns to earlier VARCHAR(191)
-        // Rolling back causes any long URLs or Filenames with more than 191 chars to be trimmed.
-        $this->addSql("UPDATE {$this->prefix}forms SET post_action_property = left(post_action,191)");
-        $this->addSql("ALTER TABLE {$this->prefix}forms MODIFY post_action_property VARCHAR(191) DEFAULT '' ");
+        $this->addSql("UPDATE {$this->prefix}forms SET post_action_property = left(post_action_property,191)");
+        $this->addSql("ALTER TABLE {$this->prefix}forms MODIFY post_action_property VARCHAR(191) DEFAULT NULL ");
     }
 }
