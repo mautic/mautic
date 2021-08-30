49582aa6425020b6e22d74b11d18d4247510f672
Donald Gilbert <don@dongilbert.net>
2021-08-30 14:36:16 -0400

    Complete tests
    These test ensure that the form tag correctly wraps all inputs for the frequency form.
    We must always include the _token and save button within the form tag.

--- app/bundles/PageBundle/Tests/Functional/EventListener/BuilderSubscriberTest.php
+++ app/bundles/PageBundle/Tests/Functional/EventListener/BuilderSubscriberTest.php
@@ -4,17 +4,86 @@ declare(strict_types=1);
 
 namespace Mautic\PageBundle\Tests\Functional\EventListener;
 
+use DateTime;
+use Mautic\CoreBundle\Test\AbstractMauticTestCase;
+use Mautic\EmailBundle\Entity\Email;
+use Mautic\EmailBundle\Entity\Stat;
+use Mautic\LeadBundle\Entity\Lead;
 use Mautic\PageBundle\Entity\Page;
-use Mautic\PageBundle\Event\PageDisplayEvent;
-use PHPUnit\Framework\TestCase;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use PHPUnit\Framework\Assert;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class BuilderSubscriberTest extends WebTestCase
+class BuilderSubscriberTest extends AbstractMauticTestCase
 {
-    public function testOnPageDisplayCorrectlyWrapsForm(): void
+    const TOKEN_SELECTOR       = '#lead_contact_frequency_rules__token';
+    const SAVE_BUTTON_SELECTOR = '#lead_contact_frequency_rules_buttons_save';
+    const FORM_SELECTOR        = 'form[name="lead_contact_frequency_rules"]';
+
+    public function testOnPageDisplayCorrectlyWrapsAllFrequencyFormInputsIncludingTokenAndSaveButton(): void
     {
-        $subscriber = static::$kernel->getContainer()->get('mautic.pagebuilder.subscriber');
-        //$event      = new PageDisplayEvent($this->getPageContent(), new Page(), )
+        $emailStat = $this->createStat(
+            $email = $this->createEmail(),
+            $lead  = $this->createLead()
+        );
+
+        $this->em->flush();
+
+        $unsubscribeUrl = $this->router->generate('mautic_email_unsubscribe', [
+            'idHash'     => $emailStat->getTrackingHash(),
+            'urlEmail'   => $lead->getEmail(),
+            'secretHash' => $this->container->get('mautic.helper.mailer_hash')->getEmailHash($lead->getEmail()),
+        ], UrlGeneratorInterface::ABSOLUTE_PATH);
+
+        $crawler = $this->client->request('GET', $unsubscribeUrl);
+        $form    = $crawler->filter(static::FORM_SELECTOR);
+
+        Assert::assertCount(1, $form->filter(static::TOKEN_SELECTOR));
+        Assert::assertCount(1, $form->filter(static::SAVE_BUTTON_SELECTOR));
+    }
+
+    private function createStat(Email $email, Lead $lead): Stat
+    {
+        $stat = new Stat();
+        $stat->setEmail($email);
+        $stat->setLead($lead);
+        $stat->setEmailAddress($lead->getEmail());
+        $stat->setDateSent(new DateTime());
+        $stat->setTrackingHash(uniqid());
+        $this->em->persist($stat);
+
+        return $stat;
+    }
+
+    private function createEmail(): Email
+    {
+        $email = new Email();
+        $email->setName('Example');
+        $email->setPreferenceCenter($this->createPage());
+        $this->em->persist($email);
+
+        return $email;
+    }
+
+    private function createLead(): Lead
+    {
+        $lead = new Lead();
+        $lead->setEmail('test@example.com');
+        $this->em->persist($lead);
+
+        return $lead;
+    }
+
+    private function createPage(): Page
+    {
+        $page = new Page();
+        $page->setTitle('Preference Center');
+        $page->setAlias('preference-center');
+        $page->setIsPreferenceCenter(true);
+        $page->setContent($this->getPageContent());
+        $page->setIsPublished(true);
+        $this->em->persist($page);
+
+        return $page;
     }
 
     private function getPageContent(): string
@@ -30,11 +99,7 @@ class BuilderSubscriberTest extends WebTestCase
         <div data-slot="channelfrequency"></div>
     </div>
     <div>
-        <div data-slot="saveprefsbutton">
-            <a href="javascript:void(null)" class="button btn btn-default btn-save" style="display:inline-block;text-decoration:none;border-color:#4e5d9d;border-width: 10px 20px;border-style:solid;-webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px; background-color: #4e5d9d; font-size: 16px; color: #ffffff;">
-                Save preferences
-            </a>        
-        </div>
+        <div data-slot="saveprefsbutton"></div>
     </div>
 </body>
 </html>
