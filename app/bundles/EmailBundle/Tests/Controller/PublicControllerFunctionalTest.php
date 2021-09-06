146e9666bc3830dc8535b87aad4271bb9c263629
damzoneuh <dre@webmecanik.com>
2021-09-06 10:18:06 +0200

    add functional test

---
+++ app/bundles/EmailBundle/Tests/Controller/PublicControllerFunctionalTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic, Inc.
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\EmailBundle\Tests\Controller;
+
+use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\EmailBundle\Entity\Email;
+use Mautic\LeadBundle\Entity\LeadList;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PublicControllerFunctionalTest extends MauticMysqlTestCase
+{
+    public function testUnsubscribeAction()
+    {
+        $client  = $this->client;
+        $segment = $this->createSegment();
+        $email   = $this->createEmail();
+        $email->addList($segment);
+
+        $this->em->persist($segment);
+        $this->em->persist($email);
+        $this->em->flush();
+
+        $client->request('GET', '/email/unsubscribe/'.$email->getId());
+        $response = $client->getResponse();
+        $this->assertSame(200, $response->getStatusCode(), $response->getContent());
+    }
+
+    private function createSegment(string $suffix = 'A'): LeadList
+    {
+        $segment = new LeadList();
+        $segment->setName("Segment $suffix");
+        $segment->setPublicName("Segment $suffix");
+        $segment->setAlias("segment-$suffix");
+
+        return $segment;
+    }
+
+    private function createEmail(string $suffix = 'A', string $emailType = 'list')
+    {
+        $email = new Email();
+        $email->setName("Email $suffix");
+        $email->setSubject("Email $suffix Subject");
+        $email->setEmailType($emailType);
+
+        return $email;
+    }
+}
