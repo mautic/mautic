aadb37407a6be939d2bbf86f0db2ad15e441bde8
Theodoros Ploumis <me@theodorosploumis.com>
2021-09-29 12:18:18 +0300

    Add gitpod.io integration

---
+++ .ddev/gitpod-setup-ddev.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Set up ddev for use on gitpod
+
+set -eu -o pipefail
+
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Generate a config.gitpod.yaml that adds the gitpod
+# proxied ports so they're known to ddev.
+shortgpurl="${GITPOD_WORKSPACE_URL#'https://'}"
+
+cat <<CONFIGEND > ${MYDIR}/config.gitpod.yaml
+#ddev-gitpod-generated
+use_dns_when_possible: false
+# Throwaway ports, otherwise Gitpod throw an error 'port needs to be > 1024'
+router_http_port: "8888"
+router_https_port: "8889"
+additional_fqdns:
+- 8888-${shortgpurl}
+- 8025-${shortgpurl}
+- 8036-${shortgpurl}
+CONFIGEND
+
+# We need host.docker.internal inside the container,
+# So add it via docker-compose.host-docker-internal.yaml
+hostip=$(awk "\$2 == \"$HOSTNAME\" { print \$1; }" /etc/hosts)
+
+cat <<COMPOSEEND >${MYDIR}/docker-compose.host-docker-internal.yaml
+#ddev-gitpod-generated
+version: "3.6"
+services:
+  web:
+    extra_hosts:
+    - "host.docker.internal:${hostip}"
+    # This adds 8080 on the host (bound on all interfaces)
+    # It goes directly to the web container without
+    # ddev-nginx
+    ports:
+    - 8080:80
+COMPOSEEND
+
+# Misc housekeeping before start
+ddev config global --router-bind-all-interfaces
+
+yes | ddev start
