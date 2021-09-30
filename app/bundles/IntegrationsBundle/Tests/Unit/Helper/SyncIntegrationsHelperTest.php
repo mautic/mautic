4ec98b237fc394509547385f16a8696522c3144a
Tejas Navghane <ts.navghane@gmail.com>
2021-09-30 12:13:00 +0530

    Sync integration unit test started

---
+++ app/bundles/IntegrationsBundle/Tests/Unit/Helper/SyncIntegrationsHelperTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\IntegrationsBundle\Tests\Unit\Helper;
+
+use Mautic\IntegrationsBundle\Helper\IntegrationsHelper;
+use Mautic\IntegrationsBundle\Helper\SyncIntegrationsHelper;
+use Mautic\IntegrationsBundle\Integration\Interfaces\SyncInterface;
+use Mautic\IntegrationsBundle\Sync\SyncDataExchange\Internal\ObjectProvider;
+use Mautic\PluginBundle\Entity\Integration;
+use PHPUnit\Framework\TestCase;
+
+class SyncIntegrationsHelperTest extends TestCase
+{
+    public function testHasObjectSyncEnabled(): void
+    {
+        $mauticObject           = 'some_integration';
+        $integrationsHelperMock = $this->createMock(IntegrationsHelper::class);
+        $objectProviderMock     = $this->createMock(ObjectProvider::class);
+        $syncIntegrationsHelper = new SyncIntegrationsHelper($integrationsHelperMock, $objectProviderMock);
+        $syncInterfaceMock      = $this->createMock(SyncInterface::class);
+        $syncInterfaceMock->method('getName')->willReturn($mauticObject);
+        $syncIntegrationsHelper->addIntegration($syncInterfaceMock);
+
+        $integrationMock = $this->createMock(Integration::class);
+        $integrationMock->method('getIsPublished')->willReturn(true);
+        $supportedFeatures = [
+            'integration' => [
+                'syncDateFrom'       => '2021-09-01',
+                'syncStrategies'     => [
+                    'Lead' => [
+                        'operator' => null,
+                        'field'    => 'IsUnreadByOwner',
+                    ],
+                ],
+                'syncStrategiesPush' => [
+                    'lead' => [
+                            'operator' => null,
+                            'field'    => null,
+                        ],
+                ],
+                'activitySync'       => null,
+                'activityEvents'     => [
+                    'point.gained',
+                    'form.submitted',
+                    'email.read',
+                ],
+                'sandbox'            => [],
+                'namespace'          => null,
+            ],
+            'sync'        => [
+                'objects'       => [
+                    'Lead',
+                ],
+                'fieldMappings' => [
+                    'Lead' => [
+                        'Company'  => [
+                            'mappedField'   => 'company',
+                            'syncDirection' => 'integration',
+                        ],
+                        'Email'    => [
+                            'mappedField'   => 'email',
+                            'syncDirection' => 'bidirectional',
+                        ],
+                        'LastName' => [
+                            'mappedField'   => 'lastname',
+                            'syncDirection' => 'mautic',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $integrationMock->method('getSupportedFeatures')->willReturn($supportedFeatures);
+        $integrationsHelperMock->method('getIntegrationConfiguration')
+            ->with($syncInterfaceMock)
+            ->willReturn($integrationMock);
+
+        $syncIntegrationsHelper->hasObjectSyncEnabled($mauticObject);
+    }
+}
