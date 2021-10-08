5f34c9ee0d7def56113764bcef54c885aa745b7d
NormanPracht <npr@webmecanik.com>
2021-10-08 11:13:47 +0200

    fix typo

--- app/bundles/LeadBundle/Controller/TimelineController.php
+++ app/bundles/LeadBundle/Controller/TimelineController.php
@@ -266,8 +266,7 @@ class TimelineController extends CommonController
                 }
             }
 
-            $items = $this->getEngagements($lead, $filters, $order, $loop + 1, 200);
-            ['events'] ?? [];
+            $items = $this->getEngagements($lead, $filters, $order, $loop + 1, 200)['events'] ?? [];
 
             $this->getDoctrine()->getManager()->clear();
 
