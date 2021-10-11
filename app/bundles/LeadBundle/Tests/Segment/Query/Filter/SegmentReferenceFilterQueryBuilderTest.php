dc1f023665239aa9528b3c877b1af50d9cc3a9b7
Rohit Pavaskar <rohit.pavaskar@acquia.com>
2021-10-11 16:31:52 +0530

    Added basic test cases for SegmentReferenceFilterQueryBuilderTest.php

---
+++ app/bundles/LeadBundle/Tests/Segment/Query/Filter/SegmentReferenceFilterQueryBuilderTest.php
@@ -0,0 +1,151 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\LeadBundle\Tests\Segment\Query\Filter;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManager;
+use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\LeadBundle\Entity\LeadList;
+use Mautic\LeadBundle\Provider\FilterOperatorProviderInterface;
+use Mautic\LeadBundle\Segment\ContactSegmentFilter;
+use Mautic\LeadBundle\Segment\ContactSegmentFilterCrate;
+use Mautic\LeadBundle\Segment\ContactSegmentFilterFactory;
+use Mautic\LeadBundle\Segment\ContactSegmentFilterOperator;
+use Mautic\LeadBundle\Segment\Decorator\BaseDecorator;
+use Mautic\LeadBundle\Segment\Query\ContactSegmentQueryBuilder;
+use Mautic\LeadBundle\Segment\Query\Filter\FilterQueryBuilderInterface;
+use Mautic\LeadBundle\Segment\Query\Filter\SegmentReferenceFilterQueryBuilder;
+use Mautic\LeadBundle\Segment\Query\QueryBuilder;
+use Mautic\LeadBundle\Segment\RandomParameterName;
+use Mautic\LeadBundle\Segment\TableSchemaColumnsCache;
+use MauticPlugin\SalesforceBundle\Segment\Query\Filter\SalesforceContactFilterQueryBuilder;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class SegmentReferenceFilterQueryBuilderTest extends MauticMysqlTestCase
+{
+    /**
+     * @var MockObject|RandomParameterName
+     */
+    private $randomParameterMock;
+
+    /**
+     * @var MockObject|EventDispatcherInterface
+     */
+    private $dispatcherMock;
+
+    /**
+     * @var Connection|MockObject
+     */
+    private $connectionMock;
+
+    /**
+     * @var SalesforceContactFilterQueryBuilder
+     */
+    private $queryBuilder;
+
+    /**
+     * @var LeadList
+     */
+    private $segment;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        defined('MAUTIC_TABLE_PREFIX') or define('MAUTIC_TABLE_PREFIX', '');
+        $this->randomParameterMock = $this->createMock(RandomParameterName::class);
+        $this->dispatcherMock      = $this->createMock(EventDispatcherInterface::class);
+        $this->connectionMock      = $this->createMock(Connection::class);
+
+        $this->queryBuilder        = new SegmentReferenceFilterQueryBuilder(
+            $this->randomParameterMock,
+            new ContactSegmentQueryBuilder($this->em, $this->randomParameterMock, $this->dispatcherMock),
+            $this->em,
+            $this->createMock(ContactSegmentFilterFactory::class),
+            $this->dispatcherMock
+        );
+
+        $this->connectionMock->method('quote')
+            ->willReturnArgument(0);
+
+        $this->segment = $this->createNewSegment();
+    }
+
+    public function testGetServiceId(): void
+    {
+        $this->assertEquals(
+            'mautic.lead.query.builder.special.leadlist',
+            $this->queryBuilder::getServiceId()
+        );
+    }
+
+    public function dataApplyQuery(): iterable
+    {
+        yield ['eq', "SELECT 1 FROM leads l WHERE EXISTS(SELECT null FROM leads queryAlias WHERE (l.id = queryAlias.id) AND (EXISTS(SELECT null FROM lead_lists_leads para1 WHERE (queryAlias.id = para1.lead_id) AND ((para1.leadlist_id = %s) AND ((para1.manually_added = 1) OR (para1.manually_removed = ''))))))"];
+        yield ['neq', "SELECT 1 FROM leads l WHERE EXISTS(SELECT null FROM leads queryAlias WHERE (l.id = queryAlias.id) AND (EXISTS(SELECT null FROM lead_lists_leads para1 WHERE (queryAlias.id = para1.lead_id) AND ((para1.leadlist_id = %s) AND ((para1.manually_added = 1) OR (para1.manually_removed = ''))))))"];
+    }
+
+    /**
+     * @dataProvider dataApplyQuery
+     */
+    public function testApplyQuery(string $operator, string $expectedQuery): void
+    {
+        $queryBuilder = new QueryBuilder($this->connectionMock);
+        $queryBuilder->select('1');
+        $queryBuilder->from(MAUTIC_TABLE_PREFIX.'leads', 'l');
+
+        $filter = $this->getContactSegmentFilter($operator, (string) $this->segment->getId());
+
+        $this->randomParameterMock->method('generateRandomParameterName')
+            ->willReturnOnConsecutiveCalls('queryAlias', 'para1', 'para2');
+
+        $this->queryBuilder->applyQuery($queryBuilder, $filter);
+
+        Assert::assertSame(sprintf($expectedQuery, $this->segment->getId()), $queryBuilder->getDebugOutput());
+    }
+
+    private function createNewSegment(): LeadList
+    {
+        $segment = new LeadList();
+        $segment->setName('Test Segment');
+        $segment->setAlias('test_segment');
+        $segment->isPublished(true);
+
+        $this->em->persist($segment);
+        $this->em->flush();
+
+        return $segment;
+    }
+
+    private function getContactSegmentFilter(string $operator, string $parameterValue): ContactSegmentFilter
+    {
+        return new ContactSegmentFilter(
+            new ContactSegmentFilterCrate(
+                [
+                    'object'     => 'lead',
+                    'glue'       => 'and',
+                    'field'      => 'leadlist',
+                    'type'       => 'leadlist',
+                    'operator'   => $operator,
+                    'properties' => [
+                            'filter' => [
+                                    0 => $parameterValue,
+                                ],
+                        ],
+                    'filter' => [
+                            0 => $parameterValue,
+                        ],
+                    'display' => null,
+                ]
+            ),
+            new BaseDecorator(new ContactSegmentFilterOperator(
+                $this->createMock(FilterOperatorProviderInterface::class)
+            )),
+            new TableSchemaColumnsCache($this->createMock(EntityManager::class)),
+            $this->createMock(FilterQueryBuilderInterface::class)
+        );
+    }
+}
