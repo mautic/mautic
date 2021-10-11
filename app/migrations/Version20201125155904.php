38659619b30816e6d6f7e34bb62e4f761410cb14
John Linhart <admin@escope.cz>
2021-10-11 13:39:58 +0200

    Fixing a migration for instances that don't use db prefix

--- app/migrations/Version20201125155904.php
+++ app/migrations/Version20201125155904.php
@@ -29,6 +29,11 @@ final class Version20201125155904 extends AbstractMauticMigration
     public function preUp(Schema $schema): void
     {
         $this->skipIf(
+            empty($this->prefix),
+            'This instance does not use prefix, so this migration does not apply. Skipping the migration'
+        );
+
+        $this->skipIf(
             !$schema->getTable($this->getTableName())->hasIndex(LeadEventLog::INDEX_SEARCH),
             sprintf("Index %s doesn't exists. Skipping the migration", LeadEventLog::INDEX_SEARCH)
         );
