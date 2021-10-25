9ccf8a21ab1b010efe6488f8c312a0c389f0274e
John Linhart <admin@escope.cz>
2021-10-25 13:59:19 +0200

    Removing failing test that belong to a future PR

--- app/bundles/FormBundle/Tests/Controller/FieldControllerFunctionalTest.php
+++ app/bundles/FormBundle/Tests/Controller/FieldControllerFunctionalTest.php
@@ -21,21 +21,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class FieldControllerFunctionalTest extends MauticMysqlTestCase
 {
-    public function testNewEmailFieldFormIsPreMapped()
-    {
-        $this->client->request(
-            Request::METHOD_GET,
-            '/s/forms/field/new?type=email&tmpl=field&formId=temporary_form_hash&inBuilder=1',
-            [],
-            [],
-            $this->createAjaxHeaders()
-        );
-        $clientResponse = $this->client->getResponse();
-        $payload        = json_decode($clientResponse->getContent(), true);
-        Assert::assertSame(Response::HTTP_OK, $clientResponse->getStatusCode());
-        Assert::assertStringContainsString('<option value="email"  selected="selected">', $payload['newContent']);
-    }
-
     public function testNewCaptchaFieldFormCanBeSaved()
     {
         $payload = [
