3a87d64e70b1a7640e511496b9da54bbfa004282
Michal Kurzeja <michal@accesto.com>
2021-11-04 10:04:21 +0100

    Switch to skip empty decrypted keys instead of returning empty array and dropping other keys

--- app/bundles/PluginBundle/Integration/AbstractIntegration.php
+++ app/bundles/PluginBundle/Integration/AbstractIntegration.php
@@ -480,7 +480,7 @@ abstract class AbstractIntegration implements UnifiedIntegrationInterface
         foreach ($keys as $name => $key) {
             $key = $this->encryptionHelper->decrypt($key, $mainDecryptOnly);
             if (false === $key) {
-                return [];
+                continue;
             }
             $decrypted[$name] = $key;
         }
