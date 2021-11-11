73c91f170c636c96c91acad434fbf94f949cb1ab
Saurabh Gupta <saurabh.gupta@acquia.com>
2021-11-11 13:42:40 +0530

    recreate indexes with 191 char limit

--- app/migrations/Version20211020114811.php
+++ app/migrations/Version20211020114811.php
@@ -14,6 +14,26 @@ final class Version20211020114811 extends AbstractMauticMigration
     public function up(Schema $schema): void
     {
         $this->addSql('SET FOREIGN_KEY_CHECKS=0;');
+
+        /*
+         * The index key prefix length limit is 3072 bytes for InnoDB tables.
+         * In utf8mb4, 1 char uses 4 bytes
+         * So if we are creating multiple columns index for 4 columns of varchar(255) then it will take 255*4*4 = 4080. 4080 bytes is more than max allowed limit 3072 bytes.
+         * So it was not allowing to convert charset for particular tables and was showing error "Specified key was too long; max key length is 3072 bytes"
+         */
+
+        $this->addSql(sprintf('DROP INDEX %s on %s', $this->prefix.'company_match', $this->getPrefixedTableName('companies')));
+        $this->addSql(sprintf('CREATE INDEX %s on %s(`companyname`(191),`companycity`(191),`companycountry`(191),`companystate`(191))', $this->prefix.'company_match', $this->getPrefixedTableName('companies')));
+
+        $this->addSql(sprintf('DROP INDEX %s on %s', $this->prefix.'citrix_event_product_name', $this->getPrefixedTableName('plugin_citrix_events')));
+        $this->addSql(sprintf('CREATE INDEX %s on %s(`product`(191),`email`(191),`event_type`,`event_name`(191))', $this->prefix.'citrix_event_product_name', $this->getPrefixedTableName('plugin_citrix_events')));
+
+        $this->addSql(sprintf('DROP INDEX %s on %s', $this->prefix.'integration_object', $this->getPrefixedTableName('sync_object_mapping')));
+        $this->addSql(sprintf('CREATE INDEX %s on %s(`integration`(191),`integration_object_name`(191),`integration_object_id`(191), `integration_reference_id`(191))', $this->prefix.'integration_object', $this->getPrefixedTableName('sync_object_mapping')));
+
+        $this->addSql(sprintf('DROP INDEX %s on %s', $this->prefix.'integration_reference', $this->getPrefixedTableName('sync_object_mapping')));
+        $this->addSql(sprintf('CREATE INDEX %s on %s(`integration`(191),`integration_object_name`(191), `integration_reference_id`(191), `integration_object_id`(191))', $this->prefix.'integration_reference', $this->getPrefixedTableName('sync_object_mapping')));
+
         $tables = $this->getTables();
         foreach ($tables as $table) {
             $tableName = $table['TABLE_NAME'];
