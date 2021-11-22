08ad34337758b3e3ac0c02677285f660541b84f1
Zdeno Kuzmany <zdeno@kuzmany.biz>
2021-11-22 17:12:53 +0100

    Fix unit tests

--- app/bundles/LeadBundle/Tests/EventListener/ImportContactSubscriberTest.php
+++ app/bundles/LeadBundle/Tests/EventListener/ImportContactSubscriberTest.php
@@ -128,7 +128,6 @@ final class ImportContactSubscriberTest extends \PHPUnit\Framework\TestCase
                     'lastActive'     => 'mautic.lead.import.label.lastActive',
                     'dateIdentified' => 'mautic.lead.import.label.dateIdentified',
                     'ip'             => 'mautic.lead.import.label.ip',
-                    'points'         => 'mautic.lead.import.label.points',
                     'stage'          => 'mautic.lead.import.label.stage',
                     'doNotEmail'     => 'mautic.lead.import.label.doNotEmail',
                     'ownerusername'  => 'mautic.lead.import.label.ownerusername',
