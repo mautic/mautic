fcea40ec93627378dadd9c6f279910379dc9f1d9
Dennis Ameling (he/him) <dennis.ameling@leap.ac>
2021-11-25 21:10:07 +0100

    Add composer.json and composer.lock to full package, but not in update package

--- build/package_release.php
+++ build/package_release.php
@@ -156,7 +156,7 @@ echo "Packaging Mautic Full Installation\n";
 system('zip -qr ../packages/'.$appVersion.'.zip . -x@../exclude_files.txt -x@../exclude_files_full.txt');
 
 echo "Packaging Mautic Update Package\n";
-system('zip -qr ../packages/'.$appVersion.'-update.zip -x@../exclude_files.txt -@ < modified_files.txt');
+system('zip -qr ../packages/'.$appVersion.'-update.zip -x@../exclude_files.txt -x@../exclude_files_update.txt -@ < modified_files.txt');
 
 // Write output to file (so that the CI pipeline can add it to the release notes), then output to console
 system('cd ../packages && openssl sha1 '.$appVersion.'.zip > build-sha1-all');
