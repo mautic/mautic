de2bc6326ee08369365a1e5e73c85660f1b25cdc
Rahul Shinde <shinde.r.a@gmail.com>
2021-12-09 16:22:18 +0530

    Initial migration script to map the permissions.

---
+++ app/migrations/Version20211209022550.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright   2021 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        https://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+use Mautic\UserBundle\Entity\Role;
+use Mautic\UserBundle\Model\RoleModel;
+
+final class Version20211209022550 extends AbstractMauticMigration
+{
+    public function postUp(Schema $schema): void
+    {
+        /** @var RoleModel $model */
+        $model = $this->container->get('mautic.model.factory')->getModel('user.role');
+
+        // Get all non admin roles.
+        $roles = $model->getEntities([
+            'orderBy'       => 'r.id',
+            'orderByDir'    => 'ASC',
+            'filter'        => [
+                'where' => [
+                    [
+                        'col'  => 'r.isAdmin',
+                        'expr' => 'eq',
+                        'val'  => 0,
+                    ],
+                ],
+            ],
+        ]);
+
+        /** @var Role $role */
+        foreach ($roles as $role) {
+            $rawPermissions = $role->getRawPermissions();
+            if (empty($rawPermissions)) {
+                continue;
+            }
+
+            $leadPermission = $rawPermissions['lead:leads'] ?? [];
+            $listPermission = $rawPermissions['lead:lists'] ?? [];
+
+            if (empty($leadPermission) && empty($listPermission)) {
+                continue;
+            }
+
+            // Map all leads permission to list.
+            // @todo, we need to improvise the mapping here.
+            $rawPermissions['lead:lists'] = $leadPermission;
+
+            $model->setRolePermissions($role, $rawPermissions);
+            $model->saveEntity($role);
+        }
+    }
+}
