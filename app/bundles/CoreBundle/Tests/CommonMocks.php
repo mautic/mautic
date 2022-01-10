894979fa7de1d20e941941e0902eff2ed5aa80f8
John Linhart <admin@escope.cz>
2022-01-10 12:17:16 +0100

    Removing double semicolons

--- app/bundles/CoreBundle/Tests/CommonMocks.php
+++ app/bundles/CoreBundle/Tests/CommonMocks.php
@@ -55,7 +55,7 @@ abstract class CommonMocks extends \PHPUnit\Framework\TestCase
      */
     protected function getCoreParametersHelperMock()
     {
-        return $this->createMock(CoreParametersHelper::class);;
+        return $this->createMock(CoreParametersHelper::class);
     }
 
     /**
