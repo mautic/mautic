23fe10284af4a4dfedb26789086e450da0ea5050
John Linhart <admin@escope.cz>
2022-01-20 14:43:26 +0100

    Finishing up the test

--- app/bundles/CampaignBundle/Tests/Controller/SourceControllerTest.php
+++ app/bundles/CampaignBundle/Tests/Controller/SourceControllerTest.php
@@ -7,18 +7,18 @@ namespace Mautic\CampaignBundle\Tests\Controller;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use Mautic\FormBundle\Entity\Form;
 
-use function GuzzleHttp\json_decode;
-
-class SourceControllerTest extends AbstractCampaignTest
+class SourceControllerTest extends MauticMysqlTestCase
 {
     public function testTwoSourcesWithSameName(): void
     {
         $form1 = new Form();
         $form1->setName('test');
+        $form1->setAlias('test');
         $form1->setFormType('campaign');
 
         $form2 = new Form();
         $form2->setName('test');
+        $form2->setAlias('test');
         $form2->setFormType('campaign');
 
         $this->em->persist($form1);
@@ -27,12 +27,13 @@ class SourceControllerTest extends AbstractCampaignTest
         $this->em->flush();
         $this->em->clear();
 
-        $this->client->request('GET', '/s/campaigns/sources/new/random_object_id?sourceType=forms');
-        $clientResponse = $this->client->getResponse();
-        $this->assertSame(200, $clientResponse->getStatusCode(), $this->client->getResponse()->getContent());
+        $this->client->request('GET', '/s/campaigns/sources/new/random_object_id?sourceType=forms', [], [], $this->createAjaxHeaders());
+        $clientResponse  = $this->client->getResponse();
+        $responseContent = $clientResponse->getContent();
+        $this->assertSame(200, $clientResponse->getStatusCode(), $responseContent);
 
-        $html = json_decode($clientResponse->getContent(), true)['newContent'];
-        $this->assertStringContainsString("<option value=\"{$form1->getId()}\">test</option></select>", $html);
-        $this->assertStringContainsString("<option value=\"{$form2->getId()}\">test</option></select>", $html);
+        $html = json_decode($responseContent, true)['newContent'];
+        $this->assertStringContainsString("<option value=\"{$form1->getId()}\" >test ({$form1->getId()})</option>", $html);
+        $this->assertStringContainsString("<option value=\"{$form2->getId()}\" >test ({$form2->getId()})</option>", $html);
     }
 }
