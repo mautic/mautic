559a1c4e2bd7b377e6b822100fda8628624ce070
Rahul Shinde <shinde.r.a@gmail.com>
2022-01-27 19:06:21 +0530

    Updated permission for clone operation.

--- app/bundles/LeadBundle/Controller/ListController.php
+++ app/bundles/LeadBundle/Controller/ListController.php
@@ -257,13 +257,6 @@ class ListController extends FormController
 
         try {
             $segment = $this->getSegment($objectId, LeadPermissions::LISTS_VIEW_OWN, LeadPermissions::LISTS_VIEW_OTHER);
-
-            return $this->createSegmentNewResponse(
-                clone $segment,
-                $postActionVars,
-                $this->generateUrl('mautic_segment_action', ['objectAction' => 'clone', 'objectId' => $objectId]),
-                $ignorePost
-            );
         } catch (AccessDeniedException $exception) {
             return $this->accessDenied();
         } catch (EntityNotFoundException $exception) {
@@ -296,16 +289,13 @@ class ListController extends FormController
         try {
             $segment = $this->getSegment($objectId, LeadPermissions::LISTS_EDIT_OWN, LeadPermissions::LISTS_EDIT_OTHER);
 
-            if ($isNew) {
-                $segment->setNew();
-            }
 
             if (!$this->get('mautic.security')->hasEntityAccess(
                 LeadPermissions::LISTS_EDIT_OWN, LeadPermissions::LISTS_EDIT_OTHER, $segment->getCreatedBy()
             )) {
                 return $this->accessDenied();
             }
-
+            
             return $this->createSegmentModifyResponse(
                 $segment,
                 $postActionVars,
