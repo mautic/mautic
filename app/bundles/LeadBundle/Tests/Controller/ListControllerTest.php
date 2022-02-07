2fafd34edb147f03c722fdb25b46b1024c3d97d6
Alfredo Arena <alfredo.arena@ctmobi.it>
2022-02-07 18:16:18 +0100

    test(listcontrollertest): modified a test
    Now test if list contains saved fields

--- app/bundles/LeadBundle/Tests/Controller/ListControllerTest.php
+++ app/bundles/LeadBundle/Tests/Controller/ListControllerTest.php
@@ -3,17 +3,30 @@
 namespace Mautic\LeadBundle\Tests\Controller;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\LeadBundle\Entity\LeadList;
 
 class ListControllerTest extends MauticMysqlTestCase
 {
     /**
-     * Index should return status code 200.
+     * Check if list contains correct values.
      */
-    public function testIndexActionWhenNotFiltered(): void
+    public function testViewList(): void
     {
+        $list = $this->createList();
+        $list->setDateAdded(new \DateTime('2020-02-07 20:29:02'));
+        $list->setDateModified(new \DateTime('2020-03-21 20:29:02'));
+        $list->setCreatedByUser('Test User');
+
+        $this->em->persist($list);
+        $this->em->flush();
+        $this->em->clear();
+
         $this->client->request('GET', '/s/segments');
         $clientResponse = $this->client->getResponse();
         $this->assertSame(200, $clientResponse->getStatusCode(), 'Return code must be 200.');
+        $this->assertStringContainsString('February 7, 2020', $clientResponse->getContent());
+        $this->assertStringContainsString('March 21, 2020', $clientResponse->getContent());
+        $this->assertStringContainsString('Test User', $clientResponse->getContent());
     }
 
     /**
@@ -24,5 +37,18 @@ class ListControllerTest extends MauticMysqlTestCase
         $this->client->request('GET', '/s/segments?search=has%3Aresults&tmpl=list');
         $clientResponse = $this->client->getResponse();
         $this->assertSame(200, $clientResponse->getStatusCode(), 'Return code must be 200.');
+    }
+
+    /**
+     * Create a list.
+     */
+    private function createList(string $suffix = 'A'): LeadList
+    {
+        $list = new LeadList();
+        $list->setName("Segment $suffix");
+        $list->setPublicName("Segment $suffix");
+        $list->setAlias("segment-$suffix");
+
+        return $list;
     }
 }
