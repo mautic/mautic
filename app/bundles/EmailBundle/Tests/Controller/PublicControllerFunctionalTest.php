2af8f0b07e43592453435535f8e330acf41c9cd8
Zdeno Kuzmany <zdeno@kuzmany.biz>
2022-02-07 16:53:21 +0100

    Add more functional tests

--- app/bundles/EmailBundle/Tests/Controller/PublicControllerFunctionalTest.php
+++ app/bundles/EmailBundle/Tests/Controller/PublicControllerFunctionalTest.php
@@ -20,35 +20,107 @@ use Mautic\FormBundle\Entity\Form;
 
 class PublicControllerFunctionalTest extends MauticMysqlTestCase
 {
-    public function testUnsubscribeAction(): void
+    protected function setUp(): void
     {
-        $form     = new Form();
-        $formName = 'Unsubscribe_test_form';
-        $form->setName($formName);
-        $form->setAlias($formName);
-        $this->em->persist($form);
+        $this->configParams['show_contact_preferences'] = 1;
+        parent::setUp();
+    }
+
+    public function testUnsubscribeFormActionWithoutTheme(): void
+    {
+        $form = $this->getForm(null);
+
+        $stat = $this->getStat($form);
+
+        $this->em->flush();
+
+        $crawler = $this->client->request('GET', '/email/unsubscribe/'.$stat->getTrackingHash());
+
+        self::assertStringContainsString('form/submit?formId='.$stat->getEmail()->getUnsubscribeForm()->getId(), $crawler->filter('form')->eq(0)->attr('action'));
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    public function testUnsubscribeFormActionWithThemeWithoutFormSupport(): void
+    {
+        $form = $this->getForm('aurora');
+
+        $stat = $this->getStat($form);
+
+        $this->em->flush();
+
+        $crawler = $this->client->request('GET', '/email/unsubscribe/'.$stat->getTrackingHash());
+
+        self::assertStringContainsString('form/submit?formId='.$stat->getEmail()->getUnsubscribeForm()->getId(), $crawler->filter('form')->eq(0)->attr('action'));
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    public function testUnsubscribeFormActionWithThemeWithFormSupport(): void
+    {
+        $form = $this->getForm('blank');
+
+        $stat = $this->getStat($form);
+
+        $this->em->flush();
+
+        $crawler = $this->client->request('GET', '/email/unsubscribe/'.$stat->getTrackingHash());
+
+        self::assertStringContainsString('form/submit?formId='.$stat->getEmail()->getUnsubscribeForm()->getId(), $crawler->filter('form')->eq(0)->attr('action'));
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    public function testWithoutUnsubscribeFormAction(): void
+    {
+        $this->getForm('blank');
+
+        $stat = $this->getStat();
+
+        $this->em->flush();
+
+        $crawler = $this->client->request('GET', '/email/unsubscribe/'.$stat->getTrackingHash());
+
+        self::assertStringNotContainsString('form/submit?formId=', $crawler->html());
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    /**
+     * @throws \Doctrine\ORM\ORMException
+     */
+    protected function getStat(Form $form = null): Stat
+    {
+        $trackingHash = 'tracking_hash_unsubscribe_form_email';
+        $emailName    = 'Test unsubscribe form email';
 
         $email = new Email();
-        $email->setName('Email');
-        $email->setSubject('EmailSubject');
+        $email->setName($emailName);
+        $email->setSubject($emailName);
         $email->setEmailType('template');
         $email->setUnsubscribeForm($form);
         $this->em->persist($email);
 
         // Create a test email stat.
-        $stat         = new Stat();
-        $trackingHash = 'test_unsubscribe_form_email';
+        $stat = new Stat();
         $stat->setTrackingHash($trackingHash);
         $stat->setEmailAddress('john@doe.email');
         $stat->setDateSent(new \DateTime());
         $stat->setEmail($email);
         $this->em->persist($stat);
 
-        $this->em->flush();
+        return $stat;
+    }
 
-        $crawler = $this->client->request('GET', '/email/unsubscribe/'.$trackingHash);
+    /**
+     * @throws \Doctrine\ORM\ORMException
+     */
+    protected function getForm(?string $formTemplate): Form
+    {
+        $formName = 'unsubscribe_test_form';
 
-        self::assertStringContainsString('form/submit?formId='.$form->getId(), $crawler->filter('form')->eq(0)->attr('action'));
-        $this->assertTrue($this->client->getResponse()->isOk());
+        $form = new Form();
+        $form->setName($formName);
+        $form->setAlias($formName);
+        $form->setTemplate($formTemplate);
+        $this->em->persist($form);
+
+        return $form;
     }
 }
