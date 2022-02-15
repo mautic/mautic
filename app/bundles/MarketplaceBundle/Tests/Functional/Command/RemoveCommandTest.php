8e9760bcea2b630a283471cd24d0fac41b8da27f
Dennis Ameling <dennis@dennisameling.com>
2022-02-15 10:44:43 +0200

    Add more tests

--- app/bundles/MarketplaceBundle/Tests/Functional/Command/RemoveCommandTest.php
+++ app/bundles/MarketplaceBundle/Tests/Functional/Command/RemoveCommandTest.php
@@ -13,24 +13,73 @@ use Psr\Log\LoggerInterface;
 
 final class RemoveCommandTest extends AbstractMauticTestCase
 {
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject&LoggerInterface
+     */
+    private $logger;
+    private string $packageName;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->logger      = $this->createMock(LoggerInterface::class);
+        $this->packageName = 'koco/mautic-recaptcha-bundle';
+    }
+
     public function testRemoveCommand(): void
     {
-        $packageName = 'koco/mautic-recaptcha-bundle';
         $composer    = $this->createMock(ComposerHelper::class);
         $composer->method('remove')
-            ->with($packageName)
+            ->with($this->packageName)
             ->willReturn(new ConsoleOutputModel(0, 'OK'));
         $composer->method('getMauticPluginPackages')
             ->willReturn(['koco/mautic-recaptcha-bundle']);
-        $logger  = $this->createMock(LoggerInterface::class);
-        $command = new RemoveCommand($composer, $logger);
+        $command = new RemoveCommand($composer, $this->logger);
 
         $result = $this->testSymfonyCommand(
             'mautic:marketplace:remove',
-            ['package' => $packageName],
+            ['package' => $this->packageName],
             $command
         );
 
         Assert::assertSame(0, $result->getStatusCode());
+    }
+
+    public function testRemoveCommandWithInvalidPackageType(): void
+    {
+        $composer    = $this->createMock(ComposerHelper::class);
+        $composer->method('remove')
+            ->with($this->packageName)
+            ->willReturn(new ConsoleOutputModel(0, 'OK'));
+        $composer->method('getMauticPluginPackages')
+            ->willReturn([]);
+        $command = new RemoveCommand($composer, $this->logger);
+
+        $result = $this->testSymfonyCommand(
+            'mautic:marketplace:remove',
+            ['package' => $this->packageName],
+            $command
+        );
+
+        Assert::assertSame(1, $result->getStatusCode());
+    }
+
+    public function testRemoveCommandWithComposerError(): void
+    {
+        $composer    = $this->createMock(ComposerHelper::class);
+        $composer->method('remove')
+            ->with($this->packageName)
+            ->willReturn(new ConsoleOutputModel(1, 'Error while removing package'));
+        $composer->method('getMauticPluginPackages')
+            ->willReturn([]);
+        $command = new RemoveCommand($composer, $this->logger);
+
+        $result = $this->testSymfonyCommand(
+            'mautic:marketplace:remove',
+            ['package' => $this->packageName],
+            $command
+        );
+
+        Assert::assertSame(1, $result->getStatusCode());
     }
 }
