b436f56fcf748a1bb024f3576c3bb0153d304430
Mattias Michaux <mattias.michaux@dropsolid.com>
2022-02-17 08:50:23 +0100

    some doctrine orm caching

--- app/config/config_test.php
+++ app/config/config_test.php
@@ -28,6 +28,13 @@ $container->loadFromExtension('framework', [
     'csrf_protection' => [
         'enabled' => true,
     ],
+    'cache' => [
+        'pools' => [
+            'doctrine.system_cache_pool' => [
+                'adapter' => 'cache.adapter.array',
+            ],
+        ],
+    ],
 ]);
 
 $container->setParameter('mautic.famework.csrf_protection', true);
@@ -64,6 +71,20 @@ $container->loadFromExtension('doctrine', [
                     'bit'   => 'string',
                 ],
             ],
+        ],
+    ],
+    'orm' => [
+        'metadata_cache_driver' => [
+            'type' => 'pool',
+            'id' => 'doctrine.system_cache_pool'
+        ],
+        'query_cache_driver' => [
+            'type' => 'pool',
+            'id' => 'doctrine.system_cache_pool'
+        ],
+        'result_cache_driver' => [
+            'type' => 'pool',
+            'id' => 'doctrine.system_cache_pool'
         ],
     ],
 ]);
