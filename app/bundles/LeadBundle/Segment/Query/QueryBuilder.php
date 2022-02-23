293357735704d157eb2d9dedb95a68f4404de8be
John Linhart <admin@escope.cz>
2022-02-23 17:55:54 +0100

    CS fix

--- app/bundles/LeadBundle/Segment/Query/QueryBuilder.php
+++ app/bundles/LeadBundle/Segment/Query/QueryBuilder.php
@@ -1596,7 +1596,7 @@ class QueryBuilder extends \Doctrine\DBAL\Query\QueryBuilder
             if (!is_int($val) and !is_float($val) and !is_array($val)) {
                 $val = "'$val'";
             } elseif (is_array($val)) {
-                if ($this->getParameterType($key) === Connection::PARAM_STR_ARRAY) {
+                if (Connection::PARAM_STR_ARRAY === $this->getParameterType($key)) {
                     $val = array_map(function ($value) {
                         return "'$value'";
                     }, $val);
