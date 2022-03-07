b1a091066915b103f0180b20e19e280d03262476
Zdeno Kuzmany <zdeno@kuzmany.biz>
2022-03-07 12:55:30 +0100

    Display just published fields in form field mapping (#10734)
    * Display just published fields in form field mapping
    
    * Add unit tests
    
    * Unit tests clean up
    
    Co-authored-by: Ruth Cheesley <ruth.cheesley@acquia.com>

--- app/bundles/LeadBundle/Model/FieldModel.php
+++ app/bundles/LeadBundle/Model/FieldModel.php
@@ -921,6 +921,7 @@ class FieldModel extends FormModel
                 'group_label'  => $this->translator->trans('mautic.lead.field.group.'.$contactField['group']),
                 'defaultValue' => $contactField['defaultValue'],
                 'properties'   => $contactField['properties'],
+                'isPublished'  => $contactField['isPublished'],
             ];
         }
 
