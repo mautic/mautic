39954cf86b647e565329f5c9be94ff23fad067c8
John Linhart <admin@escope.cz>
2022-03-16 16:19:13 +0100

    Remove doc blocks TPROD-131 (#10970)
    * Adding new CS Fixer rule to remove doc blocks
    
    * Removing docblocks
    
    See https://mautic.atlassian.net/browse/TPROD-131
    
    * Running the CS fixer again to remove the double new lines
    
    * Runing CS Fixer for the 3rd time
    
    * More CS fixes
    
    * Fixing PHPSTAN issues in anonymous classes as those depend on line number which has changed after removing doc blocks

--- app/middlewares/TrustMiddleware.php
+++ app/middlewares/TrustMiddleware.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * @copyright   20169 Mautic Contributors. All rights reserved
- * @author      Mautic
- *
- * @link        https://mautic.org
- *
- * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- */
-
 namespace Mautic\Middleware;
 
 use Symfony\Component\HttpFoundation\Request;
