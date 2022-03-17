c75adc98ade2e4834bf9750f285cacf2af563938
John Linhart <admin@escope.cz>
2022-03-17 13:28:50 +0100

    Segment query optimizatioin  (#10904)
    * Optimized DoNotContactFilterQueryBuilder
    
    * Optimized addNewContactsRestrictions query
    
    * DoNotContactFilterQueryBuilder test
    
    * ContactSegmentQueryBuilder test
    
    * Fixed failing tests when merged into development branch
    
    * Removed call-time unnecessary parameters
    
    * CS fixes
    
    * Fixing test
    
    The string must have an underscore in it
    
    * Fixing DoNotContactParts for all options
    
    Co-authored-by: fedys <miroslav.fedeles@gmail.com>

--- app/bundles/LeadBundle/Segment/Query/ContactSegmentQueryBuilder.php
+++ app/bundles/LeadBundle/Segment/Query/ContactSegmentQueryBuilder.php
@@ -138,32 +138,24 @@ class ContactSegmentQueryBuilder
      * Restrict the query to NEW members of segment.
      *
      * @param $segmentId
-     * @param $batchRestrictions
      *
      * @return QueryBuilder
      *
      * @throws QueryException
      */
-    public function addNewContactsRestrictions(QueryBuilder $queryBuilder, $segmentId, $batchRestrictions)
+    public function addNewContactsRestrictions(QueryBuilder $queryBuilder, $segmentId)
     {
-        $parts     = $queryBuilder->getQueryParts();
-        $setHaving = (count($parts['groupBy']) || !is_null($parts['having']));
+        $expr               = $queryBuilder->expr();
+        $tableAlias         = $this->generateRandomParameterName();
+        $segmentIdParameter = ":{$tableAlias}segmentId";
 
-        $tableAlias = $this->generateRandomParameterName();
-        $queryBuilder->leftJoin('l', MAUTIC_TABLE_PREFIX.'lead_lists_leads', $tableAlias, $tableAlias.'.lead_id = l.id');
-        $queryBuilder->addSelect($tableAlias.'.lead_id AS '.$tableAlias.'_lead_id');
+        $segmentQueryBuilder = $queryBuilder->createQueryBuilder()
+            ->select($tableAlias.'.lead_id')
+            ->from(MAUTIC_TABLE_PREFIX.'lead_lists_leads', $tableAlias)
+            ->andWhere($expr->eq($tableAlias.'.leadlist_id', $segmentIdParameter));
 
-        $expression = $queryBuilder->expr()->eq($tableAlias.'.leadlist_id', $segmentId);
-
-        $queryBuilder->addJoinCondition($tableAlias, $expression);
-
-        if ($setHaving) {
-            $restrictionExpression = $queryBuilder->expr()->isNull($tableAlias.'_lead_id');
-            $queryBuilder->andHaving($restrictionExpression);
-        } else {
-            $restrictionExpression = $queryBuilder->expr()->isNull($tableAlias.'.lead_id');
-            $queryBuilder->andWhere($restrictionExpression);
-        }
+        $queryBuilder->setParameter($segmentIdParameter, $segmentId);
+        $queryBuilder->andWhere($expr->notIn('l.id', $segmentQueryBuilder->getSQL()));
 
         return $queryBuilder;
     }
