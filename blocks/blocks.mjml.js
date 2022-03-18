3c2273783662ee8695ee2b0aa6027995df5e30e6
Volha Pivavarchyk <volha@aivie.ch>
2022-03-18 11:36:52 +0300

    Fix issues

--- blocks/blocks.mjml.js
+++ blocks/blocks.mjml.js
@@ -9,28 +9,28 @@ export default class BlocksMjml {
   }
 
   addBlocks() {
+    const sections37 = `<mj-column width="30%"><mj-text>Content 1</mj-text></mj-column>
+        <mj-column width="70%"><mj-text>Content 2</mj-text></mj-column>`;
+
     this.blockManager.add('mj-37-columns', {
       label: Mautic.translate('grapesjsbuilder.components.names.twoColumnThirdSevens'),
       category: Mautic.translate('grapesjsbuilder.categorySectionLabel'),
       attributes: { class: 'gjs-fonts gjs-f-b37' },
-      content: `<mj-section>
-        <mj-column width="30%"><mj-text>Content 1</mj-text></mj-column>
-        <mj-column width="70%"><mj-text>Content 2</mj-text></mj-column>
-      </mj-section>`,
+      content: `<mj-section>${sections37}</mj-section>`,
     });
+
+    const textSect = `<mj-column>
+          <mj-text>
+            <h1>Content 1</h1>
+            <p>Content 2</p>
+          </mj-text>
+        </mj-column>`;
 
     this.blockManager.add('text-sect', {
       label: Mautic.translate('grapesjsbuilder.components.names.textSectionBlkLabel'),
       category: Mautic.translate('grapesjsbuilder.reusableDynamicContentBlockLabel'),
       attributes: { class: 'gjs-fonts gjs-f-h1p' },
-      content: `<mj-section>
-        <mj-column>
-          <mj-text>
-            <h1>Content 1</h1>
-            <p>Content 2</p>
-          </mj-text>
-        </mj-column>
-      </mj-section>`,
+      content: `<mj-section>${textSect}</mj-section>`,
     });
 
     const gridItem = `<mj-group>
@@ -54,11 +54,11 @@ export default class BlocksMjml {
       label: Mautic.translate('grapesjsbuilder.components.names.gridItemsBlkLabel'),
       category: Mautic.translate('grapesjsbuilder.reusableDynamicContentBlockLabel'),
       attributes: { class: 'fa fa-th' },
-      content: `<section>${gridItem}</section>`,
+      content: `<mj-section>${gridItem}</mj-section>`,
     });
 
     const listItem = `<mj-group>
-        <mj-column width="30%" height="100%">
+        <mj-column width="30%">
           <mj-image src="" height="100%"></mj-image>
         </mj-column>
         <mj-column width="70%">
@@ -73,7 +73,7 @@ export default class BlocksMjml {
       label: Mautic.translate('grapesjsbuilder.components.names.listItemsBlkLabel'),
       category: Mautic.translate('grapesjsbuilder.reusableDynamicContentBlockLabel'),
       attributes: { class: 'fa fa-th-list' },
-      content: `<section>${listItem}</section>`,
+      content: `<mj-section>${listItem}</mj-section>`,
     });
   }
 }
