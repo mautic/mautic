a620762d02539d8f973fb2dfece231c9608bf566
John Linhart <admin@escope.cz>
2022-03-25 16:04:00 +0100

    Segment filter 'Asset Downloads' not showing all assets (#11002)
    * MAUT-3795: Segment filter 'Asset Downloads' not showing all assets
    
    * MAUT-3795: Keep existing filter data for asset downloads after ajax search
    
    * MAUT-3795: Fixing submit issue for asset downloads filter
    
    * STAN fixes
    
    * There is no request service in AssetModel
    
    Co-authored-by: Liju P M <liju.pm@acquia.com>

--- app/bundles/LeadBundle/Assets/js/lead.js
+++ app/bundles/LeadBundle/Assets/js/lead.js
@@ -549,7 +549,7 @@ Mautic.activateSegmentFilterTypeahead = function(displayId, filterId, fieldOptio
     mQuery = mQueryBackup;
 };
 
-Mautic.loadFilterForm = function(filterNum, fieldObject, fieldAlias, operator, resultHtml) {
+Mautic.loadFilterForm = function(filterNum, fieldObject, fieldAlias, operator, resultHtml, search = null) {
     mQuery.ajax({
         showLoadingBar: true,
         url: mauticAjaxUrl,
@@ -560,11 +560,15 @@ Mautic.loadFilterForm = function(filterNum, fieldObject, fieldAlias, operator, r
             fieldObject: fieldObject,
             operator: operator,
             filterNum: filterNum,
+            search: search,
         },
         dataType: 'json',
         success: function (response) {
             Mautic.stopPageLoadingBar();
             resultHtml(response.viewParameters.form);
+            if (fieldAlias == 'lead_asset_download') {
+                Mautic.handleAssetDownloadSearch(filterNum, fieldObject, fieldAlias, operator, resultHtml, search);
+            }
         },
         error: function (request, textStatus, errorThrown) {
             Mautic.processAjaxError(request, textStatus, errorThrown);
@@ -1455,3 +1459,29 @@ Mautic.setAsPrimaryCompany = function (companyId,leadId){
         }
     });
 };
+
+Mautic.handleAssetDownloadSearch = function(filterNum, fieldObject, fieldAlias, operator, resultHtml, search) {
+    var assetDownloadFilter = mQuery('#leadlist_filters_' + filterNum + '_properties_filter');
+    var assetDownloadInput = mQuery('#leadlist_filters_' + filterNum + '_properties input');
+    var assetDownloadProperties = mQuery('#leadlist_filters_' + filterNum + '_properties');
+    assetDownloadFilter.on('chosen:no_results', function () {
+        var search = assetDownloadInput.val();
+        mQuery('#leadlist_filters_' + filterNum + '_properties .chosen-drop').remove();
+        clearTimeout(mQuery.data(this, 'timer'));
+        var existingOptions = mQuery('#leadlist_filters_' + filterNum + '_properties_filter option');
+        mQuery(assetDownloadProperties).data('existing-options', existingOptions);
+        mQuery(this).data('timer', setTimeout(function () {
+            assetDownloadInput.width('auto').prop('disabled', true).val(Mautic.translate('mautic.core.lookup.loading_data'));
+            Mautic.loadFilterForm(filterNum, fieldObject, fieldAlias, operator, resultHtml, search)
+        }, 1000, search))
+    });
+    var existingOptions = mQuery(assetDownloadProperties).data('existing-options');
+    assetDownloadFilter.append(existingOptions);
+    assetDownloadFilter.trigger('chosen:updated');
+    if (mQuery('#leadlist_filters_' + filterNum + '_properties_filter option').length === 0 ) {
+        assetDownloadInput.val(mauticLang['chosenNoResults']);
+    }
+    else if (search !== null) {
+        assetDownloadFilter.trigger('chosen:open.chosen')
+    }
+}
