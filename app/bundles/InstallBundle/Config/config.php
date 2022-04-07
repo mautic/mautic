c907b511419fdc899f74f947e85dbe885ab50fb2
Mattias Michaux <mattias.michaux@dropsolid.com>
2022-04-07 15:38:38 +0200

    Move removal of duplicate index from fixture to Doctrine event listener

--- app/bundles/InstallBundle/Config/config.php
+++ app/bundles/InstallBundle/Config/config.php
@@ -46,11 +46,6 @@ return [
                 'tag'       => \Doctrine\Bundle\FixturesBundle\DependencyInjection\CompilerPass\FixturesCompilerPass::FIXTURE_TAG,
                 'arguments' => [],
             ],
-             'mautic.install.fixture.remove_duplicate_index' => [
-                 'class'     => \Mautic\InstallBundle\InstallFixtures\ORM\RemoveDuplicateIndexData::class,
-                 'tag'       => \Doctrine\Bundle\FixturesBundle\DependencyInjection\CompilerPass\FixturesCompilerPass::FIXTURE_TAG,
-                 'arguments' => [],
-             ],
             'mautic.install.fixture.grape_js' => [
                 'class'     => \Mautic\InstallBundle\InstallFixtures\ORM\GrapesJsData::class,
                 'tag'       => \Doctrine\Bundle\FixturesBundle\DependencyInjection\CompilerPass\FixturesCompilerPass::FIXTURE_TAG,
