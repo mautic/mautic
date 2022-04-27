8373b7b1721f3b1cbc674e5bde3d23421c0ae080
John Linhart <admin@escope.cz>
2022-04-27 11:03:45 +0200

    Code missing from the merge added

--- app/bundles/LeadBundle/Segment/Query/Filter/SegmentReferenceFilterQueryBuilder.php
+++ app/bundles/LeadBundle/Segment/Query/Filter/SegmentReferenceFilterQueryBuilder.php
@@ -111,7 +111,11 @@ class SegmentReferenceFilterQueryBuilder extends BaseFilterQueryBuilder
                 $expression = $queryBuilder->expr()->exists($segmentQueryBuilder->getSQL());
             }
 
-            $logic[] = $expression;
+            if (!$exclusion && count($segmentIds) > 1) {
+                $orLogic[] = $expression;
+            } else {
+                $queryBuilder->addLogic($expression, $filter->getGlue());
+            }
         }
 
         if (count($orLogic)) {
