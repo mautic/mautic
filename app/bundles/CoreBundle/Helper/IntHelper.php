650d13077dcd71f1d8889425259bcc3c8fbcd569
Aarohi Prasad <aarohi.prasad@acquia.com>
2022-05-05 00:21:43 +0530

    removed unnecessary function

--- app/bundles/CoreBundle/Helper/IntHelper.php
+++ app/bundles/CoreBundle/Helper/IntHelper.php
@@ -6,14 +6,4 @@ class IntHelper
 {
     public const MIN_INTEGER_VALUE = -2147483648;
     public const MAX_INTEGER_VALUE = 2147483647;
-
-    public function getMaxIntValue(): int
-    {
-        return self::MAX_INTEGER_VALUE;
-    }
-
-    public function getMinIntValue(): int
-    {
-        return self::MIN_INTEGER_VALUE;
-    }
 }
