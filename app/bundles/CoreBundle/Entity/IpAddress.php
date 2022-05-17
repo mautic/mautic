362eb2e190778f29df8805ad32269bc65a5662ac
John Linhart <jan@linhart.email>
2022-05-17 09:23:37 +0200

    typo fix

--- app/bundles/CoreBundle/Entity/IpAddress.php
+++ app/bundles/CoreBundle/Entity/IpAddress.php
@@ -129,7 +129,7 @@ class IpAddress
      *
      * @return IpAddress
      */
-    public function setIpDetails(ipDetails)
+    public function setIpDetails($ipDetails)
     {
         $this->ipDetails = $ipDetails;
 
