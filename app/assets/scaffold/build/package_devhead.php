287872b1a72cc1d16bf0a30ca220c0373dbea435
Mattias Michaux <mattias.michaux@dropsolid.com>
2022-05-18 09:52:04 +0200

    CS remove obsolete docblock

--- app/assets/scaffold/build/package_devhead.php
+++ app/assets/scaffold/build/package_devhead.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * @copyright   2014 Mautic Contributors. All rights reserved
- * @author      Mautic
- *
- * @link        http://mautic.org
- *
- * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- */
 
 /*
  * Build a "production" package from the current development HEAD, this should be run after a 'composer install --no-dev --no-scripts --optimize-autoloader'
