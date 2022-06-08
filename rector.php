3241b6f71db12303a2957a9305c15b7ac5210979
John Linhart <admin@escope.cz>
2022-06-08 17:31:47 +0200

    CS Fix

--- rector.php
+++ rector.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-return static function (\Rector\Config\RectorConfig $rectorConfig): void {
+return static function (Rector\Config\RectorConfig $rectorConfig): void {
     $rectorConfig->paths([__DIR__.'/app/bundles', __DIR__.'/plugins']);
     $rectorConfig->skip(
         [
