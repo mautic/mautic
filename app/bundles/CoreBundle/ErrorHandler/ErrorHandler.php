47df66396e32c3fcc71615489d48391ca72f0a52
John Linhart <admin@escope.cz>
2022-06-09 11:05:13 +0200

    CS fixes

--- app/bundles/CoreBundle/ErrorHandler/ErrorHandler.php
+++ app/bundles/CoreBundle/ErrorHandler/ErrorHandler.php
@@ -42,9 +42,6 @@ namespace Mautic\CoreBundle\ErrorHandler {
          */
         private static $root;
 
-        /**
-         * ErrorHandler constructor.
-         */
         public function __construct()
         {
             self::$root = realpath(__DIR__.'/../../../../');
@@ -422,8 +419,9 @@ namespace Mautic\CoreBundle\ErrorHandler {
         }
 
         /**
-         * @param      $error
-         * @param bool $inTemplate
+         * @param mixed[] $error
+         * @param bool    $inTemplate
+         *
          * @return mixed|string
          */
         private function generateResponse($error, $inTemplate = false)
