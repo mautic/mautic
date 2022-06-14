fd3cfc5d65d8c29f4a7d111e3f7fb1e6f34fd277
Zdeno Kuzmany <zdeno@kuzmany.biz>
2022-06-14 15:22:00 +0200

    Fix for tags empty/notEmpty segment filter

--- app/bundles/LeadBundle/Segment/Query/Filter/ForeignValueFilterQueryBuilder.php
+++ app/bundles/LeadBundle/Segment/Query/Filter/ForeignValueFilterQueryBuilder.php
@@ -41,15 +41,16 @@ class ForeignValueFilterQueryBuilder extends BaseFilterQueryBuilder
         switch ($filterOperator) {
             case 'empty':
                 $subQueryBuilder->select($tableAlias.'.lead_id')
-                    ->from($filter->getTable(), $tableAlias)
-                    ->andWhere($subQueryBuilder->expr()->isNull($tableAlias.'.'.$filter->getField()));
-                $queryBuilder->addLogic($queryBuilder->expr()->in($leadsTableAlias.'.id', $subQueryBuilder->getSQL()), $filter->getGlue());
+                    ->from($filter->getTable(), $tableAlias);
+                $queryBuilder->addLogic($queryBuilder->expr()->notIn($leadsTableAlias.'.id', $subQueryBuilder->getSQL()), $filter->getGlue());
                 break;
             case 'notEmpty':
                 $subQueryBuilder->select($tableAlias.'.lead_id')
-                    ->from($filter->getTable(), $tableAlias)
-                    ->andWhere($subQueryBuilder->expr()->isNotNull($tableAlias.'.'.$filter->getField()));
-                $queryBuilder->addLogic($queryBuilder->expr()->in($leadsTableAlias.'.id', $subQueryBuilder->getSQL()), $filter->getGlue());
+                    ->from($filter->getTable(), $tableAlias);
+                $queryBuilder->addLogic(
+                    $queryBuilder->expr()->in($leadsTableAlias.'.id', $subQueryBuilder->getSQL()),
+                    $filter->getGlue()
+                );
                 break;
             case 'notIn':
                 $subQueryBuilder
