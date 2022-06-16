f8758c8f1dd5def9a1b42ca9dc5f39f2802f390e
Zdeno Kuzmany <zdeno@kuzmany.biz>
2022-06-16 16:34:36 +0200

    Add testAddContactsErrorMessageForEmailWithTwoDots

--- app/bundles/LeadBundle/Tests/Controller/LeadControllerTest.php
+++ app/bundles/LeadBundle/Tests/Controller/LeadControllerTest.php
@@ -412,6 +412,22 @@ class LeadControllerTest extends MauticMysqlTestCase
         $this->assertStringContainsString('firstname: This field is required.', $clientResponse->getContent());
     }
 
+    public function testAddContactsErrorMessageForEmailWithTwoDots(): void
+    {
+        $crawler = $this->client->request('GET', 's/contacts/new/');
+        $form    = $crawler->filterXPath('//form[@name="lead"]')->form();
+        $form->setValues(
+            [
+                'email' => 'john..doe@email.com',
+            ]
+        );
+
+        $this->client->submit($form);
+        $clientResponse = $this->client->getResponse();
+
+        $this->assertStringContainsString('email: john..doe@email.com is invalid.', $clientResponse->getContent());
+    }
+
     public function testCompanyIdSearchCommand(): void
     {
         $contactA = $this->createContact('contact@a.email');
