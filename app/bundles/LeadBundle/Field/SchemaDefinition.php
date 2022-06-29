7b42de72ad7bdb07911f82c596277a4ef888ce10
fedys <miroslav.fedeles@gmail.com>
2022-06-29 16:04:47 +0200

    SchemaDefinition updated

--- app/bundles/LeadBundle/Field/SchemaDefinition.php
+++ app/bundles/LeadBundle/Field/SchemaDefinition.php
@@ -9,11 +9,17 @@ use Mautic\CoreBundle\Doctrine\Mapping\ClassMetadataBuilder;
 class SchemaDefinition
 {
     /**
+     * Max length of VARCHAR fields.
+     * Fields: charLengthLimit.
+     */
+    public const MAX_VARCHAR_LENGTH = 191;
+
+    /**
      * Get the MySQL database type based on the field type
      * Use a static function so that it's accessible from DoctrineSubscriber
      * without causing a circular service injection error.
      */
-    public static function getSchemaDefinition(string $alias, string $type, bool $isUnique = false): array
+    public static function getSchemaDefinition(string $alias, string $type, bool $isUnique = false, int $length = null): array
     {
         $options = ['notnull' => false];
 
@@ -25,7 +31,7 @@ class SchemaDefinition
                 'options' => $options,
             ];
         }
-        $schemaLength = null;
+
         switch ($type) {
             case 'datetime':
             case 'date':
@@ -45,20 +51,19 @@ class SchemaDefinition
             case 'region':
             case 'tel':
                 $schemaType        = 'string';
-                $options['length'] = $length;
+                $options['length'] = $length ?: self::MAX_VARCHAR_LENGTH;
                 break;
             case 'text':
                 $schemaType        = (false !== strpos($alias, 'description')) ? 'text' : 'string';
                 $options['length'] = $length;
                 break;
             case 'multiselect':
+                $schemaType        = 'text';
+                $options['length'] = 65535;
+                break;
             case 'html':
             default:
                 $schemaType = 'text';
-        }
-
-        if ('string' === $schemaType) {
-            $schemaLength = 191;
         }
 
         return [
@@ -87,7 +92,7 @@ class SchemaDefinition
     /**
      * Get the MySQL database type based on the field type.
      */
-    public function getSchemaDefinitionNonStatic(string $alias, string $type, bool $isUnique = false, $length = null)
+    public function getSchemaDefinitionNonStatic(string $alias, string $type, bool $isUnique = false, int $length = null): array
     {
         return self::getSchemaDefinition($alias, $type, $isUnique, $length);
     }
