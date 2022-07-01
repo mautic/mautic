f193b2d3cb36e7c0fc5503a0c6193583e54e9ab4
Aarohi Prasad <aarohi.prasad@acquia.com>
2022-07-01 13:41:43 +0530

    button disabled test complete

--- app/bundles/PointBundle/Tests/Functional/EmailTriggerTest.php
+++ app/bundles/PointBundle/Tests/Functional/EmailTriggerTest.php
@@ -7,17 +7,14 @@ namespace Mautic\PointBundle\Tests\Functional\EmailTriggerTest;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use Mautic\EmailBundle\Entity\Email;
 use Mautic\PointBundle\Entity\Trigger;
+use Mautic\PointBundle\Entity\TriggerEvent;
 use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\Form;
 use Symfony\Component\HttpFoundation\Request;
 
 class EmailTriggerTest extends MauticMysqlTestCase
 {
-    /**
-     * @var Email
-     */
-    private $email;
-
-    public function testPreviewSendEmailToUser()
+    public function testButtonsAreEnabledOnEditSendEmailToUserWhenEmailIsSelected(): void
     {
         $email = new Email();
         $email->setName('Some name');
@@ -27,33 +24,73 @@ class EmailTriggerTest extends MauticMysqlTestCase
         $this->em->persist($email);
         $this->em->flush();
 
+        $trigger      = $this->createTrigger();
+        $triggerEvent = $this->createTriggerEvent($trigger);
+        $triggerEvent->setProperties(['useremail' => ['email' => $email->getId()]]);
+
+        $this->em->flush();
+        $this->em->clear();
+
+        [$crawler, $form] = $this->fetchForm($trigger, $triggerEvent);
+
+        self::assertEquals($email->getId(), $form->get('pointtriggerevent[properties][useremail][email]')->getValue(), 'Current email should be selected.');
+        self::assertNull($crawler->selectButton('Preview')->attr('disabled'), 'Preview button should not be disabled.');
+        self::assertNull($crawler->selectButton('Edit Email')->attr('disabled'), 'Edit Email button should not be disabled.');
+    }
+
+    public function testButtonsAreDisabledWhenEmailIsNotSelected(): void
+    {
+        $trigger      = $this->createTrigger();
+        $triggerEvent = $this->createTriggerEvent($trigger);
+
+        $this->em->flush();
+        $this->em->clear();
+
+        [$crawler, $form] = $this->fetchForm($trigger, $triggerEvent);
+
+        self::assertEmpty($form->get('pointtriggerevent[properties][useremail][email]')->getValue(), 'No email should be selected.');
+        self::assertNotNull($crawler->selectButton('Preview')->attr('disabled'), 'Preview button should be disabled.');
+        self::assertNotNull($crawler->selectButton('Edit Email')->attr('disabled'), 'Edit Email button should be disabled.');
+    }
+
+    /**
+     * @return array{Crawler,Form}
+     */
+    private function fetchForm(Trigger $trigger, TriggerEvent $triggerEvent): array
+    {
+        $this->client->request(Request::METHOD_GET, '/s/points/triggers/edit/'.$trigger->getId());
+        self::assertTrue($this->client->getResponse()->isSuccessful());
+
+        $uri = sprintf('/s/points/triggers/events/edit/%s?triggerId=%s', $triggerEvent->getId(), $trigger->getId());
+        $this->client->request(Request::METHOD_GET, $uri, [], [], $this->createAjaxHeaders());
+        self::assertTrue($this->client->getResponse()->isSuccessful());
+
+        $responseData = json_decode($this->client->getResponse()->getContent(), true);
+        $crawler      = new Crawler($responseData['newContent'], $this->client->getInternalRequest()->getUri());
+
+        $form = $crawler->filterXPath('//form[@name="pointtriggerevent"]')->form();
+
+        return [$crawler, $form];
+    }
+
+    private function createTrigger(): Trigger
+    {
         $trigger = new Trigger();
         $trigger->setName('Email Trigger');
         $this->em->persist($trigger);
 
-        $triggerEvent = new \Mautic\PointBundle\Entity\TriggerEvent();
+        return $trigger;
+    }
+
+    private function createTriggerEvent(Trigger $trigger): TriggerEvent
+    {
+        $triggerEvent = new TriggerEvent();
         $triggerEvent->setTrigger($trigger);
         $triggerEvent->setName('Send email to user');
         $triggerEvent->setType('email.send_to_user');
-        $triggerEvent->setProperties(['useremail' => ['email' => $email->getId()]]);
+        $triggerEvent->setProperties([]);
         $this->em->persist($triggerEvent);
 
-        $this->em->flush();
-
-        $this->em->clear();
-
-        $this->client->request(Request::METHOD_GET, '/s/points/triggers/edit/'.$trigger->getId());
-        self::assertTrue($this->client->getResponse()->isSuccessful());
-
-        $uri     = sprintf('/s/points/triggers/events/edit/%s?triggerId=%s', $triggerEvent->getId(), $trigger->getId());
-        $crawler = $this->client->request(Request::METHOD_GET, $uri, [], [], $this->createAjaxHeaders());
-
-        $responseData = json_decode($this->client->getResponse()->getContent(), true);
-        $crawler      = new Crawler($responseData['newContent'], $this->client->getInternalRequest()->getUri());
-//
-//        $buttonCrawler = $crawler
-//            ->selectImage('Events')
-//            ->selectButton('Add an event')->filter('.dropdown-menu .list-group-item')->attr('href');
-//
+        return $triggerEvent;
     }
 }
