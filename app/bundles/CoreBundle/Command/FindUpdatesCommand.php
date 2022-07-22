1db5b335602fff3e21a32632b7ecdebf06cbc0e7
Artem Lopata <biozshock@gmail.com>
2022-07-22 14:23:50 +0200

    Command mautic:update:find will return exit codes.

--- app/bundles/CoreBundle/Command/FindUpdatesCommand.php
+++ app/bundles/CoreBundle/Command/FindUpdatesCommand.php
@@ -22,6 +22,11 @@ class FindUpdatesCommand extends ContainerAwareCommand
 The <info>%command.name%</info> command checks for updates for the Mautic application.
 
 <info>php %command.full_name%</info>
+
+Return codes:
+0 - An update is available for Mautic.
+1 - Running latest version of Mautic.
+2 - An error occurred while fetching available updates of Mautic.
 EOT
         );
     }
@@ -35,17 +40,24 @@ EOT
         $translator = $this->getContainer()->get('translator');
         $translator->setLocale($this->getContainer()->get('mautic.factory')->getParameter('locale'));
 
+        /** @var \Mautic\CoreBundle\Helper\UpdateHelper $updateHelper */
         $updateHelper = $this->getContainer()->get('mautic.helper.update');
         $updateData   = $updateHelper->fetchData(true);
 
-        if ($updateData['error']) {
+        if (true === $updateData['error']) {
             $output->writeln('<error>'.$translator->trans($updateData['message']).'</error>');
-        } elseif ('mautic.core.updater.running.latest.version' == $updateData['message']) {
-            $output->writeln('<info>'.$translator->trans($updateData['message']).'</info>');
-        } else {
-            $output->writeln($translator->trans($updateData['message'], ['%version%' => $updateData['version'], '%announcement%' => $updateData['announcement']]));
-            $output->writeln($translator->trans('mautic.core.updater.cli.update'));
+
+            return 2;
         }
+
+        if ('mautic.core.updater.running.latest.version' === $updateData['message']) {
+            $output->writeln('<info>'.$translator->trans($updateData['message']).'</info>');
+
+            return 1;
+        }
+
+        $output->writeln($translator->trans($updateData['message'], ['%version%' => $updateData['version'], '%announcement%' => $updateData['announcement']]));
+        $output->writeln($translator->trans('mautic.core.updater.cli.update'));
 
         return 0;
     }
