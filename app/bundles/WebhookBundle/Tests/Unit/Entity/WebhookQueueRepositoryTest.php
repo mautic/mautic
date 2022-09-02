7904324f0a4b8ed9b6025d636e7f155930e5ec87
John Linhart <admin@escope.cz>
2022-09-02 15:15:26 +0200

    Fixing return type

--- app/bundles/WebhookBundle/Tests/Unit/Entity/WebhookQueueRepositoryTest.php
+++ app/bundles/WebhookBundle/Tests/Unit/Entity/WebhookQueueRepositoryTest.php
@@ -40,7 +40,7 @@ class WebhookQueueRepositoryTest extends TestCase
      */
     private $repository;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
