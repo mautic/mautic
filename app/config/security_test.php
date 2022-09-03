332ea8e681493f43b8ba1609c0bbe2e2a31c8b49
Mattias Michaux <mattias.michaux@dropsolid.com>
2022-09-03 22:35:19 +0200

    phpcs fixes

--- app/config/security_test.php
+++ app/config/security_test.php
@@ -16,14 +16,14 @@ $container->loadFromExtension(
   [
     'encoders' => [
       'Symfony\Component\Security\Core\User\User' => [
-        'algorithm'  => 'md5',
+        'algorithm'        => 'md5',
         'encode_as_base64' => false,
-        'iterations' => 0
+        'iterations'       => 0
       ],
       'Mautic\UserBundle\Entity\User' => [
-        'algorithm'  => 'md5',
+        'algorithm'        => 'md5',
         'encode_as_base64' => false,
-        'iterations' => 0
+        'iterations'       => 0
       ],
     ],
   ]
