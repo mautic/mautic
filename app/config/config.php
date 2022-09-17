cdd20142bc2074aa7b8dc212c71fac054d3a965a
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-09-17 05:55:27 +0000

    Fix misprint in config for consumer name

--- app/config/config.php
+++ app/config/config.php
@@ -99,7 +99,7 @@ $container->loadFromExtension('framework', [
 =======
                 'dsn'            => '%env(MAUTIC_MESSENGER_EMAIL_TRANSPORT_DSN)%',
                 'options'        => [
-                    'consumer'    => '%env(MAUTIC_MESSENGER_CONSUMER_NAME)',
+                    'consumer'    => '%env(MAUTIC_MESSENGER_CONSUMER_NAME)%',
                 ],
                 'retry_strategy' => [
                     'max_retries' => $configParameterBag->get('messenger_retry_strategy_max_retries', 3),
