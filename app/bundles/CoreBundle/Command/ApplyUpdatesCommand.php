0f95875946e208b3c60a42a629480bcdf75ad271
fedys <miroslav.fedeles@gmail.com>
2022-09-23 16:14:02 +0200

    PHPStan fixes

--- app/bundles/CoreBundle/Command/ApplyUpdatesCommand.php
+++ app/bundles/CoreBundle/Command/ApplyUpdatesCommand.php
@@ -13,6 +13,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * CLI Command to update the application.
