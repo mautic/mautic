f6130b291b31fc847ca661bfb39fffc64c33a400
Mattias Michaux <mattias.michaux@dropsolid.com>
2022-10-12 08:59:07 +0200

    fix dangerous situation in command

--- app/bundles/EmailBundle/Tests/Command/ProcessEmailQueueCommandTest.php
+++ app/bundles/EmailBundle/Tests/Command/ProcessEmailQueueCommandTest.php
@@ -100,7 +100,7 @@ class ProcessEmailQueueCommandTest extends \PHPUnit\Framework\TestCase
                 ['mautic.mailer_spool_msg_limit']
             )
             ->willReturnOnConsecutiveCalls(
-                true,
+                'file',
                 $tmpSpoolDir,
                 10
             );
