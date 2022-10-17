e7a466ae1dadb7082398731908a143963166c335
John Linhart <admin@escope.cz>
2022-10-17 15:48:34 +0200

    Moving integrations-related DI excludes from global config to IntegrationslBundle

---
+++ app/bundles/IntegrationsBundle/Config/services.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Mautic\CoreBundle\DependencyInjection\MauticCoreExtension;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $configurator) {
+    $services = $configurator->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure()
+        ->public();
+
+    $excludes = [
+        'Helper/FieldFilterHelper.php',
+        'Helper/FieldMergerHelper.php',
+        'Auth/Support/Oauth2/Token',
+        'Sync/DAO',
+        'Sync/Exception',
+        'Sync/SyncProcess/SyncProcess.php',
+        'Integration/IntegrationObject.php',
+    ];
+
+    $services->load('Mautic\\IntegrationsBundle\\', '../')
+        ->exclude('../{'.implode(',', array_merge(MauticCoreExtension::DEFAULT_EXCLUDES, $excludes)).'}');
+};
