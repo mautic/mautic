c7d345bb686c9ddd399a08592b2907a58f1f3423
Zdeno Kuzmany <zdeno@kuzmany.biz>
2022-11-21 20:58:30 +0100

    Fix unit tests + PHP stan

--- app/bundles/CoreBundle/Tests/Unit/Helper/DateTokenHelperTest.php
+++ app/bundles/CoreBundle/Tests/Unit/Helper/DateTokenHelperTest.php
@@ -4,25 +4,25 @@ namespace Mautic\CoreBundle\Tests\Helper;
 
 use Mautic\CoreBundle\Helper\CoreParametersHelper;
 use Mautic\CoreBundle\Helper\DateTokenHelper;
-use Mautic\CoreBundle\ParametersStorage\ParametersStorage;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class DateTokenHelperTest extends \PHPUnit\Framework\TestCase
 {
-    public const DATE_FORMAT = 'F j, Y';
+    public const DATE_FORMAT      = 'F j, Y';
 
-    public const TIME_FORMAT  = 'g:i a';
+    public const TIME_FORMAT      = 'g:i a';
 
     public const DATE_TIME_FORMAT = self::DATE_FORMAT.' '.self::TIME_FORMAT;
 
-    public const TIMEZONE = 'Europe/Paris';
+    public const TIMEZONE        = 'Europe/Paris';
+    public const TIMEZONE_CUSTOM = 'America/Chicago';
 
     /**
      * @dataProvider getContents
      */
-    public function testGetTokens(string $content, array $expected)
+    public function testGetTokens(string $content, array $expected, string $contactTimezone = null): void
     {
-        $coreParametersHelper = new class($this->createMock(ContainerInterface::class), $this->createMock(ParametersStorage::class)) extends CoreParametersHelper {
+        $coreParametersHelper = new class($this->createMock(ContainerInterface::class)) extends CoreParametersHelper {
             public function get($name, $default = null)
             {
                 switch ($name) {
@@ -37,13 +37,13 @@ class DateTokenHelperTest extends \PHPUnit\Framework\TestCase
         };
 
         $dateTokenHelper = new DateTokenHelper($coreParametersHelper);
-        $tokens          = $dateTokenHelper->getTokens($content);
+        $tokens          = $dateTokenHelper->getTokens($content, $contactTimezone);
 
         $this->assertSame($expected, $tokens);
     }
 
     /**
-     * @return iterable<array<string>>
+     * @return iterable<array<int,array<string,string>|string>>
      */
     public function getContents(): iterable
     {
@@ -100,6 +100,15 @@ class DateTokenHelperTest extends \PHPUnit\Framework\TestCase
         yield [
             '<html lang="en"><head><title></title></head><body></body></html>',
             [],
+        ];
+
+        $now = (clone $now)->setTimezone(new \DateTimeZone(self::TIMEZONE_CUSTOM));
+        yield [
+            '<html lang="en"><head><title></title></head><body>{today|'.self::DATE_TIME_FORMAT.'|+1 day}</body></html>',
+            [
+                '{today|'.self::DATE_TIME_FORMAT.'|+1 day}' => $now->modify('+1 day')->format(self::DATE_TIME_FORMAT),
+            ],
+            self::TIMEZONE_CUSTOM,
         ];
     }
 }
