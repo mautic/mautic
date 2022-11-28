60b1e2d460c893b18adec116cef7197eed7a6be0
John Linhart <admin@escope.cz>
2022-11-28 15:35:58 +0100

    Updating Pipedrive tests to use standard MockHandler over a hack

--- app/bundles/CoreBundle/Tests/Functional/Helper/LanguageHelperTest.php
+++ app/bundles/CoreBundle/Tests/Functional/Helper/LanguageHelperTest.php
@@ -18,8 +18,7 @@ final class LanguageHelperTest extends MauticMysqlTestCase
         $languageFiles = $languageHelper->getLanguageFiles();
 
         // As the list depends on linstalled plugins, let's assert only for random files that should exist.
-        Assert::assertStringEndsWith('app/bundles/EmailBundle/Translations/en_US/messages.ini', $languageFiles['EmailBundle'][0]);
-        Assert::assertStringEndsWith('app/bundles/EmailBundle/Translations/en_US/flashes.ini', $languageFiles['EmailBundle'][1]);
-        Assert::assertStringEndsWith('app/bundles/LeadBundle/Translations/en_US/flashes.ini', $languageFiles['LeadBundle'][1]);
+        Assert::assertMatchesRegularExpression('/app\/bundles\/EmailBundle\/Translations\/en_US\/(messages|validators|flashes)\.ini/', $languageFiles['EmailBundle'][0]);
+        Assert::assertMatchesRegularExpression('/app\/bundles\/LeadBundle\/Translations\/en_US\/(messages|validators|flashes)\.ini/', $languageFiles['LeadBundle'][1]);
     }
 }
