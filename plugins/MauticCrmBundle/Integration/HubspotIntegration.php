bec949e45b93b39c5d0275bac79f85999ebb735f
Norman Pracht - Webmecanik <npr@webmecanik.com>
2022-11-30 16:04:20 +0100

    Fix/change hubspot api auth (#11470)
    * Add support for bearer token from private apps
    
    * Fix API  V2 error response
    
    * Fix If keys are load directly from appendForm in integration
    
    * Fix PHP stan
    
    * Remove readonly statement
    
    * Fix PHP Notice - Trying to access array offset on value of type null
    
    Co-authored-by: Zdeno Kuzmany <zdeno@kuzmany.biz>

--- plugins/MauticCrmBundle/Integration/HubspotIntegration.php
+++ plugins/MauticCrmBundle/Integration/HubspotIntegration.php
@@ -23,6 +23,8 @@ use MauticPlugin\MauticCrmBundle\Api\HubspotApi;
 use Monolog\Logger;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Router;
@@ -33,6 +35,8 @@ use Symfony\Contracts\Translation\TranslatorInterface;
  */
 class HubspotIntegration extends CrmAbstractIntegration
 {
+    const ACCESS_KEY = 'accessKey';
+
     /**
      * @var UserHelper
      */
@@ -96,9 +100,7 @@ class HubspotIntegration extends CrmAbstractIntegration
      */
     public function getRequiredKeyFields()
     {
-        return [
-            $this->getApiKey() => 'mautic.hubspot.form.apikey',
-        ];
+        return [];
     }
 
     /**
@@ -128,13 +130,36 @@ class HubspotIntegration extends CrmAbstractIntegration
     }
 
     /**
+     * @param bool $inAuthorization
+     *
+     * @return mixed|string|null
+     */
+    public function getBearerToken($inAuthorization = false)
+    {
+        $tokenData = $this->getKeys();
+
+        return $tokenData[self::ACCESS_KEY] ?? null;
+    }
+
+    /**
+     * @return array<string,bool>
+     */
+    public function getFormSettings()
+    {
+        return [
+            'requires_callback'      => false,
+            'requires_authorization' => false,
+        ];
+    }
+
+    /**
      * {@inheritdoc}
      *
      * @return string
      */
     public function getAuthenticationType()
     {
-        return 'key';
+        return $this->getBearerToken() ? 'oauth2' : 'key';
     }
 
     /**
@@ -295,7 +320,7 @@ class HubspotIntegration extends CrmAbstractIntegration
     {
         $keys = $this->getKeys();
 
-        return isset($keys[$this->getAuthTokenKey()]);
+        return isset($keys[$this->getAuthTokenKey()]) || isset($keys[self::ACCESS_KEY]);
     }
 
     /**
@@ -309,12 +334,40 @@ class HubspotIntegration extends CrmAbstractIntegration
     }
 
     /**
-     * @param \Mautic\PluginBundle\Integration\Form|FormBuilder $builder
-     * @param array                                             $data
-     * @param string                                            $formArea
+     * @param FormBuilder $builder
+     * @param array       $data
+     * @param string      $formArea
      */
     public function appendToForm(&$builder, $data, $formArea)
     {
+        if ('keys' === $formArea) {
+            $builder->add(
+                   self::ACCESS_KEY,
+                   TextType::class,
+                   [
+                       'label'       => 'mautic.hubspot.form.accessKey',
+                       'label_attr'  => ['class' => 'control-label'],
+                       'attr'        => [
+                           'class'    => 'form-control',
+                       ],
+                       'required'    => false,
+                   ]
+               );
+
+            $builder->add(
+                $this->getApiKey(),
+                TextType::class,
+                [
+                    'label'       => 'mautic.hubspot.form.apikey',
+                    'label_attr'  => ['class' => 'control-label'],
+                    'attr'        => [
+                        'class'    => 'form-control',
+                        'readonly' => true,
+                    ],
+                    'required'    => false,
+                ]
+            );
+        }
         if ('features' == $formArea) {
             $builder->add(
                 'objects',
