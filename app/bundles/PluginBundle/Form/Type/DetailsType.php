f7c9a6918a87ef6b269ca94dcbaee5bbcc07930d
Artem Lopata <biozshock@gmail.com>
2022-12-01 23:50:04 +0100

    Tests for #11767.

--- app/bundles/PluginBundle/Form/Type/DetailsType.php
+++ app/bundles/PluginBundle/Form/Type/DetailsType.php
@@ -6,6 +6,7 @@ use Mautic\CoreBundle\Form\Type\FormButtonsType;
 use Mautic\CoreBundle\Form\Type\StandAloneButtonType;
 use Mautic\CoreBundle\Form\Type\YesNoButtonGroupType;
 use Mautic\PluginBundle\Entity\Integration;
+use Mautic\PluginBundle\Integration\AbstractIntegration;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
@@ -26,9 +27,13 @@ class DetailsType extends AbstractType
     {
         $builder->add('isPublished', YesNoButtonGroupType::class);
 
-        $formSettings  = $options['integration_object']->getFormDisplaySettings();
-        $decryptedKeys = $options['integration_object']->decryptApiKeys($options['data']->getApiKeys());
-        $keys          = $options['integration_object']->getRequiredKeyFields();
+        /** @var AbstractIntegration $integrationObject */
+        $integrationObject = $options['integration_object'];
+        /** @var Integration $integration */
+        $integration   = $options['data'];
+        $formSettings  = $integrationObject->getFormDisplaySettings();
+        $decryptedKeys = $integrationObject->decryptApiKeys($integration->getApiKeys());
+        $keys          = $integrationObject->getRequiredKeyFields();
 
         if (!empty($formSettings['hide_keys'])) {
             foreach ($formSettings['hide_keys'] as $key) {
@@ -43,7 +48,7 @@ class DetailsType extends AbstractType
                     'label'              => false,
                     'integration_keys'   => $keys,
                     'data'               => $decryptedKeys,
-                    'integration_object' => $options['integration_object'],
+                    'integration_object' => $integrationObject,
                 ]
             );
 
@@ -68,8 +73,7 @@ class DetailsType extends AbstractType
             );
 
         if (!empty($formSettings['requires_authorization'])) {
-            $disabled = false;
-            $label    = ($options['integration_object']->isAuthorized()) ? 'reauthorize' : 'authorize';
+            $label = ($integrationObject->isAuthorized()) ? 'reauthorize' : 'authorize';
 
             $builder->add(
                     'authButton',
@@ -81,17 +85,17 @@ class DetailsType extends AbstractType
                             'icon'    => 'fa fa-key',
                         ],
                         'label'    => 'mautic.integration.form.'.$label,
-                        'disabled' => $disabled,
+                        'disabled' => false,
                     ]
                 );
         }
 
-        $features = $options['integration_object']->getSupportedFeatures();
-        $tooltips = $options['integration_object']->getSupportedFeatureTooltips();
+        $features = $integrationObject->getSupportedFeatures();
+        $tooltips = $integrationObject->getSupportedFeatureTooltips();
         if (!empty($features)) {
             // Check to see if the integration is a new entry and thus not configured
-            $configured      = null !== $options['data']->getId();
-            $enabledFeatures = $options['data']->getSupportedFeatures();
+            $configured      = null !== $integration->getId();
+            $enabledFeatures = $integration->getSupportedFeatures();
             $data            = ($configured) ? $enabledFeatures : $features;
 
             $choices = [];
@@ -110,15 +114,15 @@ class DetailsType extends AbstractType
                     'label'       => 'mautic.integration.form.features',
                     'required'    => false,
                     'data'        => $data,
-                    'choice_attr' => function ($val, $key, $index) use ($tooltips) {
+                    'choice_attr' => function ($val) use ($tooltips) {
                         if (array_key_exists($val, $tooltips)) {
                             return [
                                 'data-toggle' => 'tooltip',
                                 'title'       => $tooltips[$val],
                             ];
-                        } else {
-                            return [];
                         }
+
+                        return [];
                     },
                 ]
             );
@@ -130,10 +134,10 @@ class DetailsType extends AbstractType
             [
                 'label'              => 'mautic.integration.form.feature.settings',
                 'required'           => true,
-                'data'               => $options['data']->getFeatureSettings(),
+                'data'               => $integration->getFeatureSettings(),
                 'label_attr'         => ['class' => 'control-label'],
                 'integration'        => $options['integration'],
-                'integration_object' => $options['integration_object'],
+                'integration_object' => $integrationObject,
                 'lead_fields'        => $options['lead_fields'],
                 'company_fields'     => $options['company_fields'],
             ]
@@ -149,7 +153,7 @@ class DetailsType extends AbstractType
             $builder->setAction($options['action']);
         }
 
-        $options['integration_object']->modifyForm($builder, $options);
+        $integrationObject->modifyForm($builder, $options);
     }
 
     /**
@@ -164,6 +168,7 @@ class DetailsType extends AbstractType
         );
 
         $resolver->setRequired(['integration', 'integration_object', 'lead_fields', 'company_fields']);
+        $resolver->setAllowedTypes('integration_object', [AbstractIntegration::class]);
     }
 
     /**
