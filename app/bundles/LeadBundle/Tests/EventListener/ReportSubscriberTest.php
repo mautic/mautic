e3a86c9b5547f353def1c089cd8be4e2f9bfc687
Saurabh Gupta <saurabh.gupta@acquia.com>
2022-12-05 17:20:54 +0530

    fix phpstan issue

--- app/bundles/LeadBundle/Tests/EventListener/ReportSubscriberTest.php
+++ app/bundles/LeadBundle/Tests/EventListener/ReportSubscriberTest.php
@@ -388,7 +388,7 @@ class ReportSubscriberTest extends \PHPUnit\Framework\TestCase
     /**
      * @dataProvider eventDataProvider
      */
-    public function testOnReportBuilder($event)
+    public function testOnReportBuilder(string $event):void
     {
         if ('companies' != $event) {
             $this->fieldsBuilderMock->expects($this->once())
