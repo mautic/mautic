a52c950a6f44fc41d872c4a902f4c42fb622fc8e
Volha Pivavarchyk <volha@aivie.ch>
2022-12-14 19:50:28 +0300

    Marketing emails in campaigns do not ignore the DNC setting

--- app/bundles/EmailBundle/EventListener/CampaignSubscriber.php
+++ app/bundles/EmailBundle/EventListener/CampaignSubscriber.php
@@ -270,7 +270,7 @@ class CampaignSubscriber implements EventSubscriberInterface
             'customHeaders'  => [
                 'X-EMAIL-ID' => $emailId,
             ],
-            'ignoreDNC'      => true,
+            'ignoreDNC'      => 'transactional' === $type,
         ];
 
         // Determine if this email is transactional/marketing
