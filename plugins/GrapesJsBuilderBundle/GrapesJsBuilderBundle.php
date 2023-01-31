68ef0b438ff5fe6be591a44f73a941b0835eb228
Zdeno Kuzmany <zdeno@kuzmany.biz>
2023-01-31 05:11:53 +0100

    Revert "Add loader to extension file"
    This reverts commit a74211d2e7cce53ab9ca493e01901a7881468dfb.

--- plugins/GrapesJsBuilderBundle/GrapesJsBuilderBundle.php
+++ plugins/GrapesJsBuilderBundle/GrapesJsBuilderBundle.php
@@ -5,16 +5,7 @@ declare(strict_types=1);
 namespace MauticPlugin\GrapesJsBuilderBundle;
 
 use Mautic\IntegrationsBundle\Bundle\AbstractPluginBundle;
-use Symfony\Component\Routing\Loader\PhpFileLoader;
 
 class GrapesJsBuilderBundle extends AbstractPluginBundle
 {
-    /**
-     * @param mixed[] $configs
-     */
-    public function load(array $configs, ContainerBuilder $container): void
-    {
-        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../Config'));
-        $loader->load('services.php');
-    }
 }
