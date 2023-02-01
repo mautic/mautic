a6b344f53dd0cf3bf31ef9b3ec7b9f1d08652f69
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-02-01 13:49:37 -0500

    cleanup

--- app/bundles/FormBundle/Templating/Twig/Extension/FormFieldExtension.php
+++ app/bundles/FormBundle/Templating/Twig/Extension/FormFieldExtension.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Mautic\FormBundle\Templating\Twig\Extension;
 
 use Mautic\FormBundle\Helper\FormFieldHelper;
-use Mautic\LeadBundle\Helper\FormFieldHelper as LeadFormFieldHelper;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -19,10 +18,6 @@ final class FormFieldExtension extends AbstractExtension
         return [
             new TwigFunction('formFieldParseBooleanList', [FormFieldHelper::class, 'parseBooleanList']),
             new TwigFunction('formFieldParseList', [FormFieldHelper::class, 'parseList']),
-            new TwigFunction('formFieldCountryChoices', [LeadFormFieldHelper::class, 'getCountryChoices']),
-            new TwigFunction('formFieldRegionChoices', [LeadFormFieldHelper::class, 'getRegionChoices']),
-            new TwigFunction('formFieldTimezonesChoices', [LeadFormFieldHelper::class, 'getTimezonesChoices']),
-            new TwigFunction('formFieldLocaleChoices', [LeadFormFieldHelper::class, 'getLocaleChoices']),
         ];
     }
 }
