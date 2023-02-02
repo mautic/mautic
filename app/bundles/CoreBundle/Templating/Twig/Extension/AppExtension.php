fc1b2dbf7ecb4928f75db91e258467476c0698b2
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-02-02 10:36:25 -0500

    fix with translation

--- app/bundles/CoreBundle/Templating/Twig/Extension/AppExtension.php
+++ app/bundles/CoreBundle/Templating/Twig/Extension/AppExtension.php
@@ -5,10 +5,21 @@ declare(strict_types=1);
 namespace Mautic\CoreBundle\Templating\Twig\Extension;
 
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 use Twig\TwigTest;
 
 class AppExtension extends AbstractExtension
 {
+    /**
+     * @return TwigFunction[]
+     */
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('ini_get', fn ($value) => ini_get($value)),
+        ];
+    }
+
     /**
      * @return TwigTest[]
      */
