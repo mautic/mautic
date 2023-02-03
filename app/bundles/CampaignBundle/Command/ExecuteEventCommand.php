3152c667bc9b881d01fe459e48b3c9d42aa2f821
Saurabh Gupta <48244990+dadarya0@users.noreply.github.com>
2023-02-03 13:22:22 +0530

    Update app/bundles/CampaignBundle/Command/ExecuteEventCommand.php
    Co-authored-by: Miroslav Fedeleš <miroslav.fedeles@gmail.com>

--- app/bundles/CampaignBundle/Command/ExecuteEventCommand.php
+++ app/bundles/CampaignBundle/Command/ExecuteEventCommand.php
@@ -35,7 +35,7 @@ class ExecuteEventCommand extends Command
             ->addOption(
                 '--execution-time',
                 null,
-                InputOption::VALUE_OPTIONAL,
+                InputOption::VALUE_REQUIRED,
                 'Scheduled execution time of event log'
             );
 
