de3aa38b111c33a0d7de6503b9741a690404479e
Patryk Gruszka <patryk.gruszka@comarch.pl>
2023-02-08 09:33:35 +0100

    fix: [DPMMA-1132] update migration

--- app/migrations/Version20230103074925.php
+++ app/migrations/Version20230103074925.php
@@ -8,7 +8,6 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\Exception\SkipMigration;
 use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 use Mautic\PointBundle\Entity\League;
-use Mautic\PointBundle\Entity\LeagueCompanyScore;
 use Mautic\PointBundle\Entity\LeagueContactScore;
 
 final class Version20230103074925 extends AbstractMauticMigration
@@ -29,17 +28,12 @@ final class Version20230103074925 extends AbstractMauticMigration
     {
         $leagueTableName             = $this->generateTableName(League::TABLE_NAME);
         $contactScoreTableName       = $this->generateTableName(LeagueContactScore::TABLE_NAME);
-        $companyScoreTableName       = $this->generateTableName(LeagueCompanyScore::TABLE_NAME);
         $contactTableName            = $this->generateTableName('leads');
-        $companyTableName            = $this->generateTableName('companies');
         $pointsTableName             = $this->generateTableName('points');
         $pointTriggersTableName      = $this->generateTableName('point_triggers');
 
         $contactScoreContactFk = $this->generatePropertyName($contactScoreTableName, 'fk', ['contact_id']);
         $contactScoreLeagueFk  = $this->generatePropertyName($contactScoreTableName, 'fk', ['league_id']);
-
-        $companyScoreCompanyFk = $this->generatePropertyName($companyScoreTableName, 'fk', ['company_id']);
-        $companyScoreLeagueFk  = $this->generatePropertyName($companyScoreTableName, 'fk', ['league_id']);
 
         $pointsLeagueFk        = $this->generatePropertyName($pointsTableName, 'fk', ['league_id']);
         $pointTriggersLeagueFk = $this->generatePropertyName($pointTriggersTableName, 'fk', ['league_id']);
@@ -86,35 +80,11 @@ final class Version20230103074925 extends AbstractMauticMigration
   ENGINE = InnoDB
   ROW_FORMAT = DYNAMIC;");
 
-        $this->addSql("CREATE TABLE `{$companyScoreTableName}`
-(
-    `company_id`          INT          NOT NULL,
-    `league_id`           INT UNSIGNED NOT NULL,
-    `is_published`        TINYINT(1)   NOT NULL,
-    `date_added`          DATETIME     DEFAULT NULL,
-    `created_by`          INT          DEFAULT NULL,
-    `created_by_user`     VARCHAR(191) DEFAULT NULL,
-    `date_modified`       DATETIME     DEFAULT NULL,
-    `modified_by`         INT          DEFAULT NULL,
-    `modified_by_user`    VARCHAR(191) DEFAULT NULL,
-    `checked_out`         DATETIME     DEFAULT NULL,
-    `checked_out_by`      INT          DEFAULT NULL,
-    `checked_out_by_user` VARCHAR(191) DEFAULT NULL,
-    `score`               INT          NOT NULL,
-    PRIMARY KEY (`company_id`, `league_id`)
-) DEFAULT CHARACTER SET utf8mb4
-  COLLATE `utf8mb4_unicode_ci`
-  ENGINE = InnoDB
-  ROW_FORMAT = DYNAMIC;");
-
         $this->addSql("ALTER TABLE `{$pointsTableName}` ADD league_id INT UNSIGNED DEFAULT NULL");
         $this->addSql("ALTER TABLE `{$pointTriggersTableName}` ADD league_id INT UNSIGNED DEFAULT NULL");
 
         $this->addSql("ALTER TABLE `{$contactScoreTableName}` ADD CONSTRAINT `{$contactScoreContactFk}` FOREIGN KEY (`contact_id`) REFERENCES `{$contactTableName}` (`id`) ON DELETE CASCADE");
         $this->addSql("ALTER TABLE `{$contactScoreTableName}` ADD CONSTRAINT `{$contactScoreLeagueFk}` FOREIGN KEY (`league_id`) REFERENCES `{$leagueTableName}` (`id`) ON DELETE CASCADE");
-
-        $this->addSql("ALTER TABLE `{$companyScoreTableName}` ADD CONSTRAINT `{$companyScoreCompanyFk}` FOREIGN KEY (`company_id`) REFERENCES `{$companyTableName}` (`id`) ON DELETE CASCADE");
-        $this->addSql("ALTER TABLE `{$companyScoreTableName}` ADD CONSTRAINT `{$companyScoreLeagueFk}` FOREIGN KEY (`league_id`) REFERENCES `{$leagueTableName}` (`id`) ON DELETE CASCADE");
 
         $this->addSql("ALTER TABLE `{$pointsTableName}` ADD CONSTRAINT `{$pointsLeagueFk}` FOREIGN KEY (`league_id`) REFERENCES `{$leagueTableName}` (`id`) ON DELETE CASCADE");
         $this->addSql("ALTER TABLE `{$pointTriggersTableName}` ADD CONSTRAINT `{$pointTriggersLeagueFk}` FOREIGN KEY (`league_id`) REFERENCES `{$leagueTableName}` (`id`) ON DELETE CASCADE");
@@ -124,9 +94,7 @@ final class Version20230103074925 extends AbstractMauticMigration
     {
         $leagueTableName       = $this->generateTableName(League::TABLE_NAME);
         $contactScoreTableName = $this->generateTableName(LeagueContactScore::TABLE_NAME);
-        $companyScoreTableName = $this->generateTableName(LeagueCompanyScore::TABLE_NAME);
 
-        $this->addSql("DROP TABLE {$companyScoreTableName}");
         $this->addSql("DROP TABLE {$contactScoreTableName}");
         $this->addSql("DROP TABLE {$leagueTableName}");
     }
