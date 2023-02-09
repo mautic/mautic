02aaf5a0f3d2e499b2e126a0b63c302b160db1de
Rembrand Le Compte <registrations@rembo.me>
2023-02-09 13:31:17 +0100

    put back the old php files for comparison/completion sake

---
+++ plugins/GrapesJsBuilderBundle/Views/Builder/template.html.php
@@ -0,0 +1,6 @@
+<h1>Test builder template</h1>
+<?php
+    echo json_encode([
+        'templateHtml' => $templateHtml,
+        'templateMjml' => $templateMjml,
+    ]);
---
+++ plugins/GrapesJsBuilderBundle/Views/Setting/vars.html.php
@@ -0,0 +1,1 @@
+<div id="grapesjsbuilder_assets" class="hide" data-assets="<?php echo $dataAssets; ?>" data-upload="<?php echo $dataUpload; ?>" data-delete="<?php echo $dataDelete; ?>"></div>
