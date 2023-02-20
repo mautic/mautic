3532d3326b2565a0d5107e5a18a07f995fc02e03
Rembrand Le Compte <registrations@rembo.me>
2023-02-20 15:14:26 +0100

    add functions to MenuExtension so we can parse attributes on tags

--- app/bundles/CoreBundle/Templating/Twig/Extension/MenuExtension.php
+++ app/bundles/CoreBundle/Templating/Twig/Extension/MenuExtension.php
@@ -24,6 +24,8 @@ class MenuExtension extends AbstractExtension
     {
         return [
             new TwigFunction('menuRender', [$this, 'menuRender'], ['is_safe' => ['all']]),
+            new TwigFunction('parseMenuAttributes', [$this, 'parseMenuAttributes'], ['is_safe' => ['all']]),
+            new TwigFunction('buildMenuClasses', [$this, 'buildMenuClasses'], ['is_safe' => ['all']]),
         ];
     }
 
@@ -36,5 +38,40 @@ class MenuExtension extends AbstractExtension
     public function menuRender($menu, array $options = [], ?string $renderer = null): string
     {
         return $this->menuHelper->render($menu, $options, $renderer);
+    }
+
+    /**
+     * Parses attributes for the menu view.
+     *
+     * @param $attributes
+     * @param $overrides
+     *
+     */
+    public function parseMenuAttributes($attributes, $overrides = []): string
+    {
+        return $this->menuHelper->parseAttributes($attributes, $overrides);
+    }
+
+    /**
+     * Concats the appropriate classes for menu links.
+     *
+     * @param array $options
+     */
+    public function buildMenuClasses($item, $matcher, $options, $extra): array
+    {
+        $isAncestor = $matcher->isAncestor($item, $options['matchingDepth']);
+        $isCurrent  = $matcher->isCurrent($item);
+
+        $class   = $item->getAttribute('class');        
+
+        $classes = ($class) ? " {$class}" : '';
+        $classes .= ($extra) ?  " {$extra}" : '';        
+        $classes .= ($isCurrent) ? " {$options['currentClass']}" : '';
+        $classes .= ($isAncestor) ? " {$options['ancestorClass']}" : '';
+        $classes .= ($isAncestor && $this->invisibleChildSelected($item, $matcher)) ? " {$options['currentClass']}" : '';
+        $classes .= ($item->actsLikeFirst()) ? " {$options['firstClass']}" : '';
+        $classes .= ($item->actsLikeLast()) ? " {$options['lastClass']}" : '';
+
+        return ['class' => trim($classes)];
     }
 }
