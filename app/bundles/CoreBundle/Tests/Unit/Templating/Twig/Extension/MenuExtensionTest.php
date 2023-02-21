fe7057fb63e227266d6931db4e2459be9d5479c3
Rembrand Le Compte <registrations@rembo.me>
2023-02-21 17:33:16 +0100

    start a new file for unit tests for Twig extension MenuExtension

---
+++ app/bundles/CoreBundle/Tests/Unit/Templating/Twig/Extension/MenuExtensionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\CoreBundle\Tests\Unit\Templating\Twig\Extension;
+
+use Mautic\CoreBundle\Templating\Twig\Extension\MenuExtension;
+use Mautic\CoreBundle\Test\AbstractMauticTestCase;
+use PHPUnit\Framework\Assert;
+
+class MenuExtensionTest extends AbstractMauticTestCase
+{
+    public function testParseMenuAttributes(): void
+    {
+        $menuExtension = self::$container->get(MenuExtension::class);
+        \assert($menuExtension instanceof MenuExtension);
+
+        $menuAttributes = [
+            'id' => 'myId',
+            'class' => 'test-a-class test-another-class'
+        ];
+
+        Assert::assertStringStartsWith(' id=', $menuExtension->parseMenuAttributes($menuAttributes));
+        Assert::assertStringContainsString('myId', $menuExtension->parseMenuAttributes($menuAttributes));
+        Assert::assertStringContainsString(' class=', $menuExtension->parseMenuAttributes($menuAttributes));
+        Assert::assertStringContainsString('test-a-class test-another-class', $menuExtension->parseMenuAttributes($menuAttributes));
+    
+    }
+
+}
