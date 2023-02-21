017f62532f8f07560295fffdadfaa8eaa30113cc
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2023-02-21 10:47:36 -0500

    test

---
+++ app/bundles/CoreBundle/Tests/Unit/Templating/Twig/Extension/EmojiExtensionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\CoreBundle\Tests\Unit\Templating\Twig\Extension;
+
+use Mautic\CoreBundle\Helper\EmojiHelper;
+use Mautic\CoreBundle\Templating\Twig\Extension\EmojiExtension;
+use PHPUnit\Framework\TestCase;
+
+final class EmojiExtensionTest extends TestCase
+{
+    public function testItContainsAtLeastOneFilter(): void
+    {
+        $extension = new EmojiExtension(new EmojiHelper());
+
+        $this->assertGreaterThan(0, $extension->getFunctions());
+    }
+
+    public function testToHtml(): void
+    {
+        $extension = new EmojiExtension(new EmojiHelper());
+
+        $text = 'This is example text';
+
+        $this->assertSame($text, $extension->toHtml($text));
+    }
+}
