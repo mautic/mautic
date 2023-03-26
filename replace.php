c3dabfce8db3ae55a1e8beb31b9833b01e5cffa0
Mohammad Abu Musa <m.abumusa@gmail.com>
2023-03-26 11:09:56 +0000

    NotificationBundle

--- replace.php
+++ replace.php
@@ -70,6 +70,6 @@ class finder
     }
 }
 
-$finder  = new finder('/var/www/html/app/bundles/MarketplaceBundle');
+$finder  = new finder('/var/www/html/app/bundles/NotificationBundle');
 $results = $finder->searchAndReplace(false);
 echo $results."\n";
