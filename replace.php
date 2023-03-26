5c1601c366bb40f9b6c096f5338f77f602298d36
Mohammad Abu Musa <m.abumusa@gmail.com>
2023-03-26 11:10:34 +0000

    PageBundle

--- replace.php
+++ replace.php
@@ -70,6 +70,6 @@ class finder
     }
 }
 
-$finder  = new finder('/var/www/html/app/bundles/NotificationBundle');
+$finder  = new finder('/var/www/html/app/bundles/PageBundle');
 $results = $finder->searchAndReplace(false);
 echo $results."\n";
