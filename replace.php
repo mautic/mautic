5238f2dd4e218b5fbdb9e6ccf34c8ddbdb916c44
Mohammad Abu Musa <m.abumusa@gmail.com>
2023-03-26 11:03:02 +0000

    EmailBundle

--- replace.php
+++ replace.php
@@ -70,6 +70,6 @@ class finder
     }
 }
 
-$finder  = new finder('/var/www/html/app/bundles/DynamicContentBundle');
+$finder  = new finder('/var/www/html/app/bundles/EmailBundle');
 $results = $finder->searchAndReplace(false);
 echo $results."\n";
