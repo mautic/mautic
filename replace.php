1985dc7208e1eab89bb6e02b6bca6b092a3140c7
Mohammad Abu Musa <m.abumusa@gmail.com>
2023-03-26 11:14:23 +0000

    UserBundle

--- replace.php
+++ replace.php
@@ -70,6 +70,6 @@ class finder
     }
 }
 
-$finder  = new finder('/var/www/html/app/bundles/StageBundle');
+$finder  = new finder('/var/www/html/app/bundles/UserBundle');
 $results = $finder->searchAndReplace(false);
 echo $results."\n";
