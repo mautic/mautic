f8049e6dbd557d780d5ad054db8b54379f082551
Artem Lopata <biozshock@gmail.com>
2023-03-28 21:03:04 +0200

    GmailBundle autowired.

--- plugins/MauticGmailBundle/DependencyInjection/MauticGmailExtension.php
+++ plugins/MauticGmailBundle/DependencyInjection/MauticGmailExtension.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace MauticPlugin\MauticFullContactBundle\DependencyInjection;
+namespace MauticPlugin\MauticGmailBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
-class MauticGmailBundleExtension extends Extension
+class MauticGmailExtension extends Extension
 {
     /**
      * @param mixed[] $configs
