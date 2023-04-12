62a709411dc27272dd5f0de53b19ec2b560d2e82
Lenon Leite <lenonleite@gmail.com>
2023-04-12 02:06:38 +0100

    Add new config file to environment refactor in index.php to work in developer and production just changing parameters in environment.php

---
+++ app/config/environment.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'debug'            => false,
+    'env'              => 'prod', //dev
+    'dev_ip_whitelist' => [
+        '127.0.0.1',
+        '::1',
+        '172.17.0.1',
+    ],
+];
