0911e533f0fd75c964724933b7c1de1a8acbfd65
fedys <miroslav.fedeles@gmail.com>
2023-04-13 16:30:20 +0200

    Email step removed from the installation controller

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -291,7 +291,7 @@ class AppKernel extends Kernel
         if (null === $this->installed) {
             $localParameters = $this->getParameterLoader()->getLocalParameterBag();
             $dbDriver        = $localParameters->get('db_driver');
-            $mailerFromName  = $localParameters->get('mailer_from_name');
+            $mailerFromName  = $localParameters->get('site_url');
 
             $this->installed = !empty($dbDriver) && !empty($mailerFromName);
         }
