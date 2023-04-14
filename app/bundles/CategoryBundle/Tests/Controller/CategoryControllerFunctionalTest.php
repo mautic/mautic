e284646c0d7d459a561046f788272f59bc6a6def
Zdeno Kuzmany <zdeno@kuzmany.biz>
2023-04-14 11:42:23 +0200

    Fix PHP Stan

--- app/bundles/CategoryBundle/Tests/Controller/CategoryControllerFunctionalTest.php
+++ app/bundles/CategoryBundle/Tests/Controller/CategoryControllerFunctionalTest.php
@@ -5,6 +5,7 @@ namespace Mautic\CategoryBundle\Tests\Controller;
 use Mautic\CategoryBundle\Entity\Category;
 use Mautic\CategoryBundle\Model\CategoryModel;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\UserBundle\Model\UserModel;
 use PHPUnit\Framework\Assert;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -90,18 +91,21 @@ class CategoryControllerFunctionalTest extends MauticMysqlTestCase
         $this->assertArrayHasKey('categoryName', $body);
     }
 
-    public function testEditLockCategory()
+    public function testEditLockCategory(): void
     {
-        /** @var CategoryModel $model */
-        $model      = self::$container->get('mautic.category.model.category');
+        /** @var CategoryModel $categoryModel */
+        $categoryModel      = self::$container->get('mautic.category.model.category');
+        /** @var UserModel $userModel */
+        $userModel      = self::$container->get('mautic.user.model.user');
+        $user           = $userModel->getEntity(2);
 
         $category = new Category();
         $category->setTitle('New Category');
         $category->setAlias('category');
         $category->setBundle('global');
-        $category->setCheckedOutBy(2);
+        $category->setCheckedOutBy($user);
         $category->setCheckedOut(new \DateTime('now'));
-        $model->saveEntity($category, false);
+        $categoryModel->saveEntity($category, false);
 
         $this->client->request(Request::METHOD_GET, 's/categories/category/edit/'.$category->getId());
         $this->assertStringContainsString('is currently checked out by', $this->client->getResponse()->getContent());
