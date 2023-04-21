b97da5ffb2d6f010b5f84eb5b154eac436472d74
Lenon Leite <lenonleite@gmail.com>
2023-04-21 18:51:15 +0100

    default env value with APP_ENV=prod
    remove env.dist
    
    writing real values to env.test based on ddev enviroment
    
    remove part of the script not necessary in mautic-setup
    
    include env value
    
    remove env.dist
    
    copy bin console to scaffold
    
    copy index.php from root folder to scaffold
    
    fixing tests

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -14,12 +14,12 @@ if (is_array($env = @include dirname(__DIR__).'/.env.local.php') && (!isset($env
     (new Dotenv(false))->populate($env);
 } else {
     // load all the .env files
-    (new Dotenv(false))->loadEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env',null,'prod');
 }
 
 $_SERVER += $_ENV;
 $_SERVER['MAUTIC_TABLE_PREFIX']     = $_ENV['MAUTIC_TABLE_PREFIX']     = ($_SERVER['MAUTIC_TABLE_PREFIX'] ?? $_ENV['MAUTIC_TABLE_PREFIX'] ?? null) ?: '';
-$_SERVER['APP_ENV']                 = $_ENV['APP_ENV']                 = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
+$_SERVER['APP_ENV']                 = $_ENV['APP_ENV']                 = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'prod';
 $_SERVER['APP_DEBUG']               = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG']               = $_ENV['APP_DEBUG']               = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
 $_SERVER['IPS_ALLOWED']             = $_ENV['IPS_ALLOWED']             = ($_SERVER['IPS_ALLOWED'] ?? $_ENV['IPS_ALLOWED'] ?? null) ?: '127.0.0.1,::1,172.17.0.1';
