5df8d773d0077644323ff83af6046590f24d906a
Mohammad AbuMusa <m.abumusa@gmail.com>
2023-04-23 14:37:04 +0000

    apply rules

--- app/bundles/LeadBundle/Form/Validator/Constraints/EmailAddress.php
+++ app/bundles/LeadBundle/Form/Validator/Constraints/EmailAddress.php
@@ -6,6 +6,10 @@ use Symfony\Component\Validator\Constraint;
 
 /**
  * @Annotation
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
  */
 class EmailAddress extends Constraint
 {
--- app/bundles/LeadBundle/Form/Validator/Constraints/LeadListAccess.php
+++ app/bundles/LeadBundle/Form/Validator/Constraints/LeadListAccess.php
@@ -6,10 +6,19 @@ use Symfony\Component\Validator\Constraint;
 
 /**
  * @Annotation
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
  */
 class LeadListAccess extends Constraint
 {
     public $message = 'mautic.lead.lists.failed';
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
 
     public function validatedBy()
     {
