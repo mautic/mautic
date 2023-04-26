b187911d6072f434b967c41f157b1c3fc8b01086
DDEV-Local User <nobody@example.com>
2023-04-26 22:47:43 +0000

    run rector again

--- app/bundles/LeadBundle/Validator/Constraints/Length.php
+++ app/bundles/LeadBundle/Validator/Constraints/Length.php
@@ -8,6 +8,9 @@ use Symfony\Component\Validator\Constraints\Length as SymfonyLength;
  * @Annotation
  * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
  * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
+ * @\Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor
  */
 class Length extends SymfonyLength
 {
