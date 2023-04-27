cff39a2ed8462d763fb44cafd6be10d3ff66d87f
John Linhart <admin@escope.cz>
2023-04-27 13:11:08 +0000

    Updating LocalFileAdapterService for Flysystem v3

--- app/bundles/CoreBundle/Service/LocalFileAdapterService.php
+++ app/bundles/CoreBundle/Service/LocalFileAdapterService.php
@@ -4,26 +4,29 @@ declare(strict_types=1);
 
 namespace Mautic\CoreBundle\Service;
 
-use League\Flysystem\Adapter\Local;
+use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
 
-class LocalFileAdapterService extends Local
+class LocalFileAdapterService extends LocalFilesystemAdapter
 {
-    /**
-     * @var array<string, array<string, int>>
-     */
-    protected static $permissions = [
-        'file' => [
-            'public'  => 0666,
-            'private' => 0600,
-        ],
-        'dir'  => [
-            'public'  => 0777,
-            'private' => 0700,
-        ],
-    ];
-
     public function __construct(string $root)
     {
-        parent::__construct($root, LOCK_EX, self::DISALLOW_LINKS, self::$permissions);
+        parent::__construct(
+            $root,
+            PortableVisibilityConverter::fromArray(
+                [
+                    'file' => [
+                        'public'  => 0666,
+                        'private' => 0600,
+                    ],
+                    'dir'  => [
+                        'public'  => 0777,
+                        'private' => 0700,
+                    ],
+                ]
+            ),
+            LOCK_EX,
+            self::DISALLOW_LINKS
+        );
     }
 }
