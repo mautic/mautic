aed31de35c0efe40ae91805862cabf308038fa37
Jan Kozak <galvani78@gmail.com>
2023-05-08 17:07:02 +0200

    add sentry to dev

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -157,6 +157,7 @@ class AppKernel extends Kernel
             new Mautic\UserBundle\MauticUserBundle(),
             new Mautic\WebhookBundle\MauticWebhookBundle(),
             new Mautic\CacheBundle\MauticCacheBundle(),
+            new Sentry\SentryBundle\SentryBundle(),
         ];
 
         $queueProtocol = $this->getParameterLoader()->getLocalParameterBag()->get('queue_protocol', '');
