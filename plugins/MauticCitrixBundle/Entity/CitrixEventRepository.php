8a0bebb9f9e376bd8fe27c152a4abdd4223f101d
Mohammad Abu Musa <m.abumusa@gmail.com>
2023-05-23 19:02:06 +0000

    fix errors

--- plugins/MauticCitrixBundle/Entity/CitrixEventRepository.php
+++ plugins/MauticCitrixBundle/Entity/CitrixEventRepository.php
@@ -111,7 +111,7 @@ class CitrixEventRepository extends CommonRepository
         $q = $this->_em
             ->createQueryBuilder()
             ->select($alias)
-            ->from(\Mautic\CitrixBundle\Entity\CitrixEvent::class, $alias, $alias.'.id');
+            ->from(\MauticPlugin\MauticCitrixBundle\Entity\CitrixEvent::class, $alias, $alias.'.id');
 
         $args['qb'] = $q;
 
--- plugins/MauticFocusBundle/Entity/FocusRepository.php
+++ plugins/MauticFocusBundle/Entity/FocusRepository.php
@@ -30,7 +30,7 @@ class FocusRepository extends CommonRepository
         $q = $this->_em
             ->createQueryBuilder()
             ->select($alias)
-            ->from(\Mautic\FocusBundle\Entity\Focus::class, $alias, $alias.'.id');
+            ->from(\MauticPlugin\MauticFocusBundle\Entity\Focus::class, $alias, $alias.'.id');
 
         if (empty($args['iterator_mode'])) {
             $q->leftJoin($alias.'.category', 'c');
