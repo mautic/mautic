f7debd8cb29156ff74e14df0be29a98ad67cd676
fedys <miroslav.fedeles@gmail.com>
2023-06-01 13:09:17 +0200

    queries brought back for SimplePaginatorTest

--- app/bundles/CoreBundle/Tests/Functional/Doctrine/Paginator/SimplePaginatorTest.php
+++ app/bundles/CoreBundle/Tests/Functional/Doctrine/Paginator/SimplePaginatorTest.php
@@ -7,6 +7,7 @@ namespace Mautic\CoreBundle\Tests\Functional\Doctrine\Paginator;
 use Mautic\CoreBundle\Doctrine\Paginator\SimplePaginator;
 use Mautic\CoreBundle\Entity\IpAddress;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Symfony\Bridge\Doctrine\Middleware\Debug\DebugDataHolder;
 
 class SimplePaginatorTest extends MauticMysqlTestCase
 {
@@ -16,10 +17,17 @@ class SimplePaginatorTest extends MauticMysqlTestCase
      * @var array<string,mixed>
      */
     protected array $clientOptions = ['debug' => true];
+    private DebugDataHolder $debugDataHolder;
 
     protected function setUp(): void
     {
         parent::setUp();
+
+        $debugDataHolder = self::$container->get('doctrine.debug_data_holder');
+        \assert($debugDataHolder instanceof DebugDataHolder);
+        $debugDataHolder->reset();
+
+        $this->debugDataHolder = $debugDataHolder;
     }
 
     public function testPaginator(): void
@@ -49,5 +57,12 @@ class SimplePaginatorTest extends MauticMysqlTestCase
             $ipAddress2->getId() => $ipAddress2,
             $ipAddress3->getId() => $ipAddress3,
         ], iterator_to_array($paginator), 'Only 2 last records should be returned.');
+
+        $prefix  = self::$container->getParameter('mautic.db_table_prefix');
+        $queries = $this->debugDataHolder->getData()['default'];
+
+        $this->assertCount(5, $queries, 'There should be exactly 5 queries executed.');
+        $this->assertMatchesRegularExpression("/^SELECT count\((.{2}_)\.id\) AS sclr_0 FROM {$prefix}ip_addresses \\1$/", $queries[3]['sql'], 'Simple paginator should not use either a DISTINCT keyword or sub-queries.');
+        $this->assertMatchesRegularExpression("/^SELECT (.{2}_)\.id AS id_0, \\1\.ip_address AS ip_address_1, \\1\.ip_details AS ip_details_2 FROM {$prefix}ip_addresses \\1 ORDER BY \\1\.id ASC LIMIT 5 OFFSET 1$/", $queries[4]['sql'], 'Ordering and limit/offset have to be reflected.');
     }
 }
