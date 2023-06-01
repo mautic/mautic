0b35bc98e258fd24a7f8ea02ebfeb61254d45274
Zdeno Kuzmany <zdeno@kuzmany.biz>
2023-06-01 10:25:52 +0200

    Allow $fromFormat as null for DateTimeHelper

--- app/bundles/CoreBundle/Helper/DateTimeHelper.php
+++ app/bundles/CoreBundle/Helper/DateTimeHelper.php
@@ -42,7 +42,7 @@ class DateTimeHelper
 
     /**
      * @param \DateTimeInterface|string $string
-     * @param string                    $fromFormat Format the string is in
+     * @param string|null               $fromFormat Format the string is in
      * @param string                    $timezone   Timezone the string is in
      */
     public function __construct($string = '', $fromFormat = self::FORMAT_DB, $timezone = 'UTC')
