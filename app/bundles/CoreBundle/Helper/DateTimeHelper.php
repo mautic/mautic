ec3b1620214b0d3e6a4b5432bba1967aefa9bc9e
John Linhart <jan@linhart.email>
2023-06-01 18:16:40 +0200

    DateTime::format(): Passing null to parameter #1 ($format) of type string is deprecated

--- app/bundles/CoreBundle/Helper/DateTimeHelper.php
+++ app/bundles/CoreBundle/Helper/DateTimeHelper.php
@@ -73,11 +73,11 @@ class DateTimeHelper
         if ($datetime instanceof \DateTimeInterface) {
             $this->datetime = $datetime;
             $this->timezone = $datetime->getTimezone()->getName();
-            $this->string   = $this->datetime->format($fromFormat);
+            $this->string   = $fromFormat ? $this->datetime->format($fromFormat) : $datetime;
         } elseif (empty($datetime)) {
             $this->datetime = new \DateTime('now', new \DateTimeZone($this->timezone));
-            $this->string   = $this->datetime->format($fromFormat);
-        } elseif (null == $fromFormat) {
+            $this->string   = $fromFormat ? $this->datetime->format($fromFormat) : $datetime;
+        } elseif (null === $fromFormat) {
             $this->string   = $datetime;
             $this->datetime = new \DateTime($datetime, new \DateTimeZone($this->timezone));
         } else {
