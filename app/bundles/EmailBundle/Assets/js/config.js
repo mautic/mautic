2416d66ff0b1585bd1892fef7dfddea17593b8d1
fedys <miroslav.fedeles@gmail.com>
2023-06-08 14:18:01 +0200

    Removed unnecessary variable

--- app/bundles/EmailBundle/Assets/js/config.js
+++ app/bundles/EmailBundle/Assets/js/config.js
@@ -64,20 +64,6 @@ Mautic.testMonitoredEmailServerConnection = function(mailbox) {
 };
 
 Mautic.testEmailServerConnection = function() {
-    var data = {
-        amazon_region:       mQuery('#config_emailconfig_mailer_amazon_region').val(),
-        amazon_other_region: mQuery('#config_emailconfig_mailer_amazon_other_region').val(),
-        host:                mQuery('#config_emailconfig_mailer_host').val(),
-        api_key:             mQuery('#config_emailconfig_mailer_api_key').val(),
-        authMode:            mQuery('#config_emailconfig_mailer_auth_mode').val(),
-        encryption:          mQuery('#config_emailconfig_mailer_encryption').val(),
-        from_email:          mQuery('#config_emailconfig_mailer_from_email').val(),
-        from_name:           mQuery('#config_emailconfig_mailer_from_name').val(),
-        password:            mQuery('#config_emailconfig_mailer_password').val(),
-        port:                mQuery('#config_emailconfig_mailer_port').val(),
-        transport:           mQuery('#config_emailconfig_mailer_transport').val(),
-        user:                mQuery('#config_emailconfig_mailer_user').val()
-    };
     var data = {}
     mQuery('.used-in-test').each(function(){
         const id = mQuery(this).attr('id')
