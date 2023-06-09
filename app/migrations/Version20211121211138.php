d8cebada447330c8ce899f8f2a1762455ce33117
Nick Vanpraet <nick.vanpraet@dropsolid.com>
2023-06-09 13:08:59 +0200

    update migrate

--- app/migrations/Version20211121211138.php
+++ app/migrations/Version20211121211138.php
@@ -20,11 +20,29 @@ final class Version20211121211138 extends AbstractMauticMigration
     public function up(Schema $schema): void
     {
         // Add schedule fields
-        $this->addSql("ALTER TABLE {$this->prefix}reports ADD schedule_time VARCHAR(191) DEFAULT '00:00' NOT NULL, ADD schedule_timezone VARCHAR(191) DEFAULT 'UTC' NOT NULL, ADD send_empty TINYINT(1) DEFAULT '0' NOT NULL");
+        $table = $schema->getTable($this->prefix.'reports');
+        if (!$table->hasColumn('schedule_time')) {
+            $this->addSql("ALTER TABLE {$this->prefix}reports ADD schedule_time VARCHAR(191) DEFAULT '00:00' NOT NULL");
+        }
+        if (!$table->hasColumn('schedule_timezone')) {
+            $this->addSql("ALTER TABLE {$this->prefix}reports ADD schedule_timezone VARCHAR(191) DEFAULT 'UTC' NOT NULL");
+        }
+        if (!$table->hasColumn('send_empty')) {
+            $this->addSql("ALTER TABLE {$this->prefix}reports ADD send_empty TINYINT(1) DEFAULT '0' NOT NULL");
+        }
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql("ALTER TABLE {$this->prefix}reports DROP COLUMN schedule_time, DROP COLUMN schedule_timezone, DROP COLUMN send_empty ");
+        $table = $schema->getTable($this->prefix.'reports');
+        if ($table->hasColumn('schedule_time')) {
+            $this->addSql("ALTER TABLE {$this->prefix}reports DROP COLUMN schedule_time");
+        }
+        if ($table->hasColumn('schedule_timezone')) {
+            $this->addSql("ALTER TABLE {$this->prefix}reports DROP COLUMN schedule_timezone");
+        }
+        if ($table->hasColumn('send_empty')) {
+            $this->addSql("ALTER TABLE {$this->prefix}reports DROP COLUMN send_empty ");
+        }
     }
 }
