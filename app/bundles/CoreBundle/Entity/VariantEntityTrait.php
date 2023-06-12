ae6872c97d3eb1b266fc4e0665e925dcf9b65fba
Zdeno Kuzmany <zdeno@kuzmany.biz>
2023-06-12 12:43:42 +0200

    Fix PHPStan

--- app/bundles/CoreBundle/Entity/VariantEntityTrait.php
+++ app/bundles/CoreBundle/Entity/VariantEntityTrait.php
@@ -16,7 +16,7 @@ trait VariantEntityTrait
     private $variantChildren;
 
     /**
-     * @var VariantEntityInterface|FormEntity|null
+     * @var VariantEntityInterface|FormEntity|object|null
      **/
     private $variantParent;
 
