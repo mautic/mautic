b8822f078165165860bdefcf5ddaeaf71973c99c
fedys <miroslav.fedeles@gmail.com>
2023-06-16 09:53:42 +0200

    Rector fix

--- app/bundles/EmailBundle/MauticEmailBundle.php
+++ app/bundles/EmailBundle/MauticEmailBundle.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Mautic\EmailBundle;
 
 use Mautic\EmailBundle\DependencyInjection\Compiler\StatHelperPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -13,5 +14,6 @@ class MauticEmailBundle extends Bundle
     public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new StatHelperPass());
+        $container->addCompilerPass(new StatHelperPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 0);
     }
 }
