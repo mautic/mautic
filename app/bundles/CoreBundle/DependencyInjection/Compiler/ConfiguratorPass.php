63e9c96dbd8cfcfe0fbeeaad196586e9b16603e3
John Linhart <jan@linhart.email>
2023-06-16 17:32:46 +0200

    Fixing the installation by using FQCN instead of alias

--- app/bundles/CoreBundle/DependencyInjection/Compiler/ConfiguratorPass.php
+++ app/bundles/CoreBundle/DependencyInjection/Compiler/ConfiguratorPass.php
@@ -1,26 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Mautic\CoreBundle\DependencyInjection\Compiler;
 
+use Mautic\CoreBundle\Configurator\Configurator;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-/**
- * Class ConfiguratorPass.
- */
 class ConfiguratorPass implements CompilerPassInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
-        if (!$container->hasDefinition('mautic.configurator')) {
-            return;
-        }
-
-        $configuratorDef = $container->findDefinition('mautic.configurator');
+        $configuratorDef = $container->findDefinition(Configurator::class);
 
         foreach ($container->findTaggedServiceIds('mautic.configurator.step') as $id => $tags) {
             $priority = isset($tags[0]['priority']) ? $tags[0]['priority'] : 0;
