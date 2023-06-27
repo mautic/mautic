af460e843642ea8f66354f4a94faf5813247916c
John Linhart <jan@linhart.email>
2023-06-27 14:01:02 +0200

    Replacing a unit test with a functional test

---
+++ app/bundles/CoreBundle/Tests/Functional/Controller/JsControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\CoreBundle\Tests\Functional\Controller;
+
+use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use PHPUnit\Framework\Assert;
+use Symfony\Component\HttpFoundation\Response;
+
+final class JsControllerTest extends MauticMysqlTestCase
+{
+    protected function setUp(): void
+    {
+        $this->configParams['google_analytics_id']                   = 'G-F3825DS9CD';
+        $this->configParams['google_analytics_trackingpage_enabled'] = true;
+        parent::setUp();
+    }
+    
+    public function testIndexActionRendersSuccessfully(): void
+    {
+        $this->client->request('GET', '/mtc.js');
+        Assert::assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        Assert::assertStringContainsString('https://www.googletagmanager.com/gtag/js?id=G-F3825DS9CD', $this->client->getResponse()->getContent());
+    }
+}
