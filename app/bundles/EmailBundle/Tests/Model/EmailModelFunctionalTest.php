344c4c989c2149f9ce2297eacad65839ea138da3
Anna Munk <anna.munk@comarch.pl>
2023-07-21 18:39:22 +0200

    feat: [DPMMA-1866] EmailModel test

--- app/bundles/EmailBundle/Tests/Model/EmailModelFunctionalTest.php
+++ app/bundles/EmailBundle/Tests/Model/EmailModelFunctionalTest.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace Mautic\EmailBundle\Tests\Model;
 
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Mautic\CoreBundle\Entity\IpAddress;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use Mautic\EmailBundle\Entity\Email;
+use Mautic\EmailBundle\Entity\Stat;
 use Mautic\EmailBundle\Model\EmailModel;
 use Mautic\LeadBundle\Entity\Lead;
 use Mautic\LeadBundle\Entity\LeadList;
 use Mautic\LeadBundle\Entity\ListLead;
 use Mautic\LeadBundle\Model\LeadModel;
+use Mautic\PageBundle\Entity\Hit;
+use Mautic\PageBundle\Entity\Redirect;
+use Mautic\PageBundle\Entity\Trackable;
 
 class EmailModelFunctionalTest extends MauticMysqlTestCase
 {
@@ -137,5 +145,155 @@ class EmailModelFunctionalTest extends MauticMysqlTestCase
 
         self::assertSame($customHtmlParent, $parentEmail->getCustomHtml());
         self::assertSame($customHtmlChildren, $childrenEmail->getCustomHtml());
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    private function emulateEmailStat(Lead $lead, Email $email, bool $isRead): void
+    {
+        $stat = new Stat();
+        $stat->setEmailAddress('test@test.com');
+        $stat->setLead($lead);
+        $stat->setDateSent(new \DateTime('2023-07-22'));
+        $stat->setEmail($email);
+        $stat->setIsRead($isRead);
+        $this->em->persist($stat);
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    private function emulateClick(Lead $lead, Email $email, string $url, int $hits, int $uniqueHits): void
+    {
+        $ipAddress = new IpAddress();
+        $ipAddress->setIpAddress('127.0.0.1');
+        $this->em->persist($ipAddress);
+        $this->em->flush();
+
+        $redirect = new Redirect();
+        $redirect->setRedirectId(uniqid());
+        $redirect->setUrl($url);
+        $redirect->setHits($hits);
+        $redirect->setUniqueHits($uniqueHits);
+        $this->em->persist($redirect);
+
+        $trackable = new Trackable();
+        $trackable->setChannelId($email->getId());
+        $trackable->setChannel('email');
+        $trackable->setHits($hits);
+        $trackable->setUniqueHits($uniqueHits);
+        $trackable->setRedirect($redirect);
+        $this->em->persist($trackable);
+
+        $pageHit = new Hit();
+        $pageHit->setRedirect($redirect);
+        $pageHit->setIpAddress($ipAddress);
+        $pageHit->setEmail($email);
+        $pageHit->setLead($lead);
+        $pageHit->setDateHit(new \DateTime());
+        $pageHit->setCode(200);
+        $pageHit->setUrl($redirect->getUrl());
+        $pageHit->setTrackingId($redirect->getRedirectId());
+        $pageHit->setSource('email');
+        $pageHit->setSourceId($email->getId());
+        $this->em->persist($pageHit);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws Exception
+     */
+    public function testGetEmailCountryStatsSingleEmail(): void
+    {
+        /** @var EmailModel $emailModel */
+        $emailModel   = self::getContainer()->get('mautic.email.model.email');
+        $dateFrom     = new \DateTime('2023-07-21');
+        $dateTo       = new \DateTime('2023-07-24');
+        $leadsPayload = [
+            [
+                'email'   => 'example1@test.com',
+                'country' => 'Italy',
+                'read'    => true,
+                'click'   => true,
+            ],
+            [
+                'email'   => 'example2@test.com',
+                'country' => 'Italy',
+                'read'    => true,
+                'click'   => false,
+            ],
+            [
+                'email'   => 'example3@test.com',
+                'country' => 'Italy',
+                'read'    => false,
+                'click'   => false,
+            ],
+            [
+                'email'   => 'example4@test.com',
+                'country' => '',
+                'read'    => true,
+                'click'   => true,
+            ],
+            [
+                'email'   => 'example5@test.com',
+                'country' => 'Poland',
+                'read'    => true,
+                'click'   => false,
+            ],
+            [
+                'email'   => 'example6@test.com',
+                'country' => 'Poland',
+                'read'    => true,
+                'click'   => true,
+            ],
+        ];
+
+        $email = new Email();
+        $email->setName('Test email');
+        $this->em->persist($email);
+        $this->em->flush();
+
+        foreach ($leadsPayload as $l) {
+            $lead = new Lead();
+            $lead->setEmail($l['email']);
+            $lead->setCountry($l['country']);
+            $this->em->persist($lead);
+
+            $this->emulateEmailStat($lead, $email, $l['read']);
+
+            if ($l['read'] && $l['click']) {
+                $hits       = rand(1, 5);
+                $uniqueHits = rand(1, $hits);
+                $this->emulateClick($lead, $email, 'https://example.com', $hits, $uniqueHits);
+            }
+        }
+        $email->getRelatedEntityIds();
+        $this->em->flush();
+        $results = $emailModel->getEmailCountryStats($email, $dateFrom, $dateTo);
+
+        self::assertCount(3, $results);
+        self::assertSame([
+            [
+                'sent_count'            => '1',
+                'read_count'            => '1',
+                'clicked_through_count' => '1',
+                'country'               => '',
+            ],
+            [
+                'sent_count'            => '3',
+                'read_count'            => '2',
+                'clicked_through_count' => '1',
+                'country'               => 'Italy',
+            ],
+            [
+                'sent_count'            => '2',
+                'read_count'            => '2',
+                'clicked_through_count' => '1',
+                'country'               => 'Poland',
+            ],
+        ], $results);
     }
 }
