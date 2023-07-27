b66b7099c5e469c2ce8210e62dc0507cfb4e8a47
Anna Munk <anna.munk@comarch.pl>
2023-07-27 14:22:47 +0200

    feat: [DPMMA-1866] EmailMap tests added

--- app/bundles/EmailBundle/Tests/Controller/EmailMapStatsControllerTest.php
+++ app/bundles/EmailBundle/Tests/Controller/EmailMapStatsControllerTest.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Mautic\EmailBundle\Tests\Controller;
 
 use Doctrine\DBAL\Exception;
+use Doctrine\ORM\Exception\ORMException;
 use Mautic\CoreBundle\Security\Permissions\CorePermissions;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use Mautic\EmailBundle\Controller\EmailMapStatsController;
 use Mautic\EmailBundle\Entity\Email;
 use Mautic\EmailBundle\Model\EmailModel;
+use Mautic\UserBundle\Entity\Role;
+use Mautic\UserBundle\Entity\User;
 use PHPUnit\Framework\MockObject\MockObject;
 
 class EmailMapStatsControllerTest extends MauticMysqlTestCase
@@ -18,15 +21,59 @@ class EmailMapStatsControllerTest extends MauticMysqlTestCase
 
     private EmailMapStatsController $mapController;
 
+    private CorePermissions $corePermissionsMock;
+
     protected function setUp(): void
     {
         parent::setUp();
-        $corePermissionsMock = $this->createMock(CorePermissions::class);
-        $corePermissionsMock->method('hasEntityAccess')
-            ->willReturn(true);
+        $this->corePermissionsMock = $this->createMock(CorePermissions::class);
 
         $this->emailModelMock = $this->createMock(EmailModel::class);
         $this->mapController  = new EmailMapStatsController($this->emailModelMock);
+    }
+
+    /**
+     * @throws ORMException
+     */
+    public function testHasAccess(): void
+    {
+        $role = new Role();
+        $role->setName('Example admin');
+        $this->em->persist($role);
+        $this->em->flush();
+
+        $user = new User();
+        $user->setFirstName('Example');
+        $user->setLastName('Example');
+        $user->setUsername('Example');
+        $user->setPassword('123456');
+        $user->setEmail('example@example.com');
+        $user->setRole($role);
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $email = new Email();
+        $email->setName('Test email 1');
+        $email->setCreatedBy($user);
+        $this->em->persist($email);
+
+        $this->corePermissionsMock->method('hasEntityAccess')
+            ->with(
+                'email:emails:viewown',
+                'email:emails:viewother',
+                $user->getId()
+            )
+            ->willReturn(false);
+
+        $result = $this->mapController->hasAccess($this->corePermissionsMock, $email);
+
+        $this->assertFalse($result);
+    }
+
+    public function testGetMapOptions(): void
+    {
+        $result = $this->mapController->getMapOptions();
+        $this->assertSame(EmailMapStatsController::MAP_OPTIONS, $result);
     }
 
     /**
