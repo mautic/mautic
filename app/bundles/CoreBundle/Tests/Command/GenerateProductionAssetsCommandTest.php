a2c6a6b5b4c8356d3ec67ae9a9eb4af03a8805ed
Saurabh Gupta <saurabh.gupta@acquia.com>
2023-08-11 10:33:57 +0530

    test commit

--- app/bundles/CoreBundle/Tests/Command/GenerateProductionAssetsCommandTest.php
+++ app/bundles/CoreBundle/Tests/Command/GenerateProductionAssetsCommandTest.php
@@ -11,6 +11,7 @@ class GenerateProductionAssetsCommandTest extends MauticMysqlTestCase
     public function testCkeditorFileNotExist(): void
     {
         $commandTester = $this->testSymfonyCommand('mautic:assets:generate');
+        
         $this->assertStringContainsString('Production assets have been regenerated.', $commandTester->getDisplay());
         $this->assertEquals(0, $commandTester->getStatusCode());
     }
