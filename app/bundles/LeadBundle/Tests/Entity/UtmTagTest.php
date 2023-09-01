ac1fb13ef0d1c115485c2f82a0270174912cdbcf
Zdeno Kuzmany <zdeno@kuzmany.biz>
2023-09-01 11:39:39 +0200

    Add unit tests

--- app/bundles/LeadBundle/Tests/Entity/UtmTagTest.php
+++ app/bundles/LeadBundle/Tests/Entity/UtmTagTest.php
@@ -28,4 +28,35 @@ class UtmTagTest extends \PHPUnit\Framework\TestCase
         yield ['custom', 6];
         yield ['UTM content longer like 191 UTM content longer like 191 UTM content longer like 191 UTM content longer like 191 UTM content longer like 191 UTM content longer like 191 UTM content longer like 191 UTM content longer like 191 UTM content longer like 191 UTM content longer like 191 UTM content longer like 191 ', 191];
     }
+
+    /**
+     * @dataProvider utmTagsDataProvider
+     */
+    public function testHasUtmTags(?string $utmCampaign, ?string $utmSource, ?string $utmMedium, ?string $utmContent, ?string $utmTerm, bool $expectedResult): void
+    {
+        $utmTag = new UtmTag();
+        $utmTag->setUtmCampaign($utmCampaign);
+        $utmTag->setUtmSource($utmSource);
+        $utmTag->setUtmMedium($utmMedium);
+        $utmTag->setUtmContent($utmContent);
+        $utmTag->setUtmTerm($utmTerm);
+
+        $this->assertEquals($expectedResult, $utmTag->hasUtmTags());
+    }
+
+    /**
+     * @return array<string|array<bool|string|null>>
+     */
+    public function utmTagsDataProvider(): array
+    {
+        return [
+            'All tags are null'       => [null, null, null, null, null, false],
+            'Only utmCampaign is set' => ['campaign', null, null, null, null, true],
+            'Only utmSource is set'   => [null, 'source', null, null, null, true],
+            'Only utmMedium is set'   => [null, null, 'medium', null, null, true],
+            'Only utmContent is set'  => [null, null, null, 'content', null, true],
+            'Only utmTerm is set'     => [null, null, null, null, 'term', true],
+            'All tags are set'        => ['campaign', 'source', 'medium', 'content', 'term', true],
+        ];
+    }
 }
