57329d066c90949c279be30caa1e4e4c6c405dd6
Patryk Gruszka <patryk.gruszka@comarch.pl>
2023-09-11 13:16:47 +0200

    feat: [DPMMA-1436] CTOR and CTR stats in email report

--- app/bundles/EmailBundle/EventListener/ReportSubscriber.php
+++ app/bundles/EmailBundle/EventListener/ReportSubscriber.php
@@ -29,6 +29,7 @@ class ReportSubscriber implements EventSubscriberInterface
     public const CLICK_PREFIX         = 'cut';
     public const TRACKABLE_PREFIX     = 'tr';
     public const REDIRECT_PREFIX      = 'pr';
+    public const CLICK_THROUGH_PREFIX = 'ct';
 
     public const DNC_COLUMNS = [
         'unsubscribed' => [
@@ -242,6 +243,26 @@ class ReportSubscriber implements EventSubscriberInterface
                 'label' => 'mautic.email.report.variant_read_count',
                 'type'  => 'int',
             ],
+            'click_through_count' => [
+                'alias'   => 'click_through_count',
+                'label'   => 'mautic.email.report.click_through_count',
+                'type'    => 'string',
+                'formula' => 'IFNULL('.self::CLICK_THROUGH_PREFIX.'.click_through_count, 0)',
+            ],
+            'click_through_rate' => [
+                'alias'   => 'click_through_rate',
+                'label'   => 'mautic.email.report.click_through_rate',
+                'type'    => 'string',
+                'formula' => 'IFNULL(ROUND('.self::CLICK_THROUGH_PREFIX.'.click_through_count/'.$prefix.'sent_count * 100, 1), \'0.0\')',
+                'suffix'  => '%',
+            ],
+            'click_to_open_rate' => [
+                'alias'   => 'click_to_open_rate',
+                'label'   => 'mautic.email.report.click_to_open_rate',
+                'type'    => 'string',
+                'formula' => 'IFNULL(ROUND('.self::CLICK_THROUGH_PREFIX.'.click_through_count/'.$prefix.'read_count * 100, 1), \'0.0\')',
+                'suffix'  => '%',
+            ],
         ];
 
         $columns = array_merge(
@@ -335,11 +356,12 @@ class ReportSubscriber implements EventSubscriberInterface
         $qb         = $event->getQueryBuilder();
         $hasGroupBy = $event->hasGroupBy();
 
-        // channel_url_trackables subquery
-        $qbcut             = $this->db->createQueryBuilder();
-        $useDncColumns     = $event->usesColumn(array_keys(self::DNC_COLUMNS));
-        $useVariantColumns = $event->usesColumn(array_keys(self::EMAIL_VARIANT_COLUMNS));
-        $useClickColumns   = $event->usesColumn(array_keys(self::CLICK_COLUMNS)) || $event->usesColumn('is_hit');
+        $qbcut                  = $this->db->createQueryBuilder(); // channel_url_trackables subquery
+        $qbct                   = $this->db->createQueryBuilder(); // click-though subquery
+        $useDncColumns          = $event->usesColumn(array_keys(self::DNC_COLUMNS));
+        $useVariantColumns      = $event->usesColumn(array_keys(self::EMAIL_VARIANT_COLUMNS));
+        $useClickColumns        = $event->usesColumn(array_keys(self::CLICK_COLUMNS)) || $event->usesColumn('is_hit');
+        $useClickThroughColumns = $event->usesColumn(['click_through_count', 'click_through_rate', 'click_to_open_rate']);
 
         switch ($context) {
             case self::CONTEXT_EMAILS:
@@ -367,6 +389,17 @@ class ReportSubscriber implements EventSubscriberInterface
 
                 if ($useDncColumns) {
                     $this->addDNCTableForEmails($qb);
+                }
+
+                if ($useClickThroughColumns) {
+                    $qbct->select(
+                        'COUNT(DISTINCT ph.lead_id) AS click_through_count',
+                        'cut.channel_id',
+                    )
+                        ->from(MAUTIC_TABLE_PREFIX.'page_hits', 'ph')
+                        ->innerJoin('ph', MAUTIC_TABLE_PREFIX.'channel_url_trackables', 'cut', 'cut.redirect_id = ph.redirect_id AND cut.channel_id = ph.source_id')
+                        ->groupBy('cut.channel_id');
+                    $qb->leftJoin(self::EMAILS_PREFIX, sprintf('(%s)', $qbct->getSQL()), self::CLICK_THROUGH_PREFIX, 'e.id = ct.channel_id');
                 }
 
                 break;
@@ -425,6 +458,17 @@ class ReportSubscriber implements EventSubscriberInterface
                         self::CLICK_PREFIX,
                         'es.email_id = cut.channel_id AND es.lead_id = cut.lead_id'
                     );
+                }
+
+                if ($useClickThroughColumns) {
+                    $qbct->select(
+                        'COUNT(DISTINCT ph.lead_id) AS click_through_count',
+                        'cut.channel_id',
+                    )
+                        ->from(MAUTIC_TABLE_PREFIX.'page_hits', 'ph')
+                        ->innerJoin('ph', MAUTIC_TABLE_PREFIX.'channel_url_trackables', 'cut', 'cut.redirect_id = ph.redirect_id AND cut.channel_id = ph.source_id')
+                        ->groupBy('cut.channel_id');
+                    $qb->leftJoin(self::EMAIL_STATS_PREFIX, sprintf('(%s)', $qbct->getSQL()), self::CLICK_THROUGH_PREFIX, 'es.email_id = ct.channel_id');
                 }
 
                 $event->addCampaignByChannelJoin(
@@ -756,6 +800,29 @@ class ReportSubscriber implements EventSubscriberInterface
             );
         }
     }
+
+    // private function joinClickTroughForEmails(QueryBuilder $qb): void
+    // {
+    //    $trTable = MAUTIC_TABLE_PREFIX.'channel_url_trackables';
+    //    $phTable = MAUTIC_TABLE_PREFIX.'page_hits';
+    //
+    //    if (!$this->isJoined($qb, $trTable, self::EMAILS_PREFIX, self::TRACKABLE_PREFIX)) {
+    //        $qb->leftJoin(
+    //            self::EMAILS_PREFIX,
+    //            $trTable,
+    //            self::TRACKABLE_PREFIX,
+    //            'e.id = tr.channel_id AND tr.channel = \'email\''
+    //        );
+    //    }
+    //    if (!$this->isJoined($qb, $prTable, self::TRACKABLE_PREFIX, self::REDIRECT_PREFIX)) {
+    //        $qb->leftJoin(
+    //            self::TRACKABLE_PREFIX,
+    //            $prTable,
+    //            self::REDIRECT_PREFIX,
+    //            'tr.redirect_id = pr.id'
+    //        );
+    //    }
+    // }
 
     private function isJoined($query, $table, $fromAlias, $alias)
     {
