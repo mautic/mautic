e8e36f7fbb675d4a19c91d2822fe49903d92c647
Lenon Leite <lenonleite@gmail.com>
2023-09-11 18:34:18 +0100

    add new twigfunction to sanitize attributes tags

--- app/bundles/FormBundle/Twig/Extension/FormFieldExtension.php
+++ app/bundles/FormBundle/Twig/Extension/FormFieldExtension.php
@@ -16,6 +16,28 @@ final class FormFieldExtension extends AbstractExtension
             new TwigFunction('formFieldParseBooleanList', [FormFieldHelper::class, 'parseBooleanList']),
             new TwigFunction('formFieldParseList', [FormFieldHelper::class, 'parseList']),
             new TwigFunction('formFieldParseListForChoices', [FormFieldHelper::class, 'parseListForChoices']),
+            new TwigFunction('formFieldCleanInputAttributes', [$this, 'cleanInputAttributes']),
         ];
+    }
+
+    /**
+     * Clean input evil attributes to prevent XSS
+     * Remove any attribute starting with "on" or xmlns or javascript:. Used in href, src, value, data, etc.
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public function cleanInputAttributes(string $value)
+    {
+        // Remove any HTML tags
+        $value = htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8', false);
+        // Remove any attribute starting with "on" or javascript used in href, src, value, data, etc.
+        preg_match('/(on[A-Za-z]*\s*=|javascript:)/i', $value, $result);
+        if (!empty($result)) {
+            return '';
+        }
+
+        return $value;
     }
 }
