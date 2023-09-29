c1652f9eefa316b1215b2f7e020a3e373f6834fe
quincia <quincia@users.noreply.github.com>
2023-09-29 16:27:08 +0200

    Update index.php
    Add check $_SERVER['HTTP_X_FORWARDED_PROTO']) and $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' to prevent infinite loop with SAML SSO integration

--- index.php
+++ index.php
@@ -15,6 +15,10 @@ date_default_timezone_set('UTC');
 
 require_once 'autoload.php';
 
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 use Mautic\CoreBundle\ErrorHandler\ErrorHandler;
 use Mautic\Middleware\MiddlewareBuilder;
 use function Stack\run;
