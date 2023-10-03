331c6c63fbc4f3c390aff5296630366ba5f214d5
Alan Wierzchoń <alan.wierzchon@comarch.pl>
2023-10-03 11:21:49 +0200

    fix:[DPMMA-1743] fix cs fixers errors

--- plugins/MauticFocusBundle/Tests/EventListener/LeadSubscriberTest.php
+++ plugins/MauticFocusBundle/Tests/EventListener/LeadSubscriberTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace MauticPlugin\MauticFocusBundle\Tests\EventListener;
 
-use DateTime;
 use Mautic\CoreBundle\Tests\CommonMocks;
 use Mautic\CoreBundle\Translation\Translator;
 use Mautic\LeadBundle\Entity\Lead;
