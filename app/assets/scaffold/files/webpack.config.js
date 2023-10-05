b822701d94e3bc79ad51ae3ae071d2ed2fa29d77
Mattias Michaux <mattias.michaux@dropsolid.com>
2023-10-05 22:28:34 +0200

    fix for correct path for composer based installs

--- app/assets/scaffold/files/webpack.config.js
+++ app/assets/scaffold/files/webpack.config.js
@@ -7,20 +7,30 @@
 
 /* eslint-env node */
 const path = require( 'path' );
+const glob = require("glob")
+const fs = require( 'fs' );
 const { loaders } = require( '@ckeditor/ckeditor5-dev-utils' );
 const { CKEditorTranslationsPlugin } = require( '@ckeditor/ckeditor5-dev-translations' );
+
+// Find the webroot path if it is not the same folder as the current dir.
+// This is the case for Composer based installations following the best practices.
+let webroot = '';
+if (!fs.existsSync('app/release_metadata.json')) {
+	let files = glob.sync("**/app/release_metadata.json");
+	webroot = path.dirname(path.dirname(files[0])) + '/';
+}
 
 module.exports = {
 	devtool: 'source-map',
 	performance: { hints: false },
 
-	entry: path.resolve( __dirname, 'app/assets/libraries/ckeditor/src', 'ckeditor.ts' ),
+	entry: path.resolve( __dirname, webroot + 'app/assets/libraries/ckeditor/src', 'ckeditor.ts' ),
 
 	output: {
 		// The name under which the editor will be exported.
 		library: 'ClassicEditor',
 
-		path: path.resolve( __dirname, 'media/libraries/ckeditor' ),
+		path: path.resolve( __dirname, webroot + 'media/libraries/ckeditor' ),
 		filename: 'ckeditor.js',
 		libraryTarget: 'umd',
 		libraryExport: 'default'
