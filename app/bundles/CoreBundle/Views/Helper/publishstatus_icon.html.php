3ae44fbea7f5e0f4980d98a7f879f2b3ad9009c0
Zdeno Kuzmany <zdeno@kuzmany.biz>
2023-10-13 09:44:33 +0200

    Fix toggle button

--- app/bundles/CoreBundle/Views/Helper/publishstatus_icon.html.php
+++ app/bundles/CoreBundle/Views/Helper/publishstatus_icon.html.php
@@ -1,16 +1,11 @@
 <?php
+// Variables
+$query      = isset($query) ? $query : '';
+$size       = (empty($size)) ? 'fa-lg' : $size;
+$attributes = isset($attributes) ? $attributes : [];
+$transKeys  = isset($transKeys) ? $transKeys : [];
 
-/*
- * @copyright   2014 Mautic Contributors. All rights reserved
- * @author      Mautic
- *
- * @link        http://mautic.org
- *
- * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- */
-$query  = (!isset($query)) ? '' : $query;
-
-// Custom toggle
+// If query exists
 if ($query) {
     parse_str($query, $queryParam);
     if (isset($queryParam['customToggle'])) {
@@ -19,58 +14,49 @@ if ($query) {
     }
 }
 
-// continue as standard published status
-if (!isset($status)) {
-    $status = $item->getPublishStatus();
-}
-$size   = (empty($size)) ? 'fa-lg' : $size;
+$status = isset($status) ? $status : $item->getPublishStatus();
+
 switch ($status) {
     case 'published':
-        $icon = ' fa-toggle-on text-success';
+        $icon = 'fa-toggle-on text-success';
         $text = $view['translator']->trans('mautic.core.form.published');
         break;
     case 'unpublished':
-        $icon = ' fa-toggle-off text-danger';
+        $icon = 'fa-toggle-off text-danger';
         $text = $view['translator']->trans('mautic.core.form.unpublished');
         break;
     case 'expired':
-        $icon = ' fa-clock-o text-danger';
-        $text = $view['translator']->trans('mautic.core.form.expired_to', [
-            '%date%' => $view['date']->toFull($item->getPublishDown()),
-        ]);
+        $icon = 'fa-clock-o text-danger';
+        $text = $view['translator']->trans('mautic.core.form.expired_to', ['%date%' => $view['date']->toFull($item->getPublishDown())]);
         break;
     case 'pending':
-        $icon = ' fa-clock-o text-warning';
-        $text = $view['translator']->trans('mautic.core.form.pending.start_at', [
-            '%date%' => $view['date']->toFull($item->getPublishUp()),
-        ]);
+        $icon = 'fa-clock-o text-warning';
+        $text = $view['translator']->trans('mautic.core.form.pending.start_at', ['%date%' => $view['date']->toFull($item->getPublishUp())]);
         break;
-}
-switch (true) {
-    case true === $status:
-        $icon = ' fa-toggle-on text-success';
+    case true:
+        $icon = 'fa-toggle-on text-success';
         $text = $view['translator']->trans('mautic.core.form.public');
         break;
-    case false === $status:
-        $icon = ' fa-toggle-off text-danger';
+    case false:
+        $icon = 'fa-toggle-off text-danger';
         $text = $view['translator']->trans('mautic.core.form.not.public');
         break;
 }
 
-if (isset($aditionalLabel)) {
-    $text .= $aditionalLabel;
-}
+$text .= isset($aditionalLabel) ? $aditionalLabel : '';
 
-if (!empty($disableToggle)) {
+if (isset($disableToggle) && !empty($disableToggle)) {
     $icon = str_replace(['success', 'danger', 'warning'], 'muted', $icon);
 }
 
-$clickAction = (isset($disableToggle) && true === $disableToggle) ? ' disabled' : ' has-click-event';
+$clickAction = (isset($disableToggle) && true === $disableToggle) ? 'disabled' : 'has-click-event';
 $idClass     = str_replace('.', '-', $model).'-publish-icon'.$item->getId().md5($query);
 
-$backdropFlag = (isset($backdrop)) ? 'true' : 'false';
+$backdropFlag = isset($backdrop) ? 'true' : 'false';
 
-$onclick = $onclick ?? "Mautic.togglePublishStatus(event, '.{$idClass}', '{$model}', '{$item->getId()}', '{$query}', {$backdropFlag})";
+if (!isset($onclick) || (isset($onclick) && empty($onclick))) {
+    $onclick = sprintf("Mautic.togglePublishStatus(event, '.%s', '%s', '%s', '%s', %s);", $idClass, $model, $item->getId(), $query, $backdropFlag);
+}
 
 $defaultAttributes = [
     'data-container' => 'body',
@@ -79,29 +65,34 @@ $defaultAttributes = [
     'data-status'    => $status,
 ];
 
-$attributes = $attributes ?? [];
-
 if (!empty($attributes)) {
-    $attributes['data-id-class']    = '.'.$idClass;
-    $attributes['data-model']       = $model;
-    $attributes['data-item-id']     = $item->getId();
-    $attributes['data-query']       = $query;
-    $attributes['data-backdrop']    = $backdropFlag;
+    $attributes = array_merge($attributes, [
+        'data-id-class' => '.'.$idClass,
+        'data-model'    => $model,
+        'data-item-id'  => $item->getId(),
+        'data-query'    => $query,
+        'data-backdrop' => $backdropFlag,
+    ]);
 }
 
-if (!empty($transKeys)) {
+if (isset($transKeys)) {
     foreach ($transKeys as $k => $v) {
-        $attributes[$k] = $view['translator']->trans($v);
+        $attributes = array_merge($attributes, [
+            $k => $view['translator']->trans($v),
+        ]);
     }
 }
 
-$allDataAttrs = array_merge($attributes + $defaultAttributes);
+$allDataAttrs   = array_merge($defaultAttributes, $attributes);
+$dataAttributes = '';
 
-$dataAttributes = implode(' ', array_map(
-    function ($v, $k) { return sprintf("%s='%s'", $k, $v); },
-    $allDataAttrs,
-    array_keys($allDataAttrs)
-));
+foreach ($allDataAttrs as $k => $v) {
+    $dataAttributes .= sprintf('%s="%s" ', $k, $v);
+}
+
 ?>
 
-<i class="fa fa-fw <?php echo $size.' '.$icon.$clickAction.' '.$idClass; ?> toggle-publish-status"  title="<?php echo $text; ?>" <?php echo $dataAttributes; ?> <?php if (empty($disableToggle)): ?> onclick="<?php echo $onclick; ?>"<?php endif; ?>></i>
+<i class="fa fa-fw <?php echo $size; ?> <?php echo $icon; ?> <?php echo $clickAction; ?> <?php echo $idClass; ?> toggle-publish-status"
+   title="<?php echo $text; ?>"
+    <?php echo $dataAttributes; ?>
+   <?php if (!isset($disableToggle)) : ?>onclick="<?php echo $onclick; ?>"<?php endif; ?>></i>
