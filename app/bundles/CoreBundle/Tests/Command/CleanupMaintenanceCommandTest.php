548f81183b0992e58f14b79d320e0e891917387f
Zdeno Kuzmany <zdeno@kuzmany.biz>
2023-11-17 11:57:06 +0100

    Add tests for check audit log after maintenance

--- app/bundles/CoreBundle/Tests/Command/CleanupMaintenanceCommandTest.php
+++ app/bundles/CoreBundle/Tests/Command/CleanupMaintenanceCommandTest.php
@@ -24,5 +24,11 @@ class CleanupMaintenanceCommandTest extends MauticMysqlTestCase
         $this->runcommand('mautic:maintenance:cleanup', ['--days-old' => 180, '--no-interaction' => true]);
 
         $this->assertNull($this->getContainer()->get('mautic.lead.model.lead')->getEntity($contactId));
+
+        // get last row sql query from audit_log table
+        $sql    = 'SELECT * FROM '.MAUTIC_TABLE_PREFIX.'audit_log ORDER BY id DESC LIMIT 1';
+        $stmt   = $this->em->getConnection()->prepare($sql);
+        $result = $stmt->executeQuery();
+        $this->assertEquals('a:2:{s:7:"options";a:4:{s:8:"days-old";i:180;s:9:"lock_mode";s:3:"pid";s:14:"no-interaction";b:1;s:3:"env";s:4:"test";}s:5:"stats";a:1:{s:8:"Visitors";i:1;}}', $result->fetchAssociative()['details']);
     }
 }
