51d6d7adcab0ec7e79da685a384e914d83a1cba7
Jan Kozak <galvani78@gmail.com>
2023-11-27 09:26:55 +0100

    fix wrong paste job
    Signed-off-by: Jan Kozak <galvani78@gmail.com>

--- app/bundles/AssetBundle/Tests/EventListener/BuilderSubscriberTest.php
+++ app/bundles/AssetBundle/Tests/EventListener/BuilderSubscriberTest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Mautic\AssetBundle\EventListener;
+namespace Mautic\AssetBundle\Tests\EventListener;
 
+use Mautic\AssetBundle\EventListener\BuilderSubscriber;
 use Mautic\AssetBundle\Helper\TokenHelper;
 use Mautic\CoreBundle\Helper\BuilderTokenHelperFactory;
 use Mautic\CoreBundle\Security\Permissions\CorePermissions;
@@ -9,37 +10,28 @@ use Mautic\LeadBundle\Entity\Lead;
 use Mautic\LeadBundle\Tracker\ContactTracker;
 use Mautic\PageBundle\Entity\Page;
 use Mautic\PageBundle\Event\PageDisplayEvent;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class BuilderSubscriberTest extends TestCase
 {
-    /**
-     * @var BuilderSubscriber
-     */
-    private $builderSubscriber;
-
-    /**
-     * @var CorePermissions|\PHPUnit\Framework\MockObject\MockObject
-     */
-    private $security;
-
-    /**
-     * @var ContactTracker|\PHPUnit\Framework\MockObject\MockObject
-     */
-    private $contactTracker;
+    private BuilderSubscriber $builderSubscriber;
+    private CorePermissions|MockObject $security;
+    private ContactTracker|MockObject $contactTracker;
+    private TokenHelper|MockObject $tokenHelper;
 
     protected function setUp(): void
     {
         $this->security                  = $this->createMock(CorePermissions::class);
         $this->contactTracker            = $this->createMock(ContactTracker::class);
         $this->tokenHelper               = $this->createMock(TokenHelper::class);
-        $this->builderTokenHelperFactory = $this->createMock(BuilderTokenHelperFactory::class);
+        $builderTokenHelperFactory       = $this->createMock(BuilderTokenHelperFactory::class);
 
         $this->builderSubscriber = new BuilderSubscriber(
             $this->security,
             $this->tokenHelper,
             $this->contactTracker,
-            $this->builderTokenHelperFactory
+            $builderTokenHelperFactory
         );
     }
 
@@ -52,8 +44,8 @@ class BuilderSubscriberTest extends TestCase
         $this->tokenHelper->expects($this->exactly(2))
                           ->method('findAssetTokens')
                           ->withConsecutive(
-                              ['content', array_filter(json_decode('{"source":["page",null],"lead":false,"email":false}', JSON_OBJECT_AS_ARRAY))],
-                              ['content', array_filter(json_decode('{"source":["page",null],"lead":5,"email":false}', JSON_OBJECT_AS_ARRAY))]
+                              ['content', array_filter(json_decode('{"source":["page",null],"lead":false,"email":false}', true))],
+                              ['content', array_filter(json_decode('{"source":["page",null],"lead":5,"email":false}', true))]
                           );
 
         $page             = new Page();
@@ -70,26 +62,5 @@ class BuilderSubscriberTest extends TestCase
 
         $this->builderSubscriber->onPageDisplay($pageDisplayEvent);
         $this->builderSubscriber->onPageDisplay($pageDisplayEvent->setTrackingDisabled(false));
-    }
-
-    public function testDecodeTokensNotRunIfEventHasNoTrackingDisabled(): void
-    {
-        $this->security->expects($this->once())
-                       ->method('isAnonymous')
-                       ->willReturn(true);
-
-        $this->tokenHelper->expects($this->never())
-                          ->method('findAssetTokens');
-
-        $eventMock = $this->getMockBuilder(PageDisplayEvent::class)
-                          ->disableOriginalConstructor()
-                          ->getMock();
-
-        $eventMock->expects($this->never())->method('getContent');
-
-        $page             = new Page();
-        $pageDisplayEvent = new PageDisplayEvent('content', $page, [], false);
-
-        $this->builderSubscriber->onPageDisplay($pageDisplayEvent);
     }
 }
