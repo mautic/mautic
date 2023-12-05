77746a02f5ff3365ccdee7727cfa864a30a7c340
Tomas Votruba <tomas.vot@gmail.com>
2023-12-05 16:33:12 +0100

    not void

--- app/bundles/ChannelBundle/Model/MessageQueueModel.php
+++ app/bundles/ChannelBundle/Model/MessageQueueModel.php
@@ -329,11 +329,11 @@ class MessageQueueModel extends FormModel
      * @param null   $channelId
      * @param bool   $persist
      */
-    public function rescheduleMessage($message, $rescheduleInterval = null, $leadId = null, $channel = null, $channelId = null, $persist = false)
+    public function rescheduleMessage($message, $rescheduleInterval = null, $leadId = null, $channel = null, $channelId = null, $persist = false): void
     {
         $rescheduleInterval = null == $rescheduleInterval ? self::DEFAULT_RESCHEDULE_INTERVAL : ('P'.$rescheduleInterval);
 
-        return $this->reschedule($message, new \DateInterval($rescheduleInterval), $leadId, $channel, $channelId, $persist);
+        $this->reschedule($message, new \DateInterval($rescheduleInterval), $leadId, $channel, $channelId, $persist);
     }
 
     /**
