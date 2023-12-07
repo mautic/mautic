57022fbe75229c11709323b7d3d07d8e4026777c
Tomas Votruba <tomas.vot@gmail.com>
2023-12-07 16:03:40 +0100

    tidy up

--- app/bundles/UserBundle/Security/Authenticator/PreAuthAuthenticator.php
+++ app/bundles/UserBundle/Security/Authenticator/PreAuthAuthenticator.php
@@ -90,9 +90,6 @@ class PreAuthAuthenticator implements AuthenticationProviderInterface
         );
     }
 
-    /**
-     * @return mixed
-     */
     public function supports(TokenInterface $token): bool
     {
         return $token instanceof PluginToken && $token->getProviderKey() === $this->providerKey;
