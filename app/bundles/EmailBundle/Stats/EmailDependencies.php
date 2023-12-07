62afbf95559fd5166d887804b7214ac3510d5841
Patryk Gruszka <patryk.gruszka@comarch.pl>
2023-12-07 14:33:06 +0100

    feat: [DPMMA-2372] email usages in form actions

--- app/bundles/EmailBundle/Stats/EmailDependencies.php
+++ app/bundles/EmailBundle/Stats/EmailDependencies.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace Mautic\EmailBundle\Stats;
 
 use Mautic\CampaignBundle\Model\CampaignModel;
+use Mautic\FormBundle\Model\ActionModel;
 use Mautic\LeadBundle\Model\ListModel;
 
 class EmailDependencies
 {
     public function __construct(
         private CampaignModel $campaignModel,
-        private ListModel $listModel
+        private ListModel $listModel,
+        private ActionModel $actionModel
     ) {
     }
 
@@ -30,6 +32,11 @@ class EmailDependencies
                 'label' => 'mautic.lead.lead.lists',
                 'route' => 'mautic_segment_index',
                 'ids'   => $this->listModel->getSegmentIdsWithDependenciesOnEmail($emailId),
+            ],
+            [
+                'label' => 'mautic.form.forms',
+                'route' => 'mautic_form_index',
+                'ids'   => $this->actionModel->getFormsIdsWithDependenciesOnEmail($emailId),
             ],
         ];
     }
