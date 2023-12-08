0c073ad6b59b62cf4cc7b70abb428ac1af50b4cc
Putzwasser <26040044+putzwasser@users.noreply.github.com>
2023-12-09 00:37:05 +0100

    TASK: Creates UI config field for `theme_email_default` in `Configuration > Theme Settings > Default theme`

--- app/bundles/CoreBundle/Form/Type/ConfigThemeType.php
+++ app/bundles/CoreBundle/Form/Type/ConfigThemeType.php
@@ -39,6 +39,18 @@ class ConfigThemeType extends AbstractType
                 ]
             )->addViewTransformer(new ArrayStringTransformer())
         );
+
+        $builder->add(
+            'theme_email_default',
+            ThemeListType::class,
+            [
+                'label' => 'mautic.core.config.form.theme_email',
+                'attr'  => [
+                    'class'   => 'form-control',
+                    'tooltip' => 'mautic.core.config.form.theme_email.tooltip',
+                ],
+            ]
+        );
     }
 
     /**
