3f77af557ba809a6f4df345860f07355e0ea25c8
Tomas Votruba <tomas.vot@gmail.com>
2023-12-08 15:56:39 +0100

    fixup! fixup! knwon type

--- app/bundles/PluginBundle/Integration/UnifiedIntegrationInterface.php
+++ app/bundles/PluginBundle/Integration/UnifiedIntegrationInterface.php
@@ -2,22 +2,9 @@
 
 namespace Mautic\PluginBundle\Integration;
 
-use Psr\Http\Message\ResponseInterface;
-
 /**
  * Interface UnifiedIntegrationInterface is used for type hinting.
  */
 interface UnifiedIntegrationInterface
 {
-    /**
-     * Make a basic call using cURL to get the data.
-     *
-     * @param string $url
-     * @param array  $parameters
-     * @param string $method
-     * @param array  $settings   Set $settings['return_raw'] to receive a ResponseInterface
-     *
-     * @return mixed|string|ResponseInterface
-     */
-    public function makeRequest($url, $parameters = [], $method = 'GET', $settings = []);
 }
