4477fe9e6bb20453fe11de00e88d52181a12d99c
Tomas Votruba <tomas.vot@gmail.com>
2023-12-11 14:18:21 +0100

    add ecs config

---
+++ ecs.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/plugins',
+        __DIR__ . '/tests',
+    ]);
+
+    // this way you add a single rule
+    $ecsConfig->rules([
+        NoUnusedImportsFixer::class,
+    ]);
+
+    // this way you can add sets - group of rules
+    $ecsConfig->sets([
+        // run and fix, one by one
+        // SetList::SPACES,
+        // SetList::ARRAY,
+        // SetList::DOCBLOCK,
+        // SetList::NAMESPACES,
+        // SetList::COMMENTS,
+        // SetList::PSR_12,
+    ]);
+};
