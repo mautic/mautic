25def52953ff22f71ee654416cc293ac6d13b980
John Linhart <jan@linhart.email>
2023-12-12 07:56:59 +0100

    MessengerNullableEnvVarProcessor is being used

--- app/bundles/MessengerBundle/DependencyInjection/EnvProcessor/MessengerNullableEnvVarProcessor.php
+++ app/bundles/MessengerBundle/DependencyInjection/EnvProcessor/MessengerNullableEnvVarProcessor.php
@@ -6,9 +6,6 @@ namespace Mautic\MessengerBundle\DependencyInjection\EnvProcessor;
 
 use Symfony\Component\DependencyInjection\EnvVarProcessorInterface;
 
-/**
- * @deprecated since Mautic 5.0, to be removed in 6.0 with no replacement.
- */
 class MessengerNullableEnvVarProcessor implements EnvVarProcessorInterface
 {
     public function getEnv($prefix, $name, \Closure $getEnv)
