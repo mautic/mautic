2432f97e1825402adf2edba9a9cbcbc3d07f5615
Tomas Votruba <tomas.vot@gmail.com>
2023-12-12 01:49:26 +0100

    cs

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 ErrorHandler::register($_SERVER['APP_ENV']);
 
-$kernel   = (new MiddlewareBuilder(new AppKernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG'])))->resolve();
-$request  = Request::createFromGlobals();
+$kernel = (new MiddlewareBuilder(new AppKernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG'])))->resolve();
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
