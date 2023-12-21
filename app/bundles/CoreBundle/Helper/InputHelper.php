957405e69e8e5792e95d16dc6db0c910b91fcdcf
John Linhart <jan@linhart.email>
2023-12-21 10:02:24 +0100

    Finding a way to replace utf8_decode() so the tests would pass

--- app/bundles/CoreBundle/Helper/InputHelper.php
+++ app/bundles/CoreBundle/Helper/InputHelper.php
@@ -417,7 +417,7 @@ class InputHelper
             // Special handling for HTML comments
             $value = str_replace(['<!-->', '<!--', '-->'], ['<mcomment></mcomment>', '<mcomment>', '</mcomment>'], $value, $commentCount);
 
-            $hasUnicode = 'UTF-8"' === mb_detect_encoding($value);
+            $hasUnicode = strlen($value) != strlen(iconv('UTF-8', 'Windows-1252', $value));
 
             $value = self::getFilter(true)->clean($value, $hasUnicode ? 'raw' : 'html');
 
