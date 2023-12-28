afd52c807ad16baa681538e151ff7e71571446fa
Anna Munk <anna.munk@comarch.pl>
2023-12-28 14:50:43 +0100

    test: [DPMMA-2234] Campaign and email tests

---
+++ app/bundles/CampaignBundle/Tests/Controller/CampaignTableStatsControllerTest.php
@@ -0,0 +1,164 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\CampaignBundle\Tests\Controller;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Mautic\CampaignBundle\Controller\CampaignTableStatsController;
+use Mautic\CampaignBundle\Entity\Campaign;
+use Mautic\CampaignBundle\Entity\Event;
+use Mautic\CampaignBundle\Model\CampaignModel;
+use Mautic\CoreBundle\Security\Permissions\CorePermissions;
+use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\EmailBundle\Entity\Email;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class CampaignTableStatsControllerTest extends MauticMysqlTestCase
+{
+    private MockObject $campaignModelMock;
+
+    private CampaignTableStatsController $controller;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $corePermissionsMock = $this->createMock(CorePermissions::class);
+        $corePermissionsMock->method('hasEntityAccess')
+            ->willReturn(true);
+
+        $this->campaignModelMock       = $this->createMock(CampaignModel::class);
+        $this->controller              = new CampaignTableStatsController($this->campaignModelMock);
+    }
+
+    /**
+     * @return array<string, array<int, array<string, string>>>
+     */
+    private function getStats(bool $addEmail = true): array
+    {
+        return $addEmail ? [
+            'Finland' => [
+                    'contacts'              => '14',
+                    'country'               => 'Finland',
+                    'sent_count'            => '14',
+                    'read_count'            => '4',
+                    'clicked_through_count' => '0',
+                ],
+            'Italy' => [
+                    'contacts'              => '5',
+                    'country'               => 'Italy',
+                    'sent_count'            => '5',
+                    'read_count'            => '5',
+                    'clicked_through_count' => '3',
+                ],
+        ] : [
+            'Finland' => [
+                    'contacts' => '14',
+                    'country'  => 'Finland',
+                ],
+            'Italy' => [
+                    'contacts' => '5',
+                    'country'  => 'Italy',
+                ],
+        ];
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws Exception
+     * @throws ORMException
+     */
+    public function testGetData(): void
+    {
+        $campaign = $this->createCampaignWithEmail();
+
+        $this->campaignModelMock->method('getCountryStats')
+            ->with($campaign)
+            ->willReturn($this->getStats());
+
+        $results = $this->controller->getData($campaign);
+
+        $this->assertCount(2, $results);
+        $this->assertSame(['Finland', 'Italy'], array_keys($results));
+        $this->assertSame([
+            'contacts'              => '5',
+            'country'               => 'Italy',
+            'sent_count'            => '5',
+            'read_count'            => '5',
+            'clicked_through_count' => '3',
+        ], $results['Italy']);
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws Exception
+     * @throws ORMException
+     */
+    public function testGetDataNoEmail(): void
+    {
+        $campaign = $this->createCampaignNoEmail();
+
+        $this->campaignModelMock->method('getCountryStats')
+            ->with($campaign)
+            ->willReturn($this->getStats(false));
+
+        $results = $this->controller->getData($campaign);
+
+        $this->assertCount(2, $results);
+        $this->assertSame(['Finland', 'Italy'], array_keys($results));
+        $this->assertSame([
+            'contacts' => '5',
+            'country'  => 'Italy',
+        ], $results['Italy']);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    private function createCampaignWithEmail(): Campaign
+    {
+        $campaign = new Campaign();
+        $campaign->setName('Test campaign');
+        $this->em->persist($campaign);
+        $this->em->flush();
+
+        // Create email
+        $email = new Email();
+        $email->setName('Test email');
+        $this->em->persist($email);
+        $this->em->flush();
+
+        // Create email events
+        $event = new Event();
+        $event->setName('Send email');
+        $event->setType('email.send');
+        $event->setEventType('action');
+        $event->setChannel('email');
+        $event->setChannelId($email->getId());
+        $event->setCampaign($campaign);
+        $this->em->persist($event);
+        $this->em->flush();
+
+        // Add events to campaign
+        $campaign->addEvent(0, $event);
+
+        return $campaign;
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    private function createCampaignNoEmail(): Campaign
+    {
+        $campaign = new Campaign();
+        $campaign->setName('Test campaign');
+        $this->em->persist($campaign);
+        $this->em->flush();
+
+        return $campaign;
+    }
+}
---
+++ app/bundles/EmailBundle/Tests/Controller/EmailTableStatsControllerTest.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\EmailBundle\Tests\Controller;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\Exception\ORMException;
+use Mautic\CoreBundle\Security\Permissions\CorePermissions;
+use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\EmailBundle\Controller\EmailTableStatsController;
+use Mautic\EmailBundle\Entity\Email;
+use Mautic\EmailBundle\Model\EmailModel;
+use Mautic\UserBundle\Entity\Role;
+use Mautic\UserBundle\Entity\User;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class EmailTableStatsControllerTest extends MauticMysqlTestCase
+{
+    private MockObject $emailModelMock;
+
+    private EmailTableStatsController $controller;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->emailModelMock = $this->createMock(EmailModel::class);
+        $this->controller     = new EmailTableStatsController($this->emailModelMock);
+    }
+
+    /**
+     * @throws ORMException
+     */
+    public function testHasAccess(): void
+    {
+        $corePermissionsMock = $this->createMock(CorePermissions::class);
+
+        $role = new Role();
+        $role->setName('Example admin');
+        $this->em->persist($role);
+        $this->em->flush();
+
+        $user = new User();
+        $user->setFirstName('Example');
+        $user->setLastName('Example');
+        $user->setUsername('Example');
+        $user->setPassword('123456');
+        $user->setEmail('example@example.com');
+        $user->setRole($role);
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $email = new Email();
+        $email->setName('Test email 1');
+        $email->setCreatedBy($user);
+        $this->em->persist($email);
+        $this->em->flush();
+
+        $corePermissionsMock->method('hasEntityAccess')
+            ->with(
+                'email:emails:viewown',
+                'email:emails:viewother',
+                $user->getId()
+            )
+            ->willReturn(false);
+
+        $result = $this->controller->hasAccess($corePermissionsMock, $email);
+
+        try {
+            $this->controller->viewAction($corePermissionsMock, $email->getId());
+        } catch (AccessDeniedHttpException|\Exception $e) {
+            $this->assertTrue($e instanceof AccessDeniedHttpException);
+        }
+
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @return array<string, array<int, array<string, string>>>
+     */
+    private function getStats(): array
+    {
+        return [
+            'Finland' => [
+                    'contacts'              => '14',
+                    'country'               => 'Finland',
+                    'sent_count'            => '14',
+                    'read_count'            => '4',
+                    'clicked_through_count' => '0',
+                ],
+            'Italy' => [
+                    'contacts'              => '5',
+                    'country'               => 'Italy',
+                    'sent_count'            => '5',
+                    'read_count'            => '5',
+                    'clicked_through_count' => '3',
+                ],
+        ];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetData(): void
+    {
+        $email = new Email();
+        $email->setName('Test email');
+
+        $this->emailModelMock->method('getCountryStats')
+            ->with($email, false)
+            ->willReturn($this->getStats());
+
+        $results = $this->controller->getData($email);
+
+        $this->assertCount(2, $results);
+        $this->assertSame($this->getStats(), $results);
+    }
+}
