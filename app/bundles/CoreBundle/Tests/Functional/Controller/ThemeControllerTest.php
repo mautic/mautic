58f83f9aa6adaf6e3cf8ecb971ab48d1cf71e9ff
John Linhart <jan@linhart.email>
2024-01-11 15:49:56 +0100

    Removing a debug line from the test

--- app/bundles/CoreBundle/Tests/Functional/Controller/ThemeControllerTest.php
+++ app/bundles/CoreBundle/Tests/Functional/Controller/ThemeControllerTest.php
@@ -13,7 +13,6 @@ final class ThemeControllerTest extends MauticMysqlTestCase
     public function testDeleteTheme(): void
     {
         $this->client->request(Request::METHOD_POST, 's/themes/batchDelete?ids=[%22aurora%22]');
-        echo $this->client->getResponse()->getContent();
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode(), $this->client->getResponse()->getContent());
         $this->assertStringContainsString('aurora is the default theme and therefore cannot be removed.', $this->client->getResponse()->getContent());
     }
