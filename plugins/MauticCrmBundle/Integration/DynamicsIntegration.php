ecc7f5ab82e035756698b055e63aa59214e4bf72
Tim Mallezie <tim.mallezie@dropsolid.com>
2024-01-12 16:25:06 +0100

    Rebase dynamics UTC patch for M5.

--- plugins/MauticCrmBundle/Integration/DynamicsIntegration.php
+++ plugins/MauticCrmBundle/Integration/DynamicsIntegration.php
@@ -2,6 +2,8 @@
 
 namespace MauticPlugin\MauticCrmBundle\Integration;
 
+use Datetime;
+use DateTimeZone;
 use Mautic\LeadBundle\Entity\Company;
 use Mautic\LeadBundle\Entity\Lead;
 use Mautic\LeadBundle\Helper\IdentifyCompanyHelper;
@@ -354,6 +356,17 @@ class DynamicsIntegration extends CrmAbstractIntegration
         return false;
     }
 
+     /**
+      * Convert to UTC date string for API call.
+      */
+    public function getFilterDateUTC(string $paramDate): string
+    {
+        $startDate = new DateTime($paramDate);
+        $startDate->setTimezone(new DateTimeZone('UTC'));
+
+        return sprintf('modifiedon ge %sZ', $startDate->format('Y-m-d\TH:i:s'));
+    }
+
     /**
      * @param array      $params
      * @param array|null $query
@@ -380,7 +393,7 @@ class DynamicsIntegration extends CrmAbstractIntegration
                 $oparams['request_settings']['headers']['Prefer'] = 'odata.maxpagesize='.$MAX_RECORDS;
                 $oparams['$select']                               = implode(',', $mappedData);
                 if (isset($params['fetchAll'], $params['start']) && !$params['fetchAll']) {
-                    $oparams['$filter'] = sprintf('modifiedon ge %sZ', substr($params['start'], 0, -6));
+                    $oparams['$filter'] = $this->getFilterDateUTC($params['start']);
                 }
 
                 if (isset($params['output']) && $params['output']->getVerbosity() < OutputInterface::VERBOSITY_VERBOSE) {
