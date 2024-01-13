4cd6c683b2c3cc55cac21d5b5c7c1ca7c3b2dd86
Mattias Michaux <mattias.michaux@dropsolid.com>
2024-01-13 19:23:56 +0100

    skip the compilerpass check during installation, as the PDO requirement is already checked there

--- app/bundles/CoreBundle/DependencyInjection/Compiler/RequirementsPass.php
+++ app/bundles/CoreBundle/DependencyInjection/Compiler/RequirementsPass.php
@@ -10,6 +10,11 @@ class RequirementsPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
+        if (defined('MAUTIC_INSTALLER')) {
+            // The installer itself does the PDO check, so no need to validate here during installation.
+            return;
+        }
+
         if (!class_exists('PDO') || !in_array('mysql', \PDO::getAvailableDrivers(), true)) {
             // We need to check this on boot, as later in the process is too late to show a message that makes the issue clear.
             throw new MessageOnlyErrorHandlerException('Mautic requires the PHP pdo_mysql extension to work. Please ensure this extension is installed and enabled');
