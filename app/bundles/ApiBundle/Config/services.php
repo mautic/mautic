1ea39cebb585dcba80d546c8991ad5a9c40a6fe5
Jan Linhart <admin@escope.cz>
2024-01-14 20:57:32 +0100

    Moving services from config.php to services.php in ApiBundle

--- app/bundles/ApiBundle/Config/services.php
+++ app/bundles/ApiBundle/Config/services.php
@@ -31,5 +31,29 @@ return function (ContainerConfigurator $configurator): void {
         ->arg('$oAuth2Server', service('fos_oauth_server.server'))
         ->arg('$clientManager', service('fos_oauth_server.client_manager.default'));
 
-    $services->alias('mautic.api.model.client', \Mautic\ApiBundle\Model\ClientModel::class);
+    $services->get(Mautic\ApiBundle\EventListener\PreAuthorizationEventListener::class)
+        ->tag(
+            'kernel.event_listener',
+            [
+                'event'  => 'fos_oauth_server.pre_authorization_process',
+                'method' => 'onPreAuthorizationProcess',
+            ]
+        )
+        ->tag(
+            'kernel.event_listener',
+            [
+                'event'  => 'fos_oauth_server.post_authorization_process',
+                'method' => 'onPostAuthorizationProcess',
+            ]
+        );
+
+    $services->get(Mautic\ApiBundle\Form\Validator\Constraints\OAuthCallbackValidator::class)
+        ->tag('validator.constraint_validator');
+
+    $services->get(Mautic\ApiBundle\Helper\EntityResultHelper::class)
+        ->deprecate('mautic/mautic', '5.1.0', 'Use as `new EntityResultHelper()` instead of a service.');
+
+    $services->alias('mautic.api.model.client', Mautic\ApiBundle\Model\ClientModel::class);
+    $services->alias('mautic.api.helper.entity_result', Mautic\ApiBundle\Helper\EntityResultHelper::class);
+    $services->alias('mautic.validator.oauthcallback', Mautic\ApiBundle\Form\Validator\Constraints\OAuthCallbackValidator::class);
 };
