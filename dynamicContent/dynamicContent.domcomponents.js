a369359134c5521f23898b950935f3573c1edf60
lordrembo <registrations@rembo.me>
2024-01-19 15:56:25 +0100

    fix breaking change grapesjs 0.20.1, for registering custom components

--- dynamicContent/dynamicContent.domcomponents.js
+++ dynamicContent/dynamicContent.domcomponents.js
@@ -10,66 +10,61 @@ export default class DynamicContentDomComponents {
     const tagName = ContentService.isMjmlMode(editor) ? 'mj-text' : 'div';
     const baseType = dc.getType(baseTypeName);
     const baseModel = baseType.model;
-    const model = baseModel.extend(
-      {
-        defaults: {
-          ...baseModel.prototype.defaults,
-          name: 'Dynamic Content',
-          tagName,
-          draggable: '[data-gjs-type=cell],[data-gjs-type=mj-column]',
-          droppable: false,
-          editable: false,
-          stylable: false,
-          propagate: ['droppable', 'editable'],
-          attributes: {
-            'data-gjs-type': 'dynamic-content', // Type for GrapesJS
-            'data-slot': 'dynamicContent', // used to find the DC component on the canvas for e.g. token transformation
-          },
+    const model = {
+      defaults: {
+        name: 'Dynamic Content',
+        tagName,
+        draggable: '[data-gjs-type=cell],[data-gjs-type=mj-column]',
+        droppable: false,
+        editable: false,
+        stylable: false,
+        propagate: ['droppable', 'editable'],
+        attributes: {
+          'data-gjs-type': 'dynamic-content', // Type for GrapesJS
+          'data-slot': 'dynamicContent', // used to find the DC component on the canvas for e.g. token transformation
         },
-        /**
-         * Initilize the component
-         */
-        init() {
-          // link component to the corresponding html store item
-          this.em
-            .get('Commands')
-            .run('preset-mautic:link-component-to-store-item', { component: this });
-
-          // Add toolbar edit button if it's not already in
-          const toolbar = this.get('toolbar');
-          const id = 'toolbar-dynamic-content';
-
-          if (!toolbar.filter((tlb) => tlb.id === id).length) {
-            toolbar.unshift({
-              id,
-              command: 'preset-mautic:dynamic-content-open',
-              attributes: { class: 'fa fa-pencil-square-o' },
-            });
-          }
-        },
-        // @todo: show the store items default content on the canvas
-        // updated(property, value, prevValue) {
-        //   console.log('Local hook: model.updated', {
-        //     property,
-        //     value,
-        //     prevValue,
-        //   });
-        // },
       },
-      {
-        // Dynamic Content component detection
-        isComponent(el) {
-          if (el.getAttribute && el.getAttribute('data-slot') === 'dynamicContent') {
-            return {
-              type: 'dynamic-content',
-            };
-          }
-          return false;
-        },
-      }
-    );
+      /**
+        * Initilize the component
+        */
+      init() {
+        // link component to the corresponding html store item
+        this.em
+          .get('Commands')
+          .run('preset-mautic:link-component-to-store-item', { component: this });
 
-    const view = baseType.view.extend({
+        // Add toolbar edit button if it's not already in
+        const toolbar = this.get('toolbar');
+        const id = 'toolbar-dynamic-content';
+
+        if (!toolbar.filter((tlb) => tlb.id === id).length) {
+          toolbar.unshift({
+            id,
+            command: 'preset-mautic:dynamic-content-open',
+            attributes: { class: 'fa fa-pencil-square-o' },
+          });
+        }
+      },
+      // @todo: show the store items default content on the canvas
+      // updated(property, value, prevValue) {
+      //   console.log('Local hook: model.updated', {
+      //     property,
+      //     value,
+      //     prevValue,
+      //   });
+      // },
+      // Dynamic Content component detection
+      isComponent(el) {
+        if (el.getAttribute && el.getAttribute('data-slot') === 'dynamicContent') {
+          return {
+            type: 'dynamic-content',
+          };
+        }
+        return false;
+      }
+    };
+
+    const view = {
       attributes: {
         style: 'pointer-events: all; display: table; width: 100%;user-select: none;',
       },
@@ -98,7 +93,7 @@ export default class DynamicContentDomComponents {
       //     .get('Commands')
       //     .run('preset-mautic:dynamic-content-delete-store-item', { component });
       // },
-    });
+    };
 
     // add the Dynamic Content component
     dc.addType('dynamic-content', {
