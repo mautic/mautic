846f75a704a8a9a58d356f17b09b21f7d8bf17c6
Tejas Navghane <ts.navghane@gmail.com>
2024-01-22 18:26:05 +0530

    Add sendtodnc permissions for email:campaigns

--- app/bundles/EmailBundle/Security/Permissions/EmailPermissions.php
+++ app/bundles/EmailBundle/Security/Permissions/EmailPermissions.php
@@ -3,6 +3,7 @@
 namespace Mautic\EmailBundle\Security\Permissions;
 
 use Mautic\CoreBundle\Security\Permissions\AbstractPermissions;
+use Mautic\UserBundle\Form\Type\PermissionListType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class EmailPermissions extends AbstractPermissions
@@ -10,6 +11,11 @@ class EmailPermissions extends AbstractPermissions
     public function __construct($params)
     {
         parent::__construct($params);
+        $this->permissions = [
+            'campaigns' => [
+                'sendtodnc' => 2,
+            ],
+        ];
         $this->addStandardPermissions('categories');
         $this->addExtendedPermissions('emails');
     }
@@ -23,5 +29,20 @@ class EmailPermissions extends AbstractPermissions
     {
         $this->addStandardFormFields('email', 'categories', $builder, $data);
         $this->addExtendedFormFields('email', 'emails', $builder, $data);
+
+        $builder->add(
+            'email:campaigns',
+            PermissionListType::class,
+            [
+                'choices'           => [
+                    'mautic.email.permissions.sendtodnc' => 'sendtodnc',
+                ],
+                'choices_as_values' => true,
+                'label'             => 'mautic.email.permissions.campaigns',
+                'data'              => (!empty($data['campaigns']) ? $data['campaigns'] : []),
+                'bundle'            => 'email',
+                'level'             => 'campaigns',
+            ]
+        );
     }
 }
