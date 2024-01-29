2fe05401e0c56145c30c3ce3fe6f5137def60133
Patryk Gruszka <patryk.gruszka@comarch.pl>
2024-01-29 13:33:12 +0100

    fix: [DPMMA-375] restore campaign service

--- app/bundles/CampaignBundle/Config/config.php
+++ app/bundles/CampaignBundle/Config/config.php
@@ -360,6 +360,15 @@ return [
                 ],
             ],
         ],
+        'services' => [
+            'mautic.campaign.service.campaign'=> [
+                'class'     => \Mautic\CampaignBundle\Service\Campaign::class,
+                'arguments' => [
+                    'mautic.campaign.repository.campaign',
+                    'mautic.email.repository.email',
+                ],
+            ],
+        ],
         'fixtures' => [
             'mautic.campaign.fixture.campaign' => [
                 'class'    => \Mautic\CampaignBundle\DataFixtures\ORM\CampaignData::class,
