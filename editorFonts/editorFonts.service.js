c7df7c7095d4cba959be1e2f3c128274b0af0f27
lordrembo <registrations@rembo.me>
2024-01-29 18:14:29 +0100

    add fallback if no list of fonts found, use options instead

--- editorFonts/editorFonts.service.js
+++ editorFonts/editorFonts.service.js
@@ -17,20 +17,30 @@ export default class EditorFontsService {
       return;
     }
 
-    const fontList = fontProperty.get('list');
-    EditorFontsService.updateFontList(editor, fontList);
-    EditorFontsService.sortFontList(fontList);
+    // first check if we have the 'list' property to work with
+    let propertyType = 'list';
+    let fontList = fontProperty.get('list');
 
-    fontProperty.set('list', fontList);
+    // use 'options' if 'list' doesn't exist
+    if (typeof fontList === 'undefined') {
+      propertyType = 'options';
+      fontList = fontProperty.get('options');
+    }
+
+    EditorFontsService.updateFontList(editor, fontList, propertyType);
+    EditorFontsService.sortFontList(fontList, propertyType);
+
+    fontProperty.set(propertyType, fontList);
     styleManager.render();
   }
 
-  static updateFontList(editor, fontList) {
+  static updateFontList(editor, fontList, propertyType) {
     const canvasHead = editor.Canvas.getDocument().head;
 
     mauticEditorFonts.forEach((item) => {
       if (!fontList.find((element) => element.name === item.name)) {
-        fontList.push({ value: item.font, name: item.name });
+        if (propertyType === 'list') fontList.push({ value: item.font, name: item.name });
+        if (propertyType === 'options') fontList.push({ id: item.font, label: item.name });
 
         if (item.url && canvasHead) {
           EditorFontsService.appendFontStyleLink(canvasHead, item.url);
@@ -41,8 +51,9 @@ export default class EditorFontsService {
     return fontList;
   }
 
-  static sortFontList(fontList) {
-    fontList.sort((a, b) => (a.name < b.name ? -1 : 1));
+  static sortFontList(fontList, propertyType) {
+    if (propertyType === 'list') fontList.sort((a, b) => (a.name < b.name ? -1 : 1));
+    if (propertyType === 'options') fontList.sort((a, b) => (a.label < b.label ? -1 : 1));
 
     return fontList;
   }
