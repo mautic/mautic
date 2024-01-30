200c396e063dfc2c1160743f0347511f632089f9
Saurabh Gupta <saurabh.gupta@acquia.com>
2024-01-30 15:35:54 +0530

    added service as it was not autowired

--- app/bundles/DashboardBundle/Config/config.php
+++ app/bundles/DashboardBundle/Config/config.php
@@ -42,11 +42,20 @@ return [
     'services' => [
         'other' => [
             'mautic.dashboard.widget' => [
-                'class'     => \Mautic\DashboardBundle\Dashboard\Widget::class,
+                'class'     => Mautic\DashboardBundle\Dashboard\Widget::class,
                 'arguments' => [
                     'mautic.dashboard.model.dashboard',
                     'mautic.helper.user',
                     'session',
+                ],
+            ],
+            'mautic.dashboard.widget_detail.event.factory' => [
+                'class'     => Mautic\DashboardBundle\Event\WidgetDetailEventFactory::class,
+                'arguments' => [
+                    'translator',
+                    'mautic.cache.provider',
+                    'mautic.security',
+                    'mautic.helper.user',
                 ],
             ],
         ],
