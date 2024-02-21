d7d632f9b3b83465354d2d9b4328ea7ba1d9dda1
John Linhart <jan@linhart.email>
2024-02-21 14:44:39 +0100

    Updates for M5

--- app/bundles/IntegrationsBundle/Event/ConfigAuthUrlEvent.php
+++ app/bundles/IntegrationsBundle/Event/ConfigAuthUrlEvent.php
@@ -2,36 +2,15 @@
 
 declare(strict_types=1);
 
-/*
- * @copyright   2021 Mautic contributors. All rights reserved
- * @author      Acquia
- *
- * @link        https://www.mautic.org
- *
- * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- */
-
 namespace Mautic\IntegrationsBundle\Event;
 
 use Mautic\PluginBundle\Entity\Integration;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 final class ConfigAuthUrlEvent extends Event
 {
-    /**
-     * @var Integration
-     */
-    private $integrationConfiguration;
-
-    /**
-     * @var string
-     */
-    private $authUrl;
-
-    public function __construct(Integration $integrationConfiguration, string $authUrl)
+    public function __construct(private Integration $integrationConfiguration, private string $authUrl)
     {
-        $this->integrationConfiguration = $integrationConfiguration;
-        $this->authUrl                  = $authUrl;
     }
 
     public function getIntegrationConfiguration(): Integration
