5b25ba90c664870260518d7b1c81231e0d38c19b
Syrgak-Alan <syrgak.alan@gmail.com>
2024-02-22 10:59:53 +0100

    checking whether response contains audit information

--- app/bundles/UserBundle/Tests/Functional/Controller/UserControllerFunctionalTest.php
+++ app/bundles/UserBundle/Tests/Functional/Controller/UserControllerFunctionalTest.php
@@ -4,28 +4,13 @@ declare(strict_types=1);
 
 namespace Mautic\UserBundle\Tests\Functional\Controller;
 
-use Doctrine\Persistence\ManagerRegistry;
 use Mautic\CoreBundle\Entity\AuditLog;
 use Mautic\CoreBundle\Entity\AuditLogRepository;
-use Mautic\CoreBundle\Factory\MauticFactory;
-use Mautic\CoreBundle\Factory\ModelFactory;
-use Mautic\CoreBundle\Helper\CoreParametersHelper;
-use Mautic\CoreBundle\Helper\LanguageHelper;
-use Mautic\CoreBundle\Helper\UserHelper;
 use Mautic\CoreBundle\Model\AuditLogModel;
-use Mautic\CoreBundle\Security\Permissions\CorePermissions;
-use Mautic\CoreBundle\Service\FlashBag;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
-use Mautic\CoreBundle\Translation\Translator;
-use Mautic\FormBundle\Helper\FormFieldHelper;
-use Mautic\UserBundle\Controller\UserController;
 use Mautic\UserBundle\Entity\User;
 use Mautic\UserBundle\Model\UserModel;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserControllerFunctionalTest extends MauticMysqlTestCase
 {
@@ -48,29 +33,11 @@ class UserControllerFunctionalTest extends MauticMysqlTestCase
 
         $userModel->method('getEntity')->willReturn($user);
 
-        $languageHelper       = $this->createMock(LanguageHelper::class);
-        $hasher               = $this->createMock(UserPasswordHasherInterface::class);
-        $formFactory          = $this->createMock(FormFactoryInterface::class);
-        $fieldHelper          = $this->createMock(FormFieldHelper::class);
-        $managerRegistry      = $this->createMock(ManagerRegistry::class);
-        $factory              = $this->createMock(MauticFactory::class);
-        $modelFactory         = $this->createMock(ModelFactory::class);
-        $userHelper           = $this->createMock(UserHelper::class);
-        $coreParametersHelper = $this->createMock(CoreParametersHelper::class);
-        $dispatcher           = $this->createMock(EventDispatcherInterface::class);
-        $translator           = $this->createMock(Translator::class);
-        $flashBag             = $this->createMock(FlashBag::class);
-        $requestStack         = $this->createMock(RequestStack::class);
-        $security             = $this->createMock(CorePermissions::class);
+        $crawler        = $this->client->request(Request::METHOD_GET, '/s/users/edit/1');
+        $clientResponse = $this->client->getResponse();
 
-        $controller = new UserController($formFactory, $fieldHelper, $managerRegistry,
-            $factory, $modelFactory, $userHelper, $coreParametersHelper, $dispatcher,
-            $translator, $flashBag, $requestStack, $security);
-
-        $response = $controller->editAction(new Request(), $languageHelper, $hasher, 1, true);
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertStringContainsString('testuser', $response->getContent());
+        $this->assertEquals(200, $clientResponse->getStatusCode());
+        $this->assertStringContainsString('<!-- Recent activity block(audit_log table) -->', $clientResponse->getContent());
     }
 
     public function auditLogSetter(int $userId, string $userName, string $bundle,
