bf6ebbdc6a7d2fb0d88ee47b01fde08b7fe76390
aarohiprasad <aarohi.prasad@acquia.com>
2024-02-26 13:21:01 +0530

    fixed changed exception and fixed styling and code standars

--- app/migrations/Version20190410143658.php
+++ app/migrations/Version20190410143658.php
@@ -1,42 +1,31 @@
 <?php
 
-/*
- * @package     Mautic
- * @copyright   2019 Mautic Contributors. All rights reserved.
- * @author      Mautic
- * @link        http://mautic.org
- * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- */
-
 namespace Mautic\Migrations;
 
-use Doctrine\DBAL\Migrations\SkipMigrationException;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Exception\SkipMigration;
 use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 
 class Version20190410143658 extends AbstractMauticMigration
 {
     /**
-     * @param Schema $schema
-     *
-     * @throws SkipMigrationException
      * @throws \Doctrine\DBAL\Schema\SchemaException
      */
-    public function preUp(Schema $schema)
+    public function preUp(Schema $schema): void
     {
         $newIndexName = $this->getNewIndexName();
         $tableName    = $this->getTableName();
         $table        = $schema->getTable($tableName);
 
-        if ($table->hasIndex($newIndexName) === true) {
-            throw new SkipMigrationException('Schema includes this migration');
+        if (true === $table->hasIndex($newIndexName)) {
+            throw new SkipMigration('Schema includes this migration');
         }
     }
 
     /**
-     * @param Schema $schema
+     * @throws \Doctrine\DBAL\Schema\SchemaException
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         $newIndexName = $this->getNewIndexName();
         $tableName    = $this->getTableName();
