14cad366486ac937439f7f6ba1392778e3a77e3e
Syrgak-Alan <syrgak.alan@gmail.com>
2024-03-21 16:19:43 +0100

    phpstan issue2

--- app/bundles/CoreBundle/Form/Type/ThemeListType.php
+++ app/bundles/CoreBundle/Form/Type/ThemeListType.php
@@ -28,7 +28,7 @@ class ThemeListType extends AbstractType
 
                     // Hide themes that are marked as hidden
                     foreach ($this->themeHelper->getInstalledThemes($options['feature'], true, true) as $themeKey => $themeInfo) {
-                        if (in_array('hide', (array) $themeInfo['config']) && true === $themeInfo['config']['hide']) {
+                        if (isset($themeInfo['config']['hide']) && true === $themeInfo['config']['hide']) {
                             unset($themes[$themeKey]);
                         }
                     }
