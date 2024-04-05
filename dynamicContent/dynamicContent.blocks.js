692a0fd973784b71b57b717a22262bec129e4efa
lordrembo <registrations@rembo.me>
2024-04-05 13:11:07 +0200

    replace Dynamic Content icon with an svg to be consistent with the other Block icons

--- dynamicContent/dynamicContent.blocks.js
+++ dynamicContent/dynamicContent.blocks.js
@@ -16,7 +16,9 @@ export default class DynamicContentBlocks {
       label: Mautic.translate('grapesjsbuilder.dynamicContentBlockLabel'),
       activate: true,
       select: true,
-      attributes: { class: 'fa fa-tag' },
+      media: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/>
+      </svg>`,
       content: {
         type: 'dynamic-content',
         content: '{dynamiccontent="Dynamic Content"}',
