3667940b6cbcf586e2e243e0ad04771eb45f8b58
Zdeno Kuzmany <zdeno@kuzmany.biz>
2024-04-12 06:38:55 +0200

    Add test for GDPR maintenance command in CleanupMaintenanceCommandTest

--- app/bundles/CoreBundle/Tests/Command/CleanupMaintenanceCommandTest.php
+++ app/bundles/CoreBundle/Tests/Command/CleanupMaintenanceCommandTest.php
@@ -9,6 +9,8 @@ use Mautic\LeadBundle\Entity\Lead;
 
 class CleanupMaintenanceCommandTest extends MauticMysqlTestCase
 {
+    protected $useCleanupRollback = false;
+
     /**
      * @throws \Exception
      */
@@ -24,5 +26,45 @@ class CleanupMaintenanceCommandTest extends MauticMysqlTestCase
         $this->runcommand('mautic:maintenance:cleanup', ['--days-old' => 180, '--no-interaction' => true]);
 
         $this->assertNull($this->getContainer()->get('mautic.lead.model.lead')->getEntity($contactId));
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testGdprMaintenanceCommand(): void
+    {
+        $gdprThreshold = '-4 years';
+        $lead          = $this->getLead($gdprThreshold);
+
+        $contactId = $lead->getId();
+
+        $this->testSymfonyCommand('mautic:maintenance:cleanup', ['--gdpr' => 1, '--no-interaction' => true]);
+
+        $this->assertNull($this->getContainer()->get('mautic.lead.model.lead')->getEntity($contactId));
+
+        $this->setUpSymfony($this->configParams + ['gdpr_user_purge_threshold' => '1825']);
+
+        $lead          = $this->getLead($gdprThreshold);
+        $contactId     = $lead->getId();
+
+        $this->testSymfonyCommand('mautic:maintenance:cleanup', ['--gdpr' => 1, '--no-interaction' => true]);
+
+        $this->assertNotNull($this->getContainer()->get('mautic.lead.model.lead')->getEntity($contactId));
+    }
+
+    /**
+     * @throws \Doctrine\ORM\Exception\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    protected function getLead(string $gdprThreshold): Lead
+    {
+        $lead = new Lead();
+        $lead->setLastActive(new \DateTime('-1 year'));
+        $lead->setDateIdentified(new \DateTime($gdprThreshold));
+        $lead->setLastActive(new \DateTime($gdprThreshold));
+        $this->em->persist($lead);
+        $this->em->flush();
+
+        return $lead;
     }
 }
