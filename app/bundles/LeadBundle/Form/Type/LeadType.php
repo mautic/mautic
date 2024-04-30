a7cb921243283e18da36410ddb1affa7eaaf4672
andersonjeccel <116097999+andersonjeccel@users.noreply.github.com>
2024-04-30 17:45:23 -0300

    contact

--- app/bundles/LeadBundle/Form/Type/LeadType.php
+++ app/bundles/LeadBundle/Form/Type/LeadType.php
@@ -166,7 +166,11 @@ class LeadType extends AbstractType
         );
 
         if (!$options['isShortForm']) {
-            $builder->add('buttons', FormButtonsType::class);
+            $builder->add('buttons', FormButtonsType::class,
+            [
+                'apply_text' => false,
+                'save_text'  => 'mautic.core.form.save',
+            ]);
         } else {
             $builder->add(
                 'buttons',
