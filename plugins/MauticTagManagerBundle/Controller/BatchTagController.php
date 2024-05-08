9ffd50540966383b4ae723e26060cb23f6211330
lenonleite <lenonleite@gmail.com>
2024-05-09 00:13:24 +0100

    - fix phpstan

--- plugins/MauticTagManagerBundle/Controller/BatchTagController.php
+++ plugins/MauticTagManagerBundle/Controller/BatchTagController.php
@@ -2,49 +2,17 @@
 
 namespace MauticPlugin\MauticTagManagerBundle\Controller;
 
-use Doctrine\Persistence\ManagerRegistry;
 use Mautic\CoreBundle\Controller\AbstractFormController;
-use Mautic\CoreBundle\Factory\MauticFactory;
-use Mautic\CoreBundle\Factory\ModelFactory;
-use Mautic\CoreBundle\Helper\CoreParametersHelper;
-use Mautic\CoreBundle\Helper\UserHelper;
-use Mautic\CoreBundle\Security\Permissions\CorePermissions;
-use Mautic\CoreBundle\Service\FlashBag;
-use Mautic\CoreBundle\Translation\Translator;
-use Mautic\FormBundle\Helper\FormFieldHelper;
 use MauticPlugin\MauticTagManagerBundle\Form\Type\BatchTagType;
 use MauticPlugin\MauticTagManagerBundle\Model\TagModel;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 
 class BatchTagController extends AbstractFormController
 {
-    public function __construct(
-        protected FormFactoryInterface $formFactory,
-        protected FormFieldHelper $fieldHelper,
-        ManagerRegistry $managerRegistry,
-        MauticFactory $factory,
-        ModelFactory $modelFactory,
-        UserHelper $userHelper,
-        CoreParametersHelper $coreParametersHelper,
-        EventDispatcherInterface $dispatcher,
-        Translator $translator,
-        FlashBag $flashBag,
-        RequestStack $requestStack,
-        CorePermissions $security,
-        private TagModel $tagModel,
-    ) {
-        parent::__construct($managerRegistry, $factory, $modelFactory, $userHelper, $coreParametersHelper, $dispatcher, $translator, $flashBag, $requestStack, $security);
-    }
-
     public function indexAction(): Response
     {
-        $model = $this->getModel('tagmanager.tag');
-        \assert($model instanceof TagModel);
         $route = $this->generateUrl('mautic_tagmanager_batch_set_action');
 
         $form = $this->createForm(BatchTagType::class, [],
@@ -80,7 +48,9 @@ class BatchTagController extends AbstractFormController
 
     public function execAction(Request $request): JsonResponse
     {
-        $params = $request->get('batch_tag');
+        $params   = $request->get('batch_tag');
+        $tagModel = $this->getModel('tagmanager.tag');
+        assert($tagModel instanceof TagModel);
         $ids    = empty($params['ids']) ? [] : json_decode($params['ids']);
         if (empty($ids)) {
             $this->addFlashMessage('mautic.core.error.ids.missing');
@@ -111,11 +81,11 @@ class BatchTagController extends AbstractFormController
         }
 
         if (!empty($tagsToAdd)) {
-            $result['added'] = $this->tagModel->getRepository()->addTagsToLeads($ids, $tagsToAdd);
+            $tagModel->getRepository()->addTagsToLeads($ids, $tagsToAdd);
         }
 
         if (!empty($tagsToRemove)) {
-            $result['removed'] = $this->tagModel->getRepository()->removeTagsFromLeads($ids, $tagsToRemove);
+            $tagModel->getRepository()->removeTagsFromLeads($ids, $tagsToRemove);
         }
 
         $this->addFlashMessage('mautic.lead.batch_leads_affected', [
