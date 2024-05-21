ad18e4c03fd6239988143e4edcad30bdf995ed67
andersonjeccel <116097999+andersonjeccel@users.noreply.github.com>
2024-05-21 15:18:21 -0300

    Fetch IP Lookup Data Store button

--- app/bundles/CoreBundle/Form/Type/IpLookupDownloadDataStoreButtonType.php
+++ app/bundles/CoreBundle/Form/Type/IpLookupDownloadDataStoreButtonType.php
@@ -33,7 +33,7 @@ class IpLookupDownloadDataStoreButtonType extends AbstractType
             [
                 'label' => ($localDataExists) ? 'mautic.core.ip_lookup.update_data' : 'mautic.core.ip_lookup.fetch_data',
                 'attr'  => [
-                    'class'   => 'btn btn-'.($localDataExists ? 'success' : 'danger'),
+                    'class'   => 'btn btn-tertiary btn-small',
                     'onclick' => 'Mautic.downloadIpLookupDataStore()',
                 ],
             ]
