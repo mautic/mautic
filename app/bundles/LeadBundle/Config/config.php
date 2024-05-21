ea93d42c48eee9ac080337a1ebeb4e11f2d9fa55
Rohit Pavaskar <rohitpavaskar.19@gmail.com>
2024-05-21 14:41:36 +0530

    File import enhancement for contact import (#13750)
    * Merge pull request #1134 from acquia/MAUT-4444-file-import
    
    MAUT-4444 / Connection close in header
    
    * Resolved conflicts
    
    * Change root_dir to project_dir
    
    * Fixed Test case
    
    * PHPSTAN
    
    ---------
    
    Co-authored-by: lukassykora <lukas.sykora@acquia.com>
    Co-authored-by: Rohit Pavaskar <rohit.pavaskar@acquia.com>
    Co-authored-by: John Linhart <admin@escope.cz>

--- app/bundles/LeadBundle/Config/config.php
+++ app/bundles/LeadBundle/Config/config.php
@@ -970,5 +970,6 @@ return [
         'contact_export_dir'                                                                    => '%mautic.application_dir%/media/files/temp',
         'contact_export_batch_size'                                                             => 20000,
         'contact_allow_multiple_companies'                                                      => true,
+        'import_leads_dir'                                                                      => '%kernel.project_dir%/var/import',
     ],
 ];
