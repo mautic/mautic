aec69b159dec4ff3b104b62eea332570e59d6f31
Abhisek Mazumdar <abhisek.mazumdar@dropsolid.com>
2024-06-14 19:00:22 +0530

    Use JMS EventSubscriber for adding customMjml.

---
+++ plugins/GrapesJsBuilderBundle/EventSubscriber/SerializerSubscriber.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace MauticPlugin\GrapesJsBuilderBundle\EventSubscriber;
+
+use JMS\Serializer\EventDispatcher\Events;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use JMS\Serializer\JsonSerializationVisitor;
+use JMS\Serializer\Metadata\StaticPropertyMetadata;
+use Mautic\EmailBundle\Entity\Email;
+use Mautic\InstallBundle\Install\InstallService;
+use MauticPlugin\GrapesJsBuilderBundle\Model\GrapesJsBuilderModel;
+
+class SerializerSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private GrapesJsBuilderModel $grapesJsBuilderModel,
+        private InstallService $installer,
+    ) {
+    }
+
+    /**
+     * @return array<int, array<string, string>>
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            [
+                'event'  => Events::POST_SERIALIZE,
+                'method' => 'addCustomMJML',
+            ],
+        ];
+    }
+
+    public function addCustomMJML(ObjectEvent $event): void
+    {
+        if (!$this->installer->checkIfInstalled()) {
+            return;
+        }
+
+        $object = $event->getObject();
+
+        if (!$object instanceof Email) {
+            return;
+        }
+
+        // Get the grapesJsBuilder data.
+        $grapesJsBuilder = $this->grapesJsBuilderModel->getRepository()->findOneBy(['email' => $object]);
+
+        // Add it to the serialized data.
+        $visitor = $event->getContext()->getVisitor();
+        if ($visitor instanceof JsonSerializationVisitor && !empty($grapesJsBuilder->getCustomMjml())) {
+            $visitor->visitProperty(
+                new StaticPropertyMetadata(
+                    '', 'grapesjsbuilder', ['customMjml' => $grapesJsBuilder->getCustomMjml()]
+                ), ['customMjml' => $grapesJsBuilder->getCustomMjml()]
+            );
+        }
+    }
+}
