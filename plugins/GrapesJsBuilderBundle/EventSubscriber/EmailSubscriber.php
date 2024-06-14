a8e9f0a1c52200bd9643a216ba0598bc85bf1fe9
Abhisek Mazumdar <abhisek.mazumdar@dropsolid.com>
2024-06-14 14:15:06 +0530

    Revert "Add customMjml to API responses."
    This reverts commit 3841a741dcfc768b263662ecda0daa02d34bfa75.

--- plugins/GrapesJsBuilderBundle/EventSubscriber/EmailSubscriber.php
+++ plugins/GrapesJsBuilderBundle/EventSubscriber/EmailSubscriber.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace MauticPlugin\GrapesJsBuilderBundle\EventSubscriber;
 
-use Mautic\ApiBundle\Helper\RequestHelper;
 use Mautic\EmailBundle\EmailEvents;
 use Mautic\EmailBundle\Event as Events;
 use MauticPlugin\GrapesJsBuilderBundle\Integration\Config;
 use MauticPlugin\GrapesJsBuilderBundle\Model\GrapesJsBuilderModel;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 
 class EmailSubscriber implements EventSubscriberInterface
 {
@@ -26,7 +23,6 @@ class EmailSubscriber implements EventSubscriberInterface
         return [
             EmailEvents::EMAIL_POST_SAVE   => ['onEmailPostSave', 0],
             EmailEvents::EMAIL_POST_DELETE => ['onEmailDelete', 0],
-            KernelEvents::RESPONSE         => ['onKernelResponse', 0],
         ];
     }
 
@@ -56,46 +52,6 @@ class EmailSubscriber implements EventSubscriberInterface
 
         if ($grapesJsBuilder) {
             $this->grapesJsBuilderModel->getRepository()->deleteEntity($grapesJsBuilder);
-        }
-    }
-
-    /**
-     * Add customMjml property to Serialization.
-     */
-    public function onKernelResponse(ResponseEvent $event): void
-    {
-        $request = $event->getRequest();
-        // Early return if not an API request
-        if (!RequestHelper::isApiRequest($request)) {
-            return;
-        }
-
-        $response = $event->getResponse();
-        $content  = $response->getContent();
-
-        // Check for status code is 200 or error in response.
-        if (200 !== $response->getStatusCode() || str_contains($content, 'error')) {
-            return;
-        }
-
-        $data = json_decode($content, true);
-        // Early return if no email data is present
-        if (!isset($data['email'])) {
-            return;
-        }
-
-        $email = reset($data);
-        // Early return if error is found in email data
-        if (isset($email['error'])) {
-            return;
-        }
-
-        // Get and update the responses for the customMjml from the GrapesJS Builder.
-        $grapesJsBuilder = $this->grapesJsBuilderModel->getRepository()->findOneBy(['email' => $email['id']]);
-        if ($grapesJsBuilder && $customMjml = $grapesJsBuilder->getCustomMjml()) {
-            $email['grapesjsbuilder']['customMjml'] = $customMjml;
-            $response->setContent(json_encode(['email' => $email]));
-            $event->setResponse($response);
         }
     }
 }
