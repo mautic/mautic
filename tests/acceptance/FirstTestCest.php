8bd011d42b563c5e7b7cc114083c2acdec47cfc0
Priyanshi Gaur <noxdot1134@gmail.com>
2024-06-15 00:29:03 +0530

    Use css selector in the test
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- tests/acceptance/FirstTestCest.php
+++ tests/acceptance/FirstTestCest.php
@@ -7,9 +7,9 @@ class FirstTestCest
         $I->amOnPage('/s/login'); // Navigate to the login page
 
         // Fill in login form with credentials
-        $I->fillField('//*[@id="username"]', 'admin');
-        $I->fillField('//*[@id="password"]', 'Maut1cR0cks!');
-        $I->click('//*[@id="main"]/div/div[1]/div/div/div/form/button');
+        $I->fillField('#username', 'admin');
+        $I->fillField('#password', 'Maut1cR0cks!');
+        $I->click('button[type=submit]');
 
         // Assert that login was successful
         $I->see('Dashboard'); // Check that the user is on the dashboard page
