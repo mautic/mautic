14d2811e9b206db9541a5e1d0fca777d718751cb
Putzwasser <26040044+putzwasser@users.noreply.github.com>
2024-06-15 22:41:00 +0200

    Fix: LeadEvent adds missing return type + unit test
    This adds the missing return type hint for `LeadEvent::getLead`. This also adds a simple unit test for `LeadEvent`

--- app/bundles/LeadBundle/Event/LeadEvent.php
+++ app/bundles/LeadBundle/Event/LeadEvent.php
@@ -7,10 +7,7 @@ use Mautic\LeadBundle\Entity\Lead;
 
 class LeadEvent extends CommonEvent
 {
-    /**
-     * @param bool $isNew
-     */
-    public function __construct(Lead &$lead, $isNew = false)
+    public function __construct(Lead &$lead, bool $isNew = false)
     {
         $this->entity = &$lead;
         $this->isNew  = $isNew;
@@ -18,10 +15,8 @@ class LeadEvent extends CommonEvent
 
     /**
      * Returns the Lead entity.
-     *
-     * @return Lead
      */
-    public function getLead()
+    public function getLead(): Lead
     {
         return $this->entity;
     }
