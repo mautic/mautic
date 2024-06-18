2faa04f78cc4c2ec414b973f2378b84f24129188
Putzwasser <26040044+putzwasser@users.noreply.github.com>
2024-06-18 12:14:12 +0200

    TASK: Removes comments

--- app/bundles/LeadBundle/Event/LeadEvent.php
+++ app/bundles/LeadBundle/Event/LeadEvent.php
@@ -7,25 +7,20 @@ use Mautic\LeadBundle\Entity\Lead;
 
 class LeadEvent extends CommonEvent
 {
-    public function __construct(Lead &$lead, bool $isNew = false)
-    {
+    public function __construct(
+        Lead &$lead,
+        protected bool $isNew = false
+    ) {
         $this->entity = &$lead;
-        $this->isNew  = $isNew;
     }
 
-    /**
-     * Returns the Lead entity.
-     */
     public function getLead(): Lead
     {
         return $this->entity;
     }
 
-    /**
-     * Sets the Lead entity.
-     */
     public function setLead(Lead $lead): void
     {
-        $this->entity = $lead;
+        $this->entity = &$lead;
     }
 }
