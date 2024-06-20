d5455f7540c22b3b3c15371b16f632d0d1876396
Putzwasser <26040044+putzwasser@users.noreply.github.com>
2024-06-20 12:22:21 +0200

    FIX: phpstan

--- app/bundles/PointBundle/Tests/Unit/Helper/EventHelperTest.php
+++ app/bundles/PointBundle/Tests/Unit/Helper/EventHelperTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 class EventHelperTest extends TestCase
 {
-    public function testEngagePointAction()
+    public function testEngagePointAction(): void
     {
         $lead = new Lead();
 
