fa9ac54b18b6ac1e90108168ecf7a553d6faaad5
Priyanshi Gaur <noxdot1134@gmail.com>
2024-06-27 18:24:53 +0530

    Add test to edit contact and remove commented out code
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- tests/acceptance/ContactManagementCest.php
+++ tests/acceptance/ContactManagementCest.php
@@ -28,9 +28,9 @@ class ContactManagementCest
         $I->fillField('#lead_lastname', 'QuickAddLastName');
         $I->fillField('#lead_email', 'quickadd@example.com');
         $I->fillField('#lead_tags_chosen input', 'TestTag');
-        $I->pressKey('#lead_tags_chosen input', \Facebook\WebDriver\WebDriverKeys::ENTER);
+        $I->pressKey('#lead_tags_chosen input', Facebook\WebDriver\WebDriverKeys::ENTER);
         $I->fillField('#lead_companies_chosen input', 'Mautic');
-        $I->pressKey('#lead_companies_chosen input', \Facebook\WebDriver\WebDriverKeys::ENTER);
+        $I->pressKey('#lead_companies_chosen input', Facebook\WebDriver\WebDriverKeys::ENTER);
 
         // Wait for the save button to be clickable and submit the form
         $I->waitForElementClickable('#MauticSharedModal > div > div > div.modal-footer > div > button.btn.btn-default.btn-save.btn-copy', 30);
@@ -42,7 +42,7 @@ class ContactManagementCest
 
         // Search for the contact we just created
         $I->fillField('#list-search', 'QuickAddFirstName');
-        $I->pressKey('#list-search', \Facebook\WebDriver\WebDriverKeys::ENTER);
+        $I->pressKey('#list-search', Facebook\WebDriver\WebDriverKeys::ENTER);
         $I->waitForElementVisible('#leadTable', 10); // Wait for the search results to appear
 
         // Verify the contact is in the list
@@ -67,16 +67,8 @@ class ContactManagementCest
         $I->fillField('#lead_firstname', 'FirstName');
         $I->fillField('#lead_lastname', 'LastName');
         $I->fillField('#lead_email', 'email@example.com');
-
-        // Fill Company field
-        // $I->click('#lead_companies_chosen');
-        // $I->fillField('#lead_companies_chosen > ul > li > input', 'TestCompany');
-        // $I->wait(2);
-        // $I->see('No matches found TestCompany');
-        // $I->fillField('#lead_companies_chosen > ul > li > input', 'Create');
-
         $I->fillField('#lead_tags_chosen input', 'TestTag');
-        $I->pressKey('#lead_tags_chosen input', \Facebook\WebDriver\WebDriverKeys::ENTER);
+        $I->pressKey('#lead_tags_chosen input', Facebook\WebDriver\WebDriverKeys::ENTER);
 
         // Scroll back to the top of the page
         $I->executeJS('window.scrollTo(0, 0);');
@@ -92,27 +84,40 @@ class ContactManagementCest
         // Click the close button on the contact details page
         $I->waitForElementClickable('#toolbar > div.std-toolbar.btn-group > a:nth-child(3)', 30);
         $I->click('a.btn.btn-default[href="/s/contacts"]');
-
-        // Verify that we are back on the contacts page
-        //$I->waitForElementVisible('h1.page-header-title', 30);
-        //$I->see('Contacts', 'h1.page-header-title');
-        //$I->makeScreenshot('after-contacts-check');
     }
 
-    public function viewContact(AcceptanceTester $I)
+    public function editContact(AcceptanceTester $I)
     {
-        // Go to contacts list
+        // Navigate to the contacts page
         $I->amOnPage('/s/contacts');
 
         // Grab the name of the first contact in the list
-        $contactName = $I->grabTextFrom('//*[@id="leadTable"]/tbody/tr[1]/td[2]/a/div[1]');
+        $contactName = $I->grabTextFrom('#leadTable > tbody > tr:nth-child(1) > td:nth-child(2) > a > div');
 
-        // Click on the first contact in the list
-        $I->click('//*[@id="leadTable"]/tbody/tr[1]/td[2]/a');
+        // Click on the contact name to view the contact details
+        $I->click(['link' => $contactName]);
 
-        $I->wait(2);
-
-        // Verify that the contact name is displayed correctly on the contact details page
+        // Wait for the contact details page to load and confirm we're on the correct page
+        $I->waitForText($contactName, 30);
         $I->see($contactName);
+
+        // Click on the edit button
+        $I->click('#toolbar > div.std-toolbar.btn-group > a:nth-child(1)');
+
+        // Wait for the edit form to be visible
+        $I->waitForElementVisible('#core > div.pa-md.bg-light-xs.bdr-b > h4', 30);
+        $I->see("Edit $contactName");
+
+        // Edit the first and last names
+        $I->fillField('#lead_firstname', 'Edited-First-Name');
+        $I->fillField('#lead_lastname', 'Edited-Last-Name');
+
+        // Save and close the form
+        $I->waitForElementClickable('#lead_buttons_save_toolbar', 30);
+        $I->click('#lead_buttons_save_toolbar');
+
+        // Verify the update message
+        $I->waitForText('Edited-First-Name Edited-Last-Name has been updated!', 30);
+        $I->see('Edited-First-Name Edited-Last-Name has been updated!');
     }
 }
