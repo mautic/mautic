71f244235c93a4467e3ad216b427d0fd8f0bf8b8
Priyanshi Gaur <noxdot1134@gmail.com>
2024-06-29 01:04:42 +0530

    Add test to delete contact
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- tests/acceptance/ContactManagementCest.php
+++ tests/acceptance/ContactManagementCest.php
@@ -86,7 +86,33 @@ class ContactManagementCest
         $I->click('a.btn.btn-default[href="/s/contacts"]');
     }
 
-    public function editContact(AcceptanceTester $I)
+    public function acessEditContactFormFromList(AcceptanceTester $I)
+    {
+        // Navigate to the contacts page
+        $I->amOnPage('/s/contacts');
+
+        // Grab the name of the first contact in the list
+        $contactName = $I->grabTextFrom('#leadTable tbody tr:first-child td:nth-child(2) a div');
+
+        // Check we can see the first contact name
+        $I->see($contactName);
+
+        // Click on the dropdown caret on the first contact
+        $I->click('#leadTable tbody tr:first-child td:first-child div div button');
+
+        // Wait for the dropdown menu to show and click the delete menu option
+        $I->waitForElementClickable('#leadTable > tbody > tr:nth-child(1) > td:nth-child(1) > div > div > ul > li:nth-child(1) > a', 30);
+        $I->click('#leadTable > tbody > tr:nth-child(1) > td:nth-child(1) > div > div > ul > li:nth-child(1) > a');
+
+        // Wait for the edit form to be visible
+        $I->waitForElementVisible('#core > div.pa-md.bg-light-xs.bdr-b > h4', 30);
+        $I->see("Edit $contactName");
+
+        // Close the edit form (No changes are made)
+        $I->click('#lead_buttons_cancel_toolbar');
+    }
+
+    public function editContactFromProfile(AcceptanceTester $I)
     {
         // Navigate to the contacts page
         $I->amOnPage('/s/contacts');
@@ -119,5 +145,63 @@ class ContactManagementCest
         // Verify the update message
         $I->waitForText('Edited-First-Name Edited-Last-Name has been updated!', 30);
         $I->see('Edited-First-Name Edited-Last-Name has been updated!');
+    }
+
+    public function deleteContactFromList(AcceptanceTester $I)
+    {
+        // Navigate to the contacts page
+        $I->amOnPage('/s/contacts');
+
+        // Grab the name of the first contact in the list
+        $contactName = $I->grabTextFrom('#leadTable tbody tr:first-child td:nth-child(2) a div');
+
+        // Check we can see the first contact name
+        $I->see($contactName);
+
+        // Click on the dropdown caret on the first contact
+        $I->click('#leadTable > tbody > tr:nth-child(1) > td:nth-child(1) > div > div > button');
+
+        // Wait for the dropdown menu to show and click the delete menu option
+        $I->waitForElementVisible('#leadTable > tbody > tr:nth-child(1) > td:nth-child(1) > div > div > ul > li:nth-child(4) > a', 5);
+        $I->click('#leadTable > tbody > tr:nth-child(1) > td:nth-child(1) > div > div > ul > li:nth-child(4) > a');
+
+        // Wait for the modal to show and confirm deletion
+        $I->waitForElementVisible('button.btn.btn-danger', 5);
+        $I->click('button.btn.btn-danger');
+
+        // Wait for the delete confirmation message
+        $I->waitForText("$contactName has been deleted!", 30);
+        $I->see("$contactName has been deleted!");
+    }
+
+    public function deleteContactFromProfile(AcceptanceTester $I)
+    {
+        // Navigate to the contacts page
+        $I->amOnPage('/s/contacts');
+
+        // Grab the name of the first contact in the list
+        $contactName = $I->grabTextFrom('#leadTable tbody tr:first-child td:nth-child(2) a div');
+
+        // Click on the contact name to view the contact details
+        $I->click(['link' => $contactName]);
+
+        // Wait for the contact details page to load and confirm we're on the correct page
+        $I->waitForText($contactName, 30);
+        $I->see($contactName);
+
+        // Click the dropdown caret to show the delete option
+        $I->click('#toolbar .std-toolbar.btn-group > button > i');
+
+        // Wait for the dropdown to be displayed and click on the delete option
+        $I->waitForElementVisible('#toolbar .std-toolbar.btn-group.open > ul', 30);
+        $I->click('#toolbar .std-toolbar.btn-group.open > ul > li:nth-child(5) > a');
+
+        // Wait for the modal to become visible and click on the button to confirm delete
+        $I->waitForElementVisible('button.btn.btn-danger', 30);
+        $I->click('button.btn.btn-danger');
+
+        // Wait for the delete to be completed and confirm the contact is deleted
+        $I->waitForText("$contactName has been deleted!", 30);
+        $I->see("$contactName has been deleted!");
     }
 }
