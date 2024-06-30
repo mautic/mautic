9ac53c28187e2e098186f5b3cdc3c3090e638f04
andersonjeccel <116097999+andersonjeccel@users.noreply.github.com>
2024-06-30 20:24:34 -0300

    [UX] Forms page keyboard shortcut

--- app/bundles/CoreBundle/Assets/js/1.core.js
+++ app/bundles/CoreBundle/Assets/js/1.core.js
@@ -140,6 +140,10 @@ var Mautic = {
             mQuery('#mautic_email_index').click();
         });
 
+        Mautic.addKeyboardShortcut('g+f', 'Load Forms', function (e) {
+            mQuery('#mautic_form_index').click();
+        });
+
         Mautic.addKeyboardShortcut('[', 'Toggle Sidebar', function (e) {
             mQuery(".navbar-right a[data-toggle='sidebar']").click();
         });
