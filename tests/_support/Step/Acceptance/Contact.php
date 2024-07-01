b449ea72ce1e066e5531f1ee3a276482f74af4d5
Priyanshi Gaur <noxdot1134@gmail.com>
2024-07-02 04:52:17 +0530

    Use step objects (common functionalities) for contact tests
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

---
+++ tests/_support/Step/Acceptance/Contact.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Step\Acceptance;
+
+use Page\Acceptance\ContactPage;
+
+class Contact extends \AcceptanceTester
+{
+    /**
+     * Fill out the quick add form for creating a contact.
+     *
+     * @param string $firstName
+     * @param string $lastName
+     * @param string $email
+     * @param string $tag
+     */
+    public function fillContactForm($firstName, $lastName, $email, $tag)
+    {
+        $I = $this;
+        $I->waitForElementVisible(ContactPage::$firstNameField, 10);
+        $I->fillField(ContactPage::$firstNameField, $firstName);
+        $I->fillField(ContactPage::$lastNameField, $lastName);
+        $I->fillField(ContactPage::$emailField, $email);
+        $I->fillField(ContactPage::$tagField, $tag);
+        $I->pressKey(ContactPage::$tagField, \Facebook\WebDriver\WebDriverKeys::ENTER);
+    }
+
+    public function selectContactFromList()
+    {
+        $I           = $this;
+        $contactName = $I->grabTextFrom('#leadTable tbody tr:first-child td:nth-child(2) a div');
+        $I->see($contactName);
+        $I->click(['link' => $contactName]);
+        $I->waitForText($contactName, 30);
+        $I->see($contactName);
+    }
+}
