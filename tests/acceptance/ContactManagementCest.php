cb6cec3fe4d41c4f93e7102239bd85e0a7561e11
Priyanshi Gaur <noxdot1134@gmail.com>
2024-07-02 04:53:09 +0530

    Edit Contact tests to use db module, page objects and step objects
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- tests/acceptance/ContactManagementCest.php
+++ tests/acceptance/ContactManagementCest.php
@@ -1,5 +1,8 @@
 <?php
 
+use Page\Acceptance\ContactPage;
+use Step\Acceptance\Contact;
+
 class ContactManagementCest
 {
     public function _before(AcceptanceTester $I)
@@ -7,83 +10,53 @@ class ContactManagementCest
         $I->login('admin', 'Maut1cR0cks!');
     }
 
-    public function createContactFromQuickAdd(AcceptanceTester $I)
-    {
-        // Navigate to the contacts page
-        $I->amOnPage('/s/contacts');
+    public function createContactFromQuickAdd(
+        AcceptanceTester $I,
+        Contact $contact
+    ) {
+        $I->amOnPage(ContactPage::$URL);
 
-        // Wait for and click on the "Quick Add" button
-        $I->waitForElementClickable('#toolbar .quickadd', 30);
-        $I->click('#toolbar .quickadd');
+        // Click on "Quick Add" button
+        $I->waitForElementClickable(ContactPage::$quickAddButton, 30);
+        $I->click(ContactPage::$quickAddButton);
 
-        // Wait for the Quick Add modal to appear and verify its presence
-        $I->waitForElementVisible('#MauticSharedModal-label', 30);
+        // Wait for the Quick Add Form to appear
+        $I->waitForElementVisible(ContactPage::$quickAddModal, 30);
         $I->see('Quick Add', 'h4.modal-title');
 
-        // Wait for the form fields to be visible
-        $I->waitForElementVisible('#lead_firstname', 10);
+        $contact->fillContactForm('QuickAddFirstName', 'QuickAddLastName', 'quickadd@example.com', 'TestTag');
 
-        // Fill out the quick add form
-        $I->fillField('#lead_firstname', 'QuickAddFirstName');
-        $I->fillField('#lead_lastname', 'QuickAddLastName');
-        $I->fillField('#lead_email', 'quickadd@example.com');
-        $I->fillField('#lead_tags_chosen input', 'TestTag');
-        $I->pressKey('#lead_tags_chosen input', Facebook\WebDriver\WebDriverKeys::ENTER);
-        $I->fillField('#lead_companies_chosen input', 'Mautic');
-        $I->pressKey('#lead_companies_chosen input', Facebook\WebDriver\WebDriverKeys::ENTER);
+        // Submit the form
+        $I->waitForElementClickable(ContactPage::$saveButton, 30);
+        $I->click(ContactPage::$saveButton);
+        $I->waitForElementNotVisible(ContactPage::$quickAddModal, 30);
 
-        // Wait for the save button to be clickable and submit the form
-        $I->waitForElementClickable('#MauticSharedModal > div > div > div.modal-footer > div > button.btn.btn-default.btn-save.btn-copy', 30);
-        $I->click('#MauticSharedModal > div > div > div.modal-footer > div > button.btn.btn-default.btn-save.btn-copy');
-        $I->waitForElementNotVisible('#MauticSharedModal-label', 30);
-
-        $I->amOnPage('/s/contacts');
         $I->reloadPage(); // Ensure the latest data is loaded
 
-        // Search for the contact we just created
-        $I->fillField('#list-search', 'QuickAddFirstName');
-        $I->pressKey('#list-search', Facebook\WebDriver\WebDriverKeys::ENTER);
-        $I->waitForElementVisible('#leadTable', 10); // Wait for the search results to appear
-
-        // Verify the contact is in the list
-        $I->see('QuickAddFirstName', '#leadTable');
-
-        // Clear the search
-        $I->click('#btn-filter');
+        $I->seeInDatabase('leads', ['firstname' => 'QuickAddFirstName', 'email' => 'quickadd@example.com']);
     }
 
-    public function createContactFromForm(AcceptanceTester $I)
-    {
-        // Navigate to the contacts page
+    public function createContactFromForm(
+        AcceptanceTester $I,
+        Contact $contact
+    ) {
         $I->amOnPage('/s/contacts');
 
         // Click on "+New" button
-        $I->waitForElementClickable('#toolbar a:nth-child(2)', 30);
-        $I->click('#toolbar a:nth-child(2)');
+        $I->waitForElementClickable(ContactPage::$newContactButton, 30);
+        $I->click(ContactPage::$newContactButton);
         $I->waitForText('New Contact', 30);
 
-        // Fill out the form fields
-        $I->waitForElementVisible('#lead_firstname', 10);
-        $I->fillField('#lead_firstname', 'FirstName');
-        $I->fillField('#lead_lastname', 'LastName');
-        $I->fillField('#lead_email', 'email@example.com');
-        $I->fillField('#lead_tags_chosen input', 'TestTag');
-        $I->pressKey('#lead_tags_chosen input', Facebook\WebDriver\WebDriverKeys::ENTER);
+        $contact->fillContactForm('FirstName', 'LastName', 'email@example.com', 'TestTag');
 
         // Scroll back to the top of the page
         $I->executeJS('window.scrollTo(0, 0);');
 
         // Click the save and close button
-        $I->waitForElementClickable('#lead_buttons_save_toolbar', 30);
-        $I->click('#lead_buttons_save_toolbar');
+        $I->waitForElementClickable(ContactPage::$saveAndCloseButton, 30);
+        $I->click(ContactPage::$saveAndCloseButton);
 
-        // Wait for the contact details page to load
-        $I->waitForElementVisible('.page-header-title .span-block', 30);
-        $I->see('FirstName LastName', '.page-header-title .span-block');
-
-        // Click the close button on the contact details page
-        $I->waitForElementClickable('#toolbar > div.std-toolbar.btn-group > a:nth-child(3)', 30);
-        $I->click('a.btn.btn-default[href="/s/contacts"]');
+        $I->seeInDatabase('leads', ['firstname' => 'FirstName', 'email' => 'email@example.com']);
     }
 
     public function acessEditContactFormFromList(AcceptanceTester $I)
