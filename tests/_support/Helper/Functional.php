54138c14f3233edec6564c12750f81f4f4973416
Priyanshi Gaur <noxdot1134@gmail.com>
2024-07-25 14:49:15 +0530

    Remove stub comments and use 'use' statement in functional helper
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- tests/_support/Helper/Functional.php
+++ tests/_support/Helper/Functional.php
@@ -2,9 +2,8 @@
 
 namespace Helper;
 
-// here you can define custom actions
-// all public methods declared in helper class will be available in $I
+use Codeception\Module;
 
-class Functional extends \Codeception\Module
+class Functional extends Module
 {
 }
