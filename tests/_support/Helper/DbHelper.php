f82233882d4419383c5269e47cd64808b8e9bb87
Priyanshi Gaur <noxdot1134@gmail.com>
2024-08-08 00:33:58 +0530

    update the dbhelper to check and use sql dump file if it exists
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- tests/_support/Helper/DbHelper.php
+++ tests/_support/Helper/DbHelper.php
@@ -3,22 +3,43 @@
 namespace Helper;
 
 use Codeception\Module;
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\Process\Process;
 
 class DbHelper extends Module
 {
     private bool $databasePrepared = false;
+    private array $dbConfig;
+    private string $dumpFilePath = 'tests/_data/dump.sql';
 
     public function _beforeSuite($settings = [])
     {
-        $this->prepareDatabase();
-        $this->generateSqlDump();
+        $this->loadEnv();
+        $this->dbConfig = [
+            'host'     => $_ENV['DB_HOST'],
+            'user'     => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASSWD'],
+            'dbname'   => $_ENV['DB_NAME'],
+        ];
 
-        if (!file_exists('tests/_data/dump.sql')) {
+        if (file_exists($this->dumpFilePath)) {
+            $this->populateDatabaseFromDump();
+        } else {
+            $this->prepareDatabase();
+            $this->generateSqlDump();
+        }
+
+        if (!file_exists($this->dumpFilePath)) {
             $this->fail('Failed to generate dump.sql');
         } else {
             $this->debug('DbHelper: dump.sql successfully generated');
         }
+    }
+
+    private function loadEnv(): void
+    {
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__.'/../../../.env.test.local');
     }
 
     private function prepareDatabase(): void
@@ -28,42 +49,54 @@ class DbHelper extends Module
         }
 
         $this->createDatabase();
-        $this->applyMigrations();
+        // $this->applyMigrations();
         $this->installFixtures();
         $this->databasePrepared = true;
     }
 
     private function createDatabase(): void
     {
-        $this->runCommand('bin/console doctrine:database:drop --if-exists --force', 'Dropping database');
-        $this->runCommand('bin/console doctrine:database:create', 'Creating database');
-        $this->runCommand('bin/console doctrine:schema:create', 'Creating database schema');
+        $this->runCommand('bin/console --env=test doctrine:database:drop --if-exists --force', 'Dropping database');
+        $this->runCommand('bin/console --env=test doctrine:database:create', 'Creating database');
+        $this->runCommand('bin/console --env=test doctrine:schema:create', 'Creating database schema');
     }
 
     private function applyMigrations(): void
     {
-        $this->runCommand('bin/console doctrine:migrations:migrate --no-interaction', 'Applying migrations');
+        $this->runCommand('bin/console --env=test doctrine:migrations:migrate --no-interaction', 'Applying migrations');
     }
 
     private function installFixtures(): void
     {
-        $this->runCommand('bin/console doctrine:fixtures:load --no-interaction', 'Loading fixtures');
+        $this->runCommand('bin/console --env=test doctrine:fixtures:load --no-interaction', 'Loading fixtures');
     }
 
     private function generateSqlDump(): void
     {
-        $dsn      = 'mysql:host=db;dbname=db';
-        $user     = 'db';
-        $password = 'db';
-        $command  = sprintf(
-            'mysqldump --opt -h%s -u%s -p%s %s > tests/_data/dump.sql',
-            parse_url($dsn, PHP_URL_HOST),
-            $user,
-            $password,
-            parse_url($dsn, PHP_URL_PATH)
+        $command = sprintf(
+            'mysqldump --opt -h%s -u%s -p%s %s > %s',
+            $this->dbConfig['host'],
+            $this->dbConfig['user'],
+            $this->dbConfig['password'],
+            $this->dbConfig['dbname'],
+            $this->dumpFilePath
         );
 
         $this->runCommand($command, 'Generating SQL dump');
+    }
+
+    private function populateDatabaseFromDump(): void
+    {
+        $command = sprintf(
+            'mysql -h%s -u%s -p%s %s < %s',
+            $this->dbConfig['host'],
+            $this->dbConfig['user'],
+            $this->dbConfig['password'],
+            $this->dbConfig['dbname'],
+            $this->dumpFilePath
+        );
+
+        $this->runCommand($command, 'Populating database from dump.sql');
     }
 
     private function runCommand($command, $description): void
