0a45163994be11c9b96a9c7e9bf7ecc9de6353a7
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2024-08-23 11:09:47 +0200

    Update app/bundles/EmailBundle/Assets/js/batch.js
    Co-authored-by: John Linhart <jan@linhart.email>

--- app/bundles/EmailBundle/Assets/js/batch.js
+++ app/bundles/EmailBundle/Assets/js/batch.js
@@ -2,8 +2,9 @@
 Mautic.emailBatchSubmit = function() {
     if (Mautic.batchActionPrecheck()) {
         if (mQuery('#email_batch_newCategory').val()) {
-            if (mQuery('#email_batch_ids').length) {
-                mQuery('#email_batch_ids').val(Mautic.getCheckedListIds(false, true));
+            const $emailBatchIds = mQuery('#email_batch_ids');
+            if ($emailBatchIds.length) {
+                $emailBatchIds.val(Mautic.getCheckedListIds(false, true));
             }
 
             return true;
