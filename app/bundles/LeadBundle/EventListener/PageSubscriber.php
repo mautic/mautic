cf92f98ccb4718628caae736264c69a0ce036e36
Putzwasser <26040044+putzwasser@users.noreply.github.com>
2024-09-17 09:40:59 +0200

    TASK: Renames TokenHelper for clarity and switches to using static methods

--- app/bundles/LeadBundle/EventListener/PageSubscriber.php
+++ app/bundles/LeadBundle/EventListener/PageSubscriber.php
@@ -2,18 +2,13 @@
 
 namespace Mautic\LeadBundle\EventListener;
 
-use Mautic\LeadBundle\Helper\TokenHelper;
+use Mautic\LeadBundle\Helper\TokenHelper as LeadTokenHelper;
 use Mautic\PageBundle\Event\PageDisplayEvent;
 use Mautic\PageBundle\PageEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class PageSubscriber implements EventSubscriberInterface
 {
-    public function __construct(
-        private TokenHelper $leadTokenHelper,
-    ) {
-    }
-
     public static function getSubscribedEvents(): array
     {
         return [
@@ -25,6 +20,6 @@ class PageSubscriber implements EventSubscriberInterface
     {
         $content = $event->getContent();
         $lead    = $event->getLead();
-        $event->setContent($this->leadTokenHelper->findLeadTokens($content, $lead?->convertToArray() ?? [], replace: true));
+        $event->setContent(LeadTokenHelper::findLeadTokens($content, $lead?->convertToArray() ?? [], replace: true));
     }
 }
