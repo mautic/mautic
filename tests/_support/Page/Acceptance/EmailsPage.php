679f033a9c47d47b92f55d0deea67b0c2357305a
John Linhart <admin@escope.cz>
2024-10-02 12:13:45 +0200

    E2e test fixes (#14157)
    * Updating according to the current state and simplifying the button paths
    
    * Simplifying the test and saving 2 seconds

--- tests/_support/Page/Acceptance/EmailsPage.php
+++ tests/_support/Page/Acceptance/EmailsPage.php
@@ -13,7 +13,7 @@ class EmailsPage
     public const NEW_CATEGORY_DROPDOWN     = '#email_batch_newCategory_chosen';
     public const CHANGE_CATEGORY_ACTION    = "a[href='/s/emails/batch/categories/view']";
     public const SELECTED_ACTIONS_DROPDOWN = '#page-list-wrapper > div.page-list > div.table-responsive > table > thead > tr > th.col-actions > div > div > button > i';
-    public const SAVE_BUTTON               = '#MauticSharedModal > div > div > div.modal-footer > div > button.btn.btn-default.btn-save.btn-copy';
+    public const SAVE_BUTTON               = 'div.modal-form-buttons > button.btn.btn-primary.btn-save.btn-copy';
     public const SELECT_ALL_CHECKBOX       = '#customcheckbox-one0';
     public const SELECT_SEGMENT_EMAIL      = '#app-content > div > div.modal.fade.in.email-type-modal > div > div > div.modal-body.form-select-modal > div > div:nth-child(2) > div > div.hidden-xs.panel-footer.text-center > button';
     public const CONTACT_SEGMENT_DROPDOWN  = '#emailform_lists_chosen';
