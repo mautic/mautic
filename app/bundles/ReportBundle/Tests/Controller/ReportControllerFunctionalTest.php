3ac8e3df964b345a6f7999fff3044c766330bee3
Alan Wierzchoń <100592709+AlanWierzchonCA@users.noreply.github.com>
2024-10-04 17:11:27 +0200

    fix:[DPMMA-2818] fixes sort reports table (#14096)
    * fix:[DPMMA-2818] fixes sort raports table
    
    * fix:[DPMMA-2818] fix unittests
    
    * fix:[DPMMA-2818] fix phpstan
    
    * fix:[DPMMA-2818] fix unitest
    
    * fix:[DPMMA-2818] fix filter in twig
    
    * fix:[DPMMA-2818] fix unittests after changes in twig
    
    * fix:[DPMMA-2818] refacor
    
    * fix:[DPMMA-2818] refacor table order links & add unittest
    
    * fix:[DPMMA-2818] unittest for order raport table
    
    * Update app/bundles/ReportBundle/Tests/Controller/ReportControllerFunctionalTest.php
    
    Co-authored-by: Patryk Gruszka <patryk.gruszka.se@gmail.com>
    
    * Update app/bundles/ReportBundle/Tests/Controller/ReportControllerFunctionalTest.php
    
    Co-authored-by: Patryk Gruszka <patryk.gruszka.se@gmail.com>
    
    ---------
    
    Co-authored-by: Patryk Gruszka <patryk.gruszka.se@gmail.com>
    Co-authored-by: Ruth Cheesley <ruth@ruthcheesley.co.uk>

--- app/bundles/ReportBundle/Tests/Controller/ReportControllerFunctionalTest.php
+++ app/bundles/ReportBundle/Tests/Controller/ReportControllerFunctionalTest.php
@@ -12,6 +12,7 @@ use Mautic\PageBundle\Entity\Page;
 use Mautic\ReportBundle\Entity\Report;
 use Mautic\ReportBundle\Scheduler\Enum\SchedulerEnum;
 use PHPUnit\Framework\Assert;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Request;
 
 class ReportControllerFunctionalTest extends MauticMysqlTestCase
@@ -51,6 +52,43 @@ class ReportControllerFunctionalTest extends MauticMysqlTestCase
         $this->client->request('GET', '/s/reports/view/'.$report->getId());
 
         Assert::assertTrue($this->client->getResponse()->isOk());
+    }
+
+    public function testReportTableOrderColumn(): void
+    {
+        $page  = $this->createPage('test page 1', 15);
+        $page2 = $this->createPage('test page 2', 9);
+        $page3 = $this->createPage('test page 3', 30);
+
+        $this->createHit($page);
+        $this->createHit($page2);
+        $this->createHit($page3);
+
+        $report = $this->createReport('Report Most Visited Pages', 'page.hits', []);
+        $report->setColumns(['p.title', 'p.hits']);
+        $this->em->persist($report);
+        $this->em->flush();
+
+        $crawler = $this->client->request('GET', '/s/reports/view/'.$report->getId().'?tmpl=list&name=report.'.$report->getId());
+
+        $crawlerReportTable = $crawler->filterXPath('//table[@id="reportTable"]')->first();
+        $crawlerReportTable = $this->domTableToArray($crawlerReportTable);
+
+        $this->assertSame([
+            ['1', 'test page 1', '15'],
+            ['2', 'test page 2', '9'],
+            ['3', 'test page 3', '30'],
+        ], array_slice($crawlerReportTable, 1, 3));
+
+        $crawler            = $this->client->request('GET', '/s/reports/view/'.$report->getId().'?tmpl=list&name=report.'.$report->getId().'&orderby=p.hits');
+        $crawlerReportTable = $crawler->filterXPath('//table[@id="reportTable"]')->first();
+        $crawlerReportTable = $this->domTableToArray($crawlerReportTable);
+
+        $this->assertSame([
+            ['1', 'test page 2', '9'],
+            ['2', 'test page 1', '15'],
+            ['3', 'test page 3', '30'],
+        ], array_slice($crawlerReportTable, 1, 3));
     }
 
     public function testCreatingNewReportAndClone(): void
@@ -477,6 +515,14 @@ class ReportControllerFunctionalTest extends MauticMysqlTestCase
     }
 
     /**
+     * @return array<int,array<int,mixed>>
+     */
+    private function domTableToArray(Crawler $crawler): array
+    {
+        return $crawler->filter('tr')->each(fn ($tr) => $tr->filter('td')->each(fn ($td) => trim($td->text())));
+    }
+
+    /**
      * @param string[] $graphs
      */
     private function createReport(string $name, string $source, array $graphs): Report
@@ -493,10 +539,11 @@ class ReportControllerFunctionalTest extends MauticMysqlTestCase
         return $report;
     }
 
-    private function createPage(string $title): Page
+    private function createPage(string $title, int $hitCount=0): Page
     {
         $page = new Page();
         $page->setTitle($title);
+        $page->setHits($hitCount);
         $page->setAlias(str_replace(' ', '_', $title));
 
         $this->em->persist($page);
