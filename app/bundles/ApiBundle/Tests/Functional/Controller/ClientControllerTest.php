bf71f42e597fbb0d333d94eff1148bbaa30e01ef
Rahul Shinde <shinde.r.a@gmail.com>
2024-10-11 15:51:33 +0530

    updated test

--- app/bundles/ApiBundle/Tests/Functional/Controller/ClientControllerTest.php
+++ app/bundles/ApiBundle/Tests/Functional/Controller/ClientControllerTest.php
@@ -14,9 +14,6 @@ class ClientControllerTest extends MauticMysqlTestCase
     private const TOTAL_COUNT     = 6;
     protected $useCleanupRollback = false;
 
-    /**
-     * @runInSeparateProcess
-     */
     public function testIndexActionForPager(): void
     {
         $this->createApiClients();
