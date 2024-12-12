6007cc5aabcfd31d01fea2d3e63571b37653a2ea
Rahul Shinde <shinde.r.a@gmail.com>
2024-12-12 12:35:44 +0530

    remove test influenced by the previous one that sets a static variable

--- app/bundles/EmailBundle/Tests/Controller/PublicControllerFunctionalTest.php
+++ app/bundles/EmailBundle/Tests/Controller/PublicControllerFunctionalTest.php
@@ -244,19 +244,6 @@ class PublicControllerFunctionalTest extends MauticMysqlTestCase
         $this->assertStringContainsString('Save preferences', $crawler->html());
     }
 
-    public function testUnsubscribeFormActionWithUsingLandingPageWithContactLocal(): void
-    {
-        $lead = $this->createLead('de');
-        $page = $this->createPage();
-
-        $stat = $this->getStat(null, $lead, $page);
-        $this->em->flush();
-
-        $crawler = $this->client->request('GET', '/email/unsubscribe/'.$stat->getTrackingHash());
-        $this->assertTrue($this->client->getResponse()->isOk());
-        $this->assertStringContainsString('Präferenzen speichern', $crawler->html());
-    }
-
     /**
      * @throws ORMException
      */
