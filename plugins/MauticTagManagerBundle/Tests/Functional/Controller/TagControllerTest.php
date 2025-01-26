6d401d0772fe83735c68605ebe9bae085a983edf
laurielim <laurie.limsam@gmail.com>
2025-01-26 17:57:24 +0200

    Add tag namespace to TagControllerTest::testBatchDeleteAction

--- plugins/MauticTagManagerBundle/Tests/Functional/Controller/TagControllerTest.php
+++ plugins/MauticTagManagerBundle/Tests/Functional/Controller/TagControllerTest.php
@@ -167,7 +167,7 @@ class TagControllerTest extends MauticMysqlTestCase
     public function testBatchDeleteAction(): void
     {
         $tags   = $this->tagRepository->findAll();
-        $tagsId = array_map(fn (Tag $tag) => $tag->getId(), $tags);
+        $tagsId = array_map(fn (\Mautic\LeadBundle\Entity\Tag $tag) => $tag->getId(), $tags);
         $this->client->request('POST', '/s/tags/batchDelete?ids='.json_encode($tagsId));
         $this->assertTrue($this->client->getResponse()->isOk(), 'Return code must be 200.');
         $this->assertEmpty($this->tagRepository->count([]), 'All tags must be deleted.');
