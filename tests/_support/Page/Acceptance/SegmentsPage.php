a0d8890c1e2c0eb265da14bf2207f1c7a3b3d1dc
Patrick Jenkner <p.jenkner@leuchtfeuer.com>
2025-02-06 10:40:25 +0100

    start tests for deletion and segment filtering

--- tests/_support/Page/Acceptance/SegmentsPage.php
+++ tests/_support/Page/Acceptance/SegmentsPage.php
@@ -10,7 +10,16 @@ class SegmentsPage
     public static $newButton             = '.list-toolbar > a#new > i';
     public static $segmentName           = '#leadlist_name';
     public static $saveAndCloseButton    = '#leadlist_buttons_save_toolbar';
+
+    // Segment table name
+    public static $tableName             = 'leadListTable';
     public static $detailsTab            = '//*[@href="#details"]';
     public static $filtersTab            = '//*[@href="#filters"]';
+    public static $filtersDropdown       = '#available_segment_filters_chosen > a';
+    public static $filtersSearchField    = '.chosen-search-input';
+
     public static $editButton            = '#toolbar > div.std-toolbar > a:nth-child(1)';
+    public static $dropDown              = '#toolbar .std-toolbar > button';
+    public static $delete                = '#toolbar > div.std-toolbar.open > ul > li:nth-child(5) > a';
+    public static $ConfirmDelete         = 'button.btn.btn-danger';
 }
--- tests/acceptance/SegmentManagementCest.php
+++ tests/acceptance/SegmentManagementCest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Page\Acceptance\SegmentsPage;
+use Step\Acceptance\ContactStep;
 use Step\Acceptance\SegmentStep;
 
 class SegmentManagementCest
@@ -46,5 +47,102 @@ class SegmentManagementCest
 
         // Verify the update message
         $I->waitForText("Edited-Segment-Name $segmentAlias has been updated!", 30);
+    }
+
+    public function deleteSingleSegment(
+        AcceptanceTester $I,
+        SegmentStep $segment
+    ): void {
+        $I->amOnPage(SegmentsPage::$URL);
+
+        // Grab the name of the first contact in the list
+        $segmentName = $segment->grabSegmentNameFromList(1);
+
+        // Click on the contact name to view the contact details
+        $I->click(['link' => $segmentName]);
+
+        // Wait for the segment details page to load and confirm we're on the correct page
+        $I->waitForText($segmentName, 30);
+        $I->see($segmentName);
+
+        // Click the dropdown caret to show the delete option
+        $I->click(SegmentsPage::$dropDown);
+
+        // Click on the delete option
+        $I->click(SegmentsPage::$delete);
+
+        // Wait for the modal to show and confirm deletion
+        $I->waitForElementVisible(SegmentsPage::$ConfirmDelete, 5);
+        $I->click(SegmentsPage::$ConfirmDelete);
+
+        // Wait for the delete confirmation message
+        $I->waitForText("$segmentName has been deleted!", 30);
+        $I->see("$segmentName has been deleted!");
+    }
+
+    public function batchDeleteSegment(
+        AcceptanceTester $I,
+        SegmentStep $segment
+    ): void {
+        $I->amOnPage(SegmentsPage::$URL);
+
+        // Grab the names of the first two segments in the list
+        $segmentName1 = $segment->grabSegmentNameFromList(1);
+        $segmentName2 = $segment->grabSegmentNameFromList(2);
+
+        // Select the segments from the list
+        $segment->selectsegmentFromList(1);
+        $segment->selectsegmentFromList(2);
+
+        // Select delete option from dropdown for multiple selections
+        $segment->selectOptionFromDropDownForMultipleSelections(SegmentsPage::$tableName, 11);
+
+        // Wait for the modal to become visible and click on the button to confirm delete
+        $I->waitForElementVisible(SegmentsPage::$ConfirmDelete, 5);
+        $I->click(SegmentsPage::$ConfirmDelete);
+        $I->wait(5);
+
+        // Confirm the segments are deleted
+        $I->dontSee($segmentName1);
+        $I->dontSee($segmentName2);
+        $I->dontSeeInDatabase('test_lead_lists', ['name' => $segmentName1]);
+        $I->dontSeeInDatabase('test_lead_lists', ['name' => $segmentName2]);
+    }
+
+    public function addToSegmentViaFilter(
+        AcceptanceTester $I,
+        SegmentStep $segment,
+        ContactStep $contact
+    ): void {
+        $I->amOnPage(ContactPage::$URL);
+
+        // Click on "Quick Add" button
+        $I->waitForElementClickable(ContactPage::$quickAddButton, 30);
+        $I->click(ContactPage::$quickAddButton);
+
+        // Wait for the Quick Add Form to appear
+        $I->waitForElementVisible(ContactPage::$quickAddModal, 30);
+        $I->see('Quick Add', 'h4.modal-title');
+
+        // Fill out the Quick Add form
+        $contact->fillContactForm('FilterFirstName', 'FilterLastName', 'filter@example.com', 'TestTag');
+
+        // Submit the form
+        $I->waitForElementClickable(ContactPage::$saveButton, 30);
+        $I->click(ContactPage::$saveButton);
+        $I->waitForElementNotVisible(ContactPage::$quickAddModal, 30);
+
+        $I->amOnPage(SegmentsPage::$URL);
+        $I->waitForElementClickable(SegmentsPage::$newButton);
+        $I->click(SegmentsPage::$newButton);
+        $I->waitForElementVisible(SegmentsPage::$segmentName);
+        $I->fillField(SegmentsPage::$segmentName, 'filterTest');
+        $I->click(SegmentsPage::$filtersTab);
+        $I->waitForElementVisible(SegmentsPage::$filtersDropdown);
+        $I->clickWithLeftButton(SegmentsPage::$filtersDropdown);
+        $I->waitForElementVisible(SegmentsPage::$filtersSearchField);
+        $I->fillField(SegmentsPage::$filtersSearchField, 'First');
+        $I->waitForElementNotVisible('#available_segment_filters_chosen > div > ul > li:nth-child(3)');
+        $I->click(SegmentsPage::$saveAndCloseButton);
     }
 }
