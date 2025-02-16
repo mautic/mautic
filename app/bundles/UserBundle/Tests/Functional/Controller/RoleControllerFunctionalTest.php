e9bfb0490997a112fd374ca9b9c4730cb97ff852
laurielim <laurie.limsam@gmail.com>
2025-02-16 22:07:06 +0200

    Create RoleControllerFunctionalTest::testBatchDeleteAction

--- app/bundles/UserBundle/Tests/Functional/Controller/RoleControllerFunctionalTest.php
+++ app/bundles/UserBundle/Tests/Functional/Controller/RoleControllerFunctionalTest.php
@@ -6,6 +6,7 @@ namespace Mautic\UserBundle\Tests\Functional\Controller;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use Mautic\UserBundle\Entity\Role;
+use Mautic\UserBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 
 class RoleControllerFunctionalTest extends MauticMysqlTestCase
@@ -31,13 +32,7 @@ class RoleControllerFunctionalTest extends MauticMysqlTestCase
 
     public function testEditRoleAction(): void
     {
-        $role = new Role();
-        $role->setName('Test Role');
-        $role->setDescription('The Description');
-
-        $this->em->persist($role);
-        $this->em->flush();
-
+        $role       = $this->createRole('Test Role');
         $crawler    = $this->client->request(Request::METHOD_GET, '/s/roles/edit/'.$role->getId());
         $saveButton = $crawler->selectButton('role[buttons][save]');
 
@@ -50,5 +45,49 @@ class RoleControllerFunctionalTest extends MauticMysqlTestCase
         $this->assertTrue($this->client->getResponse()->isOk());
 
         $this->assertStringContainsString($updatedName, $this->client->getResponse()->getContent());
+    }
+
+    public function testBatchDeleteRoleAction(): void
+    {
+        $usedRole   = $this->createRole('Used Role');
+        $unusedRole = $this->createRole('Unused Role');
+        $user       = $this->createUser($usedRole);
+
+        $this->client->request(Request::METHOD_POST, '/s/roles/batchDelete?ids=all');
+        $response = $this->client->getResponse();
+        $this->assertTrue($response->isOk());
+
+        $this->assertSame($user->getRole(), $usedRole);
+        $this->assertStringContainsString($usedRole->getName().' cannot be deleted because it still has users assigned to it', $response->getContent());
+        $this->assertNull($unusedRole->getId());
+    }
+
+    private function createRole(string $title): Role
+    {
+        $role = new Role();
+        $role->setName($title);
+        $role->setDescription('The Description');
+
+        $this->em->persist($role);
+        $this->em->flush();
+
+        return $role;
+    }
+
+    private function createUser(Role $role): User
+    {
+        $user = new User();
+        $user->setUsername('testuser');
+        $user->setEmail('test@email.com');
+        $user->setFirstName('Test');
+        $user->setLastName('User');
+        $user->setPassword('password');
+        $user->setRole($role);
+        $user->setLastLogin('2024-02-22 10:30:00');
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return $user;
     }
 }
