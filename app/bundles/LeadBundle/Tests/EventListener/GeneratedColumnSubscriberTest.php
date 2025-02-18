46e59616ce32f4e3aafba2ea8f81affbf70af7ee
Rahul Shinde <shinde.r.a@gmail.com>
2025-02-18 08:10:46 +0530

    CS fix

--- app/bundles/LeadBundle/Tests/EventListener/GeneratedColumnSubscriberTest.php
+++ app/bundles/LeadBundle/Tests/EventListener/GeneratedColumnSubscriberTest.php
@@ -91,7 +91,7 @@ class GeneratedColumnSubscriberTest extends TestCase
                     'mautic.core.operator.contains'             => 'contains',
                 ],
                 'object'    => 'lead',
-                'iconClass' => 'ri-list-check-2'
+                'iconClass' => 'ri-list-check-2',
             ],
             $event->getChoices()['lead']['generated_email_domain']
         );
