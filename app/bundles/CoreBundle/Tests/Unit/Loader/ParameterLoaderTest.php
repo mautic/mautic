454ca4252ff631a971658c1452e9290938b06622
Nitsche, Alexander <alexander.nitsche@governikus.de>
2025-03-10 09:08:56 +0100

    Revert "Set Mautic configuration param via .env file"
    This reverts commit 2389d0faa98448d108c24b557d6be8b24eff33f3.

--- app/bundles/CoreBundle/Tests/Unit/Loader/ParameterLoaderTest.php
+++ app/bundles/CoreBundle/Tests/Unit/Loader/ParameterLoaderTest.php
@@ -4,36 +4,9 @@ namespace Mautic\CoreBundle\Tests\Unit\Loader;
 
 use Mautic\CoreBundle\Loader\ParameterLoader;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Dotenv\Dotenv;
 
 class ParameterLoaderTest extends TestCase
 {
-    /**
-     * @var array<string, mixed>
-     */
-    public array $env;
-
-    /**
-     * @var array<string, mixed>
-     */
-    public array $server;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->env    = $_ENV;
-        $this->server = $_SERVER;
-    }
-
-    public function tearDown(): void
-    {
-        $_ENV    = $this->env;
-        $_SERVER = $this->server;
-
-        parent::tearDown();
-    }
-
     public function testParametersAreLoaded(): void
     {
         $envParameters = json_encode(['default_daterange_filter' => '-1 day']);
@@ -58,28 +31,5 @@ class ParameterLoaderTest extends TestCase
         $loader = new ParameterLoader(__DIR__.'/TestRoot/app');
         $this->assertIsArray($loader->getDefaultParameters());
         $this->assertFalse($loader->getDefaultParameters()['api_enabled']);
-    }
-
-    public function testEnvironmentVariablesOverruleParameters(): void
-    {
-        $_ENV['MAUTIC_MAILER_DSN'] = 'mailer-dsn-system-environment';
-
-        $loader = new ParameterLoader(__DIR__.'/TestRoot/app');
-        $loader->loadIntoEnvironment();
-
-        $this->assertEquals('foobar.com', $loader->getParameterBag()->get('mailer_dsn'));
-        $this->assertEquals('mailer-dsn-system-environment', $_ENV['MAUTIC_MAILER_DSN']);
-    }
-
-    public function testDotEnvVariablesOverruleParameters(): void
-    {
-        $dotenv = new Dotenv();
-        $dotenv->load(__DIR__.'/TestRoot/.env');
-
-        $loader = new ParameterLoader(__DIR__.'/TestRoot/app');
-        $loader->loadIntoEnvironment();
-
-        $this->assertEquals('foobar.com', $loader->getParameterBag()->get('mailer_dsn'));
-        $this->assertEquals('mailer-dsn-dotenv', $_ENV['MAUTIC_MAILER_DSN']);
     }
 }
