fb3e4a9751e7c3bf6fe5e42e9e70971ae5cf1cba
Anderson, from Dropsolid <116097999+andersonjeccel@users.noreply.github.com>
2025-03-14 09:25:35 -0300

    phpunit

--- app/bundles/CampaignBundle/Tests/Command/ExecuteEventCommandTest.php
+++ app/bundles/CampaignBundle/Tests/Command/ExecuteEventCommandTest.php
@@ -109,7 +109,7 @@ class ExecuteEventCommandTest extends AbstractCampaignCommand
 
         $commandResult = $this->testSymfonyCommand('mautic:campaigns:execute', ['--scheduled-log-ids' => $log->getId()]);
 
-        Assert::assertStringContainsString('0 total events were executed', $commandResult->getDisplay());
+        Assert::assertStringContainsString('0 total events(s) to be processed', $commandResult->getDisplay());
         Assert::assertStringContainsString('0 total events were run', $commandResult->getDisplay());
         Assert::assertStringContainsString('0 total events were scheduled', $commandResult->getDisplay());
 
