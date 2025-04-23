0a07db25857f0459eba7c997003abe46fd033c0e
Nilesh Lohar <nilesh.lohar@acquia.com>
2025-04-23 19:21:05 +0530

    Removed redundant changes

--- app/bundles/PluginBundle/Bundle/PluginBundleBase.php
+++ app/bundles/PluginBundle/Bundle/PluginBundleBase.php
@@ -9,38 +9,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 abstract class PluginBundleBase extends Bundle
 {
-    /**
-     * @param null $metadata
-     * @param null $installedSchema
-     *
-     * @throws \Exception
-     *
-     * @deprecated To be removed in 5.0. Listen to PluginEvents::ON_PLUGIN_INSTALL instead
-     */
-    public static function onPluginInstall($plugin, $factory, $metadata = null, $installedSchema = null)
-    {
-    }
-
-    /**
-     * @param null $metadata
-     * @param null $installedSchema
-     *
-     * @throws \Exception
-     *
-     * @deprecated To be removed in 5.0. Listen to PluginEvents::ON_PLUGIN_UPDATE instead
-     */
-    public static function onPluginUpdate($plugin, $factory, $metadata = null, $installedSchema = null)
-    {
-    }
-
-    /**
-     * Not used yet :-).
-     *
-     * @param null $metadata
-     *
-     * @deprecated To be removed in 5.0. Listen to PluginEvents::ON_PLUGIN_UNINSTALL instead
-     */
-    public static function onPluginUninstall($plugin, $factory, $metadata = null)
-    {
-    }
 }
