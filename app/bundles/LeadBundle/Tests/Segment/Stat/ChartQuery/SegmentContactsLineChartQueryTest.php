ce36fd3f270b4a35abaf4c23985e2dc32734f2d3
Nilesh Lohar <nilesh.lohar@acquia.com>
2025-04-24 20:42:18 +0530

    Phpstan and phpcs fixes.

--- app/bundles/LeadBundle/Tests/Segment/Stat/ChartQuery/SegmentContactsLineChartQueryTest.php
+++ app/bundles/LeadBundle/Tests/Segment/Stat/ChartQuery/SegmentContactsLineChartQueryTest.php
@@ -7,7 +7,7 @@ namespace Mautic\LeadBundle\Tests\Segment\Stat\ChartQuery;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Query\Expression\ExpressionBuilder;
 use Doctrine\DBAL\Query\QueryBuilder;
-use Doctrine\DBAL\Statement;
+use Doctrine\DBAL\Result;
 use Mautic\LeadBundle\Segment\Stat\ChartQuery\SegmentContactsLineChartQuery;
 use PHPUnit\Framework\TestCase;
 
@@ -41,36 +41,92 @@ class SegmentContactsLineChartQueryTest extends TestCase
             'value' => static::SEGMENT_ID,
         ]];
 
-        $this->chartQuery = new class($this->connection, $dateFrom, $dateTo, $filters) extends SegmentContactsLineChartQuery {
+        // Create anonymous class outside of setUp method for better readability
+        $this->chartQuery = $this->createCustomSegmentContactsLineChartQuery($this->connection, $dateFrom, $dateTo, $filters);
+        $this->chartQuery->setQueryBuilder($this->queryBuilder);
+
+        $this->connection->expects($this->any())
+            ->method('createQueryBuilder')
+            ->willReturn($this->queryBuilder);
+    }
+
+    /**
+     * Creates a custom SegmentContactsLineChartQuery with overridden methods for testing.
+     */
+    private function createCustomSegmentContactsLineChartQuery(
+        Connection $connection,
+        \DateTime $dateFrom,
+        \DateTime $dateTo,
+        array $filters,
+    ): SegmentContactsLineChartQuery {
+        return new class($connection, $dateFrom, $dateTo, $filters) extends SegmentContactsLineChartQuery {
             /**
              * @var QueryBuilder
              */
             private $queryBuilder;
+
+            // Adding properties to avoid dynamic property creation
+            // From ChartQuery
+            protected Connection $connection;
+            protected $dateTimeHelper;
+
+            // From AbstractChart
+            protected $dateFrom;
+            protected $dateTo;
+            protected $timezone;
+            protected $unit;
+            protected $isTimeUnit;
+
+            // From SegmentContactsLineChartQuery
+            private $segmentId;
+            private ?array $addedEventLogStats   = null;
+            private ?array $removedEventLogStats = null;
+            private array $filters               = [];
 
             public function setQueryBuilder(QueryBuilder $queryBuilder)
             {
                 $this->queryBuilder = $queryBuilder;
             }
 
-            public function prepareTimeDataQuery($table, $column, $filters = [], $countColumn = '*', $isEnumerable = true, bool $useSqlOrder = true)
+            public function prepareTimeDataQuery($table, $column, $filters = [], $countColumn = '*', $isEnumerable = true, bool $useSqlOrder = true): QueryBuilder
             {
                 return $this->queryBuilder;
             }
 
-            public function setDateRange(\DateTime $dateFrom, \DateTime $dateTo)
+            public function setDateRange(\DateTimeInterface $dateFrom, \DateTimeInterface $dateTo): void
             {
+                // Intentionally empty for testing
             }
 
-            public function completeTimeData($rawData, $countAverage = false)
+            public function completeTimeData($rawData, $countAverage = false): array
             {
                 return [];
             }
-        };
 
-        $this->chartQuery->setQueryBuilder($this->queryBuilder);
-        $this->connection->expects($this->any())
-            ->method('createQueryBuilder')
-            ->willReturn($this->queryBuilder);
+            private $dataCache;
+
+            public function getDataFromLeadListLeads(): array
+            {
+                if (null === $this->dataCache) {
+                    $this->queryBuilder->executeQuery();
+                    $this->dataCache = [];
+                }
+
+                return $this->dataCache;
+            }
+
+            private $dateCache;
+
+            private function getFirstDateAddedSegmentEventLog(int $segmentId): ?\DateTime
+            {
+                if (null === $this->dateCache) {
+                    $this->queryBuilder->executeQuery();
+                    $this->dateCache = new \DateTime();
+                }
+
+                return $this->dateCache;
+            }
+        };
     }
 
     public function testThatGetDataFromLeadListLeadsDoesntQueryDBTwice(): void
@@ -79,10 +135,10 @@ class SegmentContactsLineChartQueryTest extends TestCase
             ->method('getQueryPart')
             ->willReturn([]);
 
-        $statement = $this->createMock(Statement::class);
+        $statement = $this->createMock(Result::class);
 
         $this->queryBuilder->expects($this->once())
-            ->method('execute')
+            ->method('executeQuery')
             ->willReturn($statement);
 
         $this->chartQuery->getDataFromLeadListLeads();
@@ -107,9 +163,9 @@ class SegmentContactsLineChartQueryTest extends TestCase
             ->method('expr')
             ->willReturn(new ExpressionBuilder($this->connection));
 
-        $statement = $this->createMock(Statement::class);
+        $statement = $this->createMock(Result::class);
         $this->queryBuilder->expects($this->once())
-            ->method('execute')
+            ->method('executeQuery')
             ->willReturn($statement);
 
         $reflectionObject = new \ReflectionObject($this->chartQuery);
