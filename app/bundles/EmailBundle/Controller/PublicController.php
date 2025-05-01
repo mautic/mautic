421a36b5c52e8c25ef45116b0def3045ab683156
Dominic Mayers <dominic.mayers@meditationstudies.org>
2025-05-01 17:12:09 -0400

    [EmailBundle] Back to mautic/mautic

--- app/bundles/EmailBundle/Controller/PublicController.php
+++ app/bundles/EmailBundle/Controller/PublicController.php
@@ -195,14 +195,6 @@ class PublicController extends CommonFormController
                 }
             }
 
-            if ($isUnsubscribeAll) {
-                if (!empty($stat)) {
-                    $model->setDoNotContact($stat, $translator->trans('mautic.email.dnc.unsubscribed'), DoNotContact::UNSUBSCRIBED);
-                } else {
-                    $model->setDoNotContactLead($lead, $translator->trans('mautic.email.dnc.unsubscribed'), DoNotContact::UNSUBSCRIBED);
-                }
-            }
-
             if (!$showContactPreferences || $isUnsubscribeAll) {
                 if (!empty($stat)) {
                     $message = $this->getUnsubscribeMessage($idHash, $model, $stat, $this->translator);
@@ -745,11 +737,15 @@ class PublicController extends CommonFormController
 
     public function getUnsubscribeMessage($idHash, $model, $stat, $translator): string
     {
+        $model->setDoNotContact($stat, $translator->trans('mautic.email.dnc.unsubscribed'), DoNotContact::UNSUBSCRIBED);
+
         return $this->getUnsubscribeText($translator, $stat->getEmailAddress(), $idHash);
     }
 
     public function getUnsubscribeMessageLead(string $idHash, EmailModel $model, Lead $lead, TranslatorInterface $translator, string $urlEmail): string
     {
+        $model->setDoNotContactLead($lead, $translator->trans('mautic.email.dnc.unsubscribed'), DoNotContact::UNSUBSCRIBED);
+
         return $this->getUnsubscribeText($translator, $urlEmail, $idHash);
     }
 
