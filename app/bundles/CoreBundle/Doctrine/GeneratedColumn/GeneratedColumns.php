7c59519bb3545f8b64e89db1e252bfddcdafef61
Nilesh Lohar <nilesh.lohar@acquia.com>
2025-05-02 17:22:57 +0530

    corrected function

--- app/bundles/CoreBundle/Doctrine/GeneratedColumn/GeneratedColumns.php
+++ app/bundles/CoreBundle/Doctrine/GeneratedColumn/GeneratedColumns.php
@@ -10,6 +10,8 @@ final class GeneratedColumns implements GeneratedColumnsInterface
 
     /**
      * Simple array of generated columns.
+     *
+     * @var GeneratedColumn[]
      */
     private array $generatedColumns = [];
 
