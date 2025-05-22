c02c3d470f6abbc17050b9e4807e0d49adcfeecc
levente <levente.bajusz@outlook.com>
2025-05-22 10:34:15 +0200

    fix getChartQuery issue

--- app/bundles/CampaignBundle/Entity/LeadEventLogRepository.php
+++ app/bundles/CampaignBundle/Entity/LeadEventLogRepository.php
@@ -366,33 +366,27 @@ class LeadEventLogRepository extends CommonRepository
             ->join('ll', MAUTIC_TABLE_PREFIX.'campaign_events', 'e', 'e.id = ll.event_id');
 
         if (isset($options['channel'])) {
-            $query->andWhere('e.channel = :channel')
-                ->setParameter('channel', $options['channel']);
+            $query->andWhere('e.channel = ' . $query->expr()->literal($options['channel']));
         }
 
         if (isset($options['channelId'])) {
-            $query->andWhere('e.channel_id = :channelId')
-                ->setParameter('channelId', (int) $options['channelId']);
+            $query->andWhere('e.channel_id = ' . (int) $options['channelId']);
         }
 
         if (isset($options['type'])) {
-            $query->andWhere('e.type = :type')
-                ->setParameter('type', $options['type']);
+            $query->andWhere('e.type = ' . $query->expr()->literal($options['type']));
         }
 
         if (isset($options['logChannel'])) {
-            $query->andWhere('ll.channel = :logChannel')
-                ->setParameter('logChannel', $options['logChannel']);
+            $query->andWhere('ll.channel = ' . $query->expr()->literal($options['logChannel']));
         }
 
         if (isset($options['logChannelId'])) {
-            $query->andWhere('ll.channel_id = :logChannelId')
-                ->setParameter('logChannelId', (int) $options['logChannelId']);
+            $query->andWhere('ll.channel_id = ' . (int) $options['logChannelId']);
         }
 
-        $query->andWhere(
-            $query->expr()->eq('ll.is_scheduled', ':isScheduled')
-        )->setParameter('isScheduled', isset($options['is_scheduled']) ? 1 : 0);
+        $isScheduled = isset($options['is_scheduled']) ? 1 : 0;
+        $query->andWhere('ll.is_scheduled = ' . $isScheduled);
 
         return $chartQuery->fetchTimeData('('.$query.')', 'date_triggered');
     }
