63b2edab2cd80d67515c75dfb05e2524f90ce25f
Anderson J. Eccel <116097999+andersonjeccel@users.noreply.github.com>
2025-08-05 15:12:59 -0300

    csfix

--- app/bundles/CoreBundle/Twig/Extension/ColorsExtension.php
+++ app/bundles/CoreBundle/Twig/Extension/ColorsExtension.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Mautic\CoreBundle\Twig\Extension;
 
-use Mautic\CoreBundle\Helper\ColorHelper;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -28,32 +27,25 @@ class ColorsExtension extends AbstractExtension
      */
     public function getContrastColor(string $hexColor): string
     {
-        // Remove # if present and ensure it's a valid hex color
         $hexColor = ltrim($hexColor, '#');
-        
-        // If the color is in short format (e.g., FED), expand it to full format (e.g., FFEEFF)
+
         if (3 === strlen($hexColor)) {
-            $hexColor = str_repeat($hexColor[0], 2) . str_repeat($hexColor[1], 2) . str_repeat($hexColor[2], 2);
+            $hexColor = str_repeat($hexColor[0], 2).str_repeat($hexColor[1], 2).str_repeat($hexColor[2], 2);
         }
-        
-        // Validate hex color
+
         if (!preg_match('/^[0-9A-Fa-f]{6}$/', $hexColor)) {
-            return 'black'; // Default to black if invalid color
+            return 'black';
         }
-        
+
         try {
-            // Parse hex color to RGB
             $r = hexdec(substr($hexColor, 0, 2));
             $g = hexdec(substr($hexColor, 2, 2));
             $b = hexdec(substr($hexColor, 4, 2));
-            
-            // Calculate perceived brightness (same as in ThemesExtension)
+
             $brightness = ($r * 299 + $g * 587 + $b * 114) / 1000;
-            
-            // Return black for light colors, white for dark colors
+
             return $brightness > 125 ? 'black' : 'white';
         } catch (\Exception) {
-            // Return black as default if color parsing fails
             return 'black';
         }
     }
