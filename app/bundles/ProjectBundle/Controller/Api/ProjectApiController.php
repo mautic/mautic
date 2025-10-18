1ce689040cfaaaa9341d125e303a26f6ef352054
aarohiprasad <aarohi.prasad@acquia.com>
2025-10-19 01:24:52 +0530

    fixed code in projectapicontroller

--- app/bundles/ProjectBundle/Controller/Api/ProjectApiController.php
+++ app/bundles/ProjectBundle/Controller/Api/ProjectApiController.php
@@ -4,20 +4,41 @@ declare(strict_types=1);
 
 namespace Mautic\ProjectBundle\Controller\Api;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Mautic\ApiBundle\Controller\CommonApiController;
+use Mautic\ApiBundle\Helper\EntityResultHelper;
+use Mautic\CoreBundle\Factory\ModelFactory;
+use Mautic\CoreBundle\Helper\AppVersion;
+use Mautic\CoreBundle\Helper\CoreParametersHelper;
+use Mautic\CoreBundle\Security\Permissions\CorePermissions;
+use Mautic\CoreBundle\Translation\Translator;
 use Mautic\ProjectBundle\Entity\Project;
-use Symfony\Component\HttpKernel\Event\ControllerEvent;
-use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
 
 final class ProjectApiController extends CommonApiController
 {
-    public function initialize(ControllerEvent|FilterControllerEvent $event)
-    {
+    public function __construct(
+        CorePermissions $security,
+        Translator $translator,
+        EntityResultHelper $entityResultHelper,
+        RouterInterface $router,
+        FormFactoryInterface $formFactory,
+        AppVersion $appVersion,
+        RequestStack $requestStack,
+        ManagerRegistry $doctrine,
+        ModelFactory $modelFactory,
+        EventDispatcherInterface $dispatcher,
+        CoreParametersHelper $coreParametersHelper,
+    ) {
         $this->model            = $this->getModel('project');
         $this->entityClass      = Project::class;
         $this->entityNameOne    = 'project';
         $this->entityNameMulti  = 'projects';
         $this->permissionBase   = $this->model->getPermissionBase();
-        parent::initialize($event);
+
+        parent::__construct($security, $translator, $entityResultHelper, $router, $formFactory, $appVersion, $requestStack, $doctrine, $modelFactory, $dispatcher, $coreParametersHelper);
     }
 }
