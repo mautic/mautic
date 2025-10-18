cdb20e28cbfc15b8eb23420008c95eaaee41b5ab
aarohiprasad <aarohi.prasad@acquia.com>
2025-10-18 15:54:15 +0530

    removed unecessary config keys

--- app/bundles/ProjectBundle/Config/config.php
+++ app/bundles/ProjectBundle/Config/config.php
@@ -12,79 +12,14 @@ return [
                 'controller' => 'Mautic\ProjectBundle\Controller\ProjectController::executeAction',
             ],
         ],
-        'api' => [
-            'mautic_projectsstandard' => [
-                'standard_entity' => true,
-                'name'            => 'projects',
-                'path'            => '/projects',
-                'controller'      => 'MauticProjectBundle:Api\ProjectApi',
-                'methods'         => 'GET',
-            ],
-        ],
     ],
-    'services' => [
-        'controllers' => [
-            'mautic.project.controller.project' => [
-                'class'     => Mautic\ProjectBundle\Controller\ProjectController::class,
-                'arguments' => [
-                    'mautic.project.model.project',
-                    'form.factory',
-                    'mautic.security',
-                    'mautic.project.service.project_entity_loader',
-                    'doctrine',
-                ],
-            ],
-            'mautic.project.controller.ajax' => [
-                'class'     => Mautic\ProjectBundle\Controller\AjaxController::class,
-                'arguments' => [
-                    'mautic.project.model.project',
-                    'mautic.project.repository.project',
-                    'mautic.security',
-                ],
-            ],
-        ],
-        'repositories' => [
-            'mautic.project.repository.project' => [
-                'class'     => Doctrine\ORM\EntityRepository::class,
-                'factory'   => ['@doctrine.orm.entity_manager', 'getRepository'],
-                'arguments' => [
-                    Mautic\ProjectBundle\Entity\Project::class,
-                ],
-            ],
-        ],
-        'models' => [
-            'mautic.project.model.project' => [
-                'class'     => Mautic\ProjectBundle\Model\ProjectModel::class,
-                'arguments' => [
-                    'mautic.project.service.project_entity_loader',
-                ],
-            ],
-        ],
-        'services' => [
-            'mautic.project.service.project_entity_loader' => [
-                'class'     => Mautic\ProjectBundle\Service\ProjectEntityLoaderService::class,
-                'arguments' => [
-                    'doctrine.orm.entity_manager',
-                    'translator',
-                    'mautic.model.factory',
-                    'mautic.security',
-                    'event_dispatcher',
-                ],
-            ],
-        ],
-        'forms' => [
-            'mautic.project.form.type.project' => [
-                'class'     => Mautic\ProjectBundle\Form\Type\ProjectType::class,
-                'arguments' => [
-                    'translator',
-                    'mautic.security',
-                ],
-            ],
-        ],
-        'events' => [
-            'mautic.project.api.subscriber' => [
-                'class' => Mautic\ProjectBundle\EventListener\ApiSubscriber::class,
-            ],
+    'api' => [
+        'mautic_projectsstandard' => [
+            'standard_entity' => true,
+            'name'            => 'projects',
+            'path'            => '/projects',
+            'controller'      => 'MauticProjectBundle:Api\ProjectApi',
+            'methods'         => 'GET',
         ],
     ],
     'menu' => [
