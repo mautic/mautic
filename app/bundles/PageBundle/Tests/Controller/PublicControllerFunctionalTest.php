ad22eb58238c343e72fd36548a22723e5b3d8ca3
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2025-10-27 10:16:31 +0100

    [MTC-8579] remove ct param from redirect

--- app/bundles/PageBundle/Tests/Controller/PublicControllerFunctionalTest.php
+++ app/bundles/PageBundle/Tests/Controller/PublicControllerFunctionalTest.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Mautic\PageBundle\Tests\Controller;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\EmailBundle\Entity\Email;
+use Mautic\EmailBundle\Entity\Stat;
+use Mautic\LeadBundle\Entity\Lead;
 use Mautic\LeadBundle\Entity\Tag;
 use Mautic\PageBundle\Entity\Page;
+use Mautic\PageBundle\Entity\Redirect;
 use PHPUnit\Framework\Assert;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -135,5 +139,68 @@ class PublicControllerFunctionalTest extends MauticMysqlTestCase
         Assert::assertTrue($clientResponse->isOk());
         $content = $clientResponse->getContent();
         Assert::assertStringNotContainsString('<img src onerror=alert(\'Company\')>', $content);
+    }
+
+    public function testRedirectNotPersistClickthrough(): void
+    {
+        $lead = new Lead();
+        $lead->setEmail('test@example.com');
+        $this->em->persist($lead);
+        $this->em->flush();
+
+        $redirectUrl = 'https://mautic.org/';
+        $redirect = new Redirect();
+        $redirectHash = uniqid();
+        $redirect->setRedirectId($redirectHash);
+        $redirect->setUrl($redirectUrl);
+        $this->em->persist($redirect);
+        $this->em->flush();
+
+        $email = new Email();
+        $email->setName('Test email');
+        $this->em->persist($email);
+        $this->em->flush();
+
+        $statHash = uniqid();
+        $stat = new Stat();
+        $stat->setEmail($email);
+        $stat->setEmailAddress($lead->getEmail());
+        $stat->setDateSent(new \DateTime());
+        $stat->setLead($lead);
+        $stat->setTrackingHash($statHash);
+        $this->em->persist($stat);
+        $this->em->flush();
+
+        $ct = [
+            'source' =>['email', $email->getId()],
+            'email' => $email->getId(),
+            'stat' => $statHash,
+            'lead' => '1',
+            'channel' =>['email' => $email->getId()]
+        ];
+        $encodedCt = base64_encode(serialize($ct));
+
+        // complete the test:
+        // make call to /r/{$redirectHash}?ct={$encodedCt}
+        // check if Hit entity was created and has set source=email and source_id=$email->getId()
+
+        $this->client->followRedirects(false);
+
+        $this->client->request(Request::METHOD_GET, "/r/{$redirectHash}?ct={$encodedCt}");
+        $response = $this->client->getResponse();
+
+        // Re-enable redirect following for subsequent tests.
+        $this->client->followRedirects();
+
+        Assert::assertTrue($response->isRedirect($redirectUrl));
+
+        $hitRepository = $this->em->getRepository(\Mautic\PageBundle\Entity\Hit::class);
+        /** @var \Mautic\PageBundle\Entity\Hit|null $hit */
+        $hit = $hitRepository->findOneBy(['lead' => $lead]);
+
+        Assert::assertNotNull($hit, 'A Hit entity should have been created.');
+        Assert::assertSame('email', $hit->getSource(), 'The hit source should be email.');
+        Assert::assertSame($email->getId(), $hit->getSourceId(), 'The hit source ID should be the email ID.');
+        Assert::assertSame($redirect->getId(), $hit->getRedirect()->getId(), 'The hit should be associated with the correct redirect.');
     }
 }
