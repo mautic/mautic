d78f74f09759865ab1615bee05746cb74adb8721
Anderson J. Eccel <116097999+andersonjeccel@users.noreply.github.com>
2025-11-19 13:37:13 -0300

    Make filename translatable

--- app/bundles/AssetBundle/Service/BatchDownloadResponder.php
+++ app/bundles/AssetBundle/Service/BatchDownloadResponder.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace Mautic\AssetBundle\Service;
 
+use Mautic\CoreBundle\Translation\Translator;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Contracts\Service\Attribute\Required;
 
 final class BatchDownloadResponder
 {
+    private Translator $translator;
+
+    #[Required]
+    public function setTranslator(Translator $translator): void
+    {
+        $this->translator = $translator;
+    }
+
     public function createResponse(string $zipPath): BinaryFileResponse
     {
-        $downloadName = sprintf('assets-batch-%s.zip', (new \DateTimeImmutable())->format('Ymd-His'));
+        $translatedName = $this->translator->trans('mautic.asset.asset.batch_download.filename', [], 'messages');
+        $downloadName   = sprintf('%s-%s.zip', $translatedName, (new \DateTimeImmutable())->format('Ymd-His'));
 
         $response = new BinaryFileResponse($zipPath);
         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $downloadName);
