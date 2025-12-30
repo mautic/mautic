33e9b202308f72ad66545d51d8f0714fc53bfe4b
Rahul Shinde <shinde.r.a@gmail.com>
2025-12-30 06:49:42 +0530

    CS fix

--- app/bundles/CoreBundle/Model/AjaxLookupModelInterface.php
+++ app/bundles/CoreBundle/Model/AjaxLookupModelInterface.php
@@ -17,7 +17,7 @@ interface AjaxLookupModelInterface
      * @param string|array<int,string> $filter
      * @param array<string, mixed>     $options
      *
-     * @return array<string, array<int, string>>|array<array<string, mixed>>
+     * @return array<string, array<int, string>>|array<array<string, mixed>>|array<int|string, string>
      */
     public function getLookupResults(string $type, string|array $filter = '', int $limit = 10, int $start = 0, array $options = []): array;
 
