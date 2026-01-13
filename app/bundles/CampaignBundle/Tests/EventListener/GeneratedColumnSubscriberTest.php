4898e157e527ba3834c98b00a7045cdeaf616234
Wieslaw Golec <wieslawgolec@crazysales.com.au>
2026-01-13 14:03:24 +0800

    Fix Test Units (Part 8) to support PostgreSQL

--- app/bundles/CampaignBundle/Tests/EventListener/GeneratedColumnSubscriberTest.php
+++ app/bundles/CampaignBundle/Tests/EventListener/GeneratedColumnSubscriberTest.php
@@ -30,7 +30,7 @@ final class GeneratedColumnSubscriberTest extends TestCase
         Assert::assertCount(5, $generatedColumns);
 
         $generatedColumns = iterator_to_array($generatedColumns);
-        $this->assertAlterTableSql('ALTER TABLE '.MAUTIC_TABLE_PREFIX."campaign_leads ADD generated_date_added_hour CHAR(16) AS (DATE_FORMAT(date_added, \"%Y-%m-%d %H:00\")) STORED COMMENT '(DC2Type:generated)';
+        $this->assertAlterTableSql('ALTER TABLE '.MAUTIC_TABLE_PREFIX."campaign_leads ADD generated_date_added_hour VARCHAR(16) AS (DATE_FORMAT(date_added, \"%Y-%m-%d %H:00\")) STORED COMMENT '(DC2Type:generated)';
             ALTER TABLE ".MAUTIC_TABLE_PREFIX.'campaign_leads ADD INDEX `'.MAUTIC_TABLE_PREFIX.'campaign_id_generated_date_added_hour`(campaign_id, generated_date_added_hour)', array_shift($generatedColumns));
         $this->assertAlterTableSql('ALTER TABLE '.MAUTIC_TABLE_PREFIX."campaign_leads ADD generated_date_added_day DATE AS (DATE_FORMAT(date_added, \"%Y-%m-%d\")) STORED COMMENT '(DC2Type:generated)';
             ALTER TABLE ".MAUTIC_TABLE_PREFIX.'campaign_leads ADD INDEX `'.MAUTIC_TABLE_PREFIX.'campaign_id_generated_date_added_day`(campaign_id, generated_date_added_day)', array_shift($generatedColumns));
@@ -38,7 +38,7 @@ final class GeneratedColumnSubscriberTest extends TestCase
             ALTER TABLE ".MAUTIC_TABLE_PREFIX.'campaign_leads ADD INDEX `'.MAUTIC_TABLE_PREFIX.'campaign_id_generated_date_added_week`(campaign_id, generated_date_added_week)', array_shift($generatedColumns));
         $this->assertAlterTableSql('ALTER TABLE '.MAUTIC_TABLE_PREFIX."campaign_leads ADD generated_date_added_month CHAR(7) AS (DATE_FORMAT(date_added, \"%Y-%m\")) STORED COMMENT '(DC2Type:generated)';
             ALTER TABLE ".MAUTIC_TABLE_PREFIX.'campaign_leads ADD INDEX `'.MAUTIC_TABLE_PREFIX.'campaign_id_generated_date_added_month`(campaign_id, generated_date_added_month)', array_shift($generatedColumns));
-        $this->assertAlterTableSql('ALTER TABLE '.MAUTIC_TABLE_PREFIX."campaign_leads ADD generated_date_added_year YEAR AS (DATE_FORMAT(date_added, \"%Y\")) STORED COMMENT '(DC2Type:generated)';
+        $this->assertAlterTableSql('ALTER TABLE '.MAUTIC_TABLE_PREFIX."campaign_leads ADD generated_date_added_year SMALLINT AS (DATE_FORMAT(date_added, \"%Y\")) STORED COMMENT '(DC2Type:generated)';
             ALTER TABLE ".MAUTIC_TABLE_PREFIX.'campaign_leads ADD INDEX `'.MAUTIC_TABLE_PREFIX.'campaign_id_generated_date_added_year`(campaign_id, generated_date_added_year)', array_shift($generatedColumns));
     }
 
--- app/bundles/PageBundle/Tests/EventListener/ReportSubscriberTest.php
+++ app/bundles/PageBundle/Tests/EventListener/ReportSubscriberTest.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Query\Expression\ExpressionBuilder;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\DBAL\Result;
 use Mautic\CoreBundle\Doctrine\Provider\VersionProvider;
+use Mautic\CoreBundle\Doctrine\Provider\VersionProviderInterface;
 use Mautic\CoreBundle\Helper\Chart\ChartQuery;
 use Mautic\LeadBundle\Model\CompanyReportData;
 use Mautic\LeadBundle\Report\DncReportService;
@@ -40,7 +41,7 @@ class ReportSubscriberTest extends TestCase
     /**
      * @var VersionProviderInterface|\PHPUnit\Framework\MockObject\MockObject
      */
-    private \PHPUnit\Framework\MockObject\MockObject $versionProvider;
+    private \PHPUnit\Framework\MockObject\MockObject&VersionProvider $versionProvider;
 
     private \PHPUnit\Framework\MockObject\MockObject&DncReportService $dncReportService;
 
