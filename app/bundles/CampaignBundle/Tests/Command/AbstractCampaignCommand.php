da1a9289ac01353e2cbf0300ae420b267ca3dbb2
Wieslaw Golec <wieslawgolec@crazysales.com.au>
2026-01-26 09:34:27 +0800

    Unti Tests.

--- app/bundles/CampaignBundle/Tests/Command/AbstractCampaignCommand.php
+++ app/bundles/CampaignBundle/Tests/Command/AbstractCampaignCommand.php
@@ -89,7 +89,7 @@ class AbstractCampaignCommand extends MauticMysqlTestCase
 
         // Optional: dump first few leads
         try {
-            $leads = $this->db->fetchAllAssociative("SELECT id, email, date_added FROM {$this->prefix}leads LIMIT 5");
+            $leads = $this->db->fetchAllAssociative("SELECT id, email, date_added FROM {$this->prefix}leads ORDER BY id ASC LIMIT 5");
             echo "[DEBUG] Sample leads:\n".print_r($leads, true)."\n";
         } catch (\Exception $e) {
             echo '[DEBUG] Cannot fetch sample leads: '.$e->getMessage()."\n";
@@ -1247,41 +1247,33 @@ HTML;
 
         $dateAdded = new \DateTime('2018-01-04 23:41:20', new \DateTimeZone(self::DATE_TIME_ZONE));
 
-        try {
-            $connection->insert($table, [
-                'id'                   => 1,
-                'is_preference_center' => false,
-                'is_published'         => true,
-                'date_added'           => $dateAdded,
-                'created_by'           => 1,
-                'created_by_user'      => 'Admin User',
-                'date_modified'        => null,
-                'modified_by'          => null,
-                'modified_by_user'     => null,
-                'checked_out'          => null,
-                'checked_out_by'       => null,
-                'checked_out_by_user'  => null,
-                'name'                 => 'Campaign Test',
-                'description'          => null,
-                'alias'                => 'campaign-test',
-                'filters'              => serialize([]), // a:0:{}
-                'is_global'            => true,
-                'public_name'          => 'campaign-test',
-            ], [
-                'is_preference_center' => Types::BOOLEAN,
-                'is_published'         => Types::BOOLEAN,
-                'date_added'           => Types::DATETIME_MUTABLE,
-                // 'date_modified'        => Types::DATETIME_MUTABLE,
-                // 'checked_out'          => Types::DATETIME_MUTABLE,
-                'is_global' => Types::BOOLEAN,
-            ]);
-            echo "[DEBUG insert] Successfully inserted lead 1 into lead_lists\n";
-        } catch (\Throwable $e) {
-            echo '[DEBUG insert FAILED on lead 1] '.get_class($e).': '.$e->getMessage()."\n";
-            echo $e->getTraceAsString()."\n\n";
-
-            throw $e;
-        }
+        $connection->insert($table, [
+            'id'                   => 1,
+            'is_preference_center' => false,
+            'is_published'         => true,
+            'date_added'           => $dateAdded,
+            'created_by'           => 1,
+            'created_by_user'      => 'Admin User',
+            'date_modified'        => null,
+            'modified_by'          => null,
+            'modified_by_user'     => null,
+            'checked_out'          => null,
+            'checked_out_by'       => null,
+            'checked_out_by_user'  => null,
+            'name'                 => 'Campaign Test',
+            'description'          => null,
+            'alias'                => 'campaign-test',
+            'filters'              => serialize([]), // a:0:{}
+            'is_global'            => true,
+            'public_name'          => 'campaign-test',
+        ], [
+            'is_preference_center' => Types::BOOLEAN,
+            'is_published'         => Types::BOOLEAN,
+            'date_added'           => Types::DATETIME_MUTABLE,
+            // 'date_modified'        => Types::DATETIME_MUTABLE,
+            // 'checked_out'          => Types::DATETIME_MUTABLE,
+            'is_global'            => Types::BOOLEAN,
+        ]);
     }
 
     private function insertCampaignLeadlistXref(): void
