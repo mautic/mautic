400eede298296b104d9689cf2858acc4aa553c0d
aarohiprasad <aarohi.prasad@acquia.com>
2026-02-02 18:20:33 +0530

    fixed syntax issue

--- app/bundles/ReportBundle/Model/ReportExporter.php
+++ app/bundles/ReportBundle/Model/ReportExporter.php
@@ -51,49 +51,50 @@ class ReportExporter
             $dateTo = clone $scheduler->getScheduleDate();
             $dateTo->setTime(0, 0, 0);
 
-        $dateFrom = clone $dateTo;
-        switch ($report->getScheduleUnit()) {
-            case SchedulerEnum::UNIT_NOW:
-                $dateFrom->sub(new \DateInterval('P10Y'));
-                $this->schedulerModel->turnOffScheduler($report);
-                break;
-            case SchedulerEnum::UNIT_DAILY:
-                $dateFrom->sub(new \DateInterval('P1D'));
-                break;
-            case SchedulerEnum::UNIT_WEEKLY:
-                $dateFrom->sub(new \DateInterval('P7D'));
-                break;
-            case SchedulerEnum::UNIT_MONTHLY:
-                $dateFrom->sub(new \DateInterval('P1M'));
-                break;
-        }
-
-        $this->reportExportOptions->setDateFrom($dateFrom);
-        $this->reportExportOptions->setDateTo($dateTo->sub(new \DateInterval('PT1S')));
-
-        // just published reports, but schedule continue
-        if ($report->isPublished()) {
-            $this->reportExportOptions->beginExport();
-            while (true) {
-                $data = $this->reportDataAdapter->getReportData($report, $this->reportExportOptions);
-
-                $this->reportFileWriter->writeReportData($scheduler, $data, $this->reportExportOptions);
-
-                $totalResults = $data->getTotalResults();
-                unset($data);
-
-                if ($this->reportExportOptions->getNumberOfProcessedResults() >= $totalResults) {
+            $dateFrom = clone $dateTo;
+            switch ($report->getScheduleUnit()) {
+                case SchedulerEnum::UNIT_NOW:
+                    $dateFrom->sub(new \DateInterval('P10Y'));
+                    $this->schedulerModel->turnOffScheduler($report);
                     break;
-                }
-                $this->eventDispatcher->dispatch(new JobExtendTimeEvent());
-                $this->reportExportOptions->nextBatch();
+                case SchedulerEnum::UNIT_DAILY:
+                    $dateFrom->sub(new \DateInterval('P1D'));
+                    break;
+                case SchedulerEnum::UNIT_WEEKLY:
+                    $dateFrom->sub(new \DateInterval('P7D'));
+                    break;
+                case SchedulerEnum::UNIT_MONTHLY:
+                    $dateFrom->sub(new \DateInterval('P1M'));
+                    break;
             }
 
-            $file  = $this->reportFileWriter->getFilePath($scheduler);
-            $event = new ReportScheduleSendEvent($scheduler, $file);
-            $this->eventDispatcher->dispatch($event, ReportEvents::REPORT_SCHEDULE_SEND);
-        }
+            $this->reportExportOptions->setDateFrom($dateFrom);
+            $this->reportExportOptions->setDateTo($dateTo->sub(new \DateInterval('PT1S')));
 
-        $this->schedulerModel->reportWasScheduled($report, $scheduleNextDownloadJob);
+            // just published reports, but schedule continue
+            if ($report->isPublished()) {
+                $this->reportExportOptions->beginExport();
+                while (true) {
+                    $data = $this->reportDataAdapter->getReportData($report, $this->reportExportOptions);
+
+                    $this->reportFileWriter->writeReportData($scheduler, $data, $this->reportExportOptions);
+
+                    $totalResults = $data->getTotalResults();
+                    unset($data);
+
+                    if ($this->reportExportOptions->getNumberOfProcessedResults() >= $totalResults) {
+                        break;
+                    }
+                    $this->eventDispatcher->dispatch(new JobExtendTimeEvent());
+                    $this->reportExportOptions->nextBatch();
+                }
+
+                $file  = $this->reportFileWriter->getFilePath($scheduler);
+                $event = new ReportScheduleSendEvent($scheduler, $file);
+                $this->eventDispatcher->dispatch($event, ReportEvents::REPORT_SCHEDULE_SEND);
+            }
+
+            $this->schedulerModel->reportWasScheduled($report, $scheduleNextDownloadJob);
+        }
     }
 }
