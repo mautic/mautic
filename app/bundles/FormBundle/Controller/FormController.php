1a6ab1eba98200940e357b11629e7e98b356dd1c
JonasLudwig1998 <j.ludwig@leuchtfeuer.com>
2026-06-09 16:05:49 +0200

    MTC-10813 Remove sort by id for form actions (only form fields remains)

--- app/bundles/FormBundle/Controller/FormController.php
+++ app/bundles/FormBundle/Controller/FormController.php
@@ -822,7 +822,7 @@ class FormController extends CommonFormController
             if (!empty($reorder)) {
                 uasort(
                     $modifiedActions,
-                    fn ($a, $b): int => $a['order'] <=> $b['order'] ?: $a['id'] <=> $b['id']
+                    fn ($a, $b): int => $a['order'] <=> $b['order']
                 );
             }
 
