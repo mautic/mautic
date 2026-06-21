1ae13f0b55d110d276d0f6562e66c7f0a5c22d8b
greichel-evrima <greichel@evrima.com.au>
2026-06-22 09:43:53 +1000

    Send Email Example Api

---
+++ plugins/MauticSendExampleApiBundle/Config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name'        => 'Send Example API',
+    'description' => 'Exposes the "Send example" email action (EmailModel::sendSampleEmailToUser) as a REST API endpoint: POST /api/emails/{id}/example/send',
+    'version'     => '1.0.0',
+    'author'      => 'Evrima',
+    'routes'      => [
+        'main'   => [],
+        'public' => [],
+        'api'    => [
+            'plugin_sendexampleapi_send' => [
+                'path'       => '/emails/{id}/example/send',
+                'controller' => 'MauticPlugin\SendExampleApiBundle\Controller\Api\ExampleSendApiController::sendExampleAction',
+                'method'     => 'POST',
+            ],
+        ],
+    ],
+    'services' => [],
+    'menu'     => [],
+];
