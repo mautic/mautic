2db78e7ee56411135ae6d6c2fb648eec1310ee65
Tomas Votruba <tomas.vot@gmail.com>
2026-06-25 10:36:28 +0200

    apply morgan

--- app/bundles/CampaignBundle/Entity/ReplicaConnectionTrait.php
+++ app/bundles/CampaignBundle/Entity/ReplicaConnectionTrait.php
@@ -24,7 +24,7 @@ trait ReplicaConnectionTrait
         if ($connection instanceof PrimaryReadReplicaConnection) {
             if (
                 !$limiter instanceof ContactLimiter
-                || !($limiter->getContactId() || $limiter->getContactIdList())
+                || !$limiter->getContactId() && !$limiter->getContactIdList()
             ) {
                 $connection->ensureConnectedToReplica();
             }
