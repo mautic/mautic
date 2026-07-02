bd66113b4809db3ee57139d8e5fc222c438b22b7
Tomas Votruba <tomas.vot@gmail.com>
2026-07-02 15:01:43 +0200

    next

--- ecs.php
+++ ecs.php
@@ -17,5 +17,17 @@ return ECSConfig::configure()
         NoUnusedImportsFixer::class,
         // Symplify\CodingStandard\Fixer\Spacing\StandaloneLinePromotedPropertyFixer::class,
     ])
+<<<<<<< HEAD
     ->withDocblockLevel(17);
+=======
+<<<<<<< HEAD
+    ->withDocblockLevel(8);
+=======
+<<<<<<< HEAD
+    ->withDocblockLevel(1);
+=======
+    ->withDocblockLevel(9);
+>>>>>>> 3d6b9d54a7 (next)
+>>>>>>> 54e9225ef7 (next)
+>>>>>>> 7706949fdb (next)
 // ->withPreparedSets(comments: true);
