02b506bbe390c84e7a306ca3931c7f7cb9d5de65
Lukas Drahy <lukas@drahy.net>
2026-07-07 15:28:32 +0200

    Add a filter dropdown to list views for focus items

---
+++ plugins/MauticFocusBundle/Tests/Unit/Helper/FocusSearchScopeProviderTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\MauticFocusBundle\Tests\Unit\Helper;
+
+use MauticPlugin\MauticFocusBundle\Helper\FocusSearchScopeProvider;
+use MauticPlugin\MauticFocusBundle\Model\FocusModel;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class FocusSearchScopeProviderTest extends TestCase
+{
+    private FocusModel&MockObject $focusModel;
+
+    private TranslatorInterface&MockObject $translator;
+
+    private FocusSearchScopeProvider $provider;
+
+    protected function setUp(): void
+    {
+        $this->focusModel = $this->createMock(FocusModel::class);
+        $this->translator = $this->createMock(TranslatorInterface::class);
+
+        $this->translator->method('trans')
+            ->willReturnCallback(static fn (string $key): string => match ($key) {
+                'mautic.core.search.scope.standard' => 'Standard',
+                'mautic.core.searchcommand.category' => 'category',
+                'mautic.core.searchcommand.ismine' => 'is:mine',
+                default => $key,
+            });
+
+        $this->focusModel->method('getCommandList')
+            ->willReturn([
+                'mautic.core.searchcommand.category',
+                'mautic.core.searchcommand.ismine',
+            ]);
+
+        $this->provider = new FocusSearchScopeProvider(
+            $this->focusModel,
+            $this->translator
+        );
+    }
+
+    public function testGetScopesIncludesStandardFirst(): void
+    {
+        $scopes = $this->provider->getScopes();
+
+        $this->assertSame('', $scopes[0]['command']);
+        $this->assertSame('mautic.core.search.scope.standard', $scopes[0]['label']);
+        $this->assertTrue($scopes[0]['default'] ?? false);
+    }
+
+    public function testGetScopesDoesNotDuplicatePinnedCommands(): void
+    {
+        $scopes = $this->provider->getScopes();
+
+        $commands = array_column($scopes, 'command');
+
+        $this->assertContains('category', $commands);
+        $this->assertSame(count($commands), count(array_unique($commands)));
+    }
+}
