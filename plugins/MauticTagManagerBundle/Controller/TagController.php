7434931717b6ee4e4ee54a0176a9221c6c1100cc
Lukas Drahy <lukas@drahy.net>
2026-07-07 15:44:19 +0200

    dd a filter dropdown to list views for tags

--- plugins/MauticTagManagerBundle/Controller/TagController.php
+++ plugins/MauticTagManagerBundle/Controller/TagController.php
@@ -12,6 +12,8 @@ use Mautic\LeadBundle\Entity\Tag;
 use Mautic\LeadBundle\Model\TagModel;
 use MauticPlugin\MauticTagManagerBundle\Entity\TagRepository;
 use MauticPlugin\MauticTagManagerBundle\Form\Type\TagMergeType;
+use MauticPlugin\MauticTagManagerBundle\Helper\TagSearchScopeProvider;
+
 use MauticPlugin\MauticTagManagerBundle\Model\TagModel as TagManagerModel;
 use MauticPlugin\MauticTagManagerBundle\Stats\TagDependencies;
 use Symfony\Component\Form\FormInterface;
@@ -53,7 +55,7 @@ final class TagController extends FormController
      *
      * @param int $page
      */
-    public function indexAction(Request $request, $page = 1): Response
+    public function indexAction(Request $request, TagSearchScopeProvider $tagSearchScopeProvider, $page = 1): Response
     {
         // Use overwritten tag model so overwritten repository can be fetched,
         // we need it to define table alias so we can define sort order.
@@ -135,15 +137,17 @@ final class TagController extends FormController
         $tagsCount = ([] !== $tagIds) ? $this->tagRepository->countByLeads($tagIds) : [];
 
         $parameters = [
-            'items'       => $items,
-            'tagsCount'   => $tagsCount,
-            'page'        => $page,
-            'limit'       => $limit,
-            'permissions' => $permissions,
-            'security'    => $this->security,
-            'tmpl'        => $tmpl,
-            'currentUser' => $this->user,
-            'searchValue' => $search,
+            'items'           => $items,
+            'tagsCount'       => $tagsCount,
+            'page'            => $page,
+            'limit'           => $limit,
+            'permissions'     => $permissions,
+            'security'        => $this->security,
+            'tmpl'            => $tmpl,
+            'currentUser'     => $this->user,
+            'searchValue'     => $search,
+            'searchScopes'    => $tagSearchScopeProvider->getScopes(),
+            'searchScopeHint' => 'mautic.core.search.scope.hint',
         ];
 
         return $this->delegateView([
---
+++ plugins/MauticTagManagerBundle/Tests/Unit/Helper/TagSearchScopeProviderTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\MauticTagManagerBundle\Tests\Unit\Helper;
+
+use MauticPlugin\MauticTagManagerBundle\Entity\TagRepository;
+use MauticPlugin\MauticTagManagerBundle\Helper\TagSearchScopeProvider;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class TagSearchScopeProviderTest extends TestCase
+{
+    private TagRepository&MockObject $tagRepository;
+
+    private TranslatorInterface&MockObject $translator;
+
+    private TagSearchScopeProvider $provider;
+
+    protected function setUp(): void
+    {
+        $this->tagRepository = $this->createMock(TagRepository::class);
+        $this->translator    = $this->createMock(TranslatorInterface::class);
+
+        $this->translator->method('trans')
+            ->willReturnCallback(static fn (string $key): string => match ($key) {
+                'mautic.core.search.scope.standard' => 'Standard',
+                'mautic.core.searchcommand.ids' => 'ids',
+                default => $key,
+            });
+
+        $this->tagRepository->method('getSearchCommands')
+            ->willReturn([
+                'mautic.core.searchcommand.ids',
+            ]);
+
+        $this->provider = new TagSearchScopeProvider(
+            $this->tagRepository,
+            $this->translator
+        );
+    }
+
+    public function testGetScopesIncludesStandardFirst(): void
+    {
+        $scopes = $this->provider->getScopes();
+
+        $this->assertSame('', $scopes[0]['command']);
+        $this->assertSame('mautic.core.search.scope.standard', $scopes[0]['label']);
+        $this->assertTrue($scopes[0]['default'] ?? false);
+    }
+
+    public function testGetScopesDoesNotDuplicatePinnedCommands(): void
+    {
+        $scopes = $this->provider->getScopes();
+
+        $commands = array_column($scopes, 'command');
+
+        $this->assertContains('ids', $commands);
+        $this->assertSame(count($commands), count(array_unique($commands)));
+    }
+}
