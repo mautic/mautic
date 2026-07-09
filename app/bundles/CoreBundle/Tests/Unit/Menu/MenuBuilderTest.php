c6957996c24caa091e93afccacdc31c7726bf121
John Linhart <jan@linhart.email>
2026-07-09 11:47:35 +0200

    Update app/bundles/CoreBundle/Tests/Unit/Menu/MenuBuilderTest.php
    Co-authored-by: Martin Vooremäe <martin.vooremae@gmail.com>

--- app/bundles/CoreBundle/Tests/Unit/Menu/MenuBuilderTest.php
+++ app/bundles/CoreBundle/Tests/Unit/Menu/MenuBuilderTest.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-class MenuBuilderTest extends TestCase
+final class MenuBuilderTest extends TestCase
 {
     private FactoryInterface $factory;
 
