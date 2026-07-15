c73d96dfb335e73616f10d8f3fe8cc95a00c6bb9
fedys <miroslav.fedeles@gmail.com>
2026-07-15 11:38:30 +0200

    Tests fixes

--- app/bundles/LeadBundle/Tests/EventListener/FilterOperatorSubscriberTest.php
+++ app/bundles/LeadBundle/Tests/EventListener/FilterOperatorSubscriberTest.php
@@ -492,11 +492,11 @@ final class FilterOperatorSubscriberTest extends TestCase
                 'properties' => [
                     'type' => 'number',
                 ],
+                'object'    => 'lead',
                 'operators' => [
                     'equals'    => '=',
                     'not equal' => '!=',
                 ],
-                'object'    => 'lead',
                 'iconClass' => 'ri-external-link-line',
             ],
             $choices['behaviors']['hit_url_count']
@@ -796,11 +796,11 @@ final class FilterOperatorSubscriberTest extends TestCase
                 'properties' => [
                     'type' => 'number',
                 ],
+                'object'    => 'lead',
                 'operators' => [
                     'equals'    => '=',
                     'not equal' => '!=',
                 ],
-                'object'    => 'lead',
                 'iconClass' => 'ri-external-link-line',
             ],
             $choices['behaviors']['hit_url_count']
