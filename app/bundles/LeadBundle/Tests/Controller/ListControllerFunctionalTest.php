dbd0a77c84c14d3f594c2288e1bce9aa5ac66a20
fedys <miroslav.fedeles@gmail.com>
2026-07-15 12:41:47 +0200

    New tests

--- app/bundles/LeadBundle/Tests/Controller/ListControllerFunctionalTest.php
+++ app/bundles/LeadBundle/Tests/Controller/ListControllerFunctionalTest.php
@@ -17,6 +17,7 @@ use Mautic\LeadBundle\Model\ListModel;
 use Mautic\ProjectBundle\Entity\Project;
 use Mautic\ProjectBundle\Model\ProjectModel;
 use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -807,7 +808,7 @@ final class ListControllerFunctionalTest extends MauticMysqlTestCase
         Assert::assertNull($segmentExistCheck);
     }
 
-    #[\PHPUnit\Framework\Attributes\DataProvider('dateFieldProvider')]
+    #[DataProvider('dateFieldProvider')]
     public function testWarningOnInvalidDateField(?string $filter, bool $shouldContainError, string $operator = '='): void
     {
         $segment = $this->saveSegment(
@@ -922,6 +923,84 @@ final class ListControllerFunctionalTest extends MauticMysqlTestCase
         $this->assertStringContainsString('2', (string) $html);
         $this->assertStringContainsString('Active contacts', (string) $html);
         $this->assertStringContainsString('1', (string) $html);
+    }
+
+    /**
+     * @param array<mixed> $filter
+     */
+    #[DataProvider('relativeDateInvalidIntervalValues')]
+    public function testSegmentRelativeDateFilterOnlySupportPositiveNumber(string $operator, array $filter, string $interval, string $message): void
+    {
+        $filterData = [[
+            'glue'        => 'and',
+            'field'       => 'date_identified',
+            'object'      => 'lead',
+            'type'        => 'datetime',
+            'operator'    => $operator,
+            'properties'  => [
+                'filter' => $filter,
+            ],
+        ]];
+        $list = $this->saveSegment('s1', 's1', $filterData);
+        $this->em->clear();
+
+        $this->client->request(Request::METHOD_POST, '/s/ajax', ['action' => 'togglePublishStatus', 'model' => 'lead.list', 'id' => $list->getId()]);
+        $this->assertTrue($this->client->getResponse()->isOk());
+
+        $crawler = $this->client->request(Request::METHOD_GET, '/s/segments/edit/'.$list->getId());
+        $form    = $crawler->selectButton('leadlist_buttons_apply')->form();
+        $form['leadlist[filters][0][properties][filter][interval]']->setValue($interval);
+
+        $this->client->submit($form);
+        $this->assertTrue($this->client->getResponse()->isOk());
+
+        $this->assertStringContainsString($message, (string) $this->client->getResponse()->getContent());
+
+        $rows = $this->listRepo->findAll();
+        $this->assertCount(1, $rows);
+    }
+
+    public static function relativeDateInvalidIntervalValues(): iterable
+    {
+        yield [
+            'inLast',
+            [
+                'interval' => '1',
+                'unit'     => 'day',
+            ],
+            '-2',
+            'This value should be positive.',
+        ];
+
+        yield [
+            'inNext',
+            [
+                'interval' => '1',
+                'unit'     => 'day',
+            ],
+            'foo',
+            'Please enter an integer.',
+        ];
+
+        yield [
+            'inLast',
+            [
+                'interval' => '1',
+                'unit'     => 'day',
+            ],
+            '2.5',
+            'Please enter an integer.',
+        ];
+
+        yield [
+            'inLast',
+            [
+                'interval' => '',
+                'unit'     => 'day',
+            ],
+            '',
+            'This value should not be blank.',
+        ];
     }
 
     /**
