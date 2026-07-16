3f1c998d6d7be7ac15127b9ed5fc96b7f149d529
Martin Vooremäe <martin.vooremae@gmail.com>
2026-07-16 12:00:12 +0300

    refactor clearbit: migrate off deprecated AbstractIntegration

--- plugins/MauticClearbitBundle/Config/config.php
+++ plugins/MauticClearbitBundle/Config/config.php
@@ -20,42 +20,4 @@ return [
             ],
         ],
     ],
-
-    'services' => [
-        'others' => [
-            'mautic.plugin.clearbit.lookup_helper' => [
-                'class'     => MauticPlugin\MauticClearbitBundle\Helper\LookupHelper::class,
-                'arguments' => [
-                    'mautic.helper.integration',
-                    'mautic.helper.user',
-                    'monolog.logger.mautic',
-                    'mautic.lead.model.lead',
-                    'mautic.lead.model.company',
-                ],
-            ],
-        ],
-        'integrations' => [
-            'mautic.integration.clearbit' => [
-                'class'     => MauticPlugin\MauticClearbitBundle\Integration\ClearbitIntegration::class,
-                'arguments' => [
-                    'event_dispatcher',
-                    'mautic.helper.cache_storage',
-                    'doctrine.orm.entity_manager',
-                    'request_stack',
-                    'router',
-                    'translator',
-                    'monolog.logger.mautic',
-                    'mautic.helper.encryption',
-                    'mautic.lead.model.lead',
-                    'mautic.lead.model.company',
-                    'mautic.helper.paths',
-                    'mautic.core.model.notification',
-                    'mautic.lead.model.field',
-                    'mautic.plugin.model.integration_entity',
-                    'mautic.lead.model.dnc',
-                    'mautic.lead.field.fields_with_unique_identifier',
-                ],
-            ],
-        ],
-    ],
 ];
