5dc07029db8d2888e7acc99bfe5d8a321199aa87
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2026-07-23 14:22:53 +0200

    MTC-11184 fix flaky tests

--- tests/acceptance/EmailManagementCest.php
+++ tests/acceptance/EmailManagementCest.php
@@ -47,6 +47,10 @@ class EmailManagementCest
     {
         $I->waitForElementClickable(EmailsPage::SELECT_ALL_CHECKBOX);
         $I->click(EmailsPage::SELECT_ALL_CHECKBOX);
+        $I->waitForJS(
+            "return document.querySelectorAll('input.list-checkbox:not(:checked)').length === 0;",
+            10
+        );
         $I->seeCheckboxIsChecked(EmailsPage::SELECT_ALL_CHECKBOX);
     }
 
