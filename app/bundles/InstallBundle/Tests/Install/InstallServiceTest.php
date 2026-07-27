4d74b4a554b663fbb1001bcf177d9245e1e55ac9
Tomas Votruba <tomas.vot@gmail.com>
2026-07-27 22:24:06 +0200

    inject UserRepository directly into InstallService

--- app/bundles/InstallBundle/Tests/Install/InstallServiceTest.php
+++ app/bundles/InstallBundle/Tests/Install/InstallServiceTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Mautic\InstallBundle\Tests\Install;
 
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityRepository;
 use Mautic\CoreBundle\Configurator\Configurator;
 use Mautic\CoreBundle\Configurator\Step\StepInterface;
 use Mautic\CoreBundle\Doctrine\Loader\FixturesLoaderInterface;
@@ -13,6 +12,7 @@ use Mautic\CoreBundle\Helper\CacheHelper;
 use Mautic\CoreBundle\Helper\PathsHelper;
 use Mautic\InstallBundle\Install\InstallService;
 use Mautic\UserBundle\Entity\User;
+use Mautic\UserBundle\Entity\UserRepository;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
@@ -53,6 +53,11 @@ final class InstallServiceTest extends \PHPUnit\Framework\TestCase
      */
     private MockObject $validator;
 
+    /**
+     * @var MockObject&UserRepository
+     */
+    private MockObject $userRepository;
+
     private InstallService $installer;
 
     protected function setUp(): void
@@ -65,6 +70,7 @@ final class InstallServiceTest extends \PHPUnit\Framework\TestCase
         $this->entityManager        = $this->createMock(EntityManager::class);
         $this->translator           = $this->createMock(TranslatorInterface::class);
         $this->validator            = $this->createMock(ValidatorInterface::class);
+        $this->userRepository       = $this->createMock(UserRepository::class);
 
         $this->installer = new InstallService(
             $this->configurator,
@@ -75,7 +81,8 @@ final class InstallServiceTest extends \PHPUnit\Framework\TestCase
             $this->createStub(KernelInterface::class),
             $this->validator,
             $this->createStub(UserPasswordHasher::class),
-            $this->createStub(FixturesLoaderInterface::class)
+            $this->createStub(FixturesLoaderInterface::class),
+            $this->userRepository
         );
     }
 
@@ -320,14 +327,9 @@ final class InstallServiceTest extends \PHPUnit\Framework\TestCase
 
     public function testCreateAdminUserStepWhenPasswordIsMissing(): void
     {
-        $mockRepo = $this->createMock(EntityRepository::class);
-        $mockRepo->expects($this->once())
+        $this->userRepository->expects($this->once())
             ->method('find')
-            ->willReturn(0);
-
-        $this->entityManager->expects($this->once())
-            ->method('getRepository')
-            ->willReturn($mockRepo);
+            ->willReturn(null);
 
         $data = [
             'firstname' => 'Demo',
@@ -341,14 +343,9 @@ final class InstallServiceTest extends \PHPUnit\Framework\TestCase
 
     public function testCreateAdminUserStepWhenPasswordIsNotLongEnough(): void
     {
-        $mockRepo = $this->createMock(EntityRepository::class);
-        $mockRepo->expects($this->once())
+        $this->userRepository->expects($this->once())
             ->method('find')
             ->willReturn(new User());
-
-        $this->entityManager->expects($this->once())
-            ->method('getRepository')
-            ->willReturn($mockRepo);
 
         $data = [
             'firstname' => 'Demo',
