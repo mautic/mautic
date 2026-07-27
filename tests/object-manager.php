c5154d9c2c56f884149597c7a1bdce30c3cbdb79
Israel <123495355+israeldatascience@users.noreply.github.com>
2026-07-27 20:42:15 -0300

    Remove obsolete PHPStan Doctrine workaround
    Removed unused event manager and related code.

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -1,8 +1,6 @@
 <?php
 
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Events;
-use Doctrine\ORM\Tools\ResolveTargetEntityListener;
 use Mautic\CoreBundle\ErrorHandler\ErrorHandler;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -23,20 +21,5 @@ $container = $kernel->getContainer();
 
 /** @var EntityManager $objectManager */
 $objectManager = $container->get('doctrine')->getManager();
-
-$eventManager = $objectManager->getEventManager();
-
-// Workaround for https://github.com/phpstan/phpstan-doctrine/issues/98
-$resolveTargetEntityListener = \current(\array_filter(
-    $eventManager->getListeners(Events::loadClassMetadata),
-    static fn ($listener): bool => $listener instanceof ResolveTargetEntityListener,
-));
-
-if (false !== $resolveTargetEntityListener) {
-    $eventManager->removeEventListener(
-        [Events::loadClassMetadata],
-        $resolveTargetEntityListener
-    );
-}
 
 return $objectManager;
