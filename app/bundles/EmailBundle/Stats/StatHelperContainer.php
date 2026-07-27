f99d29413a70aad2ab2dd940d86f41c43c5e8d73
Tomas Votruba <tomas.vot@gmail.com>
2026-07-27 23:58:52 +0200

    [di] flip compiler pass to #[AutowireIterator]

--- app/bundles/EmailBundle/Stats/StatHelperContainer.php
+++ app/bundles/EmailBundle/Stats/StatHelperContainer.php
@@ -13,6 +13,7 @@ class StatHelperContainer
      */
     private array $helpers = [];
 
+<<<<<<< HEAD
     public function __construct(
         #[AutowireIterator(tag: 'mautic.email_stat_helper')]
         iterable $helpersIterator,
@@ -20,6 +21,23 @@ class StatHelperContainer
         foreach ($helpersIterator as $helper) {
             $this->helpers[$helper->getName()] = $helper;
         }
+=======
+    /**
+     * @param iterable<StatHelperInterface> $statHelpers
+     */
+    public function __construct(
+        #[AutowireIterator('mautic.email_stat_helper')]
+        iterable $statHelpers = [],
+    ) {
+        foreach ($statHelpers as $statHelper) {
+            $this->addHelper($statHelper);
+        }
+    }
+
+    private function addHelper(StatHelperInterface $helper): void
+    {
+        $this->helpers[$helper->getName()] = $helper;
+>>>>>>> bcbf4a0307 ([di] flip compiler pass to #[AutowireIterator])
     }
 
     /**
