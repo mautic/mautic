d5b898e08a251ec4aeb237da4b045fd59ac265f6
Anderson J. Eccel <116097999+andersonjeccel@users.noreply.github.com>
2026-07-28 07:52:24 -0300

    fix phpstan

--- app/bundles/FormBundle/Controller/FormController.php
+++ app/bundles/FormBundle/Controller/FormController.php
@@ -53,7 +53,6 @@ class FormController extends CommonFormController
         private readonly \Mautic\FormBundle\Entity\SubmissionRepository $submissionRepository,
         private readonly \Mautic\FormBundle\Entity\FormRepository $formRepository,
     ) {
-        // @phpstan-ignore-next-line FormController extends deprecated AbstractStandardFormController; fix requires class hierarchy refactoring
         parent::__construct($formFactory, $fieldHelper, $doctrine, $modelFactory, $userHelper, $coreParametersHelper, $dispatcher, $translator, $flashBag, $requestStack, $security);
     }
 
