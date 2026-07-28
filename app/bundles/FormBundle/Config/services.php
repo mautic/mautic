95cabd831712dd0beb105fcb4d3a577a5b4d09cb
Tomas Votruba <tomas.vot@gmail.com>
2026-07-28 11:07:03 +0200

    [FormBundle] flip 13 services from config to autowired services.php

--- app/bundles/FormBundle/Config/services.php
+++ app/bundles/FormBundle/Config/services.php
@@ -22,6 +22,32 @@ return function (ContainerConfigurator $configurator): void {
 
     $services->load('Mautic\\FormBundle\\Entity\\', '../Entity/*Repository.php')
         ->tag(Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\ServiceRepositoryCompilerPass::REPOSITORY_SERVICE_TAG);
+    $services->set('mautic.form.collector.object', Mautic\FormBundle\Collector\ObjectCollector::class);
+    $services->alias(Mautic\FormBundle\Collector\ObjectCollector::class, 'mautic.form.collector.object');
+    $services->set('mautic.form.collector.field', Mautic\FormBundle\Collector\FieldCollector::class);
+    $services->alias(Mautic\FormBundle\Collector\FieldCollector::class, 'mautic.form.collector.field');
+    $services->set('mautic.form.collector.mapped.object', Mautic\FormBundle\Collector\MappedObjectCollector::class);
+    $services->alias(Mautic\FormBundle\Collector\MappedObjectCollector::class, 'mautic.form.collector.mapped.object');
+    $services->set('mautic.form.collector.already.mapped.field', Mautic\FormBundle\Collector\AlreadyMappedFieldCollector::class);
+    $services->alias(Mautic\FormBundle\Collector\AlreadyMappedFieldCollector::class, 'mautic.form.collector.already.mapped.field');
+    $services->set('mautic.helper.form.field_helper', Mautic\FormBundle\Helper\FormFieldHelper::class);
+    $services->alias(Mautic\FormBundle\Helper\FormFieldHelper::class, 'mautic.helper.form.field_helper');
+    $services->set('mautic.form.helper.form_uploader', Mautic\FormBundle\Helper\FormUploader::class);
+    $services->alias(Mautic\FormBundle\Helper\FormUploader::class, 'mautic.form.helper.form_uploader');
+    $services->set('mautic.form.helper.token', Mautic\FormBundle\Helper\TokenHelper::class);
+    $services->alias(Mautic\FormBundle\Helper\TokenHelper::class, 'mautic.form.helper.token');
+    $services->set('mautic.form.service.field.value.transformer', Mautic\FormBundle\Event\Service\FieldValueTransformer::class);
+    $services->alias(Mautic\FormBundle\Event\Service\FieldValueTransformer::class, 'mautic.form.service.field.value.transformer');
+    $services->set('mautic.form.helper.properties.accessor', Mautic\FormBundle\Helper\PropertiesAccessor::class);
+    $services->alias(Mautic\FormBundle\Helper\PropertiesAccessor::class, 'mautic.form.helper.properties.accessor');
+    $services->set('mautic.form.validator.upload_field_validator', Mautic\FormBundle\Validator\UploadFieldValidator::class);
+    $services->alias(Mautic\FormBundle\Validator\UploadFieldValidator::class, 'mautic.form.validator.upload_field_validator');
+    $services->set('mautic.form.validator.constraint.file_extension_constraint_validator', Mautic\FormBundle\Validator\Constraint\FileExtensionConstraintValidator::class)->tag('validator.constraint_validator')->tag('file_extension_constraint_validator');
+    $services->alias(Mautic\FormBundle\Validator\Constraint\FileExtensionConstraintValidator::class, 'mautic.form.validator.constraint.file_extension_constraint_validator');
+    $services->set('mautic.form.command.form_submissions_records_clean', Mautic\FormBundle\Command\DeleteOrphanSubmissionRecordsFromFormResultsTableCommand::class)->tag('console.command');
+    $services->alias(Mautic\FormBundle\Command\DeleteOrphanSubmissionRecordsFromFormResultsTableCommand::class, 'mautic.form.command.form_submissions_records_clean');
+    $services->set('mautic.form.command.form_submissions_table_clean', Mautic\FormBundle\Command\DeleteOrphanFormResultsTableCommand::class)->tag('console.command');
+    $services->alias(Mautic\FormBundle\Command\DeleteOrphanFormResultsTableCommand::class, 'mautic.form.command.form_submissions_table_clean');
 
     $services->alias('mautic.form.model.action', Mautic\FormBundle\Model\ActionModel::class);
     $services->alias('mautic.form.model.field', Mautic\FormBundle\Model\FieldModel::class);
