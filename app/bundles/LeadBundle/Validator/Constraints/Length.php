f0d283698e2afe82bf1325e3363aefc9ceb63d62
Tomas Votruba <tomas.vot@gmail.com>
2026-07-30 21:47:34 +0200

    refactor: mark validators without children as final

--- app/bundles/LeadBundle/Validator/Constraints/Length.php
+++ app/bundles/LeadBundle/Validator/Constraints/Length.php
@@ -5,7 +5,7 @@ namespace Mautic\LeadBundle\Validator\Constraints;
 use Symfony\Component\Validator\Constraints\Length as SymfonyLength;
 
 #[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::TARGET_CLASS)]
-class Length extends SymfonyLength
+final class Length extends SymfonyLength
 {
     public function validatedBy(): string
     {
