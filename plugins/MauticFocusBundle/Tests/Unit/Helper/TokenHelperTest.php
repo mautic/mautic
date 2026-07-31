29b971bc0f1ed29537c79b2ee84eecb1e0b5ef49
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2026-07-31 08:13:35 +0200

    MTC-11100 test {focus=id|display} token

--- plugins/MauticFocusBundle/Tests/Unit/Helper/TokenHelperTest.php
+++ plugins/MauticFocusBundle/Tests/Unit/Helper/TokenHelperTest.php
@@ -10,6 +10,7 @@ use MauticPlugin\MauticFocusBundle\Helper\TokenHelper;
 use MauticPlugin\MauticFocusBundle\Model\FocusModel;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 
 final class TokenHelperTest extends TestCase
@@ -64,6 +65,34 @@ final class TokenHelperTest extends TestCase
             ->willReturn($focusItem);
 
         $this->assertSame(['{focus=1}' => '<script src="" type="text/javascript" charset="utf-8" async="async"></script>'], $this->helper->findFocusTokens($content));
+    }
+
+    public function testFindFocusTokensDisplayModeUsesDisplayEndpoint(): void
+    {
+        $focusItem = new Focus();
+        $focusItem->setIsPublished(true);
+
+        $this->model->expects($this->once())
+            ->method('getEntity')
+            ->with(1)
+            ->willReturn($focusItem);
+
+        $router = $this->createMock(RouterInterface::class);
+        $router->expects($this->once())
+            ->method('generate')
+            ->with(
+                'mautic_focus_generate_display',
+                ['id' => 1],
+                UrlGeneratorInterface::ABSOLUTE_URL,
+            )
+            ->willReturn('https://example.test/focus/1/display.js');
+
+        $helper = new TokenHelper($this->model, $router, $this->security);
+
+        $this->assertSame(
+            ['{focus=1|display}' => '<script src="https://example.test/focus/1/display.js" type="text/javascript" charset="utf-8" async="async"></script>'],
+            $helper->findFocusTokens('content {focus=1|display}'),
+        );
     }
 
     public function testFindFocusTokensFoundAddScriptByAccessCheck(): void
