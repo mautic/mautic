321eae9670cef8420cc20e04b99752e90d822b87
Tomas Votruba <tomas.vot@gmail.com>
2026-08-04 19:07:42 +0200

    [validator] make 4 constraints usable as attributes with named arguments

--- app/bundles/DynamicContentBundle/Validator/Constraints/NoNesting.php
+++ app/bundles/DynamicContentBundle/Validator/Constraints/NoNesting.php
@@ -4,12 +4,28 @@ declare(strict_types=1);
 
 namespace Mautic\DynamicContentBundle\Validator\Constraints;
 
+use Symfony\Component\Validator\Attribute\HasNamedArguments;
 use Symfony\Component\Validator\Constraint;
 
 /**
  * This constraint checks if the content does not include another DWC token.
  */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 final class NoNesting extends Constraint
 {
-    public string $message = 'mautic.dynamicContent.no_nesting';
+    public string $message;
+
+    /**
+     * @param string[]|null $groups
+     */
+    #[HasNamedArguments]
+    public function __construct(
+        string $message = 'mautic.dynamicContent.no_nesting',
+        ?array $groups = null,
+        mixed $payload = null,
+    ) {
+        parent::__construct(null, $groups, $payload);
+
+        $this->message = $message;
+    }
 }
