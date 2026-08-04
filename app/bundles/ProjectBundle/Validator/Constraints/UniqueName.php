96b0c28fb5b67948f6b33041dbaf456a28e8c5b5
Tomas Votruba <tomas.vot@gmail.com>
2026-08-04 19:07:42 +0200

    [validator] make 4 constraints usable as attributes with named arguments

--- app/bundles/ProjectBundle/Validator/Constraints/UniqueName.php
+++ app/bundles/ProjectBundle/Validator/Constraints/UniqueName.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 namespace Mautic\ProjectBundle\Validator\Constraints;
 
+use Symfony\Component\Validator\Attribute\HasNamedArguments;
 use Symfony\Component\Validator\Constraint;
 
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
 final class UniqueName extends Constraint
 {
-    public string $message = 'project.name.already.exists';
+    public string $message;
+
+    /**
+     * @param string[]|null $groups
+     */
+    #[HasNamedArguments]
+    public function __construct(
+        string $message = 'project.name.already.exists',
+        ?array $groups = null,
+        mixed $payload = null,
+    ) {
+        parent::__construct(null, $groups, $payload);
+
+        $this->message = $message;
+    }
 
     public function getTargets(): string
     {
