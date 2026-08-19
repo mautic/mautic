ae52fc840f84b1b523e9d1c4f08dff69f9c807b8
Tomas Votruba <tomas.vot@gmail.com>
2026-08-19 15:18:19 +0200

    remove menu logic from ServicePass

--- app/bundles/CoreBundle/DependencyInjection/Compiler/ServicePass.php
+++ app/bundles/CoreBundle/DependencyInjection/Compiler/ServicePass.php
@@ -15,33 +15,5 @@ final class ServicePass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
-        // Store menu renderer options to create unique renderering classes per menu
-        // since KNP menus doesn't seem to support a Renderer factory
-        $menus = [];
-
-        // Setup default menu details
-        if ('menus' == $type) {
-            $details = array_merge(
-                [
-                    'class'   => MenuItem::class,
-                    'factory' => ['@mautic.menu.builder', $details['alias'].'Menu'],
-                ],
-                $details
-            );
-
-            $menus[$details['alias']] = $details['options'] ?? [];
-        }
-
-        foreach ($menus as $alias => $options) {
-            $container->setDefinition('mautic.menu_renderer.'.$alias, new Definition(
-                MenuRenderer::class,
-                [
-                    new Reference('knp_menu.matcher'),
-                    new Reference('twig'),
-                    $options,
-                ]
-            ))
-                ->addTag('knp_menu.renderer', ['alias' => $alias]);
-        }
     }
 }
