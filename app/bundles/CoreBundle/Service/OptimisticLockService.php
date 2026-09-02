78ae2809313cf0c62dd1445e1ad6b1162dfd97a6
Pavel Aleksandrov <skaalex92@gmail.com>
2026-09-02 10:14:10 +0300

    refactor(campaigns): keep existing lock implementation

--- app/bundles/CoreBundle/Service/OptimisticLockService.php
+++ app/bundles/CoreBundle/Service/OptimisticLockService.php
@@ -39,21 +39,9 @@ final class OptimisticLockService implements OptimisticLockServiceInterface
 
     public function acquireLock(OptimisticLockInterface $entity, int $expectedVersion = OptimisticLockInterface::INITIAL_VERSION): bool
     {
-        $newVersion   = $expectedVersion + 1;
-        $affectedRows = $this->buildUpdateQuery($entity, $versionColumn)
-            ->set($versionColumn, ':newVersion')
-            ->andWhere($versionColumn.' = :expectedVersion')
-            ->setParameter('newVersion', $newVersion)
-            ->setParameter('expectedVersion', $expectedVersion)
-            ->executeStatement();
+        $this->incrementVersion($entity);
 
-        if (1 !== $affectedRows) {
-            return false;
-        }
-
-        $entity->setVersion($newVersion);
-
-        return true;
+        return ++$expectedVersion === $entity->getVersion();
     }
 
     private function buildUpdateQuery(OptimisticLockInterface $entity, ?string &$versionColumn = null): QueryBuilder // @phpstan-ignore parameterByRef.unusedType
