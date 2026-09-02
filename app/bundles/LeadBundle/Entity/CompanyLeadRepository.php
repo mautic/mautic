81d2c73febca91060fc47bea37ecc5ed6bbb6813
rahuld-dev <rahul.dhande@acquia.com>
2026-09-02 17:12:46 +0530

    fix error

--- app/bundles/LeadBundle/Entity/CompanyLeadRepository.php
+++ app/bundles/LeadBundle/Entity/CompanyLeadRepository.php
@@ -77,31 +77,6 @@ class CompanyLeadRepository extends CommonRepository
     }
 
     /**
-     * @param int[] $ids
-     *
-     * @return list<array<string, mixed>>
-     */
-    public function getPrimaryCompaniesByLeadIds(array $ids): array
-    {
-        $ids = array_filter($ids);
-
-        if ([] === $ids) {
-            return [];
-        }
-
-        $q = $this->_em->getConnection()->createQueryBuilder();
-
-        $q->select('comp.*')
-            ->from(MAUTIC_TABLE_PREFIX.'companies', 'comp')
-            ->join('comp', MAUTIC_TABLE_PREFIX.'companies_leads', 'cl', 'cl.company_id = comp.id')
-            ->andWhere('cl.is_primary = 1')
-            ->andWhere('cl.lead_id IN (:ids)')
-            ->setParameter('ids', $ids, ArrayParameterType::INTEGER);
-
-        return $q->executeQuery()->fetchAllAssociative();
-    }
-
-    /**
      * @return mixed[]
      *
      * @throws PrimaryCompanyNotFoundException
