3765357ee6bf5df4626c2dc1d0e7078a14eca1d6
Tomas Votruba <tomas.vot@gmail.com>
2026-09-03 00:20:08 +0200

    remove unused EmailModel::getCopyRepository() service getter

--- app/bundles/EmailBundle/Model/EmailModel.php
+++ app/bundles/EmailBundle/Model/EmailModel.php
@@ -165,11 +165,6 @@ class EmailModel extends FormModel implements AjaxLookupModelInterface, GlobalSe
         return $this->emailStatModel->getRepository();
     }
 
-    public function getCopyRepository(): CopyRepository
-    {
-        return $this->copyRepository;
-    }
-
     public function getStatDeviceRepository(): StatDeviceRepository
     {
         return $this->statDeviceRepository;
