c850bb2e71fd7cf20c6ed051dffc05910c2675b3
Tomas Votruba <tomas.vot@gmail.com>
2026-09-08 13:28:57 +0200

    cs

--- app/bundles/CampaignBundle/Config/services.php
+++ app/bundles/CampaignBundle/Config/services.php
@@ -28,7 +28,7 @@ return function (ContainerConfigurator $configurator): void {
 
     $services->set(Mautic\CampaignBundle\Executioner\ScheduledExecutioner::class)->tag('kernel.reset', ['method' => 'reset']);
 
-    $services->set(\Mautic\CampaignBundle\Service\CampaignShareService::class);
+    $services->set(Mautic\CampaignBundle\Service\CampaignShareService::class);
 
     if ('test' === ($_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? 'prod')) {
         $services->set(Mautic\CampaignBundle\Executioner\TestInactiveExecutioner::class)
