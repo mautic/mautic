97b40e53f57278b6234b82a5cb274bb311f4c404
Tomas Votruba <tomas.vot@gmail.com>
2026-09-08 17:34:33 +0200

    [phpstan] forbid readonly on Doctrine entity classes
    Revert readonly wrongly applied to EmailReply entity and add a PHPStan
    rule that flags readonly entity classes, detected by a public
    loadMetadata() method.

---
+++ utils/phpstan/tests/Rule/Fixture/NonReadonlyEntity.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils\PHPStan\Tests\Rule\Fixture;
+
+use Doctrine\ORM\Mapping\ClassMetadata;
+
+class NonReadonlyEntity
+{
+    public static function loadMetadata(ClassMetadata $metadata): void
+    {
+    }
+}
