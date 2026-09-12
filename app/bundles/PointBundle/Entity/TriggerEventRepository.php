fc2254c95b534a0941dbbda828cdaa8218ebf6eb
Tomas Votruba <tomas.vot@gmail.com>
2026-09-12 10:45:54 +0200

    Remove unused import after docblock change

--- app/bundles/PointBundle/Entity/TriggerEventRepository.php
+++ app/bundles/PointBundle/Entity/TriggerEventRepository.php
@@ -2,7 +2,6 @@
 
 namespace Mautic\PointBundle\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Mautic\CoreBundle\Entity\CommonRepository;
 
