6a6ea7fefeec365568921d4704495ce804ac4056
Tomas Votruba <tomas.vot@gmail.com>
2026-09-16 11:44:35 +0200

    add return type declarations to TrackableRepository public methods

--- app/bundles/PageBundle/Entity/TrackableRepository.php
+++ app/bundles/PageBundle/Entity/TrackableRepository.php
@@ -38,10 +38,8 @@ class TrackableRepository extends CommonRepository
 
     /**
      * Get a Trackable by Redirect URL.
-     *
-     * @return array
      */
-    public function findByUrl($url, $channel, $channelId)
+    public function findByUrl($url, $channel, $channelId): ?Trackable
     {
         $alias = $this->getTableAlias();
         $q     = $this->createQueryBuilder($alias)
@@ -64,10 +62,8 @@ class TrackableRepository extends CommonRepository
 
     /**
      * Get an array of Trackable entities by Redirect URLs.
-     *
-     * @return array
      */
-    public function findByUrls(array $urls, $channel, $channelId)
+    public function findByUrls(array $urls, $channel, $channelId): array
     {
         $alias = $this->getTableAlias();
         $q     = $this->createQueryBuilder($alias)
@@ -115,10 +111,8 @@ class TrackableRepository extends CommonRepository
      * Get hit count.
      *
      * @param string $countColumn
-     *
-     * @return array|int
      */
-    public function getCount($channel, $channelIds, $listId, ?ChartQuery $chartQuery = null, bool $combined = false, $countColumn = 'ph.id')
+    public function getCount($channel, $channelIds, $listId, ?ChartQuery $chartQuery = null, bool $combined = false, $countColumn = 'ph.id'): array|int
     {
         $q = $this->getEntityManager()->getConnection()->createQueryBuilder()
             ->select('count('.$countColumn.') as click_count')
