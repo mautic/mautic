be6f6da2ce4a75fedf53a4ce349c71382aba2f8d
Tomas Votruba <tomas.vot@gmail.com>
2026-09-16 11:44:36 +0200

    add return type declarations to SmsRepository public methods

--- app/bundles/SmsBundle/Entity/SmsRepository.php
+++ app/bundles/SmsBundle/Entity/SmsRepository.php
@@ -19,7 +19,7 @@ class SmsRepository extends CommonRepository
      *
      * @return Paginator
      */
-    public function getEntities(array $args = [])
+    public function getEntities(array $args = []): iterable
     {
         $q = $this->getEntityManager()
             ->createQueryBuilder()
@@ -62,10 +62,7 @@ class SmsRepository extends CommonRepository
         return $qb->getQuery();
     }
 
-    /**
-     * @return \Doctrine\DBAL\Query\QueryBuilder
-     */
-    public function getSegmentsContactsQuery(int $smsId)
+    public function getSegmentsContactsQuery(int $smsId): \Doctrine\DBAL\Query\QueryBuilder
     {
         // Main query
         $q = $this->getEntityManager()->getConnection()->createQueryBuilder();
@@ -87,10 +84,8 @@ class SmsRepository extends CommonRepository
 
     /**
      * Get amounts of sent and read emails.
-     *
-     * @return array
      */
-    public function getSentCount()
+    public function getSentCount(): array
     {
         $q = $this->getEntityManager()->createQueryBuilder();
         $q->select('SUM(e.sentCount) as sent_count')
