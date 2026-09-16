0d929ee748cedd4416e92ae1701df7a49f40106a
Tomas Votruba <tomas.vot@gmail.com>
2026-09-16 11:44:34 +0200

    add return type declarations to SubmissionRepository public methods

--- app/bundles/FormBundle/Entity/SubmissionRepository.php
+++ app/bundles/FormBundle/Entity/SubmissionRepository.php
@@ -45,7 +45,7 @@ class SubmissionRepository extends CommonRepository
         }
     }
 
-    public function getEntities(array $args = [])
+    public function getEntities(array $args = []): array
     {
         $form = $args['form'];
 
@@ -274,12 +274,10 @@ class SubmissionRepository extends CommonRepository
      *
      * @param array<string, mixed> $options
      *
-     * @return array
-     *
      * @throws \Doctrine\ORM\NoResultException
      * @throws \Doctrine\ORM\NonUniqueResultException
      */
-    public function getSubmissions(array $options = [])
+    public function getSubmissions(array $options = []): array
     {
         $query = $this->getEntityManager()->getConnection()->createQueryBuilder();
         $query->select('fs.id, f.name, fs.form_id, fs.page_id, fs.date_submitted AS "dateSubmitted", fs.lead_id')
