42bacebc04aee919e528f46624f3a5e97e9b6ca7
Tomas Votruba <tomas.vot@gmail.com>
2026-09-17 21:11:15 +0200

    remove method from contract

--- app/bundles/CoreBundle/Entity/UuidInterface.php
+++ app/bundles/CoreBundle/Entity/UuidInterface.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Mautic\CoreBundle\Entity;
 
-use Mautic\CoreBundle\Doctrine\Mapping\ClassMetadataBuilder;
 
 interface UuidInterface
 {
     public function getUuid(): ?string;
 
     public function setUuid(string $uuid): void;
-
-    public static function addUuidField(ClassMetadataBuilder $builder): void;
 }
