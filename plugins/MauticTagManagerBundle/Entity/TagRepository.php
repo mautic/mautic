7bf8d02fbbf86f0fbe359ff81131d880f54f7520
Tomas Votruba <tomas.vot@gmail.com>
2026-09-17 12:36:21 +0200

    Update plugins/MauticTagManagerBundle/Entity/TagRepository.php
    Co-authored-by: Martin Vooremäe <martin.vooremae@gmail.com>

--- plugins/MauticTagManagerBundle/Entity/TagRepository.php
+++ plugins/MauticTagManagerBundle/Entity/TagRepository.php
@@ -46,7 +46,7 @@ final class TagRepository extends BaseTagRepository
      *
      * @return int|array<int, int>
      */
-    public function countByLeads($tagIds): mixed
+    public function countByLeads($tagIds): int|array
     {
         $q = $this->getEntityManager()->getConnection()->createQueryBuilder();
 
