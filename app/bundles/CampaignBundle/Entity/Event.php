8947f6519ea7ac319977eabb4e6101b63995440a
Tomas Votruba <tomas.vot@gmail.com>
2026-09-17 16:32:13 +0200

    [types] add known types to CampaignBundle event classes

--- app/bundles/CampaignBundle/Entity/Event.php
+++ app/bundles/CampaignBundle/Entity/Event.php
@@ -459,11 +459,7 @@ class Event implements ChannelInterface, UuidInterface
     {
         $this->id = null;
     }
-
-    /**
-     * @param int $order
-     */
-    public function setOrder($order): static
+    public function setOrder(int $order): static
     {
         $this->isChanged('order', $order);
 
@@ -479,11 +475,7 @@ class Event implements ChannelInterface, UuidInterface
     {
         return $this->order;
     }
-
-    /**
-     * @param array $properties
-     */
-    public function setProperties($properties): static
+    public function setProperties(array|null $properties): static
     {
         $this->isChanged('properties', $properties);
 
@@ -514,11 +506,7 @@ class Event implements ChannelInterface, UuidInterface
     {
         return $this->campaign;
     }
-
-    /**
-     * @param string $type
-     */
-    public function setType($type): static
+    public function setType(string $type): static
     {
         $this->isChanged('type', $type);
         $this->type = $type;
@@ -539,10 +527,7 @@ class Event implements ChannelInterface, UuidInterface
         return get_object_vars($this);
     }
 
-    /**
-     * @param string $description
-     */
-    public function setDescription($description): static
+    public function setDescription(?string $description): static
     {
         $this->isChanged('description', $description);
         $this->description = $description;
@@ -550,18 +535,11 @@ class Event implements ChannelInterface, UuidInterface
         return $this;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getDescription()
+    public function getDescription(): ?string
     {
         return $this->description;
     }
-
-    /**
-     * @param string $name
-     */
-    public function setName($name): static
+    public function setName(string $name): static
     {
         $this->isChanged('name', $name);
         $this->name = $name;
@@ -679,11 +657,9 @@ class Event implements ChannelInterface, UuidInterface
     }
 
     /**
-     * @param string $type
-     *
      * @return ArrayCollection<int,Event>
      */
-    public function getChildrenByEventType($type)
+    public function getChildrenByEventType(string $type)
     {
         $criteria = Criteria::create()->where(Criteria::expr()->eq('eventType', $type));
 
@@ -739,11 +715,7 @@ class Event implements ChannelInterface, UuidInterface
     {
         return $this->triggerInterval;
     }
-
-    /**
-     * @param int $triggerInterval
-     */
-    public function setTriggerInterval($triggerInterval): void
+    public function setTriggerInterval(int $triggerInterval): void
     {
         $this->isChanged('triggerInterval', $triggerInterval);
         $this->triggerInterval = $triggerInterval;
@@ -758,9 +730,9 @@ class Event implements ChannelInterface, UuidInterface
     }
 
     /**
-     * @param \DateTime|string|array<string,string> $triggerHour
+     * @param array<string,string>|string|null $triggerHour
      */
-    public function setTriggerHour($triggerHour): static
+    public function setTriggerHour(array|string|null $triggerHour): static
     {
         $triggerHour = $this->convertToDateTime($triggerHour);
         $this->isChanged('triggerHour', $triggerHour ? $triggerHour->format('H:i') : $triggerHour);
@@ -777,10 +749,7 @@ class Event implements ChannelInterface, UuidInterface
         return $this->triggerIntervalUnit;
     }
 
-    /**
-     * @param mixed $triggerIntervalUnit
-     */
-    public function setTriggerIntervalUnit($triggerIntervalUnit): void
+    public function setTriggerIntervalUnit(string|null $triggerIntervalUnit): void
     {
         $this->isChanged('triggerIntervalUnit', $triggerIntervalUnit);
         $this->triggerIntervalUnit = $triggerIntervalUnit;
@@ -794,7 +763,7 @@ class Event implements ChannelInterface, UuidInterface
         return $this->eventType;
     }
 
-    public function setEventType($eventType): static
+    public function setEventType(string $eventType): static
     {
         $this->isChanged('eventType', $eventType);
         $this->eventType = $eventType;
@@ -814,61 +783,40 @@ class Event implements ChannelInterface, UuidInterface
         return $this;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getTriggerMode()
+    public function getTriggerMode(): ?string
     {
         return $this->triggerMode;
     }
 
-    /**
-     * @param mixed $triggerMode
-     */
-    public function setTriggerMode($triggerMode): void
+    public function setTriggerMode(?string $triggerMode): void
     {
         $this->isChanged('triggerMode', $triggerMode);
         $this->triggerMode = $triggerMode;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getDecisionPath()
+    public function getDecisionPath(): ?string
     {
         return $this->decisionPath;
     }
 
-    /**
-     * @param mixed $decisionPath
-     */
-    public function setDecisionPath($decisionPath): void
+    public function setDecisionPath(?string $decisionPath): void
     {
         $this->isChanged('decisionPath', $decisionPath);
         $this->decisionPath = $decisionPath;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getTempId()
+    public function getTempId(): ?string
     {
         return $this->tempId;
     }
 
-    /**
-     * @param mixed $tempId
-     */
-    public function setTempId($tempId): void
+    public function setTempId(?string $tempId): void
     {
         $this->isChanged('tempId', $tempId);
         $this->tempId = $tempId;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getChannel()
+    public function getChannel(): ?string
     {
         return $this->channel;
     }
@@ -952,10 +900,8 @@ class Event implements ChannelInterface, UuidInterface
 
     /**
      * Set the value of triggerRestrictedStartHour.
-     *
-     * @param \DateTime|string|array<string,string>|null $triggerRestrictedStartHour
      */
-    public function setTriggerRestrictedStartHour($triggerRestrictedStartHour): static
+    public function setTriggerRestrictedStartHour(string|\DateTimeInterface|null $triggerRestrictedStartHour): static
     {
         $triggerRestrictedStartHour = $this->convertToDateTime($triggerRestrictedStartHour);
 
@@ -978,10 +924,8 @@ class Event implements ChannelInterface, UuidInterface
 
     /**
      * Set the value of triggerRestrictedStopHour.
-     *
-     * @param \DateTime|string|array<string,string>|null $triggerRestrictedStopHour
      */
-    public function setTriggerRestrictedStopHour($triggerRestrictedStopHour): static
+    public function setTriggerRestrictedStopHour(string|\DateTime|null $triggerRestrictedStopHour): static
     {
         $triggerRestrictedStopHour = $this->convertToDateTime($triggerRestrictedStopHour);
 
