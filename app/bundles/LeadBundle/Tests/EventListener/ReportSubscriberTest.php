8db42848ed737242ef31a8c1b5097c7d03190083
Tomas Votruba <tomas.vot@gmail.com>
2026-09-17 21:12:20 +0200

    checkContext mock

--- app/bundles/LeadBundle/Tests/EventListener/ReportSubscriberTest.php
+++ app/bundles/LeadBundle/Tests/EventListener/ReportSubscriberTest.php
@@ -925,7 +925,7 @@ final class ReportSubscriberTest extends \PHPUnit\Framework\TestCase
     {
         $this->reportBuilderEventMock
         ->method('checkContext')
-        ->willReturn($event);
+        ->willReturn(true);
 
         $this->fieldsBuilderMock
     ->method('getLeadFieldsColumns')
@@ -971,7 +971,9 @@ final class ReportSubscriberTest extends \PHPUnit\Framework\TestCase
 
         $this->reportDataEventMock->expects($this->once())
             ->method('checkContext')
-            ->willReturn($event);
+            ->with($event)
+            ->willReturn(true);
+
         $this->reportDataEventMock->expects($this->once())
             ->method('getData')
             ->willReturn([[
