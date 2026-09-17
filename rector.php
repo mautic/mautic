3a09449b3c987c136cf71f55b67eda96ef430a57
Tomas Votruba <tomas.vot@gmail.com>
2026-09-17 16:17:52 +0200

    Move entity mapped-superclass mapping to Doctrine attributes

--- rector.php
+++ rector.php
@@ -34,7 +34,7 @@ return RectorConfig::configure()
         \Utils\Rector\LoadMetadataRepositoryToDoctrineAttributeRector::class,
         \Utils\Rector\LoadMetadataIndexToDoctrineAttributeRector::class,
         \Utils\Rector\LoadMetadataLifecycleToDoctrineAttributeRector::class,
-        // \Utils\Rector\LoadMetadataClassToDoctrineAttributeRector::class,
+        \Utils\Rector\LoadMetadataClassToDoctrineAttributeRector::class,
         // \Utils\Rector\AddDoctrineOrmMappingAliasImportRector::class,
 
         Rector\PHPUnit\CodeQuality\Rector\ClassMethod\AssertClassToThisAssertRector::class,
