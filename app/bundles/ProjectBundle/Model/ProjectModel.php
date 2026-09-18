3824bcde634db33835586021aaa98983a25765e8
Aarohi Prasad <prasadaarohi.98@gmail.com>
2026-09-18 17:26:34 +0530

    changed type to ?int
    Co-authored-by: John Linhart <jan@linhart.email>

--- app/bundles/ProjectBundle/Model/ProjectModel.php
+++ app/bundles/ProjectBundle/Model/ProjectModel.php
@@ -68,7 +68,7 @@ final class ProjectModel extends FormModel implements AjaxLookupModelInterface
     }
 
     /**
-     * @param mixed $id
+     * @param ?int $id
      */
     public function getEntity($id = null): ?object
     {
