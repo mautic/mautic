6630b726581dd0a727722add094c5f78ebc088db
Tomas Votruba <tomas.vot@gmail.com>
2026-09-23 12:56:54 +0200

    nullable

--- app/bundles/EmailBundle/Entity/StatDevice.php
+++ app/bundles/EmailBundle/Entity/StatDevice.php
@@ -36,7 +36,7 @@ class StatDevice
 
     private ?IpAddress $ipAddress = null;
 
-    private \DateTimeInterface $dateOpened;
+    private ?\DateTimeInterface $dateOpened = null;
 
     public static function loadMetadata(ORM\ClassMetadata $metadata): void
     {
