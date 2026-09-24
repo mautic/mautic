9249bba72c5ac662c8a3c37f8a06a7fbf57b6cc2
John Linhart <jan@linhart.email>
2026-09-24 13:54:09 +0200

    Finishing updating OidcConfigTypeTest

--- app/bundles/UserBundle/Tests/Form/Type/OidcConfigTypeTest.php
+++ app/bundles/UserBundle/Tests/Form/Type/OidcConfigTypeTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Mautic\UserBundle\Tests\Form\Extension;
+namespace Mautic\UserBundle\Tests\Form\Type;
 
 use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\QueryBuilder;
@@ -10,9 +10,8 @@ use Mautic\CoreBundle\Helper\CoreParametersHelper;
 use Mautic\UserBundle\Entity\OidcSubjectIdRepository;
 use Mautic\UserBundle\Form\Type\ConfigType;
 use Mautic\UserBundle\Security\OIDC\ClientCredentials;
-use Mautic\UserBundle\Security\OIDC\ConfigTypeExtension;
+use Mautic\UserBundle\Security\OIDC\Settings;
 use Mautic\UserBundle\Tests\Security\OIDC\Builder\DTO\ParametersBuilder;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Form\Event\PostSubmitEvent;
@@ -24,30 +23,11 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class OidcConfigTypeTest extends TestCase
 {
-    private OidcSubjectIdRepository&MockObject $subjectIdRepository;
-
-    private ConfigType $configType;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->subjectIdRepository = $this->createStub(OidcSubjectIdRepository::class);
-
-        $this->configType = new ConfigType(
-            $this->createStub(CoreParametersHelper::class),
-            $this->createStub(TranslatorInterface::class),
-            (new ParametersBuilder())->build(),
-            new ClientCredentials('https://example.com', 'client_id', 'client_secret', 'sub'),
-            $this->subjectIdRepository,
-        );
-    }
-
     public function testOnPreSubmitSetsSecretToParamValueIfEmpty(): void
     {
         $form  = $this->createStub(FormInterface::class);
         $event = new PreSubmitEvent($form, ['open_id_client_secret' => '']);
-        $this->configType->onPreSubmit($event);
+        $this->createConfigForm()->onPreSubmit($event);
 
         $this->assertSame('client_secret', $event->getData()['open_id_client_secret']);
     }
@@ -56,20 +36,20 @@ final class OidcConfigTypeTest extends TestCase
     {
         $form  = $this->createStub(FormInterface::class);
         $event = new PreSubmitEvent($form, ['open_id_client_secret' => 'notNull']);
-        $this->configType->onPreSubmit($event);
+        $this->createConfigForm()->onPreSubmit($event);
 
         $this->assertSame('notNull', $event->getData()['open_id_client_secret']);
     }
 
     public function testOnPostEventTruncatesTableWhenMappingFieldIsChanged(): void
     {
-        $this->subjectIdRepository = $this->createMock(OidcSubjectIdRepository::class);
+        $subjectIdRepository = $this->createMock(OidcSubjectIdRepository::class);
         $queryBuilder = $this->createMock(QueryBuilder::class);
         $query        = $this->createMock(AbstractQuery::class);
         $form         = $this->createStub(FormInterface::class);
         $event        = new PostSubmitEvent($form, ['open_id_mapping_field' => 'platform_sub']);
 
-        $this->subjectIdRepository->expects($this->once())
+        $subjectIdRepository->expects($this->once())
             ->method('createQueryBuilder')
             ->willReturn($queryBuilder);
 
@@ -84,13 +64,11 @@ final class OidcConfigTypeTest extends TestCase
         $query->expects($this->once())
             ->method('execute');
 
-        $this->configType->onPostSubmit($event);
+        $this->createConfigForm($subjectIdRepository)->onPostSubmit($event);
     }
 
     public function testOnPostEventDoesNotTruncatesTableWhenMappingFieldIsUnchanged(): void
     {
-        $parameters          = (new ParametersBuilder())->build();
-        $clientCredentials   = new ClientCredentials('https://example.com', 'client_id', 'client_secret', 'sub');
         $subjectIdRepository = $this->createMock(OidcSubjectIdRepository::class);
         $form                = $this->createStub(FormInterface::class);
         $event               = new PostSubmitEvent($form, ['open_id_mapping_field' => 'sub']);
@@ -98,45 +76,36 @@ final class OidcConfigTypeTest extends TestCase
         $subjectIdRepository->expects($this->never())
             ->method('createQueryBuilder');
 
-        $configTypeExtension = new ConfigTypeExtension($parameters, $clientCredentials, $subjectIdRepository);
-        $configTypeExtension->onPostSubmit($event);
+        $this->createConfigForm($subjectIdRepository)->onPostSubmit($event);
     }
 
     public function testBuildFormAddsFieldsWhenOpenIdIsEnabled(): void
     {
-        $parameters          = (new ParametersBuilder())->build();
-        $clientCredentials   = new ClientCredentials('https://example.com', 'client_id', 'client_secret', 'sub');
-        $subjectIdRepository = $this->createStub(OidcSubjectIdRepository::class);
-        $eventDispatcher     = $this->createStub(EventDispatcherInterface::class);
-        $formFactory         = $this->createStub(FormFactoryInterface::class);
-        $builder             = new FormBuilder('config', ConfigType::class, $eventDispatcher, $formFactory);
-
-        $configTypeExtension = new ConfigTypeExtension($parameters, $clientCredentials, $subjectIdRepository);
-        $configTypeExtension->buildForm($builder, []);
-
-        $this->assertTrue($builder->has('open_id_is_enabled'));
-        $this->assertTrue($builder->has('open_id_is_required'));
-        $this->assertTrue($builder->has('open_id_is_user_registration_allowed'));
-        $this->assertTrue($builder->has('open_id_registered_user_role'));
-        $this->assertTrue($builder->has('open_id_client_url'));
-        $this->assertTrue($builder->has('open_id_client_id'));
-        $this->assertTrue($builder->has('open_id_client_secret'));
-        $this->assertTrue($builder->has('open_id_mapping_field'));
+        $builder = $this->createFormBuilder();
+        $this->createConfigForm()->buildForm($builder, []);
+        $this->assertOpenIdFieldsExist($builder);
     }
 
     // we always want to add the fields so the user can enable/disable OpenID
     public function testBuildFormAddsFieldsWhenOpenIdIsDisabled(): void
     {
-        $parameters          = (new ParametersBuilder())->withIsEnabled(false)->build();
-        $clientCredentials   = new ClientCredentials('https://example.com', 'client_id', 'client_secret', 'sub');
-        $subjectIdRepository = $this->createStub(OidcSubjectIdRepository::class);
-        $eventDispatcher     = $this->createStub(EventDispatcherInterface::class);
-        $formFactory         = $this->createStub(FormFactoryInterface::class);
-        $builder             = new FormBuilder('config', ConfigType::class, $eventDispatcher, $formFactory);
+        $builder = $this->createFormBuilder();
+        $this->createConfigForm(null, (new ParametersBuilder())->withIsEnabled(false)->build())->buildForm($builder, []);
+        $this->assertOpenIdFieldsExist($builder);
+    }
 
-        $configTypeExtension = new ConfigTypeExtension($parameters, $clientCredentials, $subjectIdRepository);
-        $configTypeExtension->buildForm($builder, []);
+    private function createFormBuilder(): FormBuilder
+    {
+        return new FormBuilder(
+            'config',
+            ConfigType::class,
+            $this->createStub(EventDispatcherInterface::class),
+            $this->createStub(FormFactoryInterface::class)
+        );
+    }
 
+    private function assertOpenIdFieldsExist(FormBuilder $builder): void
+    {
         $this->assertTrue($builder->has('open_id_is_enabled'));
         $this->assertTrue($builder->has('open_id_is_required'));
         $this->assertTrue($builder->has('open_id_is_user_registration_allowed'));
@@ -145,5 +114,20 @@ final class OidcConfigTypeTest extends TestCase
         $this->assertTrue($builder->has('open_id_client_id'));
         $this->assertTrue($builder->has('open_id_client_secret'));
         $this->assertTrue($builder->has('open_id_mapping_field'));
+    }
+
+    private function createConfigForm(?OidcSubjectIdRepository $subjectIdRepository = null, ?Settings $settings = null): ConfigType
+    {
+        $coreParameterHelper = $this->createStub(CoreParametersHelper::class);
+
+        $coreParameterHelper->method('get')->willReturn('some_value');
+
+        return new ConfigType(
+            $coreParameterHelper,
+            $this->createStub(TranslatorInterface::class),
+            $settings ?? (new ParametersBuilder())->build(),
+            new ClientCredentials('https://example.com', 'client_id', 'client_secret', 'sub'),
+            $subjectIdRepository ?? $this->createStub(OidcSubjectIdRepository::class),
+        );
     }
 }
